3d0c07b707ec0f2dc5f8729e40ddc503d9c287f8
Bradley Bell <btb@icculus.org>
2001-11-02 02:01:52 +0000

    fix swapint, swapshort macros, get rid of functions

--- include/byteswap.h
+++ include/byteswap.h
@@ -18,18 +18,23 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #include "pstypes.h"
 
+#if 0
 extern ushort swapshort(ushort s);
 extern uint swapint(uint i);
+#endif
 
 #define SWAPSHORT(x) (((ubyte)x << 8) | (((ushort)x) >> 8))
 #define SWAPINT(x)   ((x<<24) | (((ulong)x) >> 24) | ((x &0x0000ff00) << 8) | ((x & 0x00ff00000) >> 8))
 
-#ifndef MACINTOSH
+#ifndef BIGENDIAN //MACINTOSH
 #define INTEL_INT(x)	x
 #define INTEL_SHORT(x)	x
 #else
 #define INTEL_INT(x)	SWAPINT(x)
 #define INTEL_SHORT(x)	SWAPSHORT(x)
 #endif
+
+#define swapint(x)   INTEL_INT(x)
+#define swapshort(x) INTEL_SHORT(x)
 
 #endif
