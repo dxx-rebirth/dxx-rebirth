a7fe6cdb8394fb9e0bfab04209da6f9d81968f8f
Bradley Bell <btb@icculus.org>
2001-11-14 03:56:16 +0000

    SDL joystick stuff

--- arch/sdl/init.c
+++ arch/sdl/init.c
@@ -1,12 +1,15 @@
 /*
  * $Source: /cvs/cvsroot/d2x/arch/sdl/init.c,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  * $Author: bradleyb $
- * $Date: 2001-10-31 07:41:54 $
+ * $Date: 2001-11-14 03:56:16 $
  *
  * SDL architecture support
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.5  2001/10/31 07:41:54  bradleyb
+ * Sync with d1x
+ *
  * Revision 1.4  2001/10/19 09:45:02  bradleyb
  * Moved arch/sdl_* to arch/sdl
  *
@@ -40,7 +43,10 @@ void arch_sdl_init()
  // Initialise the library
 //edited on 01/03/99 by Matt Mueller - if we use SDL_INIT_EVERYTHING, cdrom is initialized even if -nocdaudio is used
  if (SDL_Init(
-#if 1 //SDL_VIDEO || SDL_GL
+#if 0 //def SDL_JOYSTICK
+	SDL_INIT_JOYSTICK |
+#endif
+#if defined(SDL_VIDEO) || defined(SDL_GL_VIDEO)
 	SDL_INIT_VIDEO
 #else
 	0
