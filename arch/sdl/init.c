5861d3c8bbc43a68341d0eb655427fc69646d59e
Bradley Bell <btb@icculus.org>
2001-12-03 02:45:02 +0000

    fix formatting

--- arch/sdl/init.c
+++ arch/sdl/init.c
@@ -1,12 +1,15 @@
 /*
  * $Source: /cvs/cvsroot/d2x/arch/sdl/init.c,v $
- * $Revision: 1.7 $
+ * $Revision: 1.8 $
  * $Author: bradleyb $
- * $Date: 2001-12-03 02:43:02 $
+ * $Date: 2001-12-03 02:45:02 $
  *
  * SDL architecture support
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.7  2001/12/03 02:43:02  bradleyb
+ * lots of makefile fixes, and sdl joystick stuff
+ *
  * Revision 1.6  2001/11/14 03:56:16  bradleyb
  * SDL joystick stuff
  *
@@ -43,26 +46,24 @@ void sdl_close()
 
 void arch_sdl_init()
 {
- // Initialise the library
-//edited on 01/03/99 by Matt Mueller - if we use SDL_INIT_EVERYTHING, cdrom is initialized even if -nocdaudio is used
- if (SDL_Init(
+	// Initialise the library
+	if (SDL_Init(
 #ifdef SDL_JOYSTICK
-	SDL_INIT_JOYSTICK |
+		SDL_INIT_JOYSTICK |
 #endif
 #if defined(SDL_VIDEO) || defined(SDL_GL_VIDEO)
-	SDL_INIT_VIDEO
+		SDL_INIT_VIDEO
 #else
-	0
+		0
 #endif
-	)<0) {
-//end edit -MM
-    Error("SDL library initialisation failed: %s.",SDL_GetError());
- }
+		)<0) {
+		Error("SDL library initialisation failed: %s.",SDL_GetError());
+	}
 #ifdef SDL_INPUT
- if (!FindArg("-nomouse"))
-  d_mouse_init();
+	if (!FindArg("-nomouse"))
+		d_mouse_init();
 #endif
- if (!FindArg("-nosound"))
-   digi_init();
- atexit(sdl_close);
+	if (!FindArg("-nosound"))
+		digi_init();
+	atexit(sdl_close);
 }
