0fcced3fdcd58ba7e662dddd0cabacc0bd5ca7ab
Bradley Bell <btb@icculus.org>
2002-08-06 04:49:43 +0000

    make pcx header available to other files, whitespace

--- 2d/pcx.c
+++ 2d/pcx.c
@@ -1,4 +1,4 @@
-/* $Id: pcx.c,v 1.4 2002-07-26 21:10:53 btb Exp $ */
+/* $Id: pcx.c,v 1.5 2002-08-06 04:49:43 btb Exp $ */
 /*
 THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
 SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
@@ -33,47 +33,6 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 int pcx_encode_byte(ubyte byt, ubyte cnt, FILE * fid);
 int pcx_encode_line(ubyte *inBuff, int inLen, FILE * fp);
-
-/* PCX Header data type */
-typedef struct	{
-	ubyte		Manufacturer;
-	ubyte		Version;
-	ubyte		Encoding;
-	ubyte		BitsPerPixel;
-	short		Xmin;
-	short		Ymin;
-	short		Xmax;
-	short		Ymax;
-	short		Hdpi;
-	short		Vdpi;
-	ubyte		ColorMap[16][3];
-	ubyte		Reserved;
-	ubyte		Nplanes;
-	short		BytesPerLine;
-	ubyte		filler[60];
-} PCXHeader;
-
-/*
- * reads a PCXHeader structure from a CFILE
- */
-static void PCXHeader_read(PCXHeader *ph, CFILE *fp)
-{
-	ph->Manufacturer = cfile_read_byte(fp);
-	ph->Version = cfile_read_byte(fp);
-	ph->Encoding = cfile_read_byte(fp);
-	ph->BitsPerPixel = cfile_read_byte(fp);
-	ph->Xmin = cfile_read_short(fp);
-	ph->Ymin = cfile_read_short(fp);
-	ph->Xmax = cfile_read_short(fp);
-	ph->Ymax = cfile_read_short(fp);
-	ph->Hdpi = cfile_read_short(fp);
-	ph->Vdpi = cfile_read_short(fp);
-	cfread(&ph->ColorMap, 16*3, 1, fp);
-	ph->Reserved = cfile_read_byte(fp);
-	ph->Nplanes = cfile_read_byte(fp);
-	ph->BytesPerLine = cfile_read_short(fp);
-	cfread(&ph->filler, 60, 1, fp);
-}
 
 int pcx_read_bitmap( char * filename, grs_bitmap * bmp,int bitmap_type ,ubyte * palette )
 {
@@ -116,13 +75,13 @@ int pcx_read_bitmap( char * filename, grs_bitmap * bmp,int bitmap_type ,ubyte * 
 			pixdata = &bmp->bm_data[bmp->bm_rowsize*row];
 			for (col=0; col< xsize ; )      {
 				if (cfread( &data, 1, 1, PCXfile )!=1 )	{
-					cfclose( PCXfile );	
+					cfclose( PCXfile );
 					return PCX_ERROR_READING;
 				}
 				if ((data & 0xC0) == 0xC0)     {
 					count =  data & 0x3F;
 					if (cfread( &data, 1, 1, PCXfile )!=1 )	{
-						cfclose( PCXfile );	
+						cfclose( PCXfile );
 						return PCX_ERROR_READING;
 					}
 					memset( pixdata, data, count );
@@ -138,13 +97,13 @@ int pcx_read_bitmap( char * filename, grs_bitmap * bmp,int bitmap_type ,ubyte * 
 		for (row=0; row< ysize ; row++)      {
 			for (col=0; col< xsize ; )      {
 				if (cfread( &data, 1, 1, PCXfile )!=1 )	{
-					cfclose( PCXfile );	
+					cfclose( PCXfile );
 					return PCX_ERROR_READING;
 				}
 				if ((data & 0xC0) == 0xC0)     {
 					count =  data & 0x3F;
 					if (cfread( &data, 1, 1, PCXfile )!=1 )	{
-						cfclose( PCXfile );	
+						cfclose( PCXfile );
 						return PCX_ERROR_READING;
 					}
 					for (i=0;i<count;i++)
@@ -171,7 +130,7 @@ int pcx_read_bitmap( char * filename, grs_bitmap * bmp,int bitmap_type ,ubyte * 
 					palette[i] >>= 2;
 			}
 		} else {
-			cfclose( PCXfile );	
+			cfclose( PCXfile );
 			return PCX_ERROR_NO_PALETTE;
 		}
 	}
@@ -214,7 +173,7 @@ int pcx_write_bitmap( char * filename, grs_bitmap * bmp, ubyte * palette )
 		}
 	}
 
-	// Mark an extended palette	
+	// Mark an extended palette
 	data = 12;
 	if (fwrite( &data, 1, 1, PCXfile )!=1)	{
 		fclose( PCXfile );
@@ -222,12 +181,12 @@ int pcx_write_bitmap( char * filename, grs_bitmap * bmp, ubyte * palette )
 	}
 
 	// Write the extended palette
-	for (i=0; i<768; i++ )	
+	for (i=0; i<768; i++ )
 		palette[i] <<= 2;
-	
+
 	retval = fwrite( palette, 768, 1, PCXfile );
 
-	for (i=0; i<768; i++ )	
+	for (i=0; i<768; i++ )
 		palette[i] >>= 2;
 
 	if (retval !=1)	{
@@ -240,15 +199,15 @@ int pcx_write_bitmap( char * filename, grs_bitmap * bmp, ubyte * palette )
 
 }
 
-// returns number of bytes written into outBuff, 0 if failed 
+// returns number of bytes written into outBuff, 0 if failed
 int pcx_encode_line(ubyte *inBuff, int inLen, FILE * fp)
-{  
+{
 	ubyte this, last;
 	int srcIndex, i;
 	register int total;
 	register ubyte runCount; 	// max single runlength is 63
 	total = 0;
-	last = *(inBuff);		
+	last = *(inBuff);
 	runCount = 1;
 
 	for (srcIndex = 1; srcIndex < inLen; srcIndex++) {
@@ -270,7 +229,7 @@ int pcx_encode_line(ubyte *inBuff, int inLen, FILE * fp)
 			last = this;
 			runCount = 1;
 		}
-	}	
+	}
 
 	if (runCount)	{		// finish up
 		if (!(i=pcx_encode_byte(last, runCount, fp)))
@@ -280,9 +239,9 @@ int pcx_encode_line(ubyte *inBuff, int inLen, FILE * fp)
 	return total;
 }
 
-// subroutine for writing an encoded byte pair 
+// subroutine for writing an encoded byte pair
 // returns count of bytes written, 0 if error
-int pcx_encode_byte(ubyte byt, ubyte cnt, FILE * fid) 
+int pcx_encode_byte(ubyte byt, ubyte cnt, FILE * fid)
 {
 	if (cnt) {
 		if ( (cnt==1) && (0xc0 != (0xc0 & byt)) )	{
@@ -340,4 +299,26 @@ int pcx_read_fullscr(char * filename, ubyte * palette)
 		show_fullscr(&bm);
 	gr_free_bitmap_data(&bm);
 	return pcx_error;
+}
+
+/*
+ * reads a PCXHeader structure from a CFILE
+ */
+void PCXHeader_read(PCXHeader *ph, CFILE *fp)
+{
+	ph->Manufacturer = cfile_read_byte(fp);
+	ph->Version = cfile_read_byte(fp);
+	ph->Encoding = cfile_read_byte(fp);
+	ph->BitsPerPixel = cfile_read_byte(fp);
+	ph->Xmin = cfile_read_short(fp);
+	ph->Ymin = cfile_read_short(fp);
+	ph->Xmax = cfile_read_short(fp);
+	ph->Ymax = cfile_read_short(fp);
+	ph->Hdpi = cfile_read_short(fp);
+	ph->Vdpi = cfile_read_short(fp);
+	cfread(&ph->ColorMap, 16*3, 1, fp);
+	ph->Reserved = cfile_read_byte(fp);
+	ph->Nplanes = cfile_read_byte(fp);
+	ph->BytesPerLine = cfile_read_short(fp);
+	cfread(&ph->filler, 60, 1, fp);
 }
--- include/pcx.h
+++ include/pcx.h
@@ -1,3 +1,4 @@
+/* $Id: pcx.h,v 1.2 2002-08-06 04:49:43 btb Exp $ */
 /*
 THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
 SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
@@ -7,55 +8,65 @@ IN USING, DISPLAYING,  AND CREATING DERIVATIVE WORKS THEREOF, SO LONG AS
 SUCH USE, DISPLAY OR CREATION IS FOR NON-COMMERCIAL, ROYALTY OR REVENUE
 FREE PURPOSES.  IN NO EVENT SHALL THE END-USER USE THE COMPUTER CODE
 CONTAINED HEREIN FOR REVENUE-BEARING PURPOSES.  THE END-USER UNDERSTANDS
-AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.  
+AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
 COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 */
 /*
- * $Source: /cvs/cvsroot/d2x/include/pcx.h,v $
- * $Revision: 1.1.1.1 $
- * $Author: bradleyb $
- * $Date: 2001-01-19 03:30:16 $
- * 
+ *
  * Routines to read/write pcx images.
- * 
- * $Log: not supported by cvs2svn $
- * Revision 1.2  1999/11/20 10:05:16  donut
- * variable size menu patch from Jan Bobrowski.  Variable menu font size support and a bunch of fixes for menus that didn't work quite right, by me (MPM).
  *
- * Revision 1.1.1.1  1999/06/14 22:02:19  donut
- * Import of d1x 1.37 source.
- *
+ * Old Log:
  * Revision 1.4  1995/01/21  17:07:34  john
  * Added out of memory error.
- * 
+ *
  * Revision 1.3  1994/11/29  02:53:10  john
  * Added error messages; made call be more similiar to iff.
- * 
+ *
  * Revision 1.2  1994/11/28  20:03:48  john
  * Added PCX functions.
- * 
+ *
  * Revision 1.1  1994/11/28  19:57:45  john
  * Initial revision
- * 
- * 
+ *
+ *
  */
-
 
 
 #ifndef _PCX_H
 #define _PCX_H
 
-#define PCX_ERROR_NONE 				0
-#define PCX_ERROR_OPENING			1
-#define PCX_ERROR_NO_HEADER		2
-#define PCX_ERROR_WRONG_VERSION	3
-#define PCX_ERROR_READING			4
-#define PCX_ERROR_NO_PALETTE		5
-#define PCX_ERROR_WRITING			6
-#define PCX_ERROR_MEMORY			7
+#include "cfile.h"
 
-// Reads filename into bitmap bmp, and fills in palette.  If bmp->bm_data==NULL, 
-// then bmp->bm_data is allocated and the w,h are filled.  
+#define PCX_ERROR_NONE          0
+#define PCX_ERROR_OPENING       1
+#define PCX_ERROR_NO_HEADER     2
+#define PCX_ERROR_WRONG_VERSION 3
+#define PCX_ERROR_READING       4
+#define PCX_ERROR_NO_PALETTE    5
+#define PCX_ERROR_WRITING       6
+#define PCX_ERROR_MEMORY        7
+
+/* PCX Header data type */
+typedef struct	{
+	ubyte		Manufacturer;
+	ubyte		Version;
+	ubyte		Encoding;
+	ubyte		BitsPerPixel;
+	short		Xmin;
+	short		Ymin;
+	short		Xmax;
+	short		Ymax;
+	short		Hdpi;
+	short		Vdpi;
+	ubyte		ColorMap[16][3];
+	ubyte		Reserved;
+	ubyte		Nplanes;
+	short		BytesPerLine;
+	ubyte		filler[60];
+} PCXHeader;
+
+// Reads filename into bitmap bmp, and fills in palette.  If bmp->bm_data==NULL,
+// then bmp->bm_data is allocated and the w,h are filled.
 // If palette==NULL the palette isn't read in.  Returns error code.
 
 extern int pcx_read_bitmap( char * filename, grs_bitmap * bmp, int bitmap_type, ubyte * palette );
@@ -68,5 +79,9 @@ extern char *pcx_errormsg(int error_number);
 
 int pcx_read_fullscr(char * filename, ubyte * palette);
 
+/*
+ * reads a PCXHeader structure from a CFILE
+ */
+void PCXHeader_read(PCXHeader *ph, CFILE *fp);
+
 #endif
- 
