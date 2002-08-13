4f04684ff3cec27d82bca2cbc63304f5d7f93aed
Bradley Bell <btb@icculus.org>
2002-08-13 06:56:57 +0000

    formatting

--- utility/hogextract.c
+++ utility/hogextract.c
@@ -1,45 +1,48 @@
-/* Written 1999 Jan 29 by Josh Cogliati
-   I grant this program to public domain.
-*/
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
+/*
+ * Written 1999 Jan 29 by Josh Cogliati
+ * I grant this program to public domain.
+ */
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-void main(int argc, char *argv[]){
-  FILE * hogfile, * writefile;
-  int fp, len, fseekret=0;
-  char filename[13];
-  char * buf;
-  struct stat statbuf;
-  if(argc != 2){
-    printf("Usage: hogextract hogfile\n"
-	   "extracts all the files in hogfile into the current directory\n"
-	   );
-    exit(0);
-  }
-  hogfile = fopen(argv[1],"r"); 
-  stat(argv[1],&statbuf);
-  printf("%i\n",statbuf.st_size);
-  buf = (char *)malloc(3);
-  fread(buf,3,1,hogfile);
-  printf("Extracting from: %s\n",argv[1]);
-  free(buf);
-  while(ftell(hogfile)<statbuf.st_size){
-    fread(filename,13,1,hogfile);
-    fread(&len,sizeof(int),1,hogfile);
-    printf("Filename: %s \tLength: %i\n",filename,len);
-    buf = (char *)malloc(len);
-    if(buf == NULL) {
-      printf("Unable to allocate memory\n");
-    } else {
-      fread(buf,len,1,hogfile);
-      writefile = fopen(filename,"w");
-      fwrite(buf, len, 1 , writefile);
-      fclose(writefile);
-      free(buf);
-    };
-  }
-  fclose(hogfile);
+void
+main(int argc, char *argv[])
+{
+	FILE *hogfile, *writefile;
+	int fp, len, fseekret=0;
+	char filename[13];
+	char *buf;
+	struct stat statbuf;
+
+	if (argc != 2) {
+		printf("Usage: hogextract hogfile\n"
+		       "extracts all the files in hogfile into the current directory\n");
+		exit(0);
+	}
+	hogfile = fopen(argv[1], "r");
+	stat(argv[1], &statbuf);
+	printf("%i\n", statbuf.st_size);
+	buf = (char *)malloc(3);
+	fread(buf, 3, 1, hogfile);
+	printf("Extracting from: %s\n", argv[1]);
+	free(buf);
+	while(ftell(hogfile)<statbuf.st_size) {
+		fread(filename, 13, 1, hogfile);
+		fread(&len, sizeof(int), 1, hogfile);
+		printf("Filename: %s \tLength: %i\n", filename, len);
+		buf = (char *)malloc(len);
+		if (buf == NULL) {
+			printf("Unable to allocate memory\n");
+		} else {
+			fread(buf, len, 1, hogfile);
+			writefile = fopen(filename, "w");
+			fwrite(buf, len, 1, writefile);
+			fclose(writefile);
+			free(buf);
+		}
+	}
+	fclose(hogfile);
 }
--- utility/txb2txt.c
+++ utility/txb2txt.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
-    
-main(int ac,char *av[]) {
-  FILE *file,*outfile;
-  char equals,ch;
-  int code;
-    
-  if (ac!=3) {
-    printf("TXB2TXT V1.0 Copyright (c) Bryan Aamot, 1995\n"
-           "TXB to Text converter for Descent HOG files.\n"
-           "Converts a *.txb descent hog file to an ascii file.\n"
-           "Usage: TXB2TXT <txb file name> <text file name>\n"
-           "Example: TXT2TXB briefing.txb briefing.txt\n");
-    exit(1);
-  }
-  file = fopen(av[1],"rb");
-  if (!file) {
-    printf("Can't open txb file (%s)\n",av[1]);
-    exit(2);
-  }
-  outfile = fopen(av[2],"wb");
-  if (!outfile) {
-    printf("Can't open file (%s)\n",av[2]);
-    fclose(file);
-    exit(2);
-  }
-  for (;;) {
-    code = getc(file);
-    if (feof(file)) break;
-    if (code == 0x0a) {
-      fprintf(outfile,"\x0d\x0a");
-    } else {
-      ch = (  ( (code&0x3f) << 2 ) + ( (code&0xc0) >> 6 )  ) ^ 0xa7;
-      fprintf(outfile,"%c",ch);
-    }
-  }
 
-  fclose(outfile);
-  fclose(file);
+void
+main(int argc, char *argv[])
+{
+	FILE *file, *outfile;
+	char equals, ch;
+	int code;
+
+	if (argc != 3) {
+		printf("TXB2TXT V1.0 Copyright (c) Bryan Aamot, 1995\n"
+		       "TXB to Text converter for Descent HOG files.\n"
+		       "Converts a *.txb descent hog file to an ascii file.\n"
+		       "Usage: TXB2TXT <txb file name> <text file name>\n"
+		       "Example: TXT2TXB briefing.txb briefing.txt\n");
+		exit(1);
+	}
+	file = fopen(argv[1], "rb");
+	if (!file) {
+		printf("Can't open txb file (%s)\n", argv[1]);
+		exit(2);
+	}
+	outfile = fopen(argv[2], "wb");
+	if (!outfile) {
+		printf("Can't open file (%s)\n", argv[2]);
+		fclose(file);
+		exit(2);
+	}
+	for (;;) {
+		code = getc(file);
+		if (feof(file)) break;
+		if (code == 0x0a) {
+			fprintf(outfile, "\x0d\x0a");
+		} else {
+			ch = (  ( (code&0x3f) << 2 ) + ( (code&0xc0) >> 6 )  ) ^ 0xa7;
+			fprintf(outfile, "%c", ch);
+		}
+	}
+
+	fclose(outfile);
+	fclose(file);
 }
