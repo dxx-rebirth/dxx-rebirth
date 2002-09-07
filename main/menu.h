c3fc9f1a833cb92a2d901778ce051f4504570479
Bradley Bell <btb@icculus.org>
2002-09-07 07:24:29 +0000

    enable -nohires with mac shareware

--- main/menu.h
+++ main/menu.h
@@ -1,4 +1,4 @@
-/* $Id: menu.h,v 1.4 2002-08-22 19:18:13 btb Exp $ */
+/* $Id: menu.h,v 1.5 2002-09-07 07:24:29 btb Exp $ */
 /*
 THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
 SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
@@ -23,17 +23,19 @@ extern void do_options_menu();
 extern int MenuHires;
 
 #ifdef RELEASE  //read only from hog file
+#define MENU_PCX_MAC_SHARE ("\x01menub.pcx")
 #define MENU_PCX_SHAREWARE ("\x01menud.pcx")
 #define MENU_PCX_OEM (MenuHires?"\x01menuob.pcx":"\x01menuo.pcx")
 #define MENU_PCX_FULL (MenuHires?"\x01menub.pcx":"\x01menu.pcx")
 #else
+#define MENU_PCX_MAC_SHARE ("menub.pcx")
 #define MENU_PCX_SHAREWARE ("menud.pcx")
 #define MENU_PCX_OEM (MenuHires?"menuob.pcx":"menuo.pcx")
 #define MENU_PCX_FULL (MenuHires?"menub.pcx":"menu.pcx")
 #endif
 
 //name of background bitmap
-#define Menu_pcx_name (cfexist(MENU_PCX_FULL)?MENU_PCX_FULL:(cfexist(MENU_PCX_OEM)?MENU_PCX_OEM:MENU_PCX_SHAREWARE))
+#define Menu_pcx_name (cfexist(MENU_PCX_FULL)?MENU_PCX_FULL:(cfexist(MENU_PCX_OEM)?MENU_PCX_OEM:cfexist(MENU_PCX_SHAREWARE)?MENU_PCX_SHAREWARE:MENU_PCX_MAC_SHARE))
 
 extern void set_detail_level_parameters(int detail_level);
 
