a0157ab4926e64a69cdc9b49482bff4c73a0c775
Bradley Bell <btb@icculus.org>
2003-04-03 06:07:35 +0000

    comments/formatting

--- main/gamemine.c
+++ main/gamemine.c
@@ -1,4 +1,4 @@
-/* $Id: */
+/* $Id: gamemine.c,v 1.22 2003-04-03 06:07:35 btb Exp $ */
 /*
 THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
 SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
@@ -12,12 +12,137 @@ AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
 COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 */
 
+/*
+ *
+ * Functions for loading mines in the game
+ *
+ * Old Log:
+ * Revision 1.2  1995/10/31  10:15:58  allender
+ * code for shareware levels
+ *
+ * Revision 1.1  1995/05/16  15:25:29  allender
+ * Initial revision
+ *
+ * Revision 2.2  1995/03/06  15:23:14  john
+ * New screen techniques.
+ *
+ * Revision 2.1  1995/02/27  13:13:37  john
+ * Removed floating point.
+ *
+ * Revision 2.0  1995/02/27  11:27:45  john
+ * New version 2.0, which has no anonymous unions, builds with
+ * Watcom 10.0, and doesn't require parsing BITMAPS.TBL.
+ *
+ * Revision 1.70  1995/02/13  20:35:09  john
+ * Lintized
+ *
+ * Revision 1.69  1995/02/07  17:12:03  rob
+ * Added ifdef's for Editor.
+ *
+ * Revision 1.68  1995/02/07  16:51:48  mike
+ * fix gray rock josh problem.
+ *
+ * Revision 1.67  1995/02/01  15:46:26  yuan
+ * Fixed matcen_nums.
+ *
+ * Revision 1.66  1995/01/19  15:19:28  mike
+ * new super-compressed registered file format.
+ *
+ * Revision 1.65  1994/12/10  16:44:59  matt
+ * Added debugging code to track down door that turns into rock
+ *
+ * Revision 1.64  1994/12/10  14:58:24  yuan
+ * *** empty log message ***
+ *
+ * Revision 1.63  1994/12/08  17:19:10  yuan
+ * Cfiling stuff.
+ *
+ * Revision 1.62  1994/12/07  14:05:33  yuan
+ * Fixed wall assert problem... Bashed highest_segment
+ * _index before WALL_IS_DOORWAY check.
+ *
+ * Revision 1.61  1994/11/27  23:14:52  matt
+ * Made changes for new mprintf calling convention
+ *
+ * Revision 1.60  1994/11/27  18:05:20  matt
+ * Compile out LVL reader when editor compiled out
+ *
+ * Revision 1.59  1994/11/26  22:51:45  matt
+ * Removed editor-only fields from segment structure when editor is compiled
+ * out, and padded segment structure to even multiple of 4 bytes.
+ *
+ * Revision 1.58  1994/11/26  21:48:02  matt
+ * Fixed saturation in short light value
+ *
+ * Revision 1.57  1994/11/20  22:11:49  mike
+ * comment out an apparently unnecessary call to fuelcen_reset().
+ *
+ * Revision 1.56  1994/11/18  21:56:42  john
+ * Added a better, leaner pig format.
+ *
+ * Revision 1.55  1994/11/17  20:09:18  john
+ * Added new compiled level format.
+ *
+ * Revision 1.54  1994/11/17  15:40:17  mike
+ * Comment out mprintf which was causing important information to scroll away.
+ *
+ * Revision 1.53  1994/11/17  14:56:37  mike
+ * moved segment validation functions from editor to main.
+ *
+ * Revision 1.52  1994/11/17  11:39:35  matt
+ * Ripped out code to load old mines
+ *
+ * Revision 1.51  1994/11/14  20:47:53  john
+ * Attempted to strip out all the code in the game
+ * directory that uses any ui code.
+ *
+ * Revision 1.50  1994/11/14  16:05:38  matt
+ * Fixed, maybe, again, errors when can't find texture during remap
+ *
+ * Revision 1.49  1994/11/14  14:34:03  matt
+ * Fixed up handling when textures can't be found during remap
+ *
+ * Revision 1.48  1994/11/14  13:01:55  matt
+ * Added Int3() when can't find texture
+ *
+ * Revision 1.47  1994/10/30  14:12:21  mike
+ * rip out local segments stuff.
+ *
+ * Revision 1.46  1994/10/27  19:43:07  john
+ * Disable the piglet option.
+ *
+ * Revision 1.45  1994/10/27  18:51:42  john
+ * Added -piglet option that only loads needed textures for a
+ * mine.  Only saved ~1MB, and code still doesn't free textures
+ * before you load a new mine.
+ *
+ * Revision 1.44  1994/10/20  12:47:22  matt
+ * Replace old save files (MIN/SAV/HOT) with new LVL files
+ *
+ * Revision 1.43  1994/10/19  16:46:40  matt
+ * Made tmap overrides for robots remap texture numbers
+ *
+ * Revision 1.42  1994/10/03  23:37:01  mike
+ * Adapt to changed fuelcen_activate parameters.
+ *
+ * Revision 1.41  1994/09/23  22:14:49  matt
+ * Took out obsolete structure fields
+ *
+ * Revision 1.40  1994/08/01  11:04:11  yuan
+ * New materialization centers.
+ *
+ * Revision 1.39  1994/07/21  19:01:47  mike
+ * Call Lsegment stuff.
+ *
+ *
+ */
+
 #ifdef HAVE_CONFIG_H
 #include <conf.h>
 #endif
 
 #ifdef RCS
-static char rcsid[] = "$Id: gamemine.c,v 1.21 2003-03-19 22:44:15 btb Exp $";
+static char rcsid[] = "$Id: gamemine.c,v 1.22 2003-04-03 06:07:35 btb Exp $";
 #endif
 
 #include <stdio.h>
