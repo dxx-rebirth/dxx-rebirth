07816527fe8c987b1336fdf298f2e34949356e85
Bradley Bell <btb@icculus.org>
2004-11-25 09:52:53 +0000

    get rid of clipping in ogl_internal_string, causes garbage on windows for some reason.

--- 2d/font.c
+++ 2d/font.c
@@ -1,4 +1,4 @@
-/* $Id: font.c,v 1.31 2004-08-01 16:28:33 schaffner Exp $ */
+/* $Id: font.c,v 1.32 2004-11-25 09:52:53 btb Exp $ */
 /*
 THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
 SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
@@ -1329,8 +1329,7 @@ int ogl_internal_string(int x, int y, char *s )
 //			ogl_ubitblt(FONT->ft_bitmaps[letter].bm_w,FONT->ft_bitmaps[letter].bm_h,xx,yy,0,0,&FONT->ft_bitmaps[letter],NULL);
 //			if (*text_ptr>='0' && *text_ptr<='9'){
 			if (FFLAGS&FT_COLOR)
-				//gr_ubitmapm(xx,yy,&FONT->ft_bitmaps[letter]);
-				gr_bitmapm(xx,yy,&FONT->ft_bitmaps[letter]); // credits need clipping
+				gr_ubitmapm(xx,yy,&FONT->ft_bitmaps[letter]);
 			else{
 				if (grd_curcanv->cv_bitmap.bm_type==BM_OGL)
 					ogl_ubitmapm_c(xx,yy,&FONT->ft_bitmaps[letter],FG_COLOR);
