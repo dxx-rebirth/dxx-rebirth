b0386ef5829426bf9e43bd4a178d3eb2a117ed1e
Chris Taylor <chris@icculus.org>
2005-07-25 03:50:37 +0000

    no checking if obj->id > -1 (it's unsigned)

--- main/editor/medrobot.c
+++ main/editor/medrobot.c
@@ -1,4 +1,4 @@
-/* $Id: medrobot.c,v 1.7 2005-07-03 13:12:47 chris Exp $ */
+/* $Id: medrobot.c,v 1.8 2005-07-25 03:50:07 chris Exp $ */
 /*
 THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
 SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
@@ -19,7 +19,7 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
  */
 
 #ifdef RCS
-static char rcsid[] = "$Id: medrobot.c,v 1.7 2005-07-03 13:12:47 chris Exp $";
+static char rcsid[] = "$Id: medrobot.c,v 1.8 2005-07-25 03:50:07 chris Exp $";
 #endif
 
 #ifdef HAVE_CONFIG_H
@@ -610,10 +610,7 @@ void do_robot_window()
 		object *obj = &Objects[Cur_object_index];
 
 		gr_set_current_canvas( RobotViewBox->canvas );
-		if ( obj->id > -1 )	
-			draw_object_picture(obj->id, &angles, obj->type );
-		else
-			gr_clear_canvas( CGREY );
+		draw_object_picture(obj->id, &angles, obj->type );
 		angles.h += fixmul(0x1000, DeltaTime );
 	} else {
 		// no object, so just blank out
