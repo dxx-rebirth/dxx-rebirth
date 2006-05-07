90d4cdbc910263c6962a972143792d9c963d1933
zicodxx <>
2006-05-07 16:10:04 +0000

    restored max_distance multiplication; using 0 instead of MIN_VOLUME to fix loud sound on fade-in functions

--- arch/win32/digi.c
+++ arch/win32/digi.c
@@ -458,7 +458,7 @@ void digi_get_sound_loc( vms_matrix * listener, vms_vector * listener_pos, int l
   *volume = 0;
   *pan = 0;
 
-        //max_distance = (max_distance*5)/4; // Make all sounds travel 1.25 times as far. // ZICO - deactivated to prevent back coupling
+        max_distance = (max_distance*5)/4; // Make all sounds travel 1.25 times as far.
 
 	//	Warning: Made the vm_vec_normalized_dir be vm_vec_normalized_dir_quick and got illegal values to acos in the fang computation.
 	distance = vm_vec_normalized_dir_quick( &vector_to_sound, sound_pos, listener_pos );
@@ -721,7 +721,7 @@ void digi_sync_sounds() // ZICO - modified to get pan/fading working
 			}
 
 			if (oldvolume != SoundObjects[i].volume) 	{
-				if ( SoundObjects[i].volume < MIN_VOLUME || Function_mode == FMODE_MENU)	 {
+				if ( SoundObjects[i].volume < 0 || Function_mode == FMODE_MENU)	 {
 					// Sound is too far away, so stop it from playing.
 					if ((SoundObjects[i].flags & SOF_PLAYING)&&(SoundObjects[i].flags & SOF_PLAY_FOREVER))	{
 						DS_release_slot(SoundObjects[i].handle, 1);
