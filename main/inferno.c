c4d4921a624d6ced41f25d2d7b9dc7df49c0c755
zicodxx <>
2006-07-16 10:59:03 +0000

    corrected command-line output in inferno.c

--- main/inferno.c
+++ main/inferno.c
@@ -319,7 +319,7 @@ void show_cmdline_help() {
 #ifdef GR_SUPPORTS_FULLSCREEN_TOGGLE 
 	printf( "  -window         %s\n", "Run the game in a window"); // ZICO - from window to fullscreen
 #endif
-	printf( " -aspect <y> <x>  %s\n"  ";use specified aspect - example: -aspect 16 9, -aspect 16 7.5 etc.");
+	printf( "  -aspect <y> <x>  %s\n",  ";use specified aspect - example: -aspect 16 9, -aspect 16 7.5 etc.");
 #ifdef OGL
 	printf( "  -gl_texmaxfilt <f> %s\n","set GL_TEXTURE_MAX_FILTER (see readme.d1x)");
 	printf( "  -gl_texminfilt <f> %s\n","set GL_TEXTURE_MIN_FILTER (see readme.d1x)");
