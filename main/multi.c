02006581559ae04743f3f398aa3363e107667466
zicodxx <>
2007-04-15 16:33:04 +0000

    forbid % to read into multi messages to not mess up the string

--- main/multi.c
+++ main/multi.c
@@ -1635,7 +1635,7 @@ void multi_message_input_sub( int key )
 	default:
 		if ( key > 0 )	{
 			int ascii = key_to_ascii(key);
-			if ((ascii < 255 ))	{
+			if ((ascii < 255 ) && (ascii != 37))	{
 				if (multi_message_index < MAX_MESSAGE_LEN-2 )	{
 					Network_message[multi_message_index++] = ascii;
 					Network_message[multi_message_index] = 0;
