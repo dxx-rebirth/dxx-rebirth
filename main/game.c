f5f0c45ded8bdd0019ee9825485dd613374aee0a
zicodxx <>
2007-05-02 18:42:46 +0000

    rev432 revisited - do not send multi De-Cloak packet in Demo Playback

--- main/game.c
+++ main/game.c
@@ -1482,7 +1482,7 @@ void do_cloak_stuff(void)
 					if (Game_mode & GM_MULTI)
 						multi_send_play_sound(SOUND_CLOAK_OFF, F1_0);
 					maybe_drop_net_powerup(POW_CLOAK);
-					if ( Newdemo_state == ND_STATE_RECORDING )
+					if ( Newdemo_state != ND_STATE_PLAYBACK )
 						multi_send_decloak(); // For demo recording
 #endif
 				}
