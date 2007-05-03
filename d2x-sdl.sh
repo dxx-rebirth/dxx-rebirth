49eaa124683bf7e864e9dbc1c55339756cc8f6fb
zicodxx <>
2007-05-03 15:16:27 +0000

    removed deprecated Makefile stuff

--- d2x-sdl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Run the real game.
-# All switches can be overwritten by command-line arguments.
-#
-nice /usr/games/d2x-sdl.real -nocdrom -nomovies -nofades $1 $2 $3 $4 $5
-
--- djgpp.bat
+++
@@ -1,1 +0,0 @@
-bash djgpp.sh
