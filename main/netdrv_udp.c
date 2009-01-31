be596fbbdd13acc0dd2fc2d88a1758d9fbfcb734
zicodxx <>
2009-01-31 14:52:06 +0000

    Fixing bug with incorrect/missing UDP port termination

--- main/netdrv_udp.c
+++ main/netdrv_udp.c
@@ -449,9 +449,10 @@ int udp_open_socket(socket_t *unused, int port)
 #else
 	struct addrinfo hints,*res,*sres;
 	int err,ai_family_;
-	char cport[16];
+	char cport[LEN_PORT];
 	
 	memset (&hints, '\0', sizeof (struct addrinfo));
+	memset(cport,'\0',sizeof(char)*LEN_PORT);
 	
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_family = _pf;
