1833474b121c1d2aee0fec80822d467668987d4d
zicodxx <>
2010-07-16 06:27:49 +0000

    Fixing showing options menu in demos - was still called (or rather not) by obsolete variable

--- main/gamecntl.c
+++ main/gamecntl.c
@@ -114,7 +114,6 @@ extern int	Mark_count;
 #endif
 
 extern int	Global_missile_firing_count;
-extern int	Config_menu_flag;
 
 extern int	*Toggle_var;
 
@@ -497,7 +496,7 @@ int HandleDemoKey(int key)
 		KEY_MAC(case KEY_COMMAND+KEY_1:)
 		case KEY_F1:	show_newdemo_help();	break;
 		KEY_MAC(case KEY_COMMAND+KEY_2:)
-		case KEY_F2:	Config_menu_flag = 1;	break;
+		case KEY_F2:	do_options_menu();	break;
 		KEY_MAC(case KEY_COMMAND+KEY_3:)
 		case KEY_F3:
 			 if (Viewer->type == OBJ_PLAYER)
@@ -822,7 +821,7 @@ int HandleSystemKey(int key)
 		case KEY_F1:				if (Game_mode & GM_MULTI) show_netgame_help(); else show_help();	break;
 
 		KEY_MAC(case KEY_COMMAND+KEY_2:)
-		case KEY_F2:				//Config_menu_flag = 1;	break;
+		case KEY_F2:
 			{
 				do_options_menu();
 				break;
