cfc7e49837d58f5cbbb221694e06fd007b6bd5b4
zicodxx <>
2010-08-09 19:19:36 +0000

    Cleaned and simplified includes to fix possible and actual problems with PATH_MAX

--- include/pstypes.h
+++ include/pstypes.h
@@ -1,25 +1,14 @@
-/* $Id: pstypes.h,v 1.1.1.1 2006/03/17 20:01:30 zicodxx Exp $ */
-/*
-THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
-SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
-END-USERS, AND SUBJECT TO ALL OF THE TERMS AND CONDITIONS HEREIN, GRANTS A
-ROYALTY-FREE, PERPETUAL LICENSE TO SUCH END-USERS FOR USE BY SUCH END-USERS
-IN USING, DISPLAYING,  AND CREATING DERIVATIVE WORKS THEREOF, SO LONG AS
-SUCH USE, DISPLAY OR CREATION IS FOR NON-COMMERCIAL, ROYALTY OR REVENUE
-FREE PURPOSES.  IN NO EVENT SHALL THE END-USER USE THE COMPUTER CODE
-CONTAINED HEREIN FOR REVENUE-BEARING PURPOSES.  THE END-USER UNDERSTANDS
-AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
-COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
-*/
-
 /*
  *
- * Common types for use in Miner
+ * Common types and defines
  *
  */
 
 #ifndef _TYPES_H
 #define _TYPES_H
+
+#include <sys/types.h>
+#include <limits.h>
 
 // define a dboolean
 typedef int dboolean;
@@ -61,23 +50,8 @@ typedef unsigned int uint;
 #define max(a,b) (((a)<(b))?(b):(a))
 #endif
 
-#ifdef _WIN32
-# ifndef __MINGW32__
-#  define PATH_MAX _MAX_PATH
-# endif
-# define FNAME_MAX 256
-#elif defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
-# include <sys/types.h>
-# ifndef PATH_MAX
-#  define PATH_MAX 1024
-# endif
-# define FNAME_MAX 256
-#elif defined __DJGPP__
-# include <sys/types.h>
-# define FNAME_MAX 9	// excluding extension
-#elif defined(macintosh)
-# define PATH_MAX 256
-# define FNAME_MAX 32
+#ifndef PATH_MAX
+#define PATH_MAX 4096
 #endif
 
 #ifndef bool
