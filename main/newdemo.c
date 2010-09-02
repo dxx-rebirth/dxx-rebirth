6e39b9f25bb35be2e03d09a8fd77306f47cf1f77
zicodxx <>
2010-09-02 13:58:06 +0000

    Using PHYSFSX_findFiles to make sure random demo playback will only find actual demo files and not quit autodemo; added DEMO_EXT for an universal definition of demo file extension

--- main/newdemo.c
+++ main/newdemo.c
@@ -3474,7 +3474,7 @@ try_again:
 		if (filename[0] != '\0') {
 			strcpy(save_file, DEMO_DIR);
 			strcat(save_file, filename);
-			strcat(save_file, ".dem");
+			strcat(save_file, DEMO_EXT);
 		} else
 			sprintf (save_file, "%stmp%d.dem", DEMO_DIR, tmpcnt++);
 		remove(save_file);
@@ -3500,7 +3500,7 @@ try_again:
 		strcat(fullname, m[1].text);
 	else
 		strcat(fullname, m[0].text);
-	strcat(fullname, ".dem");
+	strcat(fullname, DEMO_EXT);
 	PHYSFS_delete(fullname);
 	PHYSFSX_rename(DEMO_FILENAME, fullname);
 }
@@ -3509,9 +3509,10 @@ try_again:
 int newdemo_count_demos()
 {
 	char **find, **i;
+	char *types[] = { DEMO_EXT, NULL };
 	int NumFiles=0;
 
-	find = PHYSFS_enumerateFiles(DEMO_DIR);
+	find = PHYSFSX_findFiles(DEMO_DIR, types);
 
 	for (i = find; *i != NULL; i++)
 		NumFiles++;
@@ -3538,6 +3539,7 @@ void newdemo_start_playback(char * filename)
 		// Randomly pick a filename
 		int NumFiles = 0, RandFileNum;
 		rnd_demo = 1;
+		char *types[] = { DEMO_EXT, NULL };
 
 		NumFiles = newdemo_count_demos();
 
@@ -3548,7 +3550,7 @@ void newdemo_start_playback(char * filename)
 		RandFileNum = d_rand() % NumFiles;
 		NumFiles = 0;
 
-		find = PHYSFS_enumerateFiles(DEMO_DIR);
+		find = PHYSFSX_findFiles(DEMO_DIR, types);
 
 		for (i = find; *i != NULL; i++)
 		{
@@ -3563,13 +3565,6 @@ void newdemo_start_playback(char * filename)
 		PHYSFS_freeList(find);
 
 		if (NumFiles > RandFileNum)
-		{
-			GameArg.SysAutoDemo = 0;
-			return;
-		}
-
-		// if in random mode, PhysFS may look for all possible files, so check if filename actually points to be a demo file...
-		if (strnicmp(".dem",&filename2[strlen(filename2)-4],4))
 		{
 			GameArg.SysAutoDemo = 0;
 			return;
