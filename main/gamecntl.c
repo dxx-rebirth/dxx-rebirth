bddb2e97a2708ef676de763a6b0f03d52aef0660
zicodxx <zicodxx@yahoo.de>
2011-02-15 14:44:03 +0100

    Fixed PRShot feature which was accidentially broken while implementation of OpenGL ES support

--- main/gamecntl.c
+++ main/gamecntl.c
@@ -599,8 +599,8 @@ int HandleDemoKey(int key)
 				render_frame(0, 0);
 				gr_set_curfont(MEDIUM2_FONT);
 				gr_printf(SWIDTH-FSPACX(92),SHEIGHT-LINE_SPACING,"DXX-Rebirth\n");
-				save_screen_shot(0);
 				gr_flip();
+				save_screen_shot(0);
 			}
 			else
 			{
@@ -608,9 +608,9 @@ int HandleDemoKey(int key)
 				old_state = Newdemo_show_percentage;
 				Newdemo_show_percentage = 0;
 				game_render_frame_mono(0);
-				save_screen_shot(0);
 				if (GameArg.DbgUseDoubleBuffer)
 					gr_flip();
+				save_screen_shot(0);
 				Newdemo_show_percentage = old_state;
 			}
 			break;
@@ -853,15 +853,15 @@ int HandleSystemKey(int key)
 				render_frame(0, 0);
 				gr_set_curfont(MEDIUM2_FONT);
 				gr_printf(SWIDTH-FSPACX(92),SHEIGHT-LINE_SPACING,"DXX-Rebirth\n");
-				save_screen_shot(0);
 				gr_flip();
+				save_screen_shot(0);
 			}
 			else
 			{
 				game_render_frame_mono(0);
-				save_screen_shot(0);
 				if(GameArg.DbgUseDoubleBuffer)
 					gr_flip();
+				save_screen_shot(0);
 			}
 			break;
 		}
