d5619e806ad9b7592d65b87ebe437d3cd20eb9ad
zicodxx <zicodxx@yahoo.de>
2011-04-08 01:38:00 +0200

    Allow multi_send_fire when necessary and not crop to PPS so fix weapons with high firing rate

--- main/net_udp.c
+++ main/net_udp.c
@@ -4216,7 +4216,6 @@ void net_udp_do_frame(int force, int listen)
 	if (time >= (last_send_time+(F1_0/Netgame.PacketsPerSec)))
 	{
 		multi_send_robot_frame(0);
-		multi_send_fire(); // Do firing if needed..
 		
 		last_send_time = time;
 		
@@ -4226,7 +4225,10 @@ void net_udp_do_frame(int force, int listen)
 	}
 
 	if (send_mdata)
+	{
+		multi_send_fire(); // Do firing if needed..
 		net_udp_send_mdata(PacketUrgent, time);
+	}
 
 	net_udp_noloss_process_queue(time);
 
