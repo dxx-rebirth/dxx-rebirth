f207000f505e263c8dc63b0bd62a68c809cedbb9
zicodxx <zicodxx@yahoo.de>
2011-04-12 12:17:24 +0200

    kconfig weapon cycling fields changed after 0.56 release will automatically fix if version number changes on next release

--- main/playsave.c
+++ main/playsave.c
@@ -309,6 +309,29 @@ int read_player_d2x(char *filename)
 				strupr(word);
 			}
 		}
+		else if (strstr(word,"PLX VERSION")) // know the version this pilot was used last with - allow modifications
+		{
+			int v1=0,v2=0,v3=0;
+			d_free(word);
+			cfgets(line,50,f);
+			word=splitword(line,'=');
+			strupr(word);
+			while(!strstr(word,"END") && !PHYSFS_eof(f))
+			{
+				sscanf(line,"%i.%i.%i",&v1,&v2,&v3);
+				d_free(word);
+				cfgets(line,50,f);
+				word=splitword(line,'=');
+				strupr(word);
+			}
+			if (v1 == 0 && v2 == 56 && v3 == 0) // was 0.56.0
+				if (D2XMAJORi != v1 || D2XMINORi != v2 || D2XMICROi != v3) // newer (presumably)
+				{
+					// reset joystick/mouse cycling fields
+					PlayerCfg.KeySettings[2][27] = 255;
+					PlayerCfg.KeySettings[2][28] = 255;
+				}
+		}
 		else if (strstr(word,"END") || PHYSFS_eof(f))
 		{
 			Stop=1;
