cd0213122ca507422412f073209fd5ca041c1440
Chris Taylor <kreatordxx@dxx-rebirth.bzr.sourceforge.net>
2012-03-24 13:26:51 +0800

    Draw the editor menubar properly - by drawing in response to EVENT_WINDOW_DRAW and initialising the screen canvas properly. The latter fixes a lot of other editor drawing issues as well

--- arch/sdl/gr.c
+++ arch/sdl/gr.c
@@ -132,13 +132,7 @@ int gr_set_mode(u_int32_t mode)
 	grd_curscreen->sc_w = w;
 	grd_curscreen->sc_h = h;
 	grd_curscreen->sc_aspect = fixdiv(grd_curscreen->sc_w*GameCfg.AspectX,grd_curscreen->sc_h*GameCfg.AspectY);
-	grd_curscreen->sc_canvas.cv_bitmap.bm_x = 0;
-	grd_curscreen->sc_canvas.cv_bitmap.bm_y = 0;
-	grd_curscreen->sc_canvas.cv_bitmap.bm_w = w;
-	grd_curscreen->sc_canvas.cv_bitmap.bm_h = h;
-	grd_curscreen->sc_canvas.cv_bitmap.bm_rowsize = canvas->pitch;
-	grd_curscreen->sc_canvas.cv_bitmap.bm_type = BM_LINEAR;
-	grd_curscreen->sc_canvas.cv_bitmap.bm_data = (unsigned char *)canvas->pixels;
+	gr_init_canvas(&grd_curscreen->sc_canvas, canvas->pixels, BM_LINEAR, w, h);
 	gr_set_current_canvas(NULL);
 
 	SDL_ShowCursor(0);
