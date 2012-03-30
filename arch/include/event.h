24d08491250a983488b2f695e27cc969067e68ef
zicodxx <zico@dxx-rebirth.com>
2012-03-30 16:29:03 +0200

    Changed declaration of event_get_idle_second() from int32_t to fix since Windows does not know it without pstypes.h. Included maths.h for this but should now work flawlessly on all platforms

--- arch/include/event.h
+++ arch/include/event.h
@@ -2,6 +2,7 @@
 
 #ifndef _EVENT_H
 #define _EVENT_H
+#include "maths.h"
 
 typedef enum event_type
 {
@@ -63,6 +64,6 @@ void event_toggle_focus(int activate_focus);
 
 // See how long we were idle for
 void event_reset_idle_seconds();
-int32_t event_get_idle_seconds();
+fix event_get_idle_seconds();
 
 #endif
