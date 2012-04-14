8ba0bdc4927795d36669f6a3e9af038e59d93176
Chris Taylor <kreatordxx@dxx-rebirth.bzr.sourceforge.net>
2012-04-14 13:34:43 +0800

    Fix crash when loading a level in the editor

--- main/gamesave.c
+++ main/gamesave.c
@@ -1337,14 +1337,14 @@ int load_level(char * filename_passed)
 	 * Descent 1 - Level 19: OBERON MINE has some ugly overlapping rooms (segment 484).
 	 * HACK to make this issue less visible by moving one vertex a little.
 	 */
-	if ( !stricmp("Descent: First Strike",Current_mission_longname) && !stricmp("level19.rdl",filename) && PHYSFS_fileLength(LoadFile) == 136706)
+	if (Current_mission && !stricmp("Descent: First Strike",Current_mission_longname) && !stricmp("level19.rdl",filename) && PHYSFS_fileLength(LoadFile) == 136706)
 		Vertices[1905].z =-385*F1_0;
 	/* !!!HACK!!!
 	 * Descent 2 - Level 12: MAGNACORE STATION has a segment (104) with illegal dimensions.
 	 * HACK to fix this by moving the Vertex and fixing the associated Normals.
 	 * NOTE: This only fixes the normals of segment 104, not the other ones connected to this Vertex but this is unsignificant.
 	 */
-	if ( !stricmp("Descent 2: Counterstrike!",Current_mission_longname) && !stricmp("d2levc-4.rl2",filename)
+	if (Current_mission && !stricmp("Descent 2: Counterstrike!",Current_mission_longname) && !stricmp("d2levc-4.rl2",filename)
 		&& ( Vertices[Segments[104].verts[0]].x == -53990800 && Vertices[Segments[104].verts[0]].y == -59927741 && Vertices[Segments[104].verts[0]].z == 23034584 )
 		&& ( Segments[104].sides[1].normals[0].x == 56775 && Segments[104].sides[1].normals[0].y == -27796 && Segments[104].sides[1].normals[0].z == -17288 && Segments[104].sides[1].normals[1].x == 50157 && Segments[104].sides[1].normals[1].y == -34561 && Segments[104].sides[1].normals[1].z == -24180 )
 		&& ( Segments[104].sides[2].normals[0].x == 60867 && Segments[104].sides[2].normals[0].y == -19485 && Segments[104].sides[2].normals[0].z == -14507 && Segments[104].sides[2].normals[1].x == 55485 && Segments[104].sides[2].normals[1].y == -29668 && Segments[104].sides[2].normals[1].z == -18332 )
