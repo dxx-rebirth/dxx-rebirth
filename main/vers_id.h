cd56a79b089454bef39304217bf285154959e37b
zicodxx <zico@dxx-rebirth.com>
2012-09-01 23:29:51 +0200

    Fixed incorrect patch merge introduced in the last commit

--- main/vers_id.h
+++ main/vers_id.h
@@ -3,12 +3,12 @@
 #ifndef _VERS_ID
 #define _VERS_ID
 
-+#define __stringize2(X)	#X
-+#define __stringize(X)	__stringize2(X)
-+
-+#define D2XMAJOR __stringize(D2XMAJORi)
-+#define D2XMINOR __stringize(D2XMINORi)
-+#define D2XMICRO __stringize(D2XMICROi)
+#define __stringize2(X)	#X
+#define __stringize(X)	__stringize2(X)
+
+#define D2XMAJOR __stringize(D2XMAJORi)
+#define D2XMINOR __stringize(D2XMINORi)
+#define D2XMICRO __stringize(D2XMICROi)
 
 #define BASED_VERSION "Full Version v1.2"
 #define VERSION D2XMAJOR "." D2XMINOR "." D2XMICRO
