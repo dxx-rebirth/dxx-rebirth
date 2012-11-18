e87b7a336312a369fe53cd248ba76708e95825d3
Kp <kp@valhallalegends.com>
2012-11-18 22:15:54 +0000

    Apply strict type checks to drawer functions

--- include/3d.h
+++ include/3d.h
@@ -231,7 +231,10 @@ bool g3_draw_bitmap(vms_vector *pos,fix width,fix height,grs_bitmap *bm);
 
 //specifies 2d drawing routines to use instead of defaults.  Passing
 //NULL for either or both restores defaults
-void g3_set_special_render(void (*tmap_drawer)(),void (*flat_drawer)(),int (*line_drawer)(fix, fix, fix, fix));
+typedef void (*tmap_drawer_type)(grs_bitmap *bm,int nv,g3s_point **vertlist);
+typedef void (*flat_drawer_type)(int nv,int *vertlist);
+typedef int (*line_drawer_type)(fix x0,fix y0,fix x1,fix y1);
+void g3_set_special_render(tmap_drawer_type tmap_drawer,flat_drawer_type flat_drawer,line_drawer_type line_drawer);
 
 //Object functions:
 
