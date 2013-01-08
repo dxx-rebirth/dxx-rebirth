fb9518dd97b34e68df9b3e9d980fa1e65bf410a5
zicodxx <zico@dxx-rebirth.com>
2013-01-08 18:00:15 +0100

    Corrected ai_fire_laser_at_player() syntax where boss should not fire when Boss_dying_start_time != 0 and boss flag is set - patch by derhass

--- main/ai2.c
+++ main/ai2.c
@@ -914,7 +914,7 @@ void ai_fire_laser_at_player(object *obj, vms_vector *fire_point, int gun_num, v
 
 	//	Don't let the boss fire while in death roll.  Sorry, this is the easiest way to do this.
 	//	If you try to key the boss off obj->ctype.ai_info.dying_start_time, it will hose the endlevel stuff.
-	if (Boss_dying_start_time & Robot_info[obj->id].boss_flag)
+	if (Boss_dying_start_time && Robot_info[obj->id].boss_flag)
 		return;
 
 	//	If player is cloaked, maybe don't fire based on how long cloaked and randomness.
