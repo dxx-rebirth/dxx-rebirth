8de1f981cf29200132e52dec9c3aaebda43b8f0b
zicodxx <zico@dxx-rebirth.com>
2013-01-08 12:08:12 +0100

    Defer free of kmatrix data until window is fully gone - patch by Kp

--- main/kmatrix.c
+++ main/kmatrix.c
@@ -328,9 +328,6 @@ int kmatrix_handler(window *wind, d_event *event, kmatrix_screen *km)
 		case EVENT_WINDOW_CLOSE:
 			game_flush_inputs();
 			newmenu_free_background();
-			
-			gr_free_bitmap_data(&km->background);
-			d_free(km);
 			break;
 			
 		default:
@@ -377,4 +374,6 @@ void kmatrix_view(int network)
 	
 	while (window_exists(wind))
 		event_process();
+	gr_free_bitmap_data(&km->background);
+	d_free(km);
 }
