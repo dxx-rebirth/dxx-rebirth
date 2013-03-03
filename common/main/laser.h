3eaec8bb39a0e4b0ef9d0d4d33c98c07c0b88589
Kp <kp@valhallalegends.com>
2013-03-03 01:03:33 +0000

    Move */main/laser.h -> common/main/laser.h

--- common/main/laser.h
+++ common/main/laser.h
@@ -22,15 +22,22 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 enum weapon_type_t
 {
+#if defined(DXX_BUILD_DESCENT_II)
 	LASER_ID_L1,
 	LASER_ID = LASER_ID_L1, //0..3 are lasers
 	LASER_ID_L2,
 	LASER_ID_L3,
 	LASER_ID_L4,
+#endif
 	CONCUSSION_ID = 8,
 	FLARE_ID = 9,   //  NOTE: This MUST correspond to the ID generated at bitmaps.tbl read time.
+#if defined(DXX_BUILD_DESCENT_I)
+	LASER_ID = 10,
+#endif
 	VULCAN_ID = 11,  //  NOTE: This MUST correspond to the ID generated at bitmaps.tbl read time.
+#if defined(DXX_BUILD_DESCENT_II)
 	SPREADFIRE_ID = 12,  //  NOTE: This MUST correspond to the ID generated at bitmaps.tbl read time.
+#endif
 	PLASMA_ID = 13,  //  NOTE: This MUST correspond to the ID generated at bitmaps.tbl read time.
 	FUSION_ID = 14,  //  NOTE: This MUST correspond to the ID generated at bitmaps.tbl read time.
 	HOMING_ID = 15,
@@ -39,12 +46,14 @@ enum weapon_type_t
 	MEGA_ID = 18,
 
 	PLAYER_SMART_HOMING_ID = 19,
+#if defined(DXX_BUILD_DESCENT_I)
+	SPREADFIRE_ID = 20,
+#endif
 	SUPER_MECH_MISS = 21,
 	REGULAR_MECH_MISS = 22,
 	SILENT_SPREADFIRE_ID = 23,
 	ROBOT_SMART_HOMING_ID = 29,
-	EARTHSHAKER_MEGA_ID = 54,
-
+#if defined(DXX_BUILD_DESCENT_II)
 	SUPER_LASER_ID = 30,  // 30,31 are super lasers (level 5,6)
 	LASER_ID_L5 = SUPER_LASER_ID,
 	LASER_ID_L6,
@@ -63,9 +72,11 @@ enum weapon_type_t
 	SMART_MINE_HOMING_ID = 47,
 	ROBOT_SMART_MINE_HOMING_ID = 49,
 	ROBOT_SUPERPROX_ID = 53,
+	EARTHSHAKER_MEGA_ID = 54,
 	ROBOT_EARTHSHAKER_ID = 58,
 
 	PMINE_ID = 51,  //the mine that the designers can place
+#endif
 };
 
 // These are new defines for the value of 'flags' passed to do_laser_firing.
@@ -74,15 +85,18 @@ enum weapon_type_t
 
 #define LASER_QUAD                  1
 #define LASER_SPREADFIRE_TOGGLED    2
+
+#define MAX_LASER_LEVEL         3   // Note, laser levels are numbered from 0.
+
+#if defined(DXX_BUILD_DESCENT_II)
 #define LASER_HELIX_FLAG0           4   // helix uses 3 bits for angle
 #define LASER_HELIX_FLAG1           8   // helix uses 3 bits for angle
 #define LASER_HELIX_FLAG2           16  // helix uses 3 bits for angle
 
 #define LASER_HELIX_SHIFT       2   // how far to shift count to put in flags
 #define LASER_HELIX_MASK        7   // must match number of bits in flags
-
-#define MAX_LASER_LEVEL         3   // Note, laser levels are numbered from 0.
 #define MAX_SUPER_LASER_LEVEL   5   // Note, laser levels are numbered from 0.
+#endif
 
 #define MAX_LASER_BITMAPS   6
 
@@ -92,7 +106,11 @@ enum weapon_type_t
 // Constants governing homing missile behavior.
 // MIN_TRACKABLE_DOT gets inversely scaled by FrameTime and stuffed in
 // Min_trackable_dot
+#if defined(DXX_BUILD_DESCENT_I)
+#define	MIN_TRACKABLE_DOT					(3*F1_0/4)
+#elif defined(DXX_BUILD_DESCENT_II)
 #define MIN_TRACKABLE_DOT               (7*F1_0/8)
+#endif
 #define MAX_TRACKABLE_DIST              (F1_0*250)
 #define HOMING_MISSILE_STRAIGHT_TIME    (F1_0/8)    //  Changed as per request of John, Adam, Yuan, but mostly John
 
@@ -100,8 +118,10 @@ struct object;
 
 extern fix Min_trackable_dot;   //  MIN_TRACKABLE_DOT inversely scaled by FrameTime
 
+#if defined(DXX_BUILD_DESCENT_II)
 extern struct object *Guided_missile[];
 extern int Guided_missile_sig[];
+#endif
 
 void Laser_render(struct object *obj);
 void Laser_player_fire(struct object * obj, int type, int gun_num, int make_sound, int harmless_flag);
@@ -135,11 +155,19 @@ extern int do_laser_firing(int objnum, int weapon_id, int level, int flags, int 
 // laser.
 int Laser_create_new_easy(vms_vector * direction, vms_vector * position, int parent, int weapon_type, int make_sound);
 
+#if defined(DXX_BUILD_DESCENT_II)
 // creates a weapon object
 int create_weapon_object(int weapon_type,int segnum,vms_vector *position);
 
 // give up control of the guided missile
 void release_guided_missile(int player_num);
+
+// Omega cannon stuff.
+#define MAX_OMEGA_CHARGE    (F1_0)  //  Maximum charge level for omega cannonw
+extern fix Omega_charge;
+// NOTE: OMEGA_CHARGE_SCALE moved to laser.c to avoid long rebuilds if changed
+extern int ok_to_do_omega_damage(struct object *weapon);
+#endif
 
 extern void create_smart_children(struct object *objp, int count);
 extern int object_to_object_visibility(struct object *obj1, struct object *obj2, int trans_type);
@@ -162,15 +190,19 @@ extern int ok_to_do_omega_damage(struct object *weapon);
 
 static inline int is_proximity_bomb_or_smart_mine(enum weapon_type_t id)
 {
+#if defined(DXX_BUILD_DESCENT_II)
 	if (id == SUPERPROX_ID)
 		return 1;
+#endif
 	return id == PROXIMITY_ID;
 }
 
 static inline int is_proximity_bomb_or_smart_mine_or_placed_mine(enum weapon_type_t id)
 {
+#if defined(DXX_BUILD_DESCENT_II)
 	if (id == SUPERPROX_ID || id == PMINE_ID)
 		return 1;
+#endif
 	return id == PROXIMITY_ID;
 }
 
