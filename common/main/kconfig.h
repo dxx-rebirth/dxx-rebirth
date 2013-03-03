30d53dca165acda11479f2ec44f36c640d6f4ae8
Kp <kp@valhallalegends.com>
2013-03-03 01:03:33 +0000

    Move */main/kconfig.h -> common/main/kconfig.h

--- common/main/kconfig.h
+++ common/main/kconfig.h
@@ -8,7 +8,7 @@ SUCH USE, DISPLAY OR CREATION IS FOR NON-COMMERCIAL, ROYALTY OR REVENUE
 FREE PURPOSES.  IN NO EVENT SHALL THE END-USER USE THE COMPUTER CODE
 CONTAINED HEREIN FOR REVENUE-BEARING PURPOSES.  THE END-USER UNDERSTANDS
 AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
-COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
+COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 */
 
 /*
@@ -38,24 +38,41 @@ typedef struct _control_info {
 	ubyte fire_primary_state, fire_primary_count, fire_secondary_state, fire_secondary_count, fire_flare_count, drop_bomb_count;
 	ubyte automap_state, automap_count;
 	ubyte cycle_primary_count, cycle_secondary_count, select_weapon_count;
+#if defined(DXX_BUILD_DESCENT_II)
+	ubyte toggle_bomb_count;
+	ubyte afterburner_state, headlight_count, energy_to_shield_state;
+#endif
 	fix joy_axis[JOY_MAX_AXES], raw_joy_axis[JOY_MAX_AXES], mouse_axis[3], raw_mouse_axis[3];
 } control_info;
 
 #define CONTROL_USING_JOYSTICK	1
 #define CONTROL_USING_MOUSE		2
 #define MOUSEFS_DELTA_RANGE 512
+#if defined(DXX_BUILD_DESCENT_I)
 #define NUM_D1X_CONTROLS    30
 #define MAX_D1X_CONTROLS    30
 #define NUM_KEY_CONTROLS 50
 #define NUM_JOYSTICK_CONTROLS 48
 #define NUM_MOUSE_CONTROLS 29
 #define MAX_CONTROLS 50
+#elif defined(DXX_BUILD_DESCENT_II)
+#define NUM_D2X_CONTROLS    30
+#define MAX_D2X_CONTROLS    30
+#define NUM_KEY_CONTROLS    57
+#define NUM_JOYSTICK_CONTROLS  56
+#define NUM_MOUSE_CONTROLS  30
+#define MAX_CONTROLS        60		// there are actually 48, so this leaves room for more
+#endif
 
 extern control_info Controls;
 extern void kconfig_read_controls(d_event *event, int automap_flag);
 extern void kconfig(int n, char *title);
 
+#if defined(DXX_BUILD_DESCENT_I)
 extern const ubyte DefaultKeySettingsD1X[MAX_D1X_CONTROLS];
+#elif defined(DXX_BUILD_DESCENT_II)
+extern const ubyte DefaultKeySettingsD2X[MAX_D2X_CONTROLS];
+#endif
 extern const ubyte DefaultKeySettings[3][MAX_CONTROLS];
 
 extern void kc_set_controls();
