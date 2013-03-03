d920420cf6c02494b464a8246cba470c033ca32b
Kp <kp@valhallalegends.com>
2013-03-03 01:01:28 +0000

    Remove unnecessary program-specific references in pstypes.h

--- include/pstypes.h
+++ include/pstypes.h
@@ -90,15 +90,11 @@ defined(__LITTLE_ENDIAN__)	// from physfs_internal.h
 # pragma options align=packed
 # define __pack__
 #else
-# error d1x will not work without packed structures
+# error "This program will not work without packed structures"
 #endif
 
 #ifdef _MSC_VER
 # define inline __inline
-#endif
-
-#ifndef PACKAGE_STRING
-# define PACKAGE_STRING "d1x"
 #endif
 
 #endif //_TYPES_H
