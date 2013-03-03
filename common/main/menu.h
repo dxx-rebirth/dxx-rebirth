4bf4710aff365283ae9cc2a85bf100405eac0ca4
Kp <kp@valhallalegends.com>
2013-03-03 01:03:33 +0000

    Move */main/menu.h -> common/main/menu.h

--- common/main/menu.h
+++ common/main/menu.h
@@ -8,7 +8,7 @@ SUCH USE, DISPLAY OR CREATION IS FOR NON-COMMERCIAL, ROYALTY OR REVENUE
 FREE PURPOSES.  IN NO EVENT SHALL THE END-USER USE THE COMPUTER CODE
 CONTAINED HEREIN FOR REVENUE-BEARING PURPOSES.  THE END-USER UNDERSTANDS
 AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
-COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
+COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 */
 
 /*
@@ -30,9 +30,19 @@ extern int RegisterPlayer();
 extern int DoMenu();
 extern void do_options_menu();
 extern int select_demo(void);
+
+#if defined(DXX_BUILD_DESCENT_I)
 #define Menu_pcx_name (((SWIDTH>=640&&SHEIGHT>=480) && PHYSFSX_exists("menuh.pcx",1))?"menuh.pcx":"menu.pcx")
 #define STARS_BACKGROUND (((SWIDTH>=640&&SHEIGHT>=480) && PHYSFSX_exists("starsb.pcx",1))?"starsb.pcx":"stars.pcx")
+#elif defined(DXX_BUILD_DESCENT_II)
+#define MENU_PCX_MAC_SHARE ("menub.pcx")
+#define MENU_PCX_SHAREWARE ("menud.pcx")
+#define MENU_PCX_OEM (HIRESMODE?"menuob.pcx":"menuo.pcx")
+#define MENU_PCX_FULL (HIRESMODE?"menub.pcx":"menu.pcx")
 
-extern char *menu_difficulty_text[];
-
+// name of background bitmap
+#define Menu_pcx_name (PHYSFSX_exists(MENU_PCX_FULL,1)?MENU_PCX_FULL:(PHYSFSX_exists(MENU_PCX_OEM,1)?MENU_PCX_OEM:PHYSFSX_exists(MENU_PCX_SHAREWARE,1)?MENU_PCX_SHAREWARE:MENU_PCX_MAC_SHARE))
+#define STARS_BACKGROUND ((HIRESMODE && PHYSFSX_exists("starsb.pcx",1))?"starsb.pcx":PHYSFSX_exists("stars.pcx",1)?"stars.pcx":"starsb.pcx")
 #endif
+
+#endif /* _MENU_H */
