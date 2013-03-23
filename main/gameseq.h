d5bae9346922a038b49000fd78020f437be59a9e
Kp <kp@valhallalegends.com>
2013-03-23 21:01:48 +0000

    Add unused init_player_stats_level parameter 'secret_flag' to match D2X

--- main/gameseq.h
+++ main/gameseq.h
@@ -90,7 +90,7 @@ extern void draw_high_scores(int place);
 extern int add_player_to_high_scores(player *pp);
 extern void input_name (int place);
 extern int reset_high_scores();
-extern void init_player_stats_level();
+extern void init_player_stats_level(int secret_flag);
 
 void open_message_window(void);
 void close_message_window(void);
