d1a095f916c94dce05df9388f13221af055d6305
Kp <kp@valhallalegends.com>
2013-03-24 23:38:52 +0000

    Remove unused hostage code

--- main/hostage.h
+++ main/hostage.h
@@ -120,7 +120,6 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #define MAX_HOSTAGE_TYPES		1
 #define MAX_HOSTAGES				10		//max per any one level
 #define HOSTAGE_MESSAGE_LEN	30
-#define HOSTAGE_MAX_GLOBALS	10
 
 // 1 per hostage
 typedef struct hostage_data {
@@ -132,12 +131,7 @@ typedef struct hostage_data {
 	char		text[HOSTAGE_MESSAGE_LEN];
 } hostage_data;
 
-extern char Hostage_global_message[HOSTAGE_MAX_GLOBALS][HOSTAGE_MESSAGE_LEN];
-extern int Hostage_num_globals;
-
 extern int N_hostage_types;
-
-extern int Num_hostages;
 
 extern int Hostage_vclip_num[MAX_HOSTAGE_TYPES];    // for each type of hostage
 
@@ -145,14 +139,12 @@ extern hostage_data Hostages[MAX_HOSTAGES];
 
 void draw_hostage(object *obj);
 void hostage_rescue( int hostage_number );
-void hostage_init();
 
 //returns true if something drew
 int do_hostage_effects();
 
 void hostage_init_all();
 void hostage_compress_all();
-int hostage_get_next_slot();
 int hostage_is_valid( int hostage_num );
 int hostage_object_is_valid( int objnum  );
 void hostage_init_info( int objnum );
