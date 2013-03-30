524d76e0f318762af3c64ee0bf3bda9971d87f3e
Kp <kp@valhallalegends.com>
2013-03-30 15:11:42 +0000

    Combine date,time into Descent version con_printf

--- main/inferno.c
+++ main/inferno.c
@@ -359,11 +359,7 @@ int main(int argc, char *argv[])
 	load_text();
 
 	//print out the banner title
-	con_printf(CON_NORMAL, "%s", DESCENT_VERSION); // D2X version
-	if (PHYSFSX_exists(MISSION_DIR "d2x.hog",1)) {
-		con_printf(CON_NORMAL, "  Vertigo Enhanced");
-	}
-	con_printf(CON_NORMAL, "  %s %s\n", __DATE__,__TIME__);
+	con_printf(CON_NORMAL, "%s%s  %s %s\n", DESCENT_VERSION, PHYSFSX_exists(MISSION_DIR "d2x.hog",1) ? "  Vertigo Enhanced" : "", __DATE__,__TIME__); // D2X version
 	con_printf(CON_NORMAL, "This is a MODIFIED version of Descent 2, based on %s.\n", BASED_VERSION);
 	con_printf(CON_NORMAL, "%s\n%s\n",TXT_COPYRIGHT,TXT_TRADEMARK);
 	con_printf(CON_NORMAL, "Copyright (C) 1999 Peter Hawkins, 2002 Bradley Bell, 2005-2011 Christian Beckhaeuser\n\n");
