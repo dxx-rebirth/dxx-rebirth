b776c96e8ad15cc812c1931645be60eef22e9e6b
Kp <kp@valhallalegends.com>
2013-06-23 16:24:43 +0000

    Make newdemo_write_string const correct

--- main/newdemo.c
+++ main/newdemo.c
@@ -257,7 +257,7 @@ int newdemo_find_object( int signature )
 	return -1;
 }
 
-int newdemo_write( void *buffer, int elsize, int nelem )
+int newdemo_write(const void *buffer, int elsize, int nelem )
 {
 	int num_written, total_size;
 
@@ -296,7 +296,7 @@ static void nd_write_int(int i)
 	newdemo_write(&i, 4, 1);
 }
 
-static void nd_write_string(char *str)
+static void nd_write_string(const char *str)
 {
 	nd_write_byte(strlen(str) + 1);
 	newdemo_write(str, strlen(str) + 1, 1);
@@ -1048,7 +1048,7 @@ void newdemo_record_control_center_destroyed()
 	start_time();
 }
 
-void newdemo_record_hud_message( char * message )
+void newdemo_record_hud_message(const char * message )
 {
 	stop_time();
 	nd_write_byte( ND_EVENT_HUD_MESSAGE );
