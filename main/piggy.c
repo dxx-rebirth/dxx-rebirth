dxx-rebirth/dxx-rebirth

commit bb57521ef11b6fab96dbdaaddd12eea1d9660cde

re-init digi system after re-setting sample rate for shareware

main/piggy.c
@@ -765,6 +765,8 @@ int read_hamfile()
 		{
 			shareware = 1;
 			GameArg.SndDigiSampleRate = SAMPLE_RATE_11K;
+			digi_close();
+			digi_init();
 		}
 	}
 

commit 8534b9705ba49ee135a13aba95a6dcf5953ea6e9

fixed regression introduced in bb57521ef1 causing the game to crash when trying to incorrectly re-init SDL_mixer for change in sample rate - which isn't needed in the first place

main/piggy.c
@@ -765,8 +765,13 @@ int read_hamfile()
 		{
 			shareware = 1;
 			GameArg.SndDigiSampleRate = SAMPLE_RATE_11K;
-			digi_close();
-			digi_init();
+#ifdef USE_SDLMIXER
+			if (GameArg.SndDisableSdlMixer)
+#endif
+			{
+				digi_close();
+				digi_init();
+			}
 		}
 	}
 