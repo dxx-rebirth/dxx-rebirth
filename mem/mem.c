dxx-rebirth/dxx-rebirth

commit 470dc4b1e245a4bb00c86042805b39d5b4cecd7e

Memory debug improvements

mem/mem.c
@@ -86,7 +86,7 @@ void PrintInfo( int id )
 }
 
 
-void * mem_malloc( unsigned int size, const char * var, const char * filename, int line, int fill_zero )
+void * mem_malloc( unsigned int size, const char * var, const char * filename, unsigned line)
 {
 	int i, id;
 	void *ptr;
@@ -152,11 +152,22 @@ void * mem_malloc( unsigned int size, const char * var, const char * filename, i
 	for (i=0; i<CHECKSIZE; i++ )
 		pc[size+i] = CHECKBYTE;
 
-	if (fill_zero)
-		memset( ptr, 0, size );
-
 	return ptr;
+}
 
+void *(mem_calloc)( size_t nmemb, size_t size, const char * var, const char * filename, unsigned line)
+{
+	size_t threshold = 1, request = nmemb * size;
+	threshold <<= (4 * sizeof(threshold));
+	if ((nmemb | size) >= threshold) {
+		/* possible overflow condition */
+		if (request / size != nmemb)
+			request = ~(size_t)0;
+	}
+	void *ptr = mem_malloc(request, var, filename, line);
+	if (ptr)
+		memset( ptr, 0, request );
+	return ptr;
 }
 
 int mem_find_id( void * buffer )
@@ -262,9 +273,9 @@ void *mem_realloc(void * buffer, unsigned int size, const char * var, const char
 		mem_free(buffer);
 		newbuffer = NULL;
 	} else if (buffer == NULL) {
-		newbuffer = mem_malloc(size, var, filename, line, 0);
+		newbuffer = mem_malloc(size, var, filename, line);
 	} else {
-		newbuffer = mem_malloc(size, var, filename, line, 0);
+		newbuffer = mem_malloc(size, var, filename, line);
 		if (newbuffer != NULL) {
 			id = mem_find_id(buffer);
 			if (MallocSize[id] < size)

include/u_mem.h
@@ -22,30 +22,32 @@ void mem_init(void);
 
 #if !defined(NDEBUG)
 void mem_display_blocks();
-extern void * mem_malloc( unsigned int size, const char * var, const char * file, int line, int fill_zero );
+extern void * mem_malloc( unsigned int size, const char * var, const char * file, unsigned line);
+void * mem_calloc( size_t nmemb, size_t size, const char * var, const char * filename, unsigned line);
 extern void * mem_realloc( void * buffer, unsigned int size, const char * var, const char * file, int line );
 extern void mem_free( void * buffer );
 
 /* DPH: Changed malloc, etc. to d_malloc. Overloading system calls is very evil and error prone */
-#define d_malloc(size)      mem_malloc((size),"Unknown", __FILE__,__LINE__, 0 )
-#define d_calloc(n,size)    mem_malloc((n*size),"Unknown", __FILE__,__LINE__, 1 )
-#define d_realloc(ptr,size) mem_realloc((ptr),(size),"Unknown", __FILE__,__LINE__ )
-#define d_free(ptr)         do{ mem_free(ptr); ptr=NULL; } while(0)
-
-#define MALLOC( var, type, count )   (var=(type *)mem_malloc((count)*sizeof(type),#var, __FILE__,__LINE__,0 ))
 
 // Checks to see if any blocks are overwritten
 void mem_validate_heap();
 
+#define mem_calloc(nmemb,size,var,file,line)	((mem_calloc)((size) ? (nmemb) : 0, (size), (var), (file), (line)))
+
 #else
 
-#define d_malloc(size)      malloc(size)
-#define d_calloc(n, size)   calloc(n, size)
-#define d_realloc(ptr,size) realloc(ptr,size)
-#define d_free(ptr)         do{ free(ptr); ptr=NULL; } while(0)
-
-#define MALLOC( var, type, count )   (var=(type *)malloc((count)*sizeof(type)))
+#define mem_malloc(size,var,file,line)	malloc((size))
+#define mem_calloc(nmemb,size,var,file,line)	calloc((nmemb), (size))
+#define mem_realloc(ptr,size,var,file,line)	realloc((ptr),(size))
+#define mem_free	free
 
 #endif
+
+#define MALLOC( var, type, count )	(var=(type *)mem_malloc((count)*sizeof(type),#var, __FILE__,__LINE__ ))
+
+#define d_malloc(size)      mem_malloc((size),"Unknown", __FILE__,__LINE__ )
+#define d_calloc(nmemb,size)    mem_calloc((nmemb),(size),"Unknown", __FILE__,__LINE__ )
+#define d_realloc(ptr,size) mem_realloc((ptr),(size),"Unknown", __FILE__,__LINE__ )
+#define d_free(ptr)         (mem_free(ptr), ptr=NULL)
 
 #endif // _U_MEM_H

commit a76211bc7e8c498dd0ebb964e3fc680b17a0f12c

Remove unused mem_malloc,mem_free
Commit 470dc4b broke debug=0 builds because debug=0 has its own unused
implementation of mem_malloc.  Delete the unused implementation.

mem/mem.c
@@ -377,96 +377,6 @@ void mem_init()
 	atexit(mem_display_blocks);
 }
 
-void * mem_malloc( unsigned int size, char * var, char * filename, int line, int fill_zero )
-{
-	size_t base;
-	void *ptr;
-	int * psize;
-
-	if (Initialized==0)
-		mem_init();
-
-#if MEMSTATS
-	{
-		unsigned long	theFreeMem = 0;
-	
-		if (sMemStatsFileInitialized)
-		{
-			theFreeMem = FreeMem();
-		
-			fprintf(sMemStatsFile,
-					"\n%9u bytes free before attempting: MALLOC %9u bytes.",
-					theFreeMem,
-					size);
-		}
-	}
-#endif	// end of ifdef memstats
-
-	if (size==0)	{
-		con_printf(CON_CRITICAL, "\nMEM_MALLOC_ZERO: Attempting to malloc 0 bytes.\n" );
-		con_printf(CON_CRITICAL, "\tVar %s, file %s, line %d.\n", var, filename, line );
-		Error( "MEM_MALLOC_ZERO" );
-		Int3();
-	}
-
-	ptr = malloc( size + CHECKSIZE );
-
-	if (ptr==NULL)	{
-		con_printf(CON_CRITICAL, "\nMEM_OUT_OF_MEMORY: Malloc returned NULL\n" );
-		con_printf(CON_CRITICAL, "\tVar %s, file %s, line %d.\n", var, filename, line );
-		Error( "MEM_OUT_OF_MEMORY" );
-		Int3();
-	}
-
-	base = (size_t)ptr;
-	if ( base < SmallestAddress ) SmallestAddress = base;
-	if ( (base+size) > LargestAddress ) LargestAddress = base+size;
-
-
-	psize = (int *)ptr;
-	psize--;
-	BytesMalloced += *psize;
-
-	if (fill_zero)
-		memset( ptr, 0, size );
-
-	return ptr;
-}
-
-void mem_free( void * buffer )
-{
-	int * psize = (int *)buffer;
-	psize--;
-
-	if (Initialized==0)
-		mem_init();
-
-#if MEMSTATS
-	{
-		unsigned long	theFreeMem = 0;
-	
-		if (sMemStatsFileInitialized)
-		{
-			theFreeMem = FreeMem();
-		
-			fprintf(sMemStatsFile,
-					"\n%9u bytes free before attempting: FREE", theFreeMem);
-		}
-	}
-#endif	// end of ifdef memstats
-
-	if (buffer==NULL)	{
-		con_printf(CON_CRITICAL, "\nMEM_FREE_NULL: An attempt was made to free the null pointer.\n" );
-		Warning( "MEM: Freeing the NULL pointer!" );
-		Int3();
-		return;
-	}
-
-	BytesMalloced -= *psize;
-
-	free( buffer );
-}
-
 void mem_display_blocks()
 {
 	if (Initialized==0) return;