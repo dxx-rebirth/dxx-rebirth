c0552c0dfa7331bb1ee9a8d4e9f4590198913402
Kp <kp@valhallalegends.com>
2013-10-06 03:05:57 +0000

    Add realloc casts

--- similar/main/songs.c
+++ similar/main/songs.c
@@ -112,7 +112,7 @@ void songs_init()
 			PHYSFSX_fgets(inputline, sizeof(inputline), fp );
 			if ( strlen( inputline ) )
 			{
-				BIMSongs = d_realloc(BIMSongs, sizeof(bim_song_info)*(i+1));
+				BIMSongs = (struct bim_song_info *) d_realloc(BIMSongs, sizeof(bim_song_info)*(i+1));
 				memset(BIMSongs[i].filename, '\0', sizeof(BIMSongs[i].filename));
 				sscanf( inputline, "%15s", BIMSongs[i].filename );
 
@@ -133,7 +133,7 @@ void songs_init()
 		// HACK: If Descent.hog is patched from 1.0 to 1.5, descent.sng is turncated. So let's patch it up here
 		if (i==12 && PHYSFSX_fsize("descent.sng")==422)
 		{
-			BIMSongs = d_realloc(BIMSongs, sizeof(bim_song_info)*(i+15));
+			BIMSongs = (struct bim_song_info *) d_realloc(BIMSongs, sizeof(bim_song_info)*(i+15));
 			for (i = 12; i <= 26; i++)
 				snprintf(BIMSongs[i].filename, sizeof(BIMSongs[i].filename), "game%02d.hmp", i-4);
 		}
