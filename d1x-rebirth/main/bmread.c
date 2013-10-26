6a8859b3a55a72f2ff455bbca6d029207d7f8abf
Kp <kp@valhallalegends.com>
2013-10-26 19:40:56 +0000

    Remove unused multi.h include in bmread.c

--- d1x-rebirth/main/bmread.c
+++ d1x-rebirth/main/bmread.c
@@ -36,7 +36,6 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "wall.h"
 #include "textures.h"
 #include "game.h"
-#include "multi.h"
 #include "iff.h"
 #include "hostage.h"
 #include "powerup.h"
