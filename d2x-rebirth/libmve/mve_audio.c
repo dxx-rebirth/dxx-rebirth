005ba16ff948d2bd0e56e94ad98b4f35003740b1
Kp <kp@valhallalegends.com>
2013-10-26 19:19:39 +0000

    Include mve_audio.h in mve_audio.c

--- d2x-rebirth/libmve/mve_audio.c
+++ d2x-rebirth/libmve/mve_audio.c
@@ -1,3 +1,5 @@
+#include "mve_audio.h"
+
 static int audio_exp_table[256] =
 {
          0,      1,      2,      3,      4,      5,      6,      7,      8,      9,     10,     11,     12,     13,     14,     15,
