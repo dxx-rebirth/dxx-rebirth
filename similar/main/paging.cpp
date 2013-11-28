2ca4c7ed3e1943e37a96e9c8c5a5a741862d3430
Kp <kp@valhallalegends.com>
2013-11-28 00:23:45 +0000

    Make super_boss_gate_type_list const

--- similar/main/paging.cpp
+++ similar/main/paging.cpp
@@ -145,7 +145,7 @@ static void paging_touch_weapon( int weapon_type )
 
 
 
-sbyte super_boss_gate_type_list[13] = {0, 1, 8, 9, 10, 11, 12, 15, 16, 18, 19, 20, 22 };
+static const sbyte super_boss_gate_type_list[13] = {0, 1, 8, 9, 10, 11, 12, 15, 16, 18, 19, 20, 22 };
 
 static void paging_touch_robot( int robot_index )
 {
