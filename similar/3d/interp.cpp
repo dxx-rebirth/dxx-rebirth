dxx-rebirth/dxx-rebirth

commit 47c00c7128d101d701f849322a79222bef7ffdea

Added code which draws activated headlights or their absence to g3_draw_polygon_model. This was never implemented in D2X - probably because most of interp.c was adopted from D1X.

similar/3d/interp.cpp
@@ -416,28 +416,36 @@ bool g3_draw_polygon_model(ubyte *p,grs_bitmap **model_bitmaps,vms_angvec *anim_
 #if defined(DXX_BUILD_DESCENT_I)
 					gr_setcolor(w(p+28));
 #elif defined(DXX_BUILD_DESCENT_II)
+					if (glow_values[glow_num] == -2)
+						gr_setcolor(255);
+					else
+					{
 #ifdef FADE_FLATPOLY
-					short c;
-					unsigned char cc;
-					int l;
+						short c;
+						unsigned char cc;
+						int l;
 #endif
 #ifndef FADE_FLATPOLY
-					gr_setcolor(gr_find_closest_color_15bpp(w(p + 28)));
+						gr_setcolor(gr_find_closest_color_15bpp(w(p + 28)));
 #else
-					//l = (32 * model_light) >> 16;
-					l = f2i(fixmul(i2f(32), (model_light.r+model_light.g+model_light.b)/3));
-					if (l<0) l = 0;
-					else if (l>32) l = 32;
-					cc = gr_find_closest_color_15bpp(w(p+28));
-					c = gr_fade_table[(l<<8)|cc];
-					gr_setcolor(c);
+						//l = (32 * model_light) >> 16;
+						l = f2i(fixmul(i2f(32), (model_light.r+model_light.g+model_light.b)/3));
+						if (l<0) l = 0;
+						else if (l>32) l = 32;
+						cc = gr_find_closest_color_15bpp(w(p+28));
+						c = gr_fade_table[(l<<8)|cc];
+						gr_setcolor(c);
 #endif
+					}
 #endif
 
 					for (i=0;i<nv;i++)
 						point_list[i] = Interp_point_list + wp(p+30)[i];
 
-					g3_draw_poly(nv,point_list);
+#if defined(DXX_BUILD_DESCENT_II)
+					if (glow_values[glow_num] != -3)
+#endif
+						g3_draw_poly(nv,point_list);
 				}
 
 				p += 30 + ((nv&~1)+1)*2;

similar/main/object.cpp
@@ -385,9 +385,9 @@ static void draw_polygon_object(object *obj)
 	g3s_lrgb light;
 	int	imsave;
 #if defined(DXX_BUILD_DESCENT_I)
-	fix engine_glow_value[1];
+	fix engine_glow_value[1] = { 0 };
 #elif defined(DXX_BUILD_DESCENT_II)
-	fix engine_glow_value[2];		//element 0 is for engine glow, 1 for headlight
+	fix engine_glow_value[2] = { 0, -1 };		//element 0 is for engine glow, 1 for headlight
 #endif
 
 	light = compute_object_light(obj,NULL);

commit 77c0b58591f49c12e6b33fe67871b44b5e1e2b2c

Fix editor crash introduced in 47c00c712
glow_values may be NULL, so check before dereferencing it.

similar/3d/interp.cpp
@@ -416,7 +416,7 @@ bool g3_draw_polygon_model(ubyte *p,grs_bitmap **model_bitmaps,vms_angvec *anim_
 #if defined(DXX_BUILD_DESCENT_I)
 					gr_setcolor(w(p+28));
 #elif defined(DXX_BUILD_DESCENT_II)
-					if (glow_values[glow_num] == -2)
+					if (glow_values && glow_values[glow_num] == -2)
 						gr_setcolor(255);
 					else
 					{
@@ -440,7 +440,7 @@ bool g3_draw_polygon_model(ubyte *p,grs_bitmap **model_bitmaps,vms_angvec *anim_
 						point_list[i] = Interp_point_list + wp(p+30)[i];
 
 #if defined(DXX_BUILD_DESCENT_II)
-					if (glow_values[glow_num] != -3)
+					if (!glow_values || glow_values[glow_num] != -3)
 #endif
 						g3_draw_poly(nv,point_list);
 				}