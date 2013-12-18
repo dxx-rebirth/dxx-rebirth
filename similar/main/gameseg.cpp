0de7bdc24cb034014ff623ac6b9ba905beafefc8
Kp <kp@valhallalegends.com>
2013-12-18 23:58:43 +0000

    Use visited_segment_bitarray_t in trace_segs

--- similar/main/gameseg.cpp
+++ similar/main/gameseg.cpp
@@ -658,14 +658,13 @@ int	Doing_lighting_hack_flag=0;
 
 // figure out what seg the given point is in, tracing through segments
 // returns segment number, or -1 if can't find segment
-static int trace_segs(const vms_vector *p0, int oldsegnum, int recursion_count)
+static int trace_segs(const vms_vector *p0, int oldsegnum, int recursion_count, visited_segment_bitarray_t &visited)
 {
 	int centermask;
 	segment *seg;
 	fix side_dists[6];
 	fix biggest_val;
 	int sidenum, bit, check, biggest_side;
-	static ubyte visited [MAX_SEGMENTS];
 
 	Assert((oldsegnum <= Highest_segment_index) && (oldsegnum >= 0));
 
@@ -673,11 +672,9 @@ static int trace_segs(const vms_vector *p0, int oldsegnum, int recursion_count)
 		con_printf (CON_DEBUG, "trace_segs: Segment not found");
 		return -1;
 	}
-	if (recursion_count == 0)
-		memset (visited, 0, sizeof (visited));
 	if (visited [oldsegnum])
 		return -1;
-	visited [oldsegnum] = 1;
+	visited[oldsegnum] = true;
 
 	centermask = get_side_dists(p0,oldsegnum,side_dists);		//check old segment
 	if (centermask == 0) // we are in the old segment
@@ -699,7 +696,7 @@ static int trace_segs(const vms_vector *p0, int oldsegnum, int recursion_count)
 
 			side_dists[biggest_side] = 0;
 			// trace into adjacent segment:
-			check = trace_segs(p0, seg->children[biggest_side], recursion_count + 1);
+			check = trace_segs(p0, seg->children[biggest_side], recursion_count + 1, visited);
 			if (check >= 0)		//we've found a segment
 				return check;
 	}
@@ -722,7 +719,8 @@ int find_point_seg(const vms_vector *p,int segnum)
 	Assert((segnum <= Highest_segment_index) && (segnum >= -1));
 
 	if (segnum != -1) {
-		newseg = trace_segs(p, segnum, 0);
+		visited_segment_bitarray_t visited;
+		newseg = trace_segs(p, segnum, 0, visited);
 
 		if (newseg != -1)			//we found a segment!
 			return newseg;
