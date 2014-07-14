b97ff9d4968450d5c806eb6988c5e12547a48905
Kp <kp@valhallalegends.com>
2014-07-14 02:12:39 +0000

    Simplify cstring_tie

--- common/include/varutil.h
+++ common/include/varutil.h
@@ -14,27 +14,18 @@
 template <std::size_t N>
 class cstring_tie
 {
-	void init_array(const char *s) __attribute_nonnull()
-	{
-		p[m_count++] = s;
-	}
-	template <typename... Args>
-		void init_array(const char *s, Args&&... args)
-		{
-			init_array(s);
-			init_array(std::forward<Args>(args)...);
-		}
 public:
+	static const std::size_t maximum_arity = N;
+	typedef array<const char *, maximum_arity> array_t;
 	template <typename... Args>
-		cstring_tie(Args&&... args) : m_count(0)
+		cstring_tie(Args&&... args) : m_count(sizeof...(Args))
 	{
 		static_assert(sizeof...(Args) <= maximum_arity, "too many arguments to cstring_tie");
-		init_array(std::forward<Args>(args)...);
+		p = array_t{{args...}};
 	}
 	unsigned count() const { return m_count; }
 	const char *string(std::size_t i) const { return p[i]; }
-	enum { maximum_arity = N };
 private:
-	array<const char *, maximum_arity> p;
+	array_t p;
 	unsigned m_count;
 };
