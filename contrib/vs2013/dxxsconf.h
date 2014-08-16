afd26ccd75a00cad9e69614e7b4ceb45c8636aba
Kp <kp@valhallalegends.com>
2014-08-16 17:02:21 +0000

    Add VS2013 build files
    This does not compile yet, but commit the files to save others the work
    of creating the framework.
    
    Known issues:
    - Some editor-specific files do not find their headers due to missing
      AdditionalIncludeDirectories
    - VS2013.2 picks incorrect variadic template overloads.  VS2013.3 might
      do better.
    - VS2013.2 C preprocessor mishandles C99 __VA_ARGS__ usage

---
+++ contrib/vs2013/dxxsconf.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#define __attribute_alloc_size(A,...)
+#define __attribute_format_printf(A,B)
+#define __attribute_format_arg(A)
+#define __attribute_malloc()
+#define __attribute_nonnull(...)
+#define __attribute_used
+#define __attribute_warn_unused_result
+#define DXX_HAVE_CXX_ARRAY
+#define DXX_HAVE_CXX11_ADDRESSOF
+#define DXX_HAVE_CXX11_RANGE_FOR
+#define DXX_HAVE_CXX11_STATIC_ASSERT
+#define DXX_HAVE_CXX11_TYPE_TRAITS
+#define DXX_HAVE_CXX11_EXPLICIT_DELETE
+#define DXX_CXX11_EXPLICIT_DELETE =delete
+#define DXX_HAVE_CXX11_TEMPLATE_ALIAS
+#define dxx_explicit_operator_bool explicit
+#define DXX_HAVE_CXX11_BEGIN
+#define dxx_builtin_constant_p(A)	(false)
+#define DXX_INHERIT_CONSTRUCTORS(D,B,...)	\
+	template <typename... Args> \
+		D(Args&&... args) : \
+			B,##__VA_ARGS__(std::forward<Args>(args)...) {}
+
+#define __func__ __FUNCTION__
+typedef signed long ssize_t;
