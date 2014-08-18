ce8165fc6c704f2c058d5eb1562d48dabdfdef56
Kp <kp@valhallalegends.com>
2014-08-18 03:00:50 +0000

    Add u_mem.h to mem.cpp to fix memdebug=1 build

--- similar/mem/mem.cpp
+++ similar/mem/mem.cpp
@@ -31,6 +31,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "dxxerror.h"
 #include "args.h"
 #include "console.h"
+#include "u_mem.h"
 
 #define MEMSTATS 0
 #define FULL_MEM_CHECKING 1
