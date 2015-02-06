e0b3f4f1b4417b512fb4ea8c1cff0bc9ca57706c
Bradley Bell <bradley.bell@gmail.com>
2015-02-05 16:54:15 -0800

    use <cstdlib> instead of <stdlib.h> to use definitions in the std namespace

--- similar/main/gamecntl.cpp
+++ similar/main/gamecntl.cpp
@@ -27,7 +27,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #include <algorithm>
 #include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
 #include <string.h>
 #include <stdarg.h>
 
