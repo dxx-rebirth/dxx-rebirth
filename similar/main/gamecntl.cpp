dxx-rebirth/dxx-rebirth

commit 7338bb2402ee27a19dfe137e47ec0efa473638d9

Extend in-game esc menu with options and saving

similar/main/gamecntl.cpp
@@ -714,12 +714,18 @@ static window_event_result HandleSystemKey(int key)
 
 			case KEY_ESC:
 			{
-				int choice;
-				choice=nm_messagebox( NULL, 2, TXT_YES, TXT_NO, TXT_ABORT_GAME );
-				if (choice == 0)
+				const auto choice = nm_messagebox(NULL, 4, "Abort Game", TXT_OPTIONS_, "Save Game...", TXT_LOAD_GAME, "Game Menu");
+				switch(choice)
 				{
-					window_close(Game_wind);
-					return window_event_result::close;
+					case 0:
+						window_close(Game_wind);
+						return window_event_result::close;
+					case 1:
+						return HandleSystemKey(KEY_F2);
+					case 2:
+						return HandleSystemKey(KEY_ALTED | KEY_F2);
+					case 3:
+						return HandleSystemKey(KEY_ALTED | KEY_F3);
 				}
 				return window_event_result::handled;
 			}

commit 4ab72a4d9a1152d33e19d63b53a9151672548371

Hide save/load game in non-coop multiplayer
Multiplayer non-coop cannot save, so offering the options is pointless.

Fixes: 7338bb2402ee27a19dfe137e47ec0efa473638d9 ("Extend in-game esc menu with options and saving")
Reported by: zicodxx @ https://github.com/dxx-rebirth/dxx-rebirth/issues/42

similar/main/gamecntl.cpp
@@ -718,7 +718,8 @@ static window_event_result HandleSystemKey(int key)
 
 			case KEY_ESC:
 			{
-				const auto choice = nm_messagebox(NULL, 4, "Abort Game", TXT_OPTIONS_, "Save Game...", TXT_LOAD_GAME, "Game Menu");
+				const bool allow_saveload = !(Game_mode & GM_MULTI) || (Game_mode & GM_MULTI_COOP);
+				const auto choice = nm_messagebox_str(nullptr, allow_saveload ? nm_messagebox_tie("Abort Game", TXT_OPTIONS_, "Save Game...", TXT_LOAD_GAME) : nm_messagebox_tie("Abort Game", TXT_OPTIONS_), "Game Menu");
 				switch(choice)
 				{
 					case 0: