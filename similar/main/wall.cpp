dxx-rebirth/dxx-rebirth

commit fd01d4c6737e1352ce3727d9f52b5c2ba658c386

Reorder wall_is_doorway to favor likely results

similar/main/wall.cpp
@@ -70,22 +70,20 @@ const char	Wall_names[7][10] = {
 };
 #endif
 
+static std::pair<uint_fast32_t, uint_fast32_t> get_transparency_check_values(const side &side)
+{
+	if (uint_fast32_t masked_tmap_num2 = side.tmap_num2 & 0x3FFF)
+		return {masked_tmap_num2, BM_FLAG_SUPER_TRANSPARENT};
+	return {side.tmap_num, BM_FLAG_TRANSPARENT};
+}
+
 // This function determines whether the current segment/side is transparent
 //		1 = YES
 //		0 = NO
-static int check_transparency(const vcsegptr_t seg, int side )
+static uint_fast32_t check_transparency(const side &side)
 {
-	if ( (seg->sides[side].tmap_num2 & 0x3FFF) == 0) {
-		if (GameBitmaps[Textures[seg->sides[side].tmap_num].index].bm_flags & BM_FLAG_TRANSPARENT )
-			return 1;
-		else
-			return 0;
-		}
-
-	if (GameBitmaps[Textures[seg->sides[side].tmap_num2 & 0x3FFF ].index].bm_flags & BM_FLAG_SUPER_TRANSPARENT )
-		return 1;
-	else
-		return 0;
+	auto v = get_transparency_check_values(side);
+	return GameBitmaps[Textures[v.first].index].bm_flags & v.second;
 }
 
 //-----------------------------------------------------------------
@@ -101,34 +99,27 @@ static int check_transparency(const vcsegptr_t seg, int side )
 //		WID_ILLUSORY_WALL			3	//	1/1/0		illusory wall
 //		WID_TRANSILLUSORY_WALL	7	//	1/1/1		transparent illusory wall
 //		WID_NO_WALL					5	//	1/0/1		no wall, can fly through
-WALL_IS_DOORWAY_result_t wall_is_doorway ( const vcsegptr_t  seg, int side )
+WALL_IS_DOORWAY_result_t wall_is_doorway(const side &side)
 {
-	int flags, type;
-	int state;
-//--Covered by macro	// No child.
-//--Covered by macro	if (seg->children[side] == -1)
-//--Covered by macro		return WID_WALL;
-
-//--Covered by macro	if (seg->children[side] == -2)
-//--Covered by macro		return WID_EXTERNAL_FLAG;
-
-//--Covered by macro // No wall present.
-//--Covered by macro	if (seg->sides[side].wall_num == -1)
-//--Covered by macro		return WID_NO_WALL;
-
-	Assert(side>=0 && side<6);
-
-	type = Walls[seg->sides[side].wall_num].type;
-	flags = Walls[seg->sides[side].wall_num].flags;
-
+	const auto &w = Walls[side.wall_num];
+	const auto type = w.type;
 	if (type == WALL_OPEN)
 		return WID_NO_WALL;
 
+	if (likely(type == WALL_DOOR) && unlikely(w.state == WALL_DOOR_OPENING))
+		return WID_TRANSPARENT_WALL;
+
+#if defined(DXX_BUILD_DESCENT_II)
+	if (unlikely(type == WALL_CLOAKED))
+		return WID_CLOAKED_WALL;
+#endif
+
+	const auto flags = w.flags;
 	if (type == WALL_ILLUSION) {
-		if (Walls[seg->sides[side].wall_num].flags & WALL_ILLUSION_OFF)
+		if (flags & WALL_ILLUSION_OFF)
 			return WID_NO_WALL;
 		else {
-			if (check_transparency( seg, side))
+			if (check_transparency(side))
 				return WID_TRANSILLUSORY_WALL;
 		 	else
 				return WID_ILLUSORY_WALL;
@@ -138,8 +129,7 @@ WALL_IS_DOORWAY_result_t wall_is_doorway ( const vcsegptr_t  seg, int side )
 	if (type == WALL_BLASTABLE) {
 	 	if (flags & WALL_BLASTED)
 			return WID_TRANSILLUSORY_WALL;
-
-		if (check_transparency( seg, side))
+		if (check_transparency(side))
 			return WID_TRANSPARENT_WALL;
 		else
 			return WID_WALL;
@@ -147,18 +137,9 @@ WALL_IS_DOORWAY_result_t wall_is_doorway ( const vcsegptr_t  seg, int side )
 	
 	if (flags & WALL_DOOR_OPENED)
 		return WID_TRANSILLUSORY_WALL;
-	
-#if defined(DXX_BUILD_DESCENT_II)
-	if (type == WALL_CLOAKED)
-		return WID_CLOAKED_WALL;
-#endif
 
-	state = Walls[seg->sides[side].wall_num].state;
-	if ((type == WALL_DOOR) && (state == WALL_DOOR_OPENING))
-		return WID_TRANSPARENT_WALL;
-	
 // If none of the above flags are set, there is no doorway.
-	if (check_transparency( seg, side))
+	if (check_transparency(side))
 		return WID_TRANSPARENT_WALL;
 	else
 		return WID_WALL; // There are children behind the door.

SConstruct
@@ -331,6 +331,18 @@ static int a(int b){
 			self.Compile(context, text=f % '2', main=main, msg='whether compiler accepts __builtin_constant_p')
 			context.sconf.Define('dxx_builtin_constant_p(A)', '((void)(A),0)')
 	@_custom_test
+	def check_builtin_expect(self,context):
+		main = '''
+return __builtin_expect(argc == 1, 1) ? 1 : 0;
+'''
+		if self.Compile(context, text='', main=main, msg='whether compiler accepts __builtin_expect'):
+			context.sconf.Define('likely(A)', '__builtin_expect(!!(A), 1)')
+			context.sconf.Define('unlikely(A)', '__builtin_expect(!!(A), 0)')
+		else:
+			macro_value = '(!!(A))'
+			context.sconf.Define('likely(A)', macro_value)
+			context.sconf.Define('unlikely(A)',  macro_value)
+	@_custom_test
 	def check_builtin_object_size(self,context):
 		"""
 help:assume compiler supports __builtin_object_size

common/main/fwdwall.h
@@ -11,6 +11,8 @@
 #include <physfs.h>
 #include "fwdvalptridx.h"
 
+struct side;
+
 #if defined(DXX_BUILD_DESCENT_I)
 const unsigned MAX_WALLS = 175;	// Maximum number of walls
 const std::size_t MAX_WALL_ANIMS = 30;		// Maximum different types of doors
@@ -177,7 +179,7 @@ void wall_init();
 #endif
 
 // Automatically checks if a there is a doorway (i.e. can fly through)
-WALL_IS_DOORWAY_result_t wall_is_doorway (vcsegptr_t seg, int side);
+WALL_IS_DOORWAY_result_t wall_is_doorway (const side &side);
 
 // Deteriorate appearance of wall. (Changes bitmap (paste-ons))
 void wall_damage(vsegptridx_t seg, int side, fix damage);

common/main/wall.h
@@ -170,14 +170,16 @@ static inline ssize_t operator-(wall *w, array<wall, MAX_WALLS> &W)
 }
 #endif
 
-static inline WALL_IS_DOORWAY_result_t WALL_IS_DOORWAY(vcsegptr_t seg, int side)
+static inline WALL_IS_DOORWAY_result_t WALL_IS_DOORWAY(const vcsegptr_t seg, const uint_fast32_t side)
 {
-	if (seg->children[side] == segment_none)
+	const auto child = seg->children[side];
+	if (unlikely(child == segment_none))
 		return WID_WALL;
-	if (seg->children[side] == segment_exit)
+	if (unlikely(child == segment_exit))
 		return WID_EXTERNAL;
-	if (seg->sides[side].wall_num == wall_none)
+	const auto &s = seg->sides[side];
+	if (likely(s.wall_num == wall_none))
 		return WID_NO_WALL;
-	return wall_is_doorway(seg, side);
+	return wall_is_doorway(s);
 }
 #endif

commit 011689f038d0bb42328778dfa566211591b1d348

Move test for opening doors back down
Locked doors, including the exit door, were permanently impassable in
Descent 1 because the door has state WALL_DOOR_OPENING even after it has
flags WALL_DOOR_OPENED.

Fixes: fd01d4c6737e1352ce3727d9f52b5c2ba658c386 ("Reorder wall_is_doorway to favor likely results")
Reported by: zicodxx @ https://github.com/dxx-rebirth/dxx-rebirth/issues/40

similar/main/wall.cpp
@@ -106,9 +106,6 @@ WALL_IS_DOORWAY_result_t wall_is_doorway(const side &side)
 	if (type == WALL_OPEN)
 		return WID_NO_WALL;
 
-	if (likely(type == WALL_DOOR) && unlikely(w.state == WALL_DOOR_OPENING))
-		return WID_TRANSPARENT_WALL;
-
 #if defined(DXX_BUILD_DESCENT_II)
 	if (unlikely(type == WALL_CLOAKED))
 		return WID_CLOAKED_WALL;
@@ -134,10 +131,10 @@ WALL_IS_DOORWAY_result_t wall_is_doorway(const side &side)
 		else
 			return WID_WALL;
 	}	
-	
-	if (flags & WALL_DOOR_OPENED)
+	if (unlikely(flags & WALL_DOOR_OPENED))
 		return WID_TRANSILLUSORY_WALL;
-
+	if (likely(type == WALL_DOOR) && unlikely(w.state == WALL_DOOR_OPENING))
+		return WID_TRANSPARENT_WALL;
 // If none of the above flags are set, there is no doorway.
 	if (check_transparency(side))
 		return WID_TRANSPARENT_WALL;