dxx-rebirth/dxx-rebirth

commit 07869d9dba381d8bd1ea830282af33c245038806

Add automatic demo recording
Various users want automatic demo recording.  Add new command line options:
	-auto-record-demo	start recording on level entry
	-record-demo-format	set demo name automatically

For -record-demo-format, specify a template containing any mix of strftime
insertions, variable insertions, and literal text.  As a special case, specify
"." to use the builtin template "%Y%m%d.%H%M%S-$p-$m".  Most users should use
".".  Supported variables:
	$p	name of pilot recording the demo
	$m	name of msn/mn2 file played

Suggested by jeffersoncarpenter in https://github.com/dxx-rebirth/dxx-rebirth/pull/33, but implemented by vLKp.

similar/main/newdemo.cpp
@@ -3676,13 +3676,87 @@ static void newdemo_write_end()
 	nd_write_byte(ND_EVENT_EOF);
 }
 
+static bool guess_demo_name(ntstring<PATH_MAX - 1> &filename)
+{
+	filename[0] = 0;
+	auto p = GameArg.SysRecordDemoNameTemplate;
+	if (!p || !*p)
+		return false;
+	if (!strcmp(p, "."))
+		p = "%Y%m%d.%H%M%S-$p-$m";
+	std::size_t i = 0;
+	time_t t = 0;
+	tm *ptm = nullptr;
+	for (;; ++p)
+	{
+		if (*p == '%')
+		{
+			if (!p[1])
+				/* Trailing bare % is ill-formed.  Ignore entire
+				 * template.
+				 */
+				return false;
+			/* Time conversions */
+			if (unlikely(!t))
+				t = time(nullptr);
+			if (unlikely(t == -1 || !(ptm = gmtime(&t))))
+				continue;
+			char sbuf[4];
+			sbuf[0] = '%';
+			sbuf[1] = *++p;
+#ifndef _WIN32
+			/* Not supported on Windows */
+			if (sbuf[1] == 'O' || sbuf[1] == 'E')
+			{
+				sbuf[2] = *++p;
+				sbuf[3] = 0;
+			}
+			else
+#endif
+			{
+				sbuf[2] = 0;
+			}
+			filename[i] = 0;
+			auto a = strftime(&filename[i], sizeof(filename) - i, sbuf, ptm);
+			if (a >= sizeof(filename) - i)
+				return false;
+			i += a;
+			continue;
+		}
+		if (*p == '$')
+		{
+			/* Variable conversions */
+			const char *insert;
+			switch(*++p)
+			{
+				case 'm':	/* mission */
+					insert = Current_mission_filename;
+					break;
+				case 'p':	/* pilot */
+					insert = Players[Player_num].callsign;
+					break;
+				default:
+					return false;
+			}
+			i += filename.copy_if(i, insert);
+			continue;
+		}
+		filename[i++] = *p;
+		if (!*p)
+			break;
+		if (i >= sizeof(filename) - 1)
+			return false;
+	}
+	return filename[0];
+}
+
 static const char demoname_allowed_chars[] = "azAZ09__--";
 #define DEMO_FORMAT_STRING(S)	DEMO_DIR S "." DEMO_EXT
 void newdemo_stop_recording()
 {
 	int exit;
-	static char filename[PATH_MAX] = "", *s;
 	static sbyte tmpcnt = 0;
+	ntstring<PATH_MAX - 1> filename;
 
 	exit = 0;
 
@@ -3695,30 +3769,14 @@ void newdemo_stop_recording()
 	outfile.reset();
 	Newdemo_state = ND_STATE_NORMAL;
 	gr_palette_load( gr_palette );
-
-	if (filename[0] != '\0') {
-		int num, i = strlen(filename) - 1;
-		char newfile[PATH_MAX];
-
-		while (isdigit(filename[i])) {
-			i--;
-			if (i == -1)
-				break;
-		}
-		i++;
-		num = atoi(&(filename[i]));
-		num++;
-		filename[i] = '\0';
-		sprintf (newfile, "%s%d", filename, num);
-		strncpy(filename, newfile, PATH_MAX);
-		filename[PATH_MAX - 1] = '\0';
-	}
-
 try_again:
 	;
 
 	Newmenu_allowed_chars = demoname_allowed_chars;
-	if (!nd_record_v_no_space) {
+	if (guess_demo_name(filename))
+	{
+	}
+	else if (!nd_record_v_no_space) {
 		array<newmenu_item, 1> m{
 			nm_item_input(filename),
 		};
@@ -3736,7 +3794,7 @@ try_again:
 		char save_file[PATH_MAX];
 
 		if (filename[0] != '\0') {
-			snprintf(save_file, sizeof(save_file), DEMO_FORMAT_STRING("%s"), filename);
+			snprintf(save_file, sizeof(save_file), DEMO_FORMAT_STRING("%s"), filename.data());
 		} else
 			snprintf(save_file, sizeof(save_file), DEMO_FORMAT_STRING("tmp%d"), tmpcnt++);
 		remove(save_file);
@@ -3752,14 +3810,19 @@ try_again:
 		goto try_again;
 
 	//check to make sure name is ok
-	for (s=filename;*s;s++)
-		if (!isalnum(*s) && *s!='_') {
+	range_for (const unsigned c, filename)
+	{
+		if (!c)
+			break;
+		if (!isalnum(c) && c != '_' && c != '-' && c != '.')
+		{
 			nm_messagebox(NULL, 1,TXT_CONTINUE, TXT_DEMO_USE_LETTERS);
 			goto try_again;
 		}
+	}
 
 	char fullname[PATH_MAX];
-	snprintf(fullname, sizeof(fullname), DEMO_FORMAT_STRING("%s"), filename);
+	snprintf(fullname, sizeof(fullname), DEMO_FORMAT_STRING("%s"), filename.data());
 	PHYSFS_delete(fullname);
 	PHYSFSX_rename(DEMO_FILENAME, fullname);
 }

common/include/args.h
@@ -55,6 +55,8 @@ struct Arg
 	int SysUsePlayersDir;
 	int SysLowMem;
 	const char *SysPilot;
+	const char *SysRecordDemoNameTemplate;
+	bool SysAutoRecordDemo;
 	int SysWindow;
 	int SysNoBorders;
 	int SysAutoDemo;

similar/main/game.cpp
@@ -1046,7 +1046,8 @@ window *game_setup(void)
 #endif
 
 	fix_object_segs();
-
+	if (GameArg.SysAutoRecordDemo && Newdemo_state == ND_STATE_NORMAL)
+		newdemo_start_recording();
 	return game_wind;
 }
 

similar/main/inferno.cpp
@@ -127,6 +127,8 @@ static void print_commandline_help()
 	printf( "  -use_players_dir              put player files and saved games in Players subdirectory\n");
 	printf( "  -lowmem                       Lowers animation detail for better performance with\n\t\t\t\tlow memory\n");
 	printf( "  -pilot <s>                    Select pilot <s> automatically\n");
+	printf( "  -auto-record-demo             Start recording on level entry\n");
+	printf( "  -record-demo-format           Set demo name automatically\n");
 	printf( "  -autodemo                     Start in demo mode\n");
 	printf( "  -window                       Run the game in a window\n");
 	printf( "  -noborders                    Do not show borders in window mode\n");

similar/misc/args.cpp
@@ -153,6 +153,10 @@ static void ReadCmdArgs(void)
 			GameArg.SysLowMem 		= 1;
 		else if (!d_stricmp(p, "-pilot"))
 			GameArg.SysPilot = arg_string(pp, end);
+		else if (!d_stricmp(p, "-record-demo-format"))
+			GameArg.SysRecordDemoNameTemplate = arg_string(pp, end);
+		else if (!d_stricmp(p, "-auto-record-demo"))
+			GameArg.SysAutoRecordDemo = 1;
 		else if (!d_stricmp(p, "-window"))
 			GameArg.SysWindow 		= 1;
 		else if (!d_stricmp(p, "-noborders"))

commit 068444476572ff7f013a56e39e660a35474f45cd

Fix Windows build
Fixes: 07869d9dba381d8bd1ea830282af33c245038806 ("Add automatic demo recording")

similar/main/newdemo.cpp
@@ -23,7 +23,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
  *
  */
 
-
+#include <ctime>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>