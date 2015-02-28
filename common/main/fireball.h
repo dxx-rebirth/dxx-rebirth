dxx-rebirth/dxx-rebirth

commit 125d9257be21dfbf6743c0f652318839e88b2e80

Use special type names for segment/object numbers

common/main/fireball.h
@@ -42,7 +42,8 @@ struct vms_vector;
 
 struct expl_wall
 {
-	int segnum,sidenum;
+	segnum_t segnum;
+	int sidenum;
 	fix time;
 };
 
@@ -51,10 +52,10 @@ struct expl_wall
 #define MAX_EXPLODING_WALLS     10
 extern expl_wall expl_wall_list[MAX_EXPLODING_WALLS];
 
-objptridx_t object_create_explosion(short segnum, vms_vector *position, fix size, int vclip_type);
-void object_create_muzzle_flash(short segnum, vms_vector *position, fix size, int vclip_type);
+objptridx_t object_create_explosion(segnum_t segnum, vms_vector *position, fix size, int vclip_type);
+void object_create_muzzle_flash(segnum_t segnum, vms_vector *position, fix size, int vclip_type);
 
-objptridx_t object_create_badass_explosion(objptridx_t objp, short segnum,
+objptridx_t object_create_badass_explosion(objptridx_t objp, segnum_t segnum,
 		vms_vector *position, fix size, int vclip_type,
 		fix maxdamage, fix maxdistance, fix maxforce, objptridx_t parent);
 
@@ -71,7 +72,7 @@ void do_debris_frame(objptridx_t obj);      // deal with debris for this frame
 
 void draw_fireball(object *obj);
 
-void explode_wall(int segnum, int sidenum);
+void explode_wall(segnum_t segnum, int sidenum);
 void do_exploding_wall_frame(void);
 void init_exploding_walls(void);
 extern void maybe_drop_net_powerup(int powerup_type);
@@ -80,7 +81,7 @@ extern void maybe_replace_powerup_with_energy(object *del_obj);
 extern int get_explosion_vclip(object *obj, int stage);
 
 #if defined(DXX_BUILD_DESCENT_II)
-extern int drop_powerup(int type, int id, int num, vms_vector *init_vel, vms_vector *pos, int segnum);
+objnum_t drop_powerup(int type, int id, int num, vms_vector *init_vel, vms_vector *pos, segnum_t segnum);
 
 // creates afterburner blobs behind the specified object
 void drop_afterburner_blobs(object *obj, int count, fix size_scale, fix lifetime);

similar/main/fireball.cpp
@@ -77,7 +77,7 @@ fix	Flash_effect=0;
 static const int	PK1=1, PK2=8;
 #endif
 
-static objptridx_t object_create_explosion_sub(objptridx_t objp, short segnum, vms_vector * position, fix size, int vclip_type, fix maxdamage, fix maxdistance, fix maxforce, objptridx_t parent )
+static objptridx_t object_create_explosion_sub(objptridx_t objp, segnum_t segnum, vms_vector * position, fix size, int vclip_type, fix maxdamage, fix maxdistance, fix maxforce, objptridx_t parent )
 {
 	objptridx_t obj = obj_create( OBJ_FIREBALL,vclip_type,segnum,position,&vmd_identity_matrix,size,
 					CT_EXPLOSION,MT_NONE,RT_FIREBALL);
@@ -274,17 +274,17 @@ static objptridx_t object_create_explosion_sub(objptridx_t objp, short segnum, v
 }
 
 
-void object_create_muzzle_flash(short segnum, vms_vector * position, fix size, int vclip_type )
+void object_create_muzzle_flash(segnum_t segnum, vms_vector * position, fix size, int vclip_type )
 {
 	object_create_explosion_sub(object_none, segnum, position, size, vclip_type, 0, 0, 0, object_none );
 }
 
-objptridx_t object_create_explosion(short segnum, vms_vector * position, fix size, int vclip_type )
+objptridx_t object_create_explosion(segnum_t segnum, vms_vector * position, fix size, int vclip_type )
 {
 	return object_create_explosion_sub(object_none, segnum, position, size, vclip_type, 0, 0, 0, object_none );
 }
 
-objptridx_t object_create_badass_explosion(objptridx_t objp, short segnum, vms_vector * position, fix size, int vclip_type, fix maxdamage, fix maxdistance, fix maxforce, objptridx_t parent )
+objptridx_t object_create_badass_explosion(objptridx_t objp, segnum_t segnum, vms_vector * position, fix size, int vclip_type, fix maxdamage, fix maxdistance, fix maxforce, objptridx_t parent )
 {
 	objptridx_t rval = object_create_explosion_sub(objp, segnum, position, size, vclip_type, maxdamage, maxdistance, maxforce, parent );
 
@@ -519,13 +519,11 @@ int pick_connected_segment(object *objp, int max_depth)
 //	For all active net players, try to create a N segment path from the player.  If possible, return that
 //	segment.  If not possible, try another player.  After a few tries, use a random segment.
 //	Don't drop if control center in segment.
-static int choose_drop_segment()
+static segnum_t choose_drop_segment()
 {
 	int	pnum = 0;
-	short	segnum = segment_none;
 	int	cur_drop_depth;
 	int	count;
-	int	player_seg;
 	vms_vector tempv,*player_pos;
 
 	d_srand((fix)timer_query());
@@ -533,8 +531,9 @@ static int choose_drop_segment()
 	cur_drop_depth = BASE_NET_DROP_DEPTH + ((d_rand() * BASE_NET_DROP_DEPTH*2) >> 15);
 
 	player_pos = &Objects[Players[Player_num].objnum].pos;
-	player_seg = Objects[Players[Player_num].objnum].segnum;
+	segnum_t player_seg = Objects[Players[Player_num].objnum].segnum;
 
+	segnum_t	segnum = segment_none;
 	while ((segnum == segment_none) && (cur_drop_depth > BASE_NET_DROP_DEPTH/2)) {
 		pnum = (d_rand() * N_players) >> 15;
 		count = 0;
@@ -566,7 +565,7 @@ static int choose_drop_segment()
 		else {	//don't drop in any children of control centers
 			int i;
 			for (i=0;i<6;i++) {
-				int ch = Segments[segnum].children[i];
+				segnum_t ch = Segments[segnum].children[i];
 				if (IS_CHILD(ch) && Segments[ch].special == SEGMENT_IS_CONTROLCEN) {
 					segnum = segment_none;
 					break;
@@ -604,7 +603,6 @@ static int choose_drop_segment()
 void maybe_drop_net_powerup(int powerup_type)
 {
 	if ((Game_mode & GM_MULTI) && !(Game_mode & GM_MULTI_COOP)) {
-		int	segnum, objnum;
 		vms_vector	new_pos;
 
 		if (Game_mode & GM_NETWORK)
@@ -616,7 +614,7 @@ void maybe_drop_net_powerup(int powerup_type)
 		if (Control_center_destroyed || Endlevel_sequence)
 			return;
 
-		segnum = choose_drop_segment();
+		segnum_t	segnum = choose_drop_segment();
 //--old-- 		segnum = (d_rand() * Highest_segment_index) >> 15;
 //--old-- 		Assert((segnum >= 0) && (segnum <= Highest_segment_index));
 //--old-- 		if (segnum < 0)
@@ -625,7 +623,7 @@ void maybe_drop_net_powerup(int powerup_type)
 //--old-- 			segnum /= 2;
 
 		Net_create_loc = 0;
-		objnum = call_object_create_egg(&Objects[Players[Player_num].objnum], 1, OBJ_POWERUP, powerup_type);
+		objnum_t objnum = call_object_create_egg(&Objects[Players[Player_num].objnum], 1, OBJ_POWERUP, powerup_type);
 
 		if (objnum < 0)
 			return;
@@ -644,7 +642,7 @@ void maybe_drop_net_powerup(int powerup_type)
 
 //	------------------------------------------------------------------------------------------------------
 //	Return true if current segment contains some object.
-static int segment_contains_object(int obj_type, int obj_id, int segnum)
+static int segment_contains_object(int obj_type, int obj_id, segnum_t segnum)
 {
 	if (segnum == segment_none)
 		return 0;
@@ -657,7 +655,7 @@ static int segment_contains_object(int obj_type, int obj_id, int segnum)
 }
 
 //	------------------------------------------------------------------------------------------------------
-static int object_nearby_aux(int segnum, int object_type, int object_id, int depth)
+static int object_nearby_aux(segnum_t segnum, int object_type, int object_id, int depth)
 {
 	int	i;
 
@@ -668,7 +666,7 @@ static int object_nearby_aux(int segnum, int object_type, int object_id, int dep
 		return 1;
 
 	for (i=0; i<MAX_SIDES_PER_SEGMENT; i++) {
-		int	seg2 = Segments[segnum].children[i];
+		segnum_t	seg2 = Segments[segnum].children[i];
 
 		if (seg2 != segment_none)
 			if (object_nearby_aux(seg2, object_type, object_id, depth-1))
@@ -772,10 +770,9 @@ void maybe_replace_powerup_with_energy(object *del_obj)
 #if defined(DXX_BUILD_DESCENT_I)
 static
 #endif
-int drop_powerup(int type, int id, int num, vms_vector *init_vel, vms_vector *pos, int segnum)
+objnum_t drop_powerup(int type, int id, int num, vms_vector *init_vel, vms_vector *pos, segnum_t segnum)
 {
-	short		objnum=object_none;
-	object	*obj;
+	objnum_t objnum = object_none;
 	vms_vector	new_velocity, new_pos;
 	fix		old_mag;
    int             count;
@@ -821,7 +818,8 @@ int drop_powerup(int type, int id, int num, vms_vector *init_vel, vms_vector *po
 					 return object_none;
 #endif
 				}
-				objnum = obj_create( OBJ_POWERUP, id, segnum, &new_pos, &vmd_identity_matrix, Powerup_info[id].size, CT_POWERUP, MT_PHYSICS, RT_POWERUP);
+				objptridx_t		obj = obj_create( OBJ_POWERUP, id, segnum, &new_pos, &vmd_identity_matrix, Powerup_info[id].size, CT_POWERUP, MT_PHYSICS, RT_POWERUP);
+				objnum = obj;
 
 				if (objnum == object_none ) {
 					Int3();
@@ -832,8 +830,6 @@ int drop_powerup(int type, int id, int num, vms_vector *init_vel, vms_vector *po
 				{
 					Net_create_objnums[Net_create_loc++] = objnum;
 				}
-
-				obj = &Objects[objnum];
 
 				obj->mtype.phys_info.velocity = new_velocity;
 
@@ -890,11 +886,12 @@ int drop_powerup(int type, int id, int num, vms_vector *init_vel, vms_vector *po
 //				new_pos.z += (d_rand()-16384)*6;
 
 #if defined(DXX_BUILD_DESCENT_I)
-				objnum = obj_create(OBJ_ROBOT, id, segnum, &new_pos, &vmd_identity_matrix, Polygon_models[Robot_info[ObjId[type]].model_num].rad, CT_AI, MT_PHYSICS, RT_POLYOBJ);
+				objptridx_t obj = obj_create(OBJ_ROBOT, id, segnum, &new_pos, &vmd_identity_matrix, Polygon_models[Robot_info[ObjId[type]].model_num].rad, CT_AI, MT_PHYSICS, RT_POLYOBJ);
 #elif defined(DXX_BUILD_DESCENT_II)
-				objnum = obj_create(OBJ_ROBOT, id, segnum, &new_pos, &vmd_identity_matrix, Polygon_models[Robot_info[id].model_num].rad, CT_AI, MT_PHYSICS, RT_POLYOBJ);
+				objptridx_t obj = obj_create(OBJ_ROBOT, id, segnum, &new_pos, &vmd_identity_matrix, Polygon_models[Robot_info[id].model_num].rad, CT_AI, MT_PHYSICS, RT_POLYOBJ);
 #endif
 
+				objnum = obj;
 				if ( objnum == object_none ) {
 					Int3();
 					return objnum;
@@ -904,8 +901,6 @@ int drop_powerup(int type, int id, int num, vms_vector *init_vel, vms_vector *po
 				{
 					Net_create_objnums[Net_create_loc++] = objnum;
 				}
-
-				obj = &Objects[objnum];
 
 				//Set polygon-object-specific data
 
@@ -950,9 +945,8 @@ int drop_powerup(int type, int id, int num, vms_vector *init_vel, vms_vector *po
 // ----------------------------------------------------------------------------
 // Returns created object number.
 // If object dropped by player, set flag.
-int object_create_egg(object *objp)
+objnum_t object_create_egg(object *objp)
 {
-	int	rval;
 #if defined(DXX_BUILD_DESCENT_II)
 	if (!(Game_mode & GM_MULTI) & (objp->type != OBJ_PLAYER))
 	{
@@ -982,7 +976,7 @@ int object_create_egg(object *objp)
 		}
 	}
 #endif
-	rval = drop_powerup(objp->contains_type, objp->contains_id, objp->contains_count, &objp->mtype.phys_info.velocity, &objp->pos, objp->segnum);
+	objnum_t	rval = drop_powerup(objp->contains_type, objp->contains_id, objp->contains_count, &objp->mtype.phys_info.velocity, &objp->pos, objp->segnum);
 #if defined(DXX_BUILD_DESCENT_II)
 	if (rval != object_none)
 	{
@@ -1006,7 +1000,7 @@ int object_create_egg(object *objp)
 //	-------------------------------------------------------------------------------------------------------
 //	Put count objects of type type (eg, powerup), id = id (eg, energy) into *objp, then drop them!  Yippee!
 //	Returns created object number.
-int call_object_create_egg(object *objp, int count, int type, int id)
+objnum_t call_object_create_egg(object *objp, int count, int type, int id)
 {
 	if (count > 0) {
 		objp->contains_count = count;
@@ -1079,7 +1073,7 @@ void explode_object(objptridx_t hitobj,fix delay_time)
 	if (hitobj->flags & OF_EXPLODING) return;
 
 	if (delay_time) {		//wait a little while before creating explosion
-		int objnum;
+		objnum_t objnum;
 		object *obj;
 
 		//create a placeholder object to do the delay, with id==-1
@@ -1277,7 +1271,7 @@ void init_exploding_walls()
 }
 
 //explode the given wall
-void explode_wall(int segnum,int sidenum)
+void explode_wall(segnum_t segnum,int sidenum)
 {
 	int i;
 	vms_vector pos;
@@ -1308,7 +1302,7 @@ void do_exploding_wall_frame()
 	int i;
 
 	for (i=0;i<MAX_EXPLODING_WALLS;i++) {
-		int segnum = expl_wall_list[i].segnum;
+		segnum_t segnum = expl_wall_list[i].segnum;
 
 		if (segnum != segment_none) {
 			int sidenum = expl_wall_list[i].sidenum;
@@ -1402,7 +1396,6 @@ void do_exploding_wall_frame()
 void drop_afterburner_blobs(object *obj, int count, fix size_scale, fix lifetime)
 {
 	vms_vector pos_left,pos_right;
-	int segnum;
 
 	vm_vec_scale_add(&pos_left, &obj->pos, &obj->orient.fvec, -obj->size);
 	vm_vec_scale_add2(&pos_left, &obj->orient.rvec, -obj->size/4);
@@ -1411,7 +1404,7 @@ void drop_afterburner_blobs(object *obj, int count, fix size_scale, fix lifetime
 	if (count == 1)
 		vm_vec_avg(&pos_left, &pos_left, &pos_right);
 
-	segnum = find_point_seg(&pos_left, obj->segnum);
+	segnum_t segnum = find_point_seg(&pos_left, obj->segnum);
 	if (segnum != segment_none)
 		object_create_explosion(segnum, &pos_left, size_scale, VCLIP_AFTERBURNER_BLOB );
 

similar/main/state.cpp
@@ -1226,7 +1226,8 @@ int state_save_all_sub(const char *filename, const char *desc)
 	PHYSFS_write(fp, &Control_center_player_been_seen, sizeof(int), 1);
 	PHYSFS_write(fp, &Control_center_next_fire_time, sizeof(int), 1);
 	PHYSFS_write(fp, &Control_center_present, sizeof(int), 1);
-	PHYSFS_write(fp, &Dead_controlcen_object_num, sizeof(int), 1);
+	int dead_controlcen_object_num = Dead_controlcen_object_num;
+	PHYSFS_write(fp, &dead_controlcen_object_num, sizeof(int), 1);
 
 // Save the AI state
 	ai_save_state( fp );
@@ -1318,7 +1319,7 @@ int state_save_all_sub(const char *filename, const char *desc)
 #if defined(DXX_BUILD_DESCENT_II)
 void set_pos_from_return_segment(void)
 {
-	int	plobjnum = Players[Player_num].objnum;
+	objnum_t	plobjnum = Players[Player_num].objnum;
 
 	compute_segment_center(&Objects[plobjnum].pos, &Segments[Secret_return_segment]);
 	obj_relink(plobjnum, Secret_return_segment);
@@ -1410,7 +1411,7 @@ int state_restore_all(int in_game, int secret_restore, const char *filename_over
 
 int state_restore_all_sub(const char *filename, int secret_restore)
 {
-	int version,i, j, segnum, coop_player_got[MAX_PLAYERS], coop_org_objnum = Players[Player_num].objnum;
+	int version,i, j, coop_player_got[MAX_PLAYERS], coop_org_objnum = Players[Player_num].objnum;
 	object * obj;
 	PHYSFS_file *fp;
 	int swap = 0;	// if file is not endian native, have to swap all shorts and ints
@@ -1580,7 +1581,7 @@ int state_restore_all_sub(const char *filename, int secret_restore)
 	Do_appearance_effect = 0;			// Don't do this for middle o' game stuff.
 
 	//Clear out all the objects from the lvl file
-	for (segnum=0; segnum <= Highest_segment_index; segnum++)
+	for (segnum_t segnum=0; segnum <= Highest_segment_index; segnum++)
 		Segments[segnum].objects = object_none;
 	reset_objects(1);
 
@@ -1599,7 +1600,7 @@ int state_restore_all_sub(const char *filename, int secret_restore)
 	for (i=0; i<=Highest_object_index; i++ )	{
 		obj = &Objects[i];
 		obj->rtype.pobj_info.alt_textures = -1;
-		segnum = obj->segnum;
+		segnum_t segnum = obj->segnum;
 		obj->next = obj->prev = object_none;
 		obj->segnum = segment_none;
 		if ( obj->type != OBJ_NONE )	{

commit a9a330bf3f450791fc45a4bd77cc1ac01df4ce49

Use array<> for expl_wall_list
Fixes: 125d9257be21dfbf6743c0f652318839e88b2e80 ("Use special type names for segment/object numbers")

common/main/fireball.h
@@ -23,14 +23,13 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
  *
  */
 
-
-#ifndef _FIREBALL_H
-#define _FIREBALL_H
+#pragma once
 
 #include <physfs.h>
 
 #ifdef __cplusplus
 #include "maths.h"
+#include "fwd-partial_range.h"
 
 struct vms_vector;
 struct objptridx_t;
@@ -44,17 +43,24 @@ struct vobjptridx_t;
 #define ET_MULTI_START  1   //first part of multi-part explosion
 #define ET_MULTI_SECOND 2   //second part of multi-part explosion
 
-struct expl_wall
+struct expl_wall : prohibit_void_ptr<expl_wall>
 {
 	segnum_t segnum;
-	int sidenum;
+	uint8_t sidenum;
 	fix time;
 };
+
+struct disk_expl_wall
+{
+	int segnum, sidenum;
+	fix time;
+};
+static_assert(sizeof(disk_expl_wall) == 12, "sizeof(disk_expl_wall) wrong");
 
 // data for exploding walls (such as hostage door)
 
 #define MAX_EXPLODING_WALLS     10
-extern expl_wall expl_wall_list[MAX_EXPLODING_WALLS];
+extern array<expl_wall, MAX_EXPLODING_WALLS> expl_wall_list;
 
 objptridx_t object_create_explosion(vsegptridx_t segnum, const vms_vector &position, fix size, int vclip_type);
 void object_create_muzzle_flash(vsegptridx_t segnum, const vms_vector &position, fix size, int vclip_type);
@@ -92,12 +98,10 @@ void drop_afterburner_blobs(vobjptr_t obj, int count, fix size_scale, fix lifeti
 /*
  * reads n expl_wall structs from a PHYSFS_file and swaps if specified
  */
-extern void expl_wall_read_n_swap(expl_wall *ew, int n, int swap, PHYSFS_file *fp);
+void expl_wall_read_n_swap(PHYSFS_file *fp, int swap, partial_range_t<expl_wall *>);
 extern fix	Flash_effect;
 #endif
 
 int pick_connected_segment(vobjptr_t objp, int max_depth);
 
 #endif
-
-#endif /* _FIREBALL_H */

similar/main/fireball.cpp
@@ -65,6 +65,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #include "compiler-range_for.h"
 #include "highest_valid.h"
+#include "partial_range.h"
 #include "segiter.h"
 
 using std::min;
@@ -1227,32 +1228,33 @@ void do_explosion_sequence(const vobjptr_t obj)
 #define EXPL_WALL_FIREBALL_SIZE 		(0x48000*6/10)	//smallest size
 #endif
 
-expl_wall expl_wall_list[MAX_EXPLODING_WALLS];
+array<expl_wall, MAX_EXPLODING_WALLS> expl_wall_list;
 
 void init_exploding_walls()
 {
-	int i;
-
-	for (i=0;i<MAX_EXPLODING_WALLS;i++)
-		expl_wall_list[i].segnum = segment_none;
+	range_for (auto &i, expl_wall_list)
+		i.segnum = segment_none;
 }
 
 //explode the given wall
 void explode_wall(const vsegptridx_t segnum,int sidenum)
 {
-	int i;
 	//find a free slot
-
-	for (i=0;i<MAX_EXPLODING_WALLS && expl_wall_list[i].segnum != segment_none;i++);
-
-	if (i==MAX_EXPLODING_WALLS) {		//didn't find slot.
+	const auto e = end(expl_wall_list);
+	const auto predicate = [](expl_wall &e) {
+		return e.segnum == segment_none;
+	};
+	const auto i = std::find_if(begin(expl_wall_list), e, predicate);
+	if (i == e)
+	{		//didn't find slot.
 		Int3();
 		return;
 	}
 
-	expl_wall_list[i].segnum	= segnum;
-	expl_wall_list[i].sidenum	= sidenum;
-	expl_wall_list[i].time		= 0;
+	auto &w = *i;
+	w.segnum	= segnum;
+	w.sidenum	= sidenum;
+	w.time		= 0;
 
 	//play one long sound for whole door wall explosion
 	const auto pos = compute_center_point_on_side(segnum,sidenum);
@@ -1264,24 +1266,23 @@ void explode_wall(const vsegptridx_t segnum,int sidenum)
 //note: this wall code assumes the wall is not triangulated
 void do_exploding_wall_frame()
 {
-	int i;
-
-	for (i=0;i<MAX_EXPLODING_WALLS;i++) {
-		auto segnum = expl_wall_list[i].segnum;
+	range_for (auto &i, expl_wall_list)
+	{
+		auto segnum = i.segnum;
 
 		if (segnum != segment_none) {
-			int sidenum = expl_wall_list[i].sidenum;
+			int sidenum = i.sidenum;
 			fix oldfrac,newfrac;
 			int old_count,new_count,e;		//n,
 
-			oldfrac = fixdiv(expl_wall_list[i].time,EXPL_WALL_TIME);
+			oldfrac = fixdiv(i.time,EXPL_WALL_TIME);
 
-			expl_wall_list[i].time += FrameTime;
-			if (expl_wall_list[i].time > EXPL_WALL_TIME)
-				expl_wall_list[i].time = EXPL_WALL_TIME;
+			i.time += FrameTime;
+			if (i.time > EXPL_WALL_TIME)
+				i.time = EXPL_WALL_TIME;
 
 			const auto seg = vsegptridx(segnum);
-			if (expl_wall_list[i].time>(EXPL_WALL_TIME*3)/4) {
+			if (i.time>(EXPL_WALL_TIME*3)/4) {
 				int a,n;
 				a = Walls[seg->sides[sidenum].wall_num].clip_num;
 				n = WallAnims[a].num_frames;
@@ -1296,7 +1297,7 @@ void do_exploding_wall_frame()
 
 			}
 
-			newfrac = fixdiv(expl_wall_list[i].time,EXPL_WALL_TIME);
+			newfrac = fixdiv(i.time,EXPL_WALL_TIME);
 
 			old_count = f2i(EXPL_WALL_TOTAL_FIREBALLS * fixmul(oldfrac,oldfrac));
 			new_count = f2i(EXPL_WALL_TOTAL_FIREBALLS * fixmul(newfrac,newfrac));
@@ -1328,9 +1329,9 @@ void do_exploding_wall_frame()
 					vm_vec_scale_add2(pos,Segments[segnum].sides[sidenum].normals[0],size*(EXPL_WALL_TOTAL_FIREBALLS-e)/EXPL_WALL_TOTAL_FIREBALLS);
 
 				if (e & 3)		//3 of 4 are normal
-					object_create_explosion(expl_wall_list[i].segnum,pos,size,VCLIP_SMALL_EXPLOSION);
+					object_create_explosion(i.segnum,pos,size,VCLIP_SMALL_EXPLOSION);
 				else
-					object_create_badass_explosion( object_none, expl_wall_list[i].segnum, pos,
+					object_create_badass_explosion( object_none, i.segnum, pos,
 					size,
 					VCLIP_SMALL_EXPLOSION,
 					i2f(4),		// damage strength
@@ -1338,13 +1339,9 @@ void do_exploding_wall_frame()
 					i2f(50),		//	damage force
 					object_none		//	parent id
 					);
-
-
 			}
-
-			if (expl_wall_list[i].time >= EXPL_WALL_TIME)
-				expl_wall_list[i].segnum = segment_none;	//flag this slot as free
-
+			if (i.time >= EXPL_WALL_TIME)
+				i.segnum = segment_none;	//flag this slot as free
 		}
 	}
 
@@ -1375,27 +1372,24 @@ void drop_afterburner_blobs(const vobjptr_t obj, int count, fix size_scale, fix 
 	}
 }
 
-static void expl_wall_swap(expl_wall *ew, int swap)
-{
-	if (!swap)
-		return;
-	
-	ew->segnum = SWAPINT(ew->segnum);
-	ew->sidenum = SWAPINT(ew->sidenum);
-	ew->time = SWAPINT(ew->time);
-}
-
 /*
  * reads n expl_wall structs from a PHYSFS_file and swaps if specified
  */
-void expl_wall_read_n_swap(expl_wall *ew, int n, int swap, PHYSFS_file *fp)
+void expl_wall_read_n_swap(PHYSFS_file *fp, int swap, partial_range_t<expl_wall *> r)
 {
-	int i;
-	
-	PHYSFS_read(fp, ew, sizeof(expl_wall), n);
-	
-	if (swap)
-		for (i = 0; i < n; i++)
-			expl_wall_swap(&ew[i], swap);
+	range_for (auto &e, r)
+	{
+		disk_expl_wall d;
+		PHYSFS_read(fp, &d, sizeof(d), 1);
+		if (swap)
+		{
+			d.segnum = SWAPINT(d.segnum);
+			d.sidenum = SWAPINT(d.sidenum);
+			d.time = SWAPINT(d.time);
+		}
+		e.segnum = d.segnum;
+		e.sidenum = d.sidenum;
+		e.time = d.time;
+	}
 }
 #endif

similar/main/state.cpp
@@ -1037,9 +1037,16 @@ int state_save_all_sub(const char *filename, const char *desc)
 
 #if defined(DXX_BUILD_DESCENT_II)
 //Save exploding wall info
-	i = MAX_EXPLODING_WALLS;
+	i = expl_wall_list.size();
 	PHYSFS_write(fp, &i, sizeof(int), 1);
-	PHYSFS_write(fp, expl_wall_list, sizeof(*expl_wall_list), i);
+	range_for (auto &e, expl_wall_list)
+	{
+		disk_expl_wall d;
+		d.segnum = e.segnum;
+		d.sidenum = e.sidenum;
+		d.time = e.time;
+		PHYSFS_write(fp, &d, sizeof(d), 1);
+	}
 #endif
 
 //Save door info
@@ -1511,8 +1518,8 @@ int state_restore_all_sub(const char *filename, int secret_restore)
 
 	//Restore exploding wall info
 	if (version >= 10) {
-		i = PHYSFSX_readSXE32(fp, swap);
-		expl_wall_read_n_swap(expl_wall_list, i, swap, fp);
+		unsigned i = PHYSFSX_readSXE32(fp, swap);
+		expl_wall_read_n_swap(fp, swap, partial_range(expl_wall_list, i));
 	}
 #endif
 