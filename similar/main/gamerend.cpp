cbe6661cee63a4c61062b4fdfbf4bdf2d7973685
Kp <kp@valhallalegends.com>
2015-03-09 03:33:07 +0000

    Fix effecting/affecting

--- similar/main/gamerend.cpp
+++ similar/main/gamerend.cpp
@@ -141,7 +141,7 @@ static void show_framerate()
 static void show_netplayerinfo()
 {
 	int x=0, y=0, color=0, eff=0;
-	static const char *const eff_strings[]={"trashing","really hurting","seriously effecting","hurting","effecting","tarnishing"};
+	static const char *const eff_strings[]={"trashing","really hurting","seriously affecting","hurting","affecting","tarnishing"};
 
 	gr_set_current_canvas(NULL);
 	gr_set_curfont(GAME_FONT);
