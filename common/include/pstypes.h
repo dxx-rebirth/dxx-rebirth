dxx-rebirth/dxx-rebirth

commit 5bafa16e7fd9bb9c3343b3307c5c2172c829ee84

Fix level load hang on >=mingw32-gcc-4.7
Starting in gcc 4.7 "Windows mingw targets are using the -mms-bitfields
option by default." <http://gcc.gnu.org/gcc-4.7/changes.html>.  This
causes __attribute__((packed)) not to be effective on structures that
are used to express the layout of on-disk data.  In turn, that causes
piggy.c to mishandle texture loads, eventually resulting in an infinite
loop.  Add a pragma pack to force the headers to pack tightly on
Windows.

common/include/pstypes.h
@@ -84,6 +84,9 @@ defined(__LITTLE_ENDIAN__)	// from physfs_internal.h
 
 #ifdef __GNUC__
 # define __pack__ __attribute__((packed))
+#ifdef WIN32
+# pragma pack(1)
+#endif
 #elif defined(_MSC_VER)
 # pragma pack(push, packing)
 # pragma pack(1)

commit 69af920e09c666d8c7ce1d51a11c0f0377014926

Fix Win64 startup crash
Fixes: 5bafa16e7fd9bb9c3343b3307c5c2172c829ee84 ("Fix level load hang on >=mingw32-gcc-4.7")

common/include/pstypes.h
@@ -89,9 +89,10 @@ defined(__LITTLE_ENDIAN__)	// from physfs_internal.h
 #endif
 
 #ifdef __GNUC__
-# define __pack__ __attribute__((packed))
 #ifdef WIN32
-# pragma pack(1)
+# define __pack__ __attribute__((gcc_struct, packed))
+#else
+# define __pack__ __attribute__((packed))
 #endif
 #elif defined(_MSC_VER)
 # pragma pack(push, packing)