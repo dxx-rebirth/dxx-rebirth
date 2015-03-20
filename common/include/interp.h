dxx-rebirth/dxx-rebirth

commit 68be3baeca7af9f24609f05b464c44228e9ac5b6

Track highest_texture_num locally

common/include/interp.h
@@ -43,7 +43,7 @@ struct glow_values_t;
 void g3_draw_polygon_model(const uint8_t *model_ptr,grs_bitmap **model_bitmaps,submodel_angles anim_angles,g3s_lrgb light,const glow_values_t *glow_values, polygon_model_points &Interp_point_list);
 
 //init code for bitmap models
-void g3_init_polygon_model(void *model_ptr);
+uint16_t g3_init_polygon_model(void *model_ptr);
 
 //un-initialize, i.e., convert color entries back to RGB15
 static inline void g3_uninit_polygon_model(void *model_ptr)

similar/3d/interp.cpp
@@ -32,9 +32,8 @@ static const unsigned OP_SUBCALL = 6;   //call a subobject
 static const unsigned OP_DEFP_START = 7;   //defpoints with start
 static const unsigned OP_GLOW = 8;   //glow value for next poly
 
-static void init_model_sub(uint8_t *p);
+static uint16_t init_model_sub(uint8_t *p, uint16_t);
 
-short highest_texture_num;
 int g3d_interp_outline;
 
 
@@ -464,6 +463,11 @@ class init_model_sub_state :
 	public interpreter_base
 {
 public:
+	uint16_t highest_texture_num;
+	init_model_sub_state(uint16_t h) :
+		highest_texture_num(h)
+	{
+	}
 	void op_flatpoly(uint8_t *const p, const uint_fast32_t nv)
 	{
 		(void)nv;
@@ -481,12 +485,12 @@ public:
 	}
 	void op_sortnorm(uint8_t *const p)
 	{
-		init_model_sub(p+w(p+28));
-		init_model_sub(p+w(p+30));
+		auto h = init_model_sub(p+w(p+28), highest_texture_num);
+		highest_texture_num = init_model_sub(p+w(p+30), h);
 	}
 	void op_subcall(uint8_t *const p)
 	{
-		init_model_sub(p+w(p+16));
+		highest_texture_num = init_model_sub(p+w(p+16), highest_texture_num);
 	}
 };
 
@@ -760,21 +764,20 @@ void g3_draw_morphing_model(const uint8_t *p,grs_bitmap **model_bitmaps,const su
 	iterate_polymodel(p, state);
 }
 
-static void init_model_sub(uint8_t *p)
+static uint16_t init_model_sub(uint8_t *p, uint16_t highest_texture_num)
 {
-	init_model_sub_state state;
+	init_model_sub_state state(highest_texture_num);
 	Assert(++nest_count < 1000);
 	iterate_polymodel(p, state);
+	return state.highest_texture_num;
 }
 
 //init code for bitmap models
-void g3_init_polygon_model(void *model_ptr)
+uint16_t g3_init_polygon_model(void *model_ptr)
 {
 	#ifndef NDEBUG
 	nest_count = 0;
 	#endif
 
-	highest_texture_num = -1;
-
-	init_model_sub((ubyte *) model_ptr);
+	return init_model_sub(reinterpret_cast<uint8_t *>(model_ptr), 0);
 }

common/include/3d.h
@@ -37,8 +37,6 @@ struct grs_bitmap;
 
 extern int g3d_interp_outline;      //if on, polygon models outlined in white
 
-extern short highest_texture_num;
-
 //Structure for storing u,v,light values.  This structure doesn't have a
 //prefix because it was defined somewhere else before it was moved here
 struct g3s_uvl {

similar/main/polyobj.cpp
@@ -659,7 +659,7 @@ int load_polygon_model(const char *filename,int n_textures,int first_texture,rob
 
 	polyobj_find_min_max(&Polygon_models[N_polygon_models]);
 
-	g3_init_polygon_model(Polygon_models[N_polygon_models].model_data.get());
+	const auto highest_texture_num = g3_init_polygon_model(Polygon_models[N_polygon_models].model_data.get());
 
 	if (highest_texture_num+1 != n_textures)
 		Error("Model <%s> references %d textures but specifies %d.",filename,highest_texture_num+1,n_textures);

commit 3a37b41192ae20e618741457c3c01138a84ac33a

Fix startup trap with D1 shareware data
D1 shareware models handle textures in a way that requires starting
highest_texture_num at -1.

Reported-by: derhass <https://github.com/dxx-rebirth/dxx-rebirth/issues/46>
Fixes: 68be3baeca7af9f24609f05b464c44228e9ac5b6 ("Track highest_texture_num locally")

common/include/interp.h
@@ -43,7 +43,7 @@ struct glow_values_t;
 void g3_draw_polygon_model(const uint8_t *model_ptr,grs_bitmap **model_bitmaps,submodel_angles anim_angles,g3s_lrgb light,const glow_values_t *glow_values, polygon_model_points &Interp_point_list);
 
 //init code for bitmap models
-uint16_t g3_init_polygon_model(void *model_ptr);
+int16_t g3_init_polygon_model(void *model_ptr);
 
 //un-initialize, i.e., convert color entries back to RGB15
 static inline void g3_uninit_polygon_model(void *model_ptr)

similar/3d/interp.cpp
@@ -32,7 +32,7 @@ static const unsigned OP_SUBCALL = 6;   //call a subobject
 static const unsigned OP_DEFP_START = 7;   //defpoints with start
 static const unsigned OP_GLOW = 8;   //glow value for next poly
 
-static uint16_t init_model_sub(uint8_t *p, uint16_t);
+static int16_t init_model_sub(uint8_t *p, int16_t);
 
 #ifdef EDITOR
 int g3d_interp_outline;
@@ -462,8 +462,8 @@ class init_model_sub_state :
 	public interpreter_base
 {
 public:
-	uint16_t highest_texture_num;
-	init_model_sub_state(uint16_t h) :
+	int16_t highest_texture_num;
+	init_model_sub_state(int16_t h) :
 		highest_texture_num(h)
 	{
 	}
@@ -764,7 +764,7 @@ void g3_draw_morphing_model(const uint8_t *p,grs_bitmap **model_bitmaps,const su
 	iterate_polymodel(p, state);
 }
 
-static uint16_t init_model_sub(uint8_t *p, uint16_t highest_texture_num)
+static int16_t init_model_sub(uint8_t *p, int16_t highest_texture_num)
 {
 	init_model_sub_state state(highest_texture_num);
 	Assert(++nest_count < 1000);
@@ -773,11 +773,11 @@ static uint16_t init_model_sub(uint8_t *p, uint16_t highest_texture_num)
 }
 
 //init code for bitmap models
-uint16_t g3_init_polygon_model(void *model_ptr)
+int16_t g3_init_polygon_model(void *model_ptr)
 {
 	#ifndef NDEBUG
 	nest_count = 0;
 	#endif
 
-	return init_model_sub(reinterpret_cast<uint8_t *>(model_ptr), 0);
+	return init_model_sub(reinterpret_cast<uint8_t *>(model_ptr), -1);
 }