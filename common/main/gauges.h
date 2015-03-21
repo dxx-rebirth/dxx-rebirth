dxx-rebirth/dxx-rebirth

commit b4147585055e2d462be05e07f25110af2bc9b7ea

Add stub for player color remapping

common/main/gauges.h
@@ -75,7 +75,20 @@ struct rgb {
 	ubyte r,g,b;
 };
 
-extern const rgb player_rgb[MAX_PLAYERS];
+typedef const array<rgb, MAX_PLAYERS> rgb_array_t;
+extern const rgb_array_t player_rgb_normal;
+
+/* Stub for mods that provide switchable player colors */
+class rgb_array_wrapper
+{
+public:
+	constexpr const rgb &operator[](std::size_t i) const
+	{
+		return player_rgb_normal[i];
+	}
+};
+
+constexpr rgb_array_wrapper player_rgb{};
 
 #if defined(DXX_BUILD_DESCENT_II)
 #define WBU_WEAPON      0       // the weapons display

similar/main/gauges.cpp
@@ -2471,7 +2471,7 @@ static void draw_invulnerable_ship()
 		draw_shield_bar(f2ir(Players[Player_num].shields));
 }
 
-const rgb player_rgb[MAX_PLAYERS] = {
+const rgb_array_t player_rgb_normal{{
 							{15,15,23},
 							{27,0,0},
 							{0,23,0},
@@ -2480,7 +2480,7 @@ const rgb player_rgb[MAX_PLAYERS] = {
 							{24,17,6},
 							{14,21,12},
 							{29,29,0},
-						};
+}};
 
 struct xy {
 	sbyte x, y;
@@ -2733,7 +2733,7 @@ static void hud_show_kill_list()
 		}
 		else
 		{
-			auto &color = player_rgb[player_num];
+			auto &color = player_rgb_normal[player_num];
 			fontcolor = BM_XRGB(color.r, color.g, color.b);
 		}
 		gr_set_fontcolor(fontcolor, -1);

similar/main/kmatrix.cpp
@@ -247,8 +247,8 @@ static void kmatrix_redraw_coop()
 			gr_set_fontcolor(gr_find_closest_color(31,31,31),-1);
 		else
 		{
-			const auto color = get_player_color(sorted[i]);
-			gr_set_fontcolor(BM_XRGB(player_rgb[color].r,player_rgb[color].g,player_rgb[color].b),-1 );
+			auto &color = player_rgb_normal[get_player_color(sorted[i])];
+			gr_set_fontcolor(BM_XRGB(color.r, color.g, color.b),-1 );
 		}
 
 		kmatrix_draw_coop_item( i, sorted );

commit 3da8a85ec3489b33768b1190f8ef58e6de4ab8dc

Remove constexpr on rgb_array_wrapper::operator[]
OS X clang (but not Linux clang) rejects constexpr
on rgb_array_wrapper::operator[].

Reported-by: btb <https://github.com/dxx-rebirth/dxx-rebirth/commit/b4147585055e2d462be05e07f25110af2bc9b7ea#commitcomment-10330686>
Fixes: b4147585055e2d462be05e07f25110af2bc9b7ea ("Add stub for player color remapping")

common/main/gauges.h
@@ -82,7 +82,7 @@ extern const rgb_array_t player_rgb_normal;
 class rgb_array_wrapper
 {
 public:
-	constexpr const rgb &operator[](std::size_t i) const
+	const rgb &operator[](std::size_t i) const
 	{
 		return player_rgb_normal[i];
 	}