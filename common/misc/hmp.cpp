3e520afc89f1cba1f806b247546f2dd037d15b96
Kp <kp@valhallalegends.com>
2015-04-19 04:18:49 +0000

    Reduce size of hmp_open buffer

--- common/misc/hmp.cpp
+++ common/misc/hmp.cpp
@@ -52,13 +52,13 @@ hmp_file::~hmp_file()
 
 std::unique_ptr<hmp_file> hmp_open(const char *filename) {
 	int data, num_tracks, tempo;
-	char buf[256];
 	auto fp = PHYSFSX_openReadBuffered(filename);
 
 	if (!fp)
 		return NULL;
 
 	std::unique_ptr<hmp_file> hmp(new hmp_file{});
+	char buf[8];
 	if ((PHYSFS_read(fp, buf, 1, 8) != 8) || (memcmp(buf, "HMIMIDIP", 8)))
 	{
 		return NULL;
