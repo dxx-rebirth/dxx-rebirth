c20cfc02cbf2dbb6cb44f2ff277c9943f7de7536
Kp <kp@valhallalegends.com>
2015-04-19 04:18:52 +0000

    Remove unused D1 definition of POW_FULL_MAP

--- common/main/powerup.h
+++ common/main/powerup.h
@@ -23,9 +23,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
  *
  */
 
-
-#ifndef _POWERUP_H
-#define _POWERUP_H
+#pragma once
 
 #include "dxxsconf.h"
 #include "vclip.h"
@@ -47,9 +45,6 @@ enum powerup_type_t
 
 //	POW_RADAR_ROBOTS = 7,
 //	POW_RADAR_POWERUPS = 8,
-#if defined(DXX_BUILD_DESCENT_I)
-	POW_FULL_MAP = 9,
-#endif
 
 	POW_MISSILE_1 = 10,
 	POW_MISSILE_4 = 11,      // 4-pack MUST follow single missile
@@ -150,5 +145,3 @@ void powerup_type_info_read(PHYSFS_file *fp, powerup_type_info &pti);
 void powerup_type_info_write(PHYSFS_file *fp, const powerup_type_info &pti);
 
 #endif
-
-#endif /* _POWERUP_H */
