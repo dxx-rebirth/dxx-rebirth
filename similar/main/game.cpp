9cfdc1d37618a7f6eb4ce363bcda80c7ca86412c
Kp <kp@valhallalegends.com>
2015-04-22 02:44:30 +0000

    Fix game_leave_menus infinite loop if menu rejects close

--- similar/main/game.cpp
+++ similar/main/game.cpp
@@ -1214,13 +1214,18 @@ static void do_ambient_sounds()
 
 void game_leave_menus(void)
 {
-	window *wind;
-
 	if (!Game_wind)
 		return;
-
-	while ((wind = window_get_front()) && (wind != Game_wind)) // go through all windows and actually close them if they want to
-		window_close(wind);
+	for (;;) // go through all windows and actually close them if they want to
+	{
+		const auto wind = window_get_front();
+		if (!wind)
+			break;
+		if (wind == Game_wind)
+			break;
+		if (!window_close(wind))
+			break;
+	}
 }
 
 void GameProcessFrame(void)
