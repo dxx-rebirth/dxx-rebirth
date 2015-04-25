dxx-rebirth/dxx-rebirth

commit 5e4bd5c73f24a36654038cef8cfe242bf49e0b83

vm_vec_delta_ang: check for vm_vec_copy_normalize failure
derhass reports that Raspbian g++-4.8.2 rejects vecmat.cpp for
-Wmaybe-uninitialized on t0, t1.  Fix it by adding code to return 0 if
vm_vec_copy_normalize leaves the temporary uninitialized.

Reported-by: derhass <https://github.com/dxx-rebirth/dxx-rebirth/issues/55>

common/include/vecmat.h
@@ -349,8 +349,10 @@ __attribute_warn_unused_result
 vm_distance vm_vec_dist_quick(const vms_vector &v0, const vms_vector &v1);
 
 //normalize a vector. returns mag of source vec
+__attribute_warn_unused_result
 vm_magnitude vm_vec_copy_normalize(vms_vector &dest, const vms_vector &src);
 
+__attribute_warn_unused_result
 vm_magnitude vm_vec_normalize(vms_vector &v);
 static inline vms_vector vm_vec_normalized(vms_vector v) __attribute_warn_unused_result;
 static inline vms_vector vm_vec_normalized(vms_vector v)

common/maths/vecmat.cpp
@@ -377,8 +377,8 @@ fixang vm_vec_delta_ang(const vms_vector &v0,const vms_vector &v1,const vms_vect
 {
 	vms_vector t0,t1;
 
-	vm_vec_copy_normalize(t0,v0);
-	vm_vec_copy_normalize(t1,v1);
+	if (!vm_vec_copy_normalize(t0,v0) || !vm_vec_copy_normalize(t1,v1))
+		return 0;
 
 	return vm_vec_delta_ang_norm(t0,t1,fvec);
 }

commit 23364a5862fdc515f7c482214f0a20ae9b7d3285

Remove warn_unused_result from vm_vec_normalize
Some callers want the side effect of normalizing the argument and do not
need the magnitude.  Per report from btb, the warning broke the OS X
build.

Reported-by: btb <https://github.com/dxx-rebirth/dxx-rebirth/commit/5e4bd5c73f24a36654038cef8cfe242bf49e0b83#commitcomment-10899878>
Fixes: 5e4bd5c73f24a36654038cef8cfe242bf49e0b83 ("vm_vec_delta_ang: check for vm_vec_copy_normalize failure")

common/include/vecmat.h
@@ -352,7 +352,6 @@ vm_distance vm_vec_dist_quick(const vms_vector &v0, const vms_vector &v1);
 __attribute_warn_unused_result
 vm_magnitude vm_vec_copy_normalize(vms_vector &dest, const vms_vector &src);
 
-__attribute_warn_unused_result
 vm_magnitude vm_vec_normalize(vms_vector &v);
 static inline vms_vector vm_vec_normalized(vms_vector v) __attribute_warn_unused_result;
 static inline vms_vector vm_vec_normalized(vms_vector v)