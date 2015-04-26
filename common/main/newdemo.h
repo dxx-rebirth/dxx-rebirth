dxx-rebirth/dxx-rebirth

commit 91d31b1952fe475199e2f457b5f051f1d281dc21

Wrap object signature in subtype

common/main/newdemo.h
@@ -33,6 +33,8 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "objnum.h"
 #include "fwdvalptridx.h"
 
+struct object_signature_t;
+
 #define ND_STATE_NORMAL			0
 #define ND_STATE_RECORDING		1
 #define ND_STATE_PLAYBACK		2
@@ -140,7 +142,7 @@ extern int newdemo_swap_endian(const char *filename);
 extern int newdemo_get_percent_done();
 
 extern void newdemo_record_link_sound_to_object3( int soundno, short objnum, fix max_volume, fix  max_distance, int loop_start, int loop_end );
-objnum_t newdemo_find_object(int signature);
+cobjptridx_t newdemo_find_object(object_signature_t signature);
 void newdemo_record_kill_sound_linked_to_object(vcobjptridx_t);
 void newdemo_record_guided_end();
 void newdemo_record_guided_start();

common/main/aistruct.h
@@ -217,14 +217,13 @@ struct ai_static : public prohibit_void_ptr<ai_static>
 	short   cur_path_index;         // Current index in path.
 	segnum_t   follow_path_start_seg;  // Start segment for robot which follows path.
 	segnum_t   follow_path_end_seg;    // End segment for robot which follows path.
-	int     danger_laser_signature;
 #elif defined(DXX_BUILD_DESCENT_II)
 	sbyte   cur_path_index;         // Current index in path.
 	sbyte   dying_sound_playing;    // !0 if this robot is playing its dying sound.
 #endif
 	objnum_t   danger_laser_num;
+	object_signature_t     danger_laser_signature;
 #if defined(DXX_BUILD_DESCENT_II)
-	int     danger_laser_signature;
 	fix64   dying_start_time;       // Time at which this robot started dying.
 #endif
 	ai_local ail;

common/main/fwdobject.h
@@ -265,7 +265,7 @@ void extract_quaternionpos(vobjptridx_t objp, quaternionpos *qpp, int swap_bytes
 void clear_transient_objects(int clear_all);
 
 // Returns a new, unique signature for a new object
-int obj_get_signature();
+object_signature_t obj_get_signature();
 
 // returns the number of a free object, updating Highest_object_index.
 // Generally, obj_create() should be called to get an object, since it

common/main/game.h
@@ -61,8 +61,9 @@ extern fix64 Next_flare_fire_time;
 extern fix Laser_delay_time;        // Delay between laser fires.
 
 #if defined(DXX_BUILD_DESCENT_II)
+class object_signature_t;
 extern struct object *Missile_viewer;
-extern int Missile_viewer_sig;
+extern object_signature_t Missile_viewer_sig;
 
 #define CV_NONE     0
 #define CV_ESCORT   1

common/main/hostage.h
@@ -23,9 +23,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
  *
  */
 
-
-#ifndef _HOSTAGE_H
-#define _HOSTAGE_H
+#pragma once
 
 #ifdef __cplusplus
 
@@ -40,10 +38,10 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 struct hostage_data
 {
 	objnum_t		objnum;
-	int		objsig;
+	object_signature_t objsig;
 };
 
-extern hostage_data Hostages[MAX_HOSTAGES];
+extern array<hostage_data, MAX_HOSTAGES> Hostages;
 
 //returns true if something drew
 int do_hostage_effects();
@@ -70,5 +68,3 @@ void draw_hostage(vobjptridx_t obj);
 void hostage_rescue();
 
 #endif
-
-#endif /* _HOSTAGE_H */

common/main/morph.h
@@ -46,7 +46,7 @@ struct morph_data
 	ubyte morph_save_control_type;
 	ubyte morph_save_movement_type;
 	physics_info morph_save_phys_info;
-	int Morph_sig;
+	object_signature_t Morph_sig;
 };
 
 #define MAX_MORPH_OBJECTS 5

common/main/object.h
@@ -191,7 +191,7 @@ struct laser_info : prohibit_void_ptr<laser_info>
 	};
 	short   parent_type;        // The type of the parent of this object
 	objnum_t   parent_num;         // The object's parent's number
-	int     parent_signature;   // The object's parent's signature...
+	object_signature_t     parent_signature;   // The object's parent's signature...
 	fix64   creation_time;      // Absolute time of creation.
 	hitobj_list_t hitobj_list;	// list of all objects persistent weapon has already damaged (useful in case it's in contact with two objects at the same time)
 	objnum_t   last_hitobj;        // For persistent weapons (survive object collision), object it most recently hit.
@@ -300,7 +300,7 @@ struct polyobj_info_rw
 
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
 struct object {
-	int     signature;      // Every object ever has a unique signature...
+	object_signature_t signature;
 	ubyte   type;           // what type of object this is... robot, weapon, hostage, powerup, fireball
 	ubyte   id;             // which form of object...which powerup, robot, etc.
 	objnum_t   next,prev;      // id of next and previous connected object in Objects, -1 = no connection

common/main/objnum.h
@@ -6,3 +6,23 @@ DEFINE_STRICT_INDEX_CONSTANT_TYPE(object_magic_constant_t, objnum_t, int16_t);
 DEFINE_STRICT_INDEX_CONSTANT_NUMBER(object_magic_constant_t, -2, object_guidebot_cannot_reach);
 DEFINE_STRICT_INDEX_CONSTANT_NUMBER(object_magic_constant_t, -1, object_none);
 DEFINE_STRICT_INDEX_CONSTANT_NUMBER(object_magic_constant_t, 0, object_first);
+
+class object_signature_t
+{
+	uint16_t signature;
+public:
+	object_signature_t() = default;
+	constexpr explicit object_signature_t(uint16_t s) :
+		signature(s)
+	{
+	}
+	bool operator==(const object_signature_t &rhs) const
+	{
+		return signature == rhs.signature;
+	}
+	bool operator!=(const object_signature_t &rhs) const
+	{
+		return !(*this == rhs);
+	}
+	uint16_t get() const { return signature; }
+};

commit bfd434fb1514116e3634be051aa6b4a88ab5b508

Switch object_signature_t forward declaration to class
Although struct and class are interchangeable in this context, clang
whines when the program mixes them.  Use 'class' consistently to silence
this warning.

Reported-by: btb <https://github.com/dxx-rebirth/dxx-rebirth/commit/91d31b1952fe475199e2f457b5f051f1d281dc21#commitcomment-10903195>
Fixes: 91d31b1952fe475199e2f457b5f051f1d281dc21 ("Wrap object signature in subtype")

common/main/newdemo.h
@@ -33,7 +33,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "objnum.h"
 #include "fwdvalptridx.h"
 
-struct object_signature_t;
+class object_signature_t;
 
 #define ND_STATE_NORMAL			0
 #define ND_STATE_RECORDING		1