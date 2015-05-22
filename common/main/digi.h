dxx-rebirth/dxx-rebirth

commit ae8b99ae7d4f8e8aa2da1206b9b0b0e4faa9df75

Use RAIIdigi_sound for briefing printing_channel

common/main/digi.h
@@ -23,17 +23,17 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
  *
  */
 
-
-
-#ifndef _DIGI_H
-#define _DIGI_H
+#pragma once
 
 #include "pstypes.h"
 #include "vecmat.h"
 
 #ifdef __cplusplus
+#include "dxxsconf.h"
 #include "segnum.h"
 #include "fwdvalptridx.h"
+#include "compiler-exchange.h"
+#include "compiler-type_traits.h"
 
 struct sound_object;
 struct digi_sound
@@ -140,4 +140,31 @@ int verify_sound_channel_free( int channel );
 
 #endif
 
-#endif
+class RAIIdigi_sound
+{
+	static constexpr tt::integral_constant<int, -1> invalid_channel{};
+	int channel;
+	static void stop(int channel)
+	{
+		if (channel != invalid_channel)
+			digi_stop_sound(channel);
+	}
+public:
+	RAIIdigi_sound() :
+		channel(invalid_channel)
+	{
+	}
+	~RAIIdigi_sound()
+	{
+		stop(channel);
+	}
+	void reset(int c = invalid_channel)
+	{
+		stop(exchange(channel, c));
+	}
+	operator int() const = delete;
+	explicit operator bool() const
+	{
+		return channel != invalid_channel;
+	}
+};

similar/main/digiobj.cpp
@@ -83,6 +83,8 @@ struct sound_object
 	} link_type;
 };
 
+constexpr tt::integral_constant<int, -1> RAIIdigi_sound::invalid_channel;
+
 #define MAX_SOUND_OBJECTS 150
 typedef array<sound_object, MAX_SOUND_OBJECTS> sound_objects_t;
 static sound_objects_t SoundObjects;

similar/main/titles.cpp
@@ -468,7 +468,7 @@ struct briefing : ignore_window_pointer_t
 	char	background_name[PATH_MAX];
 #if defined(DXX_BUILD_DESCENT_II)
 	int		got_z;
-	int		hum_channel, printing_channel;
+	RAIIdigi_sound		hum_channel, printing_channel;
 	MVESTREAM_ptr_t pMovie;
 #endif
 	std::unique_ptr<char[]>	text;
@@ -509,7 +509,6 @@ static void briefing_init(briefing *br, short level_num)
 	gr_init_bitmap_data(br->background);
 	strncpy(br->background_name, DEFAULT_BRIEFING_BKG, sizeof(br->background_name));
 #if defined(DXX_BUILD_DESCENT_II)
-	br->hum_channel = br->printing_channel = -1;
 	br->robot_playing = 0;
 #endif
 	br->robot_num = 0;
@@ -689,7 +688,7 @@ static void put_char_delay(briefing *br, char ch)
 
 #if defined(DXX_BUILD_DESCENT_II)
 	if (!EMULATING_D1 && !br->chattering) {
-		br->printing_channel  = digi_start_sound( digi_xlat_sound(SOUND_BRIEFING_PRINTING), F1_0, 0xFFFF/2, 1, -1, -1, sound_object_none);
+		br->printing_channel.reset(digi_start_sound(digi_xlat_sound(SOUND_BRIEFING_PRINTING), F1_0, 0xFFFF/2, 1, -1, -1, sound_object_none));
 		br->chattering=1;
 	}
 #endif
@@ -846,9 +845,7 @@ static int briefing_process_char(briefing *br)
 		} else if (ch == 'S') {
 #if defined(DXX_BUILD_DESCENT_II)
 			br->chattering = 0;
-			if (br->printing_channel >- 1)
-				digi_stop_sound(br->printing_channel);
-			br->printing_channel =- 1;
+			br->printing_channel.reset();
 #endif
 
 			br->new_screen = 1;
@@ -862,9 +859,7 @@ static int briefing_process_char(briefing *br)
 			}
 
 			br->chattering = 0;
-			if (br->printing_channel >- 1)
-				digi_stop_sound(br->printing_channel);
-			br->printing_channel =- 1;
+			br->printing_channel.reset();
 #endif
 
 			br->new_page = 1;
@@ -1315,8 +1310,7 @@ static void free_briefing_screen(briefing *br)
 #endif
 	br->robot_canv.reset();
 #if defined(DXX_BUILD_DESCENT_II)
-	if (br->printing_channel>-1)
-		digi_stop_sound( br->printing_channel );
+	br->printing_channel.reset();
 #endif
 	if (EMULATING_D1)
 		br->screen.reset();
@@ -1388,7 +1382,7 @@ static int new_briefing_screen(briefing *br, int first)
 		return 0;	// finished
 
 	br->message = get_briefing_message(br, EMULATING_D1 ? Briefing_screens[br->cur_screen].message_num : br->cur_screen);
-	br->printing_channel = -1;
+	br->printing_channel.reset();
 	br->dumb_adjust = 0;
 	br->line_adjustment = 1;
 	br->chattering = 0;
@@ -1411,8 +1405,8 @@ static int new_briefing_screen(briefing *br, int first)
 	br->prev_ch = -1;
 
 #if defined(DXX_BUILD_DESCENT_II)
-	if ((songs_is_playing() == -1) && (br->hum_channel == -1))
-		br->hum_channel  = digi_start_sound( digi_xlat_sound(SOUND_BRIEFING_HUM), F1_0/2, 0xFFFF/2, 1, -1, -1, sound_object_none);
+	if (songs_is_playing() == -1 && !br->hum_channel)
+		br->hum_channel.reset(digi_start_sound(digi_xlat_sound(SOUND_BRIEFING_HUM), F1_0/2, 0xFFFF/2, 1, -1, -1, sound_object_none));
 #endif
 
 	return 1;
@@ -1528,11 +1522,7 @@ static window_event_result briefing_handler(window *wind,const d_event &event, b
 		case EVENT_WINDOW_CLOSE:
 			free_briefing_screen(br);
 #if defined(DXX_BUILD_DESCENT_II)
-			if (br->hum_channel>-1)
-			{
-				digi_stop_sound( br->hum_channel );
-				br->hum_channel = -1;
-			}
+			br->hum_channel.reset();
 #endif
 			break;
 

commit ef1abd87e8e29f14a64b9394254640e7863d0034

Fix gcc-4.6 digi build
gcc-4.6 chokes on `static constexpr type value{};`, but accepts
`static constexpr auto value = type{};`

Fixes: ae8b99ae7d4f8e8aa2da1206b9b0b0e4faa9df75 ("Use RAIIdigi_sound for briefing printing_channel")

common/main/digi.h
@@ -142,7 +142,7 @@ int verify_sound_channel_free( int channel );
 
 class RAIIdigi_sound
 {
-	static constexpr tt::integral_constant<int, -1> invalid_channel{};
+	static constexpr auto invalid_channel = tt::integral_constant<int, -1>{};
 	int channel;
 	static void stop(int channel)
 	{