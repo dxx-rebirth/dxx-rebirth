dxx-rebirth/dxx-rebirth

commit 12ad357b8d3898efc202667e6c396424ed1da84d

Move Wimp Menu to macro

similar/main/gamecntl.cpp
@@ -1694,67 +1694,116 @@ static window_event_result FinalCheats()
 
 // Internal Cheat Menu
 #ifndef RELEASE
-static void do_cheat_menu()
+
+namespace {
+
+class menu_fix_wrapper
 {
-	int mmn;
-	newmenu_item mm[16];
-	char score_text[21];
+	fix &m_value;
+public:
+	constexpr menu_fix_wrapper(fix &t) :
+		m_value(t)
+	{
+	}
+	constexpr operator int() const
+	{
+		return f2i(m_value);
+	}
+	menu_fix_wrapper &operator=(int n)
+	{
+		m_value = i2f(n);
+		return *this;
+	}
+};
 
-	sprintf( score_text, "%d", Players[Player_num].score );
-
-	nm_set_item_checkbox(mm[0],TXT_INVULNERABILITY,Players[Player_num].flags & PLAYER_FLAGS_INVULNERABLE);
-	nm_set_item_checkbox(mm[1],TXT_CLOAKED,Players[Player_num].flags & PLAYER_FLAGS_CLOAKED);
-	nm_set_item_checkbox(mm[2],"All keys",0);
-	nm_set_item_number(mm[3], "% Energy", f2i(Players[Player_num].energy), 0, 200);
-	nm_set_item_number(mm[4], "% Shields", f2i(Players[Player_num].shields), 0, 200);
-	nm_set_item_text(mm[5], "Score:");
-	nm_set_item_input(mm[6], score_text);
-#if defined(DXX_BUILD_DESCENT_I)
-	nm_set_item_radio(mm[7], "Laser level 1", (Players[Player_num].laser_level==0), 0);
-	nm_set_item_radio(mm[8], "Laser level 2", (Players[Player_num].laser_level==1), 0);
-	nm_set_item_radio(mm[9], "Laser level 3", (Players[Player_num].laser_level==2), 0);
-	nm_set_item_radio(mm[10], "Laser level 4", (Players[Player_num].laser_level==3), 0);
-	nm_set_item_number(mm[11], "Missiles", Players[Player_num].secondary_ammo[CONCUSSION_INDEX], 0, 200);
-
-	mmn = newmenu_do("Wimp Menu",NULL,12, mm, unused_newmenu_subfunction, unused_newmenu_userdata);
-#elif defined(DXX_BUILD_DESCENT_II)
-	nm_set_item_number(mm[7], "Laser Level", Players[Player_num].laser_level+1, 0, MAX_SUPER_LASER_LEVEL+1);
-	nm_set_item_number(mm[8], "Missiles", Players[Player_num].secondary_ammo[CONCUSSION_INDEX], 0, 200);
-
-	mmn = newmenu_do("Wimp Menu",NULL,9, mm, unused_newmenu_subfunction, unused_newmenu_userdata);
-#endif
-
-	if (mmn > -1 )  {
-		if ( mm[0].value )  {
-			Players[Player_num].flags |= PLAYER_FLAGS_INVULNERABLE;
-			Players[Player_num].invulnerable_time = GameTime64+i2f(1000);
-		} else
-			Players[Player_num].flags &= ~PLAYER_FLAGS_INVULNERABLE;
-		if ( mm[1].value )
+class cheat_menu_bit_invulnerability : public menu_bit_wrapper_t<uint32_t, uint32_t>
+{
+	player &m_player;
+public:
+	cheat_menu_bit_invulnerability(player &plr) :
+		menu_bit_wrapper_t(plr.flags, PLAYER_FLAGS_INVULNERABLE),
+		m_player(plr)
+	{
+	}
+	cheat_menu_bit_invulnerability &operator=(uint32_t n)
+	{
+		this->menu_bit_wrapper_t::operator=(n);
+		if (n)
 		{
-			Players[Player_num].flags |= PLAYER_FLAGS_CLOAKED;
+			m_player.invulnerable_time = GameTime64+i2f(1000);
+		}
+		return *this;
+	}
+};
+
+class cheat_menu_bit_cloak : public menu_bit_wrapper_t<uint32_t, uint32_t>
+{
+	player &m_player;
+public:
+	cheat_menu_bit_cloak(player &plr) :
+		menu_bit_wrapper_t(plr.flags, PLAYER_FLAGS_CLOAKED),
+		m_player(plr)
+	{
+	}
+	cheat_menu_bit_cloak &operator=(uint32_t n)
+	{
+		this->menu_bit_wrapper_t::operator=(n);
+		if (n)
+		{
 			if (Game_mode & GM_MULTI)
 				multi_send_cloak();
 			ai_do_cloak_stuff();
-			Players[Player_num].cloak_time = GameTime64;
+			m_player.cloak_time = GameTime64;
 		}
-		else
-			Players[Player_num].flags &= ~PLAYER_FLAGS_CLOAKED;
+		return *this;
+	}
+};
 
-		if (mm[2].value) Players[Player_num].flags |= PLAYER_FLAGS_BLUE_KEY | PLAYER_FLAGS_RED_KEY | PLAYER_FLAGS_GOLD_KEY;
-		Players[Player_num].energy=i2f(mm[3].value);
-		Players[Player_num].shields=i2f(mm[4].value);
-		Players[Player_num].score = atoi(mm[6].text);
+}
+
 #if defined(DXX_BUILD_DESCENT_I)
-		if (mm[7].value) Players[Player_num].laser_level=0;
-		if (mm[8].value) Players[Player_num].laser_level=1;
-		if (mm[9].value) Players[Player_num].laser_level=2;
-		if (mm[10].value) Players[Player_num].laser_level=3;
-		Players[Player_num].secondary_ammo[CONCUSSION_INDEX] = mm[11].value;
+#define WIMP_MENU_DXX(VERB)
 #elif defined(DXX_BUILD_DESCENT_II)
-		Players[Player_num].laser_level = mm[7].value-1;
-		Players[Player_num].secondary_ammo[CONCUSSION_INDEX] = mm[8].value;
+/* Adding an afterburner like this adds it at 0% charge.  This is OK for
+ * a cheat.  The player can change his energy up if he needs more.
+ */
+#define WIMP_MENU_DXX(VERB)	\
+	DXX_##VERB##_CHECK(TXT_AFTERBURNER, opt_afterburner, menu_bit_wrapper(plr.flags, PLAYER_FLAGS_AFTERBURNER))	\
+
 #endif
+
+#define DXX_WIMP_MENU(VERB)	\
+	DXX_##VERB##_CHECK(TXT_INVULNERABILITY, opt_invul, cheat_menu_bit_invulnerability(plr))	\
+	DXX_##VERB##_CHECK(TXT_CLOAKED, opt_cloak, cheat_menu_bit_cloak(plr))	\
+	DXX_##VERB##_CHECK("BLUE KEY", opt_key_blue, menu_bit_wrapper(plr.flags, PLAYER_FLAGS_BLUE_KEY))	\
+	DXX_##VERB##_CHECK("GOLD KEY", opt_key_gold, menu_bit_wrapper(plr.flags, PLAYER_FLAGS_GOLD_KEY))	\
+	DXX_##VERB##_CHECK("RED KEY", opt_key_red, menu_bit_wrapper(plr.flags, PLAYER_FLAGS_RED_KEY))	\
+	WIMP_MENU_DXX(VERB)	\
+	DXX_##VERB##_NUMBER(TXT_ENERGY, opt_energy, menu_fix_wrapper(plr.energy), 0, 200)	\
+	DXX_##VERB##_NUMBER("Shields", opt_shields, menu_fix_wrapper(plr.shields), 0, 200)	\
+	DXX_##VERB##_TEXT(TXT_SCORE, opt_txt_score)	\
+	DXX_##VERB##_INPUT(score_text, opt_score)	\
+	DXX_##VERB##_NUMBER("Laser Level", opt_laser_level, menu_number_bias_wrapper(plr.laser_level, 1), LASER_LEVEL_1 + 1, DXX_MAXIMUM_LASER_LEVEL + 1)	\
+	DXX_##VERB##_NUMBER("Concussion", opt_concussion, plr.secondary_ammo[CONCUSSION_INDEX], 0, 200)	\
+
+static void do_cheat_menu()
+{
+	enum {
+		DXX_WIMP_MENU(ENUM)
+	};
+	int mmn;
+	array<newmenu_item, DXX_WIMP_MENU(COUNT)> m;
+	char score_text[sizeof("2147483647")];
+	auto &plr = Players[Player_num];
+	snprintf(score_text, sizeof(score_text), "%d", plr.score);
+	DXX_WIMP_MENU(ADD);
+	mmn = newmenu_do("Wimp Menu",NULL,m, unused_newmenu_subfunction, unused_newmenu_userdata);
+	if (mmn > -1 )  {
+		DXX_WIMP_MENU(READ);
+		char *p;
+		auto ul = strtoul(score_text, &p, 10);
+		if (!*p)
+			plr.score = static_cast<int>(ul);
 		init_gauges();
 	}
 }

common/main/laser.h
@@ -150,6 +150,12 @@ enum laser_level_t
 #endif
 };
 
+#if defined(DXX_BUILD_DESCENT_I)
+#define DXX_MAXIMUM_LASER_LEVEL	LASER_LEVEL_4
+#elif defined(DXX_BUILD_DESCENT_II)
+#define DXX_MAXIMUM_LASER_LEVEL	MAX_SUPER_LASER_LEVEL
+#endif
+
 #define MAX_LASER_BITMAPS   6
 
 // For muzzle firing casting light.

common/main/newmenu.h
@@ -399,6 +399,7 @@ static inline void nm_set_item_slider(newmenu_item &ni, const char *text, unsign
 #define DXX_NEWMENU_VARIABLE	m
 #define DXX_ENUM_CHECK(S,OPT,V)	OPT,
 #define DXX_ENUM_RADIO(S,OPT,C,G)	OPT,
+#define DXX_ENUM_NUMBER DXX_ENUM_SLIDER
 #define DXX_ENUM_SLIDER(S,OPT,V,MIN,MAX)	OPT,
 #define DXX_ENUM_SCALE_SLIDER(S,OPT,V,MIN,MAX,SCALE)	OPT,
 #define DXX_ENUM_MENU(S,OPT)	OPT,
@@ -406,6 +407,7 @@ static inline void nm_set_item_slider(newmenu_item &ni, const char *text, unsign
 #define DXX_ENUM_INPUT(S,OPT)	OPT,
 #define DXX_COUNT_CHECK(S,OPT,V)	+1
 #define DXX_COUNT_RADIO(S,OPT,C,G)	+1
+#define DXX_COUNT_NUMBER DXX_COUNT_SLIDER
 #define DXX_COUNT_SLIDER(S,OPT,V,MIN,MAX)	+1
 #define DXX_COUNT_SCALE_SLIDER(S,OPT,V,MIN,MAX,SCALE)	+1
 #define DXX_COUNT_MENU(S,OPT)	+1
@@ -415,6 +417,8 @@ static inline void nm_set_item_slider(newmenu_item &ni, const char *text, unsign
 	nm_set_item_checkbox(((DXX_NEWMENU_VARIABLE)[(OPT)]), (S), (V));
 #define DXX_ADD_RADIO(S,OPT,C,G)	\
 	nm_set_item_radio(((DXX_NEWMENU_VARIABLE)[(OPT)]), (S), (C), (G));
+#define DXX_ADD_NUMBER(S,OPT,V,MIN,MAX)	\
+	nm_set_item_number(((DXX_NEWMENU_VARIABLE)[(OPT)]), (S), (V), (MIN), (MAX));
 #define DXX_ADD_SLIDER(S,OPT,V,MIN,MAX)	\
 	nm_set_item_slider(((DXX_NEWMENU_VARIABLE)[(OPT)]), (S), (V), (MIN), (MAX));
 #define DXX_ADD_SCALE_SLIDER(S,OPT,V,MIN,MAX,SCALE)	\
@@ -426,13 +430,72 @@ static inline void nm_set_item_slider(newmenu_item &ni, const char *text, unsign
 #define DXX_ADD_INPUT(S,OPT)	\
 	nm_set_item_input(((DXX_NEWMENU_VARIABLE)[(OPT)]),(S));
 #define DXX_READ_CHECK(S,OPT,V)	\
-	V = (DXX_NEWMENU_VARIABLE)[(OPT)].value;
+	(V) = (DXX_NEWMENU_VARIABLE)[(OPT)].value;
+#define DXX_READ_NUMBER(S,OPT,V,MIN,MAX)	\
+	(V) = (DXX_NEWMENU_VARIABLE)[(OPT)].value;
 #define DXX_READ_SLIDER(S,OPT,V,MIN,MAX)	\
-	V = (DXX_NEWMENU_VARIABLE)[(OPT)].value;
+	(V) = (DXX_NEWMENU_VARIABLE)[(OPT)].value;
 #define DXX_READ_SCALE_SLIDER(S,OPT,V,MIN,MAX,SCALE)	\
-	V = (DXX_NEWMENU_VARIABLE)[(OPT)].value * (SCALE);
+	(V) = (DXX_NEWMENU_VARIABLE)[(OPT)].value * (SCALE);
 #define DXX_READ_MENU(S,OPT)	/* handled specially */
 #define DXX_READ_TEXT(S,OPT)	/* handled specially */
 #define DXX_READ_INPUT(S,OPT)	/* handled specially */
+
+template <typename T, typename B>
+class menu_bit_wrapper_t
+{
+	T &m_mask;
+	B m_bit;
+public:
+	constexpr menu_bit_wrapper_t(T &t, B bit) :
+		m_mask(t), m_bit(bit)
+	{
+	}
+	constexpr operator T() const
+	{
+		return m_mask & m_bit;
+	}
+	menu_bit_wrapper_t &operator=(bool n)
+	{
+		if (n)
+			m_mask |= m_bit;
+		else
+			m_mask &= ~m_bit;
+		return *this;
+	}
+};
+
+template <typename T, typename B>
+static constexpr menu_bit_wrapper_t<T, B> menu_bit_wrapper(T &t, B b)
+{
+	return {t, b};
+}
+
+template <typename T, typename B>
+class menu_number_bias_wrapper_t
+{
+	T &m_value;
+	B m_bias;
+public:
+	constexpr menu_number_bias_wrapper_t(T &t, B bias) :
+		m_value(t), m_bias(bias)
+	{
+	}
+	constexpr operator T() const
+	{
+		return m_value + m_bias;
+	}
+	menu_number_bias_wrapper_t &operator=(T n)
+	{
+		m_value = n - m_bias;
+		return *this;
+	}
+};
+
+template <typename T, typename B>
+static constexpr menu_number_bias_wrapper_t<T, B> menu_number_bias_wrapper(T &t, B b)
+{
+	return {t, b};
+}
 
 #endif

commit 4cd329d2b4fb13dd2acf3e9a4213bf75b6941658

Fix gcc-4.6 gamecntl build
gcc-4.6 chokes on constexpr operator int(); later gcc accepts this.
menu_fix_wrapper is never converted in constexpr context, so remove the
constexpr specifier.

Fixes: 12ad357b8d3898efc202667e6c396424ed1da84d ("Move Wimp Menu to macro")

similar/main/gamecntl.cpp
@@ -1710,7 +1710,7 @@ public:
 		m_value(t)
 	{
 	}
-	constexpr operator int() const
+	operator int() const
 	{
 		return f2i(m_value);
 	}