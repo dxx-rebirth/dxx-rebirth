dxx-rebirth/dxx-rebirth

commit 9595aec2fb7b369d93aa4d687d7c0e0a66773d59

Use integral_constant for endian constants

common/include/serial.h
@@ -106,10 +106,10 @@ struct endian_access
 	 * - native_endian: assume buffered data is native endian
 	 *   Copy regardless of host byte order
 	 */
-	static const uint16_t foreign_endian = 0;
-	static const uint16_t little_endian = 255;
-	static const uint16_t big_endian = 256;
-	static const uint16_t native_endian = 257;
+	static constexpr tt::integral_constant<uint16_t, 0> foreign_endian{};
+	static constexpr tt::integral_constant<uint16_t, 255> little_endian{};
+	static constexpr tt::integral_constant<uint16_t, 256> big_endian{};
+	static constexpr tt::integral_constant<uint16_t, 257> native_endian{};
 };
 
 	/* Implementation details - avoid namespace pollution */

commit 0fc7b1898c6b946eb5bbd373a64601194cd3c895

Fix gcc-4.6 serial build
gcc-4.6 chokes on `static constexpr type value{};`, but accepts
`static constexpr auto value = type{};`

Fixes: 9595aec2fb7b369d93aa4d687d7c0e0a66773d59 ("Use integral_constant for endian constants")

common/include/serial.h
@@ -106,10 +106,10 @@ struct endian_access
 	 * - native_endian: assume buffered data is native endian
 	 *   Copy regardless of host byte order
 	 */
-	static constexpr tt::integral_constant<uint16_t, 0> foreign_endian{};
-	static constexpr tt::integral_constant<uint16_t, 255> little_endian{};
-	static constexpr tt::integral_constant<uint16_t, 256> big_endian{};
-	static constexpr tt::integral_constant<uint16_t, 257> native_endian{};
+	static constexpr auto foreign_endian= tt::integral_constant<uint16_t, 0>{};
+	static constexpr auto little_endian	= tt::integral_constant<uint16_t, 255>{};
+	static constexpr auto big_endian	= tt::integral_constant<uint16_t, 256>{};
+	static constexpr auto native_endian	= tt::integral_constant<uint16_t, 257>{};
 };
 
 	/* Implementation details - avoid namespace pollution */