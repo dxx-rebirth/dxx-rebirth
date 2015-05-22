dxx-rebirth/dxx-rebirth

commit 8a28f7e1c2ce0c8e1fabd82e29ae9b85e8851d90

Use array<> for movie decoder

d2x-rebirth/libmve/decoder8.cpp
@@ -12,6 +12,9 @@
 #include "decoders.h"
 #include "console.h"
 
+#include "dxxsconf.h"
+#include "compiler-array.h"
+
 static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, const unsigned char **pData, int *pDataRemain, int *curXb, int *curYb);
 
 void decodeFrame8(unsigned char *pFrame, const unsigned char *pMap, int mapRemain, const unsigned char *pData, int dataRemain)
@@ -84,7 +87,7 @@ static void copyFrame(unsigned char *pDest, unsigned char *pSrc)
 // depending on the corresponding two-bit value in pat0 and pat1
 static void patternRow4Pixels(unsigned char *pFrame,
 							  unsigned char pat0, unsigned char pat1,
-							  unsigned char *p)
+							  const array<uint8_t, 4> &p)
 {
 	unsigned short mask=0x0003;
 	unsigned short shift=0;
@@ -102,7 +105,7 @@ static void patternRow4Pixels(unsigned char *pFrame,
 // depending on the corresponding two-bit value in pat0.
 static void patternRow4Pixels2(unsigned char *pFrame,
 							   unsigned char pat0,
-							   unsigned char *p)
+							   const array<uint8_t, 4> &p)
 {
 	unsigned char mask=0x03;
 	unsigned char shift=0;
@@ -123,7 +126,7 @@ static void patternRow4Pixels2(unsigned char *pFrame,
 
 // Fill in the next four 2x1 pixel blocks with p[0], p[1], p[2], or p[3],
 // depending on the corresponding two-bit value in pat.
-static void patternRow4Pixels2x1(unsigned char *pFrame, unsigned char pat, unsigned char *p)
+static void patternRow4Pixels2x1(unsigned char *pFrame, unsigned char pat, const array<uint8_t, 4> &p)
 {
 	unsigned char mask=0x03;
 	unsigned char shift=0;
@@ -142,7 +145,7 @@ static void patternRow4Pixels2x1(unsigned char *pFrame, unsigned char pat, unsig
 
 // Fill in the next 4x4 pixel block with p[0], p[1], p[2], or p[3],
 // depending on the corresponding two-bit value in pat0, pat1, pat2, and pat3.
-static void patternQuadrant4Pixels(unsigned char *pFrame, unsigned char pat0, unsigned char pat1, unsigned char pat2, unsigned char pat3, unsigned char *p)
+static void patternQuadrant4Pixels(unsigned char *pFrame, unsigned char pat0, unsigned char pat1, unsigned char pat2, unsigned char pat3, const array<uint8_t, 4> &p)
 {
 	unsigned long mask = 0x00000003UL;
 	int shift=0;
@@ -161,7 +164,7 @@ static void patternQuadrant4Pixels(unsigned char *pFrame, unsigned char pat0, un
 }
 
 // fills the next 8 pixels with either p[0] or p[1], depending on pattern
-static void patternRow2Pixels(unsigned char *pFrame, unsigned char pat, unsigned char *p)
+static void patternRow2Pixels(unsigned char *pFrame, unsigned char pat, const array<uint8_t, 4> &p)
 {
 	unsigned char mask=0x01;
 
@@ -173,7 +176,7 @@ static void patternRow2Pixels(unsigned char *pFrame, unsigned char pat, unsigned
 }
 
 // fills the next four 2 x 2 pixel boxes with either p[0] or p[1], depending on pattern
-static void patternRow2Pixels2(unsigned char *pFrame, unsigned char pat, unsigned char *p)
+static void patternRow2Pixels2(unsigned char *pFrame, unsigned char pat, const array<uint8_t, 4> &p)
 {
 	unsigned char pel;
 	unsigned char mask=0x1;
@@ -193,7 +196,7 @@ static void patternRow2Pixels2(unsigned char *pFrame, unsigned char pat, unsigne
 }
 
 // fills pixels in the next 4 x 4 pixel boxes with either p[0] or p[1], depending on pat0 and pat1.
-static void patternQuadrant2Pixels(unsigned char *pFrame, unsigned char pat0, unsigned char pat1, unsigned char *p)
+static void patternQuadrant2Pixels(unsigned char *pFrame, unsigned char pat0, unsigned char pat1, const array<uint8_t, 4> &p)
 {
 	unsigned char pel;
 	unsigned short mask = 0x0001;
@@ -214,8 +217,7 @@ static void patternQuadrant2Pixels(unsigned char *pFrame, unsigned char pat0, un
 
 static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, const unsigned char **pData, int *pDataRemain, int *curXb, int *curYb)
 {
-	unsigned char p[4];
-	unsigned char pat[16];
+	array<uint8_t, 4> p, pat;
 	int x, y;
 
 	/* Data is processed in 8x8 pixel blocks.

d2x-rebirth/libmve/decoder16.cpp
@@ -157,7 +157,7 @@ static void copyFrame(unsigned short *pDest, unsigned short *pSrc)
 
 static void patternRow4Pixels(unsigned short *pFrame,
                               unsigned char pat0, unsigned char pat1,
-                              unsigned short *p)
+                              const array<uint16_t, 4> &p)
 {
     unsigned short mask=0x0003;
     unsigned short shift=0;
@@ -173,7 +173,7 @@ static void patternRow4Pixels(unsigned short *pFrame,
 
 static void patternRow4Pixels2(unsigned short *pFrame,
                                unsigned char pat0,
-                               unsigned short *p)
+                               const array<uint16_t, 4> &p)
 {
     unsigned char mask=0x03;
     unsigned char shift=0;
@@ -208,7 +208,7 @@ static void patternRow4Pixels2(unsigned short *pFrame,
 }
 
 static void patternRow4Pixels2x1(unsigned short *pFrame, unsigned char pat,
-								 unsigned short *p)
+								 const array<uint16_t, 4> &p)
 {
     unsigned char mask=0x03;
     unsigned char shift=0;
@@ -227,7 +227,7 @@ static void patternRow4Pixels2x1(unsigned short *pFrame, unsigned char pat,
 
 static void patternQuadrant4Pixels(unsigned short *pFrame,
 								   unsigned char pat0, unsigned char pat1, unsigned char pat2,
-								   unsigned char pat3, unsigned short *p)
+								   unsigned char pat3, const array<uint16_t, 4> &p)
 {
     unsigned long mask = 0x00000003UL;
     int shift=0;
@@ -248,7 +248,7 @@ static void patternQuadrant4Pixels(unsigned short *pFrame,
 
 
 static void patternRow2Pixels(unsigned short *pFrame, unsigned char pat,
-							  unsigned short *p)
+							  const array<uint16_t, 4> &p)
 {
     unsigned char mask=0x01;
 
@@ -260,7 +260,7 @@ static void patternRow2Pixels(unsigned short *pFrame, unsigned char pat,
 }
 
 static void patternRow2Pixels2(unsigned short *pFrame, unsigned char pat,
-							   unsigned short *p)
+							   const array<uint16_t, 4> &p)
 {
     unsigned short pel;
     unsigned char mask=0x1;
@@ -293,7 +293,7 @@ static void patternRow2Pixels2(unsigned short *pFrame, unsigned char pat,
 }
 
 static void patternQuadrant2Pixels(unsigned short *pFrame, unsigned char pat0,
-								   unsigned char pat1, unsigned short *p)
+								   unsigned char pat1, const array<uint16_t, 4> &p)
 {
     unsigned short mask = 0x0001;
     int i;
@@ -312,8 +312,8 @@ static void patternQuadrant2Pixels(unsigned short *pFrame, unsigned char pat0,
 
 static void dispatchDecoder16(unsigned short **pFrame, unsigned char codeType, const unsigned char **pData, const unsigned char **pOffData, int *pDataRemain, int *curXb, int *curYb)
 {
-    unsigned short p[4];
-    unsigned char pat[16];
+	array<uint16_t, 4> p;
+	array<uint8_t, 4> pat;
     int i, j, k;
     int x, y;
     unsigned short *pDstBak;

commit 7243e76c80524ebf2c2f3cc45806b185056805b5

Fix gcc-4.6 decoder8 build
Fixes: 8a28f7e1c2ce0c8e1fabd82e29ae9b85e8851d90 ("Use array<> for movie decoder")

d2x-rebirth/libmve/decoder8.cpp
@@ -6,6 +6,7 @@
  */
 /* 8 bit decoding routines */
 
+#include <cstdint>
 #include <stdio.h>
 #include <string.h>
 