dxx-rebirth/dxx-rebirth

commit 1037a92d601f1d0ae745530bf6b972bf8ef97ae9

Allow parallel lto

SConstruct
@@ -1207,6 +1207,7 @@ class DXXCommon(LazyObjectConstructor):
 					('prefix', self._default_prefix, 'installation prefix directory (Linux only)'),
 					('sharepath', self.__default_DATA_DIR, 'directory for shared game data (Linux only)'),
 					('pch', None, 'pre-compile headers used this many times'),
+					('lto', 0, 'enable gcc link time optimization'),
 				),
 			},
 			{
@@ -1215,7 +1216,6 @@ class DXXCommon(LazyObjectConstructor):
 					('check_header_includes', False, 'compile test each header (developer option)'),
 					('debug', False, 'build DEBUG binary which includes asserts, debugging output, cheats and more output'),
 					('memdebug', self.default_memdebug, 'build with malloc tracking'),
-					('lto', False, 'enable gcc link time optimization'),
 					('profiler', False, 'profiler build'),
 					('opengl', True, 'build with OpenGL support'),
 					('opengles', self.default_opengles, 'build with OpenGL ES support'),
@@ -1551,7 +1551,7 @@ class DXXCommon(LazyObjectConstructor):
 		if self.user_settings.LDFLAGS:
 			self.env.Append(LINKFLAGS = SCons.Util.CLVar(self.user_settings.LDFLAGS))
 		if self.user_settings.lto:
-			f = ['-flto', '-fno-fat-lto-objects']
+			f = ['-flto=%s' % self.user_settings.lto, '-fno-fat-lto-objects']
 			self.env.Append(CXXFLAGS = f)
 
 	def check_endian(self):

commit 9f84f0851de47aa0f119f7784e1dcb0d15b2124f

Enable clang LTO
clang does not support -flto=N syntax for parallel LTO.

Remove -fno-fat-lto-objects too.  clang does not understand it;
gcc 4.9 and later default to non-fat LTO.

Fixes: 1037a92d601f1d0ae745530bf6b972bf8ef97ae9 ("Allow parallel lto")

SConstruct
@@ -1703,7 +1703,10 @@ class DXXCommon(LazyObjectConstructor):
 		if self.user_settings.LDFLAGS:
 			self.env.Append(LINKFLAGS = SCons.Util.CLVar(self.user_settings.LDFLAGS))
 		if self.user_settings.lto:
-			f = ['-flto=%s' % self.user_settings.lto, '-fno-fat-lto-objects']
+			f = [
+				# clang does not support =N syntax
+				('-flto=%s' % self.user_settings.lto) if self.user_settings.lto > 1 else '-flto',
+			]
 			self.env.Append(CXXFLAGS = f)
 
 	def check_endian(self):