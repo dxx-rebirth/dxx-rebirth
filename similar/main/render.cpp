dxx-rebirth/dxx-rebirth

commit fa8dcc911372f95b4ce263342ae374f0ddde2d27

Explicitly delete basic_ptridx(pointer_type); require array
Require the array argument for basic_ptridx, which is supplied
automatically when the factory is used.

similar/main/render.cpp
@@ -588,7 +588,7 @@ static void render_object_search(const vobjptridx_t obj)
 
 	if (changed) {
 		if (obj->segnum != segment_none)
-			Cursegp = &Segments[obj->segnum];
+			Cursegp = segptridx(obj->segnum);
 		found_seg = segment_none;
 		found_obj = obj;
 	}

common/include/valptridx.h
@@ -500,6 +500,10 @@ public:
 	basic_ptridx(basic_ptridx &&) = default;
 	basic_ptridx &operator=(const basic_ptridx &) = default;
 	basic_ptridx &operator=(basic_ptridx &&) = default;
+	basic_ptridx(std::nullptr_t) = delete;
+	/* Prevent implicit conversion.  Require use of the factory function.
+	 */
+	basic_ptridx(pointer_type p) = delete;
 	template <typename rpolicy>
 		basic_ptridx(const basic_ptridx<rpolicy> &rhs) :
 			vptr_type(static_cast<const basic_ptr<rpolicy> &>(rhs)),
@@ -513,8 +517,8 @@ public:
 	{
 	}
 	template <integral_type v>
-		basic_ptridx(const magic_constant<v> &m) :
-			vptr_type(m),
+		basic_ptridx(const magic_constant<v> &m, array_managed_type &a = get_array()) :
+			vptr_type(m, a),
 			vidx_type(m)
 	{
 	}
@@ -523,7 +527,7 @@ public:
 		vidx_type(i, a)
 	{
 	}
-	basic_ptridx(pointer_type p, array_managed_type &a = get_array(pointer_type())) :	// default argument deprecated
+	basic_ptridx(pointer_type p, array_managed_type &a) :
 		/* Null pointer is never allowed when an index must be computed.
 		 * Check for null, then use the reference constructor for
 		 * vptr_type to avoid checking again.
@@ -650,12 +654,19 @@ template <typename managed_type>
 template <typename ptridx>
 class valptridx<managed_type>::basic_ptridx_global_factory
 {
+	using containing_type = valptridx<managed_type>;
 public:
 	__attribute_warn_unused_result
 	ptridx operator()(typename ptridx::index_type i) const
 	{
 		return ptridx{i, get_array()};
 	}
+	template <containing_type::integral_type v>
+		__attribute_warn_unused_result
+		ptridx operator()(const containing_type::magic_constant<v> &m) const
+		{
+			return ptridx(m);
+		}
 	template <typename T>
 		ptridx operator()(T) const = delete;
 	void *operator &() const = delete;

similar/editor/curves.cpp
@@ -44,8 +44,8 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "compiler-range_for.h"
 #include "partial_range.h"
 
-static segment *OriginalSeg;
-static segment *OriginalMarkedSeg;
+static segptridx_t OriginalSeg = segment_none;
+static segptridx_t OriginalMarkedSeg = segment_none;
 static int OriginalSide;
 static int OriginalMarkedSide;
 static array<segment *, MAX_SEGMENTS> CurveSegs;
@@ -368,7 +368,7 @@ void delete_curve() {
 	range_for (auto &i, partial_range(CurveSegs, CurveNumSegs))
 	{
         if (i->segnum != segment_none)
-            med_delete_segment(i);
+			med_delete_segment(vsegptridx(i));
     }
     Markedsegp = OriginalMarkedSeg;
     Markedside = OriginalMarkedSide;

similar/editor/eobject.cpp
@@ -391,7 +391,7 @@ int ObjectSelectNextInMine()
 		if (Cur_object_index>= MAX_OBJECTS ) Cur_object_index= 0;
 
 		if ((Objects[Cur_object_index ].type != OBJ_NONE) && (Cur_object_index != (ConsoleObject-Objects)) )	{
-			Cursegp = &Segments[Objects[Cur_object_index ].segnum];
+			Cursegp = segptridx(Objects[Cur_object_index].segnum);
 			med_create_new_segment_from_cursegp();
 			//Cur_object_seg = Cursegp;
 			return 1;
@@ -412,7 +412,7 @@ int ObjectSelectPrevInMine()
 			Cur_object_index = MAX_OBJECTS-1;
 
 		if ((Objects[Cur_object_index ].type != OBJ_NONE) && (Cur_object_index != (ConsoleObject-Objects)) )	{
-			Cursegp = &Segments[Objects[Cur_object_index ].segnum];
+			Cursegp = segptridx(Objects[Cur_object_index].segnum);
 			med_create_new_segment_from_cursegp();
 			//Cur_object_seg = Cursegp;
 			return 1;
@@ -650,7 +650,7 @@ int	ObjectMoveDown(void)
 		return 1;
 	}
 
-	auto obj = &Objects[Cur_object_index];
+	const auto &&obj = vobjptridx(Cur_object_index);
 
 	extract_up_vector_from_segment(&Segments[obj->segnum], uvec);
 	vm_vec_normalize(uvec);

similar/editor/group.cpp
@@ -405,7 +405,6 @@ static void med_rotate_group(const vms_matrix &rotmat, group::segment_array_type
 // ------------------------------------------------------------------------------------------------
 static void cgl_aux(const vsegptridx_t segp, group::segment_array_type_t &seglistp, selected_segment_array_t *ignore_list, visited_segment_bitarray_t &visited)
 {
-	int	side;
 	if (ignore_list)
 		if (ignore_list->contains(segp))
 			return;
@@ -414,9 +413,9 @@ static void cgl_aux(const vsegptridx_t segp, group::segment_array_type_t &seglis
 		visited[segp] = true;
 		seglistp.emplace_back(segp);
 
-		for (side=0; side<MAX_SIDES_PER_SEGMENT; side++)
-			if (IS_CHILD(segp->children[side]))
-				cgl_aux(&Segments[segp->children[side]], seglistp, ignore_list, visited);
+		range_for (const auto c, segp->children)
+			if (IS_CHILD(c))
+				cgl_aux(vsegptridx(c), seglistp, ignore_list, visited);
 	}
 }
 
@@ -592,7 +591,7 @@ static int med_copy_group(int delta_flag, const vsegptridx_t base_seg, int base_
 	// Breaking connections between segments in the current group and segments not in the group.
 	range_for(const auto &gs, GroupList[new_current_group].segments)
 	{
-		auto segp = &Segments[gs];
+		const auto &&segp = vsegptridx(gs);
 		for (c=0; c < MAX_SIDES_PER_SEGMENT; c++) 
 			if (IS_CHILD(segp->children[c])) {
 				if (!in_group(segp->children[c], new_current_group)) {
@@ -641,7 +640,7 @@ static int med_copy_group(int delta_flag, const vsegptridx_t base_seg, int base_
 	current_group = new_current_group;
 
 	//	Now, form joint on connecting sides.
-	med_form_joint(base_seg,base_side,Groupsegp[current_group],Groupside[new_current_group]);
+	med_form_joint(base_seg,base_side,vsegptridx(Groupsegp[current_group]),Groupside[new_current_group]);
 
 	validate_selected_segments();
 	med_combine_duplicate_vertices(in_vertex_list);
@@ -728,17 +727,17 @@ static int med_move_group(int delta_flag, const vsegptridx_t base_seg, int base_
 	// Breaking connections between segments in the group and segments not in the group.
 	range_for(const auto &gs, GroupList[current_group].segments)
 		{
-		auto segp = &Segments[gs];
+		const auto &&segp = vsegptridx(gs);
 		for (c=0; c < MAX_SIDES_PER_SEGMENT; c++) 
 			if (IS_CHILD(segp->children[c]))
 				{
-				auto csegp = &Segments[segp->children[c]];
+				const auto &&csegp = vsegptridx(segp->children[c]);
 				if (csegp->group != current_group)
 					{
 					for (d=0; d<MAX_SIDES_PER_SEGMENT; d++)
 						if (IS_CHILD(csegp->children[d]))
 							{
-							auto dsegp = &Segments[csegp->children[d]];
+							const auto &&dsegp = vsegptr(csegp->children[d]);
 							if (dsegp->group == current_group)
 								{
 								csegp->children[d] = segment_none;
@@ -815,19 +814,20 @@ static segnum_t place_new_segment_in_world(void)
 //	Attach segment in the new-fangled way, which is by using the CopyGroup code.
 static int AttachSegmentNewAng(const vms_angvec &pbh)
 {
-	int			newseg;
 	GroupList[current_group].segments.clear();
-	newseg = place_new_segment_in_world();
+	const auto newseg = place_new_segment_in_world();
 	GroupList[current_group].segments.emplace_back(newseg);
 
-	if (!med_move_group(1, Cursegp, Curside, &Segments[newseg], AttachSide, vm_angles_2_matrix(pbh),0)) {
+	const auto &&nsegp = vsegptridx(newseg);
+	if (!med_move_group(1, Cursegp, Curside, nsegp, AttachSide, vm_angles_2_matrix(pbh),0))
+	{
 		autosave_mine(mine_filename);
 
-		med_propagate_tmaps_to_segments(Cursegp,&Segments[newseg],0);
-		med_propagate_tmaps_to_back_side(&Segments[newseg], Side_opposite[AttachSide],0);
-		copy_uvs_seg_to_seg(&New_segment,&Segments[newseg]);
+		med_propagate_tmaps_to_segments(Cursegp,nsegp,0);
+		med_propagate_tmaps_to_back_side(nsegp, Side_opposite[AttachSide],0);
+		copy_uvs_seg_to_seg(&New_segment,nsegp);
 
-		Cursegp = &Segments[newseg];
+		Cursegp = nsegp;
 		Curside = Side_opposite[AttachSide];
 		med_create_new_segment_from_cursegp();
 
@@ -859,7 +859,7 @@ int AttachSegmentNew(void)
 void validate_selected_segments(void)
 {
 	range_for (const auto &gs, GroupList[current_group].segments)
-		validate_segment(&Segments[gs]);
+		validate_segment(vsegptridx(gs));
 }
 
 // =====================================================================================
@@ -933,11 +933,13 @@ int rotate_segment_new(const vms_angvec &pbh)
 	Segments[baseseg].children[baseseg_side] = segment_none;
 	newseg->children[newseg_side] = segment_none;
 
-	if (!med_move_group(1, &Segments[baseseg], baseseg_side, newseg, newseg_side, orient_matrix, 0)) {
+	const auto &&basesegp = vsegptridx(baseseg);
+	if (!med_move_group(1, basesegp, baseseg_side, newseg, newseg_side, orient_matrix, 0))
+	{
 		Cursegp = newseg;
 		med_create_new_segment_from_cursegp();
 //		validate_selected_segments();
-		med_propagate_tmaps_to_segments(&Segments[baseseg], newseg, 1);
+		med_propagate_tmaps_to_segments(basesegp, newseg, 1);
 		med_propagate_tmaps_to_back_side(newseg, Curside, 1);
 	}
 
@@ -1236,10 +1238,11 @@ static int med_load_group( const char *filename, group::vertex_array_type_t &ver
 				
 			group::segment_array_type_t::value_type s = get_free_segment_number();
 			segment_ids.emplace_back(s);
-			Segments[s] = tseg; 
-			Segments[s].objects = object_none;
+			const auto &&segp = vsegptridx(s);
+			*segp = tseg; 
+			segp->objects = object_none;
 
-			fuelcen_activate( &Segments[s], Segments[s].special );
+			fuelcen_activate(segp, segp->special);
 			}
 
 		range_for (const auto &gs, segment_ids)
@@ -1434,7 +1437,8 @@ int LoadGroup()
       checkforgrpext(group_filename);
 	  med_load_group(group_filename, GroupList[current_group].vertices, GroupList[current_group].segments);
 		
-	if (!med_move_group(0, Cursegp, Curside, Groupsegp[current_group], Groupside[current_group], vmd_identity_matrix, 0)) {
+	if (!med_move_group(0, Cursegp, Curside, vsegptridx(Groupsegp[current_group]), Groupside[current_group], vmd_identity_matrix, 0))
+	{
 		autosave_mine(mine_filename);
 		set_view_target_from_segment(Cursegp);
 		Update_flags |= UF_WORLD_CHANGED;
@@ -1560,7 +1564,8 @@ int MoveGroup(void)
 
 	med_compress_mine();
 
-	if (!med_move_group(0, Cursegp, Curside, Groupsegp[current_group], Groupside[current_group], vmd_identity_matrix, 0)) {
+	if (!med_move_group(0, Cursegp, Curside, vsegptridx(Groupsegp[current_group]), Groupside[current_group], vmd_identity_matrix, 0))
+	{
 		autosave_mine(mine_filename);
 		Update_flags |= UF_WORLD_CHANGED;
 		mine_changed = 1;
@@ -1619,7 +1624,7 @@ int RotateGroup(void)
 
 	med_compress_mine();
 	
-	if (!med_move_group(0, Cursegp, Curside, Groupsegp[current_group], Groupside[current_group],
+	if (!med_move_group(0, Cursegp, Curside, vsegptridx(Groupsegp[current_group]), Groupside[current_group],
 								vmd_identity_matrix, Group_orientation[current_group]))
 			{
 			Update_flags |= UF_WORLD_CHANGED;
@@ -1747,8 +1752,9 @@ int DeleteGroup( void )
 
 	range_for (const auto &gs, GroupList[current_group].segments)
 	{
-		Segments[gs].group = -1;
-		med_delete_segment(&Segments[gs]);
+		const auto &&segp = vsegptridx(gs);
+		segp->group = -1;
+		med_delete_segment(segp);
 	}
 
 	for (i=current_group;i<num_groups-1;i++) {

similar/editor/ksegsize.cpp
@@ -232,7 +232,7 @@ static int segsize_common(int dimension, fix amount)
 			for (s=0; s<MAX_SIDES_PER_SEGMENT; s++)
 				propagated[s]++;
                         propagated[(int) Side_opposite[i]]--;
-			med_propagate_tmaps_to_segments(&Segments[Cursegp->children[i]],Cursegp,1);
+			med_propagate_tmaps_to_segments(vsegptridx(Cursegp->children[i]),Cursegp,1);
 		}
 
 	//	Now, for all sides that were not adjacent to another side, and therefore did not get tmaps

similar/editor/med.cpp
@@ -344,7 +344,7 @@ void init_editor()
 	Draw_all_segments = 1;						// Say draw all segments, not just connected ones
 	
 	if (!Cursegp)
-		Cursegp = &Segments[0];
+		Cursegp = segptridx(segment_first);
 
 	init_autosave();
   
@@ -1018,7 +1018,7 @@ int editor_handler(UI_DIALOG *, const d_event &event, unused_ui_userdata_t *)
 					Update_flags |= UF_GAME_VIEW_CHANGED;
 					if (Gameview_lockstep)
 					{
-						Cursegp = &Segments[ConsoleObject->segnum];
+						Cursegp = segptridx(ConsoleObject->segnum);
 						med_create_new_segment_from_cursegp();
 						Update_flags |= UF_ED_STATE_CHANGED;
 					}
@@ -1177,7 +1177,7 @@ int editor_handler(UI_DIALOG *, const d_event &event, unused_ui_userdata_t *)
 	
 		if (Found_segs.count()) {
 			sort_seg_list(Found_segs,ConsoleObject->pos);
-			Cursegp = &Segments[Found_segs[0]];
+			Cursegp = segptridx(Found_segs[0]);
 			med_create_new_segment_from_cursegp();
 			if (Lock_view_to_cursegp)
 				set_view_target_from_segment(Cursegp);
@@ -1233,7 +1233,7 @@ int editor_handler(UI_DIALOG *, const d_event &event, unused_ui_userdata_t *)
 
 				//	See if either shift key is down and, if so, assign texture map
 				if (keyd_pressed[KEY_LSHIFT] || keyd_pressed[KEY_RSHIFT]) {
-					Cursegp = &Segments[seg];
+					Cursegp = segptridx(seg);
 					Curside = side;
 					AssignTexture();
 					med_create_new_segment_from_cursegp();
@@ -1245,7 +1245,7 @@ int editor_handler(UI_DIALOG *, const d_event &event, unused_ui_userdata_t *)
 				} else if (keyd_pressed[ KEY_LAPOSTRO] ) {
 					move_object_to_mouse_click();
 				} else {
-					Cursegp = &Segments[seg];
+					Cursegp = segptridx(seg);
 					Curside = side;
 					med_create_new_segment_from_cursegp();
 					editor_status("Curseg and curside selected");

similar/editor/meddraw.cpp
@@ -567,17 +567,21 @@ static void draw_mine_all(int automap_flag)
 	n_used = 0;
 
 	range_for (const auto s, highest_valid(Segments))
-		if (Segments[s].segnum != segment_none) {
+	{
+		const auto &&segp = vsegptridx(s);
+		if (segp->segnum != segment_none)
+		{
 			for (i=0; i<MAX_SIDES_PER_SEGMENT; i++)
-				if (Segments[s].sides[i].wall_num != wall_none)
-					draw_special_wall(&Segments[s], i);
+				if (segp->sides[i].wall_num != wall_none)
+					draw_special_wall(segp, i);
 			if (Search_mode)
-				check_segment(&Segments[s]);
+				check_segment(segp);
 			else {
-				add_edges(&Segments[s]);
-				draw_seg_objects(&Segments[s]);
+				add_edges(segp);
+				draw_seg_objects(segp);
 			}
 		}
+	}
 
 	draw_mine_edges(automap_flag);
 

commit 1658e05604d64101a87948f0e9e1a7b3bdaab120

Fix SDL-only build for valptridx changes
Fixes: fa8dcc911372f95b4ce263342ae374f0ddde2d27 ("Explicitly delete basic_ptridx(pointer_type); require array")

similar/main/render.cpp
@@ -730,19 +730,17 @@ static void project_list(array<int, 8> &pointnumlist)
 
 // -----------------------------------------------------------------------------------
 #if !defined(OGL)
-static void render_segment(segnum_t segnum)
+static void render_segment(const vcsegptridx_t seg)
 {
-	segment		*seg = &Segments[segnum];
 	int			sn;
 
-	Assert(segnum!=segment_none && segnum<=Highest_segment_index);
 	g3s_codes 	cc=rotate_list(seg->verts);
 	if (! cc.uand) {		//all off screen?
 
 #if defined(DXX_BUILD_DESCENT_II)
       if (Viewer->type!=OBJ_ROBOT)
 #endif
-  	   	Automap_visited[segnum]=1;
+		Automap_visited[seg]=1;
 
 		for (sn=0; sn<MAX_SIDES_PER_SEGMENT; sn++)
 			render_side(seg, sn);
@@ -1586,7 +1584,7 @@ void render_mine(segnum_t start_seg_num,fix eye_offset, window_rendered_data &wi
 				Window_clip_bot   = rw.bot;
 			}
 
-			render_segment(segnum);
+			render_segment(vcsegptridx(segnum));
 			visited[segnum]=3;
 			if (srsm.objects.empty())
 				continue;