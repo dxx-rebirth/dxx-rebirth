dxx-rebirth/dxx-rebirth

commit 0df3209fad14198d19772deff0a38bd5348443e9

Remove unused energy center capacity tracking

similar/main/fuelcen.cpp
@@ -530,21 +530,10 @@ static void robotmaker_proc( FuelCenter * robotcen )
 // Called once per frame, replenishes fuel supply.
 void fuelcen_update_all()
 {
-	fix AmountToreplenish;
-	AmountToreplenish = fixmul(FrameTime,Fuelcen_refill_speed);
-
 	for (uint_fast32_t i = 0; i < Num_fuelcenters; i++ )	{
 		if ( Station[i].Type == SEGMENT_IS_ROBOTMAKER )	{
 			if (! (Game_suspended & SUSP_ROBOTS))
 				robotmaker_proc( &Station[i] );
-		} else if ( (Station[i].MaxCapacity > 0) && (PlayerSegment!=&Segments[Station[i].segnum]) )	{
-			if ( Station[i].Capacity < Station[i].MaxCapacity )	{
- 				Station[i].Capacity += AmountToreplenish;
-				if ( Station[i].Capacity >= Station[i].MaxCapacity )		{
-					Station[i].Capacity = Station[i].MaxCapacity;
-					//gauge_message( "Fuel center is fully recharged!    " );
-				}
-			}
 		}
 	}
 }

commit 1378ec21766046325449a9cb4b9d9ee60c0e8edb

Remove unused Fuelcen_refill_speed
Fixes: 0df3209fad14198d19772deff0a38bd5348443e9 ("Remove unused energy center capacity tracking")

similar/main/fuelcen.cpp
@@ -63,7 +63,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 // The max number of fuel stations per mine.
 
-static const fix Fuelcen_refill_speed = i2f(1);
 static const fix Fuelcen_give_amount = i2f(25);
 static const fix Fuelcen_max_amount = i2f(100);
 