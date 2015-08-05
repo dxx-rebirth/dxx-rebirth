dxx-rebirth/dxx-rebirth

commit 8b7c5c3e2b4a08f5db27f60dffd503f7d89289c0

Rewrite valptridx
Move decisions out to policy classes.  Provide trivial copy
construction/assignment.  Simplify checking for unsafe conversions.

common/include/fwdvalptridx.h
@@ -1,15 +1,115 @@
 #pragma once
 
-#define _DECLARE_VALPTR_SUBTYPE(prefix)	\
-	struct prefix##ptr_t;	\
-	struct prefix##ptridx_t;	\
-	struct v##prefix##ptr_t;	\
-	struct v##prefix##ptridx_t	\
+#include <cstddef>
+#include "dxxsconf.h"
+#include "objnum.h"
+#include "segnum.h"
 
-#define DECLARE_VALPTR_SUBTYPE(prefix, type)	\
-	struct type;	\
-	_DECLARE_VALPTR_SUBTYPE(prefix);	\
-	_DECLARE_VALPTR_SUBTYPE(c##prefix);	\
+template <typename managed_type>
+class valptridx_specialized_types;
 
-DECLARE_VALPTR_SUBTYPE(obj, object);
-DECLARE_VALPTR_SUBTYPE(seg, segment);
+template <typename managed_type>
+class valptridx :
+	protected valptridx_specialized_types<managed_type>
+{
+	using specialized_types = valptridx_specialized_types<managed_type>;
+	class partial_policy
+	{
+	public:
+		class require_valid;
+		class allow_invalid;
+		class const_policy;
+		class mutable_policy;
+		template <typename policy>
+			class apply_cv_policy;
+	};
+	class vc;	/* require_valid + const_policy */
+	class ic;	/* allow_invalid + const_policy */
+	class vm;	/* require_valid + mutable_policy */
+	class im;	/* allow_invalid + mutable_policy */
+protected:
+	using const_pointer_type = const managed_type *;
+	using const_reference_type = const managed_type &;
+	using mutable_pointer_type = managed_type *;
+	using typename specialized_types::array_managed_type;
+	using typename specialized_types::index_type;
+	using typename specialized_types::integral_type;
+	template <integral_type constant>
+		using magic_constant = typename specialized_types::template magic_constant<constant>;
+
+	template <typename policy>
+		class basic_idx;
+	template <typename policy>
+		class basic_ptr;
+	template <typename policy>
+		class basic_ptridx;
+
+	static constexpr const array_managed_type &get_array(const_pointer_type p)
+	{
+		return get_global_array(p);
+	}
+	static constexpr array_managed_type &get_array(mutable_pointer_type p = mutable_pointer_type())
+	{
+		return get_global_array(p);
+	}
+	static inline void check_index_match(const_reference_type, index_type, const array_managed_type &);
+	static inline index_type check_index_range(index_type, const array_managed_type &);
+	static inline void check_explicit_index_range_ref(const_reference_type, std::size_t, const array_managed_type &);
+	static inline void check_implicit_index_range_ref(const_reference_type, const array_managed_type &);
+	static inline void check_null_pointer(const_pointer_type, const array_managed_type &);
+	static void check_null_pointer(std::nullptr_t, ...) = delete;
+
+#define DXX_VALPTRIDX_SUBTYPE(VERB,managed_type,derived_type_prefix)	\
+	DXX_VALPTRIDX_SUBTYPE_C(VERB, managed_type, derived_type_prefix, c);	\
+	DXX_VALPTRIDX_SUBTYPE_C(VERB, managed_type, derived_type_prefix, )
+
+#define DXX_VALPTRIDX_SUBTYPE_C(VERB,managed_type,derived_type_prefix,cprefix)	\
+	DXX_VALPTRIDX_SUBTYPE_VC(VERB, managed_type, derived_type_prefix, cprefix);	\
+	DXX_VALPTRIDX_SUBTYPE_VC(VERB, managed_type, derived_type_prefix, v##cprefix)
+
+#define DXX_VALPTRIDX_SUBTYPE_VC(VERB,managed_type,derived_type_prefix,vcprefix)	\
+	VERB(managed_type, derived_type_prefix, vcprefix, idx);	\
+	VERB(managed_type, derived_type_prefix, vcprefix, ptr);	\
+	VERB(managed_type, derived_type_prefix, vcprefix, ptridx)
+
+public:
+#define DXX_VALPTRIDX_FWD_DECLARE_SUBTYPE(managed_type,derived_type_prefix,vcprefix,suffix)	class vcprefix##suffix
+	DXX_VALPTRIDX_SUBTYPE(DXX_VALPTRIDX_FWD_DECLARE_SUBTYPE,,);
+#undef DXX_VALPTRIDX_FWD_DECLARE_SUBTYPE
+	class index_mismatch_exception;
+	class index_range_exception;
+	class null_pointer_exception;
+
+	template <typename vptr>
+		class basic_ptr_global_factory;
+	template <typename ptridx>
+		class basic_ptridx_global_factory;
+	template <typename vptridx>
+		class basic_vptridx_global_factory;
+};
+
+#define DXX_VALPTRIDX_DEFINE_SUBTYPE_TYPEDEF(managed_type,derived_type_prefix,vcprefix,suffix)	\
+	typedef valptridx<managed_type>::vcprefix##suffix vcprefix##derived_type_prefix##suffix##_t
+#define DXX_VALPTRIDX_DECLARE_GLOBAL_SUBTYPE(managed_type,derived_type_prefix,global_array)	\
+	struct managed_type;	\
+	struct managed_type##_array_t;	\
+	extern managed_type##_array_t global_array;	\
+	template <>	\
+	class valptridx_specialized_types<managed_type> {	\
+	public:	\
+		using array_managed_type = managed_type##_array_t;	\
+		using index_type = derived_type_prefix##num_t;	\
+		using integral_type = derived_type_prefix##num_t;	\
+		template <integral_type constant>	\
+		using magic_constant = managed_type##_magic_constant_t<constant>;	\
+	};	\
+	DXX_VALPTRIDX_SUBTYPE(DXX_VALPTRIDX_DEFINE_SUBTYPE_TYPEDEF, managed_type, derived_type_prefix)
+
+DXX_VALPTRIDX_DECLARE_GLOBAL_SUBTYPE(object, obj, Objects);
+DXX_VALPTRIDX_DECLARE_GLOBAL_SUBTYPE(segment, seg, Segments);
+
+#undef DXX_VALPTRIDX_DECLARE_GLOBAL_SUBTYPE
+#undef DXX_VALPTRIDX_DEFINE_SUBTYPE_TYPEDEF
+#undef DXX_VALPTRIDX_SUBTYPE_VC
+#undef DXX_VALPTRIDX_SUBTYPE_C
+#undef DXX_VALPTRIDX_SUBTYPE

common/main/segiter.h
@@ -14,8 +14,8 @@ struct unspecified_pointer_t;
 template <typename T>
 class segment_object_iterator_t : public std::iterator<std::forward_iterator_tag, T, std::ptrdiff_t, typename detail::unspecified_pointer_t<T>, T>, T
 {
-	using T::p;
-	using T::i;
+	using T::m_ptr;
+	using T::m_idx;
 public:
 	segment_object_iterator_t(T o) :
 		T(o)
@@ -24,14 +24,14 @@ public:
 	T operator *() { return *static_cast<T *>(this); }
 	segment_object_iterator_t &operator++()
 	{
-		auto ni = p->next;
-		p = (ni == object_none) ? NULL : p + (static_cast<std::size_t>(ni) - static_cast<std::size_t>(i));
-		i = ni;
+		const auto ni = m_ptr->next;
+		m_ptr = (ni == object_none) ? nullptr : m_ptr + (static_cast<std::size_t>(ni) - static_cast<std::size_t>(m_idx));
+		m_idx = ni;
 		return *this;
 	}
 	bool operator==(const segment_object_iterator_t &rhs) const
 	{
-		return i == rhs.i;
+		return m_idx == rhs.m_idx;
 	}
 	bool operator!=(const segment_object_iterator_t &rhs) const
 	{

commit e3dbe7a07ceb1d16832aa5abf28d4c8c646373c7

Add workaround for gcc-4.7 type inheritance quirk
In gcc-4.7, a type inherited via using is distinct from a type defined
by using.

	$ cat t.cpp
	template <typename>
	class A
	{
	public:
	    typedef void a;
	    typedef void b;
	};

	template <typename T>
	class B : protected A<T>
	{
	protected:
	    using typename A<T>::a;
	    using b = typename A<T>::b;
	public:
	    static inline a f1();
	    static inline b f2();
	};

	template <typename T>
	typename B<T>::a B<T>::f1() {}	// fails for gcc-4.7, works in later

	template <typename T>
	typename B<T>::b B<T>::f2() {}	// works in both

	$ gcc-4.7 -Wall -Wextra -std=gnu++0x -O2 -c t.cpp
	t.cpp:21:18: error: prototype for 'typename B<T>::a B<T>::f1()' does not match any in class 'B<T>'
	t.cpp:16:18: error: candidate is: static typename A<T>::a B<T>::f1()
	t.cpp:16:18: warning: inline function 'static typename A<T>::a B<T>::f1() [with T = int; typename A<T>::a = void]' used but never defined [enabled by default]

	$ gcc-4.8 -Wall -Wextra -std=gnu++0x -O2 -c t.cpp
	$

Fixes: 8b7c5c3e2b4a08f5db27f60dffd503f7d89289c0 ("Rewrite valptridx")

common/include/fwdvalptridx.h
@@ -32,7 +32,7 @@ protected:
 	using const_reference_type = const managed_type &;
 	using mutable_pointer_type = managed_type *;
 	using typename specialized_types::array_managed_type;
-	using typename specialized_types::index_type;
+	using index_type = typename specialized_types::index_type;
 	using typename specialized_types::integral_type;
 
 	template <typename policy>