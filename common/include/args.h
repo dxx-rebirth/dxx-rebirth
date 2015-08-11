dxx-rebirth/dxx-rebirth

commit 6bd93e466fc7d77fab915c0553d2c1bda0874a46

Guard args.h for LTO -Wodr

common/include/args.h
@@ -61,6 +61,15 @@ typedef enum {
 #include "pack.h"
 #include "compiler-type_traits.h"
 
+struct CArg : prohibit_void_ptr<CArg>
+{
+	bool CtlNoCursor;
+	bool CtlNoMouse;
+	bool CtlNoStickyKeys;
+	bool DbgForbidConsoleGrab;
+};
+
+#if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
 struct Arg : prohibit_void_ptr<Arg>
 {
 	int SysMaxFPS;
@@ -76,10 +85,7 @@ struct Arg : prohibit_void_ptr<Arg>
 	bool SysWindow;
 	bool SysNoBorders;
 	bool SysAutoDemo;
-	bool CtlNoCursor;
-	bool CtlNoMouse;
 	bool CtlNoJoystick;
-	bool CtlNoStickyKeys;
 	bool SndNoSound;
 	bool SndNoMusic;
 #ifdef USE_SDLMIXER
@@ -88,7 +94,6 @@ struct Arg : prohibit_void_ptr<Arg>
 	static constexpr tt::true_type SndDisableSdlMixer{};
 #endif
 	bool GfxSkipHiresFNT;
-#if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
 #ifdef DXX_BUILD_DESCENT_I
 	bool SysNoTitles;
 	bool EdiNoBm;
@@ -120,7 +125,6 @@ struct Arg : prohibit_void_ptr<Arg>
 	int DbgVerbose;
 	bool DbgSafelog;
 	bool DbgNoRun;
-	bool DbgForbidConsoleGrab;
 	bool DbgRenderStats;
 	std::string DbgAltTex;
 	std::string DbgTexMap;
@@ -138,10 +142,11 @@ struct Arg : prohibit_void_ptr<Arg>
 	int DbgSdlHWSurface;
 	int DbgSdlASyncBlit;
 #endif
-#endif
 };
 
 extern struct Arg GameArg;
+#endif
+extern CArg CGameArg;
 
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
 static inline const char *PLAYER_DIRECTORY_STRING(const char *s, const char *f) __attribute_format_arg(2);

similar/misc/args.cpp
@@ -98,6 +98,7 @@ class nesting_depth_exceeded
 {
 };
 
+CArg CGameArg;
 struct Arg GameArg;
 
 static void ReadCmdArgs(Inilist &ini, Arglist &Args);
@@ -228,13 +229,13 @@ static void ReadCmdArgs(Inilist &ini, Arglist &Args)
 	// Control Options
 
 		else if (!d_stricmp(p, "-nocursor"))
-			GameArg.CtlNoCursor 		= 1;
+			CGameArg.CtlNoCursor 		= true;
 		else if (!d_stricmp(p, "-nomouse"))
-			GameArg.CtlNoMouse 		= 1;
+			CGameArg.CtlNoMouse 		= true;
 		else if (!d_stricmp(p, "-nojoystick"))
 			GameArg.CtlNoJoystick 		= 1;
 		else if (!d_stricmp(p, "-nostickykeys"))
-			GameArg.CtlNoStickyKeys		= 1;
+			CGameArg.CtlNoStickyKeys	= true;
 
 	// Sound Options
 
@@ -330,7 +331,7 @@ static void ReadCmdArgs(Inilist &ini, Arglist &Args)
 			GameArg.DbgVerbose 	= CON_VERBOSE;
 
 		else if (!d_stricmp(p, "-no-grab"))
-			GameArg.DbgForbidConsoleGrab = 1;
+			CGameArg.DbgForbidConsoleGrab = true;
 		else if (!d_stricmp(p, "-safelog"))
 			GameArg.DbgSafelog 		= 1;
 		else if (!d_stricmp(p, "-norun"))
@@ -390,7 +391,7 @@ static void PostProcessGameArg()
 		GameArg.SysMaxFPS = MAXIMUM_FPS;
 
 	static char sdl_disable_lock_keys[] = "SDL_DISABLE_LOCK_KEYS=0";
-	if (GameArg.CtlNoStickyKeys) // Must happen before SDL_Init!
+	if (CGameArg.CtlNoStickyKeys) // Must happen before SDL_Init!
 		sdl_disable_lock_keys[sizeof(sdl_disable_lock_keys) - 1] = '1';
 	SDL_putenv(sdl_disable_lock_keys);
 }

similar/arch/sdl/event.cpp
@@ -169,7 +169,7 @@ void event_process(void)
 template <bool activate_focus>
 static void event_change_focus()
 {
-	SDL_WM_GrabInput(activate_focus && GameCfg.Grabinput && likely(!GameArg.DbgForbidConsoleGrab) ? SDL_GRAB_ON : SDL_GRAB_OFF);
+	SDL_WM_GrabInput(activate_focus && GameCfg.Grabinput && likely(!CGameArg.DbgForbidConsoleGrab) ? SDL_GRAB_ON : SDL_GRAB_OFF);
 	if (activate_focus)
 		mouse_disable_cursor();
 	else

similar/arch/sdl/key.cpp
@@ -483,7 +483,7 @@ void key_flush()
 	//Clear the unicode buffer
 	unicode_frame_buffer = {};
 	keyd_pressed = {};
-	if (unlikely(GameArg.CtlNoStickyKeys))
+	if (unlikely(CGameArg.CtlNoStickyKeys))
 		return;
 	const auto &keystate = SDL_GetKeyState(NULL);
 	restore_sticky_key(keystate, KEY_NUMLOCK);

similar/arch/sdl/mouse.cpp
@@ -101,7 +101,7 @@ static void maybe_send_doubleclick(const fix64 now, const unsigned button)
 
 void mouse_button_handler(SDL_MouseButtonEvent *mbe)
 {
-	if (unlikely(GameArg.CtlNoMouse))
+	if (unlikely(CGameArg.CtlNoMouse))
 		return;
 	// to bad, SDL buttons use a different mapping as descent expects,
 	// this is at least true and tested for the first three buttons 
@@ -149,7 +149,7 @@ void mouse_motion_handler(SDL_MouseMotionEvent *mme)
 {
 	d_event_mouse_moved event;
 	
-	if (GameArg.CtlNoMouse)
+	if (CGameArg.CtlNoMouse)
 		return;
 
 	Mouse.cursor_time = timer_query();
@@ -201,7 +201,7 @@ void mouse_get_delta( int *dx, int *dy, int *dz )
 template <bool noactivate>
 static void mouse_change_cursor()
 {
-	Mouse.cursor_enabled = (!noactivate && !GameArg.CtlNoMouse && !GameArg.CtlNoCursor);
+	Mouse.cursor_enabled = (!noactivate && !CGameArg.CtlNoMouse && !CGameArg.CtlNoCursor);
 	if (!Mouse.cursor_enabled)
 		SDL_ShowCursor(SDL_DISABLE);
 }

similar/main/kconfig.cpp
@@ -1317,7 +1317,7 @@ static void kc_change_key( kc_menu &menu,const d_event &event, kc_mitem &mitem )
 	keycode = event_key_get_raw(event);
 
 	auto e = end(system_keys);
-	if (unlikely(GameArg.CtlNoStickyKeys))
+	if (unlikely(CGameArg.CtlNoStickyKeys))
 		e = std::prev(e, 3);
 	const auto predicate = [keycode](uint8_t k) { return keycode == k; };
 	if (std::any_of(begin(system_keys), e, predicate))

commit fa9f2e626db0079f90c3bfd9d54e13c5001d86ab

Move DbgShowMemInfo to CArg
Members of Arg are not visible to common code, so common/mem/mem.cpp
broke during the -Wodr cleanup.

Reported-by: zicodxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/114>
Fixes: 6bd93e466fc7d77fab915c0553d2c1bda0874a46 ("Guard args.h for LTO -Wodr")

common/include/args.h
@@ -67,6 +67,7 @@ struct CArg : prohibit_void_ptr<CArg>
 	bool CtlNoMouse;
 	bool CtlNoStickyKeys;
 	bool DbgForbidConsoleGrab;
+	bool DbgShowMemInfo;
 };
 
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
@@ -128,7 +129,6 @@ struct Arg : prohibit_void_ptr<Arg>
 	bool DbgRenderStats;
 	std::string DbgAltTex;
 	std::string DbgTexMap;
-	bool DbgShowMemInfo;
 	bool DbgNoDoubleBuffer;
 	bool DbgNoCompressPigBitmap;
 	int DbgBpp;

common/mem/mem.cpp
@@ -313,7 +313,7 @@ void mem_display_blocks()
 		if (Present[i]==1 &&  (!out_of_memory))
 		{
 			numleft++;
-			if (GameArg.DbgShowMemInfo)	{
+			if (CGameArg.DbgShowMemInfo)	{
 				con_printf(CON_CRITICAL, "\nMEM_LEAKAGE: Memory block has not been freed." );
 				PrintInfo( i );
 			}
@@ -375,7 +375,7 @@ void mem_display_blocks()
 		con_printf(CON_CRITICAL, "\nMEM_LEAKAGE: %d bytes of memory have not been freed.", BytesMalloced );
 	}
 
-	if (GameArg.DbgShowMemInfo)	{
+	if (CGameArg.DbgShowMemInfo)	{
 		con_printf(CON_CRITICAL, "\n\nMEMORY USAGE:" );
 		con_printf(CON_CRITICAL, "  %u Kbytes dynamic data", (LargestAddress-SmallestAddress+512)/1024 );
 		con_printf(CON_CRITICAL, "  %u Kbytes code/static data.", (SmallestAddress-(4*1024*1024)+512)/1024 );

similar/misc/args.cpp
@@ -343,7 +343,7 @@ static void ReadCmdArgs(Inilist &ini, Arglist &Args)
 		else if (!d_stricmp(p, "-tmap"))
 			GameArg.DbgTexMap = arg_string(pp, end);
 		else if (!d_stricmp(p, "-showmeminfo"))
-			GameArg.DbgShowMemInfo 		= 1;
+			CGameArg.DbgShowMemInfo 		= 1;
 		else if (!d_stricmp(p, "-nodoublebuffer"))
 			GameArg.DbgNoDoubleBuffer 	= 1;
 		else if (!d_stricmp(p, "-bigpig"))