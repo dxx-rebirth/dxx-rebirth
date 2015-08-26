dxx-rebirth/dxx-rebirth

commit 92814475a99b3104fcc364037cb5e711aa121467

Use array<> for RobotCenters

similar/main/state.cpp
@@ -1208,7 +1208,8 @@ int state_save_all_sub(const char *filename, const char *desc)
 	PHYSFS_write(fp, &Countdown_timer, sizeof(int), 1);
 #endif
 	PHYSFS_write(fp, &Num_robot_centers, sizeof(int), 1);
-	PHYSFS_write(fp, RobotCenters, sizeof(matcen_info), Num_robot_centers);
+	range_for (auto &r, partial_range(RobotCenters, Num_robot_centers))
+		matcen_info_write(&r, 0x7f, fp);
 	control_center_triggers_write(&ControlCenterTriggers, fp);
 	PHYSFS_write(fp, &Num_fuelcenters, sizeof(int), 1);
 #if defined(DXX_BUILD_DESCENT_I)
@@ -1681,7 +1682,8 @@ int state_restore_all_sub(const char *filename, int secret_restore)
 	Countdown_timer = PHYSFSX_readSXE32(fp, swap);
 #endif
 	Num_robot_centers = PHYSFSX_readSXE32(fp, swap);
-	matcen_info_read_n_swap(RobotCenters, Num_robot_centers, swap, fp);
+	range_for (auto &r, partial_range(RobotCenters, Num_robot_centers))
+		matcen_info_read_swap(fp, r, swap);
 	control_center_triggers_read_swap(&ControlCenterTriggers, swap, fp);
 	Num_fuelcenters = PHYSFSX_readSXE32(fp, swap);
 	range_for (auto &s, partial_range(Station, Num_fuelcenters))

common/main/fuelcen.h
@@ -119,8 +119,6 @@ struct FuelCenter
 // The max number of robot centers per mine.
 #define MAX_ROBOT_CENTERS  20
 
-extern int Num_robot_centers;
-
 struct d1_matcen_info
 {
 	int     robot_flags[1];    // Up to 32 different robots
@@ -146,7 +144,8 @@ struct matcen_info
 
 extern const char Special_names[MAX_CENTER_TYPES][11];
 
-extern matcen_info RobotCenters[MAX_ROBOT_CENTERS];
+extern unsigned Num_robot_centers;
+extern array<matcen_info, MAX_ROBOT_CENTERS> RobotCenters;
 extern array<FuelCenter, MAX_NUM_FUELCENS> Station;
 
 static inline long operator-(FuelCenter *s, array<FuelCenter, MAX_NUM_FUELCENS> &a)
@@ -186,9 +185,9 @@ void d1_matcen_info_read(d1_matcen_info *mi, PHYSFS_file *fp);
 /*
  * reads n matcen_info structs from a PHYSFS_file and swaps if specified
  */
-void matcen_info_read_n_swap(matcen_info *mi, int n, int swap, PHYSFS_file *fp);
+void matcen_info_read_swap(PHYSFS_file *fp, matcen_info &mi, int swap);
 
-void matcen_info_write(matcen_info *mi, short version, PHYSFS_file *fp);
+void matcen_info_write(const matcen_info *mi, short version, PHYSFS_file *fp);
 #endif
 
 void fuelcen_read(PHYSFS_file *fp, FuelCenter &fc);

similar/main/fuelcen.cpp
@@ -72,8 +72,8 @@ const fix EnergyToCreateOneRobot = i2f(1);
 #define MATCEN_HP_DEFAULT			F1_0*500; // Hitpoints
 #define MATCEN_INTERVAL_DEFAULT	F1_0*5;	//  5 seconds
 
-matcen_info RobotCenters[MAX_ROBOT_CENTERS];
-int Num_robot_centers;
+unsigned Num_robot_centers;
+array<matcen_info, MAX_ROBOT_CENTERS> RobotCenters;
 
 array<FuelCenter, MAX_NUM_FUELCENS> Station;
 unsigned Num_fuelcenters;
@@ -824,18 +824,13 @@ static void matcen_info_swap(matcen_info *mi, int swap)
 /*
  * reads n matcen_info structs from a PHYSFS_file and swaps if specified
  */
-void matcen_info_read_n_swap(matcen_info *mi, int n, int swap, PHYSFS_file *fp)
+void matcen_info_read_swap(PHYSFS_file *fp, matcen_info &mi, int swap)
 {
-	int i;
-	
-	PHYSFS_read(fp, mi, sizeof(*mi), n);
-	
-	if (swap)
-		for (i = 0; i < n; i++)
-			matcen_info_swap(&mi[i], swap);
+	PHYSFS_read(fp, &mi, sizeof(mi), 1);
+	matcen_info_swap(&mi, swap);
 }
 
-void matcen_info_write(matcen_info *mi, short version, PHYSFS_file *fp)
+void matcen_info_write(const matcen_info *mi, short version, PHYSFS_file *fp)
 {
 	PHYSFS_writeSLE32(fp, mi->robot_flags[0]);
 	if (version >= 27)

similar/main/gamesave.cpp
@@ -1720,8 +1720,8 @@ static int save_game_data(PHYSFS_file *SaveFile)
 	//================ SAVE MATERIALIZATION CENTER TRIGGER INFO ===============
 
 	matcen_offset = PHYSFS_tell(SaveFile);
-	for (i = 0; i < Num_robot_centers; i++)
-		matcen_info_write(&RobotCenters[i], game_top_fileinfo_version, SaveFile);
+	range_for (auto &r, partial_range(RobotCenters, Num_robot_centers))
+		matcen_info_write(SaveFile, r, game_top_fileinfo_version);
 
 	//================ SAVE DELTA LIGHT INFO ===============
 #if defined(DXX_BUILD_DESCENT_II)

commit 4434559e8ed56c986b4d069dd71810d629e0c4aa

Write old matcen_info to D1 savegames
Fixes: 92814475a99b3104fcc364037cb5e711aa121467 ("Use array<> for RobotCenters")

similar/main/state.cpp
@@ -1093,7 +1093,11 @@ int state_save_all_sub(const char *filename, const char *desc)
 #endif
 	PHYSFS_write(fp, &Num_robot_centers, sizeof(int), 1);
 	range_for (auto &r, partial_range(RobotCenters, Num_robot_centers))
+#if defined(DXX_BUILD_DESCENT_I)
+		matcen_info_write(fp, r, STATE_VERSION);
+#elif defined(DXX_BUILD_DESCENT_II)
 		matcen_info_write(fp, r, 0x7f);
+#endif
 	control_center_triggers_write(&ControlCenterTriggers, fp);
 	PHYSFS_write(fp, &Num_fuelcenters, sizeof(int), 1);
 	range_for (auto &s, partial_range(Station, Num_fuelcenters))