dxx-rebirth/dxx-rebirth

commit c4a84320817b9e02f86f3ec2dd2678e3367cddcc

Remove WORDS_NEED_ALIGNMENT memcpy src cast to const uint8_t*
The cast provokes a warning on systems that require alignment, so remove
it.  memcpy takes const void* input, so the cast is unnecessary.

Reported-by: MattWatt <https://github.com/dxx-rebirth/dxx-rebirth/issues/119>

similar/3d/interp.cpp
@@ -20,7 +20,6 @@
 #include "common/3d/globvars.h"
 #include "polyobj.h"
 #include "gr.h"
-#include "byteutil.h"
 #include "u_mem.h"
 
 static const unsigned OP_EOF = 0;   //eof

common/include/byteutil.h
@@ -75,7 +75,7 @@ const int words_bigendian = 1;
 #ifndef WORDS_NEED_ALIGNMENT
 #define byteutil_unaligned_copy(dt, d, s)	(static_cast<dt &>(d) = *reinterpret_cast<const dt *>(s))
 #else // ! WORDS_NEED_ALIGNMENT
-#define byteutil_unaligned_copy(dt, d, s)	memcpy(&static_cast<dt &>(d), reinterpret_cast<const uint8_t *>(s), sizeof(d))
+#define byteutil_unaligned_copy(dt, d, s)	memcpy(&static_cast<dt &>(d), (s), sizeof(d))
 #endif // ! WORDS_NEED_ALIGNMENT
 
 template <typename T>

d2x-rebirth/main/movie.cpp
@@ -48,7 +48,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "strutil.h"
 #include "dxxerror.h"
 #include "u_mem.h"
-#include "byteutil.h"
 #include "gr.h"
 #include "gamefont.h"
 #include "menu.h"

similar/2d/font.cpp
@@ -37,7 +37,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "gr.h"
 #include "grdef.h"
 #include "dxxerror.h"
-#include "byteutil.h"
 #include "common/2d/bitmap.h"
 #include "makesig.h"
 #include "physfsx.h"

similar/main/cntrlcen.cpp
@@ -48,7 +48,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "fireball.h"
 #include "endlevel.h"
 #include "state.h"
-#include "byteutil.h"
 #include "args.h"
 
 #include "compiler-range_for.h"

similar/main/fuelcen.cpp
@@ -53,7 +53,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "multi.h"
 #include "multibot.h"
 #include "escort.h"
-#include "byteutil.h"
 #include "poison.h"
 
 #include "compiler-range_for.h"

similar/main/gamemine.cpp
@@ -50,7 +50,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "hash.h"
 #include "key.h"
 #include "piggy.h"
-#include "byteutil.h"
 #include "gamesave.h"
 #include "poison.h"
 #include "compiler-range_for.h"

similar/main/gamesave.cpp
@@ -67,7 +67,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "gamepal.h"
 #include "physics.h"
 #include "laser.h"
-#include "byteutil.h"
 #include "multi.h"
 #include "makesig.h"
 #include "textures.h"

commit 85691b37844d2935eefb5a683b83114b2c14e8e5

Restore inclusion of byteutil.h in similiar/3d/interp.cpp
4a84320817b9e02f86f3ec2dd2678e3367cddcc removed lots of inclusions of
byteutil.h in various source files. However, when building with
WORDS_NEED_ALIGNMENT, interp.cpp makes use of the INTEL_SHORT and
GET_INTEL_SHORT macros defined there.

Fixes: c4a84320817b9e02f86f3ec2dd2678e3367cddcc  ("Remove WORDS_NEED_ALIGNMENT memcpy src cast to const uint8_t*")

similar/3d/interp.cpp
@@ -20,6 +20,7 @@
 #include "common/3d/globvars.h"
 #include "polyobj.h"
 #include "gr.h"
+#include "byteutil.h"
 #include "u_mem.h"
 
 static const unsigned OP_EOF = 0;   //eof