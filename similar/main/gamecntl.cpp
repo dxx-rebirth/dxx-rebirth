dxx-rebirth/dxx-rebirth

commit 29aa820dc0a2245158a52369634a0a7490d31997

Only respawn when the player presses fire
Only respawn when the player presses a fire key/button (primary,
secondary, or flare).  Only clear primary, second, flare, and bomb when
respawning.  This allows dead players to use the automap or change their
camera views.

This is a necessary step to flushing only the firing inputs on respawn,
as requested by Mako88 in
<https://github.com/dxx-rebirth/dxx-rebirth/issues/104>.  Currently,
inputs are flushed again by the activation of the game window, so
respawn still flushes all inputs.

	#1  0x00005555555cee19 in game_flush_inputs () at similar/main/game.cpp:374
	#2  game_handler (event=...) at similar/main/game.cpp:1114
	#3  0x0000555555576113 in window_send_event (wind=..., event=...) at common/arch/sdl/window.cpp:208
	#4  0x000055555557627b in WINDOW_SEND_EVENT (file=0x5555556dc126 "common/arch/sdl/window.cpp", e=0x5555556dc141 "EVENT_WINDOW_ACTIVATED", line=179, event=..., w=...) at common/include/window.h:111
	#5  window_set_visible (w=..., visible=visible@entry=1) at common/arch/sdl/window.cpp:179
	#6  0x00005555555e90f5 in window_set_visible (visible=1, wind=<optimized out>) at common/include/window.h:90
	#7  DoPlayerDead () at similar/main/gameseq.cpp:1479
	#8  0x000055555563ed65 in dead_player_frame () at similar/main/object.cpp:1486
	#9  0x00005555555cf4df in GameProcessFrame () at similar/main/game.cpp:1329
	#10 game_handler (event=...) at similar/main/game.cpp:1157

similar/main/gamecntl.cpp
@@ -422,19 +422,17 @@ static int HandleDeathInput(const d_event &event)
 	{
 		int key = event_key_get(event);
 
-		if (Player_exploded && !key_isfunc(key) && key != KEY_PAUSE && key)
-			Death_sequence_aborted  = 1;		//Any key but func or modifier aborts
 		if (key == KEY_ESC)
 			if (ConsoleObject->flags & OF_EXPLODING)
 				Death_sequence_aborted = 1;
 	}
 
-	if (Player_exploded && (event.type == EVENT_JOYSTICK_BUTTON_UP || event.type == EVENT_MOUSE_BUTTON_UP))
+	if (Player_exploded && (Controls.state.fire_primary || Controls.state.fire_secondary || Controls.state.fire_flare))
 		Death_sequence_aborted = 1;
 
 	if (Death_sequence_aborted)
 	{
-		game_flush_inputs();
+		game_flush_respawn_inputs();
 		return 1;
 	}
 
@@ -1926,10 +1924,11 @@ window_event_result ReadControls(const d_event &event)
 			return window_event_result::handled;
 	}
 
-	if (!Endlevel_sequence && !Player_is_dead && (Newdemo_state != ND_STATE_PLAYBACK))
+	if (!Endlevel_sequence && Newdemo_state != ND_STATE_PLAYBACK)
 	{
-
 		kconfig_read_controls(event, 0);
+		if (Player_is_dead && HandleDeathInput(event))
+			return window_event_result::handled;
 
 		check_rear_view();
 

common/main/game.h
@@ -162,6 +162,7 @@ extern int draw_gauges_on;
 extern void init_game_screen(void);
 
 extern void game_flush_inputs();    // clear all inputs
+void game_flush_respawn_inputs();
 
 extern int Playing_game;    // True if playing game
 extern int Auto_flythrough; // if set, start flythough automatically

common/main/kconfig.h
@@ -50,7 +50,13 @@ struct control_info {
 			key_bank_left,
 			key_bank_right;
 	};
-	struct state_controls_t : public ramp_controls_t<ubyte>
+	struct fire_controls_t
+	{
+		uint8_t fire_primary, fire_secondary, fire_flare, drop_bomb;
+	};
+	struct state_controls_t :
+		public fire_controls_t,
+		public ramp_controls_t<ubyte>
 	{
 		ubyte btn_slide_left, btn_slide_right,
 			btn_slide_up, btn_slide_down,
@@ -59,7 +65,6 @@ struct control_info {
 			accelerate, reverse,
 			cruise_plus, cruise_minus, cruise_off,
 			rear_view,
-			fire_primary, fire_secondary, fire_flare, drop_bomb,
 			automap,
 			cycle_primary, cycle_secondary, select_weapon;
 #if defined(DXX_BUILD_DESCENT_II)

similar/main/game.cpp
@@ -361,15 +361,24 @@ void start_time()
 	}
 }
 
-void game_flush_inputs()
+static void game_flush_common_inputs()
 {
-	int dx,dy,dz;
 	event_flush();
 	key_flush();
 	joy_flush();
 	mouse_flush();
-	mouse_get_delta( &dx, &dy, &dz );	// Read mouse
+}
+
+void game_flush_inputs()
+{
 	Controls = {};
+	game_flush_common_inputs();
+}
+
+void game_flush_respawn_inputs()
+{
+	static_cast<control_info::fire_controls_t &>(Controls.state) = {};
+	game_flush_common_inputs();
 }
 
 /*

similar/main/hud.cpp
@@ -255,6 +255,6 @@ void player_dead_message(void)
 		if (HUD_color == -1)
 			HUD_color = BM_XRGB(0,28,0);
 		gr_set_fontcolor( HUD_color, -1);
-		gr_string(0x8000, GHEIGHT-LINE_SPACING, TXT_PRESS_ANY_KEY);
+		gr_string(0x8000, GHEIGHT - LINE_SPACING, "Press fire key or button to continue...");
 	}
 }

commit bf52ebf4cd38cbfb4a01da44c16dcb8e2bfd7448

Skip do_weapon_n_item_stuff if Player_is_dead
Fixes: 29aa820dc0a2245158a52369634a0a7490d31997 ("Only respawn when the player presses fire")

similar/main/gamecntl.cpp
@@ -1926,6 +1926,7 @@ window_event_result ReadControls(const d_event &event)
 	if (!Endlevel_sequence && Newdemo_state != ND_STATE_PLAYBACK)
 	{
 		kconfig_read_controls(event, 0);
+		const auto Player_is_dead = ::Player_is_dead;
 		if (Player_is_dead && HandleDeathInput(event))
 			return window_event_result::handled;
 
@@ -1935,12 +1936,14 @@ window_event_result ReadControls(const d_event &event)
 		if ( Controls.state.automap )
 		{
 			Controls.state.automap = 0;
-			if (!((Game_mode & GM_MULTI) && Control_center_destroyed && (Countdown_seconds_left < 10)))
+			if (Player_is_dead || (!((Game_mode & GM_MULTI) && Control_center_destroyed && (Countdown_seconds_left < 10))))
 			{
 				do_automap();
 				return window_event_result::handled;
 			}
 		}
+		if (Player_is_dead)
+			return window_event_result::ignored;
 		do_weapon_n_item_stuff();
 	}
 	return window_event_result::ignored;