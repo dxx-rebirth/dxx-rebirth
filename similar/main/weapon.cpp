dxx-rebirth/dxx-rebirth

commit e130ea7b365b8141d0e5a33fb1bffac2e2d255ea

Remove unnecessary secondary_weapon_flags

similar/main/weapon.cpp
@@ -229,9 +229,6 @@ has_weapon_result player_has_secondary_weapon(int weapon_num)
 	int	return_value = 0;
 	const auto weapon_index = Secondary_weapon_to_weapon_info[weapon_num];
 
-		if (get_local_player().secondary_weapon_flags & HAS_SECONDARY_FLAG(weapon_num))
-			return_value |= has_weapon_result::has_weapon_flag;
-
 		if (Weapon_info[weapon_index].ammo_usage <= get_local_player().secondary_ammo[weapon_num])
 			return_value |= has_weapon_result::has_ammo_flag;
 
@@ -710,7 +707,6 @@ int pick_up_secondary(int weapon_index,int count)
 		return 0;
 	}
 
-	get_local_player().secondary_weapon_flags |= (1<<weapon_index);
 	get_local_player().secondary_ammo[weapon_index] += count;
 
 	num_picked_up = count;
@@ -1451,7 +1447,6 @@ void DropSecondaryWeapon ()
 
 	if (get_local_player().secondary_ammo[Secondary_weapon]==0)
 	{
-		get_local_player().secondary_weapon_flags &= (~(1<<Secondary_weapon));
 		auto_select_secondary_weapon();
 	}
 }

common/main/multi.h
@@ -93,7 +93,7 @@ extern int multi_protocol; // set and determinate used protocol
 #define MULTI_PROTO_UDP 1 // UDP protocol
 
 // What version of the multiplayer protocol is this? Increment each time something drastic changes in Multiplayer without the version number changes. Reset to 0 each time the version of the game changes
-#define MULTI_PROTO_VERSION	static_cast<uint16_t>(23)
+#define MULTI_PROTO_VERSION	static_cast<uint16_t>(24)
 // PROTOCOL VARIABLES AND DEFINES - END
 
 // limits for Packets (i.e. positional updates) per sec

common/main/player.h
@@ -63,10 +63,8 @@ struct player : public prohibit_void_ptr<player>
 	objnum_t   killer_objnum;          // Who killed me.... (-1 if no one)
 #if defined(DXX_BUILD_DESCENT_I)
 	ubyte		primary_weapon_flags;					//	bit set indicates the player has this weapon.
-	ubyte		secondary_weapon_flags;					//	bit set indicates the player has this weapon.
 #elif defined(DXX_BUILD_DESCENT_II)
 	ushort  primary_weapon_flags;   // bit set indicates the player has this weapon.
-	ushort  secondary_weapon_flags; // bit set indicates the player has this weapon.
 #endif
 	ushort  vulcan_ammo;
 	array<uint8_t, MAX_SECONDARY_WEAPONS>  secondary_ammo; // How much ammo of each type.

d2x-rebirth/main/escort.cpp
@@ -1386,8 +1386,6 @@ static int maybe_steal_flag_item(const vobjptr_t playerobjp, unsigned flagval)
 static int maybe_steal_secondary_weapon(const vobjptr_t playerobjp, int weapon_num)
 {
 	auto &plr = Players[get_player_id(playerobjp)];
-	if (!(plr.secondary_weapon_flags & HAS_SECONDARY_FLAG(weapon_num)))
-		return 0;
 	if (auto &secondary_ammo = plr.secondary_ammo[weapon_num])
 		if (d_rand() < THIEF_PROBABILITY) {
 			if (weapon_index_is_player_bomb(weapon_num))

similar/main/collide.cpp
@@ -1811,15 +1811,12 @@ static objptridx_t maybe_drop_primary_weapon_egg(const vobjptr_t playerobj, int 
 
 static void maybe_drop_secondary_weapon_egg(const vobjptr_t playerobj, int weapon_index, int count)
 {
-	int weapon_flag = HAS_SECONDARY_FLAG(weapon_index);
 	const auto powerup_num = Secondary_weapon_to_powerup[weapon_index];
-	if (Players[get_player_id(playerobj)].secondary_weapon_flags & weapon_flag) {
 		int	max_count;
 
 		max_count = min(count, 3);
 		for (int i=0; i<max_count; i++)
 			call_object_create_egg(playerobj, 1, OBJ_POWERUP, powerup_num);
-	}
 }
 
 static void drop_missile_1_or_4(const vobjptr_t playerobj,int missile_index)

similar/main/gamecntl.cpp
@@ -1474,7 +1474,6 @@ static window_event_result FinalCheats()
 		HUD_init_message_literal(HM_DEFAULT, TXT_WOWIE_ZOWIE);
 
 		get_local_player().primary_weapon_flags |= (HAS_LASER_FLAG | HAS_VULCAN_FLAG | HAS_SPREADFIRE_FLAG);
-		get_local_player().secondary_weapon_flags |= (HAS_CONCUSSION_FLAG | HAS_HOMING_FLAG | HAS_PROXIMITY_BOMB_FLAG);
 
 		get_local_player().vulcan_ammo = VULCAN_AMMO_MAX;
 		for (unsigned i=0; i<3; i++)
@@ -1494,7 +1493,6 @@ static window_event_result FinalCheats()
 		HUD_init_message(HM_DEFAULT, "SUPER %s",TXT_WOWIE_ZOWIE);
 
 		get_local_player().primary_weapon_flags |= (HAS_LASER_FLAG | HAS_VULCAN_FLAG | HAS_SPREADFIRE_FLAG | HAS_PLASMA_FLAG | HAS_FUSION_FLAG);
-		get_local_player().secondary_weapon_flags |= (HAS_CONCUSSION_FLAG | HAS_HOMING_FLAG | HAS_PROXIMITY_BOMB_FLAG | HAS_SMART_FLAG | HAS_MEGA_FLAG);
 
 		get_local_player().vulcan_ammo = VULCAN_AMMO_MAX;
 		for (unsigned i=0; i<MAX_SECONDARY_WEAPONS; i++)
@@ -1522,12 +1520,10 @@ static window_event_result FinalCheats()
 		if (Piggy_hamfile_version < 3) // SHAREWARE
 		{
 			get_local_player().primary_weapon_flags |= (HAS_LASER_FLAG | HAS_VULCAN_FLAG | HAS_SPREADFIRE_FLAG | HAS_PLASMA_FLAG) | (HAS_GAUSS_FLAG | HAS_HELIX_FLAG);
-			get_local_player().secondary_weapon_flags |= (HAS_CONCUSSION_FLAG | HAS_HOMING_FLAG | HAS_PROXIMITY_BOMB_FLAG | HAS_SMART_FLAG) | (HAS_FLASH_FLAG | HAS_GUIDED_FLAG | HAS_SMART_BOMB_FLAG);
 		}
 		else
 		{
 			get_local_player().primary_weapon_flags |= (HAS_LASER_FLAG | HAS_VULCAN_FLAG | HAS_SPREADFIRE_FLAG | HAS_PLASMA_FLAG | HAS_FUSION_FLAG) | (HAS_GAUSS_FLAG | HAS_HELIX_FLAG | HAS_PHOENIX_FLAG | HAS_OMEGA_FLAG);
-			get_local_player().secondary_weapon_flags |= (HAS_CONCUSSION_FLAG | HAS_HOMING_FLAG | HAS_PROXIMITY_BOMB_FLAG | HAS_SMART_FLAG | HAS_MEGA_FLAG) | (HAS_FLASH_FLAG | HAS_GUIDED_FLAG | HAS_SMART_BOMB_FLAG | HAS_MERCURY_FLAG | HAS_EARTHSHAKER_FLAG);
 		}
 
 		get_local_player().vulcan_ammo = VULCAN_AMMO_MAX;

similar/main/gameseq.cpp
@@ -457,7 +457,6 @@ void init_player_stats_new_ship(ubyte pnum)
 		i = 0;
 	Players[pnum].secondary_ammo[0] = 2 + NDL - Difficulty_level;
 	Players[pnum].primary_weapon_flags = HAS_LASER_FLAG;
-	Players[pnum].secondary_weapon_flags = HAS_CONCUSSION_FLAG;
 	Players[pnum].flags &= ~(PLAYER_FLAGS_QUAD_LASERS | PLAYER_FLAGS_CLOAKED | PLAYER_FLAGS_INVULNERABLE);
 #if defined(DXX_BUILD_DESCENT_II)
 	Players[pnum].flags &= ~(PLAYER_FLAGS_AFTERBURNER | PLAYER_FLAGS_MAP_ALL | PLAYER_FLAGS_CONVERTER | PLAYER_FLAGS_AMMO_RACK | PLAYER_FLAGS_HEADLIGHT | PLAYER_FLAGS_HEADLIGHT_ON | PLAYER_FLAGS_FLAG);

similar/main/multi.cpp
@@ -1679,7 +1679,6 @@ static void multi_do_player_deres(const playernum_t pnum, const ubyte *buf)
 #define GET_WEAPON_FLAGS(buf,count)	(count += sizeof(uint16_t), GET_INTEL_SHORT(buf + (count - sizeof(uint16_t))))
 #endif
 	Players[pnum].primary_weapon_flags = GET_WEAPON_FLAGS(buf,count);
-	Players[pnum].secondary_weapon_flags = GET_WEAPON_FLAGS(buf,count);
 	Players[pnum].laser_level = stored_laser_level(buf[count]);                                                 count++;
 	auto &secondary_ammo = Players[pnum].secondary_ammo;
 	secondary_ammo[HOMING_INDEX] = buf[count];                count++;
@@ -2511,7 +2510,6 @@ void multi_send_player_deres(deres_type_t type)
 #define PUT_WEAPON_FLAGS(buf,count,value)	((PUT_INTEL_SHORT(buf+count, value)), count+=sizeof(uint16_t))
 #endif
 	PUT_WEAPON_FLAGS(multibuf, count, get_local_player().primary_weapon_flags);
-	PUT_WEAPON_FLAGS(multibuf, count, get_local_player().secondary_weapon_flags);
 	multibuf[count++] = (char)get_local_player().laser_level;
 
 	multibuf[count++] = (char)get_local_player().secondary_ammo[HOMING_INDEX];

commit a0a5aaef8df93c29703da779ebdc1aa41ce25d77

Fix bogus "You have no" selecting missiles
Fixes: e130ea7b365b8141d0e5a33fb1bffac2e2d255ea ("Remove unnecessary secondary_weapon_flags")

similar/main/weapon.cpp
@@ -229,12 +229,8 @@ has_weapon_result player_has_secondary_weapon(int weapon_num)
 {
 	int	return_value = 0;
 	const auto weapon_index = Secondary_weapon_to_weapon_info[weapon_num];
-
 		if (Weapon_info[weapon_index].ammo_usage <= get_local_player_secondary_ammo()[weapon_num])
-			return_value |= has_weapon_result::has_ammo_flag;
-
-		if (Weapon_info[weapon_index].energy_usage <= get_local_player_energy())
-			return_value |= has_weapon_result::has_energy_flag;
+		return_value = has_weapon_result::has_weapon_flag | has_weapon_result::has_energy_flag | has_weapon_result::has_ammo_flag;
 	return return_value;
 }
 