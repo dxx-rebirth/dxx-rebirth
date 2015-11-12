dxx-rebirth/dxx-rebirth

commit 7da64d37825e1d32b4672eea86390ef941bdaad5

Add new autoselect-while-firing mode: "when firing stops"
Mako88 reports that some users want not to autoselect while firing, but
do not notice when a new weapon is added to the HUD, and end up never
activating a preferred weapon.  Add a new autoselect mode that remembers
what would be selected and switches to it once the player ceases firing.

Changes since Mako88's proposed version:
- Use `enum class` for autoselection mode and a radio button to
  represent the decision of Immediate/Never/Delayed.
- Handle delayed autoselect for vulcan ammo.
- Set Delayed_primary == Primary_weapon to indicate no change is needed,
  rather than Delayed_primary==-1.  This lets some paths use
  Delayed_primary without checking for a magic value.
- Likewise Delayed_secondary / Secondary_weapon.
- Update Delayed_primary/Delayed_secondary when changing active
  primary/secondary weapon, so that a player who changes weapons while
  firing will automatically clear any deferred change.

Requested-by: Mako88 <https://github.com/dxx-rebirth/dxx-rebirth/issues/97>
Based-on-patch-by: Mako88 <https://github.com/dxx-rebirth/dxx-rebirth/pull/147>

similar/main/weapon.cpp
@@ -111,6 +111,8 @@ weapon_info_array Weapon_info;
 unsigned N_weapon_types;
 primary_weapon_index_t Primary_weapon;
 sbyte Secondary_weapon;
+static primary_weapon_index_t Delayed_primary;
+static sbyte Delayed_secondary;
 
 // autoselect ordering
 
@@ -355,7 +357,7 @@ void select_primary_weapon(const char *const weapon_name, const uint_fast32_t we
 			if (wait_for_rearm) digi_play_sample( SOUND_ALREADY_SELECTED, F1_0 );
 #endif
 		}
-		Primary_weapon = static_cast<primary_weapon_index_t>(weapon_num);
+		Delayed_primary = Primary_weapon = static_cast<primary_weapon_index_t>(weapon_num);
 #if defined(DXX_BUILD_DESCENT_II)
 		//save flag for whether was super version
 		Primary_last_was_super[weapon_num % SUPER_WEAPON] = (weapon_num >= SUPER_WEAPON);
@@ -394,7 +396,7 @@ void select_secondary_weapon(const char *const weapon_name, const uint_fast32_t 
 			}
 
 		}
-		Secondary_weapon = weapon_num;
+		Delayed_secondary = Secondary_weapon = weapon_num;
 #if defined(DXX_BUILD_DESCENT_II)
 		//save flag for whether was super version
 		Secondary_last_was_super[weapon_num % SUPER_WEAPON] = (weapon_num >= SUPER_WEAPON);
@@ -667,6 +669,33 @@ void auto_select_secondary_weapon()
 		}
 }
 
+void delayed_autoselect()
+{
+	if (!Controls.state.fire_primary && Delayed_primary != Primary_weapon)
+		select_primary_weapon(nullptr, Delayed_primary, 1);
+	if (!Controls.state.fire_secondary && Delayed_secondary != Secondary_weapon)
+		select_secondary_weapon(nullptr, Delayed_secondary, 1);
+}
+
+static void maybe_autoselect_primary_weapon(int weapon_index)
+{
+	const auto want_switch = [weapon_index]{
+		const auto cutpoint = POrderList(255);
+		const auto weapon_order = POrderList(weapon_index);
+		return weapon_order < cutpoint && weapon_order < POrderList(get_mapped_weapon_index(Delayed_primary));
+	};
+	if (Controls.state.fire_primary && PlayerCfg.NoFireAutoselect != FiringAutoselectMode::Immediate)
+	{
+		if (PlayerCfg.NoFireAutoselect == FiringAutoselectMode::Delayed)
+		{
+			if (want_switch())
+				Delayed_primary = static_cast<primary_weapon_index_t>(weapon_index);
+		}
+	}
+	else if (want_switch())
+		select_primary_weapon(nullptr, weapon_index, 1);
+}
+
 //	---------------------------------------------------------------------
 //called when one of these weapons is picked up
 //when you pick up a secondary, you always get the weapon & ammo for it
@@ -693,13 +722,21 @@ int pick_up_secondary(int weapon_index,int count)
 	if (get_local_player().secondary_ammo[weapon_index] == count)	// only autoselect if player didn't have any
 	{
 		const auto weapon_order = SOrderList(weapon_index);
-		if (!(Controls.state.fire_secondary && PlayerCfg.NoFireAutoselect) &&
-			weapon_order < SOrderList(255) &&
-			(
-				get_local_player().secondary_ammo[Secondary_weapon] == 0 ||
-				weapon_order < SOrderList(Secondary_weapon)
-			)
-		)
+		const auto want_switch = [weapon_index, weapon_order]{
+			return weapon_order < SOrderList(255) && (
+				get_local_player().secondary_ammo[Delayed_secondary] == 0 ||
+				weapon_order < SOrderList(Delayed_secondary)
+				);
+		};
+		if (Controls.state.fire_secondary && PlayerCfg.NoFireAutoselect != FiringAutoselectMode::Immediate)
+		{
+			if (PlayerCfg.NoFireAutoselect == FiringAutoselectMode::Delayed)
+			{
+				if (want_switch())
+					Delayed_secondary = weapon_index;
+			}
+		}
+		else if (want_switch())
 			select_secondary_weapon(nullptr, weapon_index, 1);
 #if defined(DXX_BUILD_DESCENT_II)
 			//if it's a proxbomb or smart mine,
@@ -800,13 +837,7 @@ int pick_up_primary(int weapon_index)
 
 	get_local_player().primary_weapon_flags |= flag;
 
-	if (!(Controls.state.fire_primary && PlayerCfg.NoFireAutoselect))
-	{
-		const auto cutpoint = POrderList(255);
-		const auto weapon_order = POrderList(weapon_index);
-		if (weapon_order < cutpoint && weapon_order < POrderList(get_mapped_weapon_index()))
-			select_primary_weapon(nullptr, weapon_index, 1);
-	}
+	maybe_autoselect_primary_weapon(weapon_index);
 
 	PALETTE_FLASH_ADD(7,14,21);
 
@@ -872,7 +903,7 @@ static void maybe_autoselect_vulcan_weapon(player &plr)
 	if (better >= POrderList(get_mapped_weapon_index(Primary_weapon)))
 		/* Preferred weapon is not as desirable as the current weapon */
 		return;
-	select_primary_weapon(nullptr, weapon_index, 1);
+	maybe_autoselect_primary_weapon(weapon_index);
 }
 
 //called when ammo (for the vulcan cannon) is picked up
@@ -894,8 +925,7 @@ int pick_up_vulcan_ammo(uint_fast32_t ammo_count, const bool change_weapon)
 		plr.vulcan_ammo = max;
 	}
 	if (change_weapon &&
-		!old_ammo &&
-		!(Controls.state.fire_primary && PlayerCfg.NoFireAutoselect))
+		!old_ammo)
 		maybe_autoselect_vulcan_weapon(plr);
 	return ammo_count;	//return amount used
 }

common/main/playsave.h
@@ -75,6 +75,13 @@ struct hli
 #include "multi.h"
 #include "fwd-weapon.h"
 
+enum class FiringAutoselectMode : uint8_t
+{
+	Immediate,
+	Never,
+	Delayed,
+};
+
 struct player_config : prohibit_void_ptr<player_config>
 {
 	ubyte ControlType;
@@ -125,7 +132,7 @@ struct player_config : prohibit_void_ptr<player_config>
 	ubyte BombGauge;
 #endif
 	ubyte AutomapFreeFlight;
-	ubyte NoFireAutoselect;
+	FiringAutoselectMode NoFireAutoselect;
 	ubyte CycleAutoselectOnly;
 	uint8_t CloakInvulTimer;
 	union {

common/main/weapon.h
@@ -38,6 +38,8 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #include "compiler-type_traits.h"
 
+void delayed_autoselect();
+
 enum laser_level_t : uint8_t
 {
 	LASER_LEVEL_1,

similar/main/game.cpp
@@ -1465,6 +1465,7 @@ void GameProcessFrame(void)
 
 		if (Global_laser_firing_count < 0)
 			Global_laser_firing_count = 0;
+		delayed_autoselect();
 	}
 
 	if (Do_appearance_effect) {

similar/main/menu.cpp
@@ -2123,6 +2123,11 @@ void do_sound_menu()
 	DXX_##VERB##_CHECK("Movie Subtitles",opt_moviesubtitle,GameCfg.MovieSubtitles)	\
 
 #endif
+
+enum {
+	optgrp_autoselect_firing,
+};
+
 #define DXX_GAMEPLAY_MENU_OPTIONS(VERB)	\
 	DXX_##VERB##_CHECK("Ship auto-leveling",opt_autolevel, PlayerCfg.AutoLeveling)	\
 	DXX_##VERB##_CHECK("Persistent Debris",opt_persist_debris,PlayerCfg.PersistentDebris)	\
@@ -2133,7 +2138,10 @@ void do_sound_menu()
         DXX_##VERB##_TEXT("Weapon Autoselect options:", opt_label_autoselect)	\
 	DXX_##VERB##_MENU("Primary ordering...", opt_gameplay_reorderprimary_menu)	\
 	DXX_##VERB##_MENU("Secondary ordering...", opt_gameplay_reordersecondary_menu)	\
-	DXX_##VERB##_CHECK("No Autoselect when firing",opt_noautoselect,PlayerCfg.NoFireAutoselect)	\
+	DXX_##VERB##_TEXT("Autoselect while firing:", opt_autoselect_firing_label)	\
+	DXX_##VERB##_RADIO("Immediately", opt_autoselect_firing_immediate, PlayerCfg.NoFireAutoselect == FiringAutoselectMode::Immediate, optgrp_autoselect_firing)	\
+	DXX_##VERB##_RADIO("Never", opt_autoselect_firing_never, PlayerCfg.NoFireAutoselect == FiringAutoselectMode::Never, optgrp_autoselect_firing)	\
+	DXX_##VERB##_RADIO("When firing stops", opt_autoselect_firing_delayed, PlayerCfg.NoFireAutoselect == FiringAutoselectMode::Delayed, optgrp_autoselect_firing)	\
 	DXX_##VERB##_CHECK("Only Cycle Autoselect Weapons",opt_only_autoselect,PlayerCfg.CycleAutoselectOnly)	\
 
 enum {
@@ -2170,6 +2178,11 @@ void gameplay_config()
 		DXX_GAMEPLAY_MENU_OPTIONS(ADD);
                 i = newmenu_do1( NULL, "Gameplay Options", sizeof(m)/sizeof(*m), m, gameplay_config_menuset, unused_newmenu_userdata, 0 );
 		DXX_GAMEPLAY_MENU_OPTIONS(READ);
+		PlayerCfg.NoFireAutoselect = m[opt_autoselect_firing_delayed].value
+			? FiringAutoselectMode::Delayed
+			: (m[opt_autoselect_firing_immediate].value
+				? FiringAutoselectMode::Immediate
+				: FiringAutoselectMode::Never);
 	} while( i>-1 );
 
 }

similar/main/playsave.cpp
@@ -251,7 +251,7 @@ int new_player_config()
 	PlayerCfg.MultiMessages = 0;
 	PlayerCfg.NoRankings = 0;
 	PlayerCfg.AutomapFreeFlight = 0;
-	PlayerCfg.NoFireAutoselect = 0;
+	PlayerCfg.NoFireAutoselect = FiringAutoselectMode::Immediate;
 	PlayerCfg.CycleAutoselectOnly = 0;
         PlayerCfg.CloakInvulTimer = 0;
 	PlayerCfg.AlphaEffects = 0;
@@ -460,7 +460,7 @@ static void read_player_dxx(const char *filename)
 				if(!strcmp(line,TOGGLES_AUTOMAPFREEFLIGHT_NAME_TEXT))
 					PlayerCfg.AutomapFreeFlight = atoi(value);
 				if(!strcmp(line,TOGGLES_NOFIREAUTOSELECT_NAME_TEXT))
-					PlayerCfg.NoFireAutoselect = atoi(value);
+					PlayerCfg.NoFireAutoselect = static_cast<FiringAutoselectMode>(atoi(value));
 				if(!strcmp(line,TOGGLES_CYCLEAUTOSELECTONLY_NAME_TEXT))
 					PlayerCfg.CycleAutoselectOnly = atoi(value);
 				if(!strcmp(line,TOGGLES_CLOAKINVULTIMER_NAME_TEXT))
@@ -756,7 +756,7 @@ static int write_player_dxx(const char *filename)
 		PHYSFSX_printf(fout,TOGGLES_MULTIMESSAGES_NAME_TEXT "=%i\n",PlayerCfg.MultiMessages);
 		PHYSFSX_printf(fout,TOGGLES_NORANKINGS_NAME_TEXT "=%i\n",PlayerCfg.NoRankings);
 		PHYSFSX_printf(fout,TOGGLES_AUTOMAPFREEFLIGHT_NAME_TEXT "=%i\n",PlayerCfg.AutomapFreeFlight);
-		PHYSFSX_printf(fout,TOGGLES_NOFIREAUTOSELECT_NAME_TEXT "=%i\n",PlayerCfg.NoFireAutoselect);
+		PHYSFSX_printf(fout,TOGGLES_NOFIREAUTOSELECT_NAME_TEXT "=%i\n",static_cast<unsigned>(PlayerCfg.NoFireAutoselect));
 		PHYSFSX_printf(fout,TOGGLES_CYCLEAUTOSELECTONLY_NAME_TEXT "=%i\n",PlayerCfg.CycleAutoselectOnly);
                 PHYSFSX_printf(fout,TOGGLES_CLOAKINVULTIMER_NAME_TEXT "=%i\n",PlayerCfg.CloakInvulTimer);
 		PHYSFSX_printf(fout,END_TEXT "\n");

commit 7df905c1db682338318b1e9bdcc4b43df4cb4fe2

Fix weapon selection after respawn
Commit 7da64d3 added the ability to select a weapon when firing stops.
However, it did not take into account that
select_primary_weapon/select_secondary_weapon do not check whether the
selected weapon is available.  The caller must perform that check.  The
draft version proposed by Mako88 in the original feature request mostly
avoided the problem by clearing the delayed auto-select when it was
first applied.  That version would only manifest the problem if the
player had a delayed auto-select and lost the weapon before resolving
it, whether due to death, a thief bot, or, in the case of mines,
direct-dropping the last mine by the "Drop Bomb" key.

The version that went in switched to using delayed==active to indicate
that no change was required.  Under that design, the incorrect
auto-select appears if the player loses the active weapon even if no
delayed selection was pending.

Add a check that the player has the weapon before switching to it and
reset delayed=current if the delayed weapon is not available.  This
fixes all known cases under which a spurious delayed selection could
activate a missing weapon and prevents repeated checks for weapon
availability.

Reported-by: Mako88 <https://github.com/dxx-rebirth/dxx-rebirth/issues/158>
Fixes: 7da64d37825e1d32b4672eea86390ef941bdaad5 ("Add new autoselect-while-firing mode: "when firing stops"")

similar/main/weapon.cpp
@@ -645,10 +645,30 @@ void auto_select_secondary_weapon()
 
 void delayed_autoselect()
 {
-	if (!Controls.state.fire_primary && Delayed_primary != Primary_weapon)
-		select_primary_weapon(nullptr, Delayed_primary, 1);
-	if (!Controls.state.fire_secondary && Delayed_secondary != Secondary_weapon)
-		select_secondary_weapon(nullptr, Delayed_secondary, 1);
+	if (!Controls.state.fire_primary)
+	{
+		const auto primary_weapon = Primary_weapon;
+		const auto delayed_primary = Delayed_primary;
+		if (delayed_primary != primary_weapon)
+		{
+			if (player_has_primary_weapon(delayed_primary).has_all())
+				select_primary_weapon(nullptr, delayed_primary, 1);
+			else
+				Delayed_primary = primary_weapon;
+		}
+	}
+	if (!Controls.state.fire_secondary)
+	{
+		const auto secondary_weapon = Secondary_weapon;
+		const auto delayed_secondary = Delayed_secondary;
+		if (delayed_secondary != secondary_weapon)
+		{
+			if (player_has_secondary_weapon(delayed_secondary).has_all())
+				select_secondary_weapon(nullptr, delayed_secondary, 1);
+			else
+				Delayed_secondary = secondary_weapon;
+		}
+	}
 }
 
 static void maybe_autoselect_primary_weapon(int weapon_index)