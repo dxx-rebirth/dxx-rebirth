dxx-rebirth/dxx-rebirth

commit 6037372cdf41fa73463fb3a2948409436f0e6298

Move player vulcan_ammo to object.ctype.player_info

similar/main/collide.cpp
@@ -1835,7 +1835,6 @@ void drop_player_eggs(const vobjptridx_t playerobj)
 {
 	if ((playerobj->type == OBJ_PLAYER) || (playerobj->type == OBJ_GHOST)) {
 		playernum_t	pnum = get_player_id(playerobj);
-		int	vulcan_ammo=0;
 
 		// Seed the random number generator so in net play the eggs will always
 		// drop the same way
@@ -1873,10 +1872,11 @@ void drop_player_eggs(const vobjptridx_t playerobj)
 			}
 			if (uint16_t subtract_vulcan_ammo = map_granted_flags_to_vulcan_ammo(GrantedItems))
 			{
-				if (plr.vulcan_ammo < subtract_vulcan_ammo)
-					plr.vulcan_ammo = 0;
+				auto &v = playerobj->ctype.player_info.vulcan_ammo;
+				if (v < subtract_vulcan_ammo)
+					v = 0;
 				else
-					plr.vulcan_ammo -= subtract_vulcan_ammo;
+					v -= subtract_vulcan_ammo;
 			}
 			plr.flags &= ~map_granted_flags_to_player_flags(GrantedItems);
 			plr.primary_weapon_flags &= ~map_granted_flags_to_primary_weapon_flags(GrantedItems);
@@ -1966,7 +1966,7 @@ void drop_player_eggs(const vobjptridx_t playerobj)
 #endif
 
 		//Drop the vulcan, gauss, and ammo
-		vulcan_ammo = Players[pnum].vulcan_ammo;
+		auto vulcan_ammo = playerobj->ctype.player_info.vulcan_ammo;
 #if defined(DXX_BUILD_DESCENT_II)
 		if ((Players[pnum].primary_weapon_flags & HAS_VULCAN_FLAG) && (Players[pnum].primary_weapon_flags & HAS_GAUSS_FLAG))
 			vulcan_ammo /= 2;		//if both vulcan & gauss, each gets half
@@ -2025,7 +2025,7 @@ void drop_player_eggs(const vobjptridx_t playerobj)
 
 		//	If player has vulcan ammo, but no vulcan cannon, drop the ammo.
 		if (!(Players[get_player_id(playerobj)].primary_weapon_flags & HAS_VULCAN_FLAG)) {
-			int	amount = Players[get_player_id(playerobj)].vulcan_ammo;
+			auto amount = playerobj->ctype.player_info.vulcan_ammo;
 			if (amount > 200) {
 				amount = 200;
 			}

common/main/object.h
@@ -87,6 +87,7 @@ struct reactor_static {
 struct player_info
 {
 	fix     energy;                 // Amount of energy remaining.
+	uint16_t vulcan_ammo;
 	array<uint8_t, MAX_SECONDARY_WEAPONS>  secondary_ammo; // How much ammo of each type.
 };
 

common/main/player.h
@@ -66,7 +66,6 @@ struct player : public prohibit_void_ptr<player>
 #elif defined(DXX_BUILD_DESCENT_II)
 	ushort  primary_weapon_flags;   // bit set indicates the player has this weapon.
 #endif
-	ushort  vulcan_ammo;
 
 	// Statistics...
 	int     last_score;             // Score at beginning of current level.
@@ -170,7 +169,7 @@ static_assert(sizeof(player_rw) == 142, "wrong size player_rw");
 #define get_local_player()	(Players[Player_num])
 #define get_local_plrobj()	(*vobjptr(get_local_player().objnum))
 #define get_local_player_energy()	(get_local_plrobj().ctype.player_info.energy)
-#define get_local_player_vulcan_ammo()	(get_local_player().vulcan_ammo)
+#define get_local_player_vulcan_ammo()	(get_local_plrobj().ctype.player_info.vulcan_ammo)
 #define get_local_player_shields()	(get_local_plrobj().shields)
 #define get_local_player_flags()	(get_local_player().flags)
 #define get_local_player_secondary_ammo()	(get_local_plrobj().ctype.player_info.secondary_ammo)

d2x-rebirth/main/escort.cpp
@@ -1419,7 +1419,7 @@ static int maybe_steal_primary_weapon(const vobjptr_t playerobjp, int weapon_num
 	auto &plr = Players[get_player_id(playerobjp)];
 	if (!(plr.primary_weapon_flags & HAS_PRIMARY_FLAG(weapon_num)))
 		return 0;
-	if (!weapon_index_uses_vulcan_ammo(weapon_num) || plr.vulcan_ammo)
+	if (!weapon_index_uses_vulcan_ammo(weapon_num) || playerobjp->ctype.player_info.vulcan_ammo)
 	{
 		if (d_rand() < THIEF_PROBABILITY) {
 			if (weapon_num == primary_weapon_index_t::LASER_INDEX)

similar/main/gameseq.cpp
@@ -382,6 +382,7 @@ void init_player_stats_new_ship(ubyte pnum)
 		static_cast<uint8_t>(2 + NDL - Difficulty_level)
 	}};
 	const auto GrantedItems = (Game_mode & GM_MULTI) ? Netgame.SpawnGrantedItems : 0;
+	player_info.vulcan_ammo = map_granted_flags_to_vulcan_ammo(GrantedItems);
 	const auto granted_primary_weapon_flags = map_granted_flags_to_primary_weapon_flags(GrantedItems);
 	const auto granted_laser_level = map_granted_flags_to_laser_level(GrantedItems);
 	if (pnum == Player_num)
@@ -452,7 +453,6 @@ void init_player_stats_new_ship(ubyte pnum)
 	Players[pnum].laser_level = granted_laser_level;
 	Players[pnum].killer_objnum = object_none;
 	Players[pnum].hostages_on_board = 0;
-	plr.vulcan_ammo = map_granted_flags_to_vulcan_ammo(GrantedItems);
 	Players[pnum].primary_weapon_flags = HAS_LASER_FLAG;
 	Players[pnum].flags &= ~(PLAYER_FLAGS_QUAD_LASERS | PLAYER_FLAGS_CLOAKED | PLAYER_FLAGS_INVULNERABLE);
 #if defined(DXX_BUILD_DESCENT_II)

similar/main/laser.cpp
@@ -1717,7 +1717,6 @@ static inline int sufficient_ammo(int ammo_used, int uses_vulcan_ammo, ushort vu
 
 int do_laser_firing_player(void)
 {
-	player *const plp = &get_local_player();
 	fix		energy_used;
 	int		ammo_used;
 	int		weapon_index;
@@ -1741,7 +1740,8 @@ int do_laser_firing_player(void)
 
 	int uses_vulcan_ammo = weapon_index_uses_vulcan_ammo(Primary_weapon);
 
-	auto &pl_energy = get_local_plrobj().ctype.player_info.energy;
+	auto &player_info = get_local_plrobj().ctype.player_info;
+	auto &pl_energy = player_info.energy;
 #if defined(DXX_BUILD_DESCENT_II)
 	if (Primary_weapon == primary_weapon_index_t::OMEGA_INDEX)
 		energy_used = 0;	//	Omega consumes energy when recharging, not when firing.
@@ -1750,12 +1750,12 @@ int do_laser_firing_player(void)
 		if (Game_mode & GM_MULTI)
 			energy_used *= 2;
 
-	if	(!(sufficient_energy(energy_used, pl_energy) && sufficient_ammo(ammo_used, uses_vulcan_ammo, plp->vulcan_ammo)))
+	if	(!(sufficient_energy(energy_used, pl_energy) && sufficient_ammo(ammo_used, uses_vulcan_ammo, player_info.vulcan_ammo)))
 		auto_select_primary_weapon();		//	Make sure the player can fire from this weapon.
 #endif
 
 	while (Next_laser_fire_time <= GameTime64) {
-		if	(sufficient_energy(energy_used, pl_energy) && sufficient_ammo(ammo_used, uses_vulcan_ammo, plp->vulcan_ammo)) {
+		if	(sufficient_energy(energy_used, pl_energy) && sufficient_ammo(ammo_used, uses_vulcan_ammo, player_info.vulcan_ammo)) {
 			int	laser_level, flags, fire_frame_overhead = 0;
 
 			if (GameTime64 - Next_laser_fire_time <= FrameTime) // if firing is prolonged by FrameTime overhead, let's try to fix that.
@@ -1795,10 +1795,11 @@ int do_laser_firing_player(void)
 				pl_energy = 0;
 
 			if (uses_vulcan_ammo) {
-				if (ammo_used > plp->vulcan_ammo)
-					plp->vulcan_ammo = 0;
+				auto &v = player_info.vulcan_ammo;
+				if (v < ammo_used)
+					v = 0;
 				else
-					plp->vulcan_ammo -= ammo_used;
+					v -= ammo_used;
 			}
 
 			auto_select_primary_weapon();		//	Make sure the player can fire from this weapon.

similar/main/multi.cpp
@@ -1692,7 +1692,8 @@ static void multi_do_player_deres(const playernum_t pnum, const ubyte *buf)
 	Players[pnum].laser_level = stored_laser_level(buf[count]);                                                 count++;
 
 	const auto &&objp = vobjptridx(Players[pnum].objnum);
-	auto &secondary_ammo = objp->ctype.player_info.secondary_ammo;
+	auto &player_info = objp->ctype.player_info;
+	auto &secondary_ammo = player_info.secondary_ammo;
 	secondary_ammo[HOMING_INDEX] = buf[count];                count++;
 	secondary_ammo[CONCUSSION_INDEX] = buf[count];count++;
 	secondary_ammo[SMART_INDEX] = buf[count];         count++;
@@ -1707,7 +1708,7 @@ static void multi_do_player_deres(const playernum_t pnum, const ubyte *buf)
 	secondary_ammo[SMISSILE5_INDEX] = buf[count]; count++;
 #endif
 
-	Players[pnum].vulcan_ammo = GET_INTEL_SHORT(buf + count); count += 2;
+	player_info.vulcan_ammo = GET_INTEL_SHORT(buf + count); count += 2;
 	Players[pnum].flags = player_flags(GET_INTEL_INT(buf + count));    count += 4;
 
 	multi_powcap_adjust_remote_cap (pnum);

similar/main/state.cpp
@@ -481,7 +481,7 @@ static void state_player_to_player_rw(const fix pl_shields, const player *pl, pl
 	pl_rw->killer_objnum             = pl->killer_objnum;
 	pl_rw->primary_weapon_flags      = pl->primary_weapon_flags;
 	pl_rw->secondary_weapon_flags    = 0;
-	pl_rw->vulcan_ammo   = pl->vulcan_ammo;
+	pl_rw->vulcan_ammo   = pl_info.vulcan_ammo;
 	for (i = 0; i < MAX_SECONDARY_WEAPONS; i++)
 		pl_rw->secondary_ammo[i] = pl_info.secondary_ammo[i];
 	pl_rw->last_score                = pl->last_score;
@@ -530,7 +530,7 @@ static void state_player_rw_to_player(const player_rw *pl_rw, player *pl, player
 	pl->starting_level            = pl_rw->starting_level;
 	pl->killer_objnum             = pl_rw->killer_objnum;
 	pl->primary_weapon_flags      = pl_rw->primary_weapon_flags;
-	pl->vulcan_ammo   = pl_rw->vulcan_ammo;
+	pl_info.vulcan_ammo   = pl_rw->vulcan_ammo;
 	for (i = 0; i < MAX_SECONDARY_WEAPONS; i++)
 		pl_info.secondary_ammo[i] = pl_rw->secondary_ammo[i];
 	pl->last_score                = pl_rw->last_score;

commit 7d22139334d0c61ba4300f7ad3a54bf6df8c3790

Fix vulcan ammo drop loop
Fixes: 6037372cdf41fa73463fb3a2948409436f0e6298 ("Move player vulcan_ammo to object.ctype.player_info")

similar/main/collide.cpp
@@ -2029,8 +2029,12 @@ void drop_player_eggs(const vobjptridx_t playerobj)
 			if (amount > 200) {
 				amount = 200;
 			}
-			while (amount > 0) {
+			if (amount)
+				for (;;)
+			{
 				call_object_create_egg(playerobj, 1, OBJ_POWERUP, POW_VULCAN_AMMO);
+				if (amount <= VULCAN_AMMO_AMOUNT)
+					break;
 				amount -= VULCAN_AMMO_AMOUNT;
 			}
 		}