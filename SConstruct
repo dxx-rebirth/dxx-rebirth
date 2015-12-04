dxx-rebirth/dxx-rebirth

commit 6111e5adbfa4977ff2ec46e96dda8561feea92da

Allow configuring max joysticks at build time

SConstruct
@@ -757,14 +757,38 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 	def check_libSDL2(self,context):
 		self._check_libSDL(context, '2')
 	def _check_libSDL(self,context,sdl2):
-		successflags = self.pkgconfig.merge(context, self.message, self.user_settings, 'sdl%s' % sdl2, 'SDL%s' % sdl2)
+		user_settings = self.user_settings
+		successflags = self.pkgconfig.merge(context, self.message, user_settings, 'sdl%s' % sdl2, 'SDL%s' % sdl2)
+		if user_settings.max_joysticks:
+			# If joysticks are enabled, but all possible inputs are
+			# disabled, then disable joystick support.
+			if not (user_settings.max_axes_per_joystick or user_settings.max_buttons_per_joystick or user_settings.max_hats_per_joystick):
+				user_settings.max_joysticks = 0
+		else:
+			# If joysticks are disabled, then disable all possible
+			# inputs.
+			user_settings.max_axes_per_joystick = user_settings.max_buttons_per_joystick = user_settings.max_hats_per_joystick = 0
+		successflags.setdefault('CPPDEFINES', []).extend((
+			('MAX_JOYSTICKS', user_settings.max_joysticks),
+			('MAX_AXES_PER_JOYSTICK', user_settings.max_axes_per_joystick),
+			('MAX_BUTTONS_PER_JOYSTICK', user_settings.max_buttons_per_joystick),
+			('MAX_HATS_PER_JOYSTICK', user_settings.max_hats_per_joystick),
+		))
+		context.Display('%s: checking whether to enable joystick support...%s\n' % (self.msgprefix, 'yes' if user_settings.max_joysticks else 'no'))
 		# SDL2 removed CD-rom support.
 		init_cdrom = '0' if sdl2 else 'SDL_INIT_CDROM'
 		self._check_system_library(context,header=['SDL.h'],main='''
 	SDL_RWops *ops = reinterpret_cast<SDL_RWops *>(argv);
-	SDL_Init(SDL_INIT_JOYSTICK | %s | SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+#if MAX_JOYSTICKS
+#define DXX_SDL_INIT_JOYSTICK	SDL_INIT_JOYSTICK |
+#else
+#define DXX_SDL_INIT_JOYSTICK
+#endif
+	SDL_Init(DXX_SDL_INIT_JOYSTICK %s | SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+#if MAX_JOYSTICKS
 	auto n = SDL_NumJoysticks();
 	(void)n;
+#endif
 	SDL_QuitSubSystem(SDL_INIT_VIDEO);
 	SDL_FreeRW(ops);
 	SDL_Quit();
@@ -2288,14 +2312,23 @@ class DXXCommon(LazyObjectConstructor):
 	class UserBuildSettings:
 		class IntVariable(object):
 			def __new__(cls,key,help,default):
-				return (key, help, default, cls.__validator, int)
+				return (key, help, default, cls._validator, int)
 			@staticmethod
-			def __validator(key, value, env):
+			def _validator(key, value, env):
 				try:
 					int(value)
 					return True
 				except ValueError:
-					raise SCons.Errors.UserError('Invalid value for integer-only option %s: %s.' % (key, val))
+					raise SCons.Errors.UserError('Invalid value for integer-only option %s: %s.' % (key, value))
+		class UIntVariable(IntVariable):
+			@staticmethod
+			def _validator(key, value, env):
+				try:
+					if int(value) < 0:
+						raise ValueError
+					return True
+				except ValueError:
+					raise SCons.Errors.UserError('Invalid value for unsigned-integer-only option %s: %s.' % (key, value))
 		# Paths for the Videocore libs/includes on the Raspberry Pi
 		RPI_DEFAULT_VC_PATH='/opt/vc'
 		default_OGLES_LIB = 'GLES_CM'
@@ -2432,11 +2465,15 @@ class DXXCommon(LazyObjectConstructor):
 				),
 			},
 			{
-				'variable': self.IntVariable,
+				'variable': self.UIntVariable,
 				'arguments': (
 					('lto', 0, 'enable gcc link time optimization'),
 					('pch', None, 'pre-compile own headers used at least this many times'),
 					('syspch', None, 'pre-compile system headers used at least this many times'),
+					('max_joysticks', 8, 'maximum number of usable joysticks'),
+					('max_axes_per_joystick', 128, 'maximum number of axes per joystick'),
+					('max_buttons_per_joystick', 128, 'maximum number of buttons per joystick'),
+					('max_hats_per_joystick', 4, 'maximum number of hats per joystick'),
 				),
 			},
 			{

common/arch/sdl/event.cpp
@@ -11,6 +11,7 @@
  *
  */
 
+#include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "event.h"

common/arch/sdl/joy.cpp
@@ -22,6 +22,9 @@
 #include "kconfig.h"
 #include "compiler-range_for.h"
 
+#if MAX_JOYSTICKS
+#include "compiler-type_traits.h"
+
 namespace {
 
 int num_joysticks = 0;
@@ -30,7 +33,9 @@ int num_joysticks = 0;
  * and buttons of every joystick found.
  */
 static struct joyinfo {
+#if MAX_BUTTONS_PER_JOYSTICK
 	array<uint8_t, JOY_MAX_BUTTONS> button_state;
+#endif
 } Joystick;
 
 struct d_event_joystickbutton : d_event
@@ -50,6 +55,12 @@ public:
 		SDL_JoystickClose(j);
 	}
 };
+
+template <typename>
+class ignore_empty {};
+
+template <typename T>
+using maybe_empty_array = typename tt::conditional<T().empty(), ignore_empty<T>, T>::type;
 
 /* This struct is an array, with one entry for each physical joystick
  * found.
@@ -82,10 +93,10 @@ class d_physical_joystick
 	struct tuple_member_type_axis_value : array<int, MAX_AXES_PER_JOYSTICK> {};
 	std::tuple<
 		tuple_member_type_handle,
-		tuple_member_type_hat_map,
-		tuple_member_type_button_map,
-		tuple_member_type_axis_map,
-		tuple_member_type_axis_value
+		maybe_empty_array<tuple_member_type_hat_map>,
+		maybe_empty_array<tuple_member_type_button_map>,
+		maybe_empty_array<tuple_member_type_axis_map>,
+		maybe_empty_array<tuple_member_type_axis_value>
 	> t;
 public:
 	for_each_tuple_item(define_getter);
@@ -97,6 +108,7 @@ public:
 
 static array<d_physical_joystick, MAX_JOYSTICKS> SDL_Joysticks;
 
+#if MAX_BUTTONS_PER_JOYSTICK
 void joy_button_handler(SDL_JoyButtonEvent *jbe)
 {
 	int button;
@@ -111,7 +123,9 @@ void joy_button_handler(SDL_JoyButtonEvent *jbe)
 	con_printf(CON_DEBUG, "Sending event %s, button %d", (jbe->type == SDL_JOYBUTTONDOWN) ? "EVENT_JOYSTICK_BUTTON_DOWN" : "EVENT_JOYSTICK_JOYSTICK_UP", event.button);
 	event_send(event);
 }
+#endif
 
+#if MAX_HATS_PER_JOYSTICK
 void joy_hat_handler(SDL_JoyHatEvent *jhe)
 {
 	int hat = SDL_Joysticks[jhe->which].hat_map()[jhe->hat];
@@ -156,7 +170,9 @@ void joy_hat_handler(SDL_JoyHatEvent *jhe)
 		event_send(event);
 	}
 }
+#endif
 
+#if MAX_AXES_PER_JOYSTICK
 int joy_axis_handler(SDL_JoyAxisEvent *jae)
 {
 	d_event_joystick_moved event;
@@ -175,6 +191,7 @@ int joy_axis_handler(SDL_JoyAxisEvent *jae)
 
 	return 1;
 }
+#endif
 
 
 /* ----------------------------------------------- */
@@ -199,8 +216,12 @@ void joy_init()
 	}
 
 	Joystick = {};
+#if MAX_AXES_PER_JOYSTICK
 	joyaxis_text.clear();
+#endif
+#if MAX_BUTTONS_PER_JOYSTICK || MAX_HATS_PER_JOYSTICK
 	joybutton_text.clear();
+#endif
 
 	const auto n = check_warn_joy_support_limit<MAX_JOYSTICKS>(SDL_NumJoysticks(), "joystick");
 	unsigned joystick_n_buttons = 0, joystick_n_axes = 0;
@@ -215,6 +236,7 @@ void joy_init()
 #endif
 		if (handle)
 		{
+#if MAX_AXES_PER_JOYSTICK
 			const auto n_axes = check_warn_joy_support_limit<MAX_AXES_PER_JOYSTICK>(SDL_JoystickNumAxes(handle), "axe");
 
 			joyaxis_text.resize(joyaxis_text.size() + n_axes);
@@ -224,17 +246,22 @@ void joy_init()
 				joystick.axis_map()[j] = joystick_n_axes++;
 				snprintf(&text[0], sizeof(text), "J%d A%d", i + 1, j + 1);
 			}
+#endif
 
+#if MAX_BUTTONS_PER_JOYSTICK || MAX_HATS_PER_JOYSTICK
 			const auto n_buttons = check_warn_joy_support_limit<MAX_BUTTONS_PER_JOYSTICK>(SDL_JoystickNumButtons(handle), "button");
 			const auto n_hats = check_warn_joy_support_limit<MAX_HATS_PER_JOYSTICK>(SDL_JoystickNumHats(handle), "hat");
 
 			joybutton_text.resize(joybutton_text.size() + n_buttons + (4 * n_hats));
+#if MAX_BUTTONS_PER_JOYSTICK
 			for (int j=0; j < n_buttons; j++)
 			{
 				auto &text = joybutton_text[joystick_n_buttons];
 				joystick.button_map()[j] = joystick_n_buttons++;
 				snprintf(&text[0], sizeof(text), "J%d B%d", i + 1, j + 1);
 			}
+#endif
+#if MAX_HATS_PER_JOYSTICK
 			for (int j=0; j < n_hats; j++)
 			{
 				joystick.hat_map()[j] = joystick_n_buttons;
@@ -244,6 +271,8 @@ void joy_init()
 				snprintf(&joybutton_text[joystick_n_buttons++][0], sizeof(joybutton_text[0]), "J%d H%d%c", i + 1, j + 1, 0200);
 				snprintf(&joybutton_text[joystick_n_buttons++][0], sizeof(joybutton_text[0]), "J%d H%d%c", i + 1, j + 1, 0201);
 			}
+#endif
+#endif
 
 			num_joysticks++;
 		}
@@ -259,8 +288,12 @@ void joy_close()
 {
 	range_for (auto &j, SDL_Joysticks)
 		j.handle().reset();
+#if MAX_AXES_PER_JOYSTICK
 	joyaxis_text.clear();
+#endif
+#if MAX_BUTTONS_PER_JOYSTICK || MAX_HATS_PER_JOYSTICK
 	joybutton_text.clear();
+#endif
 }
 
 const d_event_joystick_axis_value &event_joystick_get_axis(const d_event &event)
@@ -276,9 +309,13 @@ void joy_flush()
 		return;
 
 	static_assert(SDL_RELEASED == uint8_t(), "SDL_RELEASED not 0.");
+#if MAX_BUTTONS_PER_JOYSTICK
 	Joystick.button_state = {};
+#endif
+#if MAX_AXES_PER_JOYSTICK
 	range_for (auto &j, SDL_Joysticks)
 		j.axis_value() = {};
+#endif
 }
 
 int event_joystick_get_button(const d_event &event)
@@ -287,3 +324,4 @@ int event_joystick_get_button(const d_event &event)
 	Assert(e.type == EVENT_JOYSTICK_BUTTON_DOWN || e.type == EVENT_JOYSTICK_BUTTON_UP);
 	return e.button;
 }
+#endif

common/include/joy.h
@@ -10,9 +10,11 @@
  *
  */
 
-#ifndef _JOY_H
-#define _JOY_H
+#pragma once
 
+#include "dxxsconf.h"
+
+#if MAX_JOYSTICKS
 #include "pstypes.h"
 #include "maths.h"
 #include <SDL.h>
@@ -21,10 +23,6 @@
 
 struct d_event;
 
-#define MAX_JOYSTICKS				8
-#define MAX_AXES_PER_JOYSTICK		128
-#define MAX_BUTTONS_PER_JOYSTICK	128
-#define MAX_HATS_PER_JOYSTICK		4
 #define JOY_MAX_AXES				(MAX_AXES_PER_JOYSTICK * MAX_JOYSTICKS)
 #define JOY_MAX_BUTTONS				(MAX_BUTTONS_PER_JOYSTICK * MAX_JOYSTICKS)
 
@@ -39,10 +37,27 @@ extern void joy_close();
 const d_event_joystick_axis_value &event_joystick_get_axis(const d_event &event);
 extern void joy_flush();
 extern int event_joystick_get_button(const d_event &event);
-extern void joy_button_handler(SDL_JoyButtonEvent *jbe);
-extern void joy_hat_handler(SDL_JoyHatEvent *jhe);
-extern int joy_axis_handler(SDL_JoyAxisEvent *jae);
-
+#endif
+#else
+#define joy_init()
+#define joy_flush()
+#define joy_close()
 #endif
 
-#endif // _JOY_H
+#if MAX_BUTTONS_PER_JOYSTICK
+extern void joy_button_handler(SDL_JoyButtonEvent *jbe);
+#else
+#define joy_button_handler(jbe)	static_cast<void>(static_cast<SDL_JoyButtonEvent *const &>(jbe))
+#endif
+
+#if MAX_HATS_PER_JOYSTICK
+extern void joy_hat_handler(SDL_JoyHatEvent *jhe);
+#else
+#define joy_hat_handler(jhe)	static_cast<void>(static_cast<SDL_JoyHatEvent *const &>(jhe))
+#endif
+
+#if MAX_AXES_PER_JOYSTICK
+extern int joy_axis_handler(SDL_JoyAxisEvent *jae);
+#else
+#define joy_axis_handler(jae)	(static_cast<void>(static_cast<SDL_JoyAxisEvent *const &>(jae)), 1)
+#endif

common/main/kconfig.h
@@ -77,8 +77,10 @@ struct control_info {
 	fix pitch_time, vertical_thrust_time, heading_time, sideways_thrust_time, bank_time, forward_thrust_time;
         fix excess_pitch_time, excess_vertical_thrust_time, excess_heading_time, excess_sideways_thrust_time, excess_bank_time, excess_forward_thrust_time;
 	state_controls_t state; // to scale movement for keys only we need them to be separate from joystick/mouse buttons
-	array<fix, JOY_MAX_AXES> joy_axis, raw_joy_axis;
 	array<fix, 3> mouse_axis, raw_mouse_axis;
+#if MAX_AXES_PER_JOYSTICK
+	array<fix, JOY_MAX_AXES> joy_axis, raw_joy_axis;
+#endif
 };
 
 extern control_info Controls;
@@ -100,7 +102,9 @@ extern void kconfig_read_controls(const d_event &event, int automap_flag);
 enum class kconfig_type
 {
 	keyboard,
+#if MAX_JOYSTICKS
 	joystick,
+#endif
 	mouse,
 	rebirth,
 };
@@ -120,6 +124,7 @@ extern void reset_cruise(void);
 extern fix Cruise_speed;
 
 
+#if MAX_JOYSTICKS
 template <std::size_t N>
 struct joystick_text_length : tt::integral_constant<std::size_t, (N >= 10) ? (joystick_text_length<N / 10>::value + 1) : 1>
 {
@@ -143,14 +148,19 @@ public:
 	reference operator[](size_type s) { return text.at(s); }
 };
 
+#if MAX_AXES_PER_JOYSTICK
 class joyaxis_text_t : public joystick_text_t<sizeof("J A") + joystick_text_length<MAX_JOYSTICKS>::value + joystick_text_length<MAX_AXES_PER_JOYSTICK>::value>
 {
 };
+extern joyaxis_text_t joyaxis_text;
+#endif
 
-class joybutton_text_t : public joystick_text_t<sizeof("J H ") + joystick_text_length<MAX_JOYSTICKS>::value + joystick_text_length<MAX_BUTTONS_PER_JOYSTICK>::value>
+#if MAX_BUTTONS_PER_JOYSTICK || MAX_HATS_PER_JOYSTICK
+class joybutton_text_t : public joystick_text_t<sizeof("J H ") + joystick_text_length<MAX_JOYSTICKS>::value + joystick_text_length<MAX_HATS_PER_JOYSTICK>::value>
 {
 };
-
 extern joybutton_text_t joybutton_text;
-extern joyaxis_text_t joyaxis_text;
+#endif
+
+#endif
 #endif

similar/main/menu.cpp
@@ -1139,6 +1139,7 @@ static void input_config_mouse()
 	PlayerCfg.MouseFSDead = m[mousefsdead].value;
 }
 
+#if MAX_AXES_PER_JOYSTICK
 static void input_config_joystick()
 {
 #define DXX_INPUT_CONFIG_MENU(VERB)	                                   \
@@ -1187,17 +1188,30 @@ static void input_config_joystick()
 		PlayerCfg.JoystickDead[i] = m[joydead+i].value;
 	}
 }
+#endif
 
 namespace {
 
 class input_config_menu_items
 {
+#if MAX_JOYSTICKS
+#define DXX_INPUT_CONFIG_JOYSTICK_ITEM(I)	I
+#else
+#define DXX_INPUT_CONFIG_JOYSTICK_ITEM(I)
+#endif
+
+#if MAX_AXES_PER_JOYSTICK
+#define DXX_INPUT_CONFIG_JOYSTICK_AXIS_ITEM(I)	I
+#else
+#define DXX_INPUT_CONFIG_JOYSTICK_AXIS_ITEM(I)
+#endif
+
 #define DXX_INPUT_CONFIG_MENU(VERB)	\
-	DXX_##VERB##_CHECK("Use joystick", opt_ic_usejoy, PlayerCfg.ControlType & CONTROL_USING_JOYSTICK)	\
+	DXX_INPUT_CONFIG_JOYSTICK_ITEM(DXX_##VERB##_CHECK("Use joystick", opt_ic_usejoy, PlayerCfg.ControlType & CONTROL_USING_JOYSTICK))	\
 	DXX_##VERB##_CHECK("Use mouse", opt_ic_usemouse, PlayerCfg.ControlType & CONTROL_USING_MOUSE)	\
 	DXX_##VERB##_TEXT("", opt_label_blank_use)	\
 	DXX_##VERB##_MENU(TXT_CUST_KEYBOARD, opt_ic_confkey)	\
-	DXX_##VERB##_MENU("Customize Joystick", opt_ic_confjoy)	\
+	DXX_INPUT_CONFIG_JOYSTICK_ITEM(DXX_##VERB##_MENU("Customize Joystick", opt_ic_confjoy))	\
 	DXX_##VERB##_MENU("Customize Mouse", opt_ic_confmouse)	\
 	DXX_##VERB##_MENU("Customize Weapon Keys", opt_ic_confweap)	\
 	DXX_##VERB##_TEXT("", opt_label_blank_customize)	\
@@ -1207,7 +1221,7 @@ class input_config_menu_items
 	DXX_##VERB##_TEXT("", opt_label_blank_mouse_control_type)	\
 	DXX_##VERB##_MENU("Keyboard Calibration", opt_ic_keyboard)	        \
 	DXX_##VERB##_MENU("Mouse Calibration", opt_ic_mouse)	              \
-	DXX_##VERB##_MENU("Joysick Calibration", opt_ic_joystick)	         \
+	DXX_INPUT_CONFIG_JOYSTICK_AXIS_ITEM(DXX_##VERB##_MENU("Joystick Calibration", opt_ic_joystick))	         \
 	DXX_##VERB##_TEXT("", opt_label_blank_sensitivity_deadzone)	\
 	DXX_##VERB##_CHECK("Keep Keyboard/Mouse focus", opt_ic_grabinput, CGameCfg.Grabinput)	\
 	DXX_##VERB##_CHECK("Mouse FlightSim Indicator", opt_ic_mousefsgauge, PlayerCfg.MouseFSIndicator)	\
@@ -1232,6 +1246,8 @@ public:
 	}
 	static int menuset(newmenu *, const d_event &event, input_config_menu_items *pitems);
 #undef DXX_INPUT_CONFIG_MENU
+#undef DXX_INPUT_CONFIG_JOYSTICK_AXIS_ITEM
+#undef DXX_INPUT_CONFIG_JOYSTICK_ITEM
 };
 
 }
@@ -1244,6 +1260,7 @@ int input_config_menu_items::menuset(newmenu *, const d_event &event, input_conf
 		case EVENT_NEWMENU_CHANGED:
 		{
 			const auto citem = static_cast<const d_change_event &>(event).citem;
+#if MAX_JOYSTICKS
 			if (citem == opt_ic_usejoy)
 			{
 				constexpr auto flag = CONTROL_USING_JOYSTICK;
@@ -1252,6 +1269,7 @@ int input_config_menu_items::menuset(newmenu *, const d_event &event, input_conf
 				else
 					PlayerCfg.ControlType &= ~flag;
 			}
+#endif
 			if (citem == opt_ic_usemouse)
 			{
 				constexpr auto flag = CONTROL_USING_MOUSE;
@@ -1275,8 +1293,10 @@ int input_config_menu_items::menuset(newmenu *, const d_event &event, input_conf
 			const auto citem = static_cast<const d_select_event &>(event).citem;
 			if (citem == opt_ic_confkey)
 				kconfig(kconfig_type::keyboard);
+#if MAX_JOYSTICKS
 			if (citem == opt_ic_confjoy)
 				kconfig(kconfig_type::joystick);
+#endif
 			if (citem == opt_ic_confmouse)
 				kconfig(kconfig_type::mouse);
 			if (citem == opt_ic_confweap)
@@ -1285,8 +1305,10 @@ int input_config_menu_items::menuset(newmenu *, const d_event &event, input_conf
 				input_config_keyboard();
 			if (citem == opt_ic_mouse)
 				input_config_mouse();
+#if MAX_AXES_PER_JOYSTICK
 			if (citem == opt_ic_joystick)
 				input_config_joystick();
+#endif
 			if (citem == opt_ic_help0)
 				show_help();
 			if (citem == opt_ic_help1)
@@ -1306,7 +1328,7 @@ int input_config_menu_items::menuset(newmenu *, const d_event &event, input_conf
 void input_config()
 {
 	input_config_menu_items menu_items;
-	newmenu_do1(nullptr, TXT_CONTROLS, menu_items.m.size(), menu_items.m.data(), &input_config_menu_items::menuset, &menu_items, 3);
+	newmenu_do1(nullptr, TXT_CONTROLS, menu_items.m.size(), menu_items.m.data(), &input_config_menu_items::menuset, &menu_items, menu_items.opt_ic_confkey);
 }
 
 static void reticle_config()

commit ff51ff547457b11bcba2b8fc9b5cda47ea96a9ea

Copy sdl-config CPPDEFINES before modification
Fixes: 6111e5adbfa4977ff2ec46e96dda8561feea92da ("Allow configuring max joysticks at build time")

SConstruct
@@ -756,7 +756,7 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 		self._check_libSDL(context, '2')
 	def _check_libSDL(self,context,sdl2):
 		user_settings = self.user_settings
-		successflags = self.pkgconfig.merge(context, self.message, user_settings, 'sdl%s' % sdl2, 'SDL%s' % sdl2)
+		successflags = self.pkgconfig.merge(context, self.message, user_settings, 'sdl%s' % sdl2, 'SDL%s' % sdl2).copy()
 		if user_settings.max_joysticks:
 			# If joysticks are enabled, but all possible inputs are
 			# disabled, then disable joystick support.
@@ -766,7 +766,8 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 			# If joysticks are disabled, then disable all possible
 			# inputs.
 			user_settings.max_axes_per_joystick = user_settings.max_buttons_per_joystick = user_settings.max_hats_per_joystick = 0
-		successflags.setdefault('CPPDEFINES', []).extend((
+		successflags['CPPDEFINES'] = CPPDEFINES = successflags.get('CPPDEFINES', [])[:]
+		CPPDEFINES.extend((
 			('MAX_JOYSTICKS', user_settings.max_joysticks),
 			('MAX_AXES_PER_JOYSTICK', user_settings.max_axes_per_joystick),
 			('MAX_BUTTONS_PER_JOYSTICK', user_settings.max_buttons_per_joystick),