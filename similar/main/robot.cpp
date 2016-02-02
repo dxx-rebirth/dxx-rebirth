dxx-rebirth/dxx-rebirth

commit ed557636033a6611309405fbe1b59017eccaf53a

Remove unused symbols

similar/main/robot.cpp
@@ -307,8 +307,10 @@ void jointpos_read(PHYSFS_file *fp, jointpos &jp)
 	PHYSFSX_readAngleVec(&jp.angles, fp);
 }
 
+#if 0
 void jointpos_write(PHYSFS_file *fp, const jointpos &jp)
 {
 	PHYSFS_writeSLE16(fp, jp.jointnum);
 	PHYSFSX_writeAngleVec(fp, jp.angles);
 }
+#endif

common/2d/bitmap.cpp
@@ -160,32 +160,6 @@ void build_colormap_good(palette_array_t &palette, array<color_t, 256> &colormap
 	}
 }
 
-void gr_remap_bitmap( grs_bitmap * bmp, palette_array_t &palette, int transparent_color, int super_transparent_color )
-{
-	array<uint8_t, 256> colormap;
-	array<unsigned, 256> freq;
-
-	if (bmp->get_type() != BM_LINEAR)
-		return;	 //can't do it
-
-	// This should be build_colormap_asm, but we're not using invert table, so...
-	build_colormap_good( palette, colormap, freq );
-
-	if ( (super_transparent_color>=0) && (super_transparent_color<=255))
-		colormap[super_transparent_color] = 254;
-
-	if ( (transparent_color>=0) && (transparent_color<=255))
-		colormap[transparent_color] = TRANSPARENCY_COLOR;
-
-	decode_data(bmp->get_bitmap_data(), bmp->bm_w * bmp->bm_h, colormap, freq );
-
-	if ( (transparent_color>=0) && (transparent_color<=255) && (freq[transparent_color]>0) )
-		gr_set_transparent(*bmp, 1);
-
-	if ( (super_transparent_color>=0) && (super_transparent_color<=255) && (freq[super_transparent_color]>0) )
-		gr_set_super_transparent(*bmp, 0);
-}
-
 void gr_remap_bitmap_good(grs_bitmap &bmp, palette_array_t &palette, uint_fast32_t transparent_color, uint_fast32_t super_transparent_color)
 {
 	array<uint8_t, 256> colormap;

common/2d/circle.cpp
@@ -24,54 +24,6 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "grdef.h"
 
 #ifndef OGL
-
-int gr_circle(fix xc1,fix yc1,fix r1)
-{
-	int p,x, y, xc, yc, r;
-
-	r = f2i(r1);
-	xc = f2i(xc1);
-	yc = f2i(yc1);
-	p=3-(r*2);
-	x=0;
-	y=r;
-
-	// Big clip
-	if ( (xc+r) < 0 ) return 1;
-	if ( (xc-r) > GWIDTH ) return 1;
-	if ( (yc+r) < 0 ) return 1;
-	if ( (yc-r) > GHEIGHT ) return 1;
-
-	while(x<y)
-	{
-		// Draw the first octant
-		gr_pixel( xc-y, yc-x );
-		gr_pixel( xc+y, yc-x );
-		gr_pixel( xc-y, yc+x );
-		gr_pixel( xc+y, yc+x );
-
-		if (p<0)
-			p=p+(x<<2)+6;
-		else {
-			// Draw the second octant
-			gr_pixel( xc-x, yc-y );
-			gr_pixel( xc+x, yc-y );
-			gr_pixel( xc-x, yc+y );
-			gr_pixel( xc+x, yc+y );
-			p=p+((x-y)<<2)+10;
-			y--;
-		}
-		x++;
-	}
-	if(x==y) {
-		gr_pixel( xc-x, yc-y );
-		gr_pixel( xc+x, yc-y );
-		gr_pixel( xc-x, yc+y );
-		gr_pixel( xc+x, yc+y );
-	}
-	return 0;
-}
-
 int gr_ucircle(fix xc1,fix yc1,fix r1)
 {
 	int p,x, y, xc, yc, r;

common/editor/autosave.cpp
@@ -42,14 +42,12 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #define AUTOSAVE_PERIOD 5			// Number of minutes for timed autosave
 
 int		Autosave_count;
-int		Autosave_numfiles;
-int		Autosave_total;
-int		undo_count;
-int		original;
+static int Autosave_numfiles;
+static int Autosave_total;
+static int undo_count;
 
-int		Timer_save_flag=0;
+static int Timer_save_flag;
 int		Autosave_flag;
-int		save_second=-1;
 
 array<const char *, 10> undo_status;
 

common/include/digi_audio.h
@@ -24,7 +24,6 @@ void digi_audio_set_channel_pan(int, int );
 void digi_audio_stop_sound(int );
 void digi_audio_end_sound(int );
 void digi_audio_set_digi_volume(int);
-void digi_audio_debug();
 
 #endif
 

common/include/digi_mixer.h
@@ -23,7 +23,6 @@ int digi_mixer_is_channel_playing(int);
 void digi_mixer_reset();
 void digi_mixer_stop_all_channels();
 void digi_mixer_set_digi_volume(int);
-void digi_mixer_debug();
 
 #endif
 

common/include/editor/editor.h
@@ -351,7 +351,6 @@ extern int CurrentTexture;
 
 void med_propagate_tmaps_to_segments(vsegptridx_t base_seg,vsegptridx_t con_seg, int uv_only_flag);
 void med_propagate_tmaps_to_back_side(vsegptridx_t base_seg, int back_side, int uv_only_flag);
-void med_propagate_tmaps_to_any_side(vsegptridx_t base_seg, int back_side, int tmap_num, int uv_only_flag);
 
 //	Find segment adjacent to sp:side.
 //	Adjacent means a segment which shares all four vertices.

common/include/editor/kdefs.h
@@ -161,7 +161,6 @@ int PropagateTexturesSelected();
 //--//int MacroLoad();
 
 // In editor.c
-int medlisp_add_segment();
 int AttachSegment();
 int DeleteSegment();
 int CallLisp();
@@ -289,7 +288,6 @@ extern int wall_add_blastable(void);
 extern int wall_add_closed_wall(void);
 extern int wall_add_external_wall(void);
 extern int wall_lock_door(void);
-extern int wall_unlock_door(void);
 extern int wall_automate_door(void);
 extern int wall_deautomate_door(void);
 extern int wall_assign_door_1(void);

commit 4d72f88ae164900394194479a6e44d3522ab9f00

Preprocess out PHYSFSX_writeAngleVec
This is unused now that jointpos_write is preprocessed out.

Fixes: ed557636033a6611309405fbe1b59017eccaf53a ("Remove unused symbols")

similar/main/robot.cpp
@@ -43,12 +43,14 @@ array<robot_info, MAX_ROBOT_TYPES> Robot_info;
 //Big array of joint positions.  All robots index into this array
 array<jointpos, MAX_ROBOT_JOINTS> Robot_joints;
 
+#if 0
 static inline void PHYSFSX_writeAngleVec(PHYSFS_File *fp, const vms_angvec &v)
 {
 	PHYSFS_writeSLE16(fp, v.p);
 	PHYSFS_writeSLE16(fp, v.b);
 	PHYSFS_writeSLE16(fp, v.h);
 }
+#endif
 
 //given an object and a gun number, return position in 3-space of gun
 //fills in gun_point