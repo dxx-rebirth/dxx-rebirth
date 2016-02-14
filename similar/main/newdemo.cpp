dxx-rebirth/dxx-rebirth

commit 553b31309946a6c2d201e17ddac53c4652baf243

Use vwallptr

similar/main/newdemo.cpp
@@ -1524,8 +1524,9 @@ void newdemo_set_new_level(int level_num)
 	if (nd_record_v_juststarted==1)
 	{
 		nd_write_int(Num_walls);
-		range_for (auto &w, partial_const_range(Walls, Num_walls))
+		range_for (const auto &&wp, vcwallptr)
 		{
+			auto &w = *wp;
 			nd_write_byte (w.type);
 			nd_write_byte (w.flags);
 			nd_write_byte (w.state);
@@ -1562,8 +1563,9 @@ static void newdemo_record_oneframeevent_update(int wallupdate)
 	// This will record tmaps for all walls and properly show doors which were opened before demo recording started.
 	if (wallupdate)
 	{
-		range_for (auto &w, partial_const_range(Walls, Num_walls))
+		range_for (const auto &&wp, vcwallptr)
 		{
+			auto &w = *wp;
 			int side;
 			auto seg = &Segments[w.segnum];
 			side = w.sidenum;
@@ -1865,13 +1867,13 @@ static int newdemo_read_demo_start(enum purpose_type purpose)
 
 static void newdemo_pop_ctrlcen_triggers()
 {
-	int anim_num, n;
+	int n;
 	for (int i = 0; i < ControlCenterTriggers.num_links; i++) {
 		const auto &&seg = vsegptridx(ControlCenterTriggers.seg[i]);
 		const auto side = ControlCenterTriggers.side[i];
 		const auto &&csegp = vsegptr(seg->children[side]);
 		auto cside = find_connect_side(seg, csegp);
-		anim_num = Walls[seg->sides[side].wall_num].clip_num;
+		const auto anim_num = vcwallptr(seg->sides[side].wall_num)->clip_num;
 		n = WallAnims[anim_num].num_frames;
 		if (WallAnims[anim_num].flags & WCF_TMAP1)	{
 		seg->sides[side].tmap_num = csegp->sides[cside].tmap_num = WallAnims[anim_num].frames[n-1];
@@ -2183,7 +2185,7 @@ static int newdemo_read_frame_information(int rewrite)
                         if (segp->sides[side].wall_num != wall_none)
                         {
 #if defined(DXX_BUILD_DESCENT_II)
-							const auto &&t = vctrgptr(Walls[segp->sides[side].wall_num].trigger);
+							const auto &&t = vctrgptr(vcwallptr(segp->sides[side].wall_num)->trigger);
 							if (t->type == TT_SECRET_EXIT)
 							{
                                         int truth;
@@ -2915,11 +2917,10 @@ static int newdemo_read_frame_information(int rewrite)
 				break;
 			}
 			if ((Newdemo_vcr_state == ND_STATE_REWINDING) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEBACKWARD)) {
-				int anim_num;
 				const auto &&segp = vsegptridx(segnum);
 				const auto &&csegp = vsegptr(segp->children[side]);
 				const auto &&cside = find_connect_side(segp, csegp);
-				anim_num = Walls[segp->sides[side].wall_num].clip_num;
+				const auto anim_num = vwallptr(segp->sides[side].wall_num)->clip_num;
 
 				if (WallAnims[anim_num].flags & WCF_TMAP1) {
 					segp->sides[side].tmap_num = csegp->sides[cside].tmap_num = WallAnims[anim_num].frames[0];
@@ -2955,9 +2956,8 @@ static int newdemo_read_frame_information(int rewrite)
 #if defined(DXX_BUILD_DESCENT_II)
 		case ND_EVENT_CLOAKING_WALL: {
 			sbyte type,state,cloak_value;
-			ubyte back_wall_num,front_wall_num;
+			wallnum_t back_wall_num, front_wall_num;
 			short l0,l1,l2,l3;
-			int sidenum;
 
 			nd_read_byte((sbyte*)&front_wall_num);
 			nd_read_byte((sbyte*)&back_wall_num);
@@ -2982,26 +2982,28 @@ static int newdemo_read_frame_information(int rewrite)
 				break;
 			}
 
-			Walls[front_wall_num].type = type;
-			Walls[front_wall_num].state = state;
-			Walls[front_wall_num].cloak_value = cloak_value;
-			auto segp = &Segments[Walls[front_wall_num].segnum];
-			sidenum = Walls[front_wall_num].sidenum;
-			segp->sides[sidenum].uvls[0].l = ((int) l0) << 8;
-			segp->sides[sidenum].uvls[1].l = ((int) l1) << 8;
-			segp->sides[sidenum].uvls[2].l = ((int) l2) << 8;
-			segp->sides[sidenum].uvls[3].l = ((int) l3) << 8;
-
-			Walls[back_wall_num].type = type;
-			Walls[back_wall_num].state = state;
-			Walls[back_wall_num].cloak_value = cloak_value;
-			segp = &Segments[Walls[back_wall_num].segnum];
-			sidenum = Walls[back_wall_num].sidenum;
-			segp->sides[sidenum].uvls[0].l = ((int) l0) << 8;
-			segp->sides[sidenum].uvls[1].l = ((int) l1) << 8;
-			segp->sides[sidenum].uvls[2].l = ((int) l2) << 8;
-			segp->sides[sidenum].uvls[3].l = ((int) l3) << 8;
-
+			{
+				auto &w = *vwallptr(front_wall_num);
+				w.type = type;
+				w.state = state;
+				w.cloak_value = cloak_value;
+				auto &uvl = vsegptr(w.segnum)->sides[w.sidenum].uvls;
+				uvl[0].l = ((int) l0) << 8;
+				uvl[1].l = ((int) l1) << 8;
+				uvl[2].l = ((int) l2) << 8;
+				uvl[3].l = ((int) l3) << 8;
+			}
+			{
+				auto &w = *vwallptr(back_wall_num);
+				w.type = type;
+				w.state = state;
+				w.cloak_value = cloak_value;
+				auto &uvl = vsegptr(w.segnum)->sides[w.sidenum].uvls;
+				uvl[0].l = ((int) l0) << 8;
+				uvl[1].l = ((int) l1) << 8;
+				uvl[2].l = ((int) l2) << 8;
+				uvl[3].l = ((int) l3) << 8;
+			}
 			break;
 		}
 #endif
@@ -3059,9 +3061,10 @@ static int newdemo_read_frame_information(int rewrite)
 				Walls.set_count(num_walls);
 				if (rewrite)
 					nd_write_int (Num_walls);
-				range_for (auto &w, partial_range(Walls, Num_walls))
+				range_for (const auto &&wp, vwallptr)
 				// restore the walls
 				{
+					auto &w = *wp;
 					nd_read_byte ((signed char *)&w.type);
 					nd_read_byte ((signed char *)&w.flags);
 					nd_read_byte ((signed char *)&w.state);

common/include/valptridx.h
@@ -316,6 +316,7 @@ class valptridx<managed_type>::basic_ptr :
 public:
 	using policy::allow_nullptr;
 	using policy::check_allowed_invalid_index;
+	using index_type = typename containing_type::index_type;
 	using const_pointer_type = typename containing_type::const_pointer_type;
 	using mutable_pointer_type = typename containing_type::mutable_pointer_type;
 	using typename policy::array_managed_type;
@@ -484,7 +485,7 @@ public:
 	typedef basic_ptr<policy, 1> vptr_type;
 	typedef basic_idx<policy, 1> vidx_type;
 	using typename vidx_type::array_managed_type;
-	using typename vidx_type::index_type;
+	using index_type = typename vidx_type::index_type;
 	using typename vidx_type::integral_type;
 	using typename vptr_type::pointer_type;
 	using vidx_type::operator==;
@@ -709,6 +710,7 @@ public:
 };
 
 #define _DEFINE_VALPTRIDX_SUBTYPE_USERTYPE(N,P,I,A,prefix,Pconst)	\
+	constexpr valptridx<P>::basic_ptridx_global_factory<prefix##ptr_t> prefix##ptr{};	\
 	constexpr valptridx<P>::basic_vptr_global_factory<v##prefix##ptr_t> v##prefix##ptr{};	\
 	constexpr valptridx<P>::basic_ptridx_global_factory<prefix##ptridx_t> prefix##ptridx{};	\
 	constexpr valptridx<P>::basic_vptr_global_factory<v##prefix##ptridx_t> v##prefix##ptridx{};	\

common/main/wall.h
@@ -145,7 +145,7 @@ namespace dcx {
 
 struct active_door : public prohibit_void_ptr<active_door>
 {
-	int     n_parts;            // for linked walls
+	unsigned n_parts;            // for linked walls
 	array<wallnum_t, 2>   front_wallnum;   // front wall numbers for this door
 	array<wallnum_t, 2>   back_wallnum;    // back wall numbers for this door
 	fix     time;               // how long been opening, closing, waiting
@@ -165,6 +165,8 @@ struct cloaking_wall : public prohibit_void_ptr<cloaking_wall>
 	fix     time;               // how long been cloaking or decloaking
 };
 #endif
+
+DEFINE_VALPTRIDX_SUBTYPE(wall, wall, wallnum_t, Walls);
 
 struct wclip : public prohibit_void_ptr<wclip>
 {

d2x-rebirth/main/escort.cpp
@@ -248,7 +248,8 @@ static int ok_for_buddy_to_talk(void)
 		auto	wall_num = segp->sides[i].wall_num;
 
 		if (wall_num != wall_none) {
-			if ((Walls[wall_num].type == WALL_BLASTABLE) && !(Walls[wall_num].flags & WALL_BLASTED))
+			auto &w = *vcwallptr(wall_num);
+			if (w.type == WALL_BLASTABLE && !(w.flags & WALL_BLASTED))
 				return 0;
 		}
 
@@ -260,7 +261,8 @@ static int ok_for_buddy_to_talk(void)
 			{
 				auto wall2 = j.wall_num;
 				if (wall2 != wall_none) {
-					if ((Walls[wall2].type == WALL_BLASTABLE) && !(Walls[wall2].flags & WALL_BLASTED))
+					auto &w = *vcwallptr(wall2);
+					if (w.type == WALL_BLASTABLE && !(w.flags & WALL_BLASTED))
 						return 0;
 				}
 			}

similar/editor/eswitch.cpp
@@ -85,19 +85,21 @@ static trgnum_t add_trigger(const vsegptr_t seg, short side)
 	if (trigger_num>=MAX_TRIGGERS) return trigger_none;
 
 	auto wall_num = seg->sides[side].wall_num;
+	trgnum_t *wt;
 	if (wall_num == wall_none) {
 		wall_add_to_markedside(WALL_OPEN);
 		wall_num = seg->sides[side].wall_num;
-		Walls[wall_num].trigger = trigger_num;
-		
+		wt = &vwallptr(wall_num)->trigger;
 		// Set default values first time trigger is added
 	} else {
-		if (Walls[wall_num].trigger != trigger_none)
-			return Walls[wall_num].trigger;
+		auto &w = *vwallptr(wall_num);
+		if (w.trigger != trigger_none)
+			return w.trigger;
 
 		// Create new trigger.
-		Walls[wall_num].trigger = trigger_num;
+		wt = &w.trigger;
 	}
+	*wt = trigger_num;
 	const auto &&t = vtrgptr(trigger_num);
 	t->flags = {};
 	t->value = F1_0*5;
@@ -125,7 +127,7 @@ static int trigger_flag_Markedside(const TRIGGER_FLAG flag, const int value)
 	// If no wall just return
 	auto wall_num = Markedsegp->sides[Markedside].wall_num;
 	if (!value && wall_num == wall_none) return 0;
-	auto trigger_num = value ? add_trigger(Markedsegp, Markedside) : Walls[wall_num].trigger;
+	const auto trigger_num = value ? add_trigger(Markedsegp, Markedside) : vcwallptr(wall_num)->trigger;
 
 	if (trigger_num == trigger_none) {
 		editor_status(value ? "Cannot add trigger at Markedside." : "No trigger at Markedside.");
@@ -153,7 +155,7 @@ static int bind_matcen_to_trigger() {
 		editor_status("No wall at Markedside.");
 		return 0;
 	}
-	auto trigger_num = Walls[wall_num].trigger;
+	const auto trigger_num = vcwallptr(wall_num)->trigger;
 	if (trigger_num == trigger_none) {
 		editor_status("No trigger at Markedside.");
 		return 0;
@@ -196,7 +198,7 @@ int bind_wall_to_trigger() {
 		editor_status("No wall at Markedside.");
 		return 0;
 	}
-	auto trigger_num = Walls[wall_num].trigger;
+	const auto trigger_num = vcwallptr(wall_num)->trigger;
 	if (trigger_num == trigger_none) {
 		editor_status("No trigger at Markedside.");
 		return 0;
@@ -239,12 +241,13 @@ int remove_trigger_num(int trigger_num)
 		Triggers.set_count(Num_triggers - 1);
 		std::move(std::next(r.begin()), r.end(), r.begin());
 	
-		range_for (auto &w, partial_range(Walls, Num_walls))
+		range_for (const auto &&w, vwallptr)
 		{
-			if (w.trigger == trigger_num)
-				w.trigger = trigger_none;	// a trigger can be shared by multiple walls
-			else if (w.trigger > trigger_num) 
-				w.trigger--;
+			auto &trigger = w->trigger;
+			if (trigger == trigger_num)
+				trigger = trigger_none;	// a trigger can be shared by multiple walls
+			else if (trigger > trigger_num) 
+				--trigger;
 		}
 
 		return 1;
@@ -262,7 +265,7 @@ int remove_trigger(const vsegptr_t seg, short side)
 		return 0;
 	}
 
-	return remove_trigger_num(Walls[wall_num].trigger);
+	return remove_trigger_num(vcwallptr(wall_num)->trigger);
 }
 
 static int trigger_remove()
@@ -380,7 +383,7 @@ int trigger_dialog_handler(UI_DIALOG *dlg,const d_event &event, trigger_dialog *
 	// of the checkboxes that control the wall flags.  
 	//------------------------------------------------------------
 	const auto Markedwall = Markedsegp->sides[Markedside].wall_num;
-	auto trigger_num = (Markedwall != wall_none) ? Walls[Markedwall].trigger : trigger_none;
+	const auto trigger_num = (Markedwall != wall_none) ? vcwallptr(Markedwall)->trigger : trigger_none;
 
 	if (t->old_trigger_num != trigger_num)
 	{
@@ -443,7 +446,7 @@ int trigger_dialog_handler(UI_DIALOG *dlg,const d_event &event, trigger_dialog *
 	{
 		gr_set_current_canvas( t->wallViewBox->canvas );
 
-		if (Markedsegp->sides[Markedside].wall_num == wall_none || Walls[Markedsegp->sides[Markedside].wall_num].trigger == trigger_none)
+		if (Markedsegp->sides[Markedside].wall_num == wall_none || vcwallptr(Markedsegp->sides[Markedside].wall_num)->trigger == trigger_none)
 			gr_clear_canvas( CBLACK );
 		else {
 			if (Markedsegp->sides[Markedside].tmap_num2 > 0)  {

similar/editor/meddraw.cpp
@@ -447,11 +447,12 @@ static void draw_wall_side(const vcsegptr_t seg,int side)
 // Draws special walls (for now these are just removable walls.)
 static void draw_special_wall(const vcsegptr_t seg, int side )
 {
+	auto &w = *vcwallptr(seg->sides[side].wall_num);
 	const auto get_color = [=]() {
-		const auto type = Walls[seg->sides[side].wall_num].type;
+		const auto type = w.type;
 		if (type != WALL_OPEN)
 		{
-			const auto flags = Walls[seg->sides[side].wall_num].flags;
+			const auto flags = w.flags;
 			if (flags & WALL_DOOR_LOCKED)
 				return (flags & WALL_DOOR_AUTO) ? WALL_AUTO_DOOR_LOCKED_COLOR : WALL_DOOR_LOCKED_COLOR;
 			if (flags & WALL_DOOR_AUTO)
@@ -470,7 +471,8 @@ static void draw_special_wall(const vcsegptr_t seg, int side )
 
 	draw_wall_side(seg,side);
 
-	if (Walls[seg->sides[side].wall_num].trigger != trigger_none) {
+	if (w.trigger != trigger_none)
+	{
 		gr_setcolor(TRIGGER_COLOR);
 		draw_trigger_side(seg,side);
 	}

similar/editor/segment.cpp
@@ -642,9 +642,9 @@ static void compress_segments(void)
 					g.segments.replace(seg, hole);
 
 				// Fix walls
-				range_for (auto &w, partial_range(Walls, Num_walls))
-					if (w.segnum == seg)
-						w.segnum = hole;
+				range_for (const auto &&w, vwallptr)
+					if (w->segnum == seg)
+						w->segnum = hole;
 
 				// Fix fuelcenters, robotcens, and triggers... added 2/1/95 -Yuan
 				range_for (auto &f, partial_range(Station, Num_fuelcenters))

commit e36abb25cb0e49a2097937bd1dcc6adbf23a40ec

Fix invalid access reading demos
Casting the address of a short to `sbyte*` invites trouble.  In this
case, it allows uninitialized garbage to be used as the high bits of the
wall index.

Fixes: 553b31309946a6c2d201e17ddac53c4652baf243 ("Use vwallptr")

similar/main/newdemo.cpp
@@ -2959,8 +2959,10 @@ static int newdemo_read_frame_information(int rewrite)
 			wallnum_t back_wall_num, front_wall_num;
 			short l0,l1,l2,l3;
 
-			nd_read_byte((sbyte*)&front_wall_num);
-			nd_read_byte((sbyte*)&back_wall_num);
+			nd_read_byte(&type);
+			front_wall_num = type;
+			nd_read_byte(&type);
+			back_wall_num = type;
 			nd_read_byte(&type);
 			nd_read_byte(&state);
 			nd_read_byte(&cloak_value);