dxx-rebirth/dxx-rebirth

commit 74dfa29d318df4f3c3343021d3ad8da08f8c36b0

Convert most Trigger references to valptridx

similar/main/newdemo.cpp
@@ -2184,7 +2184,9 @@ static int newdemo_read_frame_information(int rewrite)
                         if (segp->sides[side].wall_num != wall_none)
                         {
 #if defined(DXX_BUILD_DESCENT_II)
-                                if (Triggers[Walls[segp->sides[side].wall_num].trigger].type == TT_SECRET_EXIT) {
+							const auto &&t = vctrgptr(Walls[segp->sides[side].wall_num].trigger);
+							if (t->type == TT_SECRET_EXIT)
+							{
                                         int truth;
 
                                         nd_read_byte(&c);

common/main/switch.h
@@ -33,6 +33,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "fwd-object.h"
 #include "fwd-segment.h"
 #include "fwd-valptridx.h"
+#include "valptridx.h"
 #include "compiler-array.h"
 
 #define MAX_TRIGGERS        100
@@ -137,6 +138,7 @@ struct trigger : public prohibit_void_ptr<trigger>
 };
 
 DXX_VALPTRIDX_DECLARE_GLOBAL_SUBTYPE(trigger, trg, Triggers, MAX_TRIGGERS);
+DEFINE_VALPTRIDX_SUBTYPE(trg, trigger, trgnum_t, Triggers);
 
 }
 
@@ -146,7 +148,7 @@ const uint8_t trigger_none = 0xff;
 
 extern void trigger_init();
 void check_trigger(vcsegptridx_t seg, short side, vcobjptridx_t objnum, int shot);
-int check_trigger_sub(unsigned trigger_num, int player_num,int shot);
+int check_trigger_sub(trgnum_t trigger_num, int player_num,int shot);
 extern void triggers_frame_process();
 
 static inline int trigger_is_exit(const trigger *t)

similar/editor/eswitch.cpp
@@ -77,9 +77,9 @@ struct trigger_dialog
 //-----------------------------------------------------------------
 // Adds a trigger to wall, and returns the trigger number. 
 // If there is a trigger already present, it returns the trigger number. (To be replaced)
-static int add_trigger(const vsegptr_t seg, short side)
+static trgnum_t add_trigger(const vsegptr_t seg, short side)
 {
-	int trigger_num = Num_triggers;
+	trgnum_t trigger_num = Num_triggers;
 
 	Assert(trigger_num < MAX_TRIGGERS);
 	if (trigger_num>=MAX_TRIGGERS) return trigger_none;
@@ -91,23 +91,18 @@ static int add_trigger(const vsegptr_t seg, short side)
 		Walls[wall_num].trigger = trigger_num;
 		
 		// Set default values first time trigger is added
-		Triggers[trigger_num].flags = 0;
-		Triggers[trigger_num].value = F1_0*5;
-		Triggers[trigger_num].num_links = 0;
-		Triggers[trigger_num].flags &= TRIGGER_ON;		
 	} else {
 		if (Walls[wall_num].trigger != trigger_none)
 			return Walls[wall_num].trigger;
 
 		// Create new trigger.
 		Walls[wall_num].trigger = trigger_num;
-
-		// Set default values first time trigger is added
-		Triggers[trigger_num].flags = 0;
-		Triggers[trigger_num].value = F1_0*5;
-		Triggers[trigger_num].num_links = 0;
-		Triggers[trigger_num].flags &= TRIGGER_ON;
 	}
+	const auto &&t = vtrgptr(trigger_num);
+	t->flags = 0;
+	t->value = F1_0*5;
+	t->num_links = 0;
+	t->flags &= TRIGGER_ON;
 	Triggers.set_count(trigger_num + 1);
 	return trigger_num;
 }		
@@ -137,10 +132,11 @@ static int trigger_flag_Markedside(short flag, int value)
 		return 0;
 	}
 
+	auto &flags = vtrgptr(trigger_num)->flags;
  	if (value)
-		Triggers[trigger_num].flags |= flag;
+		flags |= flag;
 	else
-		Triggers[trigger_num].flags &= ~flag;
+		flags &= ~flag;
 
 	return 1;
 }
@@ -169,16 +165,18 @@ static int bind_matcen_to_trigger() {
 		return 0;
 	}
 
-	auto link_num = Triggers[trigger_num].num_links;
+	const auto &&t = vtrgptr(trigger_num);
+	const auto link_num = t->num_links;
 	for (int i=0;i<link_num;i++)
-		if (Cursegp == Triggers[trigger_num].seg[i]) {
+		if (Cursegp == t->seg[i])
+		{
 			editor_status("Matcen already bound to Markedside.");
 			return 0;
 		}
 
 	// Error checking completed, actual binding begins
-	Triggers[trigger_num].seg[link_num] = Cursegp;
-	Triggers[trigger_num].num_links++;
+	t->seg[link_num] = Cursegp;
+	t->num_links++;
 
 	editor_status("Matcen linked to trigger");
 
@@ -214,18 +212,19 @@ int bind_wall_to_trigger() {
 		return 0;
 	}
 
-	auto link_num = Triggers[trigger_num].num_links;
+	const auto &&t = vtrgptr(trigger_num);
+	const auto link_num = t->num_links;
 	for (int i=0;i<link_num;i++)
-		if (Cursegp == Triggers[trigger_num].seg[i] && Curside == Triggers[trigger_num].side[i])
+		if (Cursegp == t->seg[i] && Curside == t->side[i])
 		{
 			editor_status("Curside already bound to Markedside.");
 			return 0;
 		}
 
 	// Error checking completed, actual binding begins
-	Triggers[trigger_num].seg[link_num] = Cursegp;
-	Triggers[trigger_num].side[link_num] = Curside;
-	Triggers[trigger_num].num_links++;
+	t->seg[link_num] = Cursegp;
+	t->side[link_num] = Curside;
+	t->num_links++;
 
 	editor_status("Wall linked to trigger");
 
@@ -388,7 +387,7 @@ int trigger_dialog_handler(UI_DIALOG *dlg,const d_event &event, trigger_dialog *
 	{
 		if (trigger_num != trigger_none)
 		{
-			trigger *trig = &Triggers[trigger_num];
+			const auto &&trig = vctrgptr(trigger_num);
 
   			ui_checkbox_check(t->triggerFlag[0].get(), trig->flags & TRIGGER_CONTROL_DOORS);
  			ui_checkbox_check(t->triggerFlag[1].get(), trig->flags & TRIGGER_SHIELD_DAMAGE);

similar/main/automap.cpp
@@ -1284,7 +1284,7 @@ static void add_segment_edges(automap *am, const vcsegptridx_t seg)
 		
 #if defined(DXX_BUILD_DESCENT_II)
 			auto trigger_num = Walls[seg->sides[sn].wall_num].trigger;
-			if (trigger_num != trigger_none && Triggers[trigger_num].type == TT_SECRET_EXIT)
+			if (trigger_num != trigger_none && vtrgptr(trigger_num)->type == TT_SECRET_EXIT)
 				{
 			    color = BM_XRGB( 29, 0, 31 );
 				 no_fade=1;

similar/main/dumpmine.cpp
@@ -186,21 +186,24 @@ static void warning_printf(PHYSFS_File *my_file, const char * format, ... )
 // ----------------------------------------------------------------------------
 static void write_exit_text(PHYSFS_File *my_file)
 {
-	int	i, j, count;
+	int	j, count;
 
 	PHYSFSX_printf(my_file, "-----------------------------------------------------------------------------\n");
 	PHYSFSX_printf(my_file, "Exit stuff\n");
 
 	//	---------- Find exit triggers ----------
 	count=0;
-	for (i=0; i<Num_triggers; i++)
-		if (trigger_is_exit(&Triggers[i])) {
+	for (trgnum_t i=0; i<Num_triggers; i++)
+	{
+		const auto &&t = vctrgptr(i);
+		if (trigger_is_exit(t))
+		{
 			int	count2;
 
-			PHYSFSX_printf(my_file, "Trigger %2i, is an exit trigger with %i links.\n", i, Triggers[i].num_links);
+			PHYSFSX_printf(my_file, "Trigger %2i, is an exit trigger with %i links.\n", i, t->num_links);
 			count++;
-			if (Triggers[i].num_links != 0)
-				err_printf(my_file, "Error: Exit triggers must have 0 links, this one has %i links.", Triggers[i].num_links);
+			if (t->num_links != 0)
+				err_printf(my_file, "Error: Exit triggers must have 0 links, this one has %i links.", t->num_links);
 
 			//	Find wall pointing to this trigger.
 			count2 = 0;
@@ -215,6 +218,7 @@ static void write_exit_text(PHYSFS_File *my_file)
 				err_printf(my_file, "Error: Trigger %i is bound to %i walls.", i, count2);
 
 		}
+	}
 
 	if (count == 0)
 		err_printf(my_file, "Error: No exit trigger in this mine.");
@@ -475,7 +479,7 @@ static void write_segment_text(PHYSFS_File *my_file)
 // which is not true.  The setting of segnum is bogus.
 static void write_matcen_text(PHYSFS_File *my_file)
 {
-	int	i, j;
+	int	i;
 
 	PHYSFSX_printf(my_file, "-----------------------------------------------------------------------------\n");
 	PHYSFSX_printf(my_file, "Materialization centers:\n");
@@ -492,10 +496,12 @@ static void write_matcen_text(PHYSFS_File *my_file)
 		auto segnum = Station[fuelcen_num].segnum;
 
 		//	Find trigger for this materialization center.
-		for (j=0; j<Num_triggers; j++) {
-			if (trigger_is_matcen(Triggers[j]))
+		for (trgnum_t j=0; j < Num_triggers; j++)
+		{
+			const auto &&t = vctrgptr(j);
+			if (trigger_is_matcen(t))
 			{
-				range_for (auto &k, partial_range(Triggers[j].seg, Triggers[j].num_links))
+				range_for (auto &k, partial_range(t->seg, t->num_links))
 					if (k == segnum)
 					{
 						PHYSFSX_printf(my_file, "Trigger = %2i  ", j );
@@ -583,21 +589,23 @@ static void write_player_text(PHYSFS_File *my_file)
 // ----------------------------------------------------------------------------
 static void write_trigger_text(PHYSFS_File *my_file)
 {
-	int	i, j, w;
+	int	w;
 
 	PHYSFSX_printf(my_file, "-----------------------------------------------------------------------------\n");
 	PHYSFSX_printf(my_file, "Triggers:\n");
-	for (i=0; i<Num_triggers; i++) {
+	for (trgnum_t i = 0; i < Num_triggers; i++)
+	{
+		const auto &&t = vctrgptr(i);
 #if defined(DXX_BUILD_DESCENT_I)
 		PHYSFSX_printf(my_file, "Trigger %03i: flags=%04x, value=%08x, time=%8x, linknum=%i, num_links=%i ", i, 
-                        Triggers[i].flags, (unsigned int) (Triggers[i].value), (unsigned int) (Triggers[i].time), Triggers[i].link_num, Triggers[i].num_links);
+                        t->flags, static_cast<unsigned>(t->value), static_cast<unsigned>(t->time), t->link_num, t->num_links);
 #elif defined(DXX_BUILD_DESCENT_II)
 		PHYSFSX_printf(my_file, "Trigger %03i: type=%02x flags=%04x, value=%08x, time=%8x, num_links=%i ", i,
-			Triggers[i].type, Triggers[i].flags, Triggers[i].value, Triggers[i].time, Triggers[i].num_links);
+			t->type, t->flags, t->value, t->time, t->num_links);
 #endif
 
-		for (j=0; j<Triggers[i].num_links; j++)
-			PHYSFSX_printf(my_file, "[%03i:%i] ", Triggers[i].seg[j], Triggers[i].side[j]);
+		for (unsigned j = 0; j < t->num_links; ++j)
+			PHYSFSX_printf(my_file, "[%03i:%i] ", t->seg[j], t->side[j]);
 
 		//	Find which wall this trigger is connected to.
 		for (w=0; w<Num_walls; w++)

similar/main/gamecntl.cpp
@@ -1131,8 +1131,11 @@ static void kill_and_so_forth(void)
 
 	do_controlcen_destroyed_stuff(object_none);
 
-	for (uint_fast32_t i = 0; i < Num_triggers; i++) {
-		if (trigger_is_exit(&Triggers[i])) {
+	for (trgnum_t i = 0; i < Num_triggers; i++)
+	{
+		const auto &&t = vctrgptr(i);
+		if (trigger_is_exit(t))
+		{
 			range_for (auto &w, partial_range(Walls, Num_walls))
 			{
 				if (w.trigger == i) {

similar/main/gamesave.cpp
@@ -1088,21 +1088,24 @@ static int load_game_data(PHYSFS_File *LoadFile)
 			w.controlling_trigger = -1;
 #endif
 
-		for (uint_fast32_t t = 0; t < Num_triggers; t++) {
-			int	l;
-			for (l=0; l<Triggers[t].num_links; l++) {
+		for (trgnum_t t = 0; t < Num_triggers; t++)
+		{
+			auto &tr = *vctrgptr(t);
+			for (unsigned l = 0; l < tr.num_links; ++l)
+			{
 				//check to see that if a trigger requires a wall that it has one,
 				//and if it requires a matcen that it has one
-				const auto seg_num = Triggers[t].seg[l];
-				if (trigger_is_matcen(Triggers[t]))
+				const auto seg_num = tr.seg[l];
+				if (trigger_is_matcen(tr))
 				{
 					if (Segments[seg_num].special != SEGMENT_IS_ROBOTMAKER)
 						throw std::runtime_error("matcen triggers non-matcen segment");
 				}
 #if defined(DXX_BUILD_DESCENT_II)
-				else if (Triggers[t].type != TT_LIGHT_OFF && Triggers[t].type != TT_LIGHT_ON) {	//light triggers don't require walls
-					int side_num = Triggers[t].side[l];
-					auto wall_num = Segments[seg_num].sides[side_num].wall_num;
+				else if (tr.type != TT_LIGHT_OFF && tr.type != TT_LIGHT_ON)
+				{	//light triggers don't require walls
+					const auto side_num = tr.side[l];
+					auto wall_num = vsegptr(seg_num)->sides[side_num].wall_num;
 					if (wall_num == wall_none)
 						Int3();	//	This is illegal.  This trigger requires a wall
 					else

similar/main/multi.cpp
@@ -4679,9 +4679,10 @@ void multi_send_trigger_specific (const playernum_t pnum,char trig)
 
 	multi_send_data_direct<MULTI_START_TRIGGER>(multibuf, 2, pnum, 2);
 }
+
 static void multi_do_start_trigger (const ubyte *buf)
 {
-	Triggers[(int)buf[1]].flags |=TF_DISABLED;
+	vtrgptr(static_cast<trgnum_t>(buf[1]))->flags |= TF_DISABLED;
 }
 #endif
 

commit 90de22b2a5bffababb4780fd0f186a47e8dba47b

Fix invalid access reading demos
Fixes: 74dfa29d318df4f3c3343021d3ad8da08f8c36b0 ("Convert most Trigger references to valptridx")

similar/main/newdemo.cpp
@@ -2181,12 +2181,16 @@ static int newdemo_read_frame_information(int rewrite)
                         /* Demo recording is buggy.  Descent records
                             * ND_EVENT_TRIGGER for every segment transition, even
                             * if there is no wall.
+							*
+							* Likewise, ND_EVENT_TRIGGER can be recorded
+							* when the wall is valid, but there is no
+							* trigger on the wall.
                             */
                         if (segp->sides[side].wall_num != wall_none)
                         {
 #if defined(DXX_BUILD_DESCENT_II)
-							const auto &&t = vctrgptr(vcwallptr(segp->sides[side].wall_num)->trigger);
-							if (t->type == TT_SECRET_EXIT)
+							auto &w = *vcwallptr(segp->sides[side].wall_num);
+							if (w.trigger != trigger_none && vctrgptr(w.trigger)->type == TT_SECRET_EXIT)
 							{
                                         int truth;
 