dxx-rebirth/dxx-rebirth

commit 5b9ccda8e85864367fcf5ecc3330301aaa12a20c

Simplify update_flicker

similar/main/game.cpp
@@ -1668,37 +1668,31 @@ static void flicker_lights()
 static std::pair<Flickering_light_array_t::iterator, Flickering_light_array_t::iterator> find_flicker(segnum_t segnum, int sidenum)
 {
 	//see if there's already an entry for this seg/side
-	auto pr = partial_range(Flickering_lights, Num_flickering_lights);
+	const auto &&pr = partial_range(Flickering_lights, Num_flickering_lights);
 	auto predicate = [segnum, sidenum](const flickering_light &f) {
 		return f.segnum == segnum && f.sidenum == sidenum;	//found it!
 	};
 	return {std::find_if(pr.begin(), pr.end(), predicate), pr.end()};
 }
 
-template <typename F>
-static inline void update_flicker(segnum_t segnum, int sidenum, F f)
+template <fix timer>
+static inline void update_flicker(segnum_t segnum, int sidenum)
 {
 	auto i = find_flicker(segnum, sidenum);
 	if (i.first != i.second)
-		f(*i.first);
+		i.first->timer = timer;
 }
 
 //turn flickering off (because light has been turned off)
 void disable_flicker(segnum_t segnum,int sidenum)
 {
-	auto F = [](flickering_light &f) {
-		f.timer = 0x80000000;
-	};
-	update_flicker(segnum, sidenum, F);
+	update_flicker<0x80000000>(segnum, sidenum);
 }
 
 //turn flickering off (because light has been turned on)
 void enable_flicker(segnum_t segnum,int sidenum)
 {
-	auto F = [](flickering_light &f) {
-		f.timer = 0;
-	};
-	update_flicker(segnum, sidenum, F);
+	update_flicker<0>(segnum, sidenum);
 }
 #endif
 

commit ea8ca1d1e231e19ef062826b0c086e22eda1c6e1

Pass update_flicker timer as argument
Clang fails to compile update_flicker<0x80000000>(...); gcc handles it
properly.  A template is not necessary here, so switch to passing the
value as an argument.  Both compilers accept this.

Reported-by: kreatordxx <private mail>
Fixes: 5b9ccda8e85864367fcf5ecc3330301aaa12a20c ("Simplify update_flicker")

similar/main/game.cpp
@@ -1633,11 +1633,13 @@ static void slide_textures(void)
 Flickering_light_array_t Flickering_lights;
 unsigned Num_flickering_lights;
 
+constexpr fix flicker_timer_disabled = 0x80000000;
+
 static void flicker_lights()
 {
 	range_for (auto &f, partial_range(Flickering_lights, Num_flickering_lights))
 	{
-		if (static_cast<unsigned>(f.timer) == 0x80000000)		//disabled
+		if (f.timer == flicker_timer_disabled)		//disabled
 			continue;
 		const auto &&segp = vsegptridx(f.segnum);
 		const auto sidenum = f.sidenum;
@@ -1675,8 +1677,7 @@ static std::pair<Flickering_light_array_t::iterator, Flickering_light_array_t::i
 	return {std::find_if(pr.begin(), pr.end(), predicate), pr.end()};
 }
 
-template <fix timer>
-static inline void update_flicker(segnum_t segnum, int sidenum)
+static void update_flicker(const segnum_t segnum, const unsigned sidenum, const fix timer)
 {
 	auto i = find_flicker(segnum, sidenum);
 	if (i.first != i.second)
@@ -1686,13 +1687,13 @@ static inline void update_flicker(segnum_t segnum, int sidenum)
 //turn flickering off (because light has been turned off)
 void disable_flicker(segnum_t segnum,int sidenum)
 {
-	update_flicker<0x80000000>(segnum, sidenum);
+	update_flicker(segnum, sidenum, flicker_timer_disabled);
 }
 
 //turn flickering off (because light has been turned on)
 void enable_flicker(segnum_t segnum,int sidenum)
 {
-	update_flicker<0>(segnum, sidenum);
+	update_flicker(segnum, sidenum, 0);
 }
 #endif
 