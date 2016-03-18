dxx-rebirth/dxx-rebirth

commit 7f18afd5d7a0fd4146a4ead16e3de09bc1684bdf

Client-side implementation for Tracker support by Matt 1360 Vandermeulen including improvements in udp_dns_filladdr and IPv4/IPv6 compability; Very little adjustments by me, too including IPv6 support for Windows (untested); Actual tracker code will follow later as seperate branch when it's done

SConstruct
@@ -33,6 +33,7 @@ sdlmixer = int(ARGUMENTS.get('sdlmixer', 1))
 ipv6 = int(ARGUMENTS.get('ipv6', 0))
 use_udp = int(ARGUMENTS.get('use_udp', 1))
 use_ipx = int(ARGUMENTS.get('use_ipx', 1))
+use_tracker = int(ARGUMENTS.get('use_tracker', 1))
 verbosebuild = int(ARGUMENTS.get('verbosebuild', 0))
 
 # endianess-checker
@@ -313,7 +314,7 @@ if sys.platform == 'win32':
 		common_sources += ['arch/win32/ipx.c']
 	common_sources += ['arch/win32/messagebox.c']
 	ogllibs = ''
-	libs += ['glu32', 'wsock32', 'winmm', 'mingw32', 'SDLmain', 'SDL']
+	libs += ['glu32', 'wsock32', 'ws2_32', 'winmm', 'mingw32', 'SDLmain', 'SDL']
 	lflags = '-mwindows  arch/win32/d2xr.res'
 elif sys.platform == 'darwin':
 	print "compiling on Mac OS X"
@@ -426,6 +427,10 @@ if (ipv6 == 1):
 if (use_udp == 1):
 	env.Append(CPPDEFINES = ['USE_UDP'])
 	common_sources += ['main/net_udp.c']
+	
+	# Tracker support?  (Relies on UDP)
+	if( use_tracker == 1 ):
+		env.Append( CPPDEFINES = [ 'USE_TRACKER' ] )
 
 # IPX support?
 if (use_ipx == 1):
@@ -470,6 +475,7 @@ Help(PROGRAM_NAME + ', SConstruct file help:' +
 	'ipv6=[0/1]'          enable IPv6 compability [default: 0]
 	'use_udp=[0/1]'       enable UDP support [default: 1]
 	'use_ipx=[0/1]'       enable IPX support (IPX available on Linux and Windows, only) [default: 1]
+	'use_tracker=[0/1]'   enable Tracker support (requires udp) [default :1]
 	'verbosebuild=[0/1]'  print out all compiler/linker messages during building [default: 0]
 	
 	Default values:

arch/win32/ipx.c
@@ -2,7 +2,7 @@
 /* Mostly taken from dosemu */
 #include <string.h>
 #include <stdlib.h>
-#include <winsock.h>
+#include <winsock2.h>
 #include <wsipx.h>
 #include "net_ipx.h"
 #include "console.h"

d2x.ini
@@ -1,59 +1,61 @@
  System Options:
 
-;-nonicefps         Don't free CPU-cycles
-;-maxfps <n>        Set maximum framerate (1-200)
-;-hogdir <s>        set shared data directory to <dir>
-;-nohogdir          don't try to use shared data directory
-;-use_players_dir   put player files and saved games in Players subdirectory
-;-lowmem            Lowers animation detail for better performance with low memory
-;-pilot <s>         Select this pilot automatically
-;-autodemo          Start in demo mode
-;-window            Run the game in a window
-;-nomovies          Don't play movies
-;-noborders         Do not show borders in window mode
+;-nonicefps             Don't free CPU-cycles
+;-maxfps <n>            Set maximum framerate (1-200)
+;-hogdir <s>            set shared data directory to <dir>
+;-nohogdir              don't try to use shared data directory
+;-use_players_dir       put player files and saved games in Players subdirectory
+;-lowmem                Lowers animation detail for better performance with low memory
+;-pilot <s>             Select this pilot automatically
+;-autodemo              Start in demo mode
+;-window                Run the game in a window
+;-nomovies              Don't play movies
+;-noborders             Do not show borders in window mode
 
  Controls:
 
-;-nomouse           Deactivate mouse
-;-nojoystick        Deactivate joystick
-;-nostickykeys      Make CapsLock and NumLock non-sticky so they can be used as normal keys
+;-nomouse               Deactivate mouse
+;-nojoystick            Deactivate joystick
+;-nostickykeys          Make CapsLock and NumLock non-sticky so they can be used as normal keys
 
  Sound:
 
-;-nosound           Disables sound output
-;-nomusic           Disables music output
-;-sound11k          Use 11KHz sounds
-;-nosdlmixer        Disable Sound output via SDL_mixer
+;-nosound               Disables sound output
+;-nomusic               Disables music output
+;-sound11k              Use 11KHz sounds
+;-nosdlmixer            Disable Sound output via SDL_mixer
 
  Graphics:
 
-;-lowresfont        Force to use LowRes fonts
-;-lowresgraphics    Force to use LowRes graphics
-;-lowresmovies      Play low resolution movies if available (for slow machines)
-;-gl_fixedfont      Do not scale fonts to current resolution
+;-lowresfont            Force to use LowRes fonts
+;-lowresgraphics        Force to use LowRes graphics
+;-lowresmovies          Play low resolution movies if available (for slow machines)
+;-gl_fixedfont          Do not scale fonts to current resolution
 
  Multiplayer:
 
-;-ipxnetwork <n>    Use IPX network number <n>
-;-udp_hostaddr <n>  When manually joining a game use default IP Address <n> to connect to
-;-udp_hostport <n>  When manually joining a game use default UDP Port <n> to connect to
-;-udp_myport <n>    When hosting/joining a game use default UDP Port <n> to send packets from
+;-ipxnetwork <n>        Use IPX network number <n>
+;-udp_hostaddr <n>      When manually joining a game use default IP Address <n> to connect to
+;-udp_hostport <n>      When manually joining a game use default UDP Port <n> to connect to
+;-udp_myport <n>        When hosting/joining a game use default UDP Port <n> to send packets from
+;-tracker_hostaddr <n>  Address of Tracker server to register/query games to/from (default: dxxtracker.reenigne.net)
+;-tracker_hostport <n>  Port of Tracker server to register/query games to/from (default: 42420)
 
  Debug (use only if you know what you're doing):
 
-;-debug            Enable very verbose output
-;-verbose          Shows initialization steps for tech support
-;-norun            Bail out after initialization
-;-renderstats      Enable renderstats info by default
-;-text <s>         Specify alternate .tex file
-;-tmap <s>         Select texmapper to use (c,fp,quad,i386,pent,ppro)
-;-showmeminfo      Show memory statistics
-;-nodoublebuffer   Disable Doublebuffering
-;-bigpig           Use uncompressed RLE bitmaps
-;-16bpp            Use 16Bpp instead of 32Bpp
-;-gl_oldtexmerge   Use old texmerge, uses more ram, but _might_ be a bit faster
+;-debug                Enable very verbose output
+;-verbose              Shows initialization steps for tech support
+;-norun                Bail out after initialization
+;-renderstats          Enable renderstats info by default
+;-text <s>             Specify alternate .tex file
+;-tmap <s>             Select texmapper to use (c,fp,quad,i386)
+;-showmeminfo          Show memory statistics
+;-nodoublebuffer       Disable Doublebuffering
+;-bigpig               Use uncompressed RLE bitmaps
+;-16bpp                Use 16Bpp instead of 32Bpp
+;-gl_oldtexmerge       Use old texmerge, uses more ram, but _might_ be a bit faster
 ;-gl_intensity4_ok <n> Override DbgGlIntensity4Ok - Default: 1
 ;-gl_luminance4_alpha4_ok <n> Override DbgGlLuminance4Alpha4Ok - Default: 1
-;-gl_rgba2_ok <n>  Override DbgGlRGBA2Ok - Default: 1
+;-gl_rgba2_ok <n>      Override DbgGlRGBA2Ok - Default: 1
 ;-gl_readpixels_ok <n> Override DbgGlReadPixelsOk - Default: 1
-;-gl_gettexlevelparam_ok <n>Override DbgGlGetTexLevelParamOk - Default: 1
+;-gl_gettexlevelparam_ok <n> Override DbgGlGetTexLevelParamOk - Default: 1

include/args.h
@@ -70,6 +70,10 @@ typedef struct Arg
 	const char *MplUdpHostAddr;
 	int MplUdpHostPort;
 	int MplUdpMyPort;
+#ifdef USE_TRACKER
+	const char *MplTrackerAddr;
+	int MplTrackerPort;
+#endif
 	char *EdiAutoLoad;
 	int EdiSaveHoardData;
 	int EdiMacData; // also used for some read routines in non-editor build

main/inferno.c
@@ -122,79 +122,83 @@ extern void arch_init(void);
 void print_commandline_help()
 {
 	printf( "\n System Options:\n\n");
-	printf( "  -nonicefps         %s\n", "Don't free CPU-cycles");
-	printf( "  -maxfps <n>        %s\n", "Set maximum framerate (1-200)");
-	printf( "  -hogdir <s>        %s\n", "set shared data directory to <dir>");
-	printf( "  -nohogdir          %s\n", "don't try to use shared data directory");
-	printf( "  -use_players_dir   %s\n", "put player files and saved games in Players subdirectory");
-	printf( "  -lowmem            %s\n", "Lowers animation detail for better performance with low memory");
-	printf( "  -pilot <s>         %s\n", "Select this pilot automatically");
-	printf( "  -autodemo          %s\n", "Start in demo mode");
-	printf( "  -window            %s\n", "Run the game in a window");
-	printf( "  -noborders        %s\n", "Do not show borders in window mode");
-	printf( "  -nomovies          %s\n", "Don't play movies");
+	printf( "  -nonicefps             %s\n", "Don't free CPU-cycles");
+	printf( "  -maxfps <n>            %s\n", "Set maximum framerate (1-200)");
+	printf( "  -hogdir <s>            %s\n", "set shared data directory to <dir>");
+	printf( "  -nohogdir              %s\n", "don't try to use shared data directory");
+	printf( "  -use_players_dir       %s\n", "put player files and saved games in Players subdirectory");
+	printf( "  -lowmem                %s\n", "Lowers animation detail for better performance with low memory");
+	printf( "  -pilot <s>             %s\n", "Select this pilot automatically");
+	printf( "  -autodemo              %s\n", "Start in demo mode");
+	printf( "  -window                %s\n", "Run the game in a window");
+	printf( "  -noborders             %s\n", "Do not show borders in window mode");
+	printf( "  -nomovies              %s\n", "Don't play movies");
 
 	printf( "\n Controls:\n\n");
-	printf( "  -nomouse           %s\n", "Deactivate mouse");
-	printf( "  -nojoystick        %s\n", "Deactivate joystick");
-	printf( "  -nostickykeys      %s\n", "Make CapsLock and NumLock non-sticky so they can be used as normal keys");
+	printf( "  -nomouse               %s\n", "Deactivate mouse");
+	printf( "  -nojoystick            %s\n", "Deactivate joystick");
+	printf( "  -nostickykeys          %s\n", "Make CapsLock and NumLock non-sticky so they can be used as normal keys");
 
 	printf( "\n Sound:\n\n");
-	printf( "  -nosound           %s\n", "Disables sound output");
-	printf( "  -nomusic           %s\n", "Disables music output");
-	printf( "  -sound11k          %s\n", "Use 11KHz sounds");
+	printf( "  -nosound               %s\n", "Disables sound output");
+	printf( "  -nomusic               %s\n", "Disables music output");
+	printf( "  -sound11k              %s\n", "Use 11KHz sounds");
 #ifdef    USE_SDLMIXER
-	printf( "  -nosdlmixer        %s\n", "Disable Sound output via SDL_mixer");
+	printf( "  -nosdlmixer            %s\n", "Disable Sound output via SDL_mixer");
 #endif // USE SDLMIXER
 
 	printf( "\n Graphics:\n\n");
-	printf( "  -lowresfont        %s\n", "Force to use LowRes fonts");
-	printf( "  -lowresgraphics    %s\n", "Force to use LowRes graphics");
-	printf( "  -lowresmovies      %s\n", "Play low resolution movies if available (for slow machines)");
+	printf( "  -lowresfont            %s\n", "Force to use LowRes fonts");
+	printf( "  -lowresgraphics        %s\n", "Force to use LowRes graphics");
+	printf( "  -lowresmovies          %s\n", "Play low resolution movies if available (for slow machines)");
 #ifdef    OGL
-	printf( "  -gl_fixedfont      %s\n", "Do not scale fonts to current resolution");
+	printf( "  -gl_fixedfont          %s\n", "Do not scale fonts to current resolution");
 #endif // OGL
 
 #ifdef    NETWORK
 	printf( "\n Multiplayer:\n\n");
-	printf( "  -ipxnetwork <n>    %s\n", "Use IPX network number <n>");
-	printf( "  -udp_hostaddr <n>  %s\n", "When manually joining a game use default IP Address <n> to connect to");
-	printf( "  -udp_hostport <n>  %s\n", "When manually joining a game use default UDP Port <n> to connect to");
-	printf( "  -udp_myport <n>	  %s\n", "When hosting/joining a game use default UDP Port <n> to send packets from");
+	printf( "  -ipxnetwork <n>        %s\n", "Use IPX network number <n>");
+	printf( "  -udp_hostaddr <n>      %s\n", "When manually joining a game use default IP Address <n> to connect to");
+	printf( "  -udp_hostport <n>      %s\n", "When manually joining a game use default UDP Port <n> to connect to");
+	printf( "  -udp_myport <n>        %s\n", "When hosting/joining a game use default UDP Port <n> to send packets from");
+#ifdef USE_TRACKER
+	printf( "  -tracker_hostaddr <n>  %s\n", "Address of Tracker server to register/query games to/from (default: dxxtracker.reenigne.net)");
+	printf( "  -tracker_hostport <n>  %s\n", "Port of Tracker server to register/query games to/from (default: 42420)");
+#endif // USE_TRACKER
 #endif // NETWORK
 
 #ifdef    EDITOR
 	printf( "\n Editor:\n\n");
-	printf( "  -autoload <s>      %s\n", "Autoload a level in the editor");
-	printf( "  -macdata           %s\n", "Read and write mac data files in editor (swap colors)");
-	printf( "  -hoarddata         %s\n", "Make the hoard ham file from some files, then exit");
+	printf( "  -autoload <s>          %s\n", "Autoload a level in the editor");
+	printf( "  -macdata               %s\n", "Read and write mac data files in editor (swap colors)");
+	printf( "  -hoarddata             %s\n", "Make the hoard ham file from some files, then exit");
 #endif // EDITOR
 
 	printf( "\n Debug (use only if you know what you're doing):\n\n");
-	printf( "  -debug             %s\n", "Enable very verbose output");
-	printf( "  -verbose           %s\n", "Shows initialization steps for tech support");
-	printf( "  -norun             %s\n", "Bail out after initialization");
-	printf( "  -renderstats       %s\n", "Enable renderstats info by default");
-	printf( "  -text <s>          %s\n", "Specify alternate .tex file");
-	printf( "  -tmap <s>          %s\n", "Select texmapper to use (c,fp,quad,i386)");
-	printf( "  -showmeminfo       %s\n", "Show memory statistics");
-	printf( "  -nodoublebuffer    %s\n", "Disable Doublebuffering");
-	printf( "  -bigpig            %s\n", "Use uncompressed RLE bitmaps");
-	printf( "  -16bpp             %s\n", "Use 16Bpp instead of 32Bpp");
+	printf( "  -debug                 %s\n", "Enable very verbose output");
+	printf( "  -verbose               %s\n", "Shows initialization steps for tech support");
+	printf( "  -norun                 %s\n", "Bail out after initialization");
+	printf( "  -renderstats           %s\n", "Enable renderstats info by default");
+	printf( "  -text <s>              %s\n", "Specify alternate .tex file");
+	printf( "  -tmap <s>              %s\n", "Select texmapper to use (c,fp,quad,i386)");
+	printf( "  -showmeminfo           %s\n", "Show memory statistics");
+	printf( "  -nodoublebuffer        %s\n", "Disable Doublebuffering");
+	printf( "  -bigpig                %s\n", "Use uncompressed RLE bitmaps");
+	printf( "  -16bpp                 %s\n", "Use 16Bpp instead of 32Bpp");
 #ifdef    OGL
-	printf( "  -gl_oldtexmerge    %s\n", "Use old texmerge, uses more ram, but _might_ be a bit faster");
-	printf( "  -gl_intensity4_ok <n> %s\n", "Override DbgGlIntensity4Ok - Default: 1");
+	printf( "  -gl_oldtexmerge        %s\n", "Use old texmerge, uses more ram, but _might_ be a bit faster");
+	printf( "  -gl_intensity4_ok <n>  %s\n", "Override DbgGlIntensity4Ok - Default: 1");
 	printf( "  -gl_luminance4_alpha4_ok <n> %s\n", "Override DbgGlLuminance4Alpha4Ok - Default: 1");
-	printf( "  -gl_rgba2_ok <n>   %s\n", "Override DbgGlRGBA2Ok - Default: 1");
-	printf( "  -gl_readpixels_ok <n> %s\n", "Override DbgGlReadPixelsOk - Default: 1");
+	printf( "  -gl_rgba2_ok <n>       %s\n", "Override DbgGlRGBA2Ok - Default: 1");
+	printf( "  -gl_readpixels_ok <n>  %s\n", "Override DbgGlReadPixelsOk - Default: 1");
 	printf( "  -gl_gettexlevelparam_ok <n> %s\n", "Override DbgGlGetTexLevelParamOk - Default: 1");
 #else
-	printf( "  -hwsurface         %s\n", "Use SDL HW Surface");
-	printf( "  -asyncblit         %s\n", "Use queued blits over SDL. Can speed up rendering");
+	printf( "  -hwsurface             %s\n", "Use SDL HW Surface");
+	printf( "  -asyncblit             %s\n", "Use queued blits over SDL. Can speed up rendering");
 #endif // OGL
 
 	printf( "\n Help:\n\n");
-	printf( "  -help, -h, -?, ?   %s\n", "View this help screen");
+	printf( "  -help, -h, -?, ?       %s\n", "View this help screen");
 	printf( "\n\n");
 }
 

main/menu.c
@@ -2007,8 +2007,11 @@ void do_multi_player_menu()
 #ifdef USE_UDP
 	m[num_options].type=NM_TYPE_TEXT; m[num_options].text="UDP:"; num_options++;
 	m[num_options].type=NM_TYPE_MENU; m[num_options].text="HOST GAME"; menu_choice[num_options]=MENU_START_UDP_NETGAME; num_options++;
+#ifdef USE_TRACKER
+	m[num_options].type=NM_TYPE_MENU; m[num_options].text="FIND LAN/ONLINE GAMES"; menu_choice[num_options]=MENU_JOIN_LIST_UDP_NETGAME; num_options++;
+#else
 	m[num_options].type=NM_TYPE_MENU; m[num_options].text="FIND LAN GAMES"; menu_choice[num_options]=MENU_JOIN_LIST_UDP_NETGAME; num_options++;
-	//m[num_options].type=NM_TYPE_MENU; m[num_options].text="FIND LAN/ONLINE GAMES"; menu_choice[num_options]=MENU_JOIN_LIST_UDP_NETGAME; num_options++;
+#endif
 	m[num_options].type=NM_TYPE_MENU; m[num_options].text="JOIN GAME MANUALLY"; menu_choice[num_options]=MENU_JOIN_MANUAL_UDP_NETGAME; num_options++;
 #endif
 

main/multi.h
@@ -28,7 +28,12 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #ifdef USE_UDP
 #ifdef _WIN32
-#include <winsock.h>
+#ifdef _WIN32_WINNT
+#undef _WIN32_WINNT
+#endif
+#define _WIN32_WINNT 0x0501 // for get/freeaddrinfo()
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <io.h>
 #else
 #include <sys/socket.h>
@@ -506,5 +511,8 @@ typedef struct netgame_info
 	short						PacketsPerSec;
 	ubyte						PacketLossPrevention;
 	ubyte						NoFriendlyFire;
+#ifdef USE_TRACKER
+	ubyte						Tracker;
+#endif
 } __pack__ netgame_info;
 #endif /* _MULTI_H */

main/net_ipx.h
@@ -25,7 +25,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "multi.h"
 #include "pstypes.h"
 #ifdef _WIN32
-#include <winsock.h>
+#include <winsock2.h>
 #else
 #include <netinet/in.h> /* for htons & co. */
 #endif

commit 20e72128de76c230920c029b169e617866224129

Add configure test and fallback code to support ancient Windows
Windows 2000 has no support for getaddrinfo.  Five years ago (Apr 5
2011), the addition of tracker support
(7f18afd5d7a0fd4146a4ead16e3de09bc1684bdf) brought a requirement for
getaddrinfo.  Recently, someone with a working Windows 2000 system
reported that Rebirth no longer starts on Windows 2000.  Although such
systems should not be connected to the Internet, they may still need
name resolution on a LAN.  Add a configure test to probe for getaddrinfo
support and fall back to gethostbyname if needed.

Compiling a binary that works on Windows 2000 requires one of:
* using an ancient SDK that does not declare getaddrinfo
* defining _WIN32_WINNT to exclude Windows XP support
* overriding the SConf test to force failure

Fixes: 7f18afd5d7a0fd4146a4ead16e3de09bc1684bdf ("Client-side implementation for Tracker support by Matt 1360 Vandermeulen including improvements in udp_dns_filladdr and IPv4/IPv6 compability; Very little adjustments by me, too including IPv6 support for Windows (untested); Actual tracker code will follow later as seperate branch when it's done")

SConstruct
@@ -1743,6 +1743,28 @@ help:always wipe certain freed memory
 	return !strcasecmp(argv[0], argv[0] + 1) && !strncasecmp(argv[0] + 1, argv[0], 1);
 '''
 		self.Compile(context, text='#include <cstring>', main=main, msg='for strcasecmp', successflags=_successflags)
+	@_custom_test
+	def check_getaddrinfo_present(self,context,_successflags={'CPPDEFINES' : ['DXX_HAVE_GETADDRINFO']}):
+		self.Compile(context, text='''
+#ifdef WIN32
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0501
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#endif
+''', main='''
+	addrinfo *res = nullptr;
+	const addrinfo *hints = nullptr;
+	int i = getaddrinfo("", "", hints, &res);
+	(void)i;
+	freeaddrinfo(res);
+	return 0;
+''', msg='for getaddrinfo', successflags=_successflags)
 	__preferred_compiler_options = (
 		'-fvisibility=hidden',
 		'-Wsuggest-attribute=noreturn',

similar/main/net_udp.cpp
@@ -523,6 +523,7 @@ public:
 // Resolve address
 int udp_dns_filladdr_t::apply(sockaddr &addr, socklen_t addrlen, int ai_family, const char *host, uint16_t port)
 {
+#ifdef DXX_HAVE_GETADDRINFO
 	// Variables
 	addrinfo hints{};
 	char sPort[6];
@@ -568,6 +569,23 @@ int udp_dns_filladdr_t::apply(sockaddr &addr, socklen_t addrlen, int ai_family, 
 	 */
 	
 	// Free memory
+#else
+	sockaddr_in &sai = reinterpret_cast<sockaddr_in &>(addr);
+	if (addrlen < sizeof(sai))
+		return -1;
+	const auto he = gethostbyname(host);
+	if (!he)
+	{
+		con_printf(CON_URGENT, "udp_dns_filladdr (gethostbyname) failed for host %s", host);
+		nm_messagebox(TXT_ERROR, 1, TXT_OK, "Could not resolve IPv4 address\n%s", host);
+		addr.sa_family = AF_UNSPEC;
+		return -1;
+	}
+	sai = {};
+	sai.sin_family = ai_family;
+	sai.sin_port = htons(port);
+	sai.sin_addr = *reinterpret_cast<const in_addr *>(he->h_addr);
+#endif
 	return 0;
 }
 