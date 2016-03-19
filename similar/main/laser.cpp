dxx-rebirth/dxx-rebirth

commit 7cce4e292dff0a5b25e95a9a2b2b1e7becb608ed

Fixed up non-NEWHOMER code for testing purposes

similar/main/laser.cpp
@@ -1612,9 +1612,9 @@ void Laser_do_weapon_sequence(const vobjptridx_t obj)
                                         fix	dist_to_player;
 
                                         dist_to_player = vm_vec_dist_quick(obj->pos, track_goal->pos);
-									auto &homing_object_dist = get_local_plrobj().ctype.player_info.homing_object_dist;
-									if (dist_to_player < homing_object_dist || homing_object_dist < 0)
-										homing_object_dist = dist_to_player;
+                                        auto &homing_object_dist = get_local_plrobj().ctype.player_info.homing_object_dist;
+                                        if (dist_to_player < homing_object_dist || homing_object_dist < 0)
+                                                homing_object_dist = dist_to_player;
                                 }
 
 
@@ -1660,15 +1660,16 @@ void Laser_do_weapon_sequence(const vobjptridx_t obj)
 			//	Make sure the object we are tracking is still trackable.
 			objptridx_t obj_track_goal = object_none;
 			if (obj->ctype.laser_info.track_goal != object_none)
-				obj_track_goal = obj->ctype.laser_info.track_goal;
-			auto track_goal = track_track_goal(obj_track_goal, obj, &dot, d_tick_count);
+				obj_track_goal = objptridx(obj->ctype.laser_info.track_goal);
+			auto track_goal = track_track_goal(objptridx(obj->ctype.laser_info.track_goal), obj, &dot, d_tick_count);
 
 			if (track_goal == get_local_player().objnum) {
 				fix	dist_to_player;
 
 				dist_to_player = vm_vec_dist_quick(obj->pos, track_goal->pos);
-				if ((dist_to_player < get_local_player().homing_object_dist) || (get_local_player().homing_object_dist < 0))
-					get_local_player().homing_object_dist = dist_to_player;
+                                auto &homing_object_dist = get_local_plrobj().ctype.player_info.homing_object_dist;
+                                if (dist_to_player < homing_object_dist || homing_object_dist < 0)
+                                        homing_object_dist = dist_to_player;
 
 			}
 

commit 34417e9b7a68625db85c559dd4644674edf4b4d2

Simplify track_goal setup
Fixes: 7cce4e292dff0a5b25e95a9a2b2b1e7becb608ed ("Fixed up non-NEWHOMER code for testing purposes")

similar/main/laser.cpp
@@ -1658,10 +1658,7 @@ void Laser_do_weapon_sequence(const vobjptridx_t obj)
                         }
 #else // old FPS-dependent homers
 			//	Make sure the object we are tracking is still trackable.
-			objptridx_t obj_track_goal = object_none;
-			if (obj->ctype.laser_info.track_goal != object_none)
-				obj_track_goal = objptridx(obj->ctype.laser_info.track_goal);
-			auto track_goal = track_track_goal(objptridx(obj->ctype.laser_info.track_goal), obj, &dot, d_tick_count);
+			const auto &&track_goal = track_track_goal(objptridx(obj->ctype.laser_info.track_goal), obj, &dot, d_tick_count);
 
 			if (track_goal == get_local_player().objnum) {
 				fix	dist_to_player;