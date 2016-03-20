dxx-rebirth/dxx-rebirth

commit 9f26e2211e71036a155aad9a7ca3ff92d7b49c3c

Warn on invalid object ID access

common/main/object.h
@@ -482,6 +482,18 @@ static inline void set_weapon_id(object &o, weapon_type_t id)
 {
 	o.id = id;
 }
+
+void check_warn_object_type(const object &, object_type_t, const char *file, unsigned line);
+#define get_hostage_id(O)	(check_warn_object_type(O, OBJ_HOSTAGE, __FILE__, __LINE__), get_hostage_id(O))
+#define get_player_id(O)	(check_warn_object_type(O, OBJ_PLAYER, __FILE__, __LINE__), get_player_id(O))
+#define get_powerup_id(O)	(check_warn_object_type(O, OBJ_POWERUP, __FILE__, __LINE__), get_powerup_id(O))
+#define get_reactor_id(O)	(check_warn_object_type(O, OBJ_CNTRLCEN, __FILE__, __LINE__), get_reactor_id(O))
+#define get_robot_id(O)	(check_warn_object_type(O, OBJ_ROBOT, __FILE__, __LINE__), get_robot_id(O))
+#define get_weapon_id(O)	(check_warn_object_type(O, OBJ_WEAPON, __FILE__, __LINE__), get_weapon_id(O))
+#define set_hostage_id(O,I)	(check_warn_object_type(O, OBJ_HOSTAGE, __FILE__, __LINE__), set_hostage_id(O, I))
+#define set_player_id(O,I)	(check_warn_object_type(O, OBJ_PLAYER, __FILE__, __LINE__), set_player_id(O, I))
+#define set_robot_id(O,I)	(check_warn_object_type(O, OBJ_ROBOT, __FILE__, __LINE__), set_robot_id(O, I))
+#define set_weapon_id(O,I)	(check_warn_object_type(O, OBJ_WEAPON, __FILE__, __LINE__), set_weapon_id(O, I))
 #endif
 
 #endif

similar/main/multi.cpp
@@ -4258,7 +4258,7 @@ void DropFlag ()
 void multi_send_drop_flag(objnum_t objnum,int seed)
 {
 	int count=0;
-	auto objp = &Objects[objnum];
+	const auto &&objp = vobjptr(objnum);
 	count++;
 	multibuf[count++]=(char)get_powerup_id(objp);
 

similar/main/ai.cpp
@@ -4142,7 +4142,7 @@ static int add_awareness_event(const vobjptr_t objp, player_awareness_type_t typ
 	if (Num_awareness_events < MAX_AWARENESS_EVENTS) {
 		if (type == player_awareness_type_t::PA_WEAPON_WALL_COLLISION ||
 			type == player_awareness_type_t::PA_WEAPON_ROBOT_COLLISION)
-			if (get_weapon_id(objp) == VULCAN_ID)
+			if (objp->type == OBJ_WEAPON && get_weapon_id(objp) == VULCAN_ID)
 				if (d_rand() > 3276)
 					return 0;       // For vulcan cannon, only about 1/10 actually cause awareness
 

similar/main/collide.cpp
@@ -520,6 +520,8 @@ static int effect_parent_is_guidebot(const laser_parent &laser)
 	if (laser.parent_type != OBJ_ROBOT)
 		return 0;
 	const auto &&robot = vcobjptr(laser.parent_num);
+	if (robot->type != OBJ_ROBOT)
+		return 0;
 	if (robot->signature != laser.parent_signature)
 		/* parent replaced, no idea what it once was */
 		return 0;

similar/main/gamesave.cpp
@@ -468,16 +468,23 @@ static void read_object(const vobjptr_t obj,PHYSFS_file *f,int version)
 			else
 				obj->ctype.powerup_info.count = 1;
 
+			if (obj->type == OBJ_POWERUP)
+			{
+				/* Hostages have control type CT_POWERUP, but object
+				 * type OBJ_HOSTAGE.  Hostages are never weapons, so
+				 * prevent checking their IDs.
+				 */
 			if (get_powerup_id(obj) == POW_VULCAN_WEAPON)
 					obj->ctype.powerup_info.count = VULCAN_WEAPON_AMMO_AMOUNT;
 
 #if defined(DXX_BUILD_DESCENT_II)
-			if (get_powerup_id(obj) == POW_GAUSS_WEAPON)
+			else if (get_powerup_id(obj) == POW_GAUSS_WEAPON)
 					obj->ctype.powerup_info.count = VULCAN_WEAPON_AMMO_AMOUNT;
 
-			if (get_powerup_id(obj) == POW_OMEGA_WEAPON)
+			else if (get_powerup_id(obj) == POW_OMEGA_WEAPON)
 					obj->ctype.powerup_info.count = MAX_OMEGA_CHARGE;
 #endif
+			}
 
 			break;
 

similar/main/object.cpp
@@ -2308,3 +2308,9 @@ void object_rw_swap(object_rw *obj, int swap)
 			
 	}
 }
+
+void check_warn_object_type(const object &o, object_type_t t, const char *file, unsigned line)
+{
+	if (o.type != t)
+		con_printf(CON_URGENT, "%s:%u: BUG: object %p has type %u, expected %u", file, line, &o, o.type, t);
+}

commit dd3cedff3d94ef1f772d295ac50027ed5f0cbc35

Add get_ghost_id for multi_do_reappear
Ghosts use the same ID space as players, so reuse function get_player_id
to fetch the ID after checking.

Reported-by: zicodxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/182>
Fixes: 9f26e2211e71036a155aad9a7ca3ff92d7b49c3c ("Warn on invalid object ID access")

common/main/object.h
@@ -543,6 +543,7 @@ void check_warn_object_type(const object &, object_type_t, const char *file, uns
 #define get_player_id(O)	(check_warn_object_type(O, OBJ_PLAYER, __FILE__, __LINE__), get_player_id(O))
 #define get_powerup_id(O)	(check_warn_object_type(O, OBJ_POWERUP, __FILE__, __LINE__), get_powerup_id(O))
 #define get_reactor_id(O)	(check_warn_object_type(O, OBJ_CNTRLCEN, __FILE__, __LINE__), get_reactor_id(O))
+#define get_ghost_id(O)	(check_warn_object_type(O, OBJ_GHOST, __FILE__, __LINE__), (get_player_id)(O))
 #define get_fireball_id(O)	(check_warn_object_type(O, OBJ_FIREBALL, __FILE__, __LINE__), get_fireball_id(O))
 #define get_robot_id(O)	(check_warn_object_type(O, OBJ_ROBOT, __FILE__, __LINE__), get_robot_id(O))
 #define get_weapon_id(O)	(check_warn_object_type(O, OBJ_WEAPON, __FILE__, __LINE__), get_weapon_id(O))

similar/main/multi.cpp
@@ -1747,10 +1747,10 @@ static void multi_do_reappear(const playernum_t pnum, const ubyte *buf)
 	const objnum_t objnum = GET_INTEL_SHORT(buf + 2);
 
 	const auto obj = vobjptridx(objnum);
-	if (pnum != get_player_id(obj))
+	if (pnum != get_ghost_id(obj))
 		return;
 
-	multi_make_ghost_player(get_player_id(obj));
+	multi_make_ghost_player(pnum);
 	create_player_appearance_effect(obj);
 }
 