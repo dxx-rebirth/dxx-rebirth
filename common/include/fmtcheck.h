dfcf28ff107f0e14f4b1f03203a0ca308ed49278
Kp <kp@valhallalegends.com>
2016-07-15 03:43:03 +0000

    Rename fmtcheck macros to follow standard conventions

--- common/include/fmtcheck.h
+++ common/include/fmtcheck.h
@@ -10,23 +10,23 @@
 
 #define _dxx_call_puts_parameter2(A,B,...)	B
 
-#define _dxx_call_printf_unwrap_args(...)	, ## __VA_ARGS__
-#define _dxx_call_printf_delete_comma2(A,...)	__VA_ARGS__
-#define _dxx_call_printf_delete_comma(A,...)	_dxx_call_printf_delete_comma2( A , ## __VA_ARGS__ )
+#define DXX_CALL_PRINTF_UNWRAP_ARGS_(...)	, ## __VA_ARGS__
+#define DXX_CALL_PRINTF_DELETE_COMMA2_(A,...)	__VA_ARGS__
+#define DXX_CALL_PRINTF_DELETE_COMMA_(A,...)	DXX_CALL_PRINTF_DELETE_COMMA2_( A , ## __VA_ARGS__ )
 
 #ifdef DXX_HAVE_BUILTIN_CONSTANT_P
-#define _dxx_printf_check_has_nontrivial_format_string(V,P,FMT)	\
+#define DXX_PRINTF_CHECK_HAS_NONTRIVIAL_FORMAT_STRING_(V,P,FMT)	\
 	((void)((dxx_builtin_constant_p((FMT)) && (FMT)[0] == '%' && (FMT)[1] == 's' && (FMT)[2] == 0) &&	\
 		(DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_trivial_string_specifier_argument_##V, "bare %s argument to " #V "; use " #P " directly"), 0)))
 #else
-#define _dxx_printf_check_has_nontrivial_format_string(V,P,FMT)	\
+#define DXX_PRINTF_CHECK_HAS_NONTRIVIAL_FORMAT_STRING_(V,P,FMT)	\
 	((void)(FMT))
 #endif
 
 #define dxx_call_printf_checked(V,P,A,FMT,...)	\
 	(	\
-		_dxx_printf_check_has_nontrivial_format_string(V, P, FMT),	\
+		DXX_PRINTF_CHECK_HAS_NONTRIVIAL_FORMAT_STRING_(V, P, FMT),	\
 		(sizeof(#__VA_ARGS__) == 1)	?	\
-			(P(_dxx_call_printf_delete_comma(1 _dxx_call_printf_unwrap_args A, FMT))) :	\
-		(V(_dxx_call_printf_delete_comma(1 _dxx_call_printf_unwrap_args A, FMT) ,##__VA_ARGS__))	\
+			(P(DXX_CALL_PRINTF_DELETE_COMMA_(1 DXX_CALL_PRINTF_UNWRAP_ARGS_ A, FMT))) :	\
+		(V(DXX_CALL_PRINTF_DELETE_COMMA_(1 DXX_CALL_PRINTF_UNWRAP_ARGS_ A, FMT) ,##__VA_ARGS__))	\
 	)
