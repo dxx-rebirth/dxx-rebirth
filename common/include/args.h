dxx-rebirth/dxx-rebirth

commit 4d483ca88f6f27589b287c23d03626dc2d0426c5

Simplify use of SysUsePlayersDir

common/include/args.h
@@ -74,7 +74,7 @@ struct CArg : prohibit_void_ptr<CArg>
 #endif
 	bool SysShowCmdHelp;
 	bool SysLowMem;
-	bool SysUsePlayersDir;
+	uint8_t SysUsePlayersDir;
 	bool SysAutoRecordDemo;
 	bool SysWindow;
 	bool SysAutoDemo;
@@ -155,12 +155,17 @@ extern struct Arg GameArg;
 bool InitArgs(int argc, char **argv);
 }
 
-static inline const char *PLAYER_DIRECTORY_STRING(const char *s, const char *f) __attribute_format_arg(2);
-static inline const char *PLAYER_DIRECTORY_STRING(const char *s, const char *)
+namespace dcx {
+#define PLAYER_DIRECTORY_TEXT	"Players/"
+
+__attribute_format_arg(1)
+static inline const char *PLAYER_DIRECTORY_STRING(const char *s);
+static inline const char *PLAYER_DIRECTORY_STRING(const char *s)
 {
-	return (CGameArg.SysUsePlayersDir) ? s : (s + sizeof("Players/") - 1);
+	return &s[CGameArg.SysUsePlayersDir];
 }
-#define PLAYER_DIRECTORY_STRING(S)	((PLAYER_DIRECTORY_STRING)("Players/" S, S))
+#define PLAYER_DIRECTORY_STRING(S)	((PLAYER_DIRECTORY_STRING)(PLAYER_DIRECTORY_TEXT S))
+}
 #endif
 
 #endif

similar/main/inferno.cpp
@@ -533,11 +533,12 @@ static int main(int argc, char *argv[])
 	{
 		if (!CGameArg.SysPilot.empty())
 		{
-			char filename[32] = "";
+			char filename[sizeof(PLAYER_DIRECTORY_TEXT) + CALLSIGN_LEN + 4];
 			unsigned j;
 
 			snprintf(filename, sizeof(filename), PLAYER_DIRECTORY_STRING("%.12s"), CGameArg.SysPilot.c_str());
-			for (j = CGameArg.SysUsePlayersDir ? 8 : 0; filename[j] != '\0'; ++j)
+			const uintptr_t SysUsePlayersDir = CGameArg.SysUsePlayersDir;
+			for (j = SysUsePlayersDir; filename[j]; ++j)
 			{
 				switch (filename[j]) {
 					case ' ':
@@ -552,7 +553,7 @@ static int main(int argc, char *argv[])
 			if(PHYSFSX_exists(filename,0))
 			{
 				filename[j - 4] = 0;
-				char *b = CGameArg.SysUsePlayersDir ? &filename[8] : filename;
+				const auto b = &filename[SysUsePlayersDir];
 				get_local_player().callsign.copy(b, std::distance(b, end(filename)));
 				read_player_file();
 				WriteConfigFile();

similar/misc/args.cpp
@@ -218,7 +218,7 @@ static void ReadCmdArgs(Inilist &ini, Arglist &Args)
 #endif
 		}
 		else if (!d_stricmp(p, "-use_players_dir"))
-			CGameArg.SysUsePlayersDir 	= 1;
+			CGameArg.SysUsePlayersDir = sizeof(PLAYER_DIRECTORY_TEXT) - 1;
 		else if (!d_stricmp(p, "-lowmem"))
 			CGameArg.SysLowMem = true;
 		else if (!d_stricmp(p, "-pilot"))

similar/main/state.cpp
@@ -890,7 +890,7 @@ int state_save_all_sub(const char *filename, const char *desc)
 	fix tmptime32 = 0;
 
 	#ifndef NDEBUG
-	if (CGameArg.SysUsePlayersDir && strncmp(filename, "Players/", 8))
+	if (CGameArg.SysUsePlayersDir && strncmp(filename, PLAYER_DIRECTORY_TEXT, sizeof(PLAYER_DIRECTORY_TEXT) - 1))
 		Int3();
 	#endif
 
@@ -1343,7 +1343,7 @@ int state_restore_all_sub(const char *filename, const secret_restore secret)
 #endif
 
 	#ifndef NDEBUG
-	if (CGameArg.SysUsePlayersDir && strncmp(filename, "Players/", 8))
+	if (CGameArg.SysUsePlayersDir && strncmp(filename, PLAYER_DIRECTORY_TEXT, sizeof(PLAYER_DIRECTORY_TEXT) - 1))
 		Int3();
 	#endif
 
@@ -1885,7 +1885,7 @@ int state_get_game_id(const char *filename)
 	callsign_t saved_callsign;
 
 	#ifndef NDEBUG
-	if (CGameArg.SysUsePlayersDir && strncmp(filename, "Players/", 8))
+	if (CGameArg.SysUsePlayersDir && strncmp(filename, PLAYER_DIRECTORY_TEXT, sizeof(PLAYER_DIRECTORY_TEXT) - 1))
 		Int3();
 	#endif
 

commit b0821cff553581d6499f5e542cd795f20c3e9bc7

Fix inverted sense of -use_players_dir
Fixes: 4d483ca88f6f27589b287c23d03626dc2d0426c5 ("Simplify use of SysUsePlayersDir")

common/include/args.h
@@ -74,7 +74,7 @@ struct CArg : prohibit_void_ptr<CArg>
 #endif
 	bool SysShowCmdHelp;
 	bool SysLowMem;
-	uint8_t SysUsePlayersDir;
+	int8_t SysUsePlayersDir;
 	bool SysAutoRecordDemo;
 	bool SysWindow;
 	bool SysAutoDemo;
@@ -162,7 +162,7 @@ __attribute_format_arg(1)
 static inline const char *PLAYER_DIRECTORY_STRING(const char *s);
 static inline const char *PLAYER_DIRECTORY_STRING(const char *s)
 {
-	return &s[CGameArg.SysUsePlayersDir];
+	return &s[CGameArg.SysUsePlayersDir + sizeof(PLAYER_DIRECTORY_TEXT) - 1];
 }
 #define PLAYER_DIRECTORY_STRING(S)	((PLAYER_DIRECTORY_STRING)(PLAYER_DIRECTORY_TEXT S))
 }

similar/main/inferno.cpp
@@ -536,17 +536,22 @@ static int main(int argc, char *argv[])
 		{
 			char filename[sizeof(PLAYER_DIRECTORY_TEXT) + CALLSIGN_LEN + 4];
 
+			/* Step over the literal PLAYER_DIRECTORY_TEXT when it is
+			 * present.  Point at &filename[0] when
+			 * PLAYER_DIRECTORY_TEXT is absent.
+			 */
+			const auto b = &filename[-CGameArg.SysUsePlayersDir];
 			snprintf(filename, sizeof(filename), PLAYER_DIRECTORY_STRING("%.12s"), CGameArg.SysPilot.c_str());
 			/* The pilot name is never used after this.  Clear it to
 			 * free the allocated memory, if any.
 			 */
 			CGameArg.SysPilot.clear();
-			const uintptr_t SysUsePlayersDir = CGameArg.SysUsePlayersDir;
-			auto j = SysUsePlayersDir;
-			for (const auto &facet = std::use_facet<std::ctype<char>>(std::locale::classic()); char &c = filename[j]; ++j)
+			auto p = b;
+			for (const auto &facet = std::use_facet<std::ctype<char>>(std::locale::classic()); char &c = *p; ++p)
 			{
 				c = facet.tolower(static_cast<uint8_t>(c));
 			}
+			auto j = p - filename;
 			if (j < sizeof(filename) - 4 && (j <= 4 || strcmp(&filename[j - 4], ".plr"))) // if player hasn't specified .plr extension in argument, add it
 			{
 				strcpy(&filename[j], ".plr");
@@ -554,9 +559,7 @@ static int main(int argc, char *argv[])
 			}
 			if(PHYSFSX_exists(filename,0))
 			{
-				filename[j - 4] = 0;
-				const auto b = &filename[SysUsePlayersDir];
-				get_local_player().callsign.copy(b, std::distance(b, end(filename)));
+				get_local_player().callsign.copy(b, std::distance(b, &filename[j - 4]));
 				read_player_file();
 				WriteConfigFile();
 			}

similar/misc/args.cpp
@@ -218,7 +218,7 @@ static void ReadCmdArgs(Inilist &ini, Arglist &Args)
 #endif
 		}
 		else if (!d_stricmp(p, "-use_players_dir"))
-			CGameArg.SysUsePlayersDir = sizeof(PLAYER_DIRECTORY_TEXT) - 1;
+			CGameArg.SysUsePlayersDir = static_cast<int8_t>(- (sizeof(PLAYER_DIRECTORY_TEXT) - 1));
 		else if (!d_stricmp(p, "-lowmem"))
 			CGameArg.SysLowMem = true;
 		else if (!d_stricmp(p, "-pilot"))