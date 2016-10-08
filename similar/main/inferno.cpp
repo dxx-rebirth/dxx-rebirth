dxx-rebirth/dxx-rebirth

commit 435aa5a020b647801c30752a6127aee7a2903271

Move USE_UDP to dxxsconf.h; rename to DXX_USE_UDP
Rename symbol USE_UDP to DXX_USE_UDP to show that it is a DXX
symbol, not one inherited from a library.  Move it to dxxsconf.h to
shorten the command line.

This is a mostly automated transform, but the changes to SConstruct were
manual.

git grep -l USE_UDP -- '*.h' '*.cpp' | xargs sed -i -e 's/^\s*#ifdef \(USE_UDP\)$/#if DXX_\1/' -e 's/\s*#\(el\)\?if \(.*\)defined(\(USE_UDP\))/#\1if \2DXX_\3/'

similar/main/inferno.cpp
@@ -105,7 +105,7 @@ char copyright[] = "DESCENT II  COPYRIGHT (C) 1994-1996 PARALLAX SOFTWARE CORPOR
 #include "ui.h"
 #endif
 #include "vers_id.h"
-#ifdef USE_UDP
+#if DXX_USE_UDP
 #include "net_udp.h"
 #endif
 

SConstruct
@@ -844,7 +844,7 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 	def _check_user_settings_ipv6(self,context,_CPPDEFINES=(('IPv6',),)):
 		self._result_check_user_setting(context, self.user_settings.ipv6, _CPPDEFINES, 'IPv6 support')
 	@_custom_test
-	def _check_user_settings_udp(self,context,_CPPDEFINES=(('USE_UDP',),)):
+	def _check_user_settings_udp(self,context,_CPPDEFINES='DXX_USE_UDP'):
 		self._result_check_user_setting(context, self.user_settings.use_udp, _CPPDEFINES, 'multiplayer over UDP')
 	@_custom_test
 	def _check_user_settings_tracker(self,context,_CPPDEFINES='DXX_USE_TRACKER'):

common/main/multi.h
@@ -792,10 +792,10 @@ void save_hoard_data(void);
  */
 struct netplayer_info : prohibit_void_ptr<netplayer_info>
 {
-#if defined(USE_UDP)
+#if DXX_USE_UDP
 	union
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		struct
 		{
 			struct _sockaddr	addr; // IP address of this peer
@@ -819,10 +819,10 @@ namespace dsx {
  */
 struct netgame_info : prohibit_void_ptr<netgame_info>, ignore_window_pointer_t
 {
-#if defined(USE_UDP)
+#if DXX_USE_UDP
 	union
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		struct
 		{
 			struct _sockaddr		addr; // IP address of this netgame's host

similar/main/menu.cpp
@@ -77,7 +77,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "strutil.h"
 #include "multi.h"
 #include "vers_id.h"
-#ifdef USE_UDP
+#if DXX_USE_UDP
 #include "net_udp.h"
 #endif
 #ifdef EDITOR
@@ -111,14 +111,14 @@ enum MENUS
     MENU_DIFFICULTY,
     MENU_HELP,
     MENU_NEW_PLAYER,
-    #if defined(USE_UDP)
+#if DXX_USE_UDP
         MENU_MULTIPLAYER,
     #endif
 
     MENU_SHOW_CREDITS,
     MENU_ORDER_INFO,
 
-    #ifdef USE_UDP
+#if DXX_USE_UDP
     MENU_START_UDP_NETGAME,
     MENU_JOIN_MANUAL_UDP_NETGAME,
     MENU_JOIN_LIST_UDP_NETGAME,
@@ -523,7 +523,7 @@ static void create_main_menu(newmenu_item *m, int *menu_choice, int *callers_num
 	ADD_ITEM(TXT_NEW_GAME,MENU_NEW_GAME,KEY_N);
 
 	ADD_ITEM(TXT_LOAD_GAME,MENU_LOAD_GAME,KEY_L);
-#if defined(USE_UDP)
+#if DXX_USE_UDP
 	ADD_ITEM(TXT_MULTIPLAYER_,MENU_MULTIPLAYER,-1);
 #endif
 
@@ -624,7 +624,7 @@ int do_option ( int select)
 			RegisterPlayer();
 			break;
 
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MENU_START_UDP_NETGAME:
 			multi_protocol = MULTI_PROTO_UDP;
 			select_mission(1, TXT_MULTI_MISSION, net_udp_setup_game);
@@ -638,7 +638,7 @@ int do_option ( int select)
 			net_udp_list_join_game();
 			break;
 #endif
-#if defined(USE_UDP)
+#if DXX_USE_UDP
 		case MENU_MULTIPLAYER:
 			do_multi_player_menu();
 			break;
@@ -2218,7 +2218,7 @@ void gameplay_config()
 
 }
 
-#if defined(USE_UDP)
+#if DXX_USE_UDP
 static int multi_player_menu_handler(newmenu *menu,const d_event &event, int *menu_choice)
 {
 	newmenu_item *items = newmenu_get_items(menu);
@@ -2261,7 +2261,7 @@ void do_multi_player_menu()
 		return;
 	}
 
-#ifdef USE_UDP
+#if DXX_USE_UDP
 	ADD_ITEM("HOST GAME", MENU_START_UDP_NETGAME, -1);
 #if DXX_USE_TRACKER
 	ADD_ITEM("FIND LAN/ONLINE GAMES", MENU_JOIN_LIST_UDP_NETGAME, -1);

similar/main/multi.cpp
@@ -80,7 +80,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "state.h"
 #include "automap.h"
 #include "event.h"
-#ifdef USE_UDP
+#if DXX_USE_UDP
 #include "net_udp.h"
 #endif
 
@@ -462,7 +462,7 @@ int multi_objnum_is_past(objnum_t objnum)
 	switch (multi_protocol)
 	{
 		case MULTI_PROTO_UDP:
-#ifdef USE_UDP
+#if DXX_USE_UDP
 			return net_udp_objnum_is_past(objnum);
 			break;
 #endif
@@ -927,7 +927,7 @@ void multi_do_protocol_frame(int force, int listen)
 {
 	switch (multi_protocol)
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MULTI_PROTO_UDP:
 			net_udp_do_frame(force, listen);
 			break;
@@ -1006,7 +1006,7 @@ void _multi_send_data(const ubyte *buf, unsigned len, int priority)
 	{
 		switch (multi_protocol)
 		{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 			case MULTI_PROTO_UDP:
 				net_udp_send_data(buf, len, priority);
 				break;
@@ -1025,7 +1025,7 @@ static void _multi_send_data_direct(const ubyte *buf, unsigned len, const player
 
 	switch (multi_protocol)
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MULTI_PROTO_UDP:
 			net_udp_send_mdata_direct(buf, len, pnum, priority);
 			break;
@@ -1079,7 +1079,7 @@ void multi_leave_game()
 	{
 		switch (multi_protocol)
 		{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 			case MULTI_PROTO_UDP:
 				net_udp_leave_game();
 				break;
@@ -1114,7 +1114,7 @@ int multi_endlevel(int *const secret)
 
 	switch (multi_protocol)
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MULTI_PROTO_UDP:
 			result = net_udp_endlevel(secret);
 			break;
@@ -1131,7 +1131,7 @@ multi_endlevel_poll *get_multi_endlevel_poll1()
 {
 	switch (multi_protocol)
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MULTI_PROTO_UDP:
 			return net_udp_kmatrix_poll1;
 #endif
@@ -1144,7 +1144,7 @@ multi_endlevel_poll *get_multi_endlevel_poll2()
 {
 	switch (multi_protocol)
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MULTI_PROTO_UDP:
 			return net_udp_kmatrix_poll2;
 #endif
@@ -1157,7 +1157,7 @@ void multi_send_endlevel_packet()
 {
 	switch (multi_protocol)
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MULTI_PROTO_UDP:
 			net_udp_send_endlevel_packet();
 			break;
@@ -1305,7 +1305,7 @@ static void kick_player(player &plr, netplayer_info &nplr)
 {
 	switch (multi_protocol)
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MULTI_PROTO_UDP:
 			net_udp_dump_player(nplr.protocol.udp.addr, DUMP_KICKED);
 			break;
@@ -2034,7 +2034,7 @@ void multi_disconnect_player(const playernum_t pnum)
 
 	switch (multi_protocol)
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MULTI_PROTO_UDP:
 			net_udp_disconnect_player(pnum);
 			break;
@@ -2587,7 +2587,7 @@ void multi_send_endlevel_start()
 		get_local_player().connected = CONNECT_ESCAPE_TUNNEL;
 		switch (multi_protocol)
 		{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 			case MULTI_PROTO_UDP:
 				net_udp_send_endlevel_packet();
 				break;
@@ -3398,7 +3398,7 @@ int multi_level_sync(void)
 {
 	switch (multi_protocol)
 	{
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		case MULTI_PROTO_UDP:
 			return net_udp_level_sync();
 			break;

similar/misc/args.cpp
@@ -24,7 +24,7 @@
 #include "game.h"
 #include "gauges.h"
 #include "console.h"
-#ifdef USE_UDP
+#if DXX_USE_UDP
 #include "net_udp.h"
 #endif
 
@@ -153,7 +153,7 @@ static void InitGameArg()
 #if defined(DXX_BUILD_DESCENT_II)
 	GameArg.SndDigiSampleRate = SAMPLE_RATE_22K;
 #endif
-#ifdef USE_UDP
+#if DXX_USE_UDP
 	CGameArg.MplUdpHostAddr = UDP_MANUAL_ADDR_DEFAULT;
 #if DXX_USE_TRACKER
 	CGameArg.MplTrackerAddr = TRACKER_ADDR_DEFAULT;
@@ -297,7 +297,7 @@ static void ReadCmdArgs(Inilist &ini, Arglist &Args)
 
 	// Multiplayer Options
 
-#ifdef USE_UDP
+#if DXX_USE_UDP
 		else if (!d_stricmp(p, "-udp_hostaddr"))
 			CGameArg.MplUdpHostAddr = arg_string(pp, end);
 		else if (!d_stricmp(p, "-udp_hostport"))

commit 8ebe98df6bff3f6b5a99b53c4b7e62c6a71f5116

Fix test for DXX_USE_UDP, DXX_USE_TRACKER
Fixes: 435aa5a020b647801c30752a6127aee7a2903271 ("Move USE_UDP to dxxsconf.h; rename to DXX_USE_UDP")
Fixes: 7c8c2b5aa492b79118053919367e0b540cd7c72d ("Move USE_TRACKER to dxxsconf.h; rename to DXX_USE_TRACKER")

similar/main/inferno.cpp
@@ -208,13 +208,13 @@ static void print_commandline_help()
 		VERB("  -gl_syncwait <n>              Wait interval (ms) for sync mode 2 (default: " DXX_STRINGIZE(OGL_SYNC_WAIT_DEFAULT) ")\n")	\
 		VERB("  -gl_darkedges                 Re-enable dark edges around filtered textures (as present in earlier versions of the engine)\n")	\
 	)	\
-	DXX_if_defined(USE_UDP, (	\
+	DXX_if_defined_01(DXX_USE_UDP, (	\
 		VERB("\n Multiplayer:\n\n")	\
 		VERB("  -udp_hostaddr <s>             Use IP address/Hostname <s> for manual game joining\n\t\t\t\t(default: %s)\n", UDP_MANUAL_ADDR_DEFAULT)	\
 		VERB("  -udp_hostport <n>             Use UDP port <n> for manual game joining (default: %i)\n", UDP_PORT_DEFAULT)	\
 		VERB("  -udp_myport <n>               Set my own UDP port to <n> (default: %i)\n", UDP_PORT_DEFAULT)	\
-		VERB("  -no-tracker                   Disable tracker (unless overridden by later -tracker_hostaddr)\n")	\
-		DXX_if_defined(USE_TRACKER, (	\
+		DXX_if_defined_01(DXX_USE_TRACKER, (	\
+			VERB("  -no-tracker                   Disable tracker (unless overridden by later -tracker_hostaddr)\n")	\
 			VERB("  -tracker_hostaddr <n>         Address of tracker server to register/query games to/from\n\t\t\t\t(default: %s)\n", TRACKER_ADDR_DEFAULT)	\
 			VERB("  -tracker_hostport <n>         Port of tracker server to register/query games to/from\n\t\t\t\t(default: %i)\n", TRACKER_PORT_DEFAULT)	\
 		))	\