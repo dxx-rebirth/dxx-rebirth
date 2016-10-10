dxx-rebirth/dxx-rebirth

commit e0785251c4f35ffa1fcb1e77f1ce9bb5b726f469

Reset homing_object_dist along with d_homer_tick_step instead of every frame to make sure lock indicator is visible in frames where homers wait for a new chance to turn; Also moved call of calc_d_tick() to calc_frame_time() - shouldn't be nested in calc_d_homer_tick()

similar/main/game.cpp
@@ -435,6 +435,8 @@ void calc_frame_time()
 		FrameTime = (last_frametime==0?1:last_frametime);		//...then use time from last frame
 
 	GameTime64 += FrameTime;
+
+	calc_d_tick();
         calc_d_homer_tick();
 }
 
@@ -1378,8 +1380,6 @@ void GameProcessFrame(void)
 	}
 	else
 	{ // Note the link to above!
-
-		get_local_player().homing_object_dist = -1;		//	Assume not being tracked.  Laser_do_weapon_sequence modifies this.
 
 		object_move_all();
 		powerup_grab_cheat_all();

similar/main/laser.cpp
@@ -1199,9 +1199,10 @@ void calc_d_homer_tick()
         // Don't let slowdowns have a lasting impact; allow you to build up at most 3 frames worth
 		if (t > HOMING_TURN_TIME*3)
 			t = HOMING_TURN_TIME*3;
+
+                get_local_player().homing_object_dist = -1; // Assume not being tracked.  Laser_do_weapon_sequence modifies this. Let's do this here since the homers do not track every frame, we may not want to reset this ever frame.
 	}
 	timer = t;
-	calc_d_tick();
 }
 
 //	------------------------------------------------------------------------------------------------------------

commit 9cab9e58c670e2abb0ebdb65514fe940f5d46bfa

Fixed regression from e0785251c4f35ffa1fcb1e77f1ce9bb5b726f469 making the homing indicator go haywire if built without NEWHOMER defined

similar/main/game.cpp
@@ -1499,7 +1499,9 @@ void GameProcessFrame(void)
 	}
 	else
 	{ // Note the link to above!
-
+#ifndef NEWHOMER
+		get_local_plrobj().ctype.player_info.homing_object_dist = -1; // Assume not being tracked.  Laser_do_weapon_sequence modifies this.
+#endif
 		object_move_all();
 		powerup_grab_cheat_all();
 