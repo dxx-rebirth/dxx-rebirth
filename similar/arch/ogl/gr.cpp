dxx-rebirth/dxx-rebirth

commit 7128141c5558190f4f2221c802ad4883b1e234fc

Use enum for newmenu item type

similar/main/newmenu.cpp
@@ -700,28 +700,34 @@ static window_event_result newmenu_mouse(window *wind,const d_event &event, newm
 						switch (citem.type)
 						{
 							case NM_TYPE_CHECK:
-								if (citem.value)
-									citem.value = 0;
-								else
-									citem.value = 1;
+								citem.value ^= 1;
 
 								if (menu->is_scroll_box)
 								changed = 1;
 								break;
 							case NM_TYPE_RADIO:
+								citem.value = 1;
+								{
+									const auto cg = citem.radio().group;
 								range_for (auto &r, menu->item_range())
 								{
-									if (&r != &citem && r.type == NM_TYPE_RADIO && r.radio().group == citem.radio().group && r.value)
+									if (&r != &citem && r.value && r.type == NM_TYPE_RADIO && r.radio().group == cg)
 									{
 										r.value = 0;
 										changed = 1;
 									}
 								}
-								citem.value = 1;
+								}
 								break;
 							case NM_TYPE_TEXT:
 								menu->citem=old_choice;
 								menu->mouse_state=0;
+								break;
+							case NM_TYPE_MENU:
+							case NM_TYPE_INPUT:
+							case NM_TYPE_NUMBER:
+							case NM_TYPE_INPUT_MENU:
+							case NM_TYPE_SLIDER:
 								break;
 						}
 						break;
@@ -990,15 +996,15 @@ static window_event_result newmenu_key_command(window *, const d_event &event, n
 			if ( menu->citem > -1 )	{
 				switch (citem.type)
 				{
+					case NM_TYPE_TEXT:
+					case NM_TYPE_NUMBER:
+					case NM_TYPE_SLIDER:
 					case NM_TYPE_MENU:
 					case NM_TYPE_INPUT:
 					case NM_TYPE_INPUT_MENU:
 						break;
 					case NM_TYPE_CHECK:
-						if (citem.value)
-							citem.value = 0;
-						else
-							citem.value = 1;
+						citem.value ^= 1;
 						if (menu->is_scroll_box)
 						{
 							if (menu->citem==(menu->max_on_menu+menu->scroll_offset-1) || menu->citem==menu->scroll_offset)
@@ -1009,15 +1015,18 @@ static window_event_result newmenu_key_command(window *, const d_event &event, n
 						changed = 1;
 						break;
 					case NM_TYPE_RADIO:
+						citem.value = 1;
+						{
+							const auto cg = citem.radio().group;
 						range_for (auto &i, menu->item_range())
 						{
-							if (&i != &citem && i.type == NM_TYPE_RADIO && i.radio().group == citem.radio().group && i.value)
+							if (&i != &citem && i.value && i.type == NM_TYPE_RADIO && i.radio().group == cg)
 							{
 								i.value = 0;
+								changed = 1;
 							}
 						}
-						citem.value = 1;
-						changed = 1;
+						}
 						break;
 				}
 			}

common/main/newmenu.h
@@ -46,14 +46,17 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 struct newmenu;
 struct listbox;
 
-#define NM_TYPE_MENU        0   // A menu item... when enter is hit on this, newmenu_do returns this item number
-#define NM_TYPE_INPUT       1   // An input box... fills the text field in, and you need to fill in text_len field.
-#define NM_TYPE_CHECK       2   // A check box. Set and get its status by looking at flags field (1=on, 0=off)
-#define NM_TYPE_RADIO       3   // Same as check box, but only 1 in a group can be set at a time. Set group fields.
-#define NM_TYPE_TEXT        4   // A line of text that does nothing.
-#define NM_TYPE_NUMBER      5   // A numeric entry counter.  Changes value from min_value to max_value;
-#define NM_TYPE_INPUT_MENU  6   // A inputbox that you hit Enter to edit, when done, hit enter and menu leaves.
-#define NM_TYPE_SLIDER      7   // A slider from min_value to max_value. Draws with text_len chars.
+enum nm_type : uint8_t
+{
+	NM_TYPE_MENU = 0,   // A menu item... when enter is hit on this, newmenu_do returns this item number
+	NM_TYPE_INPUT = 1,   // An input box... fills the text field in, and you need to fill in text_len field.
+	NM_TYPE_CHECK = 2,   // A check box. Set and get its status by looking at flags field (1=on, 0=off)
+	NM_TYPE_RADIO = 3,   // Same as check box, but only 1 in a group can be set at a time. Set group fields.
+	NM_TYPE_TEXT = 4,   // A line of text that does nothing.
+	NM_TYPE_NUMBER = 5,   // A numeric entry counter.  Changes value from min_value to max_value;
+	NM_TYPE_INPUT_MENU = 6,   // A inputbox that you hit Enter to edit, when done, hit enter and menu leaves.
+	NM_TYPE_SLIDER = 7,   // A slider from min_value to max_value. Draws with text_len chars.
+};
 
 #define NM_MAX_TEXT_LEN     255
 
@@ -107,7 +110,6 @@ class newmenu_item
 			return v;
 		}
 public:
-	int     type;           // What kind of item this is, see NM_TYPE_????? defines
 	int     value;          // For checkboxes and radio buttons, this is 1 if marked initially, else 0
 	union {
 		input_specific_type nm_private_input;
@@ -146,6 +148,7 @@ public:
 	short   x, y;
 	short   w, h;
 	short   right_offset;
+	nm_type type;           // What kind of item this is, see NM_TYPE_????? defines
 	ntstring<NM_MAX_TEXT_LEN> saved_text;
 };
 

commit d955eda96fbaa58a734b026af6c277d5b0d42709

Fix fullscreen checkbox handling
Reported-by: Mako88 <https://github.com/dxx-rebirth/dxx-rebirth/issues/224>
Fixes: 7128141c5558190f4f2221c802ad4883b1e234fc ("Use enum for newmenu item type")

similar/arch/ogl/gr.cpp
@@ -461,7 +461,7 @@ namespace dcx {
 
 int gr_check_fullscreen(void)
 {
-	return (sdl_video_flags & SDL_FULLSCREEN);
+	return !!(sdl_video_flags & SDL_FULLSCREEN);
 }
 
 void gr_toggle_fullscreen()

similar/arch/sdl/gr.cpp
@@ -147,7 +147,7 @@ namespace dcx {
 
 int gr_check_fullscreen(void)
 {
-	return (sdl_video_flags & SDL_FULLSCREEN);
+	return !!(sdl_video_flags & SDL_FULLSCREEN);
 }
 
 void gr_toggle_fullscreen()

similar/main/newmenu.cpp
@@ -688,6 +688,21 @@ static void newmenu_scroll(newmenu *const menu, const int amount)
 	}
 }
 
+static int nm_trigger_radio_button(newmenu &menu, newmenu_item &citem)
+{
+	citem.value = 1;
+	const auto cg = citem.radio().group;
+	range_for (auto &r, menu.item_range())
+	{
+		if (&r != &citem && r.value && r.type == NM_TYPE_RADIO && r.radio().group == cg)
+		{
+			r.value = 0;
+			return 1;
+		}
+	}
+	return 0;
+}
+
 static window_event_result newmenu_mouse(window *wind,const d_event &event, newmenu *menu, int button)
 {
 	int old_choice, mx=0, my=0, mz=0, x1 = 0, x2, y1, y2, changed = 0;
@@ -721,24 +736,11 @@ static window_event_result newmenu_mouse(window *wind,const d_event &event, newm
 						switch (citem.type)
 						{
 							case NM_TYPE_CHECK:
-								citem.value ^= 1;
-
-								if (menu->is_scroll_box)
+								citem.value = !citem.value;
 								changed = 1;
 								break;
 							case NM_TYPE_RADIO:
-								citem.value = 1;
-								{
-									const auto cg = citem.radio().group;
-								range_for (auto &r, menu->item_range())
-								{
-									if (&r != &citem && r.value && r.type == NM_TYPE_RADIO && r.radio().group == cg)
-									{
-										r.value = 0;
-										changed = 1;
-									}
-								}
-								}
+								changed = nm_trigger_radio_button(*menu, citem);
 								break;
 							case NM_TYPE_TEXT:
 								menu->citem=old_choice;
@@ -1025,29 +1027,11 @@ static window_event_result newmenu_key_command(window *, const d_event &event, n
 					case NM_TYPE_INPUT_MENU:
 						break;
 					case NM_TYPE_CHECK:
-						citem.value ^= 1;
-						if (menu->is_scroll_box)
-						{
-							if (menu->citem==(menu->max_on_menu+menu->scroll_offset-1) || menu->citem==menu->scroll_offset)
-							{
-							}
-						}
-
+						citem.value = !citem.value;
 						changed = 1;
 						break;
 					case NM_TYPE_RADIO:
-						citem.value = 1;
-						{
-							const auto cg = citem.radio().group;
-						range_for (auto &i, menu->item_range())
-						{
-							if (&i != &citem && i.value && i.type == NM_TYPE_RADIO && i.radio().group == cg)
-							{
-								i.value = 0;
-								changed = 1;
-							}
-						}
-						}
+						changed = nm_trigger_radio_button(*menu, citem);
 						break;
 				}
 			}