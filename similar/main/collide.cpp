8c4d24ea6f2bc4c022e5ef46948884619c3ec703
Kp <kp@valhallalegends.com>
2016-11-19 17:24:53 +0000

    Shorten collide_robot_and_weapon hitobj test

--- similar/main/collide.cpp
+++ similar/main/collide.cpp
@@ -1638,34 +1638,20 @@ static void collide_robot_and_weapon(const vobjptridx_t  robot, const vobjptridx
 		smega_rock_stuff();
 #endif
 
-#if 1
 	/*
 	 * Check if persistent weapon already hit this object. If yes, abort.
 	 * If no, add this object to hitobj_list and do it's damage.
 	 */
 	if (weapon->mtype.phys_info.flags & PF_PERSISTENT)
 	{
-		auto was_hit = weapon->ctype.laser_info.hitobj_list[robot];
-		if (!was_hit)
+		if (auto &&was_hit = weapon->ctype.laser_info.hitobj_list[robot])
+			return;
+		else
 		{
 			was_hit = true;
 			weapon->ctype.laser_info.last_hitobj = robot;
 		}
-		else
-		{
-			return;
-		}
 	}
-#else
-	//	If a persistent weapon hit robot most recently, quick abort, else we cream the same robot many times,
-	//	depending on frame rate.
-	if (weapon->mtype.phys_info.flags & PF_PERSISTENT) {
-		if (weapon->ctype.laser_info.last_hitobj == robot-Objects)
-			return;
-		else
-			weapon->ctype.laser_info.last_hitobj = robot-Objects;
-	}
-#endif
 
 	if (weapon->ctype.laser_info.parent_signature == robot->signature)
 		return;
