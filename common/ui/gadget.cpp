dxx-rebirth/dxx-rebirth

commit 917f079f023c42d88fb18df59eb445c1d1e14b30

Check for window_event_result::deleted in ui_dialog_do_gadgets
This is instead of using window_exists, which could give a false positive if a new window was allocated with the same pointer value as a deleted one.

common/ui/gadget.cpp
@@ -318,7 +318,7 @@ window_event_result ui_dialog_do_gadgets(UI_DIALOG * dlg,const d_event &event)
 		//if (!is_under_another_window( dlg, tmp ))
 			rval = ui_gadget_do(dlg, tmp, event);
 		
-		if (!window_exists(wind))
+		if (rval == window_event_result::deleted)
 			break;
 
 		tmp = tmp->next;

commit 98afafb80f676b7fdae41e457291023c1b44a3f7

Fix gadget.cpp -Wunused-but-set-variable
Commit 917f079 removed all reads of `wind`, but did not remove its
assignment or declaration.  This breaks the build with gcc due to
-Wunused-but-set-variable.

Fixes: 917f079f023c42d88fb18df59eb445c1d1e14b30 ("Check for window_event_result::deleted in ui_dialog_do_gadgets")

common/ui/gadget.cpp
@@ -225,7 +225,6 @@ window_event_result ui_dialog_do_gadgets(UI_DIALOG * dlg,const d_event &event)
 {
 	int keypress = 0;
 	UI_GADGET * tmp, * tmp1;
-	window *wind;
 
 	if (event.type == EVENT_KEY_COMMAND)
 		keypress = event_key_get(event);
@@ -310,7 +309,6 @@ window_event_result ui_dialog_do_gadgets(UI_DIALOG * dlg,const d_event &event)
 	}
 
 	tmp = dlg->gadget;
-	wind = ui_dialog_get_window(dlg);
 	do
 	{
 		// If it is under another dialog, that dialog's handler would have returned 1 for mouse events.