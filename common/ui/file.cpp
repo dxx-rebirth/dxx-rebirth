dxx-rebirth/dxx-rebirth

commit 1c8ad24da3d6b1370d4bd71558ffebef525b93db

Check for window_event_result::deleted for ui_get_filename polling loop
This is instead of using window_exists, which could give a false positive if a new window was allocated with the same pointer value as a deleted one.

common/ui/file.cpp
@@ -301,8 +301,7 @@ int ui_get_filename(char (&filename)[PATH_MAX], const char *const filespec, cons
 	
 	wind = ui_dialog_get_window(dlg);
 	
-	while (window_exists(wind))
-		event_process();
+	while (event_process() != window_event_result::deleted) {}
 
 	//key_flush();
 

commit 6db96d1a6db3f5170cf42d88c3c71066bcd3e51d

Fix file.cpp -Wunused-but-set-variable
Commit 1c8ad24 removed all reads of `wind`, but did not remove its
assignment or declaration.  This breaks the build with gcc due to
-Wunused-but-set-variable.

Fixes: 1c8ad24da3d6b1370d4bd71558ffebef525b93db ("Check for window_event_result::deleted for ui_get_filename polling loop")

common/ui/file.cpp
@@ -240,7 +240,6 @@ int ui_get_filename(char (&filename)[PATH_MAX], const char *const filespec, cons
 	char		InputText[PATH_MAX];
 	char		*p;
 	UI_DIALOG	*dlg;
-	window		*wind;
 	int			rval = 0;
 	auto b = make_unique<ui_file_browser>();
 	if ((p = strrchr(filename, '/')))
@@ -298,8 +297,6 @@ int ui_get_filename(char (&filename)[PATH_MAX], const char *const filespec, cons
 	b->filename = filename;
 	b->filespec = filespec;
 	b->message = message;
-	
-	wind = ui_dialog_get_window(dlg);
 	
 	event_process_all();
 