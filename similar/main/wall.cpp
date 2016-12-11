dxx-rebirth/dxx-rebirth

commit 1f434f98ad156bff3303adcd442491f1c9332583

Use valptridx for ActiveDoors

similar/main/wall.cpp
@@ -42,8 +42,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 namespace dcx {
 unsigned Num_wall_anims;
-unsigned Num_open_doors;						// Number of open doors
-array<active_door, MAX_DOORS> ActiveDoors;
 }
 
 namespace dsx {
@@ -226,7 +224,7 @@ void wall_init()
 		w.clip_num = -1;
 		w.linked_wall = -1;
 	}
-	Num_open_doors = 0;
+	ActiveDoors.set_count(0);
 #if defined(DXX_BUILD_DESCENT_II)
 	CloakingWalls.set_count(0);
 #endif
@@ -368,14 +366,14 @@ void wall_open_door(const vsegptridx_t seg, int side)
 #endif
 
 	if (w->state == WALL_DOOR_CLOSING) {		//closing, so reuse door
-		const auto &&r = partial_range(ActiveDoors, Num_open_doors);
+		const auto &&r = make_range(vactdoorptr);
 		const auto &&i = std::find_if(r.begin(), r.end(), find_active_door_predicate(wall_num));
 		if (i == r.end())	// likely in demo playback or multiplayer
 		{
-			d = &ActiveDoors[Num_open_doors];
+			const auto c = ActiveDoors.get_count();
+			ActiveDoors.set_count(c + 1);
+			d = vactdoorptr(static_cast<actdoornum_t>(c));
 			d->time = 0;
-			Num_open_doors++;
-			Assert( Num_open_doors < MAX_DOORS );
 		}
 		else
 		{
@@ -387,10 +385,10 @@ void wall_open_door(const vsegptridx_t seg, int side)
 	}
 	else {											//create new door
 		Assert(w->state == WALL_DOOR_CLOSED);
-		d = &ActiveDoors[Num_open_doors];
+		const auto i = ActiveDoors.get_count();
+		ActiveDoors.set_count(i + 1);
+		d = vactdoorptr(static_cast<actdoornum_t>(i));
 		d->time = 0;
-		Num_open_doors++;
-		Assert( Num_open_doors < MAX_DOORS );
 	}
 
 
@@ -686,7 +684,7 @@ void wall_close_door(const vsegptridx_t seg, int side)
 		return;
 
 	if (w->state == WALL_DOOR_OPENING) {	//reuse door
-		const auto &&r = partial_range(ActiveDoors, Num_open_doors);
+		const auto &&r = make_range(vactdoorptr);
 		const auto &&i = std::find_if(r.begin(), r.end(), find_active_door_predicate(wall_num));
 		if (i == r.end())
 		{
@@ -702,10 +700,10 @@ void wall_close_door(const vsegptridx_t seg, int side)
 	}
 	else {											//create new door
 		Assert(w->state == WALL_DOOR_OPEN);
-		d = &ActiveDoors[Num_open_doors];
+		const auto i = ActiveDoors.get_count();
+		ActiveDoors.set_count(i + 1);
+		d = vactdoorptr(static_cast<actdoornum_t>(i));
 		d->time = 0;
-		Num_open_doors++;
-		Assert( Num_open_doors < MAX_DOORS );
 	}
 
 	w->state = WALL_DOOR_CLOSING;
@@ -1242,9 +1240,9 @@ namespace dsx {
 void wall_frame_process()
 {
 	{
-		const auto &&r = partial_range(ActiveDoors, Num_open_doors);
+		const auto &&r = make_range(vactdoorptr);
 		auto &&i = std::remove_if(r.begin(), r.end(), ad_removal_predicate());
-		Num_open_doors = std::distance(r.begin(), i);
+		ActiveDoors.set_count(std::distance(r.begin(), i));
 	}
 #if defined(DXX_BUILD_DESCENT_II)
 	if (Newdemo_state != ND_STATE_PLAYBACK)

common/main/fwd-wall.h
@@ -189,9 +189,9 @@ extern array<wclip, MAX_WALL_ANIMS> WallAnims;
 }
 
 namespace dcx {
-extern array<active_door, MAX_DOORS> ActiveDoors;  //  Master doors array
+using actdoornum_t = uint8_t;
+DXX_VALPTRIDX_DECLARE_GLOBAL_SUBTYPE(active_door, actdoor, ActiveDoors, MAX_DOORS);
 #define Num_walls Walls.get_count()
-extern unsigned Num_open_doors;              // Number of open doors
 extern unsigned Num_wall_anims;
 }
 #endif

common/main/wall.h
@@ -151,6 +151,8 @@ struct active_door : public prohibit_void_ptr<active_door>
 	fix     time;               // how long been opening, closing, waiting
 };
 
+DXX_VALPTRIDX_DEFINE_GLOBAL_FACTORIES(active_door, actdoor);
+
 }
 
 #ifdef dsx

similar/editor/medwall.cpp
@@ -598,7 +598,7 @@ int wall_restore_all()
 		w.flags &= ~(WALL_BLASTED | WALL_DOOR_OPENED | WALL_DOOR_OPENING);
 	}
 
-	range_for (auto &i, partial_range(ActiveDoors, Num_open_doors))
+	range_for (auto &&i, vactdoorptr)
 		wall_close_door_ref(i);
 
 	range_for (auto &&i, vsegptr)

similar/main/dumpmine.cpp
@@ -650,7 +650,7 @@ void write_game_text_file(const char *filename)
 	PHYSFSX_printf(my_file, "\nNumber of segments:   %4i\n", Highest_segment_index+1);
 	PHYSFSX_printf(my_file, "Number of objects:    %4i\n", Highest_object_index+1);
 	PHYSFSX_printf(my_file, "Number of walls:      %4i\n", Num_walls);
-	PHYSFSX_printf(my_file, "Number of open doors: %4i\n", Num_open_doors);
+	PHYSFSX_printf(my_file, "Number of open doors: %4i\n", ActiveDoors.get_count());
 	PHYSFSX_printf(my_file, "Number of triggers:   %4i\n", Num_triggers);
 	PHYSFSX_printf(my_file, "Number of matcens:    %4i\n", Num_robot_centers);
 	PHYSFSX_printf(my_file, "\n");

similar/main/gamesave.cpp
@@ -1059,7 +1059,7 @@ static int load_game_data(PHYSFS_File *LoadFile)
 
 	reset_walls();
 
-	Num_open_doors = 0;
+	ActiveDoors.set_count(0);
 
 	//go through all walls, killing references to invalid triggers
 	range_for (const auto &&p, vwallptr)
@@ -1485,7 +1485,7 @@ int create_new_mine(void)
 	}
 	
 	Num_robot_centers = 0;
-	Num_open_doors = 0;
+	ActiveDoors.set_count(0);
 	wall_init();
 	trigger_init();
 	
@@ -1551,7 +1551,7 @@ static int save_game_data(PHYSFS_File *SaveFile)
 
 	WRITE_HEADER_ENTRY(object, Highest_object_index + 1);
 	WRITE_HEADER_ENTRY(wall, Num_walls);
-	WRITE_HEADER_ENTRY(active_door, Num_open_doors);
+	WRITE_HEADER_ENTRY(active_door, ActiveDoors.get_count());
 	WRITE_HEADER_ENTRY(trigger, Num_triggers);
 	WRITE_HEADER_ENTRY(0, 0);		// links (removed by Parallax)
 	WRITE_HEADER_ENTRY(control_center_triggers, 1);

similar/main/mglobal.cpp
@@ -40,6 +40,7 @@ namespace dcx {
 unsigned Num_segments;
 // Global array of vertices, common to one mine.
 array<vertex, MAX_VERTICES> Vertices;
+valptridx<active_door>::array_managed_type ActiveDoors;
 }
 array<g3s_point, MAX_VERTICES> Segment_points;
 
@@ -111,6 +112,7 @@ valptridx<trigger>::array_managed_type Triggers;
 valptridx<wall>::array_managed_type Walls;
 }
 
+template class valptridx<active_door>;
 #if defined(DXX_BUILD_DESCENT_II)
 template class valptridx<cloaking_wall>;
 #endif

similar/main/state.cpp
@@ -1088,10 +1088,10 @@ int state_save_all_sub(const char *filename, const char *desc)
 
 //Save door info
 	{
-		const int i = Num_open_doors;
+		const int i = ActiveDoors.get_count();
 	PHYSFS_write(fp, &i, sizeof(int), 1);
 	}
-	range_for (auto &ad, partial_const_range(ActiveDoors, Num_open_doors))
+	range_for (auto &&ad, vcactdoorptr)
 		active_door_write(fp, ad);
 
 #if defined(DXX_BUILD_DESCENT_II)
@@ -1626,8 +1626,8 @@ int state_restore_all_sub(const char *filename, const secret_restore secret)
 #endif
 
 	//Restore door info
-	Num_open_doors = PHYSFSX_readSXE32(fp, swap);
-	range_for (auto &ad, partial_range(ActiveDoors, Num_open_doors))
+	ActiveDoors.set_count(PHYSFSX_readSXE32(fp, swap));
+	range_for (auto &&ad, vactdoorptr)
 		active_door_read(fp, ad);
 
 #if defined(DXX_BUILD_DESCENT_II)

commit 4a01fab66d98cf593e9e46e64d763393de019037

Fix failure to remove expired walls
std::remove_if can be called on an iterator that returns a proxy object,
but the results of that call are not generally useful.  When remove_if
attempts to move-assign over removed elements, it instead assigns over
the temporary proxy object.  If the proxy object has typical move
semantics, move-assigning over it has no effect on the underlying
container.

Revert to using partial_range on the underlying container.

Reported-by: Mako88 <https://github.com/dxx-rebirth/dxx-rebirth/issues/283>
Fixes: 1f434f98ad156bff3303adcd442491f1c9332583 ("Use valptridx for ActiveDoors")

similar/main/wall.cpp
@@ -1240,14 +1240,14 @@ namespace dsx {
 void wall_frame_process()
 {
 	{
-		const auto &&r = make_range(vactdoorptr);
+		const auto &&r = partial_range(ActiveDoors, ActiveDoors.get_count());
 		auto &&i = std::remove_if(r.begin(), r.end(), ad_removal_predicate());
 		ActiveDoors.set_count(std::distance(r.begin(), i));
 	}
 #if defined(DXX_BUILD_DESCENT_II)
 	if (Newdemo_state != ND_STATE_PLAYBACK)
 	{
-		const auto &&r = make_range(vclwallptr);
+		const auto &&r = partial_range(CloakingWalls, CloakingWalls.get_count());
 		cw_removal_predicate rp;
 		std::remove_if(r.begin(), r.end(), std::ref(rp));
 		CloakingWalls.set_count(rp.num_cloaking_walls);