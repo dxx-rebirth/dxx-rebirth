dxx-rebirth/dxx-rebirth

commit 2bd538f3538c4e60758e0300fc20716d38f3f6c7

Sort delta light indices

similar/main/gameseg.cpp
@@ -1641,9 +1641,14 @@ static void change_segment_light(const vsegptridx_t segp,int sidenum,int dir)
 static void change_light(const vsegptridx_t segnum, int sidenum, int dir)
 {
 	const fix ds = dir * DL_SCALE;
-	range_for (auto &i, partial_range(Dl_indices, Num_static_lights))
-		if (i.segnum == segnum && i.sidenum == sidenum)
+	range_for (const auto &i, partial_range(Dl_indices, Num_static_lights))
+	{
+		if (i.segnum == segnum)
 		{
+			if (i.sidenum < sidenum)
+				break;
+			if (i.sidenum > sidenum)
+				continue;
 			range_for (auto &j, partial_range(Delta_lights, static_cast<uint_fast32_t>(i.index), static_cast<uint_fast32_t>(i.count)))
 			{
 				const auto &&segp = vsegptr(j.segnum);
@@ -1657,6 +1662,9 @@ static void change_light(const vsegptridx_t segnum, int sidenum, int dir)
 				}
 			}
 		}
+		else if (i.segnum < segnum)
+			break;
+	}
 
 	//recompute static light for segment
 	change_segment_light(segnum,sidenum,dir);

similar/main/gamesave.cpp
@@ -991,8 +991,17 @@ static int load_game_data(PHYSFS_file *LoadFile)
 	}
 	else
 	{
-		range_for (auto &i, partial_range(Dl_indices, Num_static_lights))
+		const auto &&lr = partial_range(Dl_indices, Num_static_lights);
+		range_for (auto &i, lr)
 			dl_index_read(&i, LoadFile);
+		const auto predicate = [](const dl_index &a, const dl_index &b) {
+			if (a.segnum < b.segnum)
+				return true;
+			if (a.segnum > b.segnum)
+				return false;
+			return a.sidenum < b.sidenum;
+		};
+		std::sort(lr.begin(), lr.end(), predicate);
 	}
 
 	//	Indicate that no light has been subtracted from any vertices.

commit e07f5072cb63e89b4aa3c87b4f8f87c791e28862

Fix inverted change_light test
An optimization meant to allow change_light to stop early caused it to
stop much too early, so it rarely did any work.  Flip the sense of the
test so it does not stop early.

Fixes: 2bd538f3538c4e60758e0300fc20716d38f3f6c7 ("Sort delta light indices")

similar/main/gameseg.cpp
@@ -1690,9 +1690,9 @@ static void change_light(const vsegptridx_t segnum, int sidenum, int dir)
 	{
 		if (i.segnum == segnum)
 		{
-			if (i.sidenum < sidenum)
-				break;
 			if (i.sidenum > sidenum)
+				break;
+			if (i.sidenum < sidenum)
 				continue;
 			range_for (auto &j, partial_const_range(Delta_lights, static_cast<uint_fast32_t>(i.index), static_cast<uint_fast32_t>(i.count)))
 			{
@@ -1707,7 +1707,7 @@ static void change_light(const vsegptridx_t segnum, int sidenum, int dir)
 				}
 			}
 		}
-		else if (i.segnum < segnum)
+		else if (i.segnum > segnum)
 			break;
 	}
 