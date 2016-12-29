dxx-rebirth/dxx-rebirth

commit f7d0c853ba878c6099a6667dcc7610d909c67f50

Use special types for distance/magnitude

common/include/vecmat.h
@@ -28,6 +28,7 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "maths.h"
 
 #ifdef __cplusplus
+#include <cstdint>
 #include "dxxsconf.h"
 #include <utility>
 
@@ -35,6 +36,56 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 struct vms_vector
 {
 	fix x, y, z;
+};
+
+class vm_distance
+{
+public:
+	fix d;
+	constexpr explicit vm_distance(const fix &f) :
+		d(f)
+	{
+	}
+	template <typename T>
+		operator T() const = delete;
+	constexpr operator fix() const
+	{
+		return d;
+	}
+};
+
+class vm_magnitude : public vm_distance
+{
+public:
+	constexpr explicit vm_magnitude(const uint32_t &f) :
+		vm_distance(f)
+	{
+	}
+};
+
+class vm_distance_squared
+{
+public:
+	fix64 d2;
+	constexpr explicit vm_distance_squared(const fix64 &f2) :
+		d2(f2)
+	{
+	}
+	template <typename T>
+		operator T() const = delete;
+	constexpr operator fix64() const
+	{
+		return d2;
+	}
+};
+
+class vm_magnitude_squared : public vm_distance_squared
+{
+public:
+	constexpr explicit vm_magnitude_squared(const uint64_t &f2) :
+		vm_distance_squared(f2)
+	{
+	}
 };
 
 #define DEFINE_SERIAL_VMS_VECTOR_TO_MESSAGE()	\
@@ -178,29 +229,35 @@ void vm_vec_scale_add2 (vms_vector &dest, const vms_vector &src, fix k);
 //dest *= n/d
 void vm_vec_scale2 (vms_vector &dest, fix n, fix d);
 
-fix64 vm_vec_mag2 (const vms_vector &v) __attribute_warn_unused_result;
+__attribute_warn_unused_result
+vm_magnitude_squared vm_vec_mag2(const vms_vector &v);
 //returns magnitude of a vector
-fix vm_vec_mag (const vms_vector &v) __attribute_warn_unused_result;
+__attribute_warn_unused_result
+vm_magnitude vm_vec_mag(const vms_vector &v);
 
 
 //computes the distance between two points. (does sub and mag)
-fix vm_vec_dist (const vms_vector &v0, const vms_vector &v1) __attribute_warn_unused_result;
-fix64 vm_vec_dist2 (const vms_vector &v0, const vms_vector &v1) __attribute_warn_unused_result;
+__attribute_warn_unused_result
+vm_distance vm_vec_dist(const vms_vector &v0, const vms_vector &v1);
+__attribute_warn_unused_result
+vm_distance_squared vm_vec_dist2(const vms_vector &v0, const vms_vector &v1);
 
 
 //computes an approximation of the magnitude of the vector
 //uses dist = largest + next_largest*3/8 + smallest*3/16
-fix vm_vec_mag_quick (const vms_vector &v) __attribute_warn_unused_result;
+__attribute_warn_unused_result
+vm_magnitude vm_vec_mag_quick(const vms_vector &v);
 
 
 //computes an approximation of the distance between two points.
 //uses dist = largest + next_largest*3/8 + smallest*3/16
-fix vm_vec_dist_quick (const vms_vector &v0, const vms_vector &v1) __attribute_warn_unused_result;
+__attribute_warn_unused_result
+vm_distance vm_vec_dist_quick(const vms_vector &v0, const vms_vector &v1);
 
 //normalize a vector. returns mag of source vec
-fix vm_vec_copy_normalize (vms_vector &dest, const vms_vector &src);
+vm_magnitude vm_vec_copy_normalize(vms_vector &dest, const vms_vector &src);
 
-fix vm_vec_normalize (vms_vector &v);
+vm_magnitude vm_vec_normalize(vms_vector &v);
 static inline vms_vector vm_vec_normalized(vms_vector v) __attribute_warn_unused_result;
 static inline vms_vector vm_vec_normalized(vms_vector v)
 {
@@ -208,9 +265,9 @@ static inline vms_vector vm_vec_normalized(vms_vector v)
 }
 
 //normalize a vector. returns mag of source vec. uses approx mag
-fix vm_vec_copy_normalize_quick (vms_vector &dest, const vms_vector &src);
+vm_magnitude vm_vec_copy_normalize_quick(vms_vector &dest, const vms_vector &src);
 
-fix vm_vec_normalize_quick (vms_vector &v);
+vm_magnitude vm_vec_normalize_quick(vms_vector &v);
 static inline vms_vector vm_vec_normalized_quick(vms_vector v) __attribute_warn_unused_result;
 static inline vms_vector vm_vec_normalized_quick(vms_vector v)
 {
@@ -221,9 +278,9 @@ static inline vms_vector vm_vec_normalized_quick(vms_vector v)
 //return the normalized direction vector between two points
 //dest = normalized(end - start).  Returns mag of direction vector
 //NOTE: the order of the parameters matches the vector subtraction
-fix vm_vec_normalized_dir (vms_vector &dest, const vms_vector &end, const vms_vector &start);
+vm_magnitude vm_vec_normalized_dir (vms_vector &dest, const vms_vector &end, const vms_vector &start);
 
-fix vm_vec_normalized_dir_quick (vms_vector &dest, const vms_vector &end, const vms_vector &start);
+vm_magnitude vm_vec_normalized_dir_quick (vms_vector &dest, const vms_vector &end, const vms_vector &start);
 
 
 ////returns dot product of two vectors

common/maths/vecmat.cpp
@@ -158,35 +158,35 @@ fix vm_vec_dot(const vms_vector &v0,const vms_vector &v1)
 }
 
 //returns magnitude of a vector
-fix64 vm_vec_mag2(const vms_vector &v)
+vm_magnitude_squared vm_vec_mag2(const vms_vector &v)
 {
 	const int64_t x = v.x;
 	const int64_t y = v.y;
 	const int64_t z = v.z;
-	return (x * x) + (y * y) + (z * z);
+	return vm_magnitude_squared{static_cast<uint64_t>((x * x) + (y * y) + (z * z))};
 }
 
-fix vm_vec_mag(const vms_vector &v)
+vm_magnitude vm_vec_mag(const vms_vector &v)
 {
 	quadint q;
-	q.q = vm_vec_mag2(v);
-	return quad_sqrt(q);
+	q.q = vm_vec_mag2(v).d2;
+	return vm_magnitude{quad_sqrt(q)};
 }
 
 //computes the distance between two points. (does sub and mag)
-fix vm_vec_dist(const vms_vector &v0,const vms_vector &v1)
+vm_distance vm_vec_dist(const vms_vector &v0,const vms_vector &v1)
 {
 	return vm_vec_mag(vm_vec_sub(v0,v1));
 }
 
-fix64 vm_vec_dist2(const vms_vector &v0,const vms_vector &v1)
+vm_distance_squared vm_vec_dist2(const vms_vector &v0,const vms_vector &v1)
 {
 	return vm_vec_mag2(vm_vec_sub(v0,v1));
 }
 
 //computes an approximation of the magnitude of the vector
 //uses dist = largest + next_largest*3/8 + smallest*3/16
-fix vm_vec_mag_quick(const vms_vector &v)
+vm_magnitude vm_vec_mag_quick(const vms_vector &v)
 {
 	fix a,b,c,bc;
 
@@ -207,23 +207,21 @@ fix vm_vec_mag_quick(const vms_vector &v)
 
 	bc = (b>>2) + (c>>3);
 
-	return a + bc + (bc>>1);
+	return vm_magnitude{static_cast<uint32_t>(a + bc + (bc>>1))};
 }
 
 
 //computes an approximation of the distance between two points.
 //uses dist = largest + next_largest*3/8 + smallest*3/16
-fix vm_vec_dist_quick(const vms_vector &v0,const vms_vector &v1)
+vm_distance vm_vec_dist_quick(const vms_vector &v0,const vms_vector &v1)
 {
 	return vm_vec_mag_quick(vm_vec_sub(v0,v1));
 }
 
 //normalize a vector. returns mag of source vec
-fix vm_vec_copy_normalize(vms_vector &dest,const vms_vector &src)
+vm_magnitude vm_vec_copy_normalize(vms_vector &dest,const vms_vector &src)
 {
-	fix m;
-
-	m = vm_vec_mag(src);
+	auto m = vm_vec_mag(src);
 	if (m > 0) {
 		vm_vec_divide(dest, src, m);
 	}
@@ -238,16 +236,15 @@ void vm_vec_divide(vms_vector &dest,const vms_vector &src, fix m)
 }
 
 //normalize a vector. returns mag of source vec
-fix vm_vec_normalize(vms_vector &v)
+vm_magnitude vm_vec_normalize(vms_vector &v)
 {
 	return vm_vec_copy_normalize(v,v);
 }
 
 //normalize a vector. returns mag of source vec. uses approx mag
-fix vm_vec_copy_normalize_quick(vms_vector &dest,const vms_vector &src)
+vm_magnitude vm_vec_copy_normalize_quick(vms_vector &dest,const vms_vector &src)
 {
-	fix m;
-	m = vm_vec_mag_quick(src);
+	auto m = vm_vec_mag_quick(src);
 	if (m > 0) {
 		vm_vec_divide(dest, src, m);
 	}
@@ -255,7 +252,7 @@ fix vm_vec_copy_normalize_quick(vms_vector &dest,const vms_vector &src)
 }
 
 //normalize a vector. returns 1/mag of source vec. uses approx 1/mag
-fix vm_vec_normalize_quick(vms_vector &v)
+vm_magnitude vm_vec_normalize_quick(vms_vector &v)
 {
 	return vm_vec_copy_normalize_quick(v,v);
 }
@@ -263,7 +260,7 @@ fix vm_vec_normalize_quick(vms_vector &v)
 //return the normalized direction vector between two points
 //dest = normalized(end - start).  Returns 1/mag of direction vector
 //NOTE: the order of the parameters matches the vector subtraction
-fix vm_vec_normalized_dir_quick(vms_vector &dest,const vms_vector &end,const vms_vector &start)
+vm_magnitude vm_vec_normalized_dir_quick(vms_vector &dest,const vms_vector &end,const vms_vector &start)
 {
 	return vm_vec_normalize_quick(vm_vec_sub(dest,end,start));
 }
@@ -271,7 +268,7 @@ fix vm_vec_normalized_dir_quick(vms_vector &dest,const vms_vector &end,const vms
 //return the normalized direction vector between two points
 //dest = normalized(end - start).  Returns mag of direction vector
 //NOTE: the order of the parameters matches the vector subtraction
-fix vm_vec_normalized_dir(vms_vector &dest,const vms_vector &end,const vms_vector &start)
+vm_magnitude vm_vec_normalized_dir(vms_vector &dest,const vms_vector &end,const vms_vector &start)
 {
 	return vm_vec_normalize(vm_vec_sub(dest,end,start));
 }

commit 3a4eafac03c432560d291e9bb00a728a17c8af82

Add workaround for passive thief bot
zicodxx reports that f7d0c85 made the thief bot passive and timid.
His analysis suggests that the problem is because f7d0c85 changed
find_connected_distance to return vm_distance::maximum_value() in places
where it previously returned magic values that were not maximum (caching
a distance of F1_0*1000 and returning a distance of -1).  Rather than
try to fix the underlying code that relied on these magic values, revert
those return paths to return these unusual values.  Move the unusual
values to named constants in file scope so that they are easier to find
and correlate.

Reported-by: zicodxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/286>
Analyzed-by: zicodxx

Fixes: f7d0c853ba878c6099a6667dcc7610d909c67f50 ("Use special types for distance/magnitude")

common/include/vecmat.h
@@ -111,14 +111,6 @@ public:
 	{
 		return d;
 	}
-	static constexpr vm_distance maximum_value()
-	{
-		return vm_distance{0x7fffffff};
-	}
-	static constexpr vm_distance minimum_value()
-	{
-		return vm_distance{0};
-	}
 	constexpr vm_distance_squared operator*(const vm_distance &) const;
 };
 

similar/main/gameseg.cpp
@@ -84,6 +84,9 @@ public:
 	}
 };
 
+constexpr vm_distance fcd_abort_cache_value{F1_0 * 1000};
+constexpr vm_distance fcd_abort_return_value{-1};
+
 }
 
 // How far a point can be from a plane, and still be "in" the plane
@@ -861,8 +864,8 @@ vm_distance find_connected_distance(const vms_vector &p0, const vcsegptridx_t se
 					if (max_depth != -1) {
 						if (depth[qtail-1] == max_depth) {
 							Connected_segment_distance = 1000;
-							add_to_fcd_cache(seg0, seg1, Connected_segment_distance, vm_distance::maximum_value());
-							return vm_distance::maximum_value();
+							add_to_fcd_cache(seg0, seg1, Connected_segment_distance, fcd_abort_cache_value);
+							return fcd_abort_return_value;
 						}
 					} else if (this_seg == seg1) {
 						goto fcd_done1;
@@ -874,8 +877,8 @@ vm_distance find_connected_distance(const vms_vector &p0, const vcsegptridx_t se
 
 		if (qhead >= qtail) {
 			Connected_segment_distance = 1000;
-			add_to_fcd_cache(seg0, seg1, Connected_segment_distance, vm_distance::maximum_value());
-			return vm_distance::maximum_value();
+			add_to_fcd_cache(seg0, seg1, Connected_segment_distance, fcd_abort_cache_value);
+			return fcd_abort_return_value;
 		}
 
 		cur_seg = seg_queue[qhead].end;
@@ -889,8 +892,8 @@ fcd_done1: ;
 	while (seg_queue[--qtail].end != seg1)
 		if (qtail < 0) {
 			Connected_segment_distance = 1000;
-			add_to_fcd_cache(seg0, seg1, Connected_segment_distance, vm_distance::maximum_value());
-			return vm_distance::maximum_value();
+			add_to_fcd_cache(seg0, seg1, Connected_segment_distance, fcd_abort_cache_value);
+			return fcd_abort_return_value;
 		}
 
 	while (qtail >= 0) {