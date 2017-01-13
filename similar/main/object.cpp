dxx-rebirth/dxx-rebirth

commit 4cc801f42fdce011c2b5e332cb63ac120420ea42

Remove calls to window_close(Game_wind) when game finished or over
Replace calls to window_close(Game_wind) with returning window_event_result::close to game handler. Applies to when DoEndGame() is called, DoGameOver() is called, aborting in the kmatrix screen (multiplayer game) during AdvanceLevel() and playing one demo frame causes playback to stop in GameProcessFrame(). Closing a window within its handler is problematic - it can result in an unstable state.

similar/main/object.cpp
@@ -1347,7 +1347,7 @@ static void set_camera_pos(vms_vector &camera_pos, const vcobjptridx_t objp)
 }
 
 //	------------------------------------------------------------------------------------------------------------------
-void dead_player_frame(void)
+window_event_result dead_player_frame()
 {
 	static fix	time_dead = 0;
 
@@ -1450,11 +1450,13 @@ void dead_player_frame(void)
 				}
 			}
 
-			DoPlayerDead();		//kill_player();
+			return DoPlayerDead();		//kill_player();
 		}
 	}
 	else
 		time_dead = 0;
+
+	return window_event_result::handled;
 }
 
 //	------------------------------------------------------------------------------------------------------------------
@@ -1619,9 +1621,10 @@ int Drop_afterburner_blob_flag;		//ugly hack
 
 //--------------------------------------------------------------------
 //move an object for the current frame
-static void object_move_one(const vobjptridx_t obj)
+static window_event_result object_move_one(const vobjptridx_t obj)
 {
 	const auto previous_segment = obj->segnum;
+	auto result = window_event_result::handled;
 
 	obj->last_pos = obj->pos;			// Save the current position
 
@@ -1676,7 +1679,7 @@ static void object_move_one(const vobjptridx_t obj)
 
 		case CT_AI:
 			//NOTE LINK TO CT_MORPH ABOVE!!!
-			if (Game_suspended & SUSP_ROBOTS) return;
+			if (Game_suspended & SUSP_ROBOTS) return window_event_result::ignored;
 			do_ai_frame(obj);
 			break;
 
@@ -1726,13 +1729,13 @@ static void object_move_one(const vobjptridx_t obj)
 	}
 
 	if (obj->type == OBJ_NONE || obj->flags&OF_SHOULD_BE_DEAD)
-		return;         // object has been deleted
+		return window_event_result::ignored;         // object has been deleted
 
 	switch (obj->movement_type) {
 
 		case MT_NONE:			break;				//this doesn't move
 
-		case MT_PHYSICS:		do_physics_sim(obj);	break;	//move by physics
+		case MT_PHYSICS:		result = do_physics_sim(obj);	break;	//move by physics
 
 		case MT_SPINNING:		spin_object(obj); break;
 
@@ -1754,11 +1757,11 @@ static void object_move_one(const vobjptridx_t obj)
 				const auto connect_side = find_connect_side(seg1, seg0);
 				if (connect_side != side_none)
 				{
-					check_trigger(seg0, connect_side, get_local_plrobj(), obj, 0);
+					result = check_trigger(seg0, connect_side, get_local_plrobj(), obj, 0);
 #if defined(DXX_BUILD_DESCENT_II)
 				//maybe we've gone on to the next level.  if so, bail!
 				if (Current_level_num != old_level)
-					return;
+					return window_event_result::ignored;
 #endif
 				}
 				seg0 = seg1;
@@ -1857,13 +1860,17 @@ static void object_move_one(const vobjptridx_t obj)
 			obj->ctype.laser_info.last_afterburner_time = GameTime64;
 		}
 	}
+
+	return result;
 #endif
 }
 
 //--------------------------------------------------------------------
 //move all objects for the current frame
-void object_move_all()
+window_event_result object_move_all()
 {
+	auto result = window_event_result::ignored;
+
 	if (Highest_object_index > MAX_USED_OBJECTS)
 		free_object_slots(MAX_USED_OBJECTS);		//	Free all possible object slots.
 
@@ -1878,13 +1885,14 @@ void object_move_all()
 	range_for (const auto &&objp, vobjptridx)
 	{
 		if ( (objp->type != OBJ_NONE) && (!(objp->flags&OF_SHOULD_BE_DEAD)) )	{
-			object_move_one( objp );
+			result = std::max(object_move_one( objp ), result);
 		}
 	}
 
 //	check_duplicate_objects();
 //	remove_incorrect_objects();
 
+	return result;
 }
 
 

common/main/cntrlcen.h
@@ -31,6 +31,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "fwd-object.h"
 #include "pack.h"
 #include "fwd-segment.h"
+#include "fwd-window.h"
 
 #include "fwd-partial_range.h"
 
@@ -120,7 +121,7 @@ void init_controlcen_for_level();
 void calc_controlcen_gun_point(object &obj);
 
 void do_controlcen_destroyed_stuff(objptridx_t objp);
-void do_controlcen_dead_frame();
+window_event_result do_controlcen_dead_frame();
 }
 #endif
 

common/main/collide.h
@@ -32,10 +32,11 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "fwd-object.h"
 #include "fwd-segment.h"
 #include "fwd-vecmat.h"
+#include "fwd-window.h"
 
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
 void collide_two_objects(vobjptridx_t A, vobjptridx_t B, vms_vector &collision_point);
-void collide_object_with_wall(vobjptridx_t A, fix hitspeed, vsegptridx_t hitseg, short hitwall, const vms_vector &hitpt);
+window_event_result collide_object_with_wall(vobjptridx_t A, fix hitspeed, vsegptridx_t hitseg, short hitwall, const vms_vector &hitpt);
 namespace dsx {
 void apply_damage_to_player(object &player, cobjptridx_t killer, fix damage, uint8_t possibly_friendly);
 }
@@ -84,7 +85,7 @@ void drop_player_eggs(vobjptridx_t playerobj);
 }
 #endif
 #if defined(DXX_BUILD_DESCENT_II)
-void do_final_boss_frame(void);
+window_event_result do_final_boss_frame(void);
 void do_final_boss_hacks(void);
 enum class volatile_wall_result : int8_t
 {

common/main/endlevel.h
@@ -29,17 +29,18 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "vecmat.h"
 #include "fwd-segment.h"
 #include "gr.h"
+#include "fwd-window.h"
 
 extern int Endlevel_sequence;
 #ifdef dsx
 namespace dsx {
-void do_endlevel_frame();
+window_event_result do_endlevel_frame();
 }
 #endif
-void stop_endlevel_sequence();
+window_event_result stop_endlevel_sequence();
 #ifdef dsx
 namespace dsx {
-void start_endlevel_sequence();
+window_event_result start_endlevel_sequence();
 }
 #endif
 void render_endlevel_frame(fix eye_offset);

common/main/fwd-object.h
@@ -16,6 +16,7 @@
 #include "cpp-valptridx.h"
 #include "fwd-vecmat.h"
 #include "fwd-segment.h"
+#include "fwd-window.h"
 
 struct bitmap_index;
 
@@ -255,7 +256,7 @@ void render_object(vobjptridx_t obj);
 void draw_object_tmap_rod(vobjptridx_t obj, bitmap_index bitmap, int lighted);
 
 // move all objects for the current frame
-void object_move_all();     // moves all objects
+window_event_result object_move_all();     // moves all objects
 
 // set viewer object to next object in array
 void object_goto_next_viewer();
@@ -328,7 +329,7 @@ void special_reset_objects();
 void obj_attach(vobjptridx_t parent,vobjptridx_t sub);
 
 void create_small_fireball_on_object(vobjptridx_t objp, fix size_scale, int sound_flag);
-void dead_player_frame();
+window_event_result dead_player_frame();
 
 #if defined(DXX_BUILD_DESCENT_II)
 extern int Drop_afterburner_blob_flag;		//ugly hack

common/main/gameseq.h
@@ -28,6 +28,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include <cstddef>
 #include "fwd-player.h"
 #include "fwd-object.h"
+#include "fwd-window.h"
 
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
 struct player;
@@ -72,13 +73,13 @@ void init_player_stats_game(ubyte pnum);      //clear all stats
 // if secret flag is true, advance to secret level, else next normal level
 #ifdef dsx
 namespace dsx {
-void PlayerFinishedLevel(int secret_flag);
+window_event_result PlayerFinishedLevel(int secret_flag);
 
 }
 #endif
 namespace dsx {
 // called when the player has died
-void DoPlayerDead(void);
+window_event_result DoPlayerDead(void);
 }
 
 #if defined(DXX_BUILD_DESCENT_I)
@@ -91,7 +92,7 @@ namespace dsx {
 // load just the hxm file
 void load_level_robots(int level_num);
 extern int	First_secret_visit;
-void ExitSecretLevel();
+window_event_result ExitSecretLevel();
 }
 #endif
 

common/main/physics.h
@@ -29,6 +29,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #include "vecmat.h"
 #include "fvi.h"
+#include "fwd-window.h"
 
 #ifdef __cplusplus
 
@@ -46,7 +47,7 @@ extern array<segnum_t, MAX_FVI_SEGS> phys_seglist;
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
 #ifdef dsx
 namespace dsx {
-void do_physics_sim(vobjptridx_t obj);
+window_event_result do_physics_sim(vobjptridx_t obj);
 
 }
 #endif

common/main/switch.h
@@ -37,6 +37,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "valptridx.h"
 #include "compiler-array.h"
 #include "dsx-ns.h"
+#include "fwd-window.h"
 
 constexpr std::integral_constant<std::size_t, 100> MAX_TRIGGERS{};
 #define MAX_WALLS_PER_LINK  10
@@ -172,8 +173,8 @@ constexpr uint8_t trigger_none = 0xff;
 
 extern void trigger_init();
 namespace dsx {
-void check_trigger(vcsegptridx_t seg, short side, object &plrobj, vcobjptridx_t objnum, int shot);
-int check_trigger_sub(object &, trgnum_t trigger_num, int player_num,int shot);
+window_event_result check_trigger(vcsegptridx_t seg, short side, object &plrobj, vcobjptridx_t objnum, int shot);
+window_event_result check_trigger_sub(object &, trgnum_t trigger_num, int player_num,int shot);
 
 static inline int trigger_is_exit(const trigger *t)
 {

commit 5201218e20293472224df5dc59a99d41b4e43b72

Fix D1 build
Commit 4cc801f changed `object_move_one` to return `window_event_result`
instead of `void` and added a default return value at the bottom.
However, it added the value inside a `#if D2` block, so the D1 build now
fails with:

    similar/main/object.cpp: In function 'dcx::window_event_result d1x::object_move_one(d1x::vobjptridx_t)':
    similar/main/object.cpp:1627:7: error: variable 'result' set but not used [-Werror=unused-but-set-variable]
    similar/main/object.cpp:1866:1: error: control reaches end of non-void function [-Werror=return-type]

Move the return statement out of the conditional block to fix both these
errors.

Fixes: 4cc801f42fdce011c2b5e332cb63ac120420ea42 ("Remove calls to window_close(Game_wind) when game finished or over")

similar/main/object.cpp
@@ -1861,8 +1861,8 @@ static window_event_result object_move_one(const vobjptridx_t obj)
 		}
 	}
 
-	return result;
 #endif
+	return result;
 }
 
 //--------------------------------------------------------------------