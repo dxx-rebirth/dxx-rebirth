dxx-rebirth/dxx-rebirth

commit 10ff2b9ea2eccc895d330c81d0522dfd3e8c6e50

Change Primary_weapon_to_weapon_info key to weapon_id_type
Fix incorrect ID in D1 Primary_weapon_to_weapon_info.  Testing suggests
that both the correct and incorrect ID produce the same results.

similar/main/weapon.cpp
@@ -68,7 +68,9 @@ constexpr tt::integral_constant<uint8_t, 4> has_weapon_result::has_ammo_flag;
 //	Convert primary weapons to indices in Weapon_info array.
 #if defined(DXX_BUILD_DESCENT_I)
 namespace dsx {
-const array<ubyte, MAX_PRIMARY_WEAPONS> Primary_weapon_to_weapon_info{{0, weapon_id_type::VULCAN_ID, 12, weapon_id_type::PLASMA_ID, weapon_id_type::FUSION_ID}};
+const array<weapon_id_type, MAX_PRIMARY_WEAPONS> Primary_weapon_to_weapon_info{{
+	weapon_id_type::LASER_ID, weapon_id_type::VULCAN_ID, weapon_id_type::SPREADFIRE_ID, weapon_id_type::PLASMA_ID, weapon_id_type::FUSION_ID
+}};
 const array<weapon_id_type, MAX_SECONDARY_WEAPONS> Secondary_weapon_to_weapon_info{{weapon_id_type::CONCUSSION_ID, weapon_id_type::HOMING_ID, weapon_id_type::PROXIMITY_ID, weapon_id_type::SMART_ID, weapon_id_type::MEGA_ID}};
 
 //for each Secondary weapon, which gun it fires out of
@@ -77,7 +79,7 @@ const array<ubyte, MAX_SECONDARY_WEAPONS> Secondary_weapon_to_gun_num{{4,4,7,7,7
 #elif defined(DXX_BUILD_DESCENT_II)
 #include "fvi.h"
 namespace dsx {
-const array<ubyte, MAX_PRIMARY_WEAPONS> Primary_weapon_to_weapon_info{{
+const array<weapon_id_type, MAX_PRIMARY_WEAPONS> Primary_weapon_to_weapon_info{{
 	weapon_id_type::LASER_ID, weapon_id_type::VULCAN_ID, weapon_id_type::SPREADFIRE_ID, weapon_id_type::PLASMA_ID, weapon_id_type::FUSION_ID,
 	weapon_id_type::SUPER_LASER_ID, weapon_id_type::GAUSS_ID, weapon_id_type::HELIX_ID, weapon_id_type::PHOENIX_ID, weapon_id_type::OMEGA_ID
 }};

common/main/fwd-weapon.h
@@ -84,7 +84,7 @@ constexpr unsigned MAX_PRIMARY_WEAPONS = 10;
 constexpr unsigned MAX_SECONDARY_WEAPONS = 10;
 #endif
 
-extern const array<ubyte, MAX_PRIMARY_WEAPONS> Primary_weapon_to_weapon_info;
+extern const array<weapon_id_type, MAX_PRIMARY_WEAPONS> Primary_weapon_to_weapon_info;
 //for each primary weapon, what kind of powerup gives weapon
 extern const array<powerup_type_t, MAX_PRIMARY_WEAPONS> Primary_weapon_to_powerup;
 extern const array<weapon_id_type, MAX_SECONDARY_WEAPONS> Secondary_weapon_to_weapon_info;

similar/main/gauges.cpp
@@ -2201,15 +2201,16 @@ static void draw_primary_weapon_info(int weapon_num, int laser_level, const loca
 	(void)laser_level;
 #endif
 	int x,y;
-	int info_index;
 
 	{
-		info_index = Primary_weapon_to_weapon_info[weapon_num];
-
+		const auto weapon_id = Primary_weapon_to_weapon_info[weapon_num];
+		const auto info_index = 
 #if defined(DXX_BUILD_DESCENT_II)
-		if (info_index == weapon_id_type::LASER_ID && laser_level > MAX_LASER_LEVEL)
-			info_index = weapon_id_type::SUPER_LASER_ID;
+			(weapon_id == weapon_id_type::LASER_ID && laser_level > MAX_LASER_LEVEL)
+			? weapon_id_type::SUPER_LASER_ID
+			:
 #endif
+			weapon_id;
 
 		const gauge_box *box;
 		int pic_x, pic_y, text_x, text_y;

commit dc090958d16caa55a1445abde7cb3cdd296ddbd9

Reduce D1 spreadfire cost
Reported-by: zicodxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/322>
Analyzed-by: kreatordxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/322#issuecomment-280896375>
Fixes: 10ff2b9ea2eccc895d330c81d0522dfd3e8c6e50 ("Change Primary_weapon_to_weapon_info key to weapon_id_type")

common/main/weapon_id.h
@@ -26,7 +26,18 @@ enum weapon_id_type : uint8_t
 	FLARE_ID = 9,   //  NOTE: This MUST correspond to the ID generated at bitmaps.tbl read time.
 	CLS2_DRONE_LASER = 10,
 	VULCAN_ID = 11,  //  NOTE: This MUST correspond to the ID generated at bitmaps.tbl read time.
-#if defined(DXX_BUILD_DESCENT_II)
+#if defined(DXX_BUILD_DESCENT_I)
+	/* Descent shipped with two spreadfire weapons.  One cost 0.5 energy
+	 * to fire; the other cost 1.0 energy to fire.  The two have
+	 * differing flight speeds.  For reasons unknown, the original code
+	 * used the cost of one spreadfire weapon, but used the other weapon
+	 * for all other purposes.  CHEAP_SPREADFIRE_ID represents the lower
+	 * cost weapon, so that it can be named in
+	 * Primary_weapon_to_weapon_info.  SPREADFIRE_ID represents the
+	 * weapon used for all other purposes.
+	 */
+	CHEAP_SPREADFIRE_ID,
+#elif defined(DXX_BUILD_DESCENT_II)
 	SPREADFIRE_ID = 12,  //  NOTE: This MUST correspond to the ID generated at bitmaps.tbl read time.
 #endif
 	PLASMA_ID = 13,  //  NOTE: This MUST correspond to the ID generated at bitmaps.tbl read time.

similar/main/laser.cpp
@@ -662,9 +662,7 @@ objptridx_t Laser_create_new(const vms_vector &direction, const vms_vector &posi
 		case weapon_id_type::FLARE_ID:
 		case weapon_id_type::CLS2_DRONE_LASER:
 		case weapon_id_type::VULCAN_ID:
-#if defined(DXX_BUILD_DESCENT_II)
 		case weapon_id_type::SPREADFIRE_ID:
-#endif
 		case weapon_id_type::PLASMA_ID:
 		case weapon_id_type::FUSION_ID:
 		case weapon_id_type::HOMING_ID:
@@ -673,9 +671,6 @@ objptridx_t Laser_create_new(const vms_vector &direction, const vms_vector &posi
 		case weapon_id_type::MEGA_ID:
 
 		case weapon_id_type::PLAYER_SMART_HOMING_ID:
-#if defined(DXX_BUILD_DESCENT_I)
-		case weapon_id_type::SPREADFIRE_ID:
-#endif
 		case weapon_id_type::SUPER_MECH_MISS:
 		case weapon_id_type::REGULAR_MECH_MISS:
 		case weapon_id_type::SILENT_SPREADFIRE_ID:

similar/main/weapon.cpp
@@ -69,7 +69,7 @@ constexpr tt::integral_constant<uint8_t, 4> has_weapon_result::has_ammo_flag;
 #if defined(DXX_BUILD_DESCENT_I)
 namespace dsx {
 const array<weapon_id_type, MAX_PRIMARY_WEAPONS> Primary_weapon_to_weapon_info{{
-	weapon_id_type::LASER_ID, weapon_id_type::VULCAN_ID, weapon_id_type::SPREADFIRE_ID, weapon_id_type::PLASMA_ID, weapon_id_type::FUSION_ID
+	weapon_id_type::LASER_ID, weapon_id_type::VULCAN_ID, weapon_id_type::CHEAP_SPREADFIRE_ID, weapon_id_type::PLASMA_ID, weapon_id_type::FUSION_ID
 }};
 const array<weapon_id_type, MAX_SECONDARY_WEAPONS> Secondary_weapon_to_weapon_info{{weapon_id_type::CONCUSSION_ID, weapon_id_type::HOMING_ID, weapon_id_type::PROXIMITY_ID, weapon_id_type::SMART_ID, weapon_id_type::MEGA_ID}};
 