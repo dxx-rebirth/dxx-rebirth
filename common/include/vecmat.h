dxx-rebirth/dxx-rebirth

commit 17208cca79769286374316707c74483713598c76

Disallow int for vm_distance_squared

common/include/vecmat.h
@@ -127,6 +127,7 @@ class vm_distance_squared
 {
 public:
 	fix64 d2;
+	vm_distance_squared(const fix &) = delete;
 	constexpr explicit vm_distance_squared(const fix64 &f2) :
 		d2(f2)
 	{
@@ -166,7 +167,7 @@ public:
 	}
 	static constexpr vm_distance_squared minimum_value()
 	{
-		return vm_distance_squared{0};
+		return vm_distance_squared{static_cast<fix64>(0)};
 	}
 };
 
@@ -174,7 +175,7 @@ class vm_magnitude_squared : public vm_distance_squared
 {
 public:
 	constexpr explicit vm_magnitude_squared(const uint64_t &f2) :
-		vm_distance_squared(f2)
+		vm_distance_squared(static_cast<fix64>(f2))
 	{
 	}
 };

similar/main/render.cpp
@@ -1050,7 +1050,7 @@ bool render_compare_context_t::operator()(const distant_object &a, const distant
 	 * First check without squaring.  If true, the square can be
 	 * skipped.
 	 */
-	if (abs_delta_dist_squared < combined_size || abs_delta_dist_squared < (combined_size * combined_size))
+	if (abs_delta_dist_squared < combined_size || abs_delta_dist_squared < (static_cast<fix64>(combined_size) * combined_size))
 	{		//same position
 
 		//these two objects are in the same position.  see if one is a fireball

similar/main/fvi.cpp
@@ -453,14 +453,14 @@ static vm_distance_squared check_vector_to_sphere_1(vms_vector &intp,const vms_v
 			if (vm_vec_dist2(p0, sphere_pos) < sphere_rad_squared)
 			{
 				intp = p0; //don't move at all
-				return vm_distance_squared{1}; // note that we do not calculate a valid collision point. This is up to collision handling.
+				return vm_distance_squared{static_cast<fix64>(1)}; // note that we do not calculate a valid collision point. This is up to collision handling.
 			} else {
 				return vm_distance_squared::minimum_value();
 			}
 		}
 
 		vm_vec_scale_add(intp,p0,dn,int_dist);         //calc intersection point
-		return vm_distance_squared{int_dist * int_dist};
+		return vm_distance_squared{static_cast<fix64>(int_dist) * int_dist};
 	}
 	else
 		return vm_distance_squared::minimum_value();

commit 08e4a6e6203bec1b44326e6e7d1081a21b8f140a

Use stdint constants for some INT*_MAX
clang becomes confused trying to determine which vm_distance_squared
constructor to use for a literal input of 0x7fffffffffffffff, even
though the size of the input requires it to be `long` and only one
constructor can take a `long`.  Switch from an explicit
0x7fffffffffffffff to the symbolic constant INT64_MAX, which has the
same value, but a platform-appropriate suffix to force the compiler to
pick the right type.

For general clarity, switch some other instances of integer maximum
literals to symbolic constants of the same value.

This commit has no effect on the generated code (except for changes to
line numbers).

Reported-by: kreatordxx <https://github.com/dxx-rebirth/dxx-rebirth/pull/324>
Fixes: 17208cca79769286374316707c74483713598c76 ("Disallow int for vm_distance_squared")

common/include/vecmat.h
@@ -163,7 +163,7 @@ public:
 	}
 	static constexpr vm_distance_squared maximum_value()
 	{
-		return vm_distance_squared{0x7fffffffffffffff};
+		return vm_distance_squared{INT64_MAX};
 	}
 	static constexpr vm_distance_squared minimum_value()
 	{

similar/editor/mine.cpp
@@ -481,8 +481,9 @@ static void dump_fix_as_short( fix value, int nbits, PHYSFS_File *SaveFile )
 	short short_value;
 
 	auto int_value = value >> nbits;
-	if( int_value > 0x7fff ) {
-		short_value = 0x7fff;
+	if (int_value > INT16_MAX)
+	{
+		short_value = INT16_MAX;
 	}
 	else if( int_value < -0x7fff ) {
 		short_value = -0x7fff;

similar/main/automap.cpp
@@ -1092,7 +1092,7 @@ void draw_all_edges(automap *am)
 	unsigned nbright = 0;
 	ubyte nfacing,nnfacing;
 	fix distance;
-	fix min_distance = 0x7fffffff;
+	fix min_distance = INT32_MAX;
 
 	for (i=0; i<=am->highest_edge_index; i++ )	{
 		//e = &am->edges[Edge_used_list[i]];

similar/main/collide.cpp
@@ -204,7 +204,7 @@ static void apply_force_damage(const vobjptridx_t obj,fix force,const vobjptridx
 
 #if defined(DXX_BUILD_DESCENT_II)
 	if ((other_obj->type == OBJ_PLAYER) && cheats.monsterdamage)
-		damage = 0x7fffffff;
+		damage = INT32_MAX;
 #endif
 
 	if (damage < FORCE_DAMAGE_THRESHOLD)

similar/main/endlevel.cpp
@@ -1122,7 +1122,7 @@ static void endlevel_render_mine(fix eye_offset)
 	}
 
 	g3_set_view_matrix(Viewer_eye, Endlevel_sequence == EL_LOOKBACK
-		? vm_matrix_x_matrix(Viewer->orient, vm_angles_2_matrix(vms_angvec{0, 0, 0x7fff}))
+		? vm_matrix_x_matrix(Viewer->orient, vm_angles_2_matrix(vms_angvec{0, 0, INT16_MAX}))
 		: Viewer->orient, Render_zoom);
 
 	window_rendered_data window;

similar/main/gamecntl.cpp
@@ -1352,7 +1352,7 @@ static window_event_result HandleTestKey(int key)
 			kill_and_so_forth();
 			break;
 		case KEY_DEBUGGED+KEY_G:
-			GameTime64 = (0x7fffffffffffffffLL) - (F1_0*10);
+			GameTime64 = (INT64_MAX) - (F1_0*10);
 			HUD_init_message(HM_DEFAULT, "GameTime %" PRIi64 " - Reset in 10 seconds!", GameTime64);
 			break;
 		default:

similar/main/gameseq.cpp
@@ -1845,7 +1845,7 @@ public:
 	{
 		const auto player_num = Player_num;
 		const auto find_closest_player = [player_num](const obj_position &candidate) {
-			fix closest_dist = 0x7fffffff;
+			fix closest_dist = INT32_MAX;
 			const auto &&candidate_segp = vcsegptridx(candidate.segnum);
 			for (uint_fast32_t i = N_players; i--;)
 			{

similar/main/morph.cpp
@@ -151,13 +151,14 @@ static void init_points(polymodel *pm,const vms_vector *box_size,int submodel_nu
 		if (box_size) {
 			fix t;
 
-			k = 0x7fffffff;
+			k = INT32_MAX;
 
 			if (vp->x && f2i(box_size->x)<abs(vp->x)/2 && (t = fixdiv(box_size->x,abs(vp->x))) < k) k=t;
 			if (vp->y && f2i(box_size->y)<abs(vp->y)/2 && (t = fixdiv(box_size->y,abs(vp->y))) < k) k=t;
 			if (vp->z && f2i(box_size->z)<abs(vp->z)/2 && (t = fixdiv(box_size->z,abs(vp->z))) < k) k=t;
 
-			if (k==0x7fffffff) k=0;
+			if (k == INT32_MAX)
+				k = 0;
 
 		}
 		else

similar/main/multibot.cpp
@@ -247,7 +247,7 @@ namespace dsx {
 int multi_add_controlled_robot(const vobjptridx_t objnum, int agitation)
 {
 	int i;
-	int lowest_agitation = 0x7fffffff; // MAX POSITIVE INT
+	int lowest_agitation = INT32_MAX; // MAX POSITIVE INT
 	int lowest_agitated_bot = -1;
 	int first_free_robot = -1;
 

similar/main/render.cpp
@@ -1217,7 +1217,7 @@ void render_frame(fix eye_offset, window_rendered_data &window)
 
 	g3_set_view_matrix(Viewer_eye,
 		(Rear_view && Viewer == ConsoleObject)
-		? vm_matrix_x_matrix(Viewer->orient, vm_angles_2_matrix(vms_angvec{0, 0, 0x7fff}))
+		? vm_matrix_x_matrix(Viewer->orient, vm_angles_2_matrix(vms_angvec{0, 0, INT16_MAX}))
 		: Viewer->orient, Render_zoom);
 
 	if (Clear_window == 1) {

similar/main/terrain.cpp
@@ -431,7 +431,7 @@ static void build_light_table()
 	light_array = make_unique<uint8_t[]>(alloc);
 	memset(light_array.get(), 0, alloc);
 	int i,j;
-	fix l,l2,min_l=0x7fffffff,max_l=0;
+	fix l, l2, min_l = INT32_MAX, max_l = 0;
 	for (i=1;i<grid_w;i++)
 		for (j=1;j<grid_h;j++) {
 			l = get_avg_light(i,j);