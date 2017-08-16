dxx-rebirth/dxx-rebirth

commit e859833f62adcae1680888a2cb613846c4777ff6

Use valptridx for Players
Currently, N_players is still a free variable and Players.count is
unused.  Future work will replace N_players with Players.count.

similar/main/newdemo.cpp
@@ -1468,7 +1468,7 @@ void newdemo_record_multi_connect(const unsigned pnum, const unsigned new_player
 	nd_write_byte(static_cast<int8_t>(pnum));
 	nd_write_byte(static_cast<int8_t>(new_player));
 	if (!new_player) {
-		auto &plr = Players[pnum];
+		auto &plr = *vcplayerptr(pnum);
 		nd_write_string(static_cast<const char *>(plr.callsign));
 		auto &player_info = vcobjptr(plr.objnum)->ctype.player_info;
 		nd_write_int(player_info.net_killed_total);
@@ -1503,7 +1503,7 @@ void newdemo_record_multi_score(const unsigned pnum, const int score)
 	pause_game_world_time p;
 	nd_write_byte(ND_EVENT_MULTI_SCORE);
 	nd_write_byte(static_cast<int8_t>(pnum));
-	nd_write_int(score - vcobjptr(Players[pnum].objnum)->ctype.player_info.mission.score);      // called before score is changed!!!!
+	nd_write_int(score - vcobjptr(vcplayerptr(pnum)->objnum)->ctype.player_info.mission.score);      // called before score is changed!!!!
 }
 
 void newdemo_record_primary_ammo(int new_ammo)
@@ -2790,7 +2790,7 @@ static int newdemo_read_frame_information(int rewrite)
 				nd_write_byte(pnum);
 				break;
 			}
-			auto &player_info = vmobjptr(Players[static_cast<unsigned>(pnum)].objnum)->ctype.player_info;
+			auto &player_info = vmobjptr(vcplayerptr(static_cast<unsigned>(pnum))->objnum)->ctype.player_info;
 			if ((Newdemo_vcr_state == ND_STATE_REWINDING) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEBACKWARD))
 				player_info.net_killed_total--;
 			else if ((Newdemo_vcr_state == ND_STATE_PLAYBACK) || (Newdemo_vcr_state == ND_STATE_FASTFORWARD) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEFORWARD))
@@ -2809,7 +2809,7 @@ static int newdemo_read_frame_information(int rewrite)
 				nd_write_byte(kill);
 				break;
 			}
-			auto &player_info = vmobjptr(Players[static_cast<unsigned>(pnum)].objnum)->ctype.player_info;
+			auto &player_info = vmobjptr(vcplayerptr(static_cast<unsigned>(pnum))->objnum)->ctype.player_info;
 			if ((Newdemo_vcr_state == ND_STATE_REWINDING) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEBACKWARD)) {
 				player_info.net_kills_total -= kill;
 				if (Newdemo_game_mode & GM_TEAM)
@@ -2850,7 +2850,7 @@ static int newdemo_read_frame_information(int rewrite)
 				nd_write_string(static_cast<const char *>(new_callsign));
 				break;
 			}
-			auto &plr = Players[static_cast<unsigned>(pnum)];
+			auto &plr = *vmplayerptr(static_cast<unsigned>(pnum));
 			auto &player_info = vmobjptr(plr.objnum)->ctype.player_info;
 			if ((Newdemo_vcr_state == ND_STATE_REWINDING) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEBACKWARD)) {
 				plr.connected = CONNECT_DISCONNECTED;
@@ -2882,9 +2882,9 @@ static int newdemo_read_frame_information(int rewrite)
 				break;
 			}
 			if ((Newdemo_vcr_state == ND_STATE_REWINDING) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEBACKWARD))
-				Players[static_cast<unsigned>(pnum)].connected = CONNECT_DISCONNECTED;
+				vmplayerptr(static_cast<unsigned>(pnum))->connected = CONNECT_DISCONNECTED;
 			else if ((Newdemo_vcr_state == ND_STATE_PLAYBACK) || (Newdemo_vcr_state == ND_STATE_FASTFORWARD) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEFORWARD))
-				Players[static_cast<unsigned>(pnum)].connected = CONNECT_PLAYING;
+				vmplayerptr(static_cast<unsigned>(pnum))->connected = CONNECT_PLAYING;
 			break;
 		}
 
@@ -2899,14 +2899,14 @@ static int newdemo_read_frame_information(int rewrite)
 			}
 #if defined(DXX_BUILD_DESCENT_I)
 			if ((Newdemo_vcr_state == ND_STATE_REWINDING) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEBACKWARD))
-				Players[static_cast<unsigned>(pnum)].connected = CONNECT_DISCONNECTED;
+				vmplayerptr(static_cast<unsigned>(pnum))->connected = CONNECT_DISCONNECTED;
 			else if ((Newdemo_vcr_state == ND_STATE_PLAYBACK) || (Newdemo_vcr_state == ND_STATE_FASTFORWARD) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEFORWARD))
-				Players[static_cast<unsigned>(pnum)].connected = CONNECT_PLAYING;
+				vmplayerptr(static_cast<unsigned>(pnum))->connected = CONNECT_PLAYING;
 #elif defined(DXX_BUILD_DESCENT_II)
 			if ((Newdemo_vcr_state == ND_STATE_REWINDING) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEBACKWARD))
-				Players[static_cast<unsigned>(pnum)].connected = CONNECT_PLAYING;
+				vmplayerptr(static_cast<unsigned>(pnum))->connected = CONNECT_PLAYING;
 			else if ((Newdemo_vcr_state == ND_STATE_PLAYBACK) || (Newdemo_vcr_state == ND_STATE_FASTFORWARD) || (Newdemo_vcr_state == ND_STATE_ONEFRAMEFORWARD))
-				Players[static_cast<unsigned>(pnum)].connected = CONNECT_DISCONNECTED;
+				vmplayerptr(static_cast<unsigned>(pnum))->connected = CONNECT_DISCONNECTED;
 #endif
 			break;
 		}
@@ -3329,7 +3329,7 @@ window_event_result newdemo_goto_end(int to_rewrite)
 			PHYSFSX_fseek(infile, -10, SEEK_END);
 			nd_read_byte(&cloaked);
 			for (uint_fast32_t i = 0; i < MAX_PLAYERS; i++) {
-				const auto &&objp = vmobjptr(Players[i].objnum);
+				const auto &&objp = vmobjptr(vcplayerptr(i)->objnum);
 				auto &player_info = objp->ctype.player_info;
 				if ((1 << i) & cloaked)
 				{
@@ -3360,7 +3360,7 @@ window_event_result newdemo_goto_end(int to_rewrite)
 		for (unsigned i = 0; i < MAX_PLAYERS; ++i)
 			if (cloaked & (1 << i))
 				{
-					const auto &&objp = vmobjptr(Players[i].objnum);
+					const auto &&objp = vmobjptr(vcplayerptr(i)->objnum);
 					objp->ctype.player_info.powerup_flags |= PLAYER_FLAGS_CLOAKED;
 				}
 	}
@@ -3803,7 +3803,7 @@ static void newdemo_write_end()
 	if (Game_mode & GM_MULTI) {
 		for (unsigned i = 0; i < N_players; ++i)
 		{
-			const auto &&objp = vmobjptr(Players[i].objnum);
+			const auto &&objp = vmobjptr(vcplayerptr(i)->objnum);
 			if (objp->ctype.player_info.powerup_flags & PLAYER_FLAGS_CLOAKED)
 				cloaked |= (1 << i);
 		}

common/main/fwd-player.h
@@ -79,10 +79,10 @@ extern playernum_t Player_num;  // The player number who is on the console.
 }
 
 #ifdef dsx
+DXX_VALPTRIDX_DECLARE_SUBTYPE(dcx::, player, playernum_t, MAX_PLAYERS);
 namespace dsx {
 struct player_rw;
 struct player_info;
-extern array<player, MAX_PLAYERS> Players;   // Misc player info
 void player_rw_swap(player_rw *p, int swap);
 int allowed_to_fire_flare(player_info &);
 int allowed_to_fire_missile(const player_info &);

common/main/gameseq.h
@@ -142,6 +142,6 @@ void do_cloak_invul_secret_stuff(fix64 old_gametime, player_info &player_info);
 #endif
 void EnterSecretLevel(void);
 void init_player_stats_ship(object &);
-void init_player_stats_new_ship(ubyte pnum);
+void init_player_stats_new_ship(playernum_t pnum);
 }
 #endif

common/main/multi.h
@@ -368,7 +368,7 @@ extern unsigned Net_create_loc;
 namespace dsx {
 
 void multi_send_fire(int laser_gun, int laser_level, int laser_flags, int laser_fired, objnum_t laser_track, imobjptridx_t is_bomb_objnum);
-void multi_send_destroy_controlcen(objnum_t objnum, int player);
+void multi_send_destroy_controlcen(objnum_t objnum, playernum_t player);
 void multi_send_position(vmobjptridx_t objnum);
 void multi_send_kill(vmobjptridx_t objnum);
 void multi_send_remobj(vmobjptridx_t objnum);
@@ -656,7 +656,7 @@ extern struct netgame_info Netgame;
 #endif
 
 #define multi_i_am_master()	(Player_num == 0)
-void change_playernum_to(int new_pnum);
+void change_playernum_to(playernum_t new_pnum);
 
 // Multiplayer powerup capping
 void MultiLevelInv_InitializeCount();

common/main/player.h
@@ -42,6 +42,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
 #include "player-flags.h"
+#include "valptridx.h"
 
 namespace dcx {
 // When this structure changes, increment the constant
@@ -75,6 +76,8 @@ struct player : public prohibit_void_ptr<player>
 	sbyte   hours_total;            // Hours played (since time_total can only go up to 9 hours)
 };
 
+DXX_VALPTRIDX_DEFINE_SUBTYPE_TYPEDEFS(player, player);
+DXX_VALPTRIDX_DEFINE_GLOBAL_FACTORIES(player, player, Players);
 }
 
 namespace dsx {
@@ -156,7 +159,7 @@ static_assert(sizeof(player_rw) == 142, "wrong size player_rw");
 
 }
 
-#define get_local_player()	(Players[Player_num])
+#define get_local_player()	(*vmplayerptr(Player_num))
 #define get_local_plrobj()	(*vmobjptr(get_local_player().objnum))
 
 namespace dcx {

similar/main/automap.cpp
@@ -705,7 +705,7 @@ static void draw_automap(fvcobjptr &vcobjptr, automap *am)
 		for (unsigned i = 0; i < N_players; ++i)
 		{
 			if ( (i != Player_num) && ((Game_mode & GM_MULTI_COOP) || (get_team(Player_num) == get_team(i)) || (Netgame.game_flag.show_on_map)) )	{
-				const auto &&objp = vcobjptr(Players[i].objnum);
+				const auto &&objp = vcobjptr(vcplayerptr(i)->objnum);
 				if (objp->type == OBJ_PLAYER)
 				{
 					const auto &other_ship_rgb = player_rgb[get_player_or_team_color(i)];

similar/main/collide.cpp
@@ -1160,7 +1160,7 @@ static void collide_player_and_marker(const object_base &playerobj, const vmobjp
 		auto &msg = MarkerMessage[marker_id];
 		if (Game_mode & GM_MULTI)
 		{
-			drawn = HUD_init_message(HM_DEFAULT|HM_MAYDUPL, "MARKER %s: %s", static_cast<const char *>(Players[marker_id / 2].callsign), &msg[0]);
+			drawn = HUD_init_message(HM_DEFAULT|HM_MAYDUPL, "MARKER %s: %s", static_cast<const char *>(vcplayerptr(marker_id / 2)->callsign), &msg[0]);
 		}
 		else
 		{

similar/main/fireball.cpp
@@ -539,7 +539,7 @@ static vmsegptridx_t choose_drop_segment(segment_array &segments, const playernu
 	int	cur_drop_depth;
 	int	count;
 	vms_vector *player_pos;
-	auto &drop_playerobj = *vmobjptr(Players[drop_pnum].objnum);
+	auto &drop_playerobj = *vmobjptr(vcplayerptr(drop_pnum)->objnum);
 
 	d_srand(static_cast<fix>(timer_query()));
 
@@ -553,9 +553,9 @@ static vmsegptridx_t choose_drop_segment(segment_array &segments, const playernu
 		pnum = (d_rand() * N_players) >> 15;
 		count = 0;
 #if defined(DXX_BUILD_DESCENT_I)
-		while (count < N_players && (Players[pnum].connected == CONNECT_DISCONNECTED || pnum == drop_pnum))
+		while (count < N_players && (vcplayerptr(pnum)->connected == CONNECT_DISCONNECTED || pnum == drop_pnum))
 #elif defined(DXX_BUILD_DESCENT_II)
-		while (count < N_players && (Players[pnum].connected == CONNECT_DISCONNECTED || pnum == drop_pnum || ((Game_mode & (GM_TEAM | GM_CAPTURE)) && get_team(pnum) == get_team(drop_pnum))))
+		while (count < N_players && (vcplayerptr(pnum)->connected == CONNECT_DISCONNECTED || pnum == drop_pnum || ((Game_mode & (GM_TEAM|GM_CAPTURE)) && (get_team(pnum)==get_team(drop_pnum)))))
 #endif
 		{
 			pnum = (pnum+1)%N_players;
@@ -567,7 +567,7 @@ static vmsegptridx_t choose_drop_segment(segment_array &segments, const playernu
 			pnum = drop_pnum;
 		}
 
-		segnum = pick_connected_segment(vcobjptr(Players[pnum].objnum)->segnum, cur_drop_depth);
+		segnum = pick_connected_segment(vcobjptr(vcplayerptr(pnum)->objnum)->segnum, cur_drop_depth);
 		if (segnum == segment_none)
 		{
 			cur_drop_depth--;
@@ -604,7 +604,7 @@ static vmsegptridx_t choose_drop_segment(segment_array &segments, const playernu
 		cur_drop_depth = BASE_NET_DROP_DEPTH;
 		while (cur_drop_depth > 0 && segnum == segment_none) // before dropping in random segment, try to find ANY segment which is connected to the player responsible for the drop so object will not spawn in inaccessible areas
 		{
-			segnum = pick_connected_segment(vcobjptr(Players[drop_pnum].objnum)->segnum, --cur_drop_depth);
+			segnum = pick_connected_segment(vcobjptr(vcplayerptr(drop_pnum)->objnum)->segnum, --cur_drop_depth);
 			if (segnum != segment_none && vcsegptr(segnum)->special == SEGMENT_IS_CONTROLCEN)
 				segnum = segment_none;
 		}
@@ -641,7 +641,7 @@ void maybe_drop_net_powerup(powerup_type_t powerup_type, bool adjust_cap, bool r
                                         break;
                                 }
                                 failsafe_count++;
-                        } while (Players[pnum].connected != CONNECT_PLAYING);
+                        } while (vcplayerptr(pnum)->connected != CONNECT_PLAYING);
                 }
 
 //--old-- 		segnum = (d_rand() * Highest_segment_index) >> 15;
@@ -652,7 +652,7 @@ void maybe_drop_net_powerup(powerup_type_t powerup_type, bool adjust_cap, bool r
 //--old-- 			segnum /= 2;
 
 		Net_create_loc = 0;
-		const auto &&objnum = call_object_create_egg(vmobjptr(Players[pnum].objnum), 1, powerup_type);
+		const auto &&objnum = call_object_create_egg(vmobjptr(vcplayerptr(pnum)->objnum), 1, powerup_type);
 
 		if (objnum == object_none)
 			return;

commit f1147572089967f2e40485cf060038f058c68ea3

Fix D1 build of similar/main/newdemo.cpp
Fixes: e859833f62adcae1680888a2cb613846c4777ff6 ("Use valptridx for Players")

similar/main/newdemo.cpp
@@ -3328,7 +3328,8 @@ window_event_result newdemo_goto_end(int to_rewrite)
 		if (Newdemo_game_mode & GM_MULTI) {
 			PHYSFSX_fseek(infile, -10, SEEK_END);
 			nd_read_byte(&cloaked);
-			for (uint_fast32_t i = 0; i < MAX_PLAYERS; i++) {
+			for (playernum_t i = 0; i < MAX_PLAYERS; i++)
+			{
 				const auto &&objp = vmobjptr(vcplayerptr(i)->objnum);
 				auto &player_info = objp->ctype.player_info;
 				if ((1 << i) & cloaked)