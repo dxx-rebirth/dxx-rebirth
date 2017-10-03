dxx-rebirth/dxx-rebirth

commit b1b5de4297df44735697bec09dde7e84db9ee701

Additional safeguard for bug #306
This is where a client in a multiplayer game hitting a wall fails an assert. Initialise obj->mtype.phys_info.flags in multi_reset_player_object() instead of adding flags. (Tested and still works OK without specifying PF_THRUST.)

similar/main/multi.cpp
@@ -2415,7 +2415,7 @@ void multi_reset_player_object(const vobjptr_t objp)
 	objp->mtype.phys_info.mass = Player_ship->mass;
 	objp->mtype.phys_info.drag = Player_ship->drag;
 	if (objp->type == OBJ_PLAYER)
-		objp->mtype.phys_info.flags |= PF_TURNROLL | PF_WIGGLE;
+		objp->mtype.phys_info.flags = PF_TURNROLL | PF_WIGGLE;
 	else
 		objp->mtype.phys_info.flags &= ~(PF_TURNROLL | PF_LEVELLING | PF_WIGGLE);
 

commit c3434e61d3e503ae6f8f3002328ff04d948afb99

Enable PF_USES_THRUST when reseting multiplayer flags
Commit b1b5de4 switched from enabling select physics flags to setting
those flags and clearing all others.  Unfortunately, flag PF_USES_THRUST
was omitted from the enabled list, so it was disabled on reset.  It is
required to let player ships move.

Reported-by: Sottises <https://github.com/dxx-rebirth/dxx-rebirth/issues/347>
Fixes: b1b5de4297df44735697bec09dde7e84db9ee701 ("Additional safeguard for bug #306")

similar/main/multi.cpp
@@ -2374,7 +2374,7 @@ void multi_reset_player_object(const vmobjptr_t objp)
 	objp->mtype.phys_info.mass = Player_ship->mass;
 	objp->mtype.phys_info.drag = Player_ship->drag;
 	if (objp->type == OBJ_PLAYER)
-		objp->mtype.phys_info.flags = PF_TURNROLL | PF_WIGGLE;
+		objp->mtype.phys_info.flags = PF_TURNROLL | PF_WIGGLE | PF_USES_THRUST;
 	else
 		objp->mtype.phys_info.flags &= ~(PF_TURNROLL | PF_LEVELLING | PF_WIGGLE);
 