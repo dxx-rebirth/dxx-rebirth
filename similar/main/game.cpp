dxx-rebirth/dxx-rebirth

commit 88832e3679b6d6303be76dee8bf825232c215d86

Use constexpr integral_constant for various magic numbers

similar/main/game.cpp
@@ -1633,7 +1633,7 @@ static void slide_textures(void)
 Flickering_light_array_t Flickering_lights;
 unsigned Num_flickering_lights;
 
-constexpr fix flicker_timer_disabled = 0x80000000;
+constexpr std::integral_constant<fix, 0x80000000> flicker_timer_disabled{};
 
 static void flicker_lights(fvmsegptridx &vmsegptridx)
 {

commit 425b78b8a63b27a37e38cc4911f3a27dc59c0661

Fix clang build failure on flicker_timer_disabled
clang rejects 0x80000000 as a non-type template argument of type `int`
due to narrowing.  Use `INT32_MIN` instead, which has the same numeric
value, and should be accepted without requiring narrowing.

Reported-by: Havner <https://github.com/dxx-rebirth/dxx-rebirth/issues/353>
Fixes: 88832e3679b6d6303be76dee8bf825232c215d86 ("Use constexpr integral_constant for various magic numbers")

similar/main/game.cpp
@@ -1633,7 +1633,7 @@ static void slide_textures(void)
 Flickering_light_array_t Flickering_lights;
 unsigned Num_flickering_lights;
 
-constexpr std::integral_constant<fix, 0x80000000> flicker_timer_disabled{};
+constexpr std::integral_constant<fix, INT32_MIN> flicker_timer_disabled{};
 
 static void flicker_lights(fvmsegptridx &vmsegptridx)
 {