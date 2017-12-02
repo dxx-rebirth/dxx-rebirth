dxx-rebirth/dxx-rebirth

commit 7d36df315e5a9d1691ab316aaba54afcf59966fd

Simplify draw_weapon_vclip

similar/main/vclip.cpp
@@ -66,37 +66,27 @@ void draw_vclip_object(grs_canvas &canvas, const vcobjptridx_t obj, const fix ti
 
 void draw_weapon_vclip(grs_canvas &canvas, const vcobjptridx_t obj)
 {
-	int	vclip_num;
-	fix	modtime,play_time;
-
 	Assert(obj->type == OBJ_WEAPON);
 
-	vclip_num = Weapon_info[get_weapon_id(obj)].weapon_vclip;
-
-	modtime = obj->lifeleft;
-	play_time = Vclip[vclip_num].play_time;
+	const auto lifeleft = obj->lifeleft;
+	const auto vclip_num = Weapon_info[get_weapon_id(obj)].weapon_vclip;
+	const auto play_time = Vclip[vclip_num].play_time;
+	fix modtime = lifeleft % play_time;
 
 #if defined(DXX_BUILD_DESCENT_II)
-	//	Special values for modtime were causing enormous slowdown for omega blobs.
-	if (modtime == IMMORTAL_TIME)
-		modtime = play_time;
-
 	if (get_weapon_id(obj) == weapon_id_type::PROXIMITY_ID) {		//make prox bombs spin out of sync
 		int objnum = obj;
 
-		modtime += (modtime * (objnum&7)) / 16;	//add variance to spin rate
+		modtime += (lifeleft * (objnum & 7)) / 16;	//add variance to spin rate
 
-		while (modtime > play_time)
+		if (modtime > play_time)
 			modtime -= play_time;
 
 		if ((objnum&1) ^ ((objnum>>1)&1))			//make some spin other way
 			modtime = play_time - modtime;
 
 	}
-	else
 #endif
-		while (modtime > play_time)
-			modtime -= play_time;
 
 	draw_vclip_object(canvas, obj, modtime, 0, Vclip[vclip_num]);
 }

commit 1f333868eb16a5d4e30842926ac21adc0dfb461c

Fix proximity bomb failure-to-spin
Commit 7d36df315e5a9d1691ab316aaba54afcf59966fd restructured
draw_weapon_vclip, but unintentionally broke the spin of proximity bombs
because `modtime` could exceed `play_time * 2`, causing
`draw_vclip_object` to clamp `bitmapnum` to the last frame of the
animation.  Switch from `-=` to `%=` so that modtime is always brought
back in range.

Reported-by: Descender1032 <https://forum.dxx-rebirth.com/showthread.php?tid=1005>
Fixes: 7d36df315e5a9d1691ab316aaba54afcf59966fd ("Simplify draw_weapon_vclip")

similar/main/vclip.cpp
@@ -80,7 +80,7 @@ void draw_weapon_vclip(grs_canvas &canvas, const vcobjptridx_t obj)
 		modtime += (lifeleft * (objnum & 7)) / 16;	//add variance to spin rate
 
 		if (modtime > play_time)
-			modtime -= play_time;
+			modtime %= play_time;
 
 		if ((objnum&1) ^ ((objnum>>1)&1))			//make some spin other way
 			modtime = play_time - modtime;