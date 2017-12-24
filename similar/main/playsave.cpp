dxx-rebirth/dxx-rebirth

commit 7311712399e1bc211785bf20028ff8b3e78c4745

Compile out joystick key settings when DXX_MAX_JOYSTICKS=0

similar/main/playsave.cpp
@@ -533,15 +533,15 @@ static void read_player_dxx(const char *filename)
 				{
 					// reset joystick/mouse cycling fields
 #if defined(DXX_BUILD_DESCENT_I)
-					PlayerCfg.KeySettings[1][44] = 255;
-					PlayerCfg.KeySettings[1][45] = 255;
-					PlayerCfg.KeySettings[1][46] = 255;
-					PlayerCfg.KeySettings[1][47] = 255;
-					PlayerCfg.KeySettings[2][27] = 255;
+					PlayerCfg.KeySettings.Joystick[44] = 255;
+					PlayerCfg.KeySettings.Joystick[45] = 255;
+					PlayerCfg.KeySettings.Joystick[46] = 255;
+					PlayerCfg.KeySettings.Joystick[47] = 255;
+					PlayerCfg.KeySettings.Mouse[27] = 255;
 #endif
-					PlayerCfg.KeySettings[2][28] = 255;
+					PlayerCfg.KeySettings.Mouse[28] = 255;
 #if defined(DXX_BUILD_DESCENT_II)
-					PlayerCfg.KeySettings[2][29] = 255;
+					PlayerCfg.KeySettings.Mouse[29] = 255;
 #endif
 				}
 		}
@@ -1025,12 +1025,17 @@ int read_player_file()
 	{
 		ubyte dummy_joy_sens;
 
-		if (PHYSFS_read(file, &PlayerCfg.KeySettings[0], sizeof(PlayerCfg.KeySettings[0]),1)!=1)
+		if (PHYSFS_read(file, &PlayerCfg.KeySettings.Keyboard, sizeof(PlayerCfg.KeySettings.Keyboard), 1) != 1)
 			goto read_player_file_failed;
-		if (PHYSFS_read(file, &PlayerCfg.KeySettings[1], sizeof(PlayerCfg.KeySettings[1]),1)!=1)
+#if DXX_MAX_JOYSTICKS
+		auto &KeySettingsJoystick = PlayerCfg.KeySettings.Joystick;
+#else
+		array<uint8_t, MAX_CONTROLS> KeySettingsJoystick;
+#endif
+		if (PHYSFS_read(file, &KeySettingsJoystick, sizeof(KeySettingsJoystick), 1) != 1)
 			goto read_player_file_failed;
 		PHYSFS_seek( file, PHYSFS_tell(file)+(sizeof(ubyte)*MAX_CONTROLS*3) ); // Skip obsolete Flightstick/Thrustmaster/Gravis map fields
-		if (PHYSFS_read(file, &PlayerCfg.KeySettings[2], sizeof(PlayerCfg.KeySettings[2]),1)!=1)
+		if (PHYSFS_read(file, &PlayerCfg.KeySettings.Mouse, sizeof(PlayerCfg.KeySettings.Mouse), 1) != 1)
 			goto read_player_file_failed;
 		PHYSFS_seek( file, PHYSFS_tell(file)+(sizeof(ubyte)*MAX_CONTROLS) ); // Skip obsolete Cyberman map field
 #if defined(DXX_BUILD_DESCENT_I)
@@ -1258,14 +1263,19 @@ void write_player_file()
 
 	//write kconfig info
 	{
-		if (PHYSFS_write(file, PlayerCfg.KeySettings[0], sizeof(PlayerCfg.KeySettings[0]), 1) != 1)
+		if (PHYSFS_write(file, PlayerCfg.KeySettings.Keyboard, sizeof(PlayerCfg.KeySettings.Keyboard), 1) != 1)
 			errno_ret=errno;
-		if (PHYSFS_write(file, PlayerCfg.KeySettings[1], sizeof(PlayerCfg.KeySettings[1]), 1) != 1)
+#if DXX_MAX_JOYSTICKS
+		auto &KeySettingsJoystick = PlayerCfg.KeySettings.Joystick;
+#else
+		const array<uint8_t, MAX_CONTROLS> KeySettingsJoystick{};
+#endif
+		if (PHYSFS_write(file, KeySettingsJoystick, sizeof(KeySettingsJoystick), 1) != 1)
 			errno_ret=errno;
 		for (unsigned i = 0; i < MAX_CONTROLS*3; i++)
 			if (PHYSFS_write(file, "0", sizeof(ubyte), 1) != 1) // Skip obsolete Flightstick/Thrustmaster/Gravis map fields
 				errno_ret=errno;
-		if (PHYSFS_write(file, PlayerCfg.KeySettings[2], sizeof(PlayerCfg.KeySettings[2]), 1) != 1)
+		if (PHYSFS_write(file, PlayerCfg.KeySettings.Mouse, sizeof(PlayerCfg.KeySettings.Mouse), 1) != 1)
 			errno_ret=errno;
 		{
 			std::array<uint8_t, MAX_CONTROLS> cyberman{};
@@ -1321,14 +1331,19 @@ void write_player_file()
 		ubyte old_avg_joy_sensitivity = 8;
 		ubyte control_type_dos = PlayerCfg.ControlType;
 
-		if (PHYSFS_write(file, PlayerCfg.KeySettings[0], sizeof(PlayerCfg.KeySettings[0]), 1) != 1)
+		if (PHYSFS_write(file, PlayerCfg.KeySettings.Keyboard, sizeof(PlayerCfg.KeySettings.Keyboard), 1) != 1)
 			goto write_player_file_failed;
-		if (PHYSFS_write(file, PlayerCfg.KeySettings[1], sizeof(PlayerCfg.KeySettings[1]), 1) != 1)
+#if DXX_MAX_JOYSTICKS
+		auto &KeySettingsJoystick = PlayerCfg.KeySettings.Joystick;
+#else
+		const array<uint8_t, MAX_CONTROLS> KeySettingsJoystick{};
+#endif
+		if (PHYSFS_write(file, KeySettingsJoystick, sizeof(KeySettingsJoystick), 1) != 1)
 			goto write_player_file_failed;
 		for (unsigned i = 0; i < MAX_CONTROLS*3; i++)
 			if (PHYSFS_write(file, "0", sizeof(ubyte), 1) != 1) // Skip obsolete Flightstick/Thrustmaster/Gravis map fields
 				goto write_player_file_failed;
-		if (PHYSFS_write(file, PlayerCfg.KeySettings[2], sizeof(PlayerCfg.KeySettings[2]), 1) != 1)
+		if (PHYSFS_write(file, PlayerCfg.KeySettings.Mouse, sizeof(PlayerCfg.KeySettings.Mouse), 1) != 1)
 			goto write_player_file_failed;
 		for (unsigned i = 0; i < MAX_CONTROLS*2; i++)
 			if (PHYSFS_write(file, "0", sizeof(ubyte), 1) != 1) // Skip obsolete Cyberman/Winjoy map fields

common/main/kconfig.h
@@ -95,7 +95,6 @@ constexpr std::integral_constant<unsigned, 50> MAX_CONTROLS{};
 #elif defined(DXX_BUILD_DESCENT_II)
 constexpr std::integral_constant<unsigned, 60> MAX_CONTROLS{};		// there are actually 48, so this leaves room for more
 #endif
-extern const array<array<uint8_t, MAX_CONTROLS>, 3> DefaultKeySettings;
 }
 namespace dcx {
 constexpr std::integral_constant<unsigned, 30> MAX_DXX_REBIRTH_CONTROLS{};

common/main/playsave.h
@@ -117,7 +117,13 @@ struct player_config : prohibit_void_ptr<player_config>
 	using secondary_weapon_order = array<uint8_t, MAX_SECONDARY_WEAPONS + 1>;
 	primary_weapon_order PrimaryOrder;
 	secondary_weapon_order SecondaryOrder;
-	array<array<ubyte, MAX_CONTROLS>, 3> KeySettings;
+	struct KeySettings {
+		array<uint8_t, MAX_CONTROLS> Keyboard,
+#if DXX_MAX_JOYSTICKS
+			Joystick,
+#endif
+			Mouse;
+	} KeySettings;
 	array<ubyte, MAX_DXX_REBIRTH_CONTROLS> KeySettingsRebirth;
 	int DefaultDifficulty;
 	int AutoLeveling;
@@ -190,6 +196,9 @@ extern struct player_config PlayerCfg;
 // Used to save kconfig values to disk.
 #ifdef dsx
 namespace dsx {
+
+extern const struct player_config::KeySettings DefaultKeySettings;
+
 void write_player_file();
 
 int new_player_config();

similar/main/kconfig.cpp
@@ -172,17 +172,31 @@ struct kc_menu : embed_window_pointer_t
 
 }
 
-const array<array<ubyte, MAX_CONTROLS>, 3> DefaultKeySettings{{
+const struct player_config::KeySettings DefaultKeySettings{
+	/* Keyboard */ {{
 #if defined(DXX_BUILD_DESCENT_I)
-	{{0xc8,0x48,0xd0,0x50,0xcb,0x4b,0xcd,0x4d,0x38,0xff,0xff,0x4f,0xff,0x51,0xff,0x4a,0xff,0x4e,0xff,0xff,0x10,0x47,0x12,0x49,0x1d,0x9d,0x39,0xff,0x21,0xff,0x1e,0xff,0x2c,0xff,0x30,0xff,0x13,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xf,0xff,0x33,0x0,0x34,0x0}},
-	{{0x0,0x1,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x1,0x0,0x0,0x0,0xff,0x0,0xff,0x0,0xff,0x0,0xff,0x0,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x0,0x0}},
-	{{0x0,0x1,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x1,0x0,0x0,0x0,0xff,0x0,0xff,0x0,0xff,0x0,0xff,0x0,0xff,0xff,0xff,0xff,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0}},
+		0xc8, 0x48, 0xd0, 0x50, 0xcb, 0x4b, 0xcd, 0x4d, 0x38, 0xff, 0xff, 0x4f, 0xff, 0x51, 0xff, 0x4a, 0xff, 0x4e, 0xff, 0xff, 0x10, 0x47, 0x12, 0x49, 0x1d, 0x9d, 0x39, 0xff, 0x21, 0xff, 0x1e, 0xff, 0x2c, 0xff, 0x30, 0xff, 0x13, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf, 0xff, 0x33, 0x0, 0x34, 0x0
 #elif defined(DXX_BUILD_DESCENT_II)
-	{{0xc8,0x48,0xd0,0x50,0xcb,0x4b,0xcd,0x4d,0x38,0xff,0xff,0x4f,0xff,0x51,0xff,0x4a,0xff,0x4e,0xff,0xff,0x10,0x47,0x12,0x49,0x1d,0x9d,0x39,0xff,0x21,0xff,0x1e,0xff,0x2c,0xff,0x30,0xff,0x13,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xf,0xff,0x1f,0xff,0x33,0xff,0x34,0xff,0x23,0xff,0x14,0xff,0xff,0xff,0x0,0x0}},
-	{{0x0,0x1,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x1,0x0,0x0,0x0,0xff,0x0,0xff,0x0,0xff,0x0,0xff,0x0,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x0,0x0,0x0,0x0}},
-	{{0x0,0x1,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x1,0x0,0x0,0x0,0xff,0x0,0xff,0x0,0xff,0x0,0xff,0x0,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x0,0x0,0x0,0x0,0x0}},
+		0xc8, 0x48, 0xd0, 0x50, 0xcb, 0x4b, 0xcd, 0x4d, 0x38, 0xff, 0xff, 0x4f, 0xff, 0x51, 0xff, 0x4a, 0xff, 0x4e, 0xff, 0xff, 0x10, 0x47, 0x12, 0x49, 0x1d, 0x9d, 0x39, 0xff, 0x21, 0xff, 0x1e, 0xff, 0x2c, 0xff, 0x30, 0xff, 0x13, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf, 0xff, 0x1f, 0xff, 0x33, 0xff, 0x34, 0xff, 0x23, 0xff, 0x14, 0xff, 0xff, 0xff, 0x0, 0x0
 #endif
-}};
+	}},
+#if DXX_MAX_JOYSTICKS
+	{{
+#if defined(DXX_BUILD_DESCENT_I)
+		0x0, 0x1, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1, 0x0, 0x0, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x0, 0x0
+#elif defined(DXX_BUILD_DESCENT_II)
+		0x0, 0x1, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1, 0x0, 0x0, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x0, 0x0, 0x0, 0x0
+#endif
+	}},
+#endif
+	/* Mouse */ {{
+#if defined(DXX_BUILD_DESCENT_I)
+	0x0, 0x1, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1, 0x0, 0x0, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0xff, 0xff, 0xff, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
+#elif defined(DXX_BUILD_DESCENT_II)
+	0x0, 0x1, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1, 0x0, 0x0, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0x0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x0, 0x0, 0x0, 0x0, 0x0
+#endif
+	}}
+};
 
 //	  id,  x,  y, w1, w2,  u,  d,   l, r,     text,   type, value
 constexpr kc_item kc_keyboard[] = {
@@ -997,13 +1011,13 @@ static window_event_result kconfig_key_command(window *, const d_event &event, k
 			return window_event_result::handled;
 		case KEY_CTRLED+KEY_R:	
 			if ( menu->items==kc_keyboard )
-				reset_mitem_values(kcm_keyboard, DefaultKeySettings[0]);
+				reset_mitem_values(kcm_keyboard, DefaultKeySettings.Keyboard);
 #if DXX_MAX_JOYSTICKS
 			else if (menu->items == kc_joystick)
-				reset_mitem_values(kcm_joystick, DefaultKeySettings[1]);
+				reset_mitem_values(kcm_joystick, DefaultKeySettings.Joystick);
 #endif
 			else if (menu->items == kc_mouse)
-				reset_mitem_values(kcm_mouse, DefaultKeySettings[2]);
+				reset_mitem_values(kcm_mouse, DefaultKeySettings.Mouse);
 			else if (menu->items == kc_rebirth)
 				reset_mitem_values(kcm_rebirth, DefaultKeySettingsRebirth);
 			return window_event_result::handled;
@@ -1135,15 +1149,15 @@ static window_event_result kconfig_handler(window *wind,const d_event &event, kc
 			// Update save values...
 			
 			for (unsigned i=0; i < lengthof(kc_keyboard); i++ ) 
-				PlayerCfg.KeySettings[0][i] = kcm_keyboard[i].value;
+				PlayerCfg.KeySettings.Keyboard[i] = kcm_keyboard[i].value;
 			
 #if DXX_MAX_JOYSTICKS
 			for (unsigned i=0; i < lengthof(kc_joystick); i++ ) 
-				PlayerCfg.KeySettings[1][i] = kcm_joystick[i].value;
+				PlayerCfg.KeySettings.Joystick[i] = kcm_joystick[i].value;
 #endif
 
 			for (unsigned i=0; i < lengthof(kc_mouse); i++ ) 
-				PlayerCfg.KeySettings[2][i] = kcm_mouse[i].value;
+				PlayerCfg.KeySettings.Mouse[i] = kcm_mouse[i].value;
 			
 			for (unsigned i=0; i < lengthof(kc_rebirth); i++)
 				PlayerCfg.KeySettingsRebirth[i] = kcm_rebirth[i].value;
@@ -1806,29 +1820,29 @@ void reset_cruise(void)
 void kc_set_controls()
 {
 	for (unsigned i=0; i < lengthof(kc_keyboard); i++ )
-		kcm_keyboard[i].value = PlayerCfg.KeySettings[0][i];
+		kcm_keyboard[i].value = PlayerCfg.KeySettings.Keyboard[i];
 
 #if DXX_MAX_JOYSTICKS
 	for (unsigned i=0; i < lengthof(kc_joystick); i++ )
 	{
-		kcm_joystick[i].value = PlayerCfg.KeySettings[1][i];
+		kcm_joystick[i].value = PlayerCfg.KeySettings.Joystick[i];
 		if (kc_joystick[i].type == BT_INVERT )
 		{
 			if (kcm_joystick[i].value!=1)
 				kcm_joystick[i].value = 0;
-			PlayerCfg.KeySettings[1][i] = kcm_joystick[i].value;
+			PlayerCfg.KeySettings.Joystick[i] = kcm_joystick[i].value;
 		}
 	}
 #endif
 
 	for (unsigned i=0; i < lengthof(kc_mouse); i++ )
 	{
-		kcm_mouse[i].value = PlayerCfg.KeySettings[2][i];
+		kcm_mouse[i].value = PlayerCfg.KeySettings.Mouse[i];
 		if (kc_mouse[i].type == BT_INVERT )
 		{
 			if (kcm_mouse[i].value!=1)
 				kcm_mouse[i].value = 0;
-			PlayerCfg.KeySettings[2][i] = kcm_mouse[i].value;
+			PlayerCfg.KeySettings.Mouse[i] = kcm_mouse[i].value;
 		}
 	}
 

commit 44154f155a422261bd7ebec7e7909d132f901d23

Fix max_joysticks=0 build
Fixes: 7311712399e1bc211785bf20028ff8b3e78c4745 ("Compile out joystick key settings when DXX_MAX_JOYSTICKS=0")

similar/main/playsave.cpp
@@ -533,10 +533,12 @@ static void read_player_dxx(const char *filename)
 				{
 					// reset joystick/mouse cycling fields
 #if defined(DXX_BUILD_DESCENT_I)
+#if DXX_MAX_JOYSTICKS
 					PlayerCfg.KeySettings.Joystick[44] = 255;
 					PlayerCfg.KeySettings.Joystick[45] = 255;
 					PlayerCfg.KeySettings.Joystick[46] = 255;
 					PlayerCfg.KeySettings.Joystick[47] = 255;
+#endif
 					PlayerCfg.KeySettings.Mouse[27] = 255;
 #endif
 					PlayerCfg.KeySettings.Mouse[28] = 255;