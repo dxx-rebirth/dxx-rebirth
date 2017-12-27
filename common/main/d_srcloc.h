dxx-rebirth/dxx-rebirth

commit 544fc0f893f7ca920c0fbae75aea2ac9aea11736

Add support for increased precision of gamelog timestamps
- Enable display of subsecond (Linux: microseconds; Windows:
  milliseconds) precision on gamelog timestamps.
- Add disabled support for YYYY-MM-DD leaders on gamelog timestamps.
  Activate it by defining DXX_CONSOLE_TIME_FORMAT_YMD to true.
- Add disabled support for capturing caller __FILE__, __LINE__ in calls
  to con_printf, con_puts.  Activate it by defining
  DXX_CONSOLE_SHOW_FILE_LINE to true.  If captured, write those to
  gamelog after the timestamp and before the text.  This feature (and
  only this feature) requires that DXX_HAVE_CXX_BUILTIN_FILE_LINE be
  defined, which is conditional on if the compiler has __builtin_FILE()
  and __builtin_LINE().  If the compiler lacks this support, attempts to
  enable this feature are ignored.
- Switch to using GetLocalTime on Windows.

common/main/d_srcloc.h
@@ -1,0 +1,117 @@
+/*
+ * This file is part of the DXX-Rebirth project <http://www.dxx-rebirth.com/>.
+ * It is copyright by its individual contributors, as recorded in the
+ * project's Git history.  See COPYING.txt at the top level for license
+ * terms and a link to the Git history.
+ */
+
+#pragma once
+
+#include <type_traits>
+#include <utility>
+#include "dxxsconf.h"
+
+template <bool capture_source_location = false>
+class location_wrapper
+{
+public:
+	location_wrapper() = default;
+	/* Allow callers to pass explicit file/line, for signature
+	 * compatibility with `location_wrapper<true>`.
+	 */
+	location_wrapper(const char *, unsigned)
+	{
+	}
+	/* This must be a template to be compatible with the `scratch_buffer`
+	 * template-typedef in `location_wrapper<true>`.
+	 */
+	template <std::size_t N>
+		using scratch_buffer = std::false_type;
+	template <std::size_t N>
+		static std::pair<char (&)[N], std::integral_constant<std::size_t, N>> insert_location_leader(char (&buffer)[N])
+		{
+			return {buffer, {}};
+		}
+	/* Define overloads to preserve const qualification */
+	static std::pair<char *, std::size_t> prepare_buffer(scratch_buffer<0> &, char *const text, const std::size_t len)
+	{
+		return {text, len};
+	}
+	static std::pair<const char *, std::size_t> prepare_buffer(scratch_buffer<0> &, const char *const text, const std::size_t len)
+	{
+		return {text, len};
+	}
+};
+
+#ifdef DXX_HAVE_CXX_BUILTIN_FILE_LINE
+#include <cstdio>
+
+template <>
+class location_wrapper<true>
+{
+	const char *file;
+	unsigned line;
+public:
+	template <std::size_t N>
+		using scratch_buffer = char[N];
+	location_wrapper(const char *const f = __builtin_FILE(), const unsigned l = __builtin_LINE()) :
+		file(f), line(l)
+	{
+	}
+	template <std::size_t N>
+		std::pair<char *, std::size_t> insert_location_leader(char (&buffer)[N]) const
+		{
+			const auto written = std::snprintf(buffer, sizeof(buffer), "%s:%u: ", file, line);
+			return {buffer + written, sizeof(buffer) - written};
+		}
+	template <std::size_t N>
+		std::pair<const char *, std::size_t> prepare_buffer(char (&buffer)[N], const char *const text, const std::size_t len) const
+		{
+			const auto written = std::snprintf(buffer, sizeof(buffer), "%s:%u: %.*s", file, line, static_cast<int>(len), text);
+			return {buffer, len + written};
+		}
+	/* Delegate to the const-qualified version, but return a `char *` to
+	 * match the non-const input `char *`, to preserve the choice of
+	 * overload for the function to receive this result.
+	 */
+	template <std::size_t N>
+		std::pair<char *, std::size_t> prepare_buffer(char (&buffer)[N], char *const text, const std::size_t len) const
+		{
+			return {buffer, prepare_buffer(buffer, const_cast<const char *>(text), len).second};
+		}
+};
+#endif
+
+template <typename T, bool capture_source_location = false>
+class location_value_wrapper : public location_wrapper<capture_source_location>
+{
+	T value;
+public:
+	/* Allow callers to pass explicit file/line, for signature
+	 * compatibility with `location_value_wrapper<T, true>`.
+	 */
+#ifndef DXX_HAVE_CXX_BUILTIN_FILE_LINE
+	location_value_wrapper(const T &v) :
+		value(v)
+	{
+	}
+#endif
+	location_value_wrapper(const T &v,
+		const char *const f
+#ifdef DXX_HAVE_CXX_BUILTIN_FILE_LINE
+		= __builtin_FILE(),
+#endif
+		const unsigned l
+#ifdef DXX_HAVE_CXX_BUILTIN_FILE_LINE
+		= __builtin_LINE()
+#endif
+		) :
+		location_wrapper<capture_source_location>(f, l),
+		value(v)
+	{
+	}
+	operator T() const
+	{
+		return value;
+	}
+};

common/include/console.h
@@ -16,6 +16,7 @@
 #include "fmtcheck.h"
 #include "cli.h"
 #include "cmd.h"
+#include "d_srcloc.h"
 
 #ifdef __cplusplus
 
@@ -38,19 +39,30 @@ struct console_buffer
 	int priority;
 };
 
+/* Define to 1 to capture the __FILE__, __LINE__ of callers to
+ * con_printf, con_puts, and show the captured value in `gamelog.txt`.
+ */
+#ifndef DXX_CONSOLE_SHOW_FILE_LINE
+#define DXX_CONSOLE_SHOW_FILE_LINE	0
+#endif
+
+using con_priority_wrapper = location_value_wrapper<con_priority, DXX_CONSOLE_SHOW_FILE_LINE>;
+
+#undef DXX_CONSOLE_SHOW_FILE_LINE
+
 void con_init(void);
-void con_puts(con_priority level, char *str, size_t len) __attribute_nonnull();
-void con_puts(con_priority level, const char *str, size_t len) __attribute_nonnull();
+void con_puts(con_priority_wrapper level, char *str, size_t len) __attribute_nonnull();
+void con_puts(con_priority_wrapper level, const char *str, size_t len) __attribute_nonnull();
 
 template <typename T>
-static inline void con_puts(const con_priority level, T &&str)
+static inline void con_puts(const con_priority_wrapper level, T &&str)
 {
 	using rr = typename std::remove_reference<T>::type;
 	constexpr std::size_t len = std::extent<rr>::value;
 	con_puts(level, str, len && std::is_const<rr>::value ? len - 1 : strlen(str));
 }
 
-void con_printf(con_priority level, const char *fmt, ...) __attribute_format_printf(2, 3);
+void con_printf(con_priority_wrapper level, const char *fmt, ...) __attribute_format_printf(2, 3);
 #ifdef DXX_CONSTANT_TRUE
 #define DXX_CON_PRINTF_CHECK_TRAILING_NEWLINE(F)	\
 	(DXX_CONSTANT_TRUE(sizeof((F)) > 1 && (F)[sizeof((F)) - 2] == '\n') &&	\

similar/main/console.cpp
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
-#include <time.h>
+#include <sys/time.h>
 #include <SDL.h>
 #include "window.h"
 #include "event.h"
@@ -33,6 +33,18 @@
 
 #include "dxxsconf.h"
 #include "compiler-array.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#ifndef DXX_CONSOLE_TIME_SHOW_YMD
+#define DXX_CONSOLE_TIME_SHOW_YMD	0
+#endif
+
+#ifndef DXX_CONSOLE_TIME_SHOW_MSEC
+#define DXX_CONSOLE_TIME_SHOW_MSEC	1
+#endif
 
 constexpr unsigned CON_LINES_ONSCREEN = 18;
 constexpr auto CON_SCROLL_OFFSET = CON_LINES_ONSCREEN - 3;
@@ -63,7 +75,7 @@ static void con_add_buffer_line(const con_priority priority, const char *const b
 	memcpy(&c.line,buffer, copy);
 }
 
-void (con_printf)(const con_priority priority, const char *const fmt, ...)
+void (con_printf)(const con_priority_wrapper priority, const char *const fmt, ...)
 {
 	va_list arglist;
 	char buffer[CON_LINE_LENGTH];
@@ -71,7 +83,8 @@ void (con_printf)(const con_priority priority, const char *const fmt, ...)
 	if (priority <= CGameArg.DbgVerbose)
 	{
 		va_start (arglist, fmt);
-		size_t len = vsnprintf (buffer, sizeof(buffer), fmt, arglist);
+		auto &&leader = priority.insert_location_leader(buffer);
+		size_t len = vsnprintf (leader.first, leader.second, fmt, arglist);
 		va_end (arglist);
 		con_force_puts(priority, buffer, len);
 	}
@@ -100,30 +113,89 @@ static void con_scrub_markup(char *buffer)
 
 static void con_print_file(const char *const buffer)
 {
+	char buf[1024];
+#ifndef _WIN32
 	/* Print output to stdout */
 	puts(buffer);
+#endif
 
 	/* Print output to gamelog.txt */
 	if (gamelog_fp)
 	{
-		time_t t;
-		t=time(NULL);
-		int tm_hour, tm_min, tm_sec;
-		if (const auto lt = localtime(&t))
+#if DXX_CONSOLE_TIME_SHOW_YMD
+#define DXX_CONSOLE_TIME_FORMAT_YMD	"%04i-%02i-%02i "
+#define DXX_CONSOLE_TIME_ARG_YMD	tm_year, tm_month, tm_day,
+#else
+#define DXX_CONSOLE_TIME_FORMAT_YMD	""
+#define DXX_CONSOLE_TIME_ARG_YMD
+#endif
+#if DXX_CONSOLE_TIME_SHOW_MSEC
+#ifdef _WIN32
+#define DXX_CONSOLE_TIME_FORMAT_MSEC	".%03i"
+#else
+#define DXX_CONSOLE_TIME_FORMAT_MSEC	".%06i"
+#endif
+#define DXX_CONSOLE_TIME_ARG_MSEC	tm_msec,
+#else
+#define DXX_CONSOLE_TIME_FORMAT_MSEC	""
+#define DXX_CONSOLE_TIME_ARG_MSEC
+#endif
+		int
+			DXX_CONSOLE_TIME_ARG_YMD
+			DXX_CONSOLE_TIME_ARG_MSEC
+			tm_hour, tm_min, tm_sec;
+#ifdef _WIN32
+#define DXX_LF	"\r\n"
+		SYSTEMTIME st = {};
+		GetLocalTime(&st);
+#if DXX_CONSOLE_TIME_SHOW_YMD
+		tm_year = st.wYear;
+		tm_month = st.wMonth;
+		tm_day = st.wDay;
+#endif
+		tm_hour = st.wHour;
+		tm_min = st.wMinute;
+		tm_sec = st.wSecond;
+#if DXX_CONSOLE_TIME_SHOW_MSEC
+		tm_msec = st.wMilliseconds;
+#endif
+#else
+#define DXX_LF	"\n"
+		struct timeval tv;
+		if (gettimeofday(&tv, nullptr))
+			tv = {};
+		if (const auto lt = localtime(&tv.tv_sec))
 		{
+#if DXX_CONSOLE_TIME_SHOW_YMD
+			tm_year = lt->tm_year;
+			tm_month = lt->tm_mon;
+			tm_day = lt->tm_mday;
+#endif
 			tm_hour = lt->tm_hour;
 			tm_min = lt->tm_min;
 			tm_sec = lt->tm_sec;
+#if DXX_CONSOLE_TIME_SHOW_MSEC
+			tm_msec = tv.tv_usec;
+#endif
 		}
 		else
-			tm_hour = tm_min = tm_sec = -1;
-#ifdef _WIN32 // stupid hack to force DOS-style newlines
-#define DXX_LF	"\r\n"
-#else
-#define DXX_LF	"\n"
+		{
+#if DXX_CONSOLE_TIME_SHOW_YMD
+			tm_year = tm_month = tm_day =
 #endif
-		PHYSFSX_printf(gamelog_fp, "%02i:%02i:%02i %s" DXX_LF, tm_hour, tm_min, tm_sec, buffer);
+#if DXX_CONSOLE_TIME_SHOW_MSEC
+			tm_msec =
+#endif
+			tm_hour = tm_min = tm_sec = -1;
+		}
+#endif
+		const size_t len = snprintf(buf, sizeof(buf), DXX_CONSOLE_TIME_FORMAT_YMD "%02i:%02i:%02i" DXX_CONSOLE_TIME_FORMAT_MSEC " %s" DXX_LF, DXX_CONSOLE_TIME_ARG_YMD tm_hour, tm_min, tm_sec, DXX_CONSOLE_TIME_ARG_MSEC buffer);
+		PHYSFS_write(gamelog_fp, buf, 1, len);
 #undef DXX_LF
+#undef DXX_CONSOLE_TIME_ARG_MSEC
+#undef DXX_CONSOLE_TIME_FORMAT_MSEC
+#undef DXX_CONSOLE_TIME_ARG_YMD
+#undef DXX_CONSOLE_TIME_FORMAT_YMD
 	}
 }
 
@@ -139,19 +211,25 @@ static void con_force_puts(const con_priority priority, char *const buffer, cons
 	con_print_file(buffer);
 }
 
-void con_puts(const con_priority priority, char *const buffer, const size_t len)
-{
-	if (priority <= CGameArg.DbgVerbose)
-		con_force_puts(priority, buffer, len);
-}
-
-void con_puts(const con_priority priority, const char *const buffer, const size_t len)
+void con_puts(const con_priority_wrapper priority, char *const buffer, const size_t len)
 {
 	if (priority <= CGameArg.DbgVerbose)
 	{
+		typename con_priority_wrapper::scratch_buffer<CON_LINE_LENGTH> scratch_buffer;
+		auto &&b = priority.prepare_buffer(scratch_buffer, buffer, len);
+		con_force_puts(priority, b.first, b.second);
+	}
+}
+
+void con_puts(const con_priority_wrapper priority, const char *const buffer, const size_t len)
+{
+	if (priority <= CGameArg.DbgVerbose)
+	{
+		typename con_priority_wrapper::scratch_buffer<CON_LINE_LENGTH> scratch_buffer;
+		auto &&b = priority.prepare_buffer(scratch_buffer, buffer, len);
 		/* add given string to con_buffer */
-		con_add_buffer_line(priority, buffer, len);
-		con_print_file(buffer);
+		con_add_buffer_line(priority, b.first, b.second);
+		con_print_file(b.first);
 	}
 }
 

commit d2612734a35d35e6890f609a3275347010386e4e

Fix compile error for !DXX_HAVE_CXX_BUILTIN_FILE_LINE
If !defined(DXX_HAVE_CXX_BUILTIN_FILE_LINE), the default value is
omitted, but the comma between values is still required.  The comma was
incorrectly guarded, so it was present only when a default value was
set.

Reported-by: kreatordxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/361>
Fixes: 544fc0f893f7ca920c0fbae75aea2ac9aea11736 ("Add support for increased precision of gamelog timestamps")

common/main/d_srcloc.h
@@ -99,9 +99,9 @@ public:
 	location_value_wrapper(const T &v,
 		const char *const f
 #ifdef DXX_HAVE_CXX_BUILTIN_FILE_LINE
-		= __builtin_FILE(),
+		= __builtin_FILE()
 #endif
-		const unsigned l
+		, const unsigned l
 #ifdef DXX_HAVE_CXX_BUILTIN_FILE_LINE
 		= __builtin_LINE()
 #endif