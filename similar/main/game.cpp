dxx-rebirth/dxx-rebirth

commit 131c1b9f4d556fbd9d67815755608602b408698b

Add support for PNG screenshots

similar/main/game.cpp
@@ -29,6 +29,10 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include <string.h>
 #include <stdarg.h>
 #include <SDL.h>
+#if DXX_USE_SCREENSHOT_FORMAT_PNG
+#include <png.h>
+#include "vers_id.h"
+#endif
 
 #if DXX_USE_OGL
 #include "ogl_init.h"
@@ -511,10 +515,260 @@ void move_player_2_segment(const vmsegptridx_t seg,int side)
 }
 
 namespace dcx {
+
+namespace {
+
+#if DXX_USE_SCREENSHOT_FORMAT_PNG
+struct RAIIpng_struct
+{
+	png_struct *png_ptr;
+	png_info *info_ptr = nullptr;
+	RAIIpng_struct(png_struct *const p) :
+		png_ptr(p)
+	{
+	}
+	~RAIIpng_struct()
+	{
+		png_destroy_write_struct(&png_ptr, &info_ptr);
+	}
+};
+
+struct d_screenshot : RAIIpng_struct
+{
+	DXX_INHERIT_CONSTRUCTORS(d_screenshot,RAIIpng_struct);
+	/* error handling callbacks */
+	[[noreturn]]
+	static void png_error_cb(png_struct *png, const char *str);
+	static void png_warn_cb(png_struct *png, const char *str);
+	/* output callbacks */
+	static void png_write_cb(png_struct *png, uint8_t *buf, png_size_t);
+	static void png_flush_cb(png_struct *png);
+	class png_exception : std::exception
+	{
+	};
+};
+
+void d_screenshot::png_error_cb(png_struct *const png, const char *const str)
+{
+	/* libpng requires that this function not return to its caller, and
+	 * will abort the program if this requirement is violated.  However,
+	 * throwing an exception that unwinds out past libpng is permitted.
+	 */
+	(void)png;
+	con_printf(CON_URGENT, "libpng error: %s", str);
+	throw png_exception();
+}
+
+void d_screenshot::png_warn_cb(png_struct *const png, const char *const str)
+{
+	(void)png;
+	con_printf(CON_URGENT, "libpng warning: %s", str);
+}
+
+void d_screenshot::png_write_cb(png_struct *const png, uint8_t *const buf, const png_size_t size)
+{
+	const auto file = reinterpret_cast<PHYSFS_File *>(png_get_io_ptr(png));
+	PHYSFS_write(file, buf, size, 1);
+}
+
+void d_screenshot::png_flush_cb(png_struct *const png)
+{
+	(void)png;
+}
+
+void record_screenshot_time(const struct tm &tm, png_struct *const png_ptr, png_info *const info_ptr)
+{
+#ifdef PNG_tIME_SUPPORTED
+	png_time pt{};
+	pt.year = tm.tm_year + 1900;
+	pt.month = tm.tm_mon + 1;
+	pt.day = tm.tm_mday;
+	pt.hour = tm.tm_hour;
+	pt.minute = tm.tm_min;
+	pt.second = tm.tm_sec;
+	png_set_tIME(png_ptr, info_ptr, &pt);
+#else
+	(void)png_ptr;
+	(void)info_ptr;
+	con_printf(CON_NORMAL, "libpng configured without support for time chunk: screenshot will lack time record.");
+#endif
+}
+
+#ifdef PNG_TEXT_SUPPORTED
+void record_screenshot_text_metadata(png_struct *const png_ptr, png_info *const info_ptr)
+{
+	array<png_text, 6> text_fields{};
+	char descent_version[80];
+	char descent_build_datetime[21];
+	std::string current_mission_path;
+	ntstring<MISSION_NAME_LEN> current_mission_name;
+	char current_level_number[4];
+	char viewer_segment[sizeof("65536")];
+	unsigned idx = 0;
+	char key_descent_version[] = "Rebirth.version";
+	{
+		auto &t = text_fields[idx++];
+		auto &text = descent_version;
+		t.key = key_descent_version;
+		text[sizeof(text) - 1] = 0;
+		strncpy(text, g_descent_version, sizeof(text) - 1);
+		t.text = text;
+		t.compression = PNG_TEXT_COMPRESSION_NONE;
+	}
+	char key_descent_build_datetime[] = "Rebirth.build_datetime";
+	{
+		auto &t = text_fields[idx++];
+		auto &text = descent_build_datetime;
+		t.key = key_descent_build_datetime;
+		text[sizeof(text) - 1] = 0;
+		strncpy(text, g_descent_build_datetime, sizeof(text) - 1);
+		t.text = text;
+		t.compression = PNG_TEXT_COMPRESSION_NONE;
+	}
+	char key_current_mission_path[] = "Rebirth.mission.pathname";
+	char key_current_mission_name[] = "Rebirth.mission.textname";
+	char key_viewer_segment[] = "Rebirth.viewer_segment";
+	char key_current_level_number[] = "Rebirth.current_level_number";
+	if (const auto current_mission = Current_mission.get())
+	{
+		{
+			auto &t = text_fields[idx++];
+			t.key = key_current_mission_path;
+			current_mission_path = current_mission->path;
+			t.text = &current_mission_path[0];
+			t.compression = PNG_TEXT_COMPRESSION_NONE;
+		}
+		{
+			auto &t = text_fields[idx++];
+			t.key = key_current_mission_name;
+			current_mission_name = current_mission->mission_name;
+			t.text = current_mission_name.data();
+			t.compression = PNG_TEXT_COMPRESSION_NONE;
+		}
+		{
+			auto &t = text_fields[idx++];
+			t.key = key_current_level_number;
+			t.text = current_level_number;
+			t.compression = PNG_TEXT_COMPRESSION_NONE;
+			snprintf(current_level_number, sizeof(current_level_number), "%i", Current_level_num);
+		}
+		if (const auto viewer = Viewer)
+		{
+			auto &t = text_fields[idx++];
+			t.key = key_viewer_segment;
+			t.text = viewer_segment;
+			t.compression = PNG_TEXT_COMPRESSION_NONE;
+			snprintf(viewer_segment, sizeof(viewer_segment), "%hu", Viewer->segnum);
+		}
+	}
+	png_set_text(png_ptr, info_ptr, text_fields.data(), idx);
+}
+#endif
+
+#if DXX_USE_OGL
+#define write_screenshot_png(F,T,B,P)	write_screenshot_png(F,T,B)
+#endif
+unsigned write_screenshot_png(PHYSFS_File *const file, const struct tm *const tm, const grs_bitmap &bitmap, const palette_array_t &pal)
+{
+	const unsigned bm_w = bitmap.bm_w;
+	const unsigned bm_h = bitmap.bm_h;
+#if DXX_USE_OGL
+	const unsigned bufsize = bm_w * bm_h * 3;
+	const auto buf = std::make_unique<uint8_t[]>(bufsize);
+	const auto begin_byte_buffer = buf.get();
+	glReadPixels(0, 0, bm_w, bm_h, GL_RGB, GL_UNSIGNED_BYTE, begin_byte_buffer);
+#else
+	const unsigned bufsize = bitmap.bm_rowsize * bm_h;
+	const auto begin_byte_buffer = bitmap.bm_mdata;
+#endif
+	d_screenshot ss(png_create_write_struct(PNG_LIBPNG_VER_STRING, &ss, &d_screenshot::png_error_cb, &d_screenshot::png_warn_cb));
+	if (!ss.png_ptr)
+	{
+		con_puts(CON_URGENT, "Cannot save screenshot: libpng png_create_write_struct failed");
+		return 1;
+	}
+	/* Assert that Rebirth type rgb_t is layout compatible with
+	 * libpng type png_color, so that the Rebirth palette_array_t
+	 * can be safely reinterpret_cast to an array of png_color.
+	 * Without this, it would be necessary to copy each rgb_t
+	 * palette entry into a libpng png_color.
+	 */
+	static_assert(sizeof(png_color) == sizeof(rgb_t), "size mismatch");
+	static_assert(offsetof(png_color, red) == offsetof(rgb_t, r), "red offsetof mismatch");
+	static_assert(offsetof(png_color, green) == offsetof(rgb_t, g), "green offsetof mismatch");
+	static_assert(offsetof(png_color, blue) == offsetof(rgb_t, b), "blue offsetof mismatch");
+	try {
+		ss.info_ptr = png_create_info_struct(ss.png_ptr);
+		if (tm)
+			record_screenshot_time(*tm, ss.png_ptr, ss.info_ptr);
+		png_set_write_fn(ss.png_ptr, file, &d_screenshot::png_write_cb, &d_screenshot::png_flush_cb);
+#if DXX_USE_OGL
+		const auto color_type = PNG_COLOR_TYPE_RGB;
+#else
+		png_set_PLTE(ss.png_ptr, ss.info_ptr, reinterpret_cast<const png_color *>(pal.data()), pal.size());
+		const auto color_type = PNG_COLOR_TYPE_PALETTE;
+#endif
+		png_set_IHDR(ss.png_ptr, ss.info_ptr, bm_w, bm_h, 8 /* always 256 colors */, color_type, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+#ifdef PNG_TEXT_SUPPORTED
+		record_screenshot_text_metadata(ss.png_ptr, ss.info_ptr);
+#endif
+		png_write_info(ss.png_ptr, ss.info_ptr);
+		array<png_byte *, 1024> row_pointers;
+		const auto rpb = row_pointers.begin();
+		auto o = rpb;
+		const auto end_byte_buffer = begin_byte_buffer + bufsize;
+#if DXX_USE_OGL
+		/* OpenGL glReadPixels returns an image with origin in bottom
+		 * left.  Write rows from end back to beginning, since PNG
+		 * expects origin in top left.  If rows were written in memory
+		 * order, the image would be vertically flipped.
+		 */
+		const uint_fast32_t stride = bm_w * 3;	/* Without palette, written data is 3-byte-sized RGB tuples of color */
+		for (auto p = end_byte_buffer; p != begin_byte_buffer;)
+#else
+		const uint_fast32_t stride = bm_w;	/* With palette, written data is byte-sized indices into a color table */
+		/* SDL canvas uses an image with origin in top left.  Write rows
+		 * in memory order, since this matches the PNG layout.
+		 */
+		for (auto p = begin_byte_buffer; p != end_byte_buffer;)
+#endif
+		{
+#if DXX_USE_OGL
+			p -= stride;
+#else
+			p += stride;
+#endif
+			*o++ = p;
+			if (o == row_pointers.end())
+			{
+				/* Internal capacity exhausted.  Flush rows and rewind
+				 * to the beginning of the array.
+				 */
+				o = rpb;
+				png_write_rows(ss.png_ptr, o, row_pointers.size());
+			}
+		}
+		/* Flush any trailing rows */
+		if (const auto len = o - rpb)
+			png_write_rows(ss.png_ptr, rpb, len);
+		png_write_end(ss.png_ptr, ss.info_ptr);
+		return 0;
+	} catch (const d_screenshot::png_exception &) {
+		/* Destructor unwind will handle the exception.  This catch is
+		 * only required to prevent further propagation.
+		 *
+		 * Return nonzero to instruct the caller to delete the failed
+		 * file.
+		 */
+		return 1;
+	}
+}
+#endif
+
+}
+
 void save_screen_shot(int automap_flag)
 {
-	static unsigned savenum;
-	char savename[FILENAME_LEN + sizeof(SCRNS_DIR)];
 #if DXX_USE_OGL
 	if (!CGameArg.DbgGlReadPixelsOk)
 	{
@@ -522,30 +776,76 @@ void save_screen_shot(int automap_flag)
 			HUD_init_message_literal(HM_DEFAULT, "glReadPixels not supported on your configuration");
 		return;
 	}
+#endif
+#if DXX_USE_SCREENSHOT_FORMAT_PNG
+#define DXX_SCREENSHOT_FILE_EXTENSION	"png"
+#elif DXX_USE_SCREENSHOT_FORMAT_LEGACY
+#if DXX_USE_OGL
 #define DXX_SCREENSHOT_FILE_EXTENSION	"tga"
 #else
 #define DXX_SCREENSHOT_FILE_EXTENSION	"pcx"
+#endif
 #endif
 
 	if (!PHYSFSX_exists(SCRNS_DIR,0))
 		PHYSFS_mkdir(SCRNS_DIR); //try making directory
 
 	pause_game_world_time p;
-	do
+	unsigned tm_sec;
+	unsigned tm_min;
+	unsigned tm_hour;
+	unsigned tm_mday;
+	unsigned tm_mon;
+	unsigned tm_year;
+	const auto t = time(nullptr);
+	struct tm *tm = nullptr;
+	if (t == static_cast<time_t>(-1) || !(tm = gmtime(&t)))
+		tm_year = tm_mon = tm_mday = tm_hour = tm_min = tm_sec = 0;
+	else
 	{
-		snprintf(savename, sizeof(savename), "%sscrn%04u." DXX_SCREENSHOT_FILE_EXTENSION, SCRNS_DIR, savenum++);
+		tm_sec = tm->tm_sec;
+		tm_min = tm->tm_min;
+		tm_hour = tm->tm_hour;
+		tm_mday = tm->tm_mday;
+		tm_mon = tm->tm_mon + 1;
+		tm_year = tm->tm_year + 1900;
+	}
+	/* Colon is not legal in Windows filenames, so use - to separate
+	 * hour:minute:second.
+	 */
+#define DXX_SCREENSHOT_TIME_FORMAT_STRING	SCRNS_DIR "%04u-%02u-%02u.%02u-%02u-%02u"
+#define DXX_SCREENSHOT_TIME_FORMAT_VALUES	tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec
+	/* Reserve extra space for the trailing -NN disambiguation.  This
+	 * is only used if multiple screenshots get the same time, so it is
+	 * unlikely to be seen in practice and very unlikely to be exhausted
+	 * (unless the clock is frozen or returns invalid times).
+	 */
+	char savename[sizeof(SCRNS_DIR) + sizeof("2000-01-01.00-00-00.NN.ext")];
+	snprintf(savename, sizeof(savename), DXX_SCREENSHOT_TIME_FORMAT_STRING "." DXX_SCREENSHOT_FILE_EXTENSION, DXX_SCREENSHOT_TIME_FORMAT_VALUES);
+	for (unsigned savenum = 0; PHYSFS_exists(savename) && savenum != 100; ++savenum)
+	{
+		snprintf(savename, sizeof(savename), DXX_SCREENSHOT_TIME_FORMAT_STRING ".%02u." DXX_SCREENSHOT_FILE_EXTENSION, DXX_SCREENSHOT_TIME_FORMAT_VALUES, savenum);
+#undef DXX_SCREENSHOT_TIME_FORMAT_VALUES
+#undef DXX_SCREENSHOT_TIME_FORMAT_STRING
 #undef DXX_SCREENSHOT_FILE_EXTENSION
-		if (savenum > 9999)
-			break; // that's enough I think.
-	} while (PHYSFSX_exists(savename,0));
+	}
+	unsigned write_error;
+	if (const auto file = PHYSFSX_openWriteBuffered(savename))
+	{
 	if (!automap_flag)
 		HUD_init_message(HM_DEFAULT, "%s '%s'", TXT_DUMPING_SCREEN, &savename[sizeof(SCRNS_DIR) - 1]);
+
 #if DXX_USE_OGL
 #if !DXX_USE_OGLES
 	glReadBuffer(GL_FRONT);
 #endif
-
-	write_bmp(savename, grd_curscreen->get_screen_width(), grd_curscreen->get_screen_height());
+#if DXX_USE_SCREENSHOT_FORMAT_PNG
+	write_error = write_screenshot_png(file, tm, grd_curscreen->sc_canvas.cv_bitmap, void /* unused */);
+#elif DXX_USE_SCREENSHOT_FORMAT_LEGACY
+	write_bmp(file, grd_curscreen->get_screen_width(), grd_curscreen->get_screen_height());
+	/* write_bmp never fails */
+	write_error = 0;
+#endif
 #else
 	grs_canvas &screen_canv = grd_curscreen->sc_canvas;
 	palette_array_t pal;
@@ -554,9 +854,30 @@ void save_screen_shot(int automap_flag)
 	gr_ubitmap(*temp_canv, screen_canv.cv_bitmap);
 
 	gr_palette_read(pal);		//get actual palette from the hardware
-	pcx_write_bitmap(savename,&temp_canv->cv_bitmap,pal);
-	gr_ubitmap(screen_canv, temp_canv->cv_bitmap);
+	// Correct palette colors
+	range_for (auto &i, pal)
+	{
+		i.r <<= 2;
+		i.g <<= 2;
+		i.b <<= 2;
+	}
+#if DXX_USE_SCREENSHOT_FORMAT_PNG
+	write_error = write_screenshot_png(file, tm, grd_curscreen->sc_canvas.cv_bitmap, pal);
+#elif DXX_USE_SCREENSHOT_FORMAT_LEGACY
+	write_error = pcx_write_bitmap(file, &temp_canv->cv_bitmap, pal);
 #endif
+#endif
+	}
+	else
+	{
+		if (!automap_flag)
+			HUD_init_message(HM_DEFAULT, "Failed to open screenshot file for writing: %s", &savename[sizeof(SCRNS_DIR) - 1]);
+		else
+			con_printf(CON_URGENT, "Failed to open screenshot file for writing: %s", savename);
+		return;
+	}
+	if (write_error)
+		PHYSFS_delete(savename);
 }
 
 //initialize flying

SConstruct
@@ -902,15 +902,15 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 	# stage failed and providing a text error message to show to the
 	# user.  Some callers handle failure by retrying with other options.
 	# Others abort the SConf run.
-	def _soft_check_system_library(self,context,header,main,lib,successflags={}):
+	def _soft_check_system_library(self,context,header,main,lib,text='',successflags={}):
 		include = '\n'.join(['#include <%s>' % h for h in header])
 		header = header[-1]
 		# Test library.  On success, good.  On failure, test header to
 		# give the user more help.
-		if self.Link(context, text=include, main=main, msg='for usable library %s' % lib, successflags=successflags):
+		if self.Link(context, text=include + text, main=main, msg='for usable library %s' % lib, successflags=successflags):
 			return
 		Compile = self.Compile
-		if Compile(context, text=include, main=main, msg='for usable header %s' % header, testflags=successflags):
+		if Compile(context, text=include + text, main=main, msg='for usable header %s' % header, testflags=successflags):
 			return (0, "Header %s is usable, but library %s is not usable." % (header, lib))
 		if Compile(context, text=include, main='', msg='for parseable header %s' % header, testflags=successflags):
 			return (1, "Header %s is parseable, but cannot compile the test program." % header)
@@ -958,9 +958,9 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 	def _check_user_settings_opengl(self,context):
 		user_settings = self.user_settings
 		Result = context.Result
-		Define = context.sconf.Define
-		Define('DXX_USE_OGL', int(user_settings.opengl))
-		Define('DXX_USE_OGLES', int(user_settings.opengles))
+		_define_macro = self._define_macro
+		_define_macro(context, 'DXX_USE_OGL', int(user_settings.opengl))
+		_define_macro(context, 'DXX_USE_OGLES', int(user_settings.opengles))
 		if user_settings.opengles:
 			s = 'OpenGL ES'
 		elif user_settings.opengl:
@@ -1001,6 +1001,78 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 		if use_tracker:
 			self.check_curl(context)
 			self.check_jsoncpp(context)
+	@_implicit_test
+	def check_libpng(self,context,
+		_header=(
+			'cstdint',
+			'png.h',
+		),
+		_guess_flags={'LIBS' : ['png']},
+		_text='''
+namespace {
+struct d_screenshot
+{
+	png_struct *png_ptr;
+	png_info *info_ptr = nullptr;
+	static void png_error_cb(png_struct *, const char *) {}
+	static void png_warn_cb(png_struct *, const char *) {}
+	static void png_write_cb(png_struct *, uint8_t *, png_size_t) {}
+	static void png_flush_cb(png_struct *) {}
+};
+}
+''',
+		_main='''
+	d_screenshot ss;
+	ss.png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, &ss, &d_screenshot::png_error_cb, &d_screenshot::png_warn_cb);
+	png_set_write_fn(ss.png_ptr, &ss, &d_screenshot::png_write_cb, &d_screenshot::png_flush_cb);
+	ss.info_ptr = png_create_info_struct(ss.png_ptr);
+#ifdef PNG_tIME_SUPPORTED
+	png_time pt{};
+	pt.year = 2018;
+	pt.month = 1;
+	pt.day = 1;
+	pt.hour = 1;
+	pt.minute = 1;
+	pt.second = 1;
+	png_set_tIME(ss.png_ptr, ss.info_ptr, &pt);
+#endif
+#if DXX_USE_OGL
+	const auto color_type = PNG_COLOR_TYPE_RGB;
+#else
+	png_set_PLTE(ss.png_ptr, ss.info_ptr, reinterpret_cast<const png_color *>(&ss), 256 * 3);
+	const auto color_type = PNG_COLOR_TYPE_PALETTE;
+#endif
+	png_set_IHDR(ss.png_ptr, ss.info_ptr, 1, 1, 8, color_type, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+#ifdef PNG_TEXT_SUPPORTED
+	png_text text_fields[1] = {};
+	png_set_text(ss.png_ptr, ss.info_ptr, text_fields, 1);
+#endif
+	png_write_info(ss.png_ptr, ss.info_ptr);
+	png_byte *row_pointers[1024]{};
+	png_write_rows(ss.png_ptr, row_pointers, 1024);
+	png_write_end(ss.png_ptr, ss.info_ptr);
+	png_destroy_write_struct(&ss.png_ptr, &ss.info_ptr);
+'''):
+		successflags = self.pkgconfig.merge(context, self.msgprefix, self.user_settings, 'libpng', 'libpng', _guess_flags)
+		return self._soft_check_system_library(context, header=_header, main=_main, lib='png', text=_text, successflags=successflags)
+	@_custom_test
+	def _check_user_settings_screenshot(self,context):
+		user_settings = self.user_settings
+		screenshot_mode = user_settings.screenshot
+		if screenshot_mode == 'png':
+			screenshot_format_type = screenshot_mode
+		elif screenshot_mode == 'legacy':
+			screenshot_format_type = 'tga' if user_settings.opengl else 'pcx'
+		else:
+			return
+		context.Result('%s: checking how to format screenshots...%s' % (self.msgprefix, screenshot_format_type))
+		Define = context.sconf.Define
+		Define('DXX_USE_SCREENSHOT_FORMAT_PNG', int(screenshot_mode == 'png'))
+		Define('DXX_USE_SCREENSHOT_FORMAT_LEGACY', int(screenshot_mode == 'legacy'))
+		if screenshot_mode == 'png':
+			e = self.check_libpng(context)
+			if e:
+				raise SCons.Errors.StopError(e[1] + '  Set screenshot=legacy to remove screenshot libpng requirement.')
 
 	# Require _WIN32_WINNT >= 0x0501 to enable getaddrinfo
 	# Require _WIN32_WINNT >= 0x0600 to enable some useful AI_* flags
@@ -3351,7 +3423,7 @@ class DXXCommon(LazyObjectConstructor):
 					('opengl', True, 'build with OpenGL support'),
 					('opengles', self.default_opengles, 'build with OpenGL ES support'),
 					('editor', False, 'include editor into build (!EXPERIMENTAL!)'),
-					('sdl2', False, 'use libSDL2+SDL2_mixer (!EXPERIMENTAL!)'),
+					('sdl2', False, 'use libSDL2+SDL2_mixer (!DEVELOPERS ONLY - KNOWN BROKEN!)'),
 					('sdlmixer', True, 'build with SDL_Mixer support for sound and music (includes external music support)'),
 					('ipv6', False, 'enable UDP/IPv6 for multiplayer'),
 					('use_udp', True, 'enable UDP support'),
@@ -3409,6 +3481,7 @@ class DXXCommon(LazyObjectConstructor):
 				'arguments': (
 					('host_endian', None, 'endianness of host platform', {'allowed_values' : ('little', 'big')}),
 					('host_platform', 'linux' if sys.platform == 'linux2' else sys.platform, 'cross-compile to specified platform', {'allowed_values' : ('darwin', 'linux', 'openbsd6', 'win32')}),
+					('screenshot', 'png', 'screenshot file format', {'allowed_values' : ('legacy', 'png')}),
 				),
 			},
 			{
@@ -3899,6 +3972,10 @@ class DXXCommon(LazyObjectConstructor):
 		env = self.env
 		user_settings = self.user_settings
 
+		# Insert default CXXFLAGS.  User-specified CXXFLAGS, if any, are
+		# appended to this list and will override these defaults.  The
+		# defaults are present to ensure that a user who does not set
+		# any options gets a good default experience.
 		env.Prepend(CXXFLAGS = ['-g', '-O2'])
 		# Raspberry Pi?
 		if user_settings.raspberrypi == 'yes':

common/include/pcx.h
@@ -28,6 +28,9 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "dxxsconf.h"
 #include "dsx-ns.h"
 #include "fwd-gr.h"
+#if !DXX_USE_OGL && DXX_USE_SCREENSHOT_FORMAT_LEGACY
+#include <physfs.h>
+#endif
 
 namespace dcx {
 struct palette_array_t;
@@ -59,7 +62,9 @@ int pcx_read_bitmap(const char * filename, grs_main_bitmap &bmp, palette_array_t
 
 // Writes the bitmap bmp to filename, using palette. Returns error code.
 
-int pcx_write_bitmap(const char *filename, const grs_bitmap *bmp, palette_array_t &palette);
+#if !DXX_USE_OGL && DXX_USE_SCREENSHOT_FORMAT_LEGACY
+int pcx_write_bitmap(PHYSFS_File *, const grs_bitmap *bmp, palette_array_t &palette);
+#endif
 
 extern const char *pcx_errormsg(int error_number);
 

common/main/game.h
@@ -168,7 +168,9 @@ void reset_time();       // called when starting level
 namespace dcx {
 
 // If automap_flag == 1, then call automap routine to write message.
-void write_bmp(const char *savename, unsigned w, unsigned h);
+#if DXX_USE_SCREENSHOT_FORMAT_LEGACY
+void write_bmp(PHYSFS_File *, unsigned w, unsigned h);
+#endif
 extern void save_screen_shot(int automap_flag);
 
 }

contrib/gentoo/games-action/dxx-rebirth/dxx-rebirth-0.59.100.ebuild
@@ -1,1 +1,200 @@
-dxx-rebirth-9999.ebuild
+# Distributed under the terms of the GNU General Public License v2
+#
+# After release 0.58.1 and before beta release 0.59.100, upstream
+# combined the source for the Descent 1 and Descent 2 engines into a
+# single tree.  The combined tree builds common code into a static
+# library, which is linked into both games, but not installed.  Users
+# who want both engines benefit from this because they can build the
+# common code once, rather than once per game.  This ebuild supports
+# building one or both engines, depending on USE=d1x and USE=d2x.
+
+EAPI=6
+
+inherit eutils scons-utils toolchain-funcs xdg
+if [[ "$PV" = 9999 ]]; then
+	inherit git-r3
+	EGIT_REPO_URI="https://github.com/dxx-rebirth/dxx-rebirth"
+else
+	SRC_URI="https://github.com/dxx-rebirth/dxx-rebirth/archive/$PV.zip -> $PN-$PVR.zip"
+
+	# Restriction only for use in private overlays.  When this is added to a
+	# public tree, post the sources to a mirror and remove this restriction.
+	RESTRICT='mirror'
+fi
+
+DESCRIPTION="Descent Rebirth - enhanced Descent 1 & 2 engine"
+HOMEPAGE="http://www.dxx-rebirth.com/"
+
+LICENSE="DXX-Rebirth GPL-3"
+SLOT="0"
+# Other architectures are reported to work, but not tested regularly by
+# the core team.
+#
+# Raspberry Pi support is tested by an outside contributor, and his
+# fixes are merged into the main source by upstream.
+#
+# Cross-compilation to Windows is also supported.
+KEYWORDS="amd64 x86"
+# Default to building both game engines.  The total size is relatively
+# small.
+IUSE="+d1x +d2x debug editor +flac ipv6 +joystick l10n_de +midi +mp3 +music +opengl opl3-musicpack sc55-musicpack tracker +vorbis"
+
+DEPEND="dev-games/physfs[hog,mvl,zip]
+	media-libs/libsdl[joystick?,opengl?,sound,video]
+	music? ( media-libs/sdl-mixer )
+	opengl? (
+		virtual/opengl
+		virtual/glu )"
+
+# As of this writing, there is no Portage shorthand syntax to express:
+# "
+# 	flag1? ( package[flag1] )
+# 	flag2? ( package[flag2] )
+# 	...
+# 	flagN? ( package[flagN] )
+# ", such that unsetting all flags removes the dependency on package.
+# Fake it by using a text fragment that is repeatedly expanded with
+# differing substitutions.
+#
+# The use of single quotes is intentional here.  The ${word} is a
+# placeholder to be matched by text substitution when the fragment is
+# expanded, not a shell variable reference.
+#
+# For each flag, depend on freedata using the same flag.  If none of the
+# flags are set, freedata is not needed.
+DXX_RDEPEND_USE_FREEDATA_FRAGMENT='
+	${USE}? ( games-action/descent${ENGINE}-freedata[${USE}] )
+'
+# Block <0.59.100 due to file collision.
+#
+# Require game data package.
+# The build process does not use the game data, nor change how the game
+# is built based on what game data will be used.  At startup, the game
+# will search for both types of game data and use what it finds.  Users
+# can switch between shareware/retail data at any time by
+# adding/removing the appropriate data packages.  A rebuild is _not_
+# required after swapping the data files.
+#
+# USE-depend on freedata for various extras, but only if any of those
+# extras are enabled.
+DXX_RDEPEND_ENGINE_FRAGMENT='
+	d${ENGINE}x? (
+		!<games-action/d${ENGINE}x-rebirth-0.59.100
+		|| (
+			games-action/descent${ENGINE}-data
+			games-action/descent${ENGINE}-demodata
+		)
+		'"
+		${DXX_RDEPEND_USE_FREEDATA_FRAGMENT//\$\{USE\}/l10n_de}
+		${DXX_RDEPEND_USE_FREEDATA_FRAGMENT//\$\{USE\}/opl3-musicpack}
+		${DXX_RDEPEND_USE_FREEDATA_FRAGMENT//\$\{USE\}/sc55-musicpack}
+		"'
+	)
+'
+unset DXX_RDEPEND_USE_FREEDATA_FRAGMENT
+RDEPEND="${DEPEND}
+	music? ( media-libs/sdl-mixer[flac?,midi?,mp3?,vorbis?] )
+	${DXX_RDEPEND_ENGINE_FRAGMENT//\$\{ENGINE\}/1}
+	${DXX_RDEPEND_ENGINE_FRAGMENT//\$\{ENGINE\}/2}
+"
+unset DXX_RDEPEND_ENGINE_FRAGMENT
+
+# This ebuild builds d1x-rebirth, d2x-rebirth, or both.  Building none
+# would mean this ebuild installs zero files.
+#
+# For each of the supported music flags, require USE=music, so that the
+# package is built with sdl-mixer support.  Individual music types can be
+# changed without a Rebirth rebuild by rebuilding sdl-mixer with
+# appropriate support, but Rebirth must have sdl-mixer support at build
+# time in order to support any of these music formats at runtime.
+#
+# All music files in the optional musicpack downloads are .ogg, so
+# require USE=vorbis if those packs are enabled.
+REQUIRED_USE='
+	|| ( d1x d2x )
+	flac? ( music )
+	midi? ( music )
+	mp3? ( music )
+	vorbis? ( music )
+	opl3-musicpack? ( vorbis )
+	sc55-musicpack? ( vorbis )
+'
+
+# As of this writing, IUSE_RUNTIME is a GLEP, but not an implemented
+# feature.  This variable is stored here to be ready to activate when
+# Portage implements this feature.
+#
+# Note that while individual music formats can be switched without a
+# rebuild, the master flag USE=music controls whether sdl-mixer is used,
+# and therefore requires a rebuild when changed.
+#IUSE_RUNTIME="flac l10n_de midi mp3 opl3-musicpack sc55-musicpack vorbis"
+
+dxx_scons() {
+	# Always build profile `m`.  If use editor, also build profile `e`.
+	# Set most variables in the default anonymous profile.  Only
+	# `builddir` and `editor` are set in the named profiles, since those
+	# must be different between the two builds.
+	#
+	# Notes for end users:
+	# - Per-engine options can be set with an engine prefix, as shown
+	#   for sharepath.  Such options are used in place of
+	#   engine-unqualified options.  For example, to disable sdlmixer
+	#   for Descent 2: Rebirth, but use the setting below for Descent 1:
+	#   Rebirth, set EXTRA_ESCONS="d2x_sdlmixer=0".
+	# - Profile `site` is searched before the anonymous profile, and is
+	#   deliberately unused in this ebuild.  Set options in the site
+	#   profile to affect both engines:
+	#   EXTRA_ESCONS="site_verbosebuild=0".
+	local scons_build_profile=m mysconsargs=(
+		sdlmixer=$(usex music 1 0)
+		verbosebuild=1
+		debug=$(usex debug 1 0)
+		ipv6=$(usex ipv6 1 0)
+		opengl=$(usex opengl 1 0)
+		use_tracker=$(usex tracker 1 0)
+		prefix="${EPREFIX}"/usr
+		m_builddir=build/main/
+		m_editor=0
+	)
+	if use editor; then
+		scons_build_profile+=+e
+		mysconsargs+=( 
+			e_builddir=build/editor/
+			e_editor=1
+		)
+	fi
+	if ! use joystick; then
+		mysconsargs+=(
+			max_joysticks=0
+		)
+	fi
+	# Add sharepath and enable build of selected games.  The trailing
+	# comma after `$scons_build_profile` is required to cause scons to
+	# search the anonymous profile.  If omitted, only settings from the
+	# named profile would be used.
+	use d1x && mysconsargs+=( d1x_sharepath="/usr/share/games/d1x" d1x="$scons_build_profile,site," )
+	use d2x && mysconsargs+=( d2x_sharepath="/usr/share/games/d2x" d2x="$scons_build_profile,site," )
+	escons "${mysconsargs[@]}" "$@"
+}
+
+src_compile() {
+	tc-export CXX PKG_CONFIG
+	dxx_scons register_install_target=0 build
+}
+
+src_install() {
+	# Use upstream install target to handle the various combinations of
+	# enabled/disabled engines and optional editor support.
+	dxx_scons register_compile_target=0 register_install_target=1 DESTDIR="$D" "$D"
+	local DV
+	for DV in 1 2; do
+		if ! use d${DV}x; then
+			continue
+		fi
+		local PROGRAM=d${DV}x-rebirth
+		docinto "${PROGRAM}"
+		dodoc "${PROGRAM}"/*.txt
+		make_desktop_entry "${PROGRAM}" "Descent ${DV} Rebirth" "${PROGRAM}"
+		doicon "${PROGRAM}/${PROGRAM}.xpm"
+	done
+}

commit b16cf4e60a33064dc19a8a820bebd48614a71113

Fix Windows build
The Windows build did not explicitly include <time.h> or equivalent, but
used `time` and `gmtime`, which are only available when <time.h> is
included.  Other platforms include <time.h> incidentally.

Fixes: 131c1b9f4d556fbd9d67815755608602b408698b ("Add support for PNG screenshots")

similar/main/game.cpp
@@ -29,6 +29,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include <string.h>
 #include <stdarg.h>
 #include <SDL.h>
+#include <ctime>
 #if DXX_USE_SCREENSHOT_FORMAT_PNG
 #include <png.h>
 #include "vers_id.h"