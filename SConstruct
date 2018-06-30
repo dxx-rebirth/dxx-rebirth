dxx-rebirth/dxx-rebirth

commit 2a82207e91f038ec20c8febcc4303657fb0d8389

Add unit tests for valptridx parameter validation

SConstruct
@@ -829,16 +829,20 @@ help:assume C++ compiler works
 		# take an action determined by the value of forced.
 		if forced is None:
 			r = action('''
+#undef main	/* avoid -Dmain=SDL_main from libSDL */
 %s
 %s
 %s
 
-#undef main	/* avoid -Dmain=SDL_main from libSDL */
+%s
+''' % (self.__tool_versions, self.__defined_macros, text, '' if main is None else
+'''
 int main(int argc,char**argv){(void)argc;(void)argv;
 %s
 
 ;}
-''' % (self.__tool_versions, self.__defined_macros, text, main), ext)
+''' % main
+	), ext)
 			# Some tests check that the compiler rejects an input.
 			# SConf considers the result a failure when the compiler
 			# rejects the input.  For tests that consider a rejection to
@@ -922,23 +926,25 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 	# stage failed and providing a text error message to show to the
 	# user.  Some callers handle failure by retrying with other options.
 	# Others abort the SConf run.
-	def _soft_check_system_library(self,context,header,main,lib,text='',successflags={}):
-		include = '\n'.join(['#include <%s>' % h for h in header])
+	def _soft_check_system_library(self,context,header,main,lib,pretext='',text='',successflags={},testflags={}):
+		include = pretext + '\n'.join(['#include <%s>' % h for h in header])
 		header = header[-1]
 		# Test library.  On success, good.  On failure, test header to
 		# give the user more help.
-		if self.Link(context, text=include + text, main=main, msg='for usable library %s' % lib, successflags=successflags):
+		if self.Link(context, text=include + text, main=main, msg='for usable library %s' % lib, successflags=successflags, testflags=testflags):
 			return
 		# If linking failed, an error report is inevitable.  Probe
 		# progressively simpler configurations to help the user trace
 		# the problem.
+		successflags = successflags.copy()
+		successflags.update(testflags)
 		Compile = self.Compile
 		if Compile(context, text=include + text, main=main, msg='for usable header %s' % header, testflags=successflags):
 			# If this Compile succeeds, then the test program can be
 			# compiled, but it cannot be linked.  The required library
 			# may be missing or broken.
 			return (0, "Header %s is usable, but library %s is not usable." % (header, lib))
-		if Compile(context, text=include, main='', msg='whether compiler can parse header %s' % header, testflags=successflags):
+		if Compile(context, text=include, main=main and '', msg='whether compiler can parse header %s' % header, testflags=successflags):
 			# If this Compile succeeds, then the test program cannot be
 			# compiled, but the header can be used with an empty test
 			# program.  Either the test program is broken or it relies
@@ -947,7 +953,7 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 			return (1, "Header %s is parseable, but cannot compile the test program." % header)
 		CXXFLAGS = successflags.setdefault('CXXFLAGS', [])
 		CXXFLAGS.append('-E')
-		if Compile(context, text=include, main='', msg='whether preprocessor can parse header %s' % header, testflags=successflags):
+		if Compile(context, text=include, main=main and '', msg='whether preprocessor can parse header %s' % header, testflags=successflags):
 			# If this Compile succeeds, then the used header can be
 			# preprocessed, but cannot be compiled as C++ even with an
 			# empty test program.  The header likely has a C++ syntax
@@ -963,7 +969,7 @@ int main(int argc,char**argv){(void)argc;(void)argv;
 		#   header)
 		# - Is present, but rejected by the preprocessor (such as from
 		#   an active `#error`)
-		if Compile(context, text=include, main='', msg='whether preprocessor can locate header %s (and supporting headers)' % header, expect_failure=True, testflags=successflags):
+		if Compile(context, text=include, main=main and '', msg='whether preprocessor can locate header %s (and supporting headers)' % header, expect_failure=True, testflags=successflags):
 			# If this Compile succeeds, then the header does not exist,
 			# or exists and includes (possibly through layers of
 			# indirection) a header which does not exist.  Passing `-MG`
@@ -2664,6 +2670,27 @@ where the cast is useless.
 			record(RecordedTest(mangle(opt), 'assume linker accepts %s' % opt))
 		assert cls.custom_tests[0].name == cls.check_cxx_works.__name__, cls.custom_tests[0].name
 		assert cls.custom_tests[-1].name == cls._restore_cxx_prefix.__name__, cls.custom_tests[-1].name
+
+	@_implicit_test
+	def check_boost_test(self,context):
+		self._check_system_library(context, header=('boost/test/unit_test.hpp',), pretext='''
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Rebirth
+''', main=None, lib='Boost.Test', text='''
+BOOST_AUTO_TEST_CASE(f)
+{
+	BOOST_TEST(true);
+}
+''', testflags={'LIBS' : ['boost_unit_test_framework']})
+
+	@_custom_test
+	def _check_boost_test_required(self,context):
+		register_runtime_test_link_targets = self.user_settings.register_runtime_test_link_targets
+		context.Display('%s: checking whether to build runtime tests...' % self.msgprefix)
+		context.Result(register_runtime_test_link_targets)
+		if register_runtime_test_link_targets:
+			self.check_boost_test(context)
+
 	# This must be the last custom test.  It does not test the environment,
 	# but is responsible for reversing test-environment-specific changes made
 	# by check_cxx_works.
@@ -3287,6 +3314,14 @@ class DXXCommon(LazyObjectConstructor):
 	VERSION_MICRO = 100
 	DXX_VERSION_SEQ = ','.join([str(VERSION_MAJOR), str(VERSION_MINOR), str(VERSION_MICRO)])
 	pch_manager = None
+	runtime_test_boost_tests = None
+
+	class RuntimeTest(LazyObjectConstructor):
+		nodefaultlibs = True
+		def __init__(self,target,source):
+			self.target = target
+			self.source = LazyObjectConstructor.create_lazy_object_getter(source)
+
 	@cached_property
 	def program_message_prefix(self):
 		return '%s.%d' % (self.PROGRAM_NAME, self.program_instance)
@@ -3516,6 +3551,7 @@ class DXXCommon(LazyObjectConstructor):
 					('words_need_alignment', self.default_words_need_alignment, 'align words at load (needed for many non-x86 systems)'),
 					('register_compile_target', True, 'report compile targets to SCons core'),
 					('register_cpp_output_targets', None, None),
+					('register_runtime_test_link_targets', False, None),
 					('enable_build_failure_summary', True, 'print failed nodes and their commands'),
 					('wrap_PHYSFS_read', False, None),
 					('wrap_PHYSFS_write', False, None),
@@ -3896,6 +3932,8 @@ class DXXCommon(LazyObjectConstructor):
 			# create_header_targets() does not call the PCHManager
 			# StaticObject hook.
 			self.create_header_targets()
+		if user_settings.register_runtime_test_link_targets:
+			self._register_runtime_test_link_targets()
 		configure_pch_flags = archive.configure_pch_flags
 		if configure_pch_flags:
 			self.pch_manager = PCHManager(self.user_settings, self.env, self.srcdir, configure_pch_flags, archive.pch_manager)
@@ -4097,11 +4135,30 @@ class DXXCommon(LazyObjectConstructor):
 				LIBS = ['bcm_host'],
 			)
 
+	def _register_runtime_test_link_targets(self):
+		runtime_test_boost_tests = self.runtime_test_boost_tests
+		if not runtime_test_boost_tests:
+			return
+		env = self.env
+		user_settings = self.user_settings
+		builddir = env.Dir(user_settings.builddir).Dir(self.srcdir)
+		for test in runtime_test_boost_tests:
+			LIBS = [] if test.nodefaultlibs else env['LIBS'][:]
+			LIBS.append('boost_unit_test_framework')
+			env.Program(target=builddir.File(test.target), source=test.source(self), LIBS=LIBS)
+
 class DXXArchive(DXXCommon):
 	PROGRAM_NAME = 'DXX-Archive'
 	_argument_prefix_list = None
 	srcdir = 'common'
 	target = 'dxx-common'
+	RuntimeTest = DXXCommon.RuntimeTest
+	runtime_test_boost_tests = (
+		RuntimeTest('test-valptridx-range', (
+			'common/unittest/valptridx-range.cpp',
+			))
+			,)
+	del RuntimeTest
 
 	def get_objects_common(self,
 		__get_objects_common=DXXCommon.create_lazy_object_getter((

common/unittest/valptridx-range.cpp
@@ -1,0 +1,224 @@
+#include "dxxsconf.h"
+#include "compiler-poison.h"
+
+/* If DXX_HAVE_POISON_UNDEFINED is not zero,
+ * array_managed_type::array_managed_type() is declared and must be
+ * defined out of line.  These tests do not need to run any code at
+ * array construction time, so force DXX_HAVE_POISON_UNDEFINED to be zero.
+ */
+#undef DXX_HAVE_POISON_UNDEFINED
+#define DXX_HAVE_POISON_UNDEFINED	0
+#include "valptridx.h"
+#include "valptridx.tcc"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Rebirth valptridx
+#include <boost/test/unit_test.hpp>
+
+/* Convenience macro to prevent the compiler optimizer from performing
+ * constant propagation of the input.  This ensures that build-time
+ * tests are suppressed, allowing the runtime test to be emitted and
+ * executed.
+ */
+#define OPTIMIZER_HIDE_VARIABLE(V)	asm("" : "=rm" (V) : "0" (V) : "memory")
+
+/* Convenience function to pass an anonymous value (such as an integer
+ * constant) through OPTIMIZER_HIDE_VARIABLE, then return it.  This is
+ * needed for contexts where a bare asm() statement would not be legal.
+ *
+ * It also has the useful secondary effect of casting the input to a
+ * type, if the caller explicitly sets the typename T.
+ */
+template <typename T>
+static inline T optimizer_hidden_variable(T v)
+{
+	OPTIMIZER_HIDE_VARIABLE(v);
+	return v;
+}
+
+/* Convenience macro to ignore the result of an expression in a way that
+ * __attribute__((warn_unused_result)) does not warn that the result was
+ * ignored.
+ */
+#define DXX_TEST_VALPTRIDX_IGNORE_RETURN(EXPR)	({	auto &&r = EXPR; static_cast<void>(r); })
+
+struct object
+{
+	/* No state needed - this module is testing valptridx, not the type
+	 * managed by valptridx.
+	 */
+};
+
+using objnum_t = uint16_t;
+constexpr std::integral_constant<std::size_t, 350> MAX_OBJECTS{};
+DXX_VALPTRIDX_DECLARE_SUBTYPE(, object, objnum_t, MAX_OBJECTS);
+DXX_VALPTRIDX_DEFINE_SUBTYPE_TYPEDEFS(object, obj);
+DXX_VALPTRIDX_DEFINE_GLOBAL_FACTORIES(object, obj, Objects);
+
+valptridx<object>::array_managed_type Objects;
+
+/* Some valptridx names are normally protected.  The tests need access
+ * to them.  Define a subclass that relaxes the permissions.
+ */
+struct valptridx_access_override : valptridx<object>
+{
+	using base_type = valptridx<object>;
+	using typename base_type::allow_end_construction;
+	using typename base_type::index_range_exception;
+	using typename base_type::null_pointer_exception;
+	using typename base_type::report_error_uses_exception;
+};
+
+/* Test that using the factory does not throw an exception on the
+ * highest valid index.
+ */
+BOOST_AUTO_TEST_CASE(idx_no_exception_near_end)
+{
+	BOOST_CHECK_NO_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(
+			Objects.vmptr(optimizer_hidden_variable<objnum_t>(MAX_OBJECTS - 1)))
+		);
+}
+
+/* Test that using the factory does throw an exception on the lowest
+ * invalid index and on the highest representable integer.
+ */
+BOOST_AUTO_TEST_CASE(idx_exception_at_end)
+{
+	if (!valptridx_access_override::report_error_uses_exception::value)
+		/* If valptridx is configured not to use exceptions to report
+		 * errors, these tests will fail even when the logic is correct.
+		 * Skip them in that case.
+		 */
+		return;
+	BOOST_CHECK_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(
+			Objects.vmptr(
+				optimizer_hidden_variable<objnum_t>(MAX_OBJECTS))),
+		valptridx_access_override::index_range_exception);
+
+	BOOST_CHECK_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(
+			Objects.vmptr(
+				optimizer_hidden_variable<objnum_t>(UINT16_MAX))),
+		valptridx_access_override::index_range_exception);
+}
+
+BOOST_AUTO_TEST_CASE(iter_no_exception_at_allowed_end)
+{
+	/* Test the allowed side of end() handling for off-by-one bugs.
+	 */
+	BOOST_CHECK_NO_THROW(DXX_TEST_VALPTRIDX_IGNORE_RETURN(
+			(Objects.set_count(optimizer_hidden_variable<objnum_t>(MAX_OBJECTS - 1)), Objects.vmptr.end())
+	));
+
+	BOOST_CHECK_NO_THROW(DXX_TEST_VALPTRIDX_IGNORE_RETURN(
+			(Objects.set_count(optimizer_hidden_variable<objnum_t>(MAX_OBJECTS)), Objects.vmptr.end())
+	));
+}
+
+BOOST_AUTO_TEST_CASE(iter_exception_if_beyond_end)
+{
+	/* Test that end() throws an exception if the range's count exceeds
+	 * the valid storage size.
+	 */
+	if (!valptridx_access_override::report_error_uses_exception::value)
+		return;
+	BOOST_CHECK_THROW((DXX_TEST_VALPTRIDX_IGNORE_RETURN(
+				(Objects.set_count(optimizer_hidden_variable<objnum_t>(MAX_OBJECTS + 1)), Objects.vmptr.end())
+	)), valptridx_access_override::index_range_exception);
+}
+
+BOOST_AUTO_TEST_CASE(guarded_in_range)
+{
+	auto &&g = Objects.vmptr.check_untrusted(optimizer_hidden_variable<objnum_t>(0));
+	optimizer_hidden_variable(g);
+	/* Test that a `guarded` returned by `check_untrusted` on a valid
+	 * index throws if accessed without testing its validity.  A runtime
+	 * check on validity is mandatory.
+	 *
+	 * These exceptions are not configurable, so no test on
+	 * `report_error_uses_exception` is required.
+	 */
+	BOOST_CHECK_THROW(DXX_TEST_VALPTRIDX_IGNORE_RETURN(*g), std::logic_error);
+	/* Test its validity */
+	BOOST_TEST(static_cast<bool>(g));
+	/* Test that it can now be read successfully. */
+	BOOST_CHECK_NO_THROW(DXX_TEST_VALPTRIDX_IGNORE_RETURN(*g));
+}
+
+BOOST_AUTO_TEST_CASE(guarded_out_of_range)
+{
+	auto &&g = Objects.vmptr.check_untrusted(optimizer_hidden_variable<objnum_t>(MAX_OBJECTS));
+	optimizer_hidden_variable(g);
+	/* Test that a `guarded` returned by `check_untrusted` on an invalid
+	 * index throws if accessed without testing its validity.
+	 *
+	 * Test its validity.
+	 *
+	 * Test that it still throws afterward.
+	 */
+	BOOST_CHECK_THROW(DXX_TEST_VALPTRIDX_IGNORE_RETURN(*g), std::logic_error);
+	BOOST_TEST(!static_cast<bool>(g));
+	BOOST_CHECK_THROW(DXX_TEST_VALPTRIDX_IGNORE_RETURN(*g), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(idx_convert_check)
+{
+	using vo = valptridx<object>;
+	BOOST_CHECK_NO_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(({
+			typename vo::imidx i(static_cast<objnum_t>(0));
+			typename vo::vmidx{i};
+			}))
+	);
+	typename vo::imidx i_none(static_cast<objnum_t>(~0));
+	if (!valptridx_access_override::report_error_uses_exception::value)
+		return;
+	BOOST_CHECK_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(({
+			typename vo::vmidx{i_none};
+			})), valptridx_access_override::index_range_exception
+	);
+	BOOST_CHECK_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(({
+			typename vo::vmidx{optimizer_hidden_variable<objnum_t>(~0)};
+			})), valptridx_access_override::index_range_exception
+	);
+}
+
+BOOST_AUTO_TEST_CASE(ptr_convert_check)
+{
+	using vo = valptridx<object>;
+	typename vo::imptr i_none(nullptr);
+	auto &&i_zero = Objects.imptr(optimizer_hidden_variable<objnum_t>(0));
+	BOOST_CHECK_NO_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(({
+			typename vo::vmptr v_zero{i_zero};
+			v_zero.get_nonnull_pointer();
+			}))
+	);
+	if (!valptridx_access_override::report_error_uses_exception::value)
+		return;
+	BOOST_CHECK_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(({
+			typename vo::vmptr{i_none};
+			})), valptridx_access_override::null_pointer_exception
+	);
+	BOOST_CHECK_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(({
+			Objects.vmptr(optimizer_hidden_variable<object *>(nullptr));
+			})), valptridx_access_override::null_pointer_exception
+	);
+	BOOST_CHECK_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(({
+			i_none.get_nonnull_pointer();
+			})), valptridx_access_override::null_pointer_exception
+	);
+	auto &&pi_zero = Objects.vmptridx(optimizer_hidden_variable<objnum_t>(0));
+	BOOST_CHECK_THROW(
+		DXX_TEST_VALPTRIDX_IGNORE_RETURN(({
+			pi_zero.absolute_sibling(optimizer_hidden_variable<objnum_t>(Objects.size()));
+			})), valptridx_access_override::index_range_exception
+	);
+}

commit 7d4c521382e25c9f3386235f3f3484ed2327c7e1

Fix SConf tests for Windows target
On Windows, SDL headers define `main` to `SDL_main` in addition to doing
it through `pkg-config`.  The countering `#undef` must occur after the
headers are included.  Commit 2a82207 changed the `#undef` to occur too
early.  Linux works fine, but Windows broke.

Move the `#undef` back down.  Switch to named format parameters to make
it easier to maintain.

Fixes: 2a82207e91f038ec20c8febcc4303657fb0d8389 ("Add unit tests for valptridx parameter validation")

SConstruct
@@ -829,20 +829,21 @@ help:assume C++ compiler works
 		# take an action determined by the value of forced.
 		if forced is None:
 			r = action('''
-#undef main	/* avoid -Dmain=SDL_main from libSDL */
-%s
-%s
-%s
+{tools}
+{macros}
+{text}
 
-%s
-''' % (self.__tool_versions, self.__defined_macros, text, '' if main is None else
+#undef main	/* avoid -Dmain=SDL_main from libSDL */
+
+{main}
+'''.format(tools=self.__tool_versions, macros=self.__defined_macros, text=text, main=('' if main is None else
 '''
 int main(int argc,char**argv){(void)argc;(void)argv;
 %s
 
 ;}
 ''' % main
-	), ext)
+	)), ext)
 			# Some tests check that the compiler rejects an input.
 			# SConf considers the result a failure when the compiler
 			# rejects the input.  For tests that consider a rejection to