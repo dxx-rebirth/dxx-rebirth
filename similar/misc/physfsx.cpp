9cfa12c0fe18e29ecb0ff150d452f4f29060f5b4
C.W. Betts <computers57@hotmail.com>
2018-08-23 21:28:04 -0600

    Use sizeof(fullPath), as suggested by @vLKp.

--- similar/misc/physfsx.cpp
+++ similar/misc/physfsx.cpp
@@ -230,8 +230,8 @@ bool PHYSFSX_init(int argc, char *argv[])
 		if (mainBundle) {
 			CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
 			if (resourcesURL) {
-				if (CFURLGetFileSystemRepresentation(resourcesURL, TRUE, reinterpret_cast<uint8_t *>(fullPath), PATH_MAX + 5)) {
-					fullPath[PATH_MAX + 4] = '\0';
+				if (CFURLGetFileSystemRepresentation(resourcesURL, TRUE, reinterpret_cast<uint8_t *>(fullPath), sizeof(fullPath))) {
+					fullPath[sizeof(fullPath) - 1] = '\0';
 					con_printf(CON_DEBUG, "PHYSFS: append resources directory \"%s\" to search path", fullPath);
 					PHYSFS_addToSearchPath(fullPath, 1);
 				}
