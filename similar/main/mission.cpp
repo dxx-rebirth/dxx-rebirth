dxx-rebirth/dxx-rebirth

commit d97afc2ad50027cf3acf5acde3f573c4b5589464

Retain directory structure in New Game dialog
User jcotton42 suggested copying a D2X-XL feature: preserving the
directory structure of the user's missions area when showing a New Game
dialog.  This was substantially more trouble than it should have been,
but the result is good.

Previously, the dialog presented all missions at any depth below the
starting point, and sorted them as if they were all in the root
directory.

Now:
- Empty directories are hidden entirely.  There is nothing for the user
  to do in them, so there is no point showing them.
- A directory with exactly one entry has that entry promoted into the
  parent, since there is no ambiguity about what the user would want.
  If the parent in turn has only that one promoted element when the scan
  of the parent finishes, then the element can be promoted up again.
  This continues until the root is reached or until a level has more
  than one entry.  For this purpose, both missions and directories count
  as entries.
- Directory entries are decorated to inform the user how many
  immediate subdirectories are present, how many missions are present
  immediately in the directory, and how many missions total are present,
  counting all subdirectories.  If there are zero immediate
  subdirectories, then the directory count is not shown.  For this
  purpose, directories that were hidden due to a lack of missions are
  not counted.
- Sub-dialog boxes for inner directories use a title that reminds the
  user of the path so far, and recaps the directory/mission statistics.
- On entry to the New Game dialog, if the last played mission is in a
  sub-dialog, appropriate sub-dialogs are opened so that the last played
  mission can be pre-selected.

Currently, there is no in-game override to return to the prior rollup
rules.

Requested-by: jcotton42 <https://github.com/dxx-rebirth/dxx-rebirth/issues/392>

similar/main/mission.cpp
@@ -73,7 +73,9 @@ using std::min;
 
 namespace {
 
+struct mle;
 using mission_candidate_search_path = array<char, PATH_MAX>;
+using mission_list_type = std::vector<mle>;
 
 //mission list entry
 struct mle : Mission_path
@@ -84,9 +86,65 @@ struct mle : Mission_path
 	descent_version_type descent_version;    // descent 1 or descent 2?
 #endif
 	ubyte   anarchy_only_flag;  // if true, mission is anarchy only
+	mission_list_type directory;
+	mle(Mission_path &&m) :
+		Mission_path(std::move(m))
+	{
+	}
+	mle(const char *const name, std::vector<mle> &&d);
 };
 
-using mission_list_type = std::vector<mle>;
+struct mission_subdir_stats
+{
+	std::size_t immediate_directories = 0, immediate_missions = 0, total_missions = 0;
+	static std::size_t count_missions(const mission_list_type &directory)
+	{
+		std::size_t total_missions = 0;
+		range_for (auto &&i, directory)
+		{
+			if (i.directory.empty())
+				++ total_missions;
+			else
+				total_missions += count_missions(i.directory);
+		}
+		return total_missions;
+	}
+	void count(const mission_list_type &directory)
+	{
+		range_for (auto &&i, directory)
+		{
+			if (i.directory.empty())
+			{
+				++ total_missions;
+				++ immediate_missions;
+			}
+			else
+			{
+				++ immediate_directories;
+				total_missions += count_missions(i.directory);
+			}
+		}
+	}
+};
+
+const char *prepare_mission_list_count_dirbuf(array<char, 12> &dirbuf, const std::size_t immediate_directories)
+{
+	if (immediate_directories)
+	{
+		snprintf(dirbuf.data(), dirbuf.size(), "DIR:%" DXX_PRI_size_type "; ", immediate_directories);
+		return dirbuf.data();
+	}
+	return "";
+}
+
+mle::mle(const char *const name, std::vector<mle> &&d) :
+	Mission_path(name, 0), directory(std::move(d))
+{
+	mission_subdir_stats ss;
+	ss.count(directory);
+	array<char, 12> dirbuf;
+	snprintf(mission_name.data(), mission_name.size(), "%s/ [%sMSN:L%" DXX_PRI_size_type ";T%" DXX_PRI_size_type "]", name, prepare_mission_list_count_dirbuf(dirbuf, ss.immediate_directories), ss.immediate_missions, ss.total_missions);
+}
 
 }
 
@@ -346,27 +404,25 @@ static int read_mission_file(mission_list_type &mission_list, mission_candidate_
 {
 	if (const auto mfile = PHYSFSX_openReadBuffered(pathname.data()))
 	{
-		char *p;
-		char *ext;
-		p = strrchr(pathname.data(), '/');
-		if (!p)
-			p = pathname.data();
-		if ((ext = strchr(p, '.')) == NULL)
+		std::string str_pathname = pathname.data();
+		const auto idx_last_slash = str_pathname.find_last_of('/');
+		const auto idx_filename = (idx_last_slash == str_pathname.npos) ? 0 : idx_last_slash + 1;
+		const auto idx_file_extension = str_pathname.find_first_of('.', idx_filename);
+		if (idx_file_extension == str_pathname.npos)
 			return 0;	//missing extension
-		mission_list.emplace_back();
+		str_pathname.resize(idx_file_extension);
+		mission_list.emplace_back(Mission_path(std::move(str_pathname), idx_filename));
 		mle *mission = &mission_list.back();
-		mission->path.assign(pathname.data(), ext);
 #if defined(DXX_BUILD_DESCENT_II)
 		// look if it's .mn2 or .msn
-		mission->descent_version = (ext[3] == MISSION_EXTENSION_DESCENT_II[3])
+		mission->descent_version = (pathname[idx_file_extension + 3] == MISSION_EXTENSION_DESCENT_II[3])
 			? Mission::descent_version_type::descent2
 			: Mission::descent_version_type::descent1;
 #endif
 		mission->anarchy_only_flag = 0;
-		mission->filename = next(begin(mission->path), mission->path.find_last_of('/') + 1);
 
 		PHYSFSX_gets_line_t<80> buf;
-		p = get_parm_value(buf, "name",mfile);
+		auto p = get_parm_value(buf, "name",mfile);
 
 #if defined(DXX_BUILD_DESCENT_II)
 		if (!p) {		//try enhanced mission
@@ -428,19 +484,17 @@ static void add_d1_builtin_mission_to_list(mission_list_type &mission_list)
 	if (size == -1)
 		return;
 
-	mission_list.emplace_back();
+	mission_list.emplace_back(Mission_path(D1_MISSION_FILENAME, 0));
 	mle *mission = &mission_list.back();
 	switch (size) {
 	case D1_SHAREWARE_MISSION_HOGSIZE:
 	case D1_SHAREWARE_10_MISSION_HOGSIZE:
 	case D1_MAC_SHARE_MISSION_HOGSIZE:
-		mission->path = D1_MISSION_FILENAME;
 		mission->mission_name.copy_if(D1_SHAREWARE_MISSION_NAME);
 		mission->anarchy_only_flag = 0;
 		break;
 	case D1_OEM_MISSION_HOGSIZE:
 	case D1_OEM_10_MISSION_HOGSIZE:
-		mission->path = D1_MISSION_FILENAME;
 		mission->mission_name.copy_if(D1_OEM_MISSION_NAME);
 		mission->anarchy_only_flag = 0;
 		break;
@@ -452,7 +506,6 @@ static void add_d1_builtin_mission_to_list(mission_list_type &mission_list)
 	case D1_MISSION_HOGSIZE2:
 	case D1_10_MISSION_HOGSIZE:
 	case D1_MAC_MISSION_HOGSIZE:
-		mission->path = D1_MISSION_FILENAME;
 		mission->mission_name.copy_if(D1_MISSION_NAME);
 		mission->anarchy_only_flag = 0;
 		break;
@@ -465,7 +518,6 @@ static void add_d1_builtin_mission_to_list(mission_list_type &mission_list)
 	mission->descent_version = Mission::descent_version_type::descent1;
 	mission->builtin_hogsize = 0;
 #endif
-	mission->filename = begin(mission->path);
 }
 }
 
@@ -473,10 +525,8 @@ static void add_d1_builtin_mission_to_list(mission_list_type &mission_list)
 template <std::size_t N1, std::size_t N2>
 static void set_hardcoded_mission(mission_list_type &mission_list, const char (&path)[N1], const char (&mission_name)[N2])
 {
-	mission_list.emplace_back();
+	mission_list.emplace_back(Mission_path(path, 0));
 	mle *mission = &mission_list.back();
-	mission->path = path;
-	mission->filename = begin(mission->path);
 	mission->mission_name.copy_if(mission_name);
 	mission->anarchy_only_flag = 0;
 }
@@ -518,7 +568,8 @@ static void add_builtin_mission_to_list(mission_list_type &mission_list, d_fname
 #endif
 
 namespace dsx {
-static void add_missions_to_list(mission_list_type &mission_list, mission_candidate_search_path &path, const mission_candidate_search_path::iterator rel_path, int anarchy_mode)
+
+static void add_missions_to_list(mission_list_type &mission_list, mission_candidate_search_path &path, const mission_candidate_search_path::iterator rel_path, const int anarchy_mode)
 {
 	/* rel_path must point within the array `path`.
 	 * rel_path must point to the null that follows a possibly empty
@@ -549,8 +600,25 @@ static void add_missions_to_list(mission_list_type &mission_list, mission_candid
 			auto null = std::prev(j);
 			*j = 0;
 			*null = '/';
-			add_missions_to_list(mission_list, path, j, anarchy_mode);
+			mission_list_type sublist;
+			add_missions_to_list(sublist, path, j, anarchy_mode);
 			*null = 0;
+			const auto found = sublist.size();
+			if (!found)
+			{
+				/* Ignore empty directories */
+			}
+			else if (found == 1)
+			{
+				/* If only one found, promote it up to the next level so
+				 * the user does not need to navigate into a
+				 * single-element directory.
+				 */
+				auto &sli = sublist.front();
+				mission_list.emplace_back(std::move(sli));
+			}
+			else
+				mission_list.emplace_back(path.data(), std::move(sublist));
 		}
 		else if (il > 5 &&
 			((ext = &i[il - 5], !d_strnicmp(ext, MISSION_EXTENSION_DESCENT_I))
@@ -583,7 +651,9 @@ static void promote (mission_list_type &mission_list, const char *const name, st
 	range_for (auto &i, partial_range(mission_list, top_place, mission_list.size()))
 		if (!d_stricmp(&*i.filename, name)) {
 			//swap mission positions
-			std::swap(mission_list[top_place++], i);
+			auto &j = mission_list[top_place++];
+			if (&j != &i)
+				std::swap(j, i);
 			break;
 		}
 }
@@ -737,14 +807,13 @@ static const char *load_mission(const mle *const mission)
 #if defined(DXX_BUILD_DESCENT_II)
 	close_extra_robot_movie();
 #endif
-	Current_mission = make_unique<Mission>();
+	Current_mission = make_unique<Mission>(static_cast<const Mission_path &>(*mission));
 	Current_mission->builtin_hogsize = mission->builtin_hogsize;
 	Current_mission->mission_name.copy_if(mission->mission_name);
 #if defined(DXX_BUILD_DESCENT_II)
 	Current_mission->descent_version = mission->descent_version;
 #endif
 	Current_mission->anarchy_only_flag = mission->anarchy_only_flag;
-	*static_cast<Mission_path *>(Current_mission.get()) = *mission;
 	Current_mission->n_secret_levels = 0;
 #if defined(DXX_BUILD_DESCENT_II)
 	Current_mission->alternate_ham_file = NULL;
@@ -826,7 +895,7 @@ static const char *load_mission(const mle *const mission)
 	if (!PLAYING_BUILTIN_MISSION)
 #endif
 	{
-		strcpy(mission_filename.data() + strlen(mission_filename.data()) - 3, "hog");		//change extension
+		strcpy(&mission_filename[mission->path.size() + 1], "hog");		//change extension
 			PHYSFSX_contfile_init(mission_filename.data(), 0);
 		set_briefing_filename(Briefing_text_filename, Current_mission_filename);
 		Ending_text_filename = Briefing_text_filename;
@@ -1011,38 +1080,123 @@ const char *load_mission_by_name(const char *const mission_name)
 	return found;
 }
 
-struct mission_menu
+namespace {
+
+class mission_menu
 {
+	mission_list_type mls;
+public:
+	static constexpr char listbox_go_up[] = "<..>";
 	using callback_type = window_event_result (*)(void);
-	mission_list_type ml;
-	std::unique_ptr<const char *[]> mission_names;
-	callback_type when_selected;
-	mission_menu(mission_list_type &&rml, std::unique_ptr<const char *[]> &&mn, const callback_type ws) :
-		ml(std::move(rml)), mission_names(std::move(mn)), when_selected(ws)
+	const mission_list_type &ml;
+	const std::unique_ptr<const char *[]> listbox_strings;
+	const RAIIdmem<char[]> title;
+	const callback_type when_selected;
+	listbox *containing_listbox = nullptr;
+	mission_menu *parent = nullptr;
+	mission_menu(mission_list_type &&rml, std::unique_ptr<const char *[]> &&mn, const char *const message, const callback_type ws) :
+		mls(std::move(rml)), ml(mls), listbox_strings(std::move(mn)),
+		title(prepare_title(message, ml)), when_selected(ws)
 	{
+	}
+	mission_menu(const mission_list_type *const p, std::unique_ptr<const char *[]> &&mn, const char *const message, const callback_type ws, mission_menu *const parent_menu) :
+		ml(*p), listbox_strings(std::move(mn)),
+		title(prepare_title(message, ml)), when_selected(ws),
+		parent(parent_menu)
+	{
+	}
+	bool is_submenu() const
+	{
+		return parent != nullptr;
+	}
+	static RAIIdmem<char[]> prepare_title(const char *const message, const mission_list_type &ml)
+	{
+		mission_subdir_stats ss;
+		ss.count(ml);
+		array<char, 12> dirbuf;
+		char buf[128];
+		snprintf(buf, sizeof(buf), "%s\n[%sMSN:LOCAL %" DXX_PRI_size_type "; TOTAL %" DXX_PRI_size_type "]", message, prepare_mission_list_count_dirbuf(dirbuf, ss.immediate_directories), ss.immediate_missions, ss.total_missions);
+		return RAIIdmem<char[]>(d_strdup(buf));
 	}
 };
 
-static window_event_result mission_menu_handler(listbox *, const d_event &event, mission_menu *const mm)
+constexpr char mission_menu::listbox_go_up[];
+
+struct mission_menu_create_state
+{
+	std::unique_ptr<const char *[]> listbox_strings;
+	unsigned initial_selection = UINT_MAX;
+	std::unique_ptr<mission_menu_create_state> submenu;
+	mission_menu_create_state(const std::size_t len) :
+		listbox_strings(make_unique<const char *[]>(len))
+	{
+	}
+	mission_menu_create_state(mission_menu_create_state &&) = default;
+};
+
+}
+
+static window_event_result mission_menu_handler(listbox *const lb, const d_event &event, mission_menu *const mm)
 {
 	switch (event.type)
 	{
+		case EVENT_WINDOW_CREATED:
+			mm->containing_listbox = lb;
+			break;
 		case EVENT_NEWMENU_SELECTED:
 		{
-			auto &citem = static_cast<const d_select_event &>(event).citem;
+			const auto raw_citem = static_cast<const d_select_event &>(event).citem;
+			auto citem = raw_citem;
+			if (mm->is_submenu())
+			{
+				if (citem == 0)
+				{
+					/* Clear parent pointer so that the parent window is
+					 * not implicitly closed during handling of
+					 * EVENT_WINDOW_CLOSE.
+					 */
+					mm->parent = nullptr;
+					return window_event_result::close;
+				}
+				/* Adjust for the "Go up" placeholder item */
+				-- citem;
+			}
 			if (citem >= 0)
 			{
-				// Chose a mission
-				if (const auto errstr = load_mission(&mm->ml[citem]))
+				auto &mli = mm->ml[citem];
+				if (!mli.directory.empty())
 				{
-					nm_messagebox(nullptr, 1, TXT_OK, "%s\n\n%s", TXT_MISSION_ERROR, errstr);
+					auto listbox_strings = make_unique<const char *[]>(mli.directory.size() + 1);
+					listbox_strings[0] = mm->listbox_go_up;
+					const auto a = [](const mle &m) -> const char * {
+						return m.mission_name;
+					};
+					std::transform(mli.directory.begin(), mli.directory.end(), &listbox_strings[1], a);
+					const auto pls = listbox_strings.get();
+					auto submm = make_unique<mission_menu>(&mli.directory, std::move(listbox_strings), mli.path.c_str(), mm->when_selected, mm);
+					const auto pmm = submm.get();
+					newmenu_listbox1(pmm->title.get(), pmm->ml.size() + 1, pls, 1, 0, mission_menu_handler, std::move(submm));
+					return window_event_result::handled;
+				}
+				// Chose a mission
+				else if (const auto errstr = load_mission(&mli))
+				{
+					nm_messagebox(nullptr, 1, TXT_OK, "%s\n\n%s\n\n%s", TXT_MISSION_ERROR, errstr, mli.path.c_str());
 					return window_event_result::handled;	// stay in listbox so user can select another one
 				}
-				CGameCfg.LastMission.copy_if(mm->mission_names[citem]);
+				CGameCfg.LastMission.copy_if(mm->listbox_strings[raw_citem]);
 			}
 			return (*mm->when_selected)();
 		}
 		case EVENT_WINDOW_CLOSE:
+			/* If the user dismisses the listbox by pressing ESCAPE,
+			 * do not close the parent listbox.
+			 */
+			if (listbox_get_citem(lb) != -1)
+				if (const auto parent = mm->parent)
+				{
+					window_close(listbox_get_window(parent->containing_listbox));
+				}
 			std::default_delete<mission_menu>()(mm);
 			break;
 		default:
@@ -1050,6 +1204,39 @@ static window_event_result mission_menu_handler(listbox *, const d_event &event,
 	}
 	
 	return window_event_result::ignored;
+}
+
+using mission_menu_create_state_ptr = std::unique_ptr<mission_menu_create_state>;
+
+static mission_menu_create_state_ptr prepare_mission_menu_state(const mission_list_type &mission_list, const char *const LastMission, const std::size_t extra_strings)
+{
+	auto mission_name_to_select = LastMission;
+	auto p = make_unique<mission_menu_create_state>(mission_list.size() + extra_strings);
+	auto &create_state = *p.get();
+	auto listbox_strings = create_state.listbox_strings.get();
+	std::fill_n(listbox_strings, extra_strings, nullptr);
+	listbox_strings += extra_strings;
+	range_for (auto &&e, enumerate(mission_list))
+	{
+		auto &mli = e.value;
+		const char *const mission_name = mli.mission_name;
+		*listbox_strings++ = mission_name;
+		if (!mission_name_to_select)
+			continue;
+		if (!mli.directory.empty())
+		{
+			auto &&substate = prepare_mission_menu_state(mli.directory, mission_name_to_select, 1);
+			if (substate->initial_selection == UINT_MAX)
+				continue;
+			substate->listbox_strings[0] = mission_menu::listbox_go_up;
+			create_state.submenu = std::move(substate);
+		}
+		else if (strcmp(mission_name, mission_name_to_select))
+			continue;
+		create_state.initial_selection = e.idx;
+		mission_name_to_select = nullptr;
+	}
+	return std::move(p);
 }
 
 int select_mission(int anarchy_mode, const char *message, window_event_result (*when_selected)(void))
@@ -1066,26 +1253,27 @@ int select_mission(int anarchy_mode, const char *message, window_event_result (*
     }
 	else
 	{
-		auto m = make_unique<const char *[]>(mission_list.size());
-		unsigned default_mission = 0;
-		const char *LastMission = CGameCfg.LastMission;
-		range_for (auto &&e, enumerate(mission_list))
+		auto &&create_state_ptr = prepare_mission_menu_state(mission_list, CGameCfg.LastMission, 0);
+		auto &create_state = *create_state_ptr.get();
+		mission_menu *parent_mission_menu;
 		{
-			const uint_fast32_t i = e.idx;
-			auto &mli = e.value;
-			const char *const mission_name = mli.mission_name;
-			m[i] = mission_name;
-			if (LastMission && !strcmp(mission_name, LastMission))
-			{
-				LastMission = nullptr;
-                default_mission = i;
-			}
-        }
-
-		const auto pm = m.get();
-		auto mm = make_unique<mission_menu>(std::move(mission_list), std::move(m), when_selected);
-		auto pmm = mm.get();
-		newmenu_listbox1( message, pmm->ml.size(), pm, 1, default_mission, mission_menu_handler, std::move(mm));
+			auto mm = make_unique<mission_menu>(std::move(mission_list), std::move(create_state.listbox_strings), message, when_selected);
+			parent_mission_menu = mm.get();
+			newmenu_listbox1(message, parent_mission_menu->ml.size(), parent_mission_menu->listbox_strings.get(), 1, create_state.initial_selection == UINT_MAX ? 0 : create_state.initial_selection, mission_menu_handler, std::move(mm));
+		}
+		for (auto parent_state = &create_state; const auto substate = parent_state->submenu.get(); parent_state = substate)
+		{
+			const auto parent_initial_selection = parent_state->initial_selection;
+			const auto parent_mission_list_size = parent_mission_menu->ml.size();
+			assert(parent_initial_selection < parent_mission_list_size);
+			if (parent_initial_selection >= parent_mission_list_size)
+				break;
+			const auto &substate_mission_list = parent_mission_menu->ml[parent_initial_selection];
+			auto mm = make_unique<mission_menu>(&substate_mission_list.directory, std::move(substate->listbox_strings), substate_mission_list.path.c_str(), when_selected, parent_mission_menu);
+			const auto pmm = mm.get();
+			parent_mission_menu = pmm;
+			newmenu_listbox1(pmm->title.get(), pmm->ml.size() + 1, pmm->listbox_strings.get(), 1, substate->initial_selection + 1, mission_menu_handler, std::move(mm));
+		}
     }
 
     return 1;	// presume success
@@ -1165,11 +1353,8 @@ static int write_mission(void)
 
 void create_new_mission(void)
 {
-	Current_mission = make_unique<Mission>();
-	*Current_mission = {};
+	Current_mission = make_unique<Mission>(Mission_path(MISSION_DIR "new_miss", sizeof(MISSION_DIR) - 1));		// limited to eight characters because of savegame format
 	Current_mission->mission_name.copy_if("Untitled");
-	Current_mission->path = MISSION_DIR "new_miss";		// limited to eight characters because of savegame format
-	Current_mission->filename = next(begin(Current_mission->path), sizeof(MISSION_DIR) - 1);
 	Current_mission->builtin_hogsize = 0;
 	Current_mission->anarchy_only_flag = 0;
 	

common/main/mission.h
@@ -84,7 +84,6 @@ constexpr std::integral_constant<uint8_t, 127> MAX_SECRET_LEVELS_PER_MISSION{};	
 class Mission_path
 {
 public:
-	Mission_path() = default;
 	Mission_path(const Mission_path &m) :
 		path(m.path),
 		filename(std::next(path.cbegin(), std::distance(m.path.cbegin(), m.filename)))
@@ -96,13 +95,13 @@ public:
 		filename = std::next(path.begin(), std::distance(m.path.cbegin(), m.filename));
 		return *this;
 	}
-	/* Caller's offset is ignored; it is only here to provide a
-	 * temporary to store the old path distance before old path is moved
-	 * to new path.
-	 */
-	Mission_path(Mission_path &&m, std::size_t offset = 0) :
-		path((offset = std::distance(m.path.cbegin(), m.filename), std::move(m.path))),
+	Mission_path(std::string &&p, const std::size_t offset) :
+		path(std::move(p)),
 		filename(std::next(path.cbegin(), offset))
+	{
+	}
+	Mission_path(Mission_path &&m) :
+		Mission_path(std::move(m).path, std::distance(m.path.cbegin(), m.filename))
 	{
 	}
 	Mission_path &operator=(Mission_path &&rhs)
@@ -156,17 +155,21 @@ struct Mission : Mission_path
 	 * With the explicit default, gcc uses operator=(Mission &&), which
 	 * works.
 	 *
-	 * Explicitly default the default constructor since the explicitly
-	 * defaulted move constructor suppresses the implicit default
-	 * constructor.
 	 * Explicitly delete copy constructor and copy operator= for
 	 * thoroughness.
 	 */
 	Mission(Mission &&) = default;
 	Mission &operator=(Mission &&) = default;
-	Mission() = default;
 	Mission(const Mission &) = delete;
 	Mission &operator=(const Mission &) = delete;
+	explicit Mission(const Mission_path &m) :
+		Mission_path(m)
+	{
+	}
+	explicit Mission(Mission_path &&m) :
+		Mission_path(std::move(m))
+	{
+	}
 	~Mission();
 };
 

common/main/newmenu.h
@@ -312,6 +312,7 @@ extern const char *Newmenu_allowed_chars;
 // 	return 0;
 // }
 
+window *listbox_get_window(listbox *lb);
 extern const char **listbox_get_items(listbox *lb);
 extern int listbox_get_citem(listbox *lb);
 extern void listbox_delete_item(listbox *lb, int item);

similar/main/newmenu.cpp
@@ -1693,6 +1693,11 @@ struct listbox : embed_window_pointer_t
 	void *userdata;
 };
 
+window *listbox_get_window(listbox *const lb)
+{
+	return lb->wind;
+}
+
 const char **listbox_get_items(listbox *lb)
 {
 	return lb->item;
@@ -1927,7 +1932,7 @@ static void listbox_create_structure( listbox *lb)
 			{
 				int w2;
 				gr_get_string_size(medium3_font, i, &w2, nullptr, nullptr, mmc);
-				if (w2 > max_box_width)
+				if (w2 > max_box_width - fspacx10 || mmc > 128)
 					break;
 			}
 			/* mmc is now the shortest initial subsequence that is wider
@@ -1942,7 +1947,7 @@ static void listbox_create_structure( listbox *lb)
 			{
 				int w2;
 				gr_get_string_size(medium3_font, j, &w2, nullptr, nullptr, mmc);
-				if (w2 > max_box_width)
+				if (w2 > max_box_width - fspacx10)
 				{
 					/* This subsequence is too long.  Reduce the length
 					 * and retry.

commit 89b9561617624dd0945797ffd55333d0e673ceca

Remove explicit std::move in prepare_mission_menu_state return
clang warns that this interferes with copy elision.  The generated code
is slightly worse after following clang's advice and removing this, but
this is not a hot path, so take the hit to silence clang instead of
complicating the source.

Fixes: d97afc2ad50027cf3acf5acde3f573c4b5589464 ("Retain directory structure in New Game dialog")

similar/main/mission.cpp
@@ -1259,7 +1259,7 @@ static mission_menu_create_state_ptr prepare_mission_menu_state(const mission_li
 		create_state.initial_selection = e.idx;
 		mission_name_to_select = nullptr;
 	}
-	return std::move(p);
+	return p;
 }
 
 int select_mission(int anarchy_mode, const char *message, window_event_result (*when_selected)(void))