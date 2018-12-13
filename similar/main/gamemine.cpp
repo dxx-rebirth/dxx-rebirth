dxx-rebirth/dxx-rebirth

commit 6d0d68d1889ffe2a88c823b1f3731559f479e1ff

Split some segment uses based on shared vs unique
Leave `sides` in `shared_segment` to minimize churn.  Parts of it must
move to `unique_segment` before this project finishes.

similar/main/gamemine.cpp
@@ -64,7 +64,7 @@ namespace dsx {
 /*
  * reads a segment2 structure from a PHYSFS_File
  */
-static void segment2_read(segment &s2, PHYSFS_File *fp)
+static void segment2_read(shared_segment &s2, unique_segment &u2, PHYSFS_File *fp)
 {
 	s2.special = PHYSFSX_readByte(fp);
 	s2.matcen_num = PHYSFSX_readByte(fp);
@@ -78,7 +78,7 @@ static void segment2_read(segment &s2, PHYSFS_File *fp)
 #elif defined(DXX_BUILD_DESCENT_II)
 	s2.s2_flags = s2_flags;
 #endif
-	s2.static_light = PHYSFSX_readFix(fp);
+	u2.static_light = PHYSFSX_readFix(fp);
 }
 
 #if defined(DXX_BUILD_DESCENT_I)
@@ -655,8 +655,14 @@ int load_mine_data(PHYSFS_File *LoadFile)
 #if defined(DXX_BUILD_DESCENT_II)
 			if (mine_top_fileinfo.fileinfo_version >= 20)
 			{
-				if (PHYSFS_read(LoadFile, &*i, mine_fileinfo.segment_sizeof, 1) != 1)
-					Error("Unable to read segment %i\n", ii);
+				/*
+				 * The format of v20 segment once matched `struct segment`, but
+				 * this was not enforced with a `static_assert` or even
+				 * commented.  The layout of `struct segment` has since changed
+				 * at least five times.  See commit
+				 * 2665869c24855040837b1864daedd4cc13ab1793 for details.
+				 */
+				Error("Sorry, v20 segment support is broken.");
 			}
 			else
 #endif
@@ -666,12 +672,12 @@ int load_mine_data(PHYSFS_File *LoadFile)
 
 				Assert(mine_fileinfo.segment_sizeof == sizeof(v16_seg));
 
-				if (PHYSFS_read( LoadFile, &v16_seg, mine_fileinfo.segment_sizeof, 1 )!=1)
-					Error( "Error reading segments in gamemine.c" );
-
 #if defined(DXX_BUILD_DESCENT_I)
+				Error("Sorry, v16 segment support is broken.");
 				*i = v16_seg;
 #elif defined(DXX_BUILD_DESCENT_II)
+				if (PHYSFS_read( LoadFile, &v16_seg, mine_fileinfo.segment_sizeof, 1 )!=1)
+					Error( "Error reading segments in gamemine.c" );
 #if DXX_USE_EDITOR
 				i->segnum = v16_seg.segnum;
 				// -- Segments[i].pad = v16_seg.pad;
@@ -732,7 +738,7 @@ int load_mine_data(PHYSFS_File *LoadFile)
 		if (mine_top_fileinfo.fileinfo_version >= 20)
 			range_for (const auto &&segp, vmsegptridx)
 			{
-				segment2_read(segp, LoadFile);
+				segment2_read(segp, segp, LoadFile);
 				fuelcen_activate(segp);
 			}
 #endif
@@ -751,8 +757,7 @@ int load_mine_data(PHYSFS_File *LoadFile)
 	{
 		if (PHYSFSX_fseek( LoadFile, mine_editor.newsegment_offset,SEEK_SET ))
 			Error( "Error seeking to newsegment_offset in gamemine.c" );
-		if (PHYSFS_read( LoadFile, &New_segment, mine_editor.newsegment_size,1 )!=1)
-			Error( "Error reading new_segment in gamemine.c" );
+		Error("Sorry, v20 segment support is broken.");
 	}
 
 	if ( (mine_fileinfo.newseg_verts_offset > -1) && (mine_fileinfo.newseg_verts_howmany > 0))
@@ -1027,7 +1032,7 @@ int load_mine_data_compiled(PHYSFS_File *LoadFile)
 	range_for (const auto &&pi, vmsegptridx)
 	{
 		if (Gamesave_current_version > 5)
-			segment2_read(pi, LoadFile);
+			segment2_read(pi, pi, LoadFile);
 		fuelcen_activate(pi);
 	}
 

common/main/fwd-segment.h
@@ -21,6 +21,8 @@ using segnum_t = uint16_t;
 }
 #ifdef dsx
 namespace dcx {
+struct shared_segment;
+struct unique_segment;
 struct segment;
 }
 DXX_VALPTRIDX_DECLARE_SUBTYPE(dcx::, segment, segnum_t, MAX_SEGMENTS);

common/main/gameseg.h
@@ -94,31 +94,31 @@ static inline side_vertnum_list_t get_side_verts(const segment &segnum, const un
 // Note: these are not absolute vertex numbers, but are relative to the segment
 // Note:  for triagulated sides, the middle vertex of each trianle is the one NOT
 //   adjacent on the diagonal edge
-uint_fast32_t create_all_vertex_lists(vertex_array_list_t &vertices, vcsegptr_t segnum, const side *sidep, uint_fast32_t sidenum);
+uint_fast32_t create_all_vertex_lists(vertex_array_list_t &vertices, const shared_segment &seg, const side &sidep, uint_fast32_t sidenum);
 __attribute_warn_unused_result
-static inline std::pair<uint_fast32_t, vertex_array_list_t> create_all_vertex_lists(vcsegptr_t segnum, const side *sidep, uint_fast32_t sidenum)
+static inline std::pair<uint_fast32_t, vertex_array_list_t> create_all_vertex_lists(const shared_segment &segnum, const side &sidep, const uint_fast32_t sidenum)
 {
 	vertex_array_list_t r;
-	auto n = create_all_vertex_lists(r, segnum, sidep, sidenum);
+	const auto &&n = create_all_vertex_lists(r, segnum, sidep, sidenum);
 	return {n, r};
 }
 #endif
 
 //like create_all_vertex_lists(), but generate absolute point numbers
-uint_fast32_t create_abs_vertex_lists(vertex_array_list_t &vertices, vcsegptr_t segnum, const side *sidep, uint_fast32_t sidenum);
+uint_fast32_t create_abs_vertex_lists(vertex_array_list_t &vertices, const shared_segment &segnum, const side &sidep, uint_fast32_t sidenum);
 
 __attribute_warn_unused_result
-static inline std::pair<uint_fast32_t, vertex_array_list_t> create_abs_vertex_lists(vcsegptr_t segnum, const side *sidep, uint_fast32_t sidenum)
+static inline std::pair<uint_fast32_t, vertex_array_list_t> create_abs_vertex_lists(const shared_segment &segnum, const side &sidep, const uint_fast32_t sidenum)
 {
 	vertex_array_list_t r;
-	auto n = create_abs_vertex_lists(r, segnum, sidep, sidenum);
+	const auto &&n = create_abs_vertex_lists(r, segnum, sidep, sidenum);
 	return {n, r};
 }
 
 __attribute_warn_unused_result
-static inline std::pair<uint_fast32_t, vertex_array_list_t> create_abs_vertex_lists(vcsegptr_t segp, uint_fast32_t sidenum)
+static inline std::pair<uint_fast32_t, vertex_array_list_t> create_abs_vertex_lists(const shared_segment &segp, const uint_fast32_t sidenum)
 {
-	return create_abs_vertex_lists(segp, &segp->sides[sidenum], sidenum);
+	return create_abs_vertex_lists(segp, segp.sides[sidenum], sidenum);
 }
 
 // -----------------------------------------------------------------------------------
@@ -127,9 +127,9 @@ static inline std::pair<uint_fast32_t, vertex_array_list_t> create_abs_vertex_li
 //      If there is one face, it has 4 vertices.
 //      If there are two faces, they both have three vertices, so face #0 is stored in vertices 0,1,2,
 //      face #1 is stored in vertices 3,4,5.
-void create_all_vertnum_lists(vertex_vertnum_array_list &vertnums, vcsegptr_t segnum, const side *const sidep, uint_fast32_t sidenum);
+void create_all_vertnum_lists(vertex_vertnum_array_list &vertnums, const shared_segment &seg, const side &sidep, uint_fast32_t sidenum);
 __attribute_warn_unused_result
-static inline vertex_vertnum_array_list create_all_vertnum_lists(vcsegptr_t segnum, const side *const sidep, uint_fast32_t sidenum)
+static inline vertex_vertnum_array_list create_all_vertnum_lists(const shared_segment &segnum, const side &sidep, const uint_fast32_t sidenum)
 {
 	vertex_vertnum_array_list r;
 	return create_all_vertnum_lists(r, segnum, sidep, sidenum), r;

common/main/segment.h
@@ -153,29 +153,37 @@ struct side
 };
 
 #ifdef dsx
-struct segment {
+struct shared_segment
+{
 #if DXX_USE_EDITOR
 	segnum_t   segnum;     // segment number, not sure what it means
 	short   group;      // group number to which the segment belongs.
 #endif
-	objnum_t objects;    // pointer to objects in this segment
 	array<segnum_t, MAX_SIDES_PER_SEGMENT>   children;    // indices of 6 children segments, front, left, top, right, bottom, back
+	array<unsigned, MAX_VERTICES_PER_SEGMENT> verts;    // vertex ids of 4 front and 4 back vertices
+	uint8_t special;    // what type of center this is
+	int8_t matcen_num; // which center segment is associated with.
+	uint8_t station_idx;
+	/* if DXX_BUILD_DESCENT_II */
+	uint8_t s2_flags;
+	/* endif */
+	array<side, MAX_SIDES_PER_SEGMENT>    sides;       // 6 sides
+};
+
+struct unique_segment
+{
+	objnum_t objects;    // pointer to objects in this segment
 	//      If bit n (1 << n) is set, then side #n in segment has had light subtracted from original (editor-computed) value.
 	uint8_t light_subtracted;
 	/* if DXX_BUILD_DESCENT_II */
 	uint8_t slide_textures;
 	/* endif */
-	array<unsigned, MAX_VERTICES_PER_SEGMENT> verts;    // vertex ids of 4 front and 4 back vertices
-	ubyte   special;    // what type of center this is
-	sbyte   matcen_num; // which center segment is associated with.
-	uint8_t station_idx;
-	/* if DXX_BUILD_DESCENT_II */
-	uint8_t s2_flags;
-	/* endif */
 	fix     static_light;
-	array<side, MAX_SIDES_PER_SEGMENT>    sides;       // 6 sides
 };
 
+struct segment : unique_segment, shared_segment
+{
+};
 #endif
 
 struct count_segment_array_t : public count_array_t<segnum_t, MAX_SEGMENTS> {};
@@ -219,16 +227,16 @@ DXX_VALPTRIDX_DEFINE_GLOBAL_FACTORIES(vertex, vert, Vertices);
 #ifdef dsx
 struct side::illegal_type : std::runtime_error
 {
-	icsegptr_t m_segment;
-	const side *m_side;
-	illegal_type(icsegptr_t seg, const side *s) :
+	const shared_segment *const m_segment;
+	const side *const m_side;
+	illegal_type(const shared_segment &seg, const side &s) :
 		runtime_error("illegal side type"),
-		m_segment(seg), m_side(s)
+		m_segment(&seg), m_side(&s)
 	{
 	}
-	illegal_type(const side *s) :
+	illegal_type(const side &s) :
 		runtime_error("illegal side type"),
-		m_segment(nullptr), m_side(s)
+		m_segment(nullptr), m_side(&s)
 	{
 	}
 };
@@ -243,7 +251,7 @@ void side::set_type(unsigned t)
 			set_type(static_cast<side_type>(t));
 			break;
 		default:
-			throw illegal_type(this);
+			throw illegal_type(*this);
 	}
 }
 

similar/editor/meddraw.cpp
@@ -384,7 +384,7 @@ static void add_edges(const vcsegptridx_t seg)
 
 		range_for (auto &&e, enumerate(seg->sides))
 		{
-			auto sidep = &e.value;
+			auto &sidep = e.value;
 			int	num_vertices;
 			const auto v = create_all_vertex_lists(seg, sidep, e.idx);
 			const auto &num_faces = v.first;
@@ -398,7 +398,7 @@ static void add_edges(const vcsegptridx_t seg)
 				int	en;
 
 				//Note: normal check appears to be the wrong way since the normals points in, but we're looking from the outside
-				if (g3_check_normal_facing(vcvertptr(seg->verts[vertex_list[fn*3]]),sidep->normals[fn]))
+				if (g3_check_normal_facing(vcvertptr(seg->verts[vertex_list[fn*3]]), sidep.normals[fn]))
 					flag = ET_NOTFACING;
 				else
 					flag = ET_FACING;

similar/editor/mine.cpp
@@ -454,6 +454,8 @@ static int save_mine_data(PHYSFS_File * SaveFile)
 
 	if (segment_offset != PHYSFS_tell(SaveFile))
 		Error( "OFFSETS WRONG IN MINE.C!" );
+	Error("Sorry, v20 segment support is broken.");
+#if 0
 	PHYSFS_write( SaveFile, &Segments.front(), sizeof(segment), Num_segments );
 
 	//===================== SAVE NEWSEGMENT INFO ======================
@@ -461,6 +463,7 @@ static int save_mine_data(PHYSFS_File * SaveFile)
 	if (newsegment_offset != PHYSFS_tell(SaveFile))
 		Error( "OFFSETS WRONG IN MINE.C!" );
 	PHYSFS_write( SaveFile, &New_segment, sizeof(segment), 1 );
+#endif
 
 	if (newseg_verts_offset != PHYSFS_tell(SaveFile))
 		Error( "OFFSETS WRONG IN MINE.C!" );

similar/main/fvi.cpp
@@ -239,7 +239,7 @@ static int check_sphere_to_face(const vms_vector &pnt, const side *s,int facenum
 __attribute_warn_unused_result
 static int check_line_to_face(vms_vector &newp,const vms_vector &p0,const vms_vector &p1,const vcsegptridx_t seg,int side,int facenum,int nv,fix rad)
 {
-	const struct side *s=&seg->sides[side];
+	auto &s = seg->sides[side];
 	vms_vector norm;
 
 		norm = seg->sides[side].normals[facenum];
@@ -269,7 +269,7 @@ static int check_line_to_face(vms_vector &newp,const vms_vector &p0,const vms_ve
 	if (rad!=0)
 		vm_vec_scale_add2(checkp,norm,-rad);
 
-	return check_sphere_to_face(checkp,s,facenum,nv,rad,vertex_list);
+	return check_sphere_to_face(checkp, &s, facenum, nv, rad, vertex_list);
 
 }
 
@@ -318,8 +318,7 @@ static int special_check_line_to_face(vms_vector &newp,const vms_vector &p0,cons
 {
 	fix edge_t=0,move_t=0,edge_t2=0,move_t2=0;
 	int edgenum;
-	uint edgemask;
-	const struct side *s=&seg->sides[side];
+	auto &s = seg->sides[side];
 
 	//calc some basic stuff
 
@@ -329,7 +328,7 @@ static int special_check_line_to_face(vms_vector &newp,const vms_vector &p0,cons
 
 	//figure out which edge(s) to check against
 
-	edgemask = check_point_to_face(p0,s,facenum,nv,vertex_list);
+	unsigned edgemask = check_point_to_face(p0, &s, facenum, nv, vertex_list);
 
 	if (edgemask == 0)
 		return check_line_to_face(newp,p0,p1,seg,side,facenum,nv,rad);
@@ -1099,7 +1098,7 @@ quit_looking:
 namespace dsx {
 fvi_hitpoint find_hitpoint_uv(const vms_vector &pnt, const vcsegptridx_t seg, const uint_fast32_t sidenum, const uint_fast32_t facenum)
 {
-	const side *side = &seg->sides[sidenum];
+	auto &side = seg->sides[sidenum];
 	fix k0,k1;
 	int i;
 
@@ -1113,7 +1112,7 @@ fvi_hitpoint find_hitpoint_uv(const vms_vector &pnt, const vcsegptridx_t seg, co
 
 	//1. find what plane to project this wall onto to make it a 2d case
 
-	const auto &normal_array = side->normals[facenum];
+	const auto &normal_array = side.normals[facenum];
 	auto fmax = [](const vms_vector &v, fix vms_vector::*a, fix vms_vector::*b) {
 		return abs(v.*a) > abs(v.*b) ? a : b;
 	};
@@ -1151,7 +1150,7 @@ fvi_hitpoint find_hitpoint_uv(const vms_vector &pnt, const vcsegptridx_t seg, co
 
 	array<uvl, 3> uvls;
 	for (i=0;i<3;i++)
-		uvls[i] = side->uvls[vn[facenum*3+i].vertnum];
+		uvls[i] = side.uvls[vn[facenum * 3 + i].vertnum];
 
 	auto p = [&uvls, k0, k1](fix uvl::*pmf) {
 		return uvls[1].*pmf + fixmul(k0,uvls[0].*pmf - uvls[1].*pmf) + fixmul(k1,uvls[2].*pmf - uvls[1].*pmf);
@@ -1222,12 +1221,12 @@ static int sphere_intersects_wall(const vms_vector &pnt, const vcsegptridx_t seg
 					int face_hit_type;      //in what way did we hit the face?
 
 					//did we go through this wall/door?
-					const auto *sidep = &seg->sides[side];
+					auto &sidep = seg->sides[side];
 					const auto v = create_abs_vertex_lists(seg, sidep, side);
 					const auto &num_faces = v.first;
 					const auto &vertex_list = v.second;
 
-					face_hit_type = check_sphere_to_face(pnt, sidep,
+					face_hit_type = check_sphere_to_face(pnt, &sidep,
 										face,((num_faces==1)?4:3),rad,vertex_list);
 
 					if (face_hit_type) {            //through this wall/door

similar/main/gameseg.cpp
@@ -67,8 +67,8 @@ class abs_vertex_lists_predicate
 	const array<unsigned, MAX_VERTICES_PER_SEGMENT> &m_vp;
 	const array<unsigned, 4> &m_sv;
 public:
-	abs_vertex_lists_predicate(const vcsegptr_t segp, uint_fast32_t sidenum) :
-		m_vp(segp->verts), m_sv(Side_to_verts_int[sidenum])
+	abs_vertex_lists_predicate(const shared_segment &seg, const uint_fast32_t sidenum) :
+		m_vp(seg.verts), m_sv(Side_to_verts_int[sidenum])
 	{
 	}
 	unsigned operator()(const uint_fast32_t vv) const
@@ -174,7 +174,7 @@ bool get_side_is_quad(const side &sidep)
 		case SIDE_IS_TRI_13:	
 			return false;
 		default:
-			throw side::illegal_type(&sidep);
+			throw side::illegal_type(sidep);
 	}
 }
 
@@ -197,15 +197,15 @@ void get_side_verts(side_vertnum_list_t &vertlist, const segment &segp, const un
 
 __attribute_cold
 __noreturn
-static void create_vertex_list_from_invalid_side(const vcsegptr_t segp, const side *const sidep)
+static void create_vertex_list_from_invalid_side(const shared_segment &segp, const side &sidep)
 {
 	throw side::illegal_type(segp, sidep);
 }
 
 template <typename T, typename V>
-static uint_fast32_t create_vertex_lists_from_values(T &va, const vcsegptr_t segp, const side *const sidep, const V &&f0, const V &&f1, const V &&f2, const V &&f3)
+static uint_fast32_t create_vertex_lists_from_values(T &va, const shared_segment &segp, const side &sidep, const V &&f0, const V &&f1, const V &&f2, const V &&f3)
 {
-	const auto type = sidep->get_type();
+	const auto type = sidep.get_type();
 	if (type == SIDE_IS_TRI_13)
 	{
 		va[0] = va[5] = f3;
@@ -241,7 +241,7 @@ static uint_fast32_t create_vertex_lists_from_values(T &va, const vcsegptr_t seg
 }
 
 template <typename T, typename F>
-static inline uint_fast32_t create_vertex_lists_by_predicate(T &va, const vcsegptr_t segp, const side *const sidep, const F &&f)
+static inline uint_fast32_t create_vertex_lists_by_predicate(T &va, const shared_segment &segp, const side &sidep, const F &&f)
 {
 	return create_vertex_lists_from_values(va, segp, sidep, f(0), f(1), f(2), f(3));
 }
@@ -258,7 +258,7 @@ static inline uint_fast32_t create_vertex_lists_by_predicate(T &va, const vcsegp
 // Note: these are not absolute vertex numbers, but are relative to the segment
 // Note:  for triagulated sides, the middle vertex of each trianle is the one NOT
 //   adjacent on the diagonal edge
-uint_fast32_t create_all_vertex_lists(vertex_array_list_t &vertices, const vcsegptr_t segp, const side *const sidep, const uint_fast32_t sidenum)
+uint_fast32_t create_all_vertex_lists(vertex_array_list_t &vertices, const shared_segment &segp, const side &sidep, const uint_fast32_t sidenum)
 {
 	assert(sidenum < Side_to_verts_int.size());
 	auto &sv = Side_to_verts_int[sidenum];
@@ -274,14 +274,14 @@ uint_fast32_t create_all_vertex_lists(vertex_array_list_t &vertices, const vcseg
 //	If there is one face, it has 4 vertices.
 //	If there are two faces, they both have three vertices, so face #0 is stored in vertices 0,1,2,
 //	face #1 is stored in vertices 3,4,5.
-void create_all_vertnum_lists(vertex_vertnum_array_list &vertnums, const vcsegptr_t segp, const side *const sidep, uint_fast32_t sidenum)
+void create_all_vertnum_lists(vertex_vertnum_array_list &vertnums, const shared_segment &segp, const side &sidep, const uint_fast32_t sidenum)
 {
 	create_vertex_lists_by_predicate(vertnums, segp, sidep, all_vertnum_lists_predicate(segp, sidenum));
 }
 
 // -----
 // like create_all_vertex_lists(), but generate absolute point numbers
-uint_fast32_t create_abs_vertex_lists(vertex_array_list_t &vertices, const vcsegptr_t segp, const side *sidep, uint_fast32_t sidenum)
+uint_fast32_t create_abs_vertex_lists(vertex_array_list_t &vertices, const shared_segment &segp, const side &sidep, const uint_fast32_t sidenum)
 {
 	return create_vertex_lists_by_predicate(vertices, segp, sidep, abs_vertex_lists_predicate(segp, sidenum));
 }
@@ -298,7 +298,7 @@ segmasks get_seg_masks(fvcvertptr &vcvertptr, const vms_vector &checkp, const vc
 	//check point against each side of segment. return bitmask
 
 	for (sn=0,facebit=sidebit=1;sn<6;sn++,sidebit<<=1) {
-		auto s = &seg->sides[sn];
+		auto &s = seg->sides[sn];
 		
 		// Get number of faces on this side, and at vertex_list, store vertices.
 		//	If one face, then vertex_list indicates a quadrilateral.
@@ -319,15 +319,15 @@ segmasks get_seg_masks(fvcvertptr &vcvertptr, const vms_vector &checkp, const vc
 			const auto &&mvert = vcvertptr(vertnum);
 
 			auto a = vertex_list[4] < vertex_list[1]
-				? std::make_pair(vertex_list[4], &s->normals[0])
-				: std::make_pair(vertex_list[1], &s->normals[1]);
+				? std::make_pair(vertex_list[4], &s.normals[0])
+				: std::make_pair(vertex_list[1], &s.normals[1]);
 			const auto mdist = vm_dist_to_plane(vcvertptr(a.first), *a.second, mvert);
 
 			side_count = center_count = 0;
 
 			for (int fn=0;fn<2;fn++,facebit<<=1) {
 
-				const auto dist = vm_dist_to_plane(checkp, s->normals[fn], mvert);
+				const auto dist = vm_dist_to_plane(checkp, s.normals[fn], mvert);
 
 				if (dist-rad < -PLANE_DIST_TOLERANCE) {
 					if (dist < -PLANE_DIST_TOLERANCE)	//in front of face
@@ -365,7 +365,7 @@ segmasks get_seg_masks(fvcvertptr &vcvertptr, const vms_vector &checkp, const vc
 			const auto vertnum = *std::min_element(b, std::next(b, 4));
 			const auto &&mvert = vcvertptr(vertnum);
 
-			const auto dist = vm_dist_to_plane(checkp, s->normals[0], mvert);
+			const auto dist = vm_dist_to_plane(checkp, s.normals[0], mvert);
 			if (dist-rad < -PLANE_DIST_TOLERANCE) {
 				if (dist < -PLANE_DIST_TOLERANCE)
 					masks.centermask |= sidebit;
@@ -396,7 +396,7 @@ static uint8_t get_side_dists(fvcvertptr &vcvertptr, const vms_vector &checkp,co
 
 	side_dists = {};
 	for (sn=0,facebit=sidebit=1;sn<6;sn++,sidebit<<=1) {
-		side	*s = &seg->sides[sn];
+		auto &s = seg->sides[sn];
 
 		// Get number of faces on this side, and at vertex_list, store vertices.
 		//	If one face, then vertex_list indicates a quadrilateral.
@@ -417,15 +417,15 @@ static uint8_t get_side_dists(fvcvertptr &vcvertptr, const vms_vector &checkp,co
 			auto &mvert = *vcvertptr(vertnum);
 
 			auto a = vertex_list[4] < vertex_list[1]
-				? std::make_pair(vertex_list[4], &s->normals[0])
-				: std::make_pair(vertex_list[1], &s->normals[1]);
+				? std::make_pair(vertex_list[4], &s.normals[0])
+				: std::make_pair(vertex_list[1], &s.normals[1]);
 			const auto mdist = vm_dist_to_plane(vcvertptr(a.first), *a.second, mvert);
 
 			center_count = 0;
 
 			for (int fn=0;fn<2;fn++,facebit<<=1) {
 
-				const auto dist = vm_dist_to_plane(checkp, s->normals[fn], mvert);
+				const auto dist = vm_dist_to_plane(checkp, s.normals[fn], mvert);
 
 				if (dist < -PLANE_DIST_TOLERANCE) {	//in front of face
 					center_count++;
@@ -461,7 +461,7 @@ static uint8_t get_side_dists(fvcvertptr &vcvertptr, const vms_vector &checkp,co
 			auto b = begin(vertex_list);
 			auto vertnum = *std::min_element(b, std::next(b, 4));
 
-			const auto dist = vm_dist_to_plane(checkp, s->normals[0], vcvertptr(vertnum));
+			const auto dist = vm_dist_to_plane(checkp, s.normals[0], vcvertptr(vertnum));
 	
 			if (dist < -PLANE_DIST_TOLERANCE) {
 				mask |= sidebit;
@@ -1392,7 +1392,7 @@ void create_walls_on_side(const vmsegptridx_t sp, int sidenum)
 	if (negate_flag)
 		vm_vec_negate(vn);
 
-	const auto s = &sp->sides[sidenum];
+	auto &s = sp->sides[sidenum];
 	if (dist_to_plane > PLANE_DIST_TOLERANCE)
 	{
 		add_side_as_2_triangles(sp, sidenum);
@@ -1409,8 +1409,8 @@ void create_walls_on_side(const vmsegptridx_t sp, int sidenum)
 
 			auto &vvn = *vcvertptr(min(vertex_list[0],vertex_list[2]));
 
-			const fix dist0 = vm_dist_to_plane(vcvertptr(vertex_list[1]), s->normals[1], vvn);
-			const fix dist1 = vm_dist_to_plane(vcvertptr(vertex_list[4]), s->normals[0], vvn);
+			const fix dist0 = vm_dist_to_plane(vcvertptr(vertex_list[1]), s.normals[1], vvn);
+			const fix dist1 = vm_dist_to_plane(vcvertptr(vertex_list[4]), s.normals[0], vvn);
 
 			s0 = sign(dist0);
 			s1 = sign(dist1);
@@ -1419,7 +1419,7 @@ void create_walls_on_side(const vmsegptridx_t sp, int sidenum)
 			return;
 		//detriangulate!
 	}
-	add_side_as_quad(s, vn);
+	add_side_as_quad(&s, vn);
 }
 
 // -------------------------------------------------------------------------------

commit a54c323e45b71e2d1f2037a9d4ab1fafb113d8e2

Remove v16_segment definition
Support for v16_segment was declared broken in 6d0d68d188.  No one
objected.  Remove the definition to simplify future work.

similar/main/gamemine.cpp
@@ -82,31 +82,10 @@ static void segment2_read(shared_segment &s2, unique_segment &u2, PHYSFS_File *f
 }
 
 #if defined(DXX_BUILD_DESCENT_I)
-typedef segment v16_segment;
 #elif defined(DXX_BUILD_DESCENT_II)
 fix Level_shake_frequency = 0, Level_shake_duration = 0;
 segnum_t Secret_return_segment;
 vms_matrix Secret_return_orient;
-
-struct v16_segment {
-#if DXX_USE_EDITOR
-	short   segnum;             // segment number, not sure what it means
-	#endif
-	side    sides[MAX_SIDES_PER_SEGMENT];       // 6 sides
-	short   children[MAX_SIDES_PER_SEGMENT];    // indices of 6 children segments, front, left, top, right, bottom, back
-	short   verts[MAX_VERTICES_PER_SEGMENT];    // vertex ids of 4 front and 4 back vertices
-	#ifdef  EDITOR
-	short   group;              // group number to which the segment belongs.
-	#endif
-	short   objects;            // pointer to objects in this segment
-	ubyte   special;            // what type of center this is
-	sbyte   matcen_num;         // which center segment is associated with.
-	short   value;
-	fix     static_light;       // average static light in segment
-#if !DXX_USE_EDITOR
-	short   pad;                // make structure longword aligned
-	#endif
-};
 
 int d1_pig_present = 0; // can descent.pig from descent 1 be loaded?
 
@@ -667,15 +646,15 @@ int load_mine_data(PHYSFS_File *LoadFile)
 #endif
 			if (mine_top_fileinfo.fileinfo_version >= 16)
 			{
+				Error("Sorry, v16 segment support is broken.");
+#if 0
 				v16_segment v16_seg;
 
 				Assert(mine_fileinfo.segment_sizeof == sizeof(v16_seg));
 
-				Error("Sorry, v16 segment support is broken.");
 #if defined(DXX_BUILD_DESCENT_I)
 				*i = v16_seg;
 #elif defined(DXX_BUILD_DESCENT_II)
-#if 0
 				i->segnum = v16_seg.segnum;
 				// -- Segments[i].pad = v16_seg.pad;
 