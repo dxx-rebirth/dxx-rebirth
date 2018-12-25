f921967be6a6d44fdc0ec09fbf17c82e07c1fcae
pkubaj <pkubaj@users.noreply.github.com>
2018-12-25 20:31:07 +0100

    Fix build on FreeBSD pt. 2

--- similar/main/inferno.cpp
+++ similar/main/inferno.cpp
@@ -715,7 +715,7 @@ static int main(int argc, char *argv[])
 int main(int argc, char *argv[])
 {
 	mem_init();
-#if defined(__linux__) || defined(__OpenBSD__)
+#if defined(__linux__) || defined(__OpenBSD__) || defined(__FreeBSD__)
 #if DXX_WORDS_NEED_ALIGNMENT
 	prctl(PR_SET_UNALIGN, PR_UNALIGN_NOPRINT, 0, 0, 0);
 #endif
