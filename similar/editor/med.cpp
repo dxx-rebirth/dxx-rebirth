ffe121fbfde60f6159c8c86a8c2d69127e80a770
pkubaj <pkubaj@users.noreply.github.com>
2018-12-25 20:28:42 +0100

    Fix build on FreeBSD

--- similar/editor/med.cpp
+++ similar/editor/med.cpp
@@ -890,7 +890,7 @@ static void close_editor()
 {
 	//	_MARK_("end of editor");//Nuked to compile -KRB
 	
-#if !defined(__linux__) && !defined(__OpenBSD__)
+#if !defined(__linux__) && !defined(__OpenBSD__) && !defined(__FreeBSD__)
 	set_warn_func(msgbox_warning);
 #else
 	clear_warn_func();
