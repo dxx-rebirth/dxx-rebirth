dxx-rebirth/dxx-rebirth

commit 8764cad4579beb9732098c32ce5c1dc35d4f7cf5

Use `unsigned` in more prototypes

similar/main/wall.cpp
@@ -396,7 +396,7 @@ void wall_damage(const vmsegptridx_t seg, const unsigned side, fix damage)
 //-----------------------------------------------------------------
 // Opens a door
 namespace dsx {
-void wall_open_door(const vmsegptridx_t seg, int side)
+void wall_open_door(const vmsegptridx_t seg, const unsigned side)
 {
 	auto &Objects = LevelUniqueObjectState.Objects;
 	auto &vmobjptr = Objects.vmptr;
@@ -1068,7 +1068,7 @@ static int special_boss_opening_allowed(segnum_t segnum, int sidenum)
 //playernum is the number the player who hit the wall or fired the weapon,
 //or -1 if a robot fired the weapon
 namespace dsx {
-wall_hit_process_t wall_hit_process(const player_flags powerup_flags, const vmsegptridx_t seg, int side, fix damage, int playernum, const object &obj)
+wall_hit_process_t wall_hit_process(const player_flags powerup_flags, const vmsegptridx_t seg, const unsigned side, const fix damage, const unsigned playernum, const object &obj)
 {
 	fix	show_message;
 

common/main/ai.h
@@ -120,7 +120,7 @@ extern vms_vector Believed_player_pos;
 
 namespace dsx {
 void move_towards_segment_center(const d_level_shared_segment_state &, object_base &objp);
-imobjptridx_t gate_in_robot(int type, vmsegptridx_t segnum);
+imobjptridx_t gate_in_robot(unsigned type, vmsegptridx_t segnum);
 void do_ai_frame(vmobjptridx_t objp);
 void do_ai_frame_all();
 }

common/main/collide.h
@@ -78,7 +78,7 @@ void collide_live_local_player_and_powerup(const vmobjptridx_t powerup);
 #endif
 #ifdef dsx
 namespace dsx {
-int check_effect_blowup(const d_level_shared_destructible_light_state &LevelSharedDestructibleLightState, const d_vclip_array &Vclip, vmsegptridx_t seg,int side,const vms_vector &pnt, const laser_parent &blower, int force_blowup_flag, int remote);
+int check_effect_blowup(const d_level_shared_destructible_light_state &LevelSharedDestructibleLightState, const d_vclip_array &Vclip, vmsegptridx_t seg, unsigned side, const vms_vector &pnt, const laser_parent &blower, int force_blowup_flag, int remote);
 }
 #endif
 void apply_damage_to_controlcen(vmobjptridx_t controlcen, fix damage, vcobjptr_t who);

common/main/digi.h
@@ -69,8 +69,8 @@ enum class sound_stack : uint8_t
 	cancel_previous,
 };
 void digi_link_sound_to_object(unsigned soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once);
-void digi_kill_sound_linked_to_segment(segnum_t segnum, int sidenum, int soundnum);
-void digi_link_sound_to_pos( int soundnum, vcsegptridx_t segnum, short sidenum, const vms_vector &pos, int forever, fix max_volume );
+void digi_kill_sound_linked_to_segment(vmsegidx_t segnum, unsigned sidenum, int soundnum);
+void digi_link_sound_to_pos(unsigned soundnum, vcsegptridx_t segnum, unsigned sidenum, const vms_vector &pos, int forever, fix max_volume);
 // Same as above, but you pass the max distance sound can be heard.  The old way uses f1_0*256 for max_distance.
 void digi_link_sound_to_object2(unsigned soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once, vm_distance max_distance);
 void digi_link_sound_to_object3(unsigned soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once, vm_distance max_distance, int loop_start, int loop_end);

common/main/fuelcen.h
@@ -67,7 +67,7 @@ void fuelcen_activate(vmsegptridx_t segp);
 void fuelcen_delete(vmsegptr_t segp);
 
 // Create a matcen robot
-imobjptridx_t create_morph_robot(vmsegptridx_t segp, const vms_vector &object_pos, int object_id);
+imobjptridx_t create_morph_robot(vmsegptridx_t segp, const vms_vector &object_pos, unsigned object_id);
 
 // Returns the amount of fuel/shields this segment can give up.
 // Can be from 0 to 100.

common/main/fwd-object.h
@@ -230,7 +230,7 @@ void obj_unlink(fvmobjptr &vmobjptr, fvmsegptr &vmsegptr, object_base &obj);
 
 // initialize a new object.  adds to the list for the given segment
 // returns the object number
-imobjptridx_t obj_create(object_type_t type, ubyte id, vmsegptridx_t segnum, const vms_vector &pos, const vms_matrix *orient, fix size, ubyte ctype, ubyte mtype, ubyte rtype);
+imobjptridx_t obj_create(object_type_t type, unsigned id, vmsegptridx_t segnum, const vms_vector &pos, const vms_matrix *orient, fix size, unsigned ctype, movement_type_t mtype, unsigned rtype);
 
 // make a copy of an object. returs num of new object
 imobjptridx_t obj_create_copy(const object &srcobj, vmsegptridx_t newsegnum);
@@ -329,7 +329,7 @@ window_event_result dead_player_frame();
 #if defined(DXX_BUILD_DESCENT_II)
 extern int Drop_afterburner_blob_flag;		//ugly hack
 // returns object number
-imobjptridx_t drop_marker_object(const vms_vector &pos, vmsegptridx_t segnum, const vms_matrix &orient, int marker_num);
+imobjptridx_t drop_marker_object(const vms_vector &pos, vmsegptridx_t segnum, const vms_matrix &orient, unsigned marker_num);
 
 void wake_up_rendered_objects(const object &gmissp, window_rendered_data &window);
 

common/main/fwd-wall.h
@@ -234,7 +234,7 @@ void wall_illusion_on(fvmwallptr &, vcsegptridx_t seg, unsigned side);
 void wall_illusion_off(fvmwallptr &, vcsegptridx_t seg, unsigned side);
 
 // Opens a door
-void wall_open_door(vmsegptridx_t seg, int side);
+void wall_open_door(vmsegptridx_t seg, unsigned side);
 
 #if defined(DXX_BUILD_DESCENT_I)
 #elif defined(DXX_BUILD_DESCENT_II)
@@ -258,7 +258,7 @@ enum class wall_hit_process_t : unsigned
 #ifdef dsx
 class player_flags;
 namespace dsx {
-wall_hit_process_t wall_hit_process(player_flags, vmsegptridx_t seg, int side, fix damage, int playernum, const object &obj);
+wall_hit_process_t wall_hit_process(player_flags, vmsegptridx_t seg, unsigned side, fix damage, unsigned playernum, const object &obj);
 
 // Opens/destroys specified door.
 }

common/main/multi.h
@@ -350,7 +350,7 @@ static inline laser_level_t map_granted_flags_to_laser_level(const packed_spawn_
 player_flags map_granted_flags_to_player_flags(packed_spawn_granted_items grant);
 uint_fast32_t map_granted_flags_to_primary_weapon_flags(packed_spawn_granted_items grant);
 uint16_t map_granted_flags_to_vulcan_ammo(packed_spawn_granted_items grant);
-void multi_digi_link_sound_to_pos(int soundnum, vcsegptridx_t segnum, short sidenum, const vms_vector &pos, int forever, fix max_volume);
+void multi_digi_link_sound_to_pos(int soundnum, vcsegptridx_t segnum, unsigned sidenum, const vms_vector &pos, int forever, fix max_volume);
 void multi_object_to_object_rw(vmobjptr_t obj, object_rw *obj_rw);
 void multi_object_rw_to_object(object_rw *obj_rw, vmobjptr_t obj);
 

commit 8f2bec3fde98390045434c9af477e350205367af

Remove bogus assertion in wall_hit_process
Fixes: 8764cad4579beb9732098c32ce5c1dc35d4f7cf5 ("Use `unsigned` in more prototypes")

similar/main/wall.cpp
@@ -1095,8 +1095,6 @@ wall_hit_process_t wall_hit_process(const player_flags powerup_flags, const vmse
 	if (playernum != Player_num)	//return if was robot fire
 		return wall_hit_process_t::WHP_NOT_SPECIAL;
 
-	Assert( playernum > -1 );
-	
 	//	Determine whether player is moving forward.  If not, don't say negative
 	//	messages because he probably didn't intentionally hit the door.
 	if (obj.type == OBJ_PLAYER)