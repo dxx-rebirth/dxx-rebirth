dxx-rebirth/dxx-rebirth

commit 063bf292255b1cbe4b8cc6e2e14f5c5670e7cef0

Enable -Wimplicit-fallthrough=5; fix resulting breaks
This enables using -Wimplicit-fallthrough with ccache+distcc, which
strip the fallthrough comments, but do not strip the attribute
annotationns.

SConstruct
@@ -2583,6 +2583,33 @@ where the cast is useless.
 	(void)ts;
 	return 0;
 ''', msg='for struct timespec', successflags=_successflags)
+	@_implicit_test
+	def check_warn_implicit_fallthrough(self,context,text,main,testflags,_successflags={'CXXFLAGS' : ['-Wimplicit-fallthrough=5']}):
+		self.Compile(context, text=text, main=main, msg='for -Wimplicit-fallthrough=5', testflags=testflags, successflags=_successflags)
+	@_custom_test
+	def check_boost_config(self,context,_successflags={'CPPDEFINES' : [('DXX_BOOST_FALLTHROUGH', 'BOOST_FALLTHROUGH')]}):
+		text ='''
+#include <boost/config.hpp>
+'''
+		main = '''
+	switch (argc) {
+		case 1:
+			DXX_BOOST_FALLTHROUGH;
+		case 2:
+			break;
+	}
+'''
+		sconf = context.sconf
+		if not self.Compile(context, text=text, main=main, msg='for Boost.Config', successflags=_successflags):
+			sconf.Define('DXX_BOOST_FALLTHROUGH', '((void)0)')
+			return
+		else:
+			sconf.config_h_text += '''
+#ifndef DXX_SCONF_NO_INCLUDES
+#include <boost/config.hpp>
+#endif
+'''
+		self.check_warn_implicit_fallthrough(context, text, main, testflags=_successflags)
 	__preferred_compiler_options = (
 		'-fvisibility=hidden',
 		'-Wduplicated-branches',
@@ -2643,6 +2670,23 @@ unsigned u2(bool b);
 unsigned u2(bool b)
 {
 	return u<1, 1>(b);
+}
+
+}
+
+namespace test_fallthrough {
+
+int a(int i);
+int a(int i)
+{
+	switch (i) {
+		case 1:
+			DXX_BOOST_FALLTHROUGH;
+		case 2:
+			return i + 1;
+		default:
+			return 0;
+	}
 }
 
 }

common/ui/dialog.cpp
@@ -105,7 +105,7 @@ static window_event_result ui_dialog_handler(window *wind,const d_event &event, 
 	{
 		case EVENT_IDLE:
 			timer_delay2(50);
-			/*-fallthrough*/
+			DXX_BOOST_FALLTHROUGH;
 		case EVENT_MOUSE_BUTTON_DOWN:
 		case EVENT_MOUSE_BUTTON_UP:
 		case EVENT_MOUSE_MOVED:

d2x-rebirth/libmve/decoder8.cpp
@@ -236,7 +236,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 	case 0x0:
 		/* block is copied from block in current frame */
 		copyFrame(*pFrame, *pFrame + (g_vBackBuf2 - g_vBackBuf1));
-		//-fallthrough
+		DXX_BOOST_FALLTHROUGH;
 	case 0x1:
 		/* block is unchanged from two frames ago */
 		*pFrame += 8;

d2x-rebirth/main/escort.cpp
@@ -1537,7 +1537,7 @@ static int maybe_steal_primary_weapon(const vmobjptr_t playerobjp, int weapon_nu
 			if (!player_info.vulcan_ammo)
 				return 0;
 			is_energy_weapon = false;
-			//-fallthrough
+			DXX_BOOST_FALLTHROUGH;
 		default:
 			if (!(player_info.primary_weapon_flags & HAS_PRIMARY_FLAG(weapon_num)))
 				return 0;

d2x-rebirth/main/movie.cpp
@@ -261,8 +261,7 @@ static window_event_result show_pause_message(window *, const d_event &event, co
 		case EVENT_MOUSE_BUTTON_DOWN:
 			if (event_mouse_get_button(event) != 0)
 				return window_event_result::ignored;
-			// else fall through
-
+			DXX_BOOST_FALLTHROUGH;
 		case EVENT_KEY_COMMAND:
 			if ((result = call_default_handler(event)) == window_event_result::ignored)
 			{

similar/arch/ogl/ogl.cpp
@@ -1373,7 +1373,7 @@ static void ogl_filltexbuf(const palette_array_t &pal, const uint8_t *const data
 					case GL_RGBA:
 						(*(texp++)) = 255;
 						(*(texp++)) = 255;
-						//-fallthrough
+						DXX_BOOST_FALLTHROUGH;
 					case GL_LUMINANCE_ALPHA:
 						(*(texp++)) = 255;
 						(*(texp++)) = 0; // transparent pixel
@@ -1394,13 +1394,13 @@ static void ogl_filltexbuf(const palette_array_t &pal, const uint8_t *const data
 				{
 					case GL_RGBA:
 						(*(texp++))=0;
-						//-fallthrough
+						DXX_BOOST_FALLTHROUGH;
 					case GL_RGB:
 						(*(texp++))=0;
-						//-fallthrough
+						DXX_BOOST_FALLTHROUGH;
 					case GL_LUMINANCE_ALPHA:
 						(*(texp++))=0;
-						//-fallthrough
+						DXX_BOOST_FALLTHROUGH;
 					case GL_LUMINANCE:
 						(*(texp++))=0;//transparent pixel
 						break;
@@ -1420,7 +1420,7 @@ static void ogl_filltexbuf(const palette_array_t &pal, const uint8_t *const data
 				{
 					case GL_LUMINANCE_ALPHA:
 						(*(texp++))=255;
-						//-fallthrough
+						DXX_BOOST_FALLTHROUGH;
 					case GL_LUMINANCE://these could prolly be done to make the intensity based upon the intensity of the resulting color, but its not needed for anything (yet?) so no point. :)
 						(*(texp++))=255;
 						break;

similar/editor/med.cpp
@@ -1118,7 +1118,7 @@ window_event_result editor_handler(UI_DIALOG *, const d_event &event, unused_ui_
 			case EVENT_MOUSE_MOVED:
 				if (!keyd_pressed[ KEY_LCTRL ] && !keyd_pressed[ KEY_RCTRL ])
 					break;
-				//-fallthrough
+				DXX_BOOST_FALLTHROUGH;
 			case EVENT_JOYSTICK_BUTTON_UP:
 			case EVENT_JOYSTICK_BUTTON_DOWN:
 			case EVENT_JOYSTICK_MOVED:

similar/editor/medrobot.cpp
@@ -729,7 +729,7 @@ window_event_result robot_dialog_handler(UI_DIALOG *dlg,const d_event &event, ro
 				editor_status_fmt("Illegal contained object type (%i), changing to powerup.", Cur_goody_type);
 				Cur_goody_type = OBJ_POWERUP;
 				Cur_goody_id = 0;
-				/*-fallthrough*/
+				DXX_BOOST_FALLTHROUGH;
 			case OBJ_POWERUP:
 				type_text = "Powerup";
 				id_text = Powerup_names[Cur_goody_id].data();

commit 1131868f6198358392064886a2a074a2d1d09133

Fix -Wimplicit-fallthrough for non-Boost users
Users who do not install Boost cannot include <boost/config.hpp>.
SConstruct handled this by disabling the macro DXX_BOOST_FALLTHROUGH,
but did not override gcc's default-enabled (via -Wextra)
use of -Wimplicit-fallthrough=3, so users would get a fallthrough
warning and the build would fail.  Adjust this area to explicitly probe
for -Wno-implicit-fallthrough when Boost.Config is not available.

Fixes: 063bf292255b1cbe4b8cc6e2e14f5c5670e7cef0 ("Enable -Wimplicit-fallthrough=5; fix resulting breaks")
Reported-by: Jayman2000 <https://github.com/dxx-rebirth/dxx-rebirth/issues/417>

SConstruct
@@ -2583,33 +2583,46 @@ where the cast is useless.
 	(void)ts;
 	return 0;
 ''', msg='for struct timespec', successflags=_successflags)
+
 	@_implicit_test
 	def check_warn_implicit_fallthrough(self,context,text,main,testflags,_successflags={'CXXFLAGS' : ['-Wimplicit-fallthrough=5']}):
 		self.Compile(context, text=text, main=main, msg='for -Wimplicit-fallthrough=5', testflags=testflags, successflags=_successflags)
+
+	@_implicit_test
+	def check_no_warn_implicit_fallthrough(self,context,_successflags={'CXXFLAGS' : ['-Wno-implicit-fallthrough']}):
+		self.Compile(context, text='', main='', msg='for -Wno-implicit-fallthrough', successflags=_successflags)
+
 	@_custom_test
 	def check_boost_config(self,context,_successflags={'CPPDEFINES' : [('DXX_BOOST_FALLTHROUGH', 'BOOST_FALLTHROUGH')]}):
 		text ='''
 #include <boost/config.hpp>
 '''
 		main = '''
+	int a = 1;
 	switch (argc) {
 		case 1:
+			++ a;
 			DXX_BOOST_FALLTHROUGH;
 		case 2:
+			++ a;
 			break;
 	}
+	(void)a;
 '''
 		sconf = context.sconf
 		if not self.Compile(context, text=text, main=main, msg='for Boost.Config', successflags=_successflags):
 			sconf.Define('DXX_BOOST_FALLTHROUGH', '((void)0)')
+			self.check_no_warn_implicit_fallthrough(context)
 			return
 		else:
 			sconf.config_h_text += '''
 #ifndef DXX_SCONF_NO_INCLUDES
+/* For BOOST_FALLTHROUGH */
 #include <boost/config.hpp>
 #endif
 '''
 		self.check_warn_implicit_fallthrough(context, text, main, testflags=_successflags)
+
 	__preferred_compiler_options = (
 		'-fvisibility=hidden',
 		'-Wduplicated-branches',