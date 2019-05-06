dxx-rebirth/dxx-rebirth

commit 1131868f6198358392064886a2a074a2d1d09133

Fix -Wimplicit-fallthrough for non-Boost users
Users who do not install Boost cannot include <boost/config.hpp>.
SConstruct handled this by disabling the macro DXX_BOOST_FALLTHROUGH,
but did not override gcc's default-enabled (via -Wextra)
use of -Wimplicit-fallthrough=3, so users would get a fallthrough
warning and the build would fail.  Adjust this area to explicitly probe
for -Wno-implicit-fallthrough when Boost.Config is not available.

Fixes: 063bf292255b1cbe4b8cc6e2e14f5c5670e7cef0 ("Enable -Wimplicit-fallthrough=5; fix resulting breaks")
Reported-by: Jayman2000 <https://github.com/dxx-rebirth/dxx-rebirth/issues/417>

SConstruct
@@ -2583,33 +2583,46 @@ where the cast is useless.
 	(void)ts;
 	return 0;
 ''', msg='for struct timespec', successflags=_successflags)
+
 	@_implicit_test
 	def check_warn_implicit_fallthrough(self,context,text,main,testflags,_successflags={'CXXFLAGS' : ['-Wimplicit-fallthrough=5']}):
 		self.Compile(context, text=text, main=main, msg='for -Wimplicit-fallthrough=5', testflags=testflags, successflags=_successflags)
+
+	@_implicit_test
+	def check_no_warn_implicit_fallthrough(self,context,_successflags={'CXXFLAGS' : ['-Wno-implicit-fallthrough']}):
+		self.Compile(context, text='', main='', msg='for -Wno-implicit-fallthrough', successflags=_successflags)
+
 	@_custom_test
 	def check_boost_config(self,context,_successflags={'CPPDEFINES' : [('DXX_BOOST_FALLTHROUGH', 'BOOST_FALLTHROUGH')]}):
 		text ='''
 #include <boost/config.hpp>
 '''
 		main = '''
+	int a = 1;
 	switch (argc) {
 		case 1:
+			++ a;
 			DXX_BOOST_FALLTHROUGH;
 		case 2:
+			++ a;
 			break;
 	}
+	(void)a;
 '''
 		sconf = context.sconf
 		if not self.Compile(context, text=text, main=main, msg='for Boost.Config', successflags=_successflags):
 			sconf.Define('DXX_BOOST_FALLTHROUGH', '((void)0)')
+			self.check_no_warn_implicit_fallthrough(context)
 			return
 		else:
 			sconf.config_h_text += '''
 #ifndef DXX_SCONF_NO_INCLUDES
+/* For BOOST_FALLTHROUGH */
 #include <boost/config.hpp>
 #endif
 '''
 		self.check_warn_implicit_fallthrough(context, text, main, testflags=_successflags)
+
 	__preferred_compiler_options = (
 		'-fvisibility=hidden',
 		'-Wduplicated-branches',

commit 39303358c83a633ce0dc779719a50d0b1fa810b6

Fix subsequent tests after detecting Boost.Config
Fixes: 1131868f6198358392064886a2a074a2d1d09133 ("Fix -Wimplicit-fallthrough for non-Boost users")

SConstruct
@@ -2686,23 +2686,6 @@ unsigned u2(bool b)
 }
 
 }
-
-namespace test_fallthrough {
-
-int a(int i);
-int a(int i)
-{
-	switch (i) {
-		case 1:
-			DXX_BOOST_FALLTHROUGH;
-		case 2:
-			return i + 1;
-		default:
-			return 0;
-	}
-}
-
-}
 ''',
 		_mangle_compiler_option_name=__mangle_compiler_option_name,
 		_mangle_linker_option_name=__mangle_linker_option_name