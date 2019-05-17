dxx-rebirth/dxx-rebirth

commit 2243cd7f58836cc1be64f5df0a62fc4fe69e3776

Use xrange for loops with zero start and constant numerical end
s/for\s*(\s*\(\w\+\)\s\+\(\w\+\)\s*=\s*0\+u\?\s*;\s*\2\s*\(!=\|<\)\s*\([0-9]\+\)u\?\s*;\s*\(++\s*\2\|\2\s*++\s*\))/range_for (const \1 \2, xrange(\4u))/

common/misc/hmp.cpp
@@ -408,7 +408,7 @@ static void setup_tempo(hmp_file *hmp, unsigned long tempo) {
 void hmp_setvolume(hmp_file *hmp, int volume)
 {
 	if (hmp)
-		for (int channel = 0; channel < 16; channel++)
+		range_for (const int channel, xrange(16u))
 			midiOutShortMsg(reinterpret_cast<HMIDIOUT>(hmp->hmidi), static_cast<DWORD>(channel | MIDI_CONTROL_CHANGE << 4 | MIDI_VOLUME << 8 | (channel_volume[channel] * volume / MIDI_VOLUME_SCALE) << 16));
 
 	midi_volume = volume;
@@ -512,7 +512,7 @@ void hmp_reset()
 		return;
 	}
 
-	for (int channel = 0; channel < 16; channel++)
+	range_for (const int channel, xrange(16u))
 	{
 		midiOutShortMsg(hmidi, static_cast<DWORD>(channel | MIDI_CONTROL_CHANGE << 4 | MIDI_ALL_SOUNDS_OFF << 8 | 0 << 16));
 		midiOutShortMsg(hmidi, static_cast<DWORD>(channel | MIDI_CONTROL_CHANGE << 4 | MIDI_RESET_ALL_CONTROLLERS << 8 | 0 << 16));
@@ -590,7 +590,7 @@ void hmp_reset()
 		}
 	}
 
-	for (int channel = 0; channel < 16; channel++)
+	range_for (const int channel, xrange(16u))
 		midiOutShortMsg(hmidi, static_cast<DWORD>(channel | MIDI_CONTROL_CHANGE << 4 | MIDI_VOLUME << 8 | (100 * midi_volume / MIDI_VOLUME_SCALE) << 16));
 	midiOutClose(hmidi);
 }

common/arch/sdl/joy.cpp
@@ -27,6 +27,7 @@
 #include "compiler-cf_assert.h"
 #include "compiler-integer_sequence.h"
 #include "d_enumerate.h"
+#include "d_range.h"
 #include "partial_range.h"
 
 namespace dcx {
@@ -221,7 +222,7 @@ window_event_result joy_hat_handler(SDL_JoyHatEvent *jhe)
 	static_assert((SDL_HAT_UP | SDL_HAT_RIGHT | SDL_HAT_DOWN | SDL_HAT_LEFT) == 0xf, "unexpected hat mask");
 
 	//determine if a hat-button up or down event based on state and last_state
-	for (unsigned i = 0; i != 4; ++i)
+	range_for (const unsigned i, xrange(4u))
 	{
 		const auto current_button_state = !!(jhe_value & (1 << i));
 		auto &saved_button_state = Joystick.button_state[hat + i];

common/ui/dialog.cpp
@@ -199,7 +199,7 @@ void ui_close_dialog( UI_DIALOG * dlg )
 void restore_state()
 {
 	_disable();
-	for (int i=0; i<256; i++ )
+	range_for (const int i, xrange(256u))
 	{
 		keyd_pressed[i] = SavedState[i];
 	}

common/ui/file.cpp
@@ -34,6 +34,8 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "physfs_list.h"
 
 #include "compiler-make_unique.h"
+#include "compiler-range_for.h"
+#include "d_range.h"
 
 namespace dcx {
 
@@ -268,7 +270,7 @@ int ui_get_filename(char (&filename)[PATH_MAX], const char *const filespec, cons
 	}
 
 	//ui_messagebox( -2,-2, 1,"DEBUG:0", "Ok" );
-	for (int i=0; i<35; i++)
+	range_for (const int i, xrange(35u))
 		b->spaces[i] = ' ';
 	b->spaces[34] = 0;
 

common/ui/keypad.cpp
@@ -33,6 +33,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #include "compiler-make_unique.h"
 #include "compiler-range_for.h"
+#include "d_range.h"
 
 namespace dcx {
 
@@ -286,7 +287,7 @@ static void ui_pad_set_active( int n )
 	const char * name;
 	
 
-	for (int i=0; i<17; i++ )
+	range_for (const int i, xrange(17u))
 	{
 		Pad[i]->text = KeyPad[n]->buttontext[i].data();
 		Pad[i]->status = 1;

common/ui/keypress.cpp
@@ -29,6 +29,8 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "ui.h"
 #include "key.h"
 #include "window.h"
+#include "compiler-range_for.h"
+#include "d_range.h"
 
 namespace dcx {
 
@@ -78,7 +80,7 @@ int DecodeKeyText( const char * text )
 	if (strstr(text, "{Shift}") )
 		code |= KEY_SHIFTED;
 
-	for (int i=0; i<256; i++ )
+	range_for (const int i, xrange(256u))
 	{
 		if ( strlen(KeyDesc[i])>0 && strstr(text, KeyDesc[i]) )
 		{

d2x-rebirth/libmve/decoder8.cpp
@@ -15,6 +15,8 @@
 
 #include "dxxsconf.h"
 #include "compiler-array.h"
+#include "compiler-range_for.h"
+#include "d_range.h"
 
 static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, const unsigned char **pData, int *pDataRemain, int *curXb, int *curYb);
 
@@ -77,7 +79,7 @@ static void relFar(int i, int sign, int *x, int *y)
 static void copyFrame(uint8_t *pDest, const uint8_t *pSrc)
 {
 	const auto width = g_width;
-	for (int i=0; i<8; i++)
+	range_for (const int i, xrange(8u))
 	{
 		(void)i;
 		memcpy(pDest, pSrc, 8);
@@ -156,7 +158,7 @@ static void patternQuadrant4Pixels(unsigned char *pFrame, unsigned char pat0, un
 	unsigned long pat = (pat3 << 24) | (pat2 << 16) | (pat1 << 8) | pat0;
 
 	const auto width = g_width;
-	for (int i=0; i<16; i++)
+	range_for (const int i, xrange(16u))
 	{
 		pFrame[i&3] = p[(pat & mask) >> shift];
 
@@ -209,9 +211,9 @@ static void patternQuadrant2Pixels(unsigned char *pFrame, unsigned char pat0, un
 	unsigned short pat = (pat1 << 8) | pat0;
 
 	const auto width = g_width;
-	for (int i=0; i<4; i++)
+	range_for (const int i, xrange(4u))
 	{
-		for (int j=0; j<4; j++)
+		range_for (const int j, xrange(4u))
 		{
 			pel = p[(pat & mask) ? 1 : 0];
 
@@ -324,7 +326,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		*/
 		{
 			const auto width = g_width;
-		for (int i=0; i<2; i++)
+		range_for (const int i, xrange(2u))
 		{
 			(void)i;
 			*pFrame += 16;
@@ -410,7 +412,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 			const auto width = g_width;
 		if (p[0] <= p[1])
 		{
-			for (int i=0; i<8; i++)
+			range_for (const int i, xrange(8u))
 			{
 				(void)i;
 				patternRow2Pixels(*pFrame, *(*pData)++, p);
@@ -420,7 +422,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		else
 		{
 			const auto width2 = 2 * width;
-			for (int i=0; i<2; i++)
+			range_for (const int i, xrange(2u))
 			{
 				(void)i;
 				patternRow2Pixels2(*pFrame, *(*pData) & 0xf, p);
@@ -527,7 +529,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		if ( (*pData)[0] <= (*pData)[1])
 		{
 			// four quadrant case
-			for (int i=0; i<4; i++)
+			range_for (const int i, xrange(4u))
 			{
 				p[0] = *(*pData)++;
 				p[1] = *(*pData)++;
@@ -545,7 +547,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		else if ( (*pData)[6] <= (*pData)[7])
 		{
 			// split horizontal
-			for (int i=0; i<4; i++)
+			range_for (const int i, xrange(4u))
 			{
 				if ((i & 1) == 0)
 				{
@@ -565,7 +567,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		else
 		{
 			// split vertical
-			for (int i=0; i<8; i++)
+			range_for (const int i, xrange(8u))
 			{
 				if ((i & 3) == 0)
 				{
@@ -617,7 +619,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 				p[2] = *(*pData)++;
 				p[3] = *(*pData)++;
 
-				for (int i=0; i<8; i++)
+				range_for (const int i, xrange(8u))
 				{
 					(void)i;
 					pat[0] = *(*pData)++;
@@ -655,7 +657,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 				p[2] = *(*pData)++;
 				p[3] = *(*pData)++;
 
-				for (int i=0; i<8; i++)
+				range_for (const int i, xrange(8u))
 				{
 					(void)i;
 					pat[0] = *(*pData)++;
@@ -671,7 +673,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 				p[2] = *(*pData)++;
 				p[3] = *(*pData)++;
 
-				for (int i=0; i<4; i++)
+				range_for (const int i, xrange(4u))
 				{
 					(void)i;
 					pat[0] = *(*pData)++;
@@ -725,7 +727,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 			const auto width = g_width;
 		if ( (*pData)[0] <= (*pData)[1])
 		{
-			for (int i=0; i<4; i++)
+			range_for (const int i, xrange(4u))
 			{
 				p[0] = *(*pData)++;
 				p[1] = *(*pData)++;
@@ -749,7 +751,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 			if ( (*pData)[12] <= (*pData)[13])
 			{
 				// split vertical
-				for (int i=0; i<4; i++)
+				range_for (const int i, xrange(4u))
 				{
 					if ((i&1) == 0)
 					{
@@ -775,7 +777,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 			else
 			{
 				// split horizontal
-				for (int i=0; i<8; i++)
+				range_for (const int i, xrange(8u))
 				{
 					if ((i&3) == 0)
 					{
@@ -804,7 +806,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		*/
 		{
 			const auto width = g_width;
-		for (int i=0; i<8; i++)
+		range_for (const int i, xrange(8u))
 		{
 			(void)i;
 			memcpy(*pFrame, *pData, 8);
@@ -823,13 +825,13 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		*/
 		{
 			const auto width = g_width;
-		for (int i=0; i<4; i++)
+		range_for (const int i, xrange(4u))
 		{
 			(void)i;
-			for (int j=0; j<2; j++)
+			range_for (const int j, xrange(2u))
 			{
 				(void)j;
-				for (int k=0; k<4; k++)
+				range_for (const int k, xrange(4u))
 				{
 					(*pFrame)[2*k]   = (*pData)[k];
 					(*pFrame)[2*k+1] = (*pData)[k];
@@ -850,12 +852,12 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		*/
 		{
 			const auto width = g_width;
-		for (int i=0; i<2; i++)
+		range_for (const int i, xrange(2u))
 		{
 			(void)i;
-			for (int j=0; j<4; j++)
+			range_for (const int j, xrange(4u))
 			{
-				for (int k=0; k<4; k++)
+				range_for (const int k, xrange(4u))
 				{
 					(*pFrame)[k * width+j] = (*pData)[0];
 					(*pFrame)[k * width+j+4] = (*pData)[1];
@@ -875,7 +877,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		*/
 		{
 			const auto width = g_width;
-		for (int i=0; i<8; i++)
+		range_for (const int i, xrange(8u))
 		{
 			(void)i;
 			memset(*pFrame, **pData, 8);
@@ -901,9 +903,9 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 		*/
 		{
 			const auto width = g_width;
-		for (int i=0; i<8; i++)
+		range_for (const int i, xrange(8u))
 		{
-			for (int j=0; j<8; j++)
+			range_for (const int j, xrange(8u))
 			{
 				(*pFrame)[j] = (*pData)[(i+j)&1];
 			}

d2x-rebirth/main/escort.cpp
@@ -68,6 +68,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "segiter.h"
 #include "compiler-exchange.h"
 #include "compiler-range_for.h"
+#include "d_range.h"
 #include "partial_range.h"
 
 #if DXX_USE_EDITOR
@@ -1611,7 +1612,7 @@ static int attempt_to_steal_item_3(const vmobjptr_t objp, const vmobjptr_t playe
 			return r;
 
 	//	Makes it more likely to steal primary than secondary.
-	for (int i=0; i<2; i++)
+	range_for (const int i, xrange(2u))
 	{
 		(void)i;
 		if (auto r = maybe_steal_primary_weapon(player_num, Primary_weapon))
@@ -1681,7 +1682,7 @@ int attempt_to_steal_item(const vmobjptridx_t objp, const vmobjptr_t player_num)
 
 	rval += attempt_to_steal_item_2(objp, player_num);
 
-	for (int i=0; i<3; i++) {
+	range_for (const int i, xrange(3u)) {
 		(void)i;
 		if (!rval || (d_rand() < 11000)) {	//	about 1/3 of time, steal another item
 			rval += attempt_to_steal_item_2(objp, player_num);

commit 3a5e20363867f583bb0b1b4e60f3e31a884b36e0

Fix Windows build of hmp.cpp
Reported-by: zicodxx <private>
Fixes: 2243cd7f58836cc1be64f5df0a62fc4fe69e3776 ("Use xrange for loops with zero start and constant numerical end")

common/misc/hmp.cpp
@@ -26,6 +26,7 @@
 #include "dsx-ns.h"
 #include "compiler-make_unique.h"
 #include "compiler-range_for.h"
+#include "d_range.h"
 #include "partial_range.h"
 
 namespace dcx {