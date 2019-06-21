dxx-rebirth/dxx-rebirth

commit 9898d136f106b96b821cc6ba7da01b98faa64988

Fix fortification crash loading missions with long filenames
The highest-level tracking code assumed filenames would always fit in a
char[9].  This was true on DOS, but has not been true in Rebirth for
many years.  Builds without fortification caused silent memory
corruption in this case.

Refuse to create highest-level entries if they would cause corruption.
Log a diagnostic telling the user that this happened.

similar/main/playsave.cpp
@@ -825,12 +825,9 @@ static int write_player_dxx(const char *filename)
 	}
 	else
 		return errno;
-
-}
 }
 
 //read in the player's saved games.  returns errno (0 == no error)
-namespace dsx {
 int read_player_file()
 {
 	char filename[PATH_MAX];
@@ -1163,27 +1160,32 @@ int read_player_file()
 }
 }
 
-
-//finds entry for this level in table.  if not found, returns ptr to 
-//empty entry.  If no empty entries, takes over last one 
-static array<hli, MAX_MISSIONS>::iterator find_hli_entry()
+/* Given a Mission_path, return a pair of pointers.
+ * - If the mission cannot be saved, both pointers are nullptr.
+ * - If the mission name was previously used, return a pointer to that
+ *   entry and a pointer to end.
+ * - If the mission name is not recorded, return a pointer to the first
+ *   unused element (which may be end() if all elements are used) and a
+ *   pointer to end().  The caller must check that the first unused
+ *   element is not end().
+ */
+static std::array<array<hli, MAX_MISSIONS>::pointer, 2> find_hli_entry(const partial_range_t<hli *> &r, const Mission_path &m)
 {
-	auto r = partial_range(PlayerCfg.HighestLevels, PlayerCfg.NHighestLevels);
-	auto a = [](const hli &h) {
-		return !d_stricmp(h.Shortname, Current_mission_filename);
-	};
-	auto i = std::find_if(r.begin(), r.end(), a);
-	if (i == r.end())
-	{ //not found. create entry
-		if (i == PlayerCfg.HighestLevels.end())
-			i--; //take last entry
-		else
-			PlayerCfg.NHighestLevels++;
-
-		strcpy(i->Shortname, Current_mission_filename);
-		i->LevelNum = 0;
+	const auto mission_filename = m.filename;
+	const auto mission_length = std::distance(mission_filename, m.path.end());
+	if (mission_length >= sizeof(hli::Shortname))
+	{
+		/* Name is too long to store, so there will never be a match
+		 * and it is never stored.
+		 */
+		con_printf(CON_URGENT, DXX_STRINGIZE_FL(__FILE__, __LINE__, "warning: highest level information cannot be tracked because the mission name is too long to store (should be at most 8 bytes, but is \"%s\")."), &*mission_filename);
+		return {{nullptr, nullptr}};
 	}
-	return i;
+	const auto &&a = [p = &*mission_filename](const hli &h) {
+		return !d_stricmp(h.Shortname.data(), p);
+	};
+	const auto i = std::find_if(r.begin(), r.end(), a);
+	return {{&*i, r.end()}};
 }
 
 //set a new highest level for player for this mission
@@ -1195,11 +1197,80 @@ void set_highest_level(int levelnum)
 		if (ret != ENOENT)		//if file doesn't exist, that's ok
 			return;
 
-	auto i = find_hli_entry();
+	const auto &&r = partial_range(PlayerCfg.HighestLevels, PlayerCfg.NHighestLevels);
+	const auto &&i = find_hli_entry(r, *Current_mission);
+	const auto ie = i[1];
+	if (!ie)
+		return;
 
-	if (levelnum > i->LevelNum)
+	auto ii = i[0];
+	const hli *d1_preferred = nullptr;
+#if defined(DXX_BUILD_DESCENT_II)
+	const hli *d2_preferred = nullptr, *d2x_preferred = nullptr;
+#endif
+	range_for (auto &m, r)
 	{
-		i->LevelNum = levelnum;
+		hli *preferred;
+		/* Swapping instead of rotating will perturb the order a bit,
+		 * but this is a one-time fix to get builtin missions to
+		 * reserved storage.
+		 */
+		const auto ms = m.Shortname.data();
+		if (!d1_preferred && !strcmp("descent", ms))
+			d1_preferred = preferred = &PlayerCfg.HighestLevels[0];
+#if defined(DXX_BUILD_DESCENT_II)
+		else if (!d2_preferred && !strcmp("d2", ms))
+			d2_preferred = preferred = &PlayerCfg.HighestLevels[1];
+		else if (!d2x_preferred && !strcmp("d2x", ms))
+			d2x_preferred = preferred = &PlayerCfg.HighestLevels[2];
+#endif
+		else
+			continue;
+		if (preferred == &m)
+			continue;
+		std::swap(*preferred, m);
+	}
+	const unsigned reserved_slots = !!d1_preferred
+#if defined(DXX_BUILD_DESCENT_II)
+		+ !!d2_preferred + !!d2x_preferred
+#endif
+		;
+	auto &hl = PlayerCfg.HighestLevels;
+	const auto irs = &hl[reserved_slots];
+
+	uint8_t previous_best_levelnum = 0;
+	if (ii == ie)
+	{
+		/*
+		 * If ii == ie, the mission is unknown.  If there is no free
+		 * space available, move everything, so that the
+		 * least-recently-used element (at *irs) is discarded to make
+		 * room to add this mission as most recently used.
+		 */
+		if (ie == PlayerCfg.HighestLevels.end())
+		{
+			std::move(std::next(irs), ie, irs);
+			--ii;
+		}
+		else
+			PlayerCfg.NHighestLevels++;
+		ii->Shortname.back() = 0;
+		strncpy(ii->Shortname.data(), Current_mission_filename, ii->Shortname.size() - 1);
+	}
+	else if (ii != ie - 1)
+	{
+		/* If this mission is not the most recently used, reorder the
+		 * list so that it becomes the most recently used.
+		 */
+		std::rotate(ii, std::next(ii), ie);
+		ii = ie - 1;
+	}
+	else
+		previous_best_levelnum = ii->LevelNum;
+
+	if (previous_best_levelnum < levelnum)
+	{
+		ii->LevelNum = levelnum;
 		write_player_file();
 	}
 }
@@ -1207,18 +1278,16 @@ void set_highest_level(int levelnum)
 //gets the player's highest level from the file for this mission
 int get_highest_level(void)
 {
-	int i;
-	int highest_saturn_level = 0;
 	read_player_file();
-	if (*Current_mission->filename == 0)	{
-		for (i=0;i < PlayerCfg.NHighestLevels;i++)
-			if (!d_stricmp(PlayerCfg.HighestLevels[i].Shortname, "DESTSAT")) // Destination Saturn.
-				highest_saturn_level = PlayerCfg.HighestLevels[i].LevelNum;
-	}
-	i = find_hli_entry()->LevelNum;
-	if ( highest_saturn_level > i )
-		i = highest_saturn_level;
-	return i;
+	const Mission_path &mission = *Current_mission;
+	// Destination Saturn.
+	const auto &&r = partial_range(PlayerCfg.HighestLevels, PlayerCfg.NHighestLevels);
+	const auto &&i = find_hli_entry(r, *mission.filename == 0 ? Mission_path{"DESTSAT", 0} : mission);
+	const auto ie = i[1];
+	const auto ii = i[0];
+	if (ii == ie)
+		return 0;
+	return ii->LevelNum;
 }
 
 //write out player's saved games.  returns errno (0 == no error)

common/main/playsave.h
@@ -70,7 +70,7 @@ void plyr_save_stats();
 
 struct hli
 {
-	char	Shortname[9];
+	array<char, 9> Shortname;
 	uint8_t LevelNum;
 };
 

similar/main/menu.cpp
@@ -848,9 +848,9 @@ window_event_result do_new_game_menu()
 	int new_level_num,player_highest_level;
 
 	new_level_num = 1;
-#ifdef NDEBUG
 	player_highest_level = get_highest_level();
 
+#ifdef NDEBUG
 	if (player_highest_level > Last_level)
 #endif
 		player_highest_level = Last_level;

commit 0105777b43c3141f7b96e71b32324a00cbf5d370

Fix highest_level tracking for D1X-Rebirth with First Strike
Commit 9898d13 fixed a crash on long filenames, but also tried to
optimize the Destination Saturn hack.  However, this optimization was
implemented incorrectly, and caused the game to read only the
Destination Saturn highest_level entry when playing the First Strike
campaign, rather than using the greater level of Destination Saturn or
First Strike.

Restore the old logic, but restrict it to D1X.  D2X never used an empty
string for the First Strike campaign, so this logic could not trigger
there, which is also how the bug was missed in initial testing.

Reported-by: zicodxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/428>
Fixes: 9898d136f106b96b821cc6ba7da01b98faa64988 ("Fix fortification crash loading missions with long filenames")

similar/main/playsave.cpp
@@ -1217,10 +1217,10 @@ void set_highest_level(int levelnum)
 		 * reserved storage.
 		 */
 		const auto ms = m.Shortname.data();
-		if (!d1_preferred && !strcmp("descent", ms))
+		if (!d1_preferred && !strcmp(D1_MISSION_FILENAME, ms))
 			d1_preferred = preferred = &PlayerCfg.HighestLevels[0];
 #if defined(DXX_BUILD_DESCENT_II)
-		else if (!d2_preferred && !strcmp("d2", ms))
+		else if (!d2_preferred && !strcmp(FULL_MISSION_FILENAME, ms))
 			d2_preferred = preferred = &PlayerCfg.HighestLevels[1];
 		else if (!d2x_preferred && !strcmp("d2x", ms))
 			d2x_preferred = preferred = &PlayerCfg.HighestLevels[2];
@@ -1283,12 +1283,32 @@ int get_highest_level(void)
 	const Mission_path &mission = *Current_mission;
 	// Destination Saturn.
 	const auto &&r = partial_range(PlayerCfg.HighestLevels, PlayerCfg.NHighestLevels);
-	const auto &&i = find_hli_entry(r, *mission.filename == 0 ? Mission_path{"DESTSAT", 0} : mission);
+#if defined(DXX_BUILD_DESCENT_I)
+	unsigned highest_saturn_level = 0;
+	if (!*mission.filename)
+		range_for (auto &m, r)
+			if (!d_stricmp("DESTSAT", m.Shortname.data()))
+				highest_saturn_level = m.LevelNum;
+#endif
+	const auto &&i = find_hli_entry(r, mission);
 	const auto ie = i[1];
 	const auto ii = i[0];
 	if (ii == ie)
 		return 0;
-	return ii->LevelNum;
+	auto LevelNum = ii->LevelNum;
+#if defined(DXX_BUILD_DESCENT_I)
+	/* Override `LevelNum` of the full campaign with the player's
+	 * progress in the OEM mini-campaign, so that users who switch can
+	 * keep their progress.
+	 *
+	 * If the player never played Destination Saturn, or if this is not
+	 * the built-in campaign, highest_saturn_level will be 0 and this
+	 * will be skipped.
+	 */
+	if (LevelNum < highest_saturn_level)
+		LevelNum = highest_saturn_level;
+#endif
+	return LevelNum;
 }
 
 //write out player's saved games.  returns errno (0 == no error)