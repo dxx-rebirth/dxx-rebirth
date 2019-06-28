dxx-rebirth/dxx-rebirth

commit 644d6fa51350c0b2a407655b871f30b5b1f54a1a

Simplify do_countdown_frame ship rocking effect

similar/main/cntrlcen.cpp
@@ -182,7 +182,6 @@ window_event_result do_controlcen_dead_frame()
 window_event_result do_countdown_frame()
 {
 	fix	old_time;
-	int	fc, div_scale;
 
 	if (!Control_center_destroyed)	return window_event_result::ignored;
 
@@ -201,19 +200,21 @@ window_event_result do_countdown_frame()
 #endif
 
 	//	Control center destroyed, rock the player's ship.
-	fc = Countdown_seconds_left;
-	if (fc > 16)
-		fc = 16;
-
-	//	At Trainee, decrease rocking of ship by 4x.
-	div_scale = 1;
-	if (Difficulty_level == 0)
-		div_scale = 4;
-
 	if (d_tick_step)
 	{
-		ConsoleObject->mtype.phys_info.rotvel.x += (fixmul(d_rand() - 16384, 3*F1_0/16 + (F1_0*(16-fc))/32))/div_scale;
-		ConsoleObject->mtype.phys_info.rotvel.z += (fixmul(d_rand() - 16384, 3*F1_0/16 + (F1_0*(16-fc))/32))/div_scale;
+		auto &rotvel = ConsoleObject->mtype.phys_info.rotvel;
+		const auto get_base_disturbance = [fc = std::max(Countdown_seconds_left, 16)]() {
+			return fixmul(d_rand() - 16384, 3 * F1_0 / 16 + (F1_0 * (16 - fc)) / 32);
+		};
+		fix disturb_x = get_base_disturbance(), disturb_z = get_base_disturbance();
+		//	At Trainee, decrease rocking of ship by 4x.
+		if (Difficulty_level == Difficulty_0)
+		{
+			disturb_x /= 4;
+			disturb_z /= 4;
+		}
+		rotvel.x += disturb_x;
+		rotvel.z += disturb_z;
 	}
 	//	Hook in the rumble sound effect here.
 

commit 75de1c23c1d65283c0f1a8fd7e2c04d3facb10f5

Reduce ship rocking during countdown
A prior change switched to using the greater, rather than the lesser, of
`16` or `Countdown_seconds_left`, which increased the rotation from the
ship rocking effect.  Switch back to use the lesser value.

Reported-by: zicodxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/431>
Fixes: 644d6fa51350c0b2a407655b871f30b5b1f54a1a ("Simplify do_countdown_frame ship rocking effect")

similar/main/cntrlcen.cpp
@@ -203,7 +203,7 @@ window_event_result do_countdown_frame()
 	if (d_tick_step)
 	{
 		auto &rotvel = ConsoleObject->mtype.phys_info.rotvel;
-		const auto get_base_disturbance = [fc = std::max(Countdown_seconds_left, 16)]() {
+		const auto get_base_disturbance = [fc = std::min(Countdown_seconds_left, 16)]() {
 			return fixmul(d_rand() - 16384, 3 * F1_0 / 16 + (F1_0 * (16 - fc)) / 32);
 		};
 		fix disturb_x = get_base_disturbance(), disturb_z = get_base_disturbance();