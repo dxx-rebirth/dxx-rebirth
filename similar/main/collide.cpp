dxx-rebirth/dxx-rebirth

commit 829e95b6f818f82c1f90502f8a3a7002479b3802

Separate hoard/proximity tracking

similar/main/collide.cpp
@@ -1927,11 +1927,7 @@ void drop_player_eggs(const vobjptridx_t playerobj)
 		drop_armed_bomb(secondary_ammo[SMART_MINE_INDEX], weapon_id_type::SUPERPROX_ID);
 
 		//	If the player had proximity bombs, maybe arm one of them.
-
-		if ((Game_mode & GM_MULTI) && !game_mode_hoard())
-		{
-			drop_armed_bomb(secondary_ammo[PROXIMITY_INDEX], weapon_id_type::PROXIMITY_ID);
-		}
+		drop_armed_bomb(secondary_ammo[PROXIMITY_INDEX], weapon_id_type::PROXIMITY_ID);
 #endif
 
 		//	If the player dies and he has powerful lasers, create the powerups here.
@@ -1978,8 +1974,7 @@ void drop_player_eggs(const vobjptridx_t playerobj)
 		if (game_mode_hoard())
 		{
 			// Drop hoard orbs
-			unsigned max_count = min(secondary_ammo[PROXIMITY_INDEX], static_cast<uint8_t>(12));
-			for (int i=0; i<max_count; i++)
+			for (unsigned max_count = std::min<uint8_t>(player_info.hoard.orbs, player_info.max_hoard_orbs); max_count--;)
 				call_object_create_egg(playerobj, 1, POW_HOARD_ORB);
 		}
 #endif
@@ -2021,10 +2016,7 @@ void drop_player_eggs(const vobjptridx_t playerobj)
 		//	Drop the secondary weapons
 		//	Note, proximity weapon only comes in packets of 4.  So drop n/2, but a max of 3 (handled inside maybe_drop..)  Make sense?
 
-#if defined(DXX_BUILD_DESCENT_II)
-		if (!game_mode_hoard())
-#endif
-			maybe_drop_secondary_weapon_egg(playerobj, PROXIMITY_INDEX, (secondary_ammo[PROXIMITY_INDEX])/4);
+		maybe_drop_secondary_weapon_egg(playerobj, PROXIMITY_INDEX, secondary_ammo[PROXIMITY_INDEX] / 4);
 
 		maybe_drop_secondary_weapon_egg(playerobj, SMART_INDEX, secondary_ammo[SMART_INDEX]);
 		maybe_drop_secondary_weapon_egg(playerobj, MEGA_INDEX, secondary_ammo[MEGA_INDEX]);

common/main/multi.h
@@ -547,7 +547,7 @@ struct marker_message_text_t;
 void multi_send_drop_marker (int player,const vms_vector &position,char messagenum,const marker_message_text_t &text);
 void multi_send_markers();
 void multi_send_guided_info (vobjptr_t miss,char);
-void multi_send_orb_bonus(playernum_t pnum);
+void multi_send_orb_bonus(playernum_t pnum, uint8_t);
 void multi_send_got_orb(playernum_t pnum);
 void multi_send_effect_blowup(vcsegidx_t segnum, unsigned side, const vms_vector &pnt);
 void multi_send_vulcan_weapon_ammo_adjust(const vobjptridx_t objnum);

common/main/object.h
@@ -136,6 +136,13 @@ struct player_info
 			uint16_t hostages_rescued_total; // Total number of hostages rescued.
 			uint8_t hostages_on_board;
 		} mission;
+		struct {
+			uint8_t orbs;
+		} hoard;
+	};
+	enum
+	{
+		max_hoard_orbs = 12,
 	};
 	fix64   cloak_time;             // Time cloaked
 	fix64   invulnerable_time;      // Time invulnerable

similar/main/fuelcen.cpp
@@ -785,11 +785,10 @@ void fuelcen_check_for_hoard_goal(const vcsegptr_t segp)
 	if (segp->special==SEGMENT_IS_GOAL_BLUE || segp->special==SEGMENT_IS_GOAL_RED  )	
 	{
 		auto &player_info = get_local_plrobj().ctype.player_info;
-		if (auto &proximity = player_info.secondary_ammo[PROXIMITY_INDEX])
+		if (auto &hoard_orbs = player_info.hoard.orbs)
 		{
-				multi_send_orb_bonus (Player_num);
 				player_info.powerup_flags &= ~PLAYER_FLAGS_FLAG;
-				proximity = 0;
+			multi_send_orb_bonus(Player_num, exchange(hoard_orbs, 0));
       }
 	}
 

similar/main/gamecntl.cpp
@@ -203,12 +203,6 @@ int which_bomb()
 {
 	//use the last one selected, unless there aren't any, in which case use
 	//the other if there are any
-
-
-   // If hoard game, only let the player drop smart mines
-	if (game_mode_hoard())
-		return SMART_MINE_INDEX;
-
 	auto &player_info = get_local_plrobj().ctype.player_info;
 	auto &Secondary_last_was_super = player_info.Secondary_last_was_super;
 	const auto mask = 1 << PROXIMITY_INDEX;

similar/main/gauges.cpp
@@ -944,7 +944,7 @@ static void hud_show_orbs(grs_canvas &canvas, const player_info &player_info, co
 		gr_set_fontcolor(canvas, BM_XRGB(0, 31, 0), -1);
 		auto &bm = Orb_icons[multires_gauge_graphic.is_hires()];
 		hud_bitblt_free(canvas, x, y, HUD_SCALE_Y_AR(multires_gauge_graphic, bm.bm_w), HUD_SCALE_Y_AR(multires_gauge_graphic, bm.bm_h), bm);
-		gr_printf(canvas, x + HUD_SCALE_X_AR(multires_gauge_graphic, bm.bm_w), y, " x %d", player_info.secondary_ammo[PROXIMITY_INDEX]);
+		gr_printf(canvas, x + HUD_SCALE_X_AR(multires_gauge_graphic, bm.bm_w), y, " x %d", player_info.hoard.orbs);
 	}
 }
 

similar/main/lighting.cpp
@@ -274,12 +274,10 @@ static g3s_lrgb compute_light_emission(const vobjptridx_t obj)
 					Headlights[Num_headlights++] = obj;
 				light_intensity = HEADLIGHT_SCALE;
 			}
-			else if (game_mode_hoard() && obj->ctype.player_info.secondary_ammo[PROXIMITY_INDEX]) // If hoard game and player, add extra light based on how many orbs you have Pulse as well.
+			else if (const auto hoard_orbs = obj->ctype.player_info.hoard.orbs) // If hoard game and player, add extra light based on how many orbs you have Pulse as well.
 			{
-				fix hoardlight;
-				hoardlight=i2f(obj->ctype.player_info.secondary_ammo[PROXIMITY_INDEX])/2; //i2f(12));
-				hoardlight++;
-				auto s = fix_sin(static_cast<fix>(GameTime64/2) & 0xFFFF); // probably a bad way to do it
+				fix hoardlight = 1 + (i2f(hoard_orbs) / 2);
+				auto s = fix_sin(static_cast<fix>(GameTime64 >> 1) & 0xFFFF); // probably a bad way to do it
 				s+=F1_0; 
 				s>>=1;
 				hoardlight=fixmul (s,hoardlight);

similar/main/multi.cpp
@@ -4063,13 +4063,12 @@ void multi_send_capture_bonus (const playernum_t pnum)
 	multi_do_capture_bonus (pnum);
 }
 
-void multi_send_orb_bonus (const playernum_t pnum)
+void multi_send_orb_bonus (const playernum_t pnum, const uint8_t hoard_orbs)
 {
 	Assert (game_mode_hoard());
 
 	multibuf[1]=pnum;
-	auto &player_info = get_local_plrobj().ctype.player_info;
-	multibuf[2] = player_info.secondary_ammo[PROXIMITY_INDEX];
+	multibuf[2] = hoard_orbs;
 
 	multi_send_data<MULTI_ORB_BONUS>(multibuf,3,2);
 	multi_do_orb_bonus (pnum, multibuf);
@@ -4256,7 +4255,7 @@ static void DropOrb ()
 		Int3(); // How did we get here? Get Leighton!
 
 	auto &player_info = get_local_plrobj().ctype.player_info;
-	auto &proximity = player_info.secondary_ammo[PROXIMITY_INDEX];
+	auto &proximity = player_info.hoard.orbs;
 	if (!proximity)
 	{
 		HUD_init_message_literal(HM_MULTI, "No orbs to drop!");
@@ -4273,9 +4272,7 @@ static void DropOrb ()
 	HUD_init_message_literal(HM_MULTI, "Orb dropped!");
 	digi_play_sample (SOUND_DROP_WEAPON,F1_0);
 
-	if (game_mode_hoard())
-		multi_send_drop_flag(objnum,seed);
-
+	multi_send_drop_flag(objnum, seed);
 	-- proximity;
 
 	// If empty, tell everyone to stop drawing the box around me
@@ -4423,11 +4420,6 @@ uint_fast32_t multi_powerup_is_allowed(const unsigned id, const unsigned BaseAll
 		case POW_HOMING_AMMO_4:
 			return AllowedItems & NETFLAG_DOHOMING;
 		case POW_PROXIMITY_WEAPON:
-			// Special: Make all proximity bombs into shields if in
-			// hoard mode because we use the proximity slot in the
-			// player struct to signify how many orbs the player has.
-			if (game_mode_hoard())
-				return 0;
 			return AllowedItems & NETFLAG_DOPROXIM;
 		case POW_SMARTBOMB_WEAPON:
 			return AllowedItems & NETFLAG_DOSMART;
@@ -5167,10 +5159,7 @@ static void MultiLevelInv_CountPlayerInventory()
 		Current[POW_VULCAN_AMMO] += player_info.vulcan_ammo;
 		Current[POW_MISSILE_1] += player_info.secondary_ammo[CONCUSSION_INDEX];
 		Current[POW_HOMING_AMMO_1] += player_info.secondary_ammo[HOMING_INDEX];
-#if defined(DXX_BUILD_DESCENT_II)
-                        if (!(Game_mode & GM_HOARD))
-#endif
-			Current[POW_PROXIMITY_WEAPON] += player_info.secondary_ammo[PROXIMITY_INDEX];
+		Current[POW_PROXIMITY_WEAPON] += player_info.secondary_ammo[PROXIMITY_INDEX];
 		Current[POW_SMARTBOMB_WEAPON] += player_info.secondary_ammo[SMART_INDEX];
 		Current[POW_MEGA_WEAPON] += player_info.secondary_ammo[MEGA_INDEX];
 #if defined(DXX_BUILD_DESCENT_II)

commit 7bd861e3749369a8acc405b25f24ab80cfac1521

Only death-spew armed proximity bombs in multiplayer
Commit 829e95b6 meant to remove the restriction that the game not be
hoard mode, but incorrectly also removed the restriction that the game
be multiplayer.  Restore the multiplayer restriction.

Reported-by: zicodxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/433>
Fixes: 829e95b6f818f82c1f90502f8a3a7002479b3802 ("Separate hoard/proximity tracking")

similar/main/collide.cpp
@@ -1956,7 +1956,8 @@ void drop_player_eggs(const vmobjptridx_t playerobj)
 		drop_armed_bomb(secondary_ammo[SMART_MINE_INDEX], weapon_id_type::SUPERPROX_ID);
 
 		//	If the player had proximity bombs, maybe arm one of them.
-		drop_armed_bomb(secondary_ammo[PROXIMITY_INDEX], weapon_id_type::PROXIMITY_ID);
+		if (Game_mode & GM_MULTI)
+			drop_armed_bomb(secondary_ammo[PROXIMITY_INDEX], weapon_id_type::PROXIMITY_ID);
 #endif
 
 		//	If the player dies and he has powerful lasers, create the powerups here.