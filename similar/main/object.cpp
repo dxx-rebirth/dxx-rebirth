dxx-rebirth/dxx-rebirth

commit bfeca84bb86ab75365f18d60c17e18f36dbfaf1d

Change object::signature to be a generation counter
This removes the need to walk all objects when creating a new one, since
each object can have a private generation counter, unaware of other
objects.  For compatibility with demos, mix in the object's index when
writing the signature value.

similar/main/object.cpp
@@ -817,7 +817,7 @@ void init_player_object()
 	const auto &&console = vmobjptr(ConsoleObject);
 	console->type = OBJ_PLAYER;
 	set_player_id(console, 0);					//no sub-types for player
-	console->signature = object_signature_t{0};			//player has zero, others start at 1
+	console->signature = object_signature_t{0};
 	auto &Polygon_models = LevelSharedPolygonModelState.Polygon_models;
 	console->size = Polygon_models[Player_ship->model_num].rad;
 	console->control_type = CT_SLEW;			//default is player slewing
@@ -924,30 +924,6 @@ void obj_unlink(fvmobjptr &vmobjptr, fvmsegptr &vmsegptr, object_base &obj)
 		vmobjptr(next)->prev = obj.prev;
 	DXX_POISON_VAR(obj.next, 0xfa);
 	DXX_POISON_VAR(obj.prev, 0xfa);
-}
-
-// Returns a new, unique signature for a new object
-object_signature_t obj_get_signature()
-{
-	auto &Objects = LevelUniqueObjectState.Objects;
-	auto &vcobjptr = Objects.vcptr;
-	static short sig = 0; // Yes! Short! a) We do not need higher values b) the demo system only stores shorts
-	uint_fast32_t lsig = sig;
-	for (const auto &&b = vcobjptr.begin(), &&e = vcobjptr.end();;)
-	{
-		if (unlikely(lsig == std::numeric_limits<decltype(sig)>::max()))
-			lsig = 0;
-		++ lsig;
-		const auto predicate = [lsig](const object_base &o) {
-			if (o.type == OBJ_NONE)
-				return false;
-			return o.signature.get() == lsig;
-		};
-		if (std::any_of(b, e, predicate))
-			continue;
-		sig = static_cast<int16_t>(lsig);
-		return object_signature_t{static_cast<uint16_t>(lsig)};
-	}
 }
 
 //returns the number of a free object, updating Highest_object_index.
@@ -1119,14 +1095,19 @@ imobjptridx_t obj_create(const object_type_t type, const unsigned id, vmsegptrid
 	if (obj == object_none)		//no free objects
 		return object_none;
 
-	auto signature = obj_get_signature();
 	// Zero out object structure to keep weird bugs from happening
 	// in uninitialized fields.
+	const auto signature = obj->signature;
+	/* Test the version in the object structure, not the local copy.
+	 * This produces a more useful diagnostic from Valgrind if the
+	 * test reports a problem.
+	 */
+	DXX_CHECK_VAR_IS_DEFINED(obj->signature);
 	*obj = {};
 	// Tell Valgrind to warn on any uninitialized fields.
 	DXX_POISON_VAR(*obj, 0xfd);
 
-	obj->signature				= signature;
+	obj->signature = object_signature_t(signature.get() + 1);
 	obj->type 				= type;
 	obj->id 				= id;
 	obj->last_pos				= pos;
@@ -1209,11 +1190,11 @@ imobjptridx_t obj_create_copy(const object &srcobj, const vmsegptridx_t newsegnu
 	if (obj == object_none)
 		return object_none;
 
+	const auto signature = obj->signature;
 	*obj = srcobj;
 
 	obj_link_unchecked(Objects.vmptr, obj, newsegnum);
-
-	obj->signature				= obj_get_signature();
+	obj->signature = object_signature_t(signature.get() + 1);
 
 	//we probably should initialize sub-structures here
 
@@ -1261,8 +1242,15 @@ void obj_delete(d_level_unique_object_state &LevelUniqueObjectState, segment_arr
 	if (obj->movement_type == MT_PHYSICS && (obj->mtype.phys_info.flags & PF_STICK))
 		LevelUniqueStuckObjectState.remove_stuck_object(obj);
 	obj_unlink(Objects.vmptr, Segments.vmptr, obj);
+	const auto signature = obj->signature;
 	DXX_POISON_VAR(*obj, 0xfa);
 	obj->type = OBJ_NONE;		//unused!
+	/* Preserve signature across the poison value.  When the object slot
+	 * is reused, the allocator will need the old signature so that the
+	 * new one can be derived from it.  No other sites should read it
+	 * until that happens.
+	 */
+	obj->signature = signature;
 	obj_free(LevelUniqueObjectState, obj);
 }
 
@@ -1278,6 +1266,14 @@ static int Player_flags_save;
 int		Death_sequence_aborted=0;
 static fix Camera_to_player_dist_goal = F1_0*4;
 static uint8_t Control_type_save, Render_type_save;
+
+unsigned laser_parent_is_matching_signature(const laser_parent &l, const object_base &o)
+{
+	if (l.parent_type != o.type)
+		return 0;
+	return l.parent_signature == o.signature;
+}
+
 }
 
 namespace dsx {
@@ -2084,6 +2080,7 @@ void reset_objects(d_level_unique_object_state &LevelUniqueObjectState, const un
 		auto &obj = *Objects.vmptr(i);
 		DXX_POISON_VAR(obj, 0xfd);
 		obj.type = OBJ_NONE;
+		obj.signature = object_signature_t{0};
 	}
 }
 
@@ -2108,6 +2105,36 @@ int update_object_seg(fvmobjptr &vmobjptr, const d_level_shared_segment_state &L
 		obj_relink(vmobjptr, LevelUniqueSegmentState.get_segments().vmptr, obj, newseg);
 
 	return 1;
+}
+
+unsigned laser_parent_is_player(fvcobjptr &vcobjptr, const laser_parent &l, const object_base &o)
+{
+	/* Player objects are never recycled, so skip the signature check.
+	 */
+	if (l.parent_type != o.type)
+		return 0;
+	auto &parent_object = *vcobjptr(l.parent_num);
+	return (&parent_object == &o);
+}
+
+unsigned laser_parent_is_object(fvcobjptr &vcobjptr, const laser_parent &l, const object_base &o)
+{
+	auto &parent_object = *vcobjptr(l.parent_num);
+	if (&parent_object != &o)
+		return 0;
+	return laser_parent_is_matching_signature(l, o);
+}
+
+unsigned laser_parent_is_object(const laser_parent &l, const vcobjptridx_t o)
+{
+	if (l.parent_num != o.get_unchecked_index())
+		return 0;
+	return laser_parent_is_matching_signature(l, *o);
+}
+
+unsigned laser_parent_object_exists(fvcobjptr &vcobjptr, const laser_parent &l)
+{
+	return laser_parent_is_matching_signature(l, *vcobjptr(l.parent_num));
 }
 
 void set_powerup_id(const d_powerup_info_array &Powerup_info, const d_vclip_array &Vclip, object_base &o, powerup_type_t id)

common/include/compiler-poison.h
@@ -19,6 +19,15 @@ static inline void DXX_MAKE_MEM_UNDEFINED(T *b, unsigned long l)
 #endif
 }
 
+template <typename T>
+static inline void DXX_CHECK_MEM_IS_DEFINED(const T *const b, unsigned long l)
+{
+	(void)b;(void)l;
+#if DXX_HAVE_POISON_VALGRIND
+	VALGRIND_CHECK_MEM_IS_DEFINED(b, l);
+#endif
+}
+
 /* Convenience function to invoke
  * `DXX_MAKE_MEM_UNDEFINED(T *, unsigned long)`
  */
@@ -27,6 +36,17 @@ static inline void DXX_MAKE_MEM_UNDEFINED(T *b, T *e)
 {
 	unsigned char *bc = reinterpret_cast<unsigned char *>(b);
 	DXX_MAKE_MEM_UNDEFINED(bc, reinterpret_cast<unsigned char *>(e) - bc);
+}
+
+/* Convenience function to invoke
+ * `DXX_CHECK_MEM_IS_DEFINED(T *, unsigned long)` for exactly one instance
+ * of `T`.
+ */
+template <typename T>
+static inline void DXX_CHECK_VAR_IS_DEFINED(const T &b)
+{
+	const unsigned char *const bc = reinterpret_cast<const unsigned char *>(&b);
+	DXX_CHECK_MEM_IS_DEFINED(bc, sizeof(T));
 }
 
 /* Convenience function to invoke

common/main/fwd-object.h
@@ -297,9 +297,6 @@ void extract_quaternionpos(vmobjptridx_t objp, quaternionpos &qpp);
 // between levels if clear_all is set, clear even proximity bombs
 void clear_transient_objects(int clear_all);
 
-// Returns a new, unique signature for a new object
-object_signature_t obj_get_signature();
-
 // returns the number of a free object, updating Highest_object_index.
 // Generally, obj_create() should be called to get an object, since it
 // fills in important fields and does the linking.  returns -1 if no

common/main/newdemo.h
@@ -94,7 +94,7 @@ extern void newdemo_record_sound_3d_once( int soundno, int angle, int volume );
 extern void newdemo_record_sound_once( int soundno );
 extern void newdemo_record_sound( int soundno );
 void newdemo_record_wall_hit_process( segnum_t segnum, int side, int damage, int playernum );
-extern void newdemo_record_morph_frame(struct morph_data *);
+void newdemo_record_morph_frame(vcobjptridx_t);
 extern void newdemo_record_player_stats(int shields, int energy, int score );
 void newdemo_record_wall_toggle(segnum_t segnum, int side );
 extern void newdemo_record_control_center_destroyed();

common/main/object.h
@@ -215,7 +215,7 @@ struct laser_parent
 {
 	int16_t parent_type = {};        // The type of the parent of this object
 	objnum_t parent_num = {};         // The object's parent's number
-	object_signature_t parent_signature = {};   // The object's parent's signature...
+	object_signature_t parent_signature = object_signature_t{0};   // The object's parent's signature...
 };
 
 struct laser_info : prohibit_void_ptr<laser_info>, laser_parent
@@ -563,6 +563,8 @@ struct object_number_array : array<T, N>
 	}
 };
 
+unsigned laser_parent_is_matching_signature(const laser_parent &l, const object_base &o);
+
 }
 
 #define Highest_object_index (Objects.get_count() - 1)
@@ -635,6 +637,11 @@ struct d_level_unique_object_state
 };
 
 extern d_level_unique_object_state LevelUniqueObjectState;
+
+unsigned laser_parent_is_player(fvcobjptr &, const laser_parent &, const object_base &);
+unsigned laser_parent_is_object(fvcobjptr &, const laser_parent &, const object_base &);
+unsigned laser_parent_is_object(const laser_parent &, vcobjptridx_t);
+unsigned laser_parent_object_exists(fvcobjptr &, const laser_parent &);
 
 static inline powerup_type_t get_powerup_id(const object_base &o)
 {

common/main/objnum.h
@@ -7,7 +7,7 @@ class object_signature_t
 {
 	uint16_t signature;
 public:
-	object_signature_t() = default;
+	explicit object_signature_t() = default;
 	constexpr explicit object_signature_t(uint16_t s) :
 		signature(s)
 	{

similar/main/collide.cpp
@@ -197,6 +197,8 @@ static int apply_damage_to_clutter(const vmobjptridx_t clutter, fix damage)
 namespace dsx {
 static void apply_force_damage(const vmobjptridx_t obj,fix force,const vmobjptridx_t other_obj)
 {
+	auto &Objects = LevelUniqueObjectState.Objects;
+	auto &vcobjptr = Objects.vcptr;
 	fix damage;
 
 	if (obj->flags & (OF_EXPLODING|OF_SHOULD_BE_DEAD))
@@ -230,7 +232,7 @@ static void apply_force_damage(const vmobjptridx_t obj,fix force,const vmobjptri
 
 			const auto console = ConsoleObject;
 			if ((other_type == OBJ_PLAYER && other_obj == console) ||
-				(other_type == OBJ_WEAPON && other_obj->ctype.laser_info.parent_signature == console->signature))
+				(other_type == OBJ_WEAPON && laser_parent_is_player(vcobjptr, other_obj->ctype.laser_info, *console)))
 				add_points_to_score(console->ctype.player_info, robptr.score_value);
 			break;
 		}
@@ -1668,7 +1670,7 @@ static void collide_robot_and_weapon(const vmobjptridx_t  robot, const vmobjptri
 			return;
 	}
 
-	if (weapon->ctype.laser_info.parent_signature == robot->signature)
+	if (laser_parent_is_object(weapon->ctype.laser_info, robot))
 		return;
 
 #if defined(DXX_BUILD_DESCENT_II)
@@ -1770,7 +1772,8 @@ static void collide_robot_and_weapon(const vmobjptridx_t  robot, const vmobjptri
 
 			if (! apply_damage_to_robot(robot, damage, weapon->ctype.laser_info.parent_num))
 				bump_two_objects(robot, weapon, 0);		//only bump if not dead. no damage from bump
-			else if (weapon->ctype.laser_info.parent_signature == ConsoleObject->signature) {
+			else if (laser_parent_is_player(vcobjptr, weapon->ctype.laser_info, *ConsoleObject))
+			{
 				add_points_to_score(ConsoleObject->ctype.player_info, robptr.score_value);
 				detect_escort_goal_accomplished(robot);
 			}

similar/main/fireball.cpp
@@ -127,6 +127,9 @@ static bool can_collide(const object *const weapon_object, const object_base &it
 
 static imobjptridx_t object_create_explosion_sub(const d_vclip_array &Vclip, fvmobjptridx &vmobjptridx, const imobjptridx_t objp, const vmsegptridx_t segnum, const vms_vector &position, fix size, int vclip_type, fix maxdamage, fix maxdistance, fix maxforce, const icobjptridx_t parent )
 {
+#if defined(DXX_BUILD_DESCENT_II)
+	auto &Objects = LevelUniqueObjectState.Objects;
+#endif
 	auto &Robot_info = LevelSharedRobotInfoState.Robot_info;
 	auto obj = obj_create( OBJ_FIREBALL,vclip_type,segnum,position,&vmd_identity_matrix,size,
 					CT_EXPLOSION,MT_NONE,RT_FIREBALL);
@@ -257,7 +260,8 @@ static imobjptridx_t object_create_explosion_sub(const d_vclip_array &Vclip, fvm
 
 									fe = min(F1_0 * 4, force*Weapon_info[get_weapon_id(objp)].flash / 32);	//	For four seconds or less
 
-									if (objp->ctype.laser_info.parent_signature == ConsoleObject->signature) {
+									if (laser_parent_is_player(Objects.vcptr, objp->ctype.laser_info, *ConsoleObject))
+									{
 										fe /= 2;
 										force /= 2;
 									}

commit ff9cff4fea8820c425fefd21ce68cc092e4dd457

Relax laser_parent_is_player check so that dead players match
Fixes: bfeca84bb86ab75365f18d60c17e18f36dbfaf1d ("Change object::signature to be a generation counter")

similar/main/object.cpp
@@ -2108,7 +2108,12 @@ unsigned laser_parent_is_player(fvcobjptr &vcobjptr, const laser_parent &l, cons
 {
 	/* Player objects are never recycled, so skip the signature check.
 	 */
-	if (l.parent_type != o.type)
+	if (l.parent_type != OBJ_PLAYER)
+		return 0;
+	/* As a special case, let the player be recognized even if he died
+	 * before the weapon hit the target.
+	 */
+	if (o.type != OBJ_PLAYER && o.type != OBJ_GHOST)
 		return 0;
 	auto &parent_object = *vcobjptr(l.parent_num);
 	return (&parent_object == &o);