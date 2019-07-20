dxx-rebirth/dxx-rebirth

commit 596ecbb38d964aa5cd9eb2bf0dbb981a8957b361

Rename segment::value to segment::station_idx
`value` is generic and unclear.  It is always meant to be used as an
index into the Station array, so rename it `station_idx` to show this.

Define and consistently use `station_none` to represent that no station
is assigned.

similar/main/gamemine.cpp
@@ -68,7 +68,8 @@ static void segment2_read(segment &s2, PHYSFS_File *fp)
 {
 	s2.special = PHYSFSX_readByte(fp);
 	s2.matcen_num = PHYSFSX_readByte(fp);
-	s2.value = PHYSFSX_readByte(fp);
+	/* station_idx overwritten by caller */
+	PHYSFSX_readByte(fp);
 	const auto s2_flags = PHYSFSX_readByte(fp);
 #if defined(DXX_BUILD_DESCENT_I)
 	(void)s2_flags;	// descent 2 ambient sound handling
@@ -686,7 +687,7 @@ int load_mine_data(PHYSFS_File *LoadFile)
 					i->verts[j] = v16_seg.verts[j];
 
 				i->special = v16_seg.special;
-				i->value = v16_seg.value;
+				i->station_idx = v16_seg.value;
 				i->s2_flags = 0;
 				i->matcen_num = v16_seg.matcen_num;
 				i->static_light = v16_seg.static_light;
@@ -855,11 +856,11 @@ static void read_special(const vmsegptr_t segp,ubyte bit_mask,PHYSFS_File *LoadF
 		// Read byte	Segments[segnum].matcen_num
 		segp->matcen_num = PHYSFSX_readByte(LoadFile);
 		// Read short	Segments[segnum].value
-		segp->value = PHYSFSX_readShort(LoadFile);
+		segp->station_idx = PHYSFSX_readShort(LoadFile);
 	} else {
 		segp->special = 0;
 		segp->matcen_num = -1;
-		segp->value = 0;
+		segp->station_idx = station_none;
 	}
 }
 

common/main/fuelcen.h
@@ -54,6 +54,10 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 
 #ifdef dsx
+namespace dcx {
+constexpr uint8_t station_none = 0xff;
+}
+
 namespace dsx {
 // Destroys all fuel centers, clears segment backpointer array.
 void fuelcen_reset();

common/main/multi.h
@@ -95,7 +95,7 @@ extern int multi_protocol; // set and determinate used protocol
 #define MULTI_PROTO_UDP 1 // UDP protocol
 
 // What version of the multiplayer protocol is this? Increment each time something drastic changes in Multiplayer without the version number changes. Reset to 0 each time the version of the game changes
-#define MULTI_PROTO_VERSION	static_cast<uint16_t>(4)
+#define MULTI_PROTO_VERSION	static_cast<uint16_t>(5)
 // PROTOCOL VARIABLES AND DEFINES - END
 
 // limits for Packets (i.e. positional updates) per sec

common/main/segment.h
@@ -172,10 +172,8 @@ struct segment {
 	array<unsigned, MAX_VERTICES_PER_SEGMENT> verts;    // vertex ids of 4 front and 4 back vertices
 	ubyte   special;    // what type of center this is
 	sbyte   matcen_num; // which center segment is associated with.
-#if defined(DXX_BUILD_DESCENT_I)
-	short   value;
-#elif defined(DXX_BUILD_DESCENT_II)
-	sbyte   value;
+	uint8_t station_idx;
+#if defined(DXX_BUILD_DESCENT_II)
 	ubyte   s2_flags;
 #endif
 	fix     static_light;

similar/editor/mine.cpp
@@ -537,7 +537,7 @@ static void write_special(const vcsegptr_t seg, ubyte bit_mask, PHYSFS_File *Sav
 	{
 		PHYSFSX_writeU8(SaveFile, seg->special);
 		PHYSFSX_writeU8(SaveFile, seg->matcen_num);
-		PHYSFS_writeSLE16(SaveFile, seg->value);
+		PHYSFS_writeULE16(SaveFile, seg->station_idx);
 	}
 }
 // -----------------------------------------------------------------------------
@@ -588,7 +588,7 @@ int save_mine_data_compiled(PHYSFS_File *SaveFile)
 				bit_mask |= (1 << sidenum);
 		}
 
-		if ((seg->special != 0) || (seg->matcen_num != 0) || (seg->value != 0))
+		if (seg->special != 0 || seg->matcen_num != 0 || seg->station_idx != station_none)
 			bit_mask |= (1 << MAX_SIDES_PER_SEGMENT);
 
 		if (New_file_format_save)

similar/editor/segment.cpp
@@ -1400,7 +1400,7 @@ void med_create_segment(const vmsegptridx_t sp,fix cx, fix cy, fix cz, fix lengt
 
 	// Assume nothing special about this segment
 	sp->special = 0;
-	sp->value = 0;
+	sp->station_idx = station_none;
 	sp->static_light = 0;
 	sp->matcen_num = -1;
 
@@ -1455,7 +1455,7 @@ void med_create_new_segment(const vms_vector &scale)
 
 	// Assume nothing special about this segment
 	sp->special = 0;
-	sp->value = 0;
+	sp->station_idx = station_none;
 	sp->static_light = 0;
 	sp->matcen_num = -1;
 }

similar/main/ai.cpp
@@ -3239,7 +3239,7 @@ _exit_cheat:
 		if (seg.special == SEGMENT_IS_ROBOTMAKER)
 		{
 #if defined(DXX_BUILD_DESCENT_II)
-			if (Station[seg.value].Enabled)
+			if (Station[seg.station_idx].Enabled)
 #endif
 		{
 			ai_follow_path(obj, 1, NULL);    // 1 = player is visible, which might be a lie, but it works.

similar/main/dumpmine.cpp
@@ -443,11 +443,11 @@ static void write_segment_text(PHYSFS_File *my_file)
 
 	range_for (const auto &&segp, vcsegptridx)
 	{
-		PHYSFSX_printf(my_file, "Segment %4hu: ", static_cast<uint16_t>(segp));
+		PHYSFSX_printf(my_file, "Segment %4hu:", static_cast<uint16_t>(segp));
 		if (segp->special != 0)
-			PHYSFSX_printf(my_file, "special = %3i (%s), value = %3i ", segp->special, Special_names[segp->special], segp->value);
+			PHYSFSX_printf(my_file, " special = %3i (%s), station_idx=%3i", segp->special, Special_names[segp->special], segp->station_idx);
 		if (segp->matcen_num != -1)
-			PHYSFSX_printf(my_file, "matcen = %3i, ", segp->matcen_num);
+			PHYSFSX_printf(my_file, " matcen = %3i", segp->matcen_num);
 		PHYSFSX_printf(my_file, "\n");
 	}
 

commit a17792c89fd49dd12fdf5981670dd7f64b42850f

Always initialize station_idx on level start
It is reinitialized if the segment is a special segment, but not
reinitialized if the segment is a normal segment.  Most segments are
normal segments.  The absence of this initialization permitted players
to keep the value from a prior level.  For normal segments, this would
normally not be a problem, because the value is only meaningful for
special segments.  However, the netgame synchronization code always
mixes in the value of every station_idx, regardless of whether the
segment is a special segment.  As a result, late-joining guests who
started directly onto the current level could disagree with the host
about the value, causing a synchronization mismatch.  The host would be
wrong, but the guests would be blocked from joining.  For this purpose,
late-joining includes both people who joined the campaign late and
people who left the game and restarted the program, even if they had
previously been in the game.

Reported-by: OldSaltyGamer <https://forum.dxx-rebirth.com/showthread.php?tid=1098&pid=12630#pid12630>
[ Possibly also previously reported by LightWolf, but that report never
had enough information to proceed.  The report from OldSaltyGamer solved
this. ]
Fixes: 596ecbb38d964aa5cd9eb2bf0dbb981a8957b361 ("Rename segment::value to segment::station_idx")

similar/main/gamemine.cpp
@@ -69,8 +69,9 @@ static void segment2_read(shared_segment &s2, unique_segment &u2, PHYSFS_File *f
 {
 	s2.special = PHYSFSX_readByte(fp);
 	s2.matcen_num = PHYSFSX_readByte(fp);
-	/* station_idx overwritten by caller */
-	PHYSFSX_readByte(fp);
+	/* station_idx is overwritten by the caller in some cases, but set
+	 * it here for compatibility with how the game previously worked */
+	s2.station_idx = PHYSFSX_readByte(fp);
 	const auto s2_flags = PHYSFSX_readByte(fp);
 #if defined(DXX_BUILD_DESCENT_I)
 	(void)s2_flags;	// descent 2 ambient sound handling