dxx-rebirth/dxx-rebirth

commit f491059ed78e05da7a5d2bf9af8299167b3550a7

Enable building with SDL2
This commit enables Rebirth to build with SDL2, but the result is not
perfect.

- SDL2 removed some sticky key support.  Rebirth may behave differently
  now in this area.
- SDL2 removed some key-repeat related support.  Rebirth may behave
  differently now in this area.
- SDL2 gained the ability to make a window fullscreen by sizing it to
  the desktop instead of by changing the desktop resolution.  Rebirth
  uses this, and it mostly works.
  - Resizing while in the automap does not notify the automap code, so
    the view is wrong until the player switches out of automap mode and
    back in.
- SDL2 changed how to enumerate available resolutions.  Since
  fitting the window to the desktop is generally more useful than
  fitting the desktop to the window, I chose to drop support for
  enumerating resolutions instead of porting to the new API.  Users can
  now enter an arbitrary window dimension and Rebirth will make an
  attempt to use it.
  - It might be useful to cap the window dimension at the desktop
    dimension, but that is not done yet.
  - Entering fullscreen mode through the Controls->Graphics submenu
    failed to notify the relevant subsystems, causing the rendered
    content not to rescale.  For now, compile out the option to toggle
    full screen through that menu.  Toggling through Alt+Enter works
    properly.

Despite these quirks, this is a substantial improvement over the prior
commit, where SDL2 cannot be used at all.  The remaining issues can be
resolved in future work.

References: <https://github.com/dxx-rebirth/dxx-rebirth/issues/82>

common/arch/sdl/event.cpp
@@ -27,6 +27,10 @@
 
 namespace dcx {
 
+#if SDL_MAJOR_VERSION == 2
+extern SDL_Window *g_pRebirthSDLMainWindow;
+#endif
+
 window_event_result event_poll()
 {
 	SDL_Event event;
@@ -42,8 +46,10 @@ window_event_result event_poll()
 			case SDL_KEYDOWN:
 			case SDL_KEYUP:
 				if (clean_uniframe)
+				{
+					clean_uniframe=0;
 					unicode_frame_buffer = {};
-				clean_uniframe=0;
+				}
 				highest_result = std::max(key_handler(&event.key), highest_result);
 				break;
 			case SDL_MOUSEBUTTONDOWN:
@@ -190,7 +196,12 @@ window_event_result event_process(void)
 template <bool activate_focus>
 static void event_change_focus()
 {
-	SDL_WM_GrabInput(activate_focus && CGameCfg.Grabinput && likely(!CGameArg.DbgForbidConsoleGrab) ? SDL_GRAB_ON : SDL_GRAB_OFF);
+	const auto enable_grab = activate_focus && CGameCfg.Grabinput && likely(!CGameArg.DbgForbidConsoleGrab);
+#if SDL_MAJOR_VERSION == 1
+	SDL_WM_GrabInput(enable_grab ? SDL_GRAB_ON : SDL_GRAB_OFF);
+#elif SDL_MAJOR_VERSION == 2
+	SDL_SetWindowGrab(g_pRebirthSDLMainWindow, enable_grab ? SDL_TRUE : SDL_FALSE);
+#endif
 	if (activate_focus)
 		mouse_disable_cursor();
 	else

SConstruct
@@ -833,10 +833,15 @@ help:assume C++ compiler works
 {macros}
 {text}
 
-#undef main	/* avoid -Dmain=SDL_main from libSDL */
+{undef_SDL_main}
 
 {main}
-'''.format(tools=self.__tool_versions, macros=self.__defined_macros, text=text, main=('' if main is None else
+'''.format(
+	tools=self.__tool_versions,
+	macros=self.__defined_macros,
+	text=text,
+	undef_SDL_main='' if self.user_settings.sdl2 else '#undef main	/* avoid -Dmain=SDL_main from libSDL */',
+	main=('' if main is None else
 '''
 int main(int argc,char**argv){(void)argc;(void)argv;
 %s
@@ -1299,6 +1304,8 @@ static void terminate_handler()
 	def check_libSDL2(self,context,_guess_flags={
 			'LIBS' : ['SDL2'] if sys.platform != 'darwin' else [],
 		}):
+		if not self.user_settings.opengl:
+			raise SCons.Errors.StopError('Rebirth does not support SDL2 without OpenGL.  Set opengl=1 or sdl2=0.')
 		self._check_libSDL(context, '2', _guess_flags)
 	def _check_libSDL(self,context,sdl2,guess_flags):
 		user_settings = self.user_settings
@@ -3540,7 +3547,7 @@ class DXXCommon(LazyObjectConstructor):
 					('opengl', True, 'build with OpenGL support'),
 					('opengles', self.default_opengles, 'build with OpenGL ES support'),
 					('editor', False, 'include editor into build (!EXPERIMENTAL!)'),
-					('sdl2', False, 'use libSDL2+SDL2_mixer (!DEVELOPERS ONLY - KNOWN BROKEN!)'),
+					('sdl2', False, 'use libSDL2+SDL2_mixer (!EXPERIMENTAL!)'),
 					('sdlmixer', True, 'build with SDL_Mixer support for sound and music (includes external music support)'),
 					('ipv6', False, 'enable UDP/IPv6 for multiplayer'),
 					('use_udp', True, 'enable UDP support'),

common/arch/sdl/key.cpp
@@ -16,6 +16,10 @@
 #include <stdlib.h>
 
 #include <SDL.h>
+#include <SDL_version.h>
+#if SDL_MAJOR_VERSION == 2
+#include <SDL_keycode.h>
+#endif
 
 #include "event.h"
 #include "dxxerror.h"
@@ -108,20 +112,34 @@ const array<key_props, 256> key_properties = {{
 { "F8",     255,    SDLK_F8            },
 { "F9",     255,    SDLK_F9            },
 { "F10",    255,    SDLK_F10           },
-{ "NMLCK",  255,    SDLK_NUMLOCK       },
-{ "SCLK",   255,    SDLK_SCROLLOCK     }, // 70
-{ "PAD7",   255,    SDLK_KP7           },
-{ "PAD8",   255,    SDLK_KP8           },
-{ "PAD9",   255,    SDLK_KP9           },
+#if SDL_MAJOR_VERSION == 1
+#define SDLK_NUMLOCKCLEAR	SDLK_NUMLOCK
+#define SDLK_SCROLLLOCK	SDLK_SCROLLOCK
+#define SDLK_KP_0	SDLK_KP0
+#define SDLK_KP_1	SDLK_KP1
+#define SDLK_KP_2	SDLK_KP2
+#define SDLK_KP_3	SDLK_KP3
+#define SDLK_KP_4	SDLK_KP4
+#define SDLK_KP_5	SDLK_KP5
+#define SDLK_KP_6	SDLK_KP6
+#define SDLK_KP_7	SDLK_KP7
+#define SDLK_KP_8	SDLK_KP8
+#define SDLK_KP_9	SDLK_KP9
+#endif
+{ "NMLCK",  255,    SDLK_NUMLOCKCLEAR  },
+{ "SCLK",   255,    SDLK_SCROLLLOCK    }, // 70
+{ "PAD7",   255,    SDLK_KP_7          },
+{ "PAD8",   255,    SDLK_KP_8          },
+{ "PAD9",   255,    SDLK_KP_9          },
 { "PAD-",   255,    SDLK_KP_MINUS      },
-{ "PAD4",   255,    SDLK_KP4           },
-{ "PAD5",   255,    SDLK_KP5           },
-{ "PAD6",   255,    SDLK_KP6           },
+{ "PAD4",   255,    SDLK_KP_4          },
+{ "PAD5",   255,    SDLK_KP_5          },
+{ "PAD6",   255,    SDLK_KP_6          },
 { "PAD+",   255,    SDLK_KP_PLUS       },
-{ "PAD1",   255,    SDLK_KP1           },
-{ "PAD2",   255,    SDLK_KP2           }, // 80
-{ "PAD3",   255,    SDLK_KP3           },
-{ "PAD0",   255,    SDLK_KP0           },
+{ "PAD1",   255,    SDLK_KP_1          },
+{ "PAD2",   255,    SDLK_KP_2          }, // 80
+{ "PAD3",   255,    SDLK_KP_3          },
+{ "PAD0",   255,    SDLK_KP_0          },
 { "PAD.",   255,    SDLK_KP_PERIOD     },
 { "",       255,    SDLK_UNKNOWN                 },
 { "",       255,    SDLK_UNKNOWN                 },
@@ -137,6 +155,60 @@ const array<key_props, 256> key_properties = {{
 { "",       255,    SDLK_UNKNOWN                 },
 { "",       255,    SDLK_UNKNOWN                 },
 { "PAUSE",  255,    SDLK_PAUSE         },
+#if SDL_MAJOR_VERSION == 2
+#define SDLK_WORLD_0	SDLK_UNKNOWN
+#define SDLK_WORLD_1	SDLK_UNKNOWN
+#define SDLK_WORLD_2	SDLK_UNKNOWN
+#define SDLK_WORLD_3	SDLK_UNKNOWN
+#define SDLK_WORLD_4	SDLK_UNKNOWN
+#define SDLK_WORLD_5	SDLK_UNKNOWN
+#define SDLK_WORLD_6	SDLK_UNKNOWN
+#define SDLK_WORLD_7	SDLK_UNKNOWN
+#define SDLK_WORLD_8	SDLK_UNKNOWN
+#define SDLK_WORLD_9	SDLK_UNKNOWN
+#define SDLK_WORLD_10	SDLK_UNKNOWN
+#define SDLK_WORLD_11	SDLK_UNKNOWN
+#define SDLK_WORLD_12	SDLK_UNKNOWN
+#define SDLK_WORLD_13	SDLK_UNKNOWN
+#define SDLK_WORLD_14	SDLK_UNKNOWN
+#define SDLK_WORLD_15	SDLK_UNKNOWN
+#define SDLK_WORLD_16	SDLK_UNKNOWN
+#define SDLK_WORLD_17	SDLK_UNKNOWN
+#define SDLK_WORLD_18	SDLK_UNKNOWN
+#define SDLK_WORLD_19	SDLK_UNKNOWN
+#define SDLK_WORLD_20	SDLK_UNKNOWN
+#define SDLK_WORLD_21	SDLK_UNKNOWN
+#define SDLK_WORLD_22	SDLK_UNKNOWN
+#define SDLK_WORLD_23	SDLK_UNKNOWN
+#define SDLK_WORLD_24	SDLK_UNKNOWN
+#define SDLK_WORLD_25	SDLK_UNKNOWN
+#define SDLK_WORLD_26	SDLK_UNKNOWN
+#define SDLK_WORLD_27	SDLK_UNKNOWN
+#define SDLK_WORLD_28	SDLK_UNKNOWN
+#define SDLK_WORLD_29	SDLK_UNKNOWN
+#define SDLK_WORLD_30	SDLK_UNKNOWN
+#define SDLK_WORLD_31	SDLK_UNKNOWN
+#define SDLK_WORLD_32	SDLK_UNKNOWN
+#define SDLK_WORLD_33	SDLK_UNKNOWN
+#define SDLK_WORLD_34	SDLK_UNKNOWN
+#define SDLK_WORLD_35	SDLK_UNKNOWN
+#define SDLK_WORLD_36	SDLK_UNKNOWN
+#define SDLK_WORLD_37	SDLK_UNKNOWN
+#define SDLK_WORLD_38	SDLK_UNKNOWN
+#define SDLK_WORLD_39	SDLK_UNKNOWN
+#define SDLK_WORLD_40	SDLK_UNKNOWN
+#define SDLK_WORLD_41	SDLK_UNKNOWN
+#define SDLK_WORLD_42	SDLK_UNKNOWN
+#define SDLK_WORLD_43	SDLK_UNKNOWN
+#define SDLK_WORLD_44	SDLK_UNKNOWN
+#define SDLK_WORLD_45	SDLK_UNKNOWN
+#define SDLK_WORLD_46	SDLK_UNKNOWN
+#define SDLK_WORLD_47	SDLK_UNKNOWN
+#define SDLK_WORLD_48	SDLK_UNKNOWN
+#define SDLK_WORLD_49	SDLK_UNKNOWN
+#define SDLK_WORLD_50	SDLK_UNKNOWN
+#define SDLK_WORLD_51	SDLK_UNKNOWN
+#endif
 { "W0",     255,    SDLK_WORLD_0       },
 { "W1",     255,    SDLK_WORLD_1       },
 { "W2",     255,    SDLK_WORLD_2       }, // 100
@@ -197,6 +269,10 @@ const array<key_props, 256> key_properties = {{
 { "",       255,    SDLK_UNKNOWN                 },
 { "PAD",    255,    SDLK_KP_ENTER      },
 { "RCTRL",  255,    SDLK_RCTRL         },
+#if SDL_MAJOR_VERSION == 2
+#define SDLK_LMETA	SDLK_UNKNOWN
+#define SDLK_RMETA	SDLK_UNKNOWN
+#endif
 { "LCMD",   255,    SDLK_LMETA         },
 { "RCMD",   255,    SDLK_RMETA         },
 { "",       255,    SDLK_UNKNOWN                 }, // 160
@@ -222,7 +298,10 @@ const array<key_props, 256> key_properties = {{
 { "",       255,    SDLK_UNKNOWN                 }, // 180
 { "PAD/",   255,    SDLK_KP_DIVIDE     },
 { "",       255,    SDLK_UNKNOWN                 },
-{ "PRSCR",  255,    SDLK_PRINT         },
+#if SDL_MAJOR_VERSION == 1
+#define SDLK_PRINTSCREEN	SDLK_PRINT
+#endif
+{ "PRSCR",  255,    SDLK_PRINTSCREEN   },
 { "RALT",   255,    SDLK_RALT          },
 { "",       255,    SDLK_UNKNOWN                 },
 { "",       255,    SDLK_UNKNOWN                 },
@@ -251,6 +330,7 @@ const array<key_props, 256> key_properties = {{
 { "PGDN",   255,    SDLK_PAGEDOWN      },
 { "INS",    255,    SDLK_INSERT        }, // 210
 { "DEL",    255,    SDLK_DELETE        },
+#if SDL_MAJOR_VERSION == 1
 { "W52",    255,    SDLK_WORLD_52      },
 { "W53",    255,    SDLK_WORLD_53      },
 { "W54",    255,    SDLK_WORLD_54      },
@@ -295,6 +375,7 @@ const array<key_props, 256> key_properties = {{
 { "W93",    255,    SDLK_WORLD_93      },
 { "W94",    255,    SDLK_WORLD_94      },
 { "W95",    255,    SDLK_WORLD_95      }, // 255
+#endif
 }};
 
 namespace {
@@ -400,23 +481,29 @@ void pressed_keys::update(const std::size_t keycode, const uint8_t down)
 
 window_event_result key_handler(SDL_KeyboardEvent *kevent)
 {
-	int event_keysym=-1;
-
 	// Read SDLK symbol and state
-        event_keysym = kevent->keysym.sym;
-		if (event_keysym == SDLK_UNKNOWN)
-			return window_event_result::ignored;
+	const auto event_keysym = kevent->keysym.sym;
+	if (event_keysym == SDLK_UNKNOWN)
+		return window_event_result::ignored;
 	const auto key_state = (kevent->state != SDL_RELEASED);
 
 	// fill the unicode frame-related unicode buffer 
-	if (key_state && kevent->keysym.unicode > 31 && kevent->keysym.unicode < 255)
+	if (key_state)
 	{
-		range_for (auto &i, unicode_frame_buffer)
-			if (i == '\0')
-			{
-				i = kevent->keysym.unicode;
-				break;
-			}
+#if SDL_MAJOR_VERSION == 1
+		const auto sym = kevent->keysym.unicode;
+#elif SDL_MAJOR_VERSION == 2
+		const auto sym = kevent->keysym.sym;
+#endif
+		if (sym > 31 && sym < 255)
+		{
+			range_for (auto &i, unicode_frame_buffer)
+				if (i == '\0')
+				{
+					i = sym;
+					break;
+				}
+		}
 	}
 
 	//=====================================================
@@ -438,6 +525,7 @@ window_event_result key_handler(SDL_KeyboardEvent *kevent)
 	{
 		// now update the key props
 		keyd_pressed.update(keycode, key_state);
+		const auto raw_keycode = keycode;
 		keycode |= keyd_pressed.get_modifiers();
 
 		// We allowed the key to be added to the queue for now,
@@ -450,17 +538,18 @@ window_event_result key_handler(SDL_KeyboardEvent *kevent)
 				(keycode & KEY_CTRLED)	? "CTRL" : "",
 				(keycode & KEY_ALTED)	? "ALT" : "",
 				(keycode & KEY_SHIFTED)	? "SHIFT" : "",
-				key_properties[keycode & 0xff].key_text
+				key_properties[raw_keycode].key_text
 				);
 		return event_send(event);
 	}
-
 	return window_event_result::ignored;
 }
 
 void key_init()
 {
+#if SDL_MAJOR_VERSION == 1
 	SDL_EnableUNICODE(1);
+#endif
 	key_toggle_repeat(1);
 
 	keyd_time_when_last_pressed = timer_query();
@@ -470,10 +559,13 @@ void key_init()
 
 static void restore_sticky_key(const uint8_t *keystate, const unsigned i)
 {
-	if (keystate[key_properties[i].sym]) // do not flush status of sticky keys
-	{
-		keyd_pressed.update_pressed(i, 1);
-	}
+#if SDL_MAJOR_VERSION == 1
+	const auto ki = key_properties[i].sym;
+#elif SDL_MAJOR_VERSION == 2
+	const auto ki = i;
+#endif
+	const auto v = keystate[ki];	// do not flush status of sticky keys
+	keyd_pressed.update_pressed(i, v);
 }
 
 void key_flush()
@@ -483,10 +575,15 @@ void key_flush()
 	keyd_pressed = {};
 	if (unlikely(CGameArg.CtlNoStickyKeys))
 		return;
-	const auto &keystate = SDL_GetKeyState(NULL);
-	restore_sticky_key(keystate, KEY_NUMLOCK);
-	restore_sticky_key(keystate, KEY_SCROLLOCK);
-	restore_sticky_key(keystate, KEY_CAPSLOCK);
+#if SDL_MAJOR_VERSION == 1
+	const auto &keystate = SDL_GetKeyState(nullptr);
+	constexpr auto sticky_keys = std::initializer_list<unsigned>{KEY_CAPSLOCK, KEY_NUMLOCK, KEY_SCROLLOCK};
+#elif SDL_MAJOR_VERSION == 2
+	const auto &keystate = SDL_GetKeyboardState(nullptr);
+	constexpr auto sticky_keys = std::initializer_list<unsigned>{SDL_SCANCODE_CAPSLOCK, SDL_SCANCODE_SCROLLLOCK, SDL_SCANCODE_NUMLOCKCLEAR};
+#endif
+	range_for (const auto key, sticky_keys)
+		restore_sticky_key(keystate, key);
 }
 
 int event_key_get(const d_event &event)
@@ -504,14 +601,18 @@ int event_key_get_raw(const d_event &event)
 
 void key_toggle_repeat1()
 {
+#if SDL_MAJOR_VERSION == 1
 	if (SDL_EnableKeyRepeat(KEY_REPEAT_DELAY, KEY_REPEAT_INTERVAL) == 0)
+#endif
 		keyd_repeat = 1;
 	key_flush();
 }
 
 void key_toggle_repeat0()
 {
+#if SDL_MAJOR_VERSION == 1
 	SDL_EnableKeyRepeat(0, 0);
+#endif
 	keyd_repeat = 0;
 	key_flush();
 }

common/include/fwd-gr.h
@@ -19,6 +19,10 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #pragma once
 
+#include <SDL_version.h>
+#if SDL_MAJOR_VERSION == 2
+#include <SDL_video.h>
+#endif
 #include <cstdint>
 #include <memory>
 #include <utility>
@@ -105,13 +109,18 @@ typedef std::unique_ptr<grs_subcanvas> grs_subcanvas_ptr;
 class grs_main_bitmap;
 typedef std::unique_ptr<grs_main_bitmap> grs_bitmap_ptr;
 
+#if SDL_MAJOR_VERSION == 1
 uint_fast32_t gr_list_modes(array<screen_mode, 50> &modes);
+#elif SDL_MAJOR_VERSION == 2
+extern SDL_Window *g_pRebirthSDLMainWindow;
+#endif
 
 }
 
 #ifdef dsx
 namespace dsx {
 int gr_set_mode(screen_mode mode);
+void gr_set_mode_from_window_size();
 
 int gr_init();
 #if DXX_USE_OGL

common/include/key.h
@@ -26,7 +26,12 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #pragma once
 
 #include <cstdint>
+#include <SDL_version.h>
+#if SDL_MAJOR_VERSION == 1
 #include <SDL_keysym.h>
+#elif SDL_MAJOR_VERSION == 2
+#include <SDL_keyboard.h>
+#endif
 #include "pstypes.h"
 #include "maths.h"
 #include "fwd-event.h"
@@ -224,7 +229,11 @@ struct key_props
 {
 	char key_text[7];
 	unsigned char ascii_value;
+#if SDL_MAJOR_VERSION == 1
 	SDLKey sym;
+#elif SDL_MAJOR_VERSION == 2
+	SDL_Keycode sym;
+#endif
 };
 
 extern const array<key_props, 256> key_properties;

common/include/mouse.h
@@ -72,15 +72,17 @@ class d_event_mouse_moved : public d_event
 {
 public:
 #if SDL_MAJOR_VERSION == 1
-	const Sint16 dx, dy;
-#else
-	const Sint32 dx, dy;
+#define SDL_MOUSE_MOVE_INT_TYPE	Sint16
+#elif SDL_MAJOR_VERSION == 2
+#define SDL_MOUSE_MOVE_INT_TYPE	Sint32
 #endif
+	const SDL_MOUSE_MOVE_INT_TYPE dx, dy;
 	const int16_t dz;
-	constexpr d_event_mouse_moved(const event_type t, const int16_t x, const int16_t y, const int16_t z) :
+	constexpr d_event_mouse_moved(const event_type t, const SDL_MOUSE_MOVE_INT_TYPE x, const SDL_MOUSE_MOVE_INT_TYPE y, const int16_t z) :
 		d_event(t), dx(x), dy(y), dz(z)
 	{
 	}
+#undef SDL_MOUSE_MOVE_INT_TYPE
 };
 
 static inline int event_mouse_get_button(const d_event &event)

common/include/rbaudio.h
@@ -22,8 +22,11 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #define RBA_MEDIA_CHANGED	-1
 
 #ifdef __cplusplus
+#include <SDL_version.h>
+
 namespace dcx {
 
+#if SDL_MAJOR_VERSION == 1
 struct RBACHANNELCTL
 {
 	unsigned int out0in, out0vol;
@@ -71,6 +74,7 @@ unsigned long RBAGetDiscID();
 
 // List the tracks on the CD
 void RBAList(void);
+#endif
 
 }
 #endif

common/main/digi.h
@@ -25,6 +25,7 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #pragma once
 
+#include <SDL_version.h>
 #include <type_traits>
 #include "pstypes.h"
 #include "vecmat.h"
@@ -122,7 +123,9 @@ extern void digi_start_sound_queued( short soundnum, fix volume );
 
 #define MUSIC_TYPE_NONE		0
 #define MUSIC_TYPE_BUILTIN	1
+#if SDL_MAJOR_VERSION == 1
 #define MUSIC_TYPE_REDBOOK	2
+#endif
 #define MUSIC_TYPE_CUSTOM	3
 
 // play-order definitions for custom music

commit 6d0cd8513e2ce4567217d5f29854fb2726af64bd

Recompute automap subcanvas on window size change
The subcanvas is positioned based on the dimensions of the containing
window, so it must be repositioned if the outer window is resized.

Reported-by: vLKp <https://github.com/dxx-rebirth/dxx-rebirth/issues/394>
Fixes: f491059ed78e05da7a5d2bf9af8299167b3550a7 ("Enable building with SDL2")

common/arch/sdl/event.cpp
@@ -29,6 +29,19 @@ namespace dcx {
 
 #if SDL_MAJOR_VERSION == 2
 extern SDL_Window *g_pRebirthSDLMainWindow;
+
+static void windowevent_handler(const SDL_WindowEvent &windowevent)
+{
+	switch (windowevent.event)
+	{
+		case SDL_WINDOWEVENT_SIZE_CHANGED:
+			{
+				const d_window_size_event e{windowevent.data1, windowevent.data2};
+				event_send(e);
+				break;
+			}
+	}
+}
 #endif
 
 window_event_result event_poll()
@@ -43,6 +56,11 @@ window_event_result event_poll()
 	while ((highest_result != window_event_result::deleted) && (wind == window_get_front()) && (event = {}, SDL_PollEvent(&event)))
 	{
 		switch(event.type) {
+#if SDL_MAJOR_VERSION == 2
+			case SDL_WINDOWEVENT:
+				windowevent_handler(event.window);
+				break;
+#endif
 			case SDL_KEYDOWN:
 			case SDL_KEYUP:
 				if (clean_uniframe)

common/include/event.h
@@ -8,8 +8,13 @@
 
 #pragma once
 
+#include <SDL_version.h>
 #include "fwd-event.h"
 #include "maths.h"
+
+#if SDL_MAJOR_VERSION == 2
+#include <SDL_video.h>
+#endif
 
 #ifdef __cplusplus
 namespace dcx {
@@ -32,6 +37,9 @@ enum event_type : unsigned
 	EVENT_KEY_RELEASE,
 
 	EVENT_WINDOW_CREATED,
+#if SDL_MAJOR_VERSION == 2
+	EVENT_WINDOW_RESIZE,
+#endif
 	EVENT_WINDOW_ACTIVATED,
 	EVENT_WINDOW_DEACTIVATED,
 	EVENT_WINDOW_DRAW,
@@ -95,6 +103,18 @@ struct d_select_event : d_event
 	{
 	}
 };
+
+#if SDL_MAJOR_VERSION == 2
+struct d_window_size_event : d_event
+{
+	Sint32 width;
+	Sint32 height;
+	d_window_size_event(const Sint32 w, const Sint32 h) :
+		d_event{EVENT_WINDOW_RESIZE}, width(w), height(h)
+	{
+	}
+};
+#endif
 
 #if DXX_USE_EDITOR
 fix event_get_idle_seconds();

similar/main/automap.cpp
@@ -139,7 +139,7 @@ struct automap : ignore_window_pointer_t
 	std::unique_ptr<Edge_info *[]>			drawingListBright;
 	
 	// Screen canvas variables
-	grs_canvas		automap_view;
+	grs_subcanvas		automap_view;
 	
 	grs_main_bitmap		automap_background;
 	
@@ -167,6 +167,16 @@ struct automap : ignore_window_pointer_t
 	control_info controls;
 	segment_depth_array_t depth_array;
 };
+
+static void init_automap_subcanvas(grs_subcanvas &view, grs_canvas &container)
+{
+#if defined(DXX_BUILD_DESCENT_I)
+	if (MacHog)
+		gr_init_sub_canvas(view, container, 38*(SWIDTH/640.0), 77*(SHEIGHT/480.0), 564*(SWIDTH/640.0), 381*(SHEIGHT/480.0));
+	else
+#endif
+		gr_init_sub_canvas(view, container, (SWIDTH/23), (SHEIGHT/6), (SWIDTH/1.1), (SHEIGHT/1.45));
+}
 
 }
 
@@ -992,6 +1002,12 @@ static window_event_result automap_handler(window *wind,const d_event &event, au
 			key_toggle_repeat(1);
 			break;
 
+#if SDL_MAJOR_VERSION == 2
+		case EVENT_WINDOW_RESIZE:
+			init_automap_subcanvas(am->automap_view, grd_curscreen->sc_canvas);
+			break;
+#endif
+
 		case EVENT_IDLE:
 		case EVENT_JOYSTICK_BUTTON_UP:
 		case EVENT_JOYSTICK_BUTTON_DOWN:
@@ -1109,12 +1125,7 @@ void do_automap()
 		else
 			gr_remap_bitmap_good(am->automap_background, pal, -1, -1);
 	}
-#if defined(DXX_BUILD_DESCENT_I)
-	if (MacHog)
-		gr_init_sub_canvas(am->automap_view, grd_curscreen->sc_canvas, 38*(SWIDTH/640.0), 77*(SHEIGHT/480.0), 564*(SWIDTH/640.0), 381*(SHEIGHT/480.0));
-	else
-#endif
-		gr_init_sub_canvas(am->automap_view, grd_curscreen->sc_canvas, (SWIDTH/23), (SHEIGHT/6), (SWIDTH/1.1), (SHEIGHT/1.45));
+	init_automap_subcanvas(am->automap_view, grd_curscreen->sc_canvas);
 
 	gr_palette_load( gr_palette );
 	Automap_active = 1;