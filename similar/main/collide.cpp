dxx-rebirth/dxx-rebirth

commit 215252b8caf33b0b4e9a7e8574ff36e597b4ca4b

Fold Final_boss_is_dead into Final_boss_countdown_time

similar/main/collide.cpp
@@ -1337,18 +1337,25 @@ namespace dsx {
 //	------------------------------------------------------------------------------------------------------
 window_event_result do_final_boss_frame(void)
 {
-	if (!GameUniqueState.Final_boss_is_dead)
+	auto Final_boss_countdown_time = GameUniqueState.Final_boss_countdown_time;
+	if (!Final_boss_countdown_time)
 		return window_event_result::ignored;
 
 	if (!Control_center_destroyed)
 		return window_event_result::ignored;
 
-	if (GameUniqueState.Final_boss_countdown_time == 0)
-		GameUniqueState.Final_boss_countdown_time = F1_0*2;
-
-	GameUniqueState.Final_boss_countdown_time -= FrameTime;
-	if (GameUniqueState.Final_boss_countdown_time > 0)
+	Final_boss_countdown_time -= FrameTime;
+	if (Final_boss_countdown_time > 0)
 		return window_event_result::ignored;
+	/* Ensure that GameUniqueState.Final_boss_countdown_time is not 0,
+	 * so that the flag remains raised.  If Final_boss_countdown_time
+	 * were greater than 0, the function already returned.  If
+	 * Final_boss_countdown_time is exactly 0, write -1.  Otherwise,
+	 * write a slightly more negative number.  If
+	 * Final_boss_countdown_time is INT32_MIN, this would roll over, but
+	 * that would only happen if FrameTime had an absurd value.
+	 */
+	GameUniqueState.Final_boss_countdown_time = --Final_boss_countdown_time;
 
 	return start_endlevel_sequence();		//pretend we hit the exit trigger
 }
@@ -1384,7 +1391,7 @@ void do_final_boss_hacks(void)
 	if (!(Game_mode & GM_MULTI))
 		buddy_message("Nice job, %s!", static_cast<const char *>(get_local_player().callsign));
 
-	GameUniqueState.Final_boss_is_dead = 1;
+	GameUniqueState.Final_boss_countdown_time = F1_0*2;
 }
 
 }

common/main/game.h
@@ -174,7 +174,6 @@ extern void PALETTE_FLASH_ADD(int dr, int dg, int db);
 struct d_game_unique_state : ::dcx::d_game_unique_state
 {
 	fix Final_boss_countdown_time;
-	uint8_t Final_boss_is_dead;
 };
 #endif
 

similar/main/ai.cpp
@@ -4594,7 +4594,7 @@ void init_robots_for_level(void)
 	BossUniqueState.Boss_dying_start_time = 0;
 	Overall_agitation = 0;
 #if defined(DXX_BUILD_DESCENT_II)
-	GameUniqueState.Final_boss_is_dead = 0;
+	GameUniqueState.Final_boss_countdown_time = 0;
 	Ai_last_missile_camera = nullptr;
 #endif
 }

similar/main/state.cpp
@@ -880,7 +880,7 @@ int state_save_all(const secret_save secret, const blind_save blind_save)
 		return 0;
 	}
 
-	if (GameUniqueState.Final_boss_is_dead)		//don't allow save while final boss is dying
+	if (GameUniqueState.Final_boss_countdown_time)		//don't allow save while final boss is dying
 		return 0;
 #endif
 

commit 275c4297fedcdf7eddb1b32218155e96db366fbe

Write back Final_boss_countdown_time on every update
Otherwise, unless FrameTime exceeds Final_boss_countdown_time in a
single frame, the time never advances and the level never ends.

Fixes: 215252b8caf33b0b4e9a7e8574ff36e597b4ca4b ("Fold Final_boss_is_dead into Final_boss_countdown_time")

similar/main/collide.cpp
@@ -1352,7 +1352,10 @@ window_event_result do_final_boss_frame(void)
 
 	Final_boss_countdown_time -= FrameTime;
 	if (Final_boss_countdown_time > 0)
+	{
+		GameUniqueState.Final_boss_countdown_time = Final_boss_countdown_time;
 		return window_event_result::ignored;
+	}
 	/* Ensure that GameUniqueState.Final_boss_countdown_time is not 0,
 	 * so that the flag remains raised.  If Final_boss_countdown_time
 	 * were greater than 0, the function already returned.  If