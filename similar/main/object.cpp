dxx-rebirth/dxx-rebirth

commit 0df57f5b0f86c80979000389d68b23403afd52eb

Fix retail bug that prevented marker spinning
Markers were given a spin rate, but a movement type of MT_NONE, so the
spin rate was ignored.  Change movement type to MT_SPINNING.

similar/main/object.cpp
@@ -2162,14 +2162,15 @@ static void obj_detach_all(object_array &Objects, object_base &parent)
 imobjptridx_t drop_marker_object(const vms_vector &pos, const vmsegptridx_t segnum, const vms_matrix &orient, int marker_num)
 {
 	Assert(Marker_model_num != -1);
-	auto obj = obj_create(OBJ_MARKER, marker_num, segnum, pos, &orient, Polygon_models[Marker_model_num].rad, CT_NONE, MT_NONE, RT_POLYOBJ);
+	const auto &&obj = obj_create(OBJ_MARKER, marker_num, segnum, pos, &orient, Polygon_models[Marker_model_num].rad, CT_NONE, MT_SPINNING, RT_POLYOBJ);
 	if (obj != object_none) {
-		obj->rtype.pobj_info.model_num = Marker_model_num;
+		auto &o = *obj;
+		o.rtype.pobj_info.model_num = Marker_model_num;
 
-		vm_vec_copy_scale(obj->mtype.spin_rate,obj->orient.uvec,F1_0/2);
+		vm_vec_copy_scale(o.mtype.spin_rate, o.orient.uvec, F1_0 / 2);
 
 		//	MK, 10/16/95: Using lifeleft to make it flash, thus able to trim lightlevel from all objects.
-		obj->lifeleft = IMMORTAL_TIME - 1;
+		o.lifeleft = IMMORTAL_TIME - 1;
 	}
 	return obj;	
 }

commit 9d22103d45cf5236537c96cff187e7dd6c7b033e

Prevent markers spinning in marker-camera-enabled games
Commit 0df57f5b0f86 changed markers' movement type from MT_NONE to
MT_SPINNING, since retail code assigned a spin rate, suggesting markers
were meant to spin.  However, in some anarchy games, markers are used as
player-positioned cameras.  Spinning the cameras makes them hard to use.
Resolve this by preventing markers from spinning in such games.

Fixes: 0df57f5b0f86c80979000389d68b23403afd52eb ("Fix retail bug that prevented marker spinning")
Reported-by: flpduarte <https://github.com/dxx-rebirth/dxx-rebirth/issues/456>

similar/main/object.cpp
@@ -2322,11 +2322,17 @@ imobjptridx_t drop_marker_object(const vms_vector &pos, const vmsegptridx_t segn
 {
 	Assert(Marker_model_num != -1);
 	auto &Polygon_models = LevelSharedPolygonModelState.Polygon_models;
-	const auto &&obj = obj_create(OBJ_MARKER, marker_num, segnum, pos, &orient, Polygon_models[Marker_model_num].rad, CT_NONE, MT_SPINNING, RT_POLYOBJ);
+	const movement_type_t movement_type =
+		((Game_mode & GM_MULTI) && !(Game_mode & GM_MULTI_COOP) && Netgame.Allow_marker_view)
+		? MT_NONE
+		: MT_SPINNING;
+	const auto &&obj = obj_create(OBJ_MARKER, marker_num, segnum, pos, &orient, Polygon_models[Marker_model_num].rad, CT_NONE, movement_type, RT_POLYOBJ);
 	if (obj != object_none) {
 		auto &o = *obj;
 		o.rtype.pobj_info.model_num = Marker_model_num;
 
+		if (movement_type == MT_SPINNING)
+		{
 		constexpr fix scale = F1_0 / 2;
 		const auto oi = obj.get_unchecked_index();
 		auto &spin_vec = o.mtype.spin_rate;
@@ -2337,6 +2343,7 @@ imobjptridx_t drop_marker_object(const vms_vector &pos, const vmsegptridx_t segn
 			vm_vec_scale_add2(spin_vec, o.orient.uvec, (oi & 16) ? scale : -scale);
 		if (oi & 4)
 			vm_vec_scale_add2(spin_vec, o.orient.rvec, (oi & 32) ? scale : -scale);
+		}
 
 		//	MK, 10/16/95: Using lifeleft to make it flash, thus able to trim lightlevel from all objects.
 		o.lifeleft = IMMORTAL_TIME - 1;