dxx-rebirth/dxx-rebirth

commit 1eaaff3016cc7631e751e451ac1c8a9973709e22

Move Difficulty_level to GameUniqueState

common/main/game.h
@@ -127,7 +127,7 @@ void calc_d_tick();
 
 extern int Game_suspended;          // if non-zero, nothing moves but player
 
-enum Difficulty_level_type : int
+enum Difficulty_level_type : unsigned
 {
 	Difficulty_0,
 	Difficulty_1,
@@ -136,13 +136,19 @@ enum Difficulty_level_type : int
 	Difficulty_4,
 };
 
-#define DEFAULT_DIFFICULTY		Difficulty_1
+constexpr Difficulty_level_type DEFAULT_DIFFICULTY = Difficulty_1;
 
-extern Difficulty_level_type Difficulty_level;    // Difficulty level in 0..NDL-1, 0 = easiest, NDL-1 = hardest
 static inline Difficulty_level_type cast_clamp_difficulty(const unsigned d)
 {
 	return (d <= Difficulty_4) ? static_cast<Difficulty_level_type>(d) : Difficulty_4;
 }
+
+struct d_game_unique_state
+{
+	Difficulty_level_type Difficulty_level;    // Difficulty level in 0..NDL-1, 0 = easiest, NDL-1 = hardest
+};
+
+extern d_game_unique_state GameUniqueState;
 
 extern int Global_missile_firing_count;
 

d2x-rebirth/main/escort.cpp
@@ -1408,6 +1408,7 @@ constexpr array<fix, NDL> Thief_wait_times = {{
 //	-------------------------------------------------------------------------------------------------
 void do_thief_frame(const vmobjptridx_t objp, const fix dist_to_player, const player_visibility_state player_visibility, const vms_vector &vec_to_player)
 {
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	auto &Robot_info = LevelSharedRobotInfoState.Robot_info;
 	ai_local		*ailp = &objp->ctype.ai_info.ail;
 	fix			connected_distance;
@@ -1785,7 +1786,7 @@ int attempt_to_steal_item(const vmobjptridx_t objp, const vmobjptr_t player_num)
 	}
 	create_n_segment_path(objp, 10, ConsoleObject->segnum);
 	ai_local		*ailp = &objp->ctype.ai_info.ail;
-	ailp->next_action_time = Thief_wait_times[Difficulty_level]/2;
+	ailp->next_action_time = Thief_wait_times[GameUniqueState.Difficulty_level] / 2;
 	ailp->mode = ai_mode::AIM_THIEF_RETREAT;
 	if (rval) {
 		PALETTE_FLASH_ADD(30, 15, -20);

similar/main/ai.cpp
@@ -610,6 +610,7 @@ void init_ai_objects(void)
 	Boss_dying_sound_playing = 0;
 	Boss_dying = 0;
 
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 #if defined(DXX_BUILD_DESCENT_I)
 	Gate_interval = F1_0*5 - Difficulty_level*F1_0/2;
 #elif defined(DXX_BUILD_DESCENT_II)
@@ -877,6 +878,7 @@ static void ai_frame_animation(object &objp)
 // ----------------------------------------------------------------------------------
 static void set_next_fire_time(const vmobjptr_t objp, ai_local &ailp, const robot_info &robptr, const unsigned gun_num)
 {
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 #if defined(DXX_BUILD_DESCENT_I)
 	(void)objp;
 	(void)gun_num;
@@ -1011,6 +1013,7 @@ static int lead_player(const object_base &objp, const vms_vector &fire_point, co
 	const auto weapon_type = get_robot_weapon(Robot_info[get_robot_id(objp)], gun_num);
 
 	const weapon_info *const wptr = &Weapon_info[weapon_type];
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	fix max_weapon_speed = wptr->speed[Difficulty_level];
 	if (max_weapon_speed < F1_0)
 		return 0;
@@ -1059,6 +1062,7 @@ static void ai_fire_laser_at_player(const d_level_shared_segment_state &LevelSha
 #endif
 									)
 {
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	const auto powerup_flags = player_info.powerup_flags;
 	ai_local &ailp = obj->ctype.ai_info.ail;
 	auto &Robot_info = LevelSharedRobotInfoState.Robot_info;
@@ -1254,6 +1258,7 @@ static void move_towards_vector(object_base &objp, const vms_vector &vec_goal, i
 		dot = (F1_0+dot)/2;
 #endif
 
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	if (dot_based && (dot < 3*F1_0/4)) {
 		//	This funny code is supposed to slow down the robot and move his velocity towards his direction
 		//	more quickly than the general code
@@ -1339,6 +1344,7 @@ static void move_around_player(const vmobjptridx_t objp, const player_flags powe
 
 	auto &Robot_info = LevelSharedRobotInfoState.Robot_info;
 	auto &robptr = Robot_info[get_robot_id(objp)];
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	//	Note: -1 means normal circling about the player.  > 0 means fast evasion.
 	if (fast_flag > 0) {
 		fix	dot;
@@ -1367,7 +1373,7 @@ static void move_around_player(const vmobjptridx_t objp, const player_flags powe
 	pptr->velocity.y += evade_vector.y;
 	pptr->velocity.z += evade_vector.z;
 
-	auto speed = vm_vec_mag_quick(pptr->velocity);
+	const auto speed = vm_vec_mag_quick(pptr->velocity);
 	if (speed > robptr.max_speed[Difficulty_level]) {
 		pptr->velocity.x = (pptr->velocity.x*3)/4;
 		pptr->velocity.y = (pptr->velocity.y*3)/4;
@@ -1396,6 +1402,7 @@ static void move_away_from_player(const vmobjptridx_t objp, const vms_vector &ve
 
 	auto &Robot_info = LevelSharedRobotInfoState.Robot_info;
 	auto &robptr = Robot_info[get_robot_id(objp)];
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	if (speed > robptr.max_speed[Difficulty_level])
 	{
 		pptr->velocity.x = (pptr->velocity.x*3)/4;
@@ -1411,6 +1418,7 @@ static void move_away_from_player(const vmobjptridx_t objp, const vms_vector &ve
 //	If the flag evade_only is set, then only allowed to evade, not allowed to move otherwise (must have mode == AIM_STILL).
 static void ai_move_relative_to_player(const vmobjptridx_t objp, ai_local &ailp, const fix dist_to_player, const fix circle_distance, const int evade_only, const robot_to_player_visibility_state &player_visibility, const player_info &player_info)
 {
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	auto &vec_to_player = player_visibility.vec_to_player;
 	auto &Robot_info = LevelSharedRobotInfoState.Robot_info;
 	auto &robptr = Robot_info[get_robot_id(objp)];
@@ -1637,6 +1645,7 @@ static void compute_vis_and_vec(fvmsegptridx &vmsegptridx, const vmobjptridx_t o
 {
 	if (player_visibility.initialized)
 		return;
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	const auto powerup_flags = player_info.powerup_flags;
 		if (powerup_flags & PLAYER_FLAGS_CLOAKED)
 		{
@@ -1752,7 +1761,7 @@ static void compute_buddy_vis_vec(const vmobjptridx_t buddy_obj, const vms_vecto
 
 	auto &ailp = buddy_obj->ctype.ai_info.ail;
 	player_visibility.visibility = (hit_type == HIT_NONE)
-		? ((ailp.time_player_seen = GameTime64, vm_vec_dot(player_visibility.vec_to_player, buddy_obj->orient.fvec) > robptr.field_of_view[Difficulty_level])
+		? ((ailp.time_player_seen = GameTime64, vm_vec_dot(player_visibility.vec_to_player, buddy_obj->orient.fvec) > robptr.field_of_view[GameUniqueState.Difficulty_level])
 		   ? player_visibility_state::visible_and_in_field_of_view
 		   : player_visibility_state::visible_not_in_field_of_view
 		)
@@ -1965,6 +1974,7 @@ static int check_object_object_intersection(const vms_vector &pos, fix size, con
 //	If pos == NULL, pick random spot in segment.
 static imobjptridx_t create_gated_robot(const d_vclip_array &Vclip, fvcobjptr &vcobjptr, const vmsegptridx_t segp, const unsigned object_id, const vms_vector *const pos)
 {
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 #if defined(DXX_BUILD_DESCENT_I)
 	const unsigned maximum_gated_robots = 2*Difficulty_level + 3;
 #elif defined(DXX_BUILD_DESCENT_II)
@@ -2667,7 +2677,7 @@ static void ai_do_actual_firing_stuff(fvmobjptridx &vmobjptridx, const vmobjptri
 					if (robptr.attack_type == 1) {
 						if (Player_dead_state != player_dead_state::exploded &&
 							dist_to_player < obj->size + ConsoleObject->size + F1_0*2)
-						{		// robptr.circle_distance[Difficulty_level] + ConsoleObject->size)
+						{
 							if (!ai_multiplayer_awareness(obj, ROBOT_FIRE_AGITATION-2))
 								return;
 							do_ai_robot_hit_attack(obj, vmobjptridx(ConsoleObject), obj->pos);
@@ -2754,7 +2764,7 @@ static void ai_do_actual_firing_stuff(fvmobjptridx &vmobjptridx, const vmobjptri
 					if (robptr.attack_type == 1) {
 						if (Player_dead_state != player_dead_state::exploded &&
 							dist_to_player < obj->size + ConsoleObject->size + F1_0*2)
-						{		// robptr.circle_distance[Difficulty_level] + ConsoleObject->size)
+						{
 							if (!ai_multiplayer_awareness(obj, ROBOT_FIRE_AGITATION-2))
 								return;
 							do_ai_robot_hit_attack(obj, vmobjptridx(ConsoleObject), obj->pos);
@@ -2831,7 +2841,7 @@ static void ai_do_actual_firing_stuff(fvmobjptridx &vmobjptridx, const vmobjptri
 
 		vms_vector	vec_to_last_pos;
 
-		if (d_rand()/2 < fixmul(FrameTime, (Difficulty_level << 12) + 0x4000)) {
+		if (d_rand()/2 < fixmul(FrameTime, (GameUniqueState.Difficulty_level << 12) + 0x4000)) {
 		if ((!object_animates || ready_to_fire_any_weapon(robptr, ailp, 0)) && (Dist_to_last_fired_upon_player_pos < FIRE_AT_NEARBY_PLAYER_THRESHOLD)) {
 			vm_vec_normalized_dir_quick(vec_to_last_pos, Believed_player_pos, obj->pos);
 			dot = vm_vec_dot(obj->orient.fvec, vec_to_last_pos);
@@ -2841,7 +2851,7 @@ static void ai_do_actual_firing_stuff(fvmobjptridx &vmobjptridx, const vmobjptri
 					if (robptr.attack_type == 1) {
 						if (Player_dead_state != player_dead_state::exploded &&
 							dist_to_player < obj->size + ConsoleObject->size + F1_0*2)
-						{		// robptr.circle_distance[Difficulty_level] + ConsoleObject->size)
+						{
 							if (!ai_multiplayer_awareness(obj, ROBOT_FIRE_AGITATION-2))
 								return;
 							do_ai_robot_hit_attack(obj, vmobjptridx(ConsoleObject), obj->pos);
@@ -3068,6 +3078,7 @@ static bool skip_ai_for_time_splice(const vcobjptridx_t robot, const robot_info 
 // --------------------------------------------------------------------------------------------------------------------
 void do_ai_frame(const vmobjptridx_t obj)
 {
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	const objnum_t &objnum = obj;
 	ai_static	*aip = &obj->ctype.ai_info;
 	ai_local &ailp = obj->ctype.ai_info.ail;
@@ -3485,7 +3496,7 @@ _exit_cheat:
 		fix sval, rval;
 
 		rval = d_rand();
-		sval = (dist_to_player * (Difficulty_level+1))/64;
+		sval = (dist_to_player * (static_cast<int>(Difficulty_level) + 1)) / 64;
 
 		if ((fixmul(rval, sval) < FrameTime) || (player_info.powerup_flags & PLAYER_FLAGS_HEADLIGHT_ON)) {
 			ailp.player_awareness_type = player_awareness_type_t::PA_PLAYER_COLLISION;
@@ -3896,7 +3907,7 @@ _exit_cheat:
 					ailp.mode = ai_mode::AIM_CHASE_OBJECT;
 				// This should not just be distance based, but also time-since-player-seen based.
 			}
-			else if ((dist_to_player > F1_0*(20*(2*Difficulty_level + robptr.pursuit)))
+			else if ((dist_to_player > F1_0 * (20 * (2 * static_cast<int>(Difficulty_level) + robptr.pursuit)))
 				&& (GameTime64 - ailp.time_player_seen > (F1_0/2*(Difficulty_level + robptr.pursuit)))
 				&& !player_is_visible(player_visibility.visibility)
 				&& (aip->behavior == ai_behavior::AIB_NORMAL)
@@ -3980,7 +3991,7 @@ _exit_cheat:
 			break;
 
 		case ai_mode::AIM_STILL:
-			if ((dist_to_player < F1_0*120+Difficulty_level*F1_0*20) || (static_cast<unsigned>(ailp.player_awareness_type) >= static_cast<unsigned>(player_awareness_type_t::PA_WEAPON_ROBOT_COLLISION) - 1))
+			if ((dist_to_player < F1_0 * 120 + static_cast<int>(Difficulty_level) * F1_0 * 20) || (static_cast<unsigned>(ailp.player_awareness_type) >= static_cast<unsigned>(player_awareness_type_t::PA_WEAPON_ROBOT_COLLISION) - 1))
 			{
 				compute_vis_and_vec(vmsegptridx, obj, player_info, vis_vec_pos, ailp, player_visibility, robptr);
 

similar/main/aipath.cpp
@@ -1276,6 +1276,7 @@ void ai_path_set_orient_and_vel(const vmobjptr_t objp, const vms_vector &goal_po
 	fix			max_speed;
 
 	//	If evading player, use highest difficulty level speed, plus something based on diff level
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	max_speed = robptr.max_speed[Difficulty_level];
 	ai_local		*ailp = &objp->ctype.ai_info.ail;
 	if (ailp->mode == ai_mode::AIM_RUN_FROM_OBJECT

similar/main/cntrlcen.cpp
@@ -208,7 +208,7 @@ window_event_result do_countdown_frame()
 		};
 		fix disturb_x = get_base_disturbance(), disturb_z = get_base_disturbance();
 		//	At Trainee, decrease rocking of ship by 4x.
-		if (Difficulty_level == Difficulty_0)
+		if (GameUniqueState.Difficulty_level == Difficulty_0)
 		{
 			disturb_x /= 4;
 			disturb_z /= 4;
@@ -290,6 +290,7 @@ void do_controlcen_destroyed_stuff(const imobjptridx_t objp)
 	// And start the countdown stuff.
 	Control_center_destroyed = 1;
 
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 #if defined(DXX_BUILD_DESCENT_II)
 	// If a secret level, delete secret.sgc to indicate that we can't return to our secret level.
 	if (Current_level_num < 0)
@@ -435,6 +436,7 @@ void do_controlcen_frame(const vmobjptridx_t obj)
 				count++;
 			}
 
+			const auto Difficulty_level = GameUniqueState.Difficulty_level;
 			delta_fire_time = (NDL - Difficulty_level) * F1_0/4;
 #if defined(DXX_BUILD_DESCENT_II)
 			if (Difficulty_level == 0)
@@ -532,7 +534,7 @@ void init_controlcen_for_level(void)
 void special_reactor_stuff()
 {
 	if (Control_center_destroyed) {
-		Countdown_timer += i2f(Base_control_center_explosion_time + (NDL-1-Difficulty_level)*Base_control_center_explosion_time/(NDL-1));
+		Countdown_timer += i2f(Base_control_center_explosion_time + (NDL - 1 - GameUniqueState.Difficulty_level) * Base_control_center_explosion_time / (NDL - 1));
 		Total_countdown_time = f2i(Countdown_timer)+2;	//	Will prevent "Self destruct sequence activated" message from replaying.
 	}
 }

similar/main/collide.cpp
@@ -244,7 +244,7 @@ static void apply_force_damage(const vmobjptridx_t obj,fix force,const vmobjptri
 
 #if defined(DXX_BUILD_DESCENT_II)
 			//	Make trainee easier.
-			if (Difficulty_level == 0)
+			if (GameUniqueState.Difficulty_level == 0)
 				damage /= 2;
 #endif
 
@@ -293,6 +293,7 @@ static void bump_this_object(const vmobjptridx_t objp, const vmobjptridx_t other
 		} else if ((objp->type == OBJ_ROBOT) || (objp->type == OBJ_CLUTTER) || (objp->type == OBJ_CNTRLCEN)) {
 			if (!Robot_info[get_robot_id(objp)].boss_flag) {
 				vms_vector force2;
+				const auto Difficulty_level = GameUniqueState.Difficulty_level;
 				force2.x = force.x/(4 + Difficulty_level);
 				force2.y = force.y/(4 + Difficulty_level);
 				force2.z = force.z/(4 + Difficulty_level);
@@ -469,7 +470,7 @@ volatile_wall_result check_volatile_wall(const vmobjptridx_t obj, const unique_s
 				fix damage = fixmul(d,((FrameTime>DESIGNATED_GAME_FRAMETIME)?FrameTime:DESIGNATED_GAME_FRAMETIME));
 
 #if defined(DXX_BUILD_DESCENT_II)
-				if (Difficulty_level == 0)
+				if (GameUniqueState.Difficulty_level == 0)
 					damage /= 2;
 #endif
 
@@ -824,6 +825,7 @@ static window_event_result collide_weapon_and_wall(
 
 	const auto wall_type = wall_hit_process(player_info.powerup_flags, hitseg, hitwall, weapon->shields, playernum, weapon);
 
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	// Wall is volatile if either tmap 1 or 2 is volatile
 	if ((TmapInfo[uhitside.tmap_num].flags & TMI_VOLATILE) ||
 		(uhitside.tmap_num2 && (TmapInfo[uhitside.tmap_num2 & 0x3fff].flags & TMI_VOLATILE))
@@ -1519,7 +1521,7 @@ enum class boss_weapon_collision_result : uint8_t
 
 static inline int Boss_invulnerable_dot()
 {
-	return F1_0/4 - i2f(Difficulty_level)/8;
+	return F1_0 / 4 - i2f(GameUniqueState.Difficulty_level) / 8;
 }
 
 //	------------------------------------------------------------------------------------------------------
@@ -1670,6 +1672,7 @@ static void collide_robot_and_weapon(const vmobjptridx_t  robot, const vmobjptri
 		return;
 
 #if defined(DXX_BUILD_DESCENT_II)
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	//	Changed, 10/04/95, put out blobs based on skill level and power of weapon doing damage.
 	//	Also, only a weapon hit from a player weapon causes smart blobs.
 	if ((weapon->ctype.laser_info.parent_type == OBJ_PLAYER) && (robptr.energy_blobs))
@@ -2236,9 +2239,7 @@ void collide_player_and_nasty_robot(const vmobjptridx_t playerobj, const vmobjpt
 
 	bump_two_objects(playerobj, robot, 0);	//no damage from bump
 
-	apply_damage_to_player( playerobj, robot, F1_0*(Difficulty_level+1), 0);
-
-	return;
+	apply_damage_to_player(playerobj, robot, F1_0 * (GameUniqueState.Difficulty_level + 1), 0);
 }
 
 static vms_vector find_exit_direction(vms_vector result, const vcobjptr_t objp, const vcsegptr_t segp)

similar/main/controls.cpp
@@ -77,7 +77,7 @@ void read_flying_controls(object &obj)
 			return false;
 		auto &gmobj = *gimobj;
 		const auto gmid = get_weapon_id(gmobj);
-		const auto speed = Weapon_info[gmid].speed[Difficulty_level];
+		const auto speed = Weapon_info[gmid].speed[GameUniqueState.Difficulty_level];
 		vms_angvec rotangs;
 		//this is a horrible hack.  guided missile stuff should not be
 		//handled in the middle of a routine that is dealing with the player

similar/main/fireball.cpp
@@ -206,6 +206,7 @@ static imobjptridx_t object_create_explosion_sub(const d_vclip_array &Vclip, fvm
 								}
 #endif
 
+								const auto Difficulty_level = GameUniqueState.Difficulty_level;
 								//	When a robot gets whacked by a badass force, he looks towards it because robots tend to get blasted from behind.
 								{
 									vms_vector neg_vforce;
@@ -284,7 +285,7 @@ static imobjptridx_t object_create_explosion_sub(const d_vclip_array &Vclip, fvm
 								if (obj0p->shields >= 0)
 								{
 #if defined(DXX_BUILD_DESCENT_II)
-								if (Difficulty_level == 0)
+									if (GameUniqueState.Difficulty_level == 0)
 									damage /= 4;
 #endif
 									apply_damage_to_player(obj0p, killer, damage, 1 );
@@ -348,6 +349,7 @@ void explode_badass_weapon(const vmobjptridx_t obj,const vms_vector &pos)
 #endif
 	digi_link_sound_to_object(SOUND_BADASS_EXPLOSION, obj, 0, F1_0, sound_stack::allow_stacking);
 
+	const auto Difficulty_level = GameUniqueState.Difficulty_level;
 	object_create_badass_explosion(obj, vmsegptridx(obj->segnum), pos,
 	                                      wi->impact_size,
 	                                      wi->robot_hit_vclip,

commit 075b14373a0f6a0f341f7fa5b953301043d98aa2

Change Difficulty_level_type from unsigned to signed
Difficulty_level_type is used in arithmetic expressions.  If
Difficulty_level_type is unsigned, then those expressions use unsigned
terms, but some of the expressions were designed to use signed terms and
produce incorrect results when used with unsigned terms.  There is no
strong reason to make Difficulty_level_type unsigned, so switch it to
signed instead of trying to fix every site that uses it.

Reported-by: AlumiuN <https://github.com/dxx-rebirth/dxx-rebirth/issues/471>
Fixes: 1eaaff3016cc7631e751e451ac1c8a9973709e22 ("Move Difficulty_level to GameUniqueState")

common/main/game.h
@@ -127,7 +127,11 @@ void calc_d_tick();
 
 extern int Game_suspended;          // if non-zero, nothing moves but player
 
-enum Difficulty_level_type : unsigned
+/* This must be a signed type.  Some sites, such as `bump_this_object`,
+ * use Difficulty_level_type in arithmetic expressions, and those
+ * expressions must be signed to produce the correct result.
+ */
+enum Difficulty_level_type : signed int
 {
 	Difficulty_0,
 	Difficulty_1,