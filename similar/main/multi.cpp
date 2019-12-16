6b637e856b65b7339a0a24b2b56978a717b00020
Kp <kp@valhallalegends.com>
2019-12-16 01:56:59 +0000

    Copy last_hitobj to a temporary before passing it to reset_hitobj
    obj_rw is a packed structure, so configurations where reset_hitobj takes
    its parameter by reference (or where its parameter requires passing
    through a constructor that takes its input by reference) fail because
    gcc refuses to bind a reference to a member of a packed structure.  Copy
    the member variable into a local, which gcc will bind.

--- similar/main/multi.cpp
+++ similar/main/multi.cpp
@@ -6094,7 +6094,10 @@ void multi_object_rw_to_object(object_rw *obj_rw, const vmobjptr_t obj)
 			obj->ctype.laser_info.parent_num       = obj_rw->ctype.laser_info.parent_num;
 			obj->ctype.laser_info.parent_signature = object_signature_t{static_cast<uint16_t>(obj_rw->ctype.laser_info.parent_signature)};
 			obj->ctype.laser_info.creation_time    = obj_rw->ctype.laser_info.creation_time;
-			obj->ctype.laser_info.reset_hitobj(obj_rw->ctype.laser_info.last_hitobj);
+			{
+				const auto last_hitobj = obj_rw->ctype.laser_info.last_hitobj;
+				obj->ctype.laser_info.reset_hitobj(last_hitobj);
+			}
 			obj->ctype.laser_info.track_goal       = obj_rw->ctype.laser_info.track_goal;
 			obj->ctype.laser_info.multiplier       = obj_rw->ctype.laser_info.multiplier;
 #if defined(DXX_BUILD_DESCENT_II)
