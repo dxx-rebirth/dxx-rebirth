dxx-rebirth/dxx-rebirth

commit 59b94a4dcb5c471109be9c9cb5692d715cbb7fed

Call arch_close before atexit hooks begin
arch_close calls various library shutdown routines, some of which may
not be in good order after atexit hooks begin executing.  Call it before
returning from main, so that the libraries are still fully initialized.

similar/main/inferno.cpp
@@ -562,7 +562,7 @@ static int main(int argc, char *argv[])
 
 	PHYSFSX_addArchiveContent();
 
-	arch_init();
+	const auto &&arch_atexit_result = arch_init();
 
 #if !DXX_USE_OGL
 	select_tmap(CGameArg.DbgTexMap);

common/include/fwd-window.h
@@ -13,7 +13,13 @@
 
 #ifdef dsx
 namespace dsx {
-void arch_init();
+struct arch_atexit
+{
+	~arch_atexit();
+};
+
+__attribute_warn_unused_result
+arch_atexit arch_init();
 }
 #endif
 namespace dcx {

similar/arch/sdl/init.cpp
@@ -39,7 +39,12 @@ static void arch_close(void)
 	SDL_Quit();
 }
 
-void arch_init(void)
+arch_atexit::~arch_atexit()
+{
+	arch_close();
+}
+
+arch_atexit arch_init()
 {
 	int t;
 
@@ -79,7 +84,7 @@ void arch_init(void)
 	if ((t = gr_init()) != 0)
 		Error(TXT_CANT_INIT_GFX,t);
 
-	atexit(arch_close);
+	return {};
 }
 
 }

commit 19c5f4064e2fc4f159b48697b83e75b966918fca

Fix bogus unused variable warning from clang in inferno.cpp
Fixes: 59b94a4dcb5c471109be9c9cb5692d715cbb7fed ("Call arch_close before atexit hooks begin")

similar/main/inferno.cpp
@@ -563,6 +563,12 @@ static int main(int argc, char *argv[])
 	PHYSFSX_addArchiveContent();
 
 	const auto &&arch_atexit_result = arch_init();
+	/* This variable exists for the side effects that occur when it is
+	 * destroyed.  clang-9 fails to recognize those side effects as a
+	 * "use" and warns that the variable is unused.  Cast it to void to
+	 * create a "use" to suppress the warning.
+	 */
+	(void)arch_atexit_result;
 
 #if !DXX_USE_OGL
 	select_tmap(CGameArg.DbgTexMap);