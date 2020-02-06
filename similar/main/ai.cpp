dxx-rebirth/dxx-rebirth

commit 595c3ca086acc18a0a6040c5bb87efe6c6ca6fc5

Move Boss_cloak_interval to d_level_shared_boss_state

similar/main/ai.cpp
@@ -160,11 +160,6 @@ static
 const
 #endif
 fix             Boss_teleport_interval = F1_0*8;
-static
-#if defined(DXX_BUILD_DESCENT_I)
-const
-#endif
-fix             Boss_cloak_interval = F1_0*10;                    //    Time between cloaks
 }
 namespace dcx {
 static fix64 Boss_dying_start_time;
@@ -622,11 +617,11 @@ void init_ai_objects(void)
 	if (Current_mission && (Current_level_num == Last_level))
 	{
 		Boss_teleport_interval = F1_0*10;
-		Boss_cloak_interval = F1_0*15;					//	Time between cloaks
+		LevelSharedBossState.Boss_cloak_interval = F1_0*15;					//	Time between cloaks
 	} else
 	{
 		Boss_teleport_interval = F1_0*7;
-		Boss_cloak_interval = F1_0*10;					//	Time between cloaks
+		LevelSharedBossState.Boss_cloak_interval = F1_0*10;					//	Time between cloaks
 	}
 #endif
 }
@@ -2510,7 +2505,7 @@ static void do_d1_boss_stuff(fvmsegptridx &vmsegptridx, const vmobjptridx_t objp
 				objp->ctype.ai_info.CLOAKED = 0;
 		} else {
 			if (Boss_hit_this_frame ||
-				GameTime64 - (Boss_cloak_start_time + Boss_cloak_duration) > Boss_cloak_interval)
+				GameTime64 - (Boss_cloak_start_time + Boss_cloak_duration) > LevelSharedBossState.Boss_cloak_interval)
 			{
 				if (ai_multiplayer_awareness(objp, 95))
 				{
@@ -2617,7 +2612,7 @@ static void do_d2_boss_stuff(fvmsegptridx &vmsegptridx, const vmobjptridx_t objp
 			if (GameTime64 > (Boss_cloak_start_time + Boss_cloak_duration) ||
 				GameTime64 < Boss_cloak_start_time)
 				objp->ctype.ai_info.CLOAKED = 0;
-		} else if (GameTime64 - (Boss_cloak_start_time + Boss_cloak_duration) > Boss_cloak_interval ||
+		} else if (GameTime64 - (Boss_cloak_start_time + Boss_cloak_duration) > LevelSharedBossState.Boss_cloak_interval ||
 				GameTime64 - (Boss_cloak_start_time + Boss_cloak_duration) < -Boss_cloak_duration) {
 			if (ai_multiplayer_awareness(objp, 95)) {
 				Boss_cloak_start_time = GameTime64;
@@ -4719,7 +4714,10 @@ int ai_save_state(PHYSFS_File *fp)
 		tmptime32 = Last_teleport_time - GameTime64;
 	PHYSFS_write(fp, &tmptime32, sizeof(fix), 1);
 	PHYSFS_write(fp, &Boss_teleport_interval, sizeof(fix), 1);
-	PHYSFS_write(fp, &Boss_cloak_interval, sizeof(fix), 1);
+	{
+		const fix Boss_cloak_interval = LevelSharedBossState.Boss_cloak_interval;
+		PHYSFS_write(fp, &Boss_cloak_interval, sizeof(fix), 1);
+	}
 	PHYSFS_write(fp, &Boss_cloak_duration, sizeof(fix), 1);
 	if (BossUniqueState.Last_gate_time - GameTime64 < F1_0*(-18000))
 		tmptime32 = F1_0*(-18000);
@@ -4948,7 +4946,7 @@ int ai_restore_state(PHYSFS_File *fp, int version, int swap)
 #endif
 		PHYSFSX_readSXE32(fp, swap);
 #if defined(DXX_BUILD_DESCENT_II)
-	Boss_cloak_interval =
+	LevelSharedBossState.Boss_cloak_interval =
 #endif
 		PHYSFSX_readSXE32(fp, swap);
 	PHYSFSX_readSXE32(fp, swap);

common/main/ai.h
@@ -109,7 +109,6 @@ struct d_level_shared_boss_state
 	teleport_segment_array_t Teleport_segs;
 };
 
-extern d_level_shared_boss_state LevelSharedBossState;
 extern fix64 Boss_cloak_start_time;
 extern fix64 Last_teleport_time;
 constexpr fix Boss_cloak_duration = F1_0*7;
@@ -119,6 +118,18 @@ extern vms_vector Believed_player_pos;
 }
 
 namespace dsx {
+
+struct d_level_shared_boss_state : ::dcx::d_level_shared_boss_state
+{
+	// Time between cloaks
+#if defined(DXX_BUILD_DESCENT_I)
+	static constexpr std::integral_constant<fix, F1_0 * 10> Boss_cloak_interval{};
+#elif defined(DXX_BUILD_DESCENT_II)
+	fix Boss_cloak_interval;
+#endif
+};
+
+extern d_level_shared_boss_state LevelSharedBossState;
 void move_towards_segment_center(const d_level_shared_segment_state &, object_base &objp);
 imobjptridx_t gate_in_robot(unsigned type, vmsegptridx_t segnum);
 void do_ai_frame(vmobjptridx_t objp);

similar/main/mglobal.cpp
@@ -52,7 +52,6 @@ static void reconstruct_global_variable(T &t)
 
 d_interface_unique_state InterfaceUniqueState;
 d_game_unique_state GameUniqueState;
-d_level_shared_boss_state LevelSharedBossState;
 d_level_shared_vertex_state LevelSharedVertexState;
 d_level_unique_fuelcenter_state LevelUniqueFuelcenterState;
 d_level_unique_segment_state LevelUniqueSegmentState;
@@ -116,6 +115,7 @@ valptridx<managed_type>::array_managed_type::array_managed_type()
 #endif
 
 namespace dsx {
+d_level_shared_boss_state LevelSharedBossState;
 d_level_unique_effects_clip_state LevelUniqueEffectsClipState;
 d_level_shared_segment_state LevelSharedSegmentState;
 d_level_unique_object_state LevelUniqueObjectState;

commit 74271443ed4c8167722f4eb667e16baea75684cc

Add definition of d_level_shared_boss_state::D1_Boss_*_interval
These variables are used, but optimized builds eliminate all reference
to their address, so the build normally succeeds.  Unoptimized builds
retain references to the address, so a definition is required for them.

Reported-by: tycho <https://github.com/dxx-rebirth/dxx-rebirth/issues/496>
Fixes: 595c3ca086acc18a0a6040c5bb87efe6c6ca6fc5 ("Move Boss_cloak_interval to d_level_shared_boss_state")
Fixes: c61dee7e6ad0e64078bfdd74e7a40888b898ae7a ("Move Boss_teleport_interval to d_level_shared_boss_state")

similar/main/ai.cpp
@@ -96,6 +96,10 @@ static void ai_multi_send_robot_position(object &objnum, int force);
 
 #if defined(DXX_BUILD_DESCENT_I)
 #define	BOSS_DEATH_SOUND_DURATION	0x2ae14		//	2.68 seconds
+
+constexpr d_level_shared_boss_state::D1_Boss_cloak_interval d_level_shared_boss_state::Boss_cloak_interval;
+constexpr d_level_shared_boss_state::D1_Boss_teleport_interval d_level_shared_boss_state::Boss_teleport_interval;
+
 #elif defined(DXX_BUILD_DESCENT_II)
 #define	FIRE_AT_NEARBY_PLAYER_THRESHOLD	(F1_0*40)
 