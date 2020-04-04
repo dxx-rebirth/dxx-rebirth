dxx-rebirth/dxx-rebirth

commit d355ef4030ef4c8e9e3a153fa2c04e56192edc26

Pass font to various drawing functions

similar/main/newmenu.cpp
@@ -129,24 +129,24 @@ void newmenu_free_background()	{
 namespace dsx {
 
 #if defined(DXX_BUILD_DESCENT_I)
-static const char *UP_ARROW_MARKER(grs_canvas &)
+static const char *UP_ARROW_MARKER(const grs_font &, const grs_font &)
 {
 	return "+";  // 135
 }
 
-static const char *DOWN_ARROW_MARKER(grs_canvas &)
+static const char *DOWN_ARROW_MARKER(const grs_font &, const grs_font &)
 {
 	return "+";  // 136
 }
 #elif defined(DXX_BUILD_DESCENT_II)
-static const char *UP_ARROW_MARKER(grs_canvas &canvas)
+static const char *UP_ARROW_MARKER(const grs_font &cv_font, const grs_font &game_font)
 {
-	return canvas.cv_font == GAME_FONT.get() ? "\202" : "\207";  // 135
+	return &cv_font == &game_font ? "\202" : "\207";  // 135
 }
 
-static const char *DOWN_ARROW_MARKER(grs_canvas &canvas)
+static const char *DOWN_ARROW_MARKER(const grs_font &cv_font, const grs_font &game_font)
 {
-	return canvas.cv_font == GAME_FONT.get() ? "\200" : "\210";  // 136
+	return &cv_font == &game_font ? "\200" : "\210";  // 136
 }
 #endif
 
@@ -781,7 +781,7 @@ static window_event_result newmenu_mouse(window *wind,const d_event &event, newm
 
 					if (menu->scroll_offset != 0) {
 						int arrow_width, arrow_height;
-						gr_get_string_size(*canvas.cv_font, UP_ARROW_MARKER(canvas), &arrow_width, &arrow_height, nullptr);
+						gr_get_string_size(*canvas.cv_font, UP_ARROW_MARKER(*canvas.cv_font, *GAME_FONT), &arrow_width, &arrow_height, nullptr);
 						x1 = canvas.cv_bitmap.bm_x + BORDERX - fspacx(12);
 						y1 = canvas.cv_bitmap.bm_y + menu->items[menu->scroll_offset].y - (line_spacing * menu->scroll_offset);
 						x2 = x1 + arrow_width;
@@ -792,7 +792,7 @@ static window_event_result newmenu_mouse(window *wind,const d_event &event, newm
 					}
 					if (menu->scroll_offset+menu->max_displayable<menu->nitems) {
 						int arrow_width, arrow_height;
-						gr_get_string_size(*canvas.cv_font, DOWN_ARROW_MARKER(canvas), &arrow_width, &arrow_height, nullptr);
+						gr_get_string_size(*canvas.cv_font, DOWN_ARROW_MARKER(*canvas.cv_font, *GAME_FONT), &arrow_width, &arrow_height, nullptr);
 						x1 = canvas.cv_bitmap.bm_x + BORDERX - fspacx(12);
 						y1 = canvas.cv_bitmap.bm_y + menu->items[menu->scroll_offset + menu->max_displayable - 1].y - (line_spacing * menu->scroll_offset);
 						x2 = x1 + arrow_width;
@@ -1186,21 +1186,19 @@ static void newmenu_create_structure( newmenu *menu )
 	gr_set_default_canvas();
 	auto &canvas = *grd_curcanv;
 
-	const grs_font *save_font = canvas.cv_font;
-
 	tw = th = 0;
 
 	if ( menu->title )	{
-		gr_set_curfont(canvas, HUGE_FONT);
 		int string_width, string_height;
-		gr_get_string_size(*canvas.cv_font, menu->title, &string_width, &string_height, nullptr);
+		auto &huge_font = *HUGE_FONT;
+		gr_get_string_size(huge_font, menu->title, &string_width, &string_height, nullptr);
 		tw = string_width;
 		th = string_height;
 	}
 	if ( menu->subtitle )	{
-		gr_set_curfont(canvas, MEDIUM3_FONT);
 		int string_width, string_height;
-		gr_get_string_size(*canvas.cv_font, menu->subtitle, &string_width, &string_height, nullptr);
+		auto &medium3_font = *MEDIUM3_FONT;
+		gr_get_string_size(medium3_font, menu->subtitle, &string_width, &string_height, nullptr);
 		if (string_width > tw )
 			tw = string_width;
 		th += string_height;
@@ -1208,7 +1206,7 @@ static void newmenu_create_structure( newmenu *menu )
 
 	th += FSPACY(5);		//put some space between titles & body
 
-	gr_set_curfont(canvas, menu->tiny_mode?GAME_FONT:MEDIUM1_FONT);
+	auto &cv_font = *(menu->tiny_mode ? GAME_FONT : MEDIUM1_FONT).get();
 
 	menu->w = aw = 0;
 	menu->h = th;
@@ -1221,7 +1219,7 @@ static void newmenu_create_structure( newmenu *menu )
 	{
 		i.y = menu->h;
 		int string_width, string_height, average_width;
-		gr_get_string_size(*canvas.cv_font, i.text, &string_width, &string_height, &average_width);
+		gr_get_string_size(cv_font, i.text, &string_width, &string_height, &average_width);
 		i.right_offset = 0;
 
 		i.saved_text[0] = '\0';
@@ -1237,7 +1235,7 @@ static void newmenu_create_structure( newmenu *menu )
 				index += snprintf(i.saved_text.data() + index, i.saved_text.size() - index, "%s", SLIDER_MIDDLE);
 			}
 			index += snprintf(i.saved_text.data() + index, i.saved_text.size() - index, "%s", SLIDER_RIGHT);
-			gr_get_string_size(*canvas.cv_font, i.saved_text.data(), &w1, nullptr, nullptr);
+			gr_get_string_size(cv_font, i.saved_text.data(), &w1, nullptr, nullptr);
 			string_width += w1 + aw;
 		}
 
@@ -1250,9 +1248,9 @@ static void newmenu_create_structure( newmenu *menu )
 		{
 			int w1;
 			nothers++;
-			gr_get_string_size(*canvas.cv_font, NORMAL_CHECK_BOX, &w1, nullptr, nullptr);
+			gr_get_string_size(cv_font, NORMAL_CHECK_BOX, &w1, nullptr, nullptr);
 			i.right_offset = w1;
-			gr_get_string_size(*canvas.cv_font, CHECKED_CHECK_BOX, &w1, nullptr, nullptr);
+			gr_get_string_size(cv_font, CHECKED_CHECK_BOX, &w1, nullptr, nullptr);
 			if (w1 > i.right_offset)
 				i.right_offset = w1;
 		}
@@ -1261,9 +1259,9 @@ static void newmenu_create_structure( newmenu *menu )
 		{
 			int w1;
 			nothers++;
-			gr_get_string_size(*canvas.cv_font, NORMAL_RADIO_BOX, &w1, nullptr, nullptr);
+			gr_get_string_size(cv_font, NORMAL_RADIO_BOX, &w1, nullptr, nullptr);
 			i.right_offset = w1;
-			gr_get_string_size(*canvas.cv_font, CHECKED_RADIO_BOX, &w1, nullptr, nullptr);
+			gr_get_string_size(cv_font, CHECKED_RADIO_BOX, &w1, nullptr, nullptr);
 			if (w1 > i.right_offset)
 				i.right_offset = w1;
 		}
@@ -1275,10 +1273,10 @@ static void newmenu_create_structure( newmenu *menu )
 			nothers++;
 			auto &number = i.number();
 			snprintf(test_text, sizeof(test_text), "%d", number.max_value);
-			gr_get_string_size(*canvas.cv_font, test_text, &w1, nullptr, nullptr);
+			gr_get_string_size(cv_font, test_text, &w1, nullptr, nullptr);
 			i.right_offset = w1;
 			snprintf(test_text, sizeof(test_text), "%d", number.min_value);
-			gr_get_string_size(*canvas.cv_font, test_text, &w1, nullptr, nullptr);
+			gr_get_string_size(cv_font, test_text, &w1, nullptr, nullptr);
 			if (w1 > i.right_offset)
 				i.right_offset = w1;
 		}
@@ -1316,7 +1314,7 @@ static void newmenu_create_structure( newmenu *menu )
 	if (menu->nitems > menu->max_on_menu)
 	{
 		menu->is_scroll_box=1;
-		menu->h = th + (LINE_SPACING(*canvas.cv_font, *GAME_FONT) * menu->max_on_menu);
+		menu->h = th + (LINE_SPACING(cv_font, *GAME_FONT) * menu->max_on_menu);
 		menu->max_displayable=menu->max_on_menu;
 
 		// if our last citem was > menu->max_on_menu, make sure we re-scroll when we call this menu again
@@ -1354,13 +1352,13 @@ static void newmenu_create_structure( newmenu *menu )
 	menu->w += BORDERX*2;
 	menu->h += BORDERY*2;
 
-	menu->x = (grd_curcanv->cv_bitmap.bm_w - menu->w) / 2;
-	menu->y = (grd_curcanv->cv_bitmap.bm_h - menu->h) / 2;
+	menu->x = (canvas.cv_bitmap.bm_w - menu->w) / 2;
+	menu->y = (canvas.cv_bitmap.bm_h - menu->h) / 2;
 
 	if ( menu->x < 0 ) menu->x = 0;
 	if ( menu->y < 0 ) menu->y = 0;
 
-	nm_draw_background1(*grd_curcanv, menu->filename);
+	nm_draw_background1(canvas, menu->filename);
 
 	// Update all item's x & y values.
 	range_for (auto &i, menu->item_range())
@@ -1408,7 +1406,6 @@ static void newmenu_create_structure( newmenu *menu )
 	menu->sheight = SHEIGHT;
 	menu->fntscalex = FNTScaleX;
 	menu->fntscaley = FNTScaleY;
-	gr_set_curfont(canvas, save_font);
 	gr_set_current_canvas(save_canvas);
 }
 
@@ -1437,18 +1434,18 @@ static window_event_result newmenu_draw(window *wind, newmenu *menu)
 	ty = BORDERY;
 
 	if ( menu->title )	{
-		gr_set_curfont(*grd_curcanv, HUGE_FONT);
 		gr_set_fontcolor(*grd_curcanv, BM_XRGB(31, 31, 31), -1);
 		int string_width, string_height;
-		gr_get_string_size(*grd_curcanv->cv_font, menu->title, &string_width, &string_height, nullptr);
+		auto &huge_font = *HUGE_FONT;
+		gr_get_string_size(huge_font, menu->title, &string_width, &string_height, nullptr);
 		th = string_height;
-		gr_string(*grd_curcanv, *grd_curcanv->cv_font, 0x8000, ty, menu->title, string_width, string_height);
+		gr_string(*grd_curcanv, huge_font, 0x8000, ty, menu->title, string_width, string_height);
 	}
 
 	if ( menu->subtitle )	{
-		gr_set_curfont(*grd_curcanv, MEDIUM3_FONT);
 		gr_set_fontcolor(*grd_curcanv, BM_XRGB(21, 21, 21), -1);
-		gr_string(*grd_curcanv, *grd_curcanv->cv_font, 0x8000, ty + th, menu->subtitle);
+		auto &medium3_font = *MEDIUM3_FONT;
+		gr_string(*grd_curcanv, medium3_font, 0x8000, ty + th, menu->subtitle);
 	}
 
 	gr_set_curfont(*grd_curcanv, menu->tiny_mode?GAME_FONT:MEDIUM1_FONT);
@@ -1461,19 +1458,19 @@ static window_event_result newmenu_draw(window *wind, newmenu *menu)
 
 	if (menu->is_scroll_box)
 	{
-		gr_set_curfont(*grd_curcanv, menu->tiny_mode?GAME_FONT:MEDIUM2_FONT);
+		auto &cv_font = *(menu->tiny_mode ? GAME_FONT : MEDIUM2_FONT);
 
-		const int line_spacing = static_cast<int>(LINE_SPACING(*grd_curcanv->cv_font, *GAME_FONT));
+		const int line_spacing = static_cast<int>(LINE_SPACING(cv_font, *GAME_FONT));
 		sy = menu->items[menu->scroll_offset].y - (line_spacing * menu->scroll_offset);
 		const auto &&fspacx = FSPACX();
 		sx = BORDERX - fspacx(12);
 
-		gr_string(*grd_curcanv, *grd_curcanv->cv_font, sx, sy, menu->scroll_offset ? UP_ARROW_MARKER(*grd_curcanv) : "  ");
+		gr_string(*grd_curcanv, cv_font, sx, sy, menu->scroll_offset ? UP_ARROW_MARKER(cv_font, *GAME_FONT) : "  ");
 
 		sy = menu->items[menu->scroll_offset + menu->max_displayable - 1].y - (line_spacing * menu->scroll_offset);
 		sx = BORDERX - fspacx(12);
 
-		gr_string(*grd_curcanv, *grd_curcanv->cv_font, sx, sy, (menu->scroll_offset + menu->max_displayable < menu->nitems) ? DOWN_ARROW_MARKER(*grd_curcanv) : "  ");
+		gr_string(*grd_curcanv, cv_font, sx, sy, (menu->scroll_offset + menu->max_displayable < menu->nitems) ? DOWN_ARROW_MARKER(*grd_curcanv->cv_font, *GAME_FONT) : "  ");
 	}
 
 		if (menu->subfunction)
@@ -1890,22 +1887,22 @@ static void listbox_create_structure( listbox *lb)
 	gr_set_default_canvas();
 	auto &canvas = *grd_curcanv;
 
-	gr_set_curfont(canvas, MEDIUM3_FONT);
+	auto &medium3_font = *MEDIUM3_FONT;
 
 	lb->box_w = 0;
 	const auto &&fspacx = FSPACX();
 	range_for (auto &i, unchecked_partial_range(lb->item, lb->nitems))
 	{
 		int w;
-		gr_get_string_size(*canvas.cv_font, i, &w, nullptr, nullptr);
+		gr_get_string_size(medium3_font, i, &w, nullptr, nullptr);
 		if ( w > lb->box_w )
 			lb->box_w = w + fspacx(10);
 	}
-	lb->height = LINE_SPACING(*canvas.cv_font, *GAME_FONT) * LB_ITEMS_ON_SCREEN;
+	lb->height = LINE_SPACING(medium3_font, *GAME_FONT) * LB_ITEMS_ON_SCREEN;
 
 	{
 		int w, h;
-		gr_get_string_size(*canvas.cv_font, lb->title, &w, &h, nullptr);
+		gr_get_string_size(medium3_font, lb->title, &w, &h, nullptr);
 		if ( w > lb->box_w )
 			lb->box_w = w;
 		lb->title_height = h+FSPACY(5);
@@ -1917,7 +1914,7 @@ static void listbox_create_structure( listbox *lb)
 		int w;
 
 		const auto box_w = lb->box_w = SWIDTH - (BORDERX*2);
-		gr_get_string_size(*canvas.cv_font, "O", &w, nullptr, nullptr);
+		gr_get_string_size(medium3_font, "O", &w, nullptr, nullptr);
 		lb->marquee = listbox::marquee::allocate(box_w / w);
 		lb->marquee->lasttime = timer_query();
 	}
@@ -1950,10 +1947,10 @@ static window_event_result listbox_draw(window *, listbox *lb)
 	gr_set_default_canvas();
 	auto &canvas = *grd_curcanv;
 	nm_draw_background(canvas, lb->box_x - BORDERX, lb->box_y - lb->title_height - BORDERY,lb->box_x + lb->box_w + BORDERX, lb->box_y + lb->height + BORDERY);
-	gr_set_curfont(canvas, MEDIUM3_FONT);
-	gr_string(canvas, *canvas.cv_font, 0x8000, lb->box_y - lb->title_height, lb->title);
+	auto &medium3_font = *MEDIUM3_FONT;
+	gr_string(canvas, medium3_font, 0x8000, lb->box_y - lb->title_height, lb->title);
 
-	const auto &&line_spacing = LINE_SPACING(*canvas.cv_font, *GAME_FONT);
+	const auto &&line_spacing = LINE_SPACING(medium3_font, *GAME_FONT);
 	for (i=lb->first_item; i<lb->first_item+LB_ITEMS_ON_SCREEN; i++ )	{
 		int y = (i - lb->first_item) * line_spacing + lb->box_y;
 		const auto &&fspacx = FSPACX();
@@ -1966,7 +1963,7 @@ static window_event_result listbox_draw(window *, listbox *lb)
 			gr_rect(canvas, lb->box_x - fspacx(1), y - fspacy(1), lb->box_x, y + line_spacing, color2);
 			gr_rect(canvas, lb->box_x, y - fspacy(1), lb->box_x + lb->box_w - fspacx(1), y + line_spacing, color0);
 		} else {
-			gr_set_curfont(canvas, (i == lb->citem) ? MEDIUM2_FONT : MEDIUM1_FONT);
+			auto &mediumX_font = *(i == lb->citem ? MEDIUM2_FONT : MEDIUM1_FONT);
 			gr_rect(canvas, lb->box_x + lb->box_w - fspacx(1), y - fspacy(1), lb->box_x + lb->box_w, y + line_spacing, color5);
 			gr_rect(canvas, lb->box_x - fspacx(1), y - fspacy(1), lb->box_x, y + line_spacing, color2);
 			gr_rect(canvas, lb->box_x, y - fspacy(1), lb->box_x + lb->box_w - fspacx(1), y + line_spacing, color0);
@@ -2012,7 +2009,7 @@ static window_event_result listbox_draw(window *, listbox *lb)
 			{
 				showstr = lb->item[i];
 			}
-			gr_string(canvas, *canvas.cv_font, lb->box_x + fspacx(5), y, showstr);
+			gr_string(canvas, mediumX_font, lb->box_x + fspacx(5), y, showstr);
 		}
 	}
 

d2x-rebirth/main/escort.cpp
@@ -1857,8 +1857,8 @@ void show_escort_menu(const array<char, 300> &amsg)
 	gr_set_default_canvas();
 
 	auto &canvas = *grd_curcanv;
-	gr_set_curfont(canvas, GAME_FONT);
-	gr_get_string_size(*canvas.cv_font, msg, &w, &h, nullptr);
+	const auto &game_font = *GAME_FONT;
+	gr_get_string_size(game_font, msg, &w, &h, nullptr);
 
 	x = (SWIDTH-w)/2;
 	y = (SHEIGHT-h)/2;
@@ -1867,7 +1867,7 @@ void show_escort_menu(const array<char, 300> &amsg)
 
 	nm_draw_background(canvas, x - BORDERX, y - BORDERY, x + w + BORDERX, y + h + BORDERY);
 
-	gr_ustring(canvas, *canvas.cv_font, x, y, msg);
+	gr_ustring(canvas, game_font, x, y, msg);
 
 	reset_cockpit();
 }

d2x-rebirth/main/movie.cpp
@@ -300,14 +300,14 @@ static window_event_result show_pause_message(window *, const d_event &event, co
 
 			gr_set_default_canvas();
 			auto &canvas = *grd_curcanv;
-			gr_set_curfont(canvas, GAME_FONT);
-			gr_get_string_size(*canvas.cv_font, msg, nullptr, &h, nullptr);
+			const auto &game_font = *GAME_FONT;
+			gr_get_string_size(game_font, msg, nullptr, &h, nullptr);
 
 			y = (grd_curscreen->get_screen_height() - h) / 2;
 
 			gr_set_fontcolor(canvas, 255, -1);
 
-			gr_ustring(canvas, *canvas.cv_font, 0x8000, y, msg);
+			gr_ustring(canvas, game_font, 0x8000, y, msg);
 			break;
 		}
 

similar/arch/ogl/ogl.cpp
@@ -324,17 +324,17 @@ static void ogl_texture_stats(void)
 	glGetIntegerv(GL_DEPTH_BITS, &depth);
 	gr_set_default_canvas();
 	auto &canvas = *grd_curcanv;
-	gr_set_curfont(canvas, GAME_FONT);
+	const auto &game_font = *GAME_FONT;
 	gr_set_fontcolor(canvas, BM_XRGB(255, 255, 255), -1);
 	colorsize = (idx * res * dbl) / 8;
 	depthsize = res * depth / 8;
 	const auto &&fspacx2 = FSPACX(2);
 	const auto &&fspacy1 = FSPACY(1);
-	const auto &&line_spacing = LINE_SPACING(*canvas.cv_font, *GAME_FONT);
-	gr_printf(canvas, *canvas.cv_font, fspacx2, fspacy1, "%i flat %i tex %i bitmaps", r_polyc, r_tpolyc, r_bitmapc);
-	gr_printf(canvas, *canvas.cv_font, fspacx2, fspacy1 + line_spacing, "%i(%i,%i,%i,%i) %iK(%iK wasted) (%i postcachedtex)", used, usedrgba, usedrgb, usedidx, usedother, truebytes / 1024, (truebytes - databytes) / 1024, r_texcount - r_cachedtexcount);
-	gr_printf(canvas, *canvas.cv_font, fspacx2, fspacy1 + (line_spacing * 2), "%ibpp(r%i,g%i,b%i,a%i)x%i=%iK depth%i=%iK", idx, r, g, b, a, dbl, colorsize / 1024, depth, depthsize / 1024);
-	gr_printf(canvas, *canvas.cv_font, fspacx2, fspacy1 + (line_spacing * 3), "total=%iK", (colorsize + depthsize + truebytes) / 1024);
+	const auto &&line_spacing = LINE_SPACING(game_font, game_font);
+	gr_printf(canvas, game_font, fspacx2, fspacy1, "%i flat %i tex %i bitmaps", r_polyc, r_tpolyc, r_bitmapc);
+	gr_printf(canvas, game_font, fspacx2, fspacy1 + line_spacing, "%i(%i,%i,%i,%i) %iK(%iK wasted) (%i postcachedtex)", used, usedrgba, usedrgb, usedidx, usedother, truebytes / 1024, (truebytes - databytes) / 1024, r_texcount - r_cachedtexcount);
+	gr_printf(canvas, game_font, fspacx2, fspacy1 + (line_spacing * 2), "%ibpp(r%i,g%i,b%i,a%i)x%i=%iK depth%i=%iK", idx, r, g, b, a, dbl, colorsize / 1024, depth, depthsize / 1024);
+	gr_printf(canvas, game_font, fspacx2, fspacy1 + (line_spacing * 3), "total=%iK", (colorsize + depthsize + truebytes) / 1024);
 }
 
 static void ogl_bindbmtex(grs_bitmap &bm, bool edgepad){

similar/editor/med.cpp
@@ -174,11 +174,11 @@ static void print_status_bar(char (&message)[DIAGNOSTIC_MESSAGE_MAX])
 {
 	gr_set_default_canvas();
 	auto &canvas = *grd_curcanv;
-	gr_set_curfont(canvas, editor_font);
+	const auto &editor_font = *::editor_font;
 	gr_set_fontcolor(canvas, CBLACK, CGREY);
 	int w,h;
-	gr_get_string_size(*canvas.cv_font, message, &w, &h, nullptr);
-	gr_string(canvas, *canvas.cv_font, 4, 583, message, w, h);
+	gr_get_string_size(editor_font, message, &w, &h, nullptr);
+	gr_string(canvas, editor_font, 4, 583, message, w, h);
 	gr_set_fontcolor(canvas, CBLACK, CWHITE);
 	gr_rect(canvas, 4+w, 583, 799, 599, CGREY);
 }
@@ -443,8 +443,6 @@ void init_editor()
 	gr_set_current_canvas(*Canv_editor);
 	init_editor_screen(*grd_curcanv); // load the main editor dialog
 	gr_set_default_canvas();
-	gr_set_curfont(*grd_curcanv, editor_font);
-	
 	set_warn_func(med_show_warning);
 	
 	//	_MARK_("start of editor");//Nuked to compile -KRB
@@ -819,7 +817,6 @@ static void init_editor_screen(grs_canvas &canvas)
 		i++;		e.pad_goto[i] = ui_add_gadget_button( EditorWindow, PAD_X+16+(i+2)*PAD_WIDTH1, PAD_Y+(30*5)+22, PAD_WIDTH, 20, "TT",  med_keypad_goto_8 );
 	}
 
-	gr_set_curfont(canvas, editor_font);
 	menubar_show();
 
 	// INIT TEXTURE STUFF
@@ -1067,8 +1064,6 @@ window_event_result editor_handler(UI_DIALOG *, const d_event &event, unused_ui_
 
 	if (event.type == EVENT_UI_DIALOG_DRAW)
 	{
-		gr_set_curfont(*grd_curcanv, editor_font);
-
 		// Draw status box
 		gr_set_default_canvas();
 		gr_rect(*grd_curcanv, STATUS_X,STATUS_Y,STATUS_X+STATUS_W-1,STATUS_Y+STATUS_H-1, CGREY);

similar/editor/texpage.cpp
@@ -230,8 +230,6 @@ int texpage_do(const d_event &event)
 		
 		texpage_redraw();
 		
-		gr_set_curfont(*grd_curcanv, editor_font);
-		
 		// Don't reset the current tmap every time we go back to the editor.
 		//	CurrentTexture = TexturePage*TMAPS_PER_PAGE;
 		texpage_show_current();

similar/main/automap.cpp
@@ -530,10 +530,10 @@ constexpr char system_name[][17] = {
 
 static void name_frame(grs_canvas &canvas, automap *const am)
 {
-	gr_set_curfont(canvas, GAME_FONT);
 	gr_set_fontcolor(canvas, am->green_31, -1);
 	char		name_level_left[128];
 
+	auto &game_font = *GAME_FONT;
 #if defined(DXX_BUILD_DESCENT_I)
 	const char *name_level;
 	if (Current_level_num > 0)
@@ -544,7 +544,7 @@ static void name_frame(grs_canvas &canvas, automap *const am)
 	else
 		name_level = Current_level_name;
 
-	gr_string(canvas, *canvas.cv_font, (SWIDTH / 64), (SHEIGHT / 48), name_level);
+	gr_string(canvas, game_font, (SWIDTH / 64), (SHEIGHT / 48), name_level);
 #elif defined(DXX_BUILD_DESCENT_II)
 	char	name_level_right[128];
 	if (Current_level_num > 0)
@@ -558,10 +558,10 @@ static void name_frame(grs_canvas &canvas, automap *const am)
 	else
 		snprintf(name_level_right, sizeof(name_level_right), " %s", current_level_name);
 
-	gr_string(canvas, *canvas.cv_font, (SWIDTH / 64), (SHEIGHT / 48), name_level_left);
+	gr_string(canvas, game_font, (SWIDTH / 64), (SHEIGHT / 48), name_level_left);
 	int wr,h;
-	gr_get_string_size(*canvas.cv_font, name_level_right, &wr, &h, nullptr);
-	gr_string(canvas, *canvas.cv_font, canvas.cv_bitmap.bm_w - wr - (SWIDTH / 64), (SHEIGHT / 48), name_level_right, wr, h);
+	gr_get_string_size(game_font, name_level_right, &wr, &h, nullptr);
+	gr_string(canvas, game_font, canvas.cv_bitmap.bm_w - wr - (SWIDTH / 64), (SHEIGHT / 48), name_level_right, wr, h);
 #endif
 }
 
@@ -652,7 +652,6 @@ static void draw_automap(fvcobjptr &vcobjptr, automap *am)
 		auto &canvas = *grd_curcanv;
 		if (am->automap_background.get_bitmap_data())
 			show_fullscr(canvas, am->automap_background);
-		gr_set_curfont(canvas, HUGE_FONT);
 		gr_set_fontcolor(canvas, BM_XRGB(20, 20, 20), -1);
 	{
 		int x, y;
@@ -662,9 +661,8 @@ static void draw_automap(fvcobjptr &vcobjptr, automap *am)
 	else
 #endif
 			x = SWIDTH / 8, y = SHEIGHT / 16;
-		gr_string(canvas, *canvas.cv_font, x, y, TXT_AUTOMAP);
+		gr_string(canvas, *HUGE_FONT, x, y, TXT_AUTOMAP);
 	}
-		gr_set_curfont(canvas, GAME_FONT);
 		gr_set_fontcolor(canvas, BM_XRGB(20, 20, 20), -1);
 	{
 		int x;
@@ -695,9 +693,10 @@ static void draw_automap(fvcobjptr &vcobjptr, automap *am)
 		y1 = SHEIGHT / 1.083;
 		y2 = SHEIGHT / 1.043;
 #endif
-		gr_string(canvas, *canvas.cv_font, x, y0, TXT_TURN_SHIP);
-		gr_string(canvas, *canvas.cv_font, x, y1, s1);
-		gr_string(canvas, *canvas.cv_font, x, y2, s2);
+		auto &game_font = *GAME_FONT;
+		gr_string(canvas, game_font, x, y0, TXT_TURN_SHIP);
+		gr_string(canvas, game_font, x, y1, s1);
+		gr_string(canvas, game_font, x, y2, s2);
 	}
 
 	}

similar/main/console.cpp
@@ -281,6 +281,7 @@ static void con_draw(void)
 
 	gr_set_default_canvas();
 	auto &canvas = *grd_curcanv;
+	auto &game_font = *GAME_FONT;
 	gr_set_curfont(canvas, GAME_FONT);
 	const uint8_t color = BM_XRGB(0, 0, 0);
 	gr_settransblend(canvas, 7, GR_BLEND_NORMAL);
@@ -301,9 +302,9 @@ static void con_draw(void)
 		auto &b = con_buffer[CON_LINES_MAX - 1 - i];
 		gr_set_fontcolor(canvas, get_console_color_by_priority(b.priority), -1);
 		int w,h;
-		gr_get_string_size(*canvas.cv_font, b.line, &w, &h, nullptr);
+		gr_get_string_size(game_font, b.line, &w, &h, nullptr);
 		y -= h + fspacy1;
-		gr_string(canvas, *canvas.cv_font, fspacx1, y, b.line, w, h);
+		gr_string(canvas, game_font, fspacx1, y, b.line, w, h);
 		i++;
 
 		if (y<=0 || CON_LINES_MAX-1-i <= 0 || i < 0)
@@ -311,8 +312,8 @@ static void con_draw(void)
 	}
 	gr_rect(canvas, 0, 0, SWIDTH, line_spacing, color);
 	gr_set_fontcolor(canvas, BM_XRGB(255, 255, 255),-1);
-	gr_printf(canvas, *canvas.cv_font, fspacx1, fspacy1, "%s LOG", DESCENT_VERSION);
-	gr_string(canvas, *canvas.cv_font, SWIDTH - fspacx(110), fspacy1, "PAGE-UP/DOWN TO SCROLL");
+	gr_printf(canvas, game_font, fspacx1, fspacy1, "%s LOG", DESCENT_VERSION);
+	gr_string(canvas, game_font, SWIDTH - fspacx(110), fspacy1, "PAGE-UP/DOWN TO SCROLL");
 }
 
 static window_event_result con_handler(window *wind,const d_event &event, const unused_window_userdata_t *)

commit d197ba42c32048a986bfd9e16635528b10bf39e2

Fix mouse-selection in listboxes
Commit d355ef4030ef removed a seemingly unnecessary modification of the
global variable grd_curcanv->cv_font, after eliminating all local reads
of it.  However, a non-local read, buried in listbox_mouse, depended on
grd_curcanv->cv_font being set to a MEDIUM font.  After that commit,
grd_curcanv->cv_font retained its prior value, which is not a MEDIUM
font.  This caused listbox_mouse to compute an incorrect height of the
lines in the listbox, which manifested as the game choosing the wrong
line when the mouse is clicked in the listbox.

Fix the problem by explicitly using MEDIUM3_FONT, since that was
usually the value left in grd_curcanv->cv_font prior to that commit.  In
some cases, a different MEDIUM font would be left there, but all the
MEDIUM fonts have the same height, so they are interchangeable for this
purpose.

Reported-by: Q3BFG10K <https://github.com/dxx-rebirth/dxx-rebirth/issues/498>
Fixes: d355ef4030ef4c8e9e3a153fa2c04e56192edc26 ("Pass font to various drawing functions")

common/include/fwd-gr.h
@@ -293,6 +293,7 @@ void gr_printt(grs_canvas &, const grs_font &, int x, int y, const char *format,
 #define gr_printf(A1,A2,A3,A4,F,...)	dxx_call_printf_checked(gr_printfs,gr_string,(A1,A2,A3,A4),(F),##__VA_ARGS__)
 #define gr_uprintf(A1,A2,A3,A4,F,...)	dxx_call_printf_checked(gr_printfus,gr_ustring,(A1,A2,A3,A4),(F),##__VA_ARGS__)
 std::pair<const char *, unsigned> gr_get_string_wrap(const grs_font &, const char *s, unsigned limit);
+int gr_get_string_height(const grs_font &cv_font, unsigned lines);
 void gr_get_string_size(const grs_font &, const char *s, int *string_width, int *string_height, int *average_width);
 void gr_get_string_size(const grs_font &, const char *s, int *string_width, int *string_height, int *average_width, const unsigned max_chars_per_line);
 

similar/2d/font.cpp
@@ -771,6 +771,12 @@ void gr_ustring(grs_canvas &canvas, const grs_font &cv_font, const int x, const 
 		gr_ustring_mono(canvas, cv_font, x, y, s);
 }
 
+int gr_get_string_height(const grs_font &cv_font, const unsigned lines)
+{
+	const auto fontscale_y = FONTSCALE_Y(cv_font.ft_h);
+	return static_cast<int>(fontscale_y + (lines * (fontscale_y + FSPACY(1))));
+}
+
 void gr_get_string_size(const grs_font &cv_font, const char *s, int *const string_width, int *const string_height, int *const average_width)
 {
 	gr_get_string_size(cv_font, s, string_width, string_height, average_width, UINT_MAX);
@@ -815,10 +821,7 @@ void gr_get_string_size(const grs_font &cv_font, const char *s, int *const strin
 	if (string_width)
 		*string_width = std::max(longest_width, string_width_f);
 	if (string_height)
-	{
-		const auto fontscale_y = FONTSCALE_Y(cv_font.ft_h);
-		*string_height = static_cast<int>(fontscale_y + (lines * (fontscale_y + FSPACY(1))));
-	}
+		*string_height = gr_get_string_height(cv_font, lines);
 }
 
 std::pair<const char *, unsigned> gr_get_string_wrap(const grs_font &cv_font, const char *s, unsigned limit)

similar/main/newmenu.cpp
@@ -1753,31 +1753,41 @@ static void update_scroll_position(listbox *lb)
 
 static window_event_result listbox_mouse(window *, const d_event &event, listbox *lb, int button)
 {
-	int mx, my, mz, x1, x2, y1, y2;
-
 	switch (button)
 	{
 		case MBTN_LEFT:
 		{
 			if (lb->mouse_state)
 			{
+				int mx, my, mz;
 				mouse_get_pos(&mx, &my, &mz);
-				const auto &&line_spacing = LINE_SPACING(*grd_curcanv->cv_font, *GAME_FONT);
-				for (int i = lb->first_item; i < lb->first_item + lb->items_on_screen; ++i)
-				{
-					if (i >= lb->nitems)
-						break;
-					int h;
-					gr_get_string_size(*grd_curcanv->cv_font, lb->item[i], nullptr, &h, nullptr);
-					x1 = lb->box_x;
-					x2 = lb->box_x + lb->box_w;
-					y1 = (i - lb->first_item) * line_spacing + lb->box_y;
-					y2 = y1+h;
-					if ( ((mx > x1) && (mx < x2)) && ((my > y1) && (my < y2)) ) {
-						lb->citem = i;
-						return window_event_result::handled;
-					}
-				}
+				const int x1 = lb->box_x;
+				if (mx <= x1)
+					break;
+				const int x2 = x1 + lb->box_w;
+				if (mx >= x2)
+					break;
+				const int by = lb->box_y;
+				if (my <= by)
+					break;
+				const int my_relative_by = my - by;
+				const auto &&line_spacing = LINE_SPACING(*MEDIUM3_FONT, *GAME_FONT);
+				if (line_spacing < 1)
+					break;
+				const int idx_relative_first_visible_item = my_relative_by / line_spacing;
+				const auto first_visible_item = lb->first_item;
+				const auto nitems = lb->nitems;
+				const auto last_visible_item = std::min(first_visible_item + lb->items_on_screen, nitems);
+				if (idx_relative_first_visible_item >= last_visible_item)
+					break;
+				const int px_within_item = my_relative_by % static_cast<int>(line_spacing);
+				const int h = gr_get_string_height(*MEDIUM3_FONT, 1);
+				if (px_within_item >= h)
+					break;
+				const int idx_absolute_item = idx_relative_first_visible_item + first_visible_item;
+				if (idx_absolute_item >= nitems)
+					break;
+				lb->citem = idx_absolute_item;
 			}
 			else if (event.type == EVENT_MOUSE_BUTTON_UP)
 			{
@@ -1786,12 +1796,13 @@ static window_event_result listbox_mouse(window *, const d_event &event, listbox
 				if (lb->citem < 0)
 					return window_event_result::ignored;
 
+				int mx, my, mz;
 				mouse_get_pos(&mx, &my, &mz);
-				gr_get_string_size(*grd_curcanv->cv_font, lb->item[lb->citem], nullptr, &h, nullptr);
-				x1 = lb->box_x;
-				x2 = lb->box_x + lb->box_w;
-				y1 = (lb->citem - lb->first_item) * LINE_SPACING(*grd_curcanv->cv_font, *GAME_FONT) + lb->box_y;
-				y2 = y1+h;
+				gr_get_string_size(*MEDIUM3_FONT, lb->item[lb->citem], nullptr, &h, nullptr);
+				const int x1 = lb->box_x;
+				const int x2 = lb->box_x + lb->box_w;
+				const int y1 = (lb->citem - lb->first_item) * LINE_SPACING(*MEDIUM3_FONT, *GAME_FONT) + lb->box_y;
+				const int y2 = y1 + h;
 				if ( ((mx > x1) && (mx < x2)) && ((my > y1) && (my < y2)) )
 				{
 					// Tell callback, if it wants to close it will return window_event_result::close