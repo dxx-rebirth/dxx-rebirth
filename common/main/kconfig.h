dxx-rebirth/dxx-rebirth

commit c621a970c6ac31e5eed8270a84b1d43e36990fc7

Reset kconfig counters once, before the event loop

common/main/kconfig.h
@@ -122,6 +122,7 @@ extern const std::array<uint8_t, MAX_DXX_REBIRTH_CONTROLS> DefaultKeySettingsReb
 #endif
 
 extern void kconfig_read_controls(const d_event &event, int automap_flag);
+void kconfig_begin_loop();
 
 enum class kconfig_type
 {

similar/editor/med.cpp
@@ -1142,7 +1142,10 @@ window_event_result editor_handler(UI_DIALOG *, const d_event &event, unused_ui_
 					rval = window_event_result::handled;
 				}
 				break;
-				
+			case EVENT_LOOP_BEGIN_LOOP:
+				kconfig_begin_loop();
+				break;
+
 			default:
 				break;
 		}

similar/main/automap.cpp
@@ -1054,6 +1054,10 @@ static window_event_result automap_handler(window *wind,const d_event &event, au
 			return window_event_result::ignored;	// continue closing
 			break;
 
+		case EVENT_LOOP_BEGIN_LOOP:
+			kconfig_begin_loop();
+			break;
+
 		default:
 			return window_event_result::ignored;
 			break;

similar/main/game.cpp
@@ -1658,6 +1658,10 @@ window_event_result game_handler(window *,const d_event &event, const unused_win
 			return window_event_result::ignored;
 			break;
 
+		case EVENT_LOOP_BEGIN_LOOP:
+			kconfig_begin_loop();
+			break;
+
 		default:
 			break;
 	}

similar/main/kconfig.cpp
@@ -1082,6 +1082,11 @@ static unsigned allow_uncapped_turning()
 		: MouselookMode::MPAnarchy);
 }
 
+void kconfig_begin_loop()
+{
+	Controls.pitch_time = Controls.vertical_thrust_time = Controls.heading_time = Controls.sideways_thrust_time = Controls.bank_time = Controls.forward_thrust_time = 0;
+}
+
 void kconfig_read_controls(const d_event &event, int automap_flag)
 {
 	static fix64 mouse_delta_time = 0;
@@ -1095,7 +1100,6 @@ void kconfig_read_controls(const d_event &event, int automap_flag)
 	}
 #endif
 
-	Controls.pitch_time = Controls.vertical_thrust_time = Controls.heading_time = Controls.sideways_thrust_time = Controls.bank_time = Controls.forward_thrust_time = 0;
 	const auto frametime = FrameTime;
 
 	switch (event.type)

common/arch/sdl/event.cpp
@@ -44,13 +44,21 @@ static void windowevent_handler(const SDL_WindowEvent &windowevent)
 }
 #endif
 
+static void event_notify_begin_loop()
+{
+	const d_event_begin_loop event;
+	event_send(event);
+}
+
 window_event_result event_poll()
 {
 	SDL_Event event;
 	int clean_uniframe=1;
 	window *wind = window_get_front();
 	window_event_result highest_result(window_event_result::ignored);
-	
+
+	event_notify_begin_loop();
+
 	// If the front window changes, exit this loop, otherwise unintended behavior can occur
 	// like pressing 'Return' really fast at 'Difficulty Level' causing multiple games to be started
 	while ((highest_result != window_event_result::deleted) && (wind == window_get_front()) && (event = {}, SDL_PollEvent(&event)))

common/include/event.h
@@ -48,7 +48,9 @@ enum event_type : unsigned
 	EVENT_NEWMENU_DRAW,					// draw after the newmenu stuff is drawn (e.g. savegame previews)
 	EVENT_NEWMENU_CHANGED,				// an item had its value/text changed
 	EVENT_NEWMENU_SELECTED,				// user chose something - pressed enter/clicked on it
-	
+
+	EVENT_LOOP_BEGIN_LOOP,
+
 	EVENT_UI_DIALOG_DRAW,				// draw after the dialog stuff is drawn (e.g. spinning robots)
 	EVENT_UI_GADGET_PRESSED,				// user 'pressed' a gadget
 	EVENT_UI_LISTBOX_MOVED,
@@ -115,6 +117,14 @@ struct d_window_size_event : d_event
 	}
 };
 #endif
+
+struct d_event_begin_loop : d_event
+{
+	d_event_begin_loop() :
+		d_event{EVENT_LOOP_BEGIN_LOOP}
+	{
+	}
+};
 
 #if DXX_USE_EDITOR
 fix event_get_idle_seconds();

commit b08620292dad79b2222ff752467b00247f2ab8df

Fix input handling in automap
The automap code keeps a mostly-private secondary copy of control_info.
The change to reset control_info before the event loop reset the main
copy of control_info for the automap, not the copy that the automap
actually used.  Fix this by passing the desired control_info to relevant
functions instead of always using the global variable Controls.

Fixes: c621a970c6ac31e5eed8270a84b1d43e36990fc7 ("Reset kconfig counters once, before the event loop")

common/main/kconfig.h
@@ -72,18 +72,37 @@ struct control_info
 			automap,
 			cycle_primary, cycle_secondary, select_weapon;
 	};
+	struct mouse_axis_values
+	{
+		std::array<fix, 3> mouse_axis, raw_mouse_axis;
+	};
+#if DXX_MAX_AXES_PER_JOYSTICK
+	struct joystick_axis_values
+	{
+		std::array<fix, JOY_MAX_AXES> joy_axis, raw_joy_axis;
+	};
+#endif
 	ramp_controls_t<float> down_time; // to scale movement depending on how long the key is pressed
 	fix pitch_time, vertical_thrust_time, heading_time, sideways_thrust_time, bank_time, forward_thrust_time;
         fix excess_pitch_time, excess_vertical_thrust_time, excess_heading_time, excess_sideways_thrust_time, excess_bank_time, excess_forward_thrust_time;
 };
 
+void kconfig_begin_loop(control_info &);
+
 }
 
 namespace dsx {
 
-struct control_info : ::dcx::control_info
+struct state_control_info
 {
-#if defined(DXX_BUILD_DESCENT_II)
+#if defined(DXX_BUILD_DESCENT_I)
+	/* Avoid creating a typedef for a single use.  Qualify the name so
+	 * that it can be found.
+	 */
+	::dcx::control_info::
+#elif defined(DXX_BUILD_DESCENT_II)
+	/* Extend the original type with new Descent2-specific members.
+	 */
 	struct state_controls_t : ::dcx::control_info::state_controls_t
 	{
 		uint8_t toggle_bomb,
@@ -91,9 +110,23 @@ struct control_info : ::dcx::control_info
 	};
 #endif
 	state_controls_t state; // to scale movement for keys only we need them to be separate from joystick/mouse buttons
-	std::array<fix, 3> mouse_axis, raw_mouse_axis;
+};
+
+/* This inheritance construct is needed so that joystick_axis_values is
+ * at the end of the structure.  The joystick values are placed at the
+ * end because they are far larger than the rest of the structure
+ * members combined, so moving them to the end provides the best
+ * locality of access.
+ */
+struct control_info : ::dcx::control_info,
+	state_control_info,
+	::dcx::control_info::mouse_axis_values
 #if DXX_MAX_AXES_PER_JOYSTICK
-	std::array<fix, JOY_MAX_AXES> joy_axis, raw_joy_axis;
+	, ::dcx::control_info::joystick_axis_values
+#endif
+{
+#if defined(DXX_BUILD_DESCENT_II)
+	using typename state_control_info::state_controls_t;
 #endif
 };
 
@@ -112,6 +145,7 @@ constexpr std::integral_constant<unsigned, 50> MAX_CONTROLS{};
 #elif defined(DXX_BUILD_DESCENT_II)
 constexpr std::integral_constant<unsigned, 60> MAX_CONTROLS{};		// there are actually 48, so this leaves room for more
 #endif
+void kconfig_read_controls(control_info &, const d_event &event, int automap_flag);
 }
 namespace dcx {
 extern fix Cruise_speed;
@@ -120,9 +154,6 @@ constexpr std::integral_constant<unsigned, 30> MAX_DXX_REBIRTH_CONTROLS{};
 extern const std::array<uint8_t, MAX_DXX_REBIRTH_CONTROLS> DefaultKeySettingsRebirth;
 }
 #endif
-
-extern void kconfig_read_controls(const d_event &event, int automap_flag);
-void kconfig_begin_loop();
 
 enum class kconfig_type
 {

similar/editor/med.cpp
@@ -1127,7 +1127,7 @@ window_event_result editor_handler(UI_DIALOG *, const d_event &event, unused_ui_
 			case EVENT_KEY_COMMAND:
 			case EVENT_KEY_RELEASE:
 			case EVENT_IDLE:
-				kconfig_read_controls(event, 1);
+				kconfig_read_controls(Controls, event, 1);
 
 				if (slew_frame(0))
 				{		//do movement and check keys
@@ -1143,7 +1143,7 @@ window_event_result editor_handler(UI_DIALOG *, const d_event &event, unused_ui_
 				}
 				break;
 			case EVENT_LOOP_BEGIN_LOOP:
-				kconfig_begin_loop();
+				kconfig_begin_loop(Controls);
 				break;
 
 			default:

similar/main/automap.cpp
@@ -962,9 +962,7 @@ static window_event_result automap_key_command(window *, const d_event &event, a
 
 static window_event_result automap_process_input(window *, const d_event &event, automap *am)
 {
-	Controls = am->controls;
-	kconfig_read_controls(event, 1);
-	am->controls = Controls;
+	kconfig_read_controls(am->controls, event, 1);
 	Controls = {};
 
 	if ( !am->controls.state.automap && (am->leave_mode==1) )
@@ -1055,7 +1053,7 @@ static window_event_result automap_handler(window *wind,const d_event &event, au
 			break;
 
 		case EVENT_LOOP_BEGIN_LOOP:
-			kconfig_begin_loop();
+			kconfig_begin_loop(am->controls);
 			break;
 
 		default:

similar/main/game.cpp
@@ -1659,7 +1659,7 @@ window_event_result game_handler(window *,const d_event &event, const unused_win
 			break;
 
 		case EVENT_LOOP_BEGIN_LOOP:
-			kconfig_begin_loop();
+			kconfig_begin_loop(Controls);
 			break;
 
 		default:

similar/main/gamecntl.cpp
@@ -1997,7 +1997,7 @@ window_event_result ReadControls(const d_event &event)
 
 	if (!Endlevel_sequence && Newdemo_state != ND_STATE_PLAYBACK)
 	{
-		kconfig_read_controls(event, 0);
+		kconfig_read_controls(Controls, event, 0);
 		const auto Player_is_dead = Player_dead_state;
 		if (Player_is_dead != player_dead_state::no && HandleDeathInput(event))
 			return window_event_result::handled;

similar/main/kconfig.cpp
@@ -120,6 +120,11 @@ struct kc_mitem {
 
 }
 
+void kconfig_begin_loop(control_info &Controls)
+{
+	Controls.pitch_time = Controls.vertical_thrust_time = Controls.heading_time = Controls.sideways_thrust_time = Controls.bank_time = Controls.forward_thrust_time = 0;
+}
+
 }
 
 namespace dsx {
@@ -975,7 +980,9 @@ void kconfig(const kconfig_type n)
 	}
 }
 
-static void input_button_matched(const kc_item& item, int down)
+namespace dsx {
+
+static void input_button_matched(control_info &Controls, const kc_item& item, int down)
 {
 	if (item.state_bit)
 	{
@@ -992,6 +999,10 @@ static void input_button_matched(const kc_item& item, int down)
 			Controls.state.*item.ci_count_ptr += 1;
 	}
 }
+
+}
+
+namespace dcx {
 
 template <template<typename> class F>
 static void adjust_ramped_keyboard_field(float& keydown_time, ubyte& state, fix& time, const float& sensitivity, const int& speed_factor, const int& speed_divisor = 1)
@@ -1031,7 +1042,7 @@ static void clamp_symmetric_value(fix& value, const fix& bound)
 }
 
 #if DXX_MAX_AXES_PER_JOYSTICK
-static void convert_raw_joy_axis(const uint_fast32_t player_cfg_index, const uint_fast32_t i)
+static void convert_raw_joy_axis(control_info::joystick_axis_values &Controls, const uint_fast32_t player_cfg_index, const uint_fast32_t i)
 {
 	const auto raw_joy_axis = Controls.raw_joy_axis[i];
 	const auto joy_axis = (abs(raw_joy_axis) <= (128 * PlayerCfg.JoystickLinear[player_cfg_index]) / 16)
@@ -1040,11 +1051,11 @@ static void convert_raw_joy_axis(const uint_fast32_t player_cfg_index, const uin
 	Controls.joy_axis[i] = joy_axis / 128;
 }
 
-static void convert_raw_joy_axis(const uint_fast32_t kcm_index, const uint_fast32_t player_cfg_index, const uint_fast32_t i)
+static void convert_raw_joy_axis(control_info::joystick_axis_values &Controls, const uint_fast32_t kcm_index, const uint_fast32_t player_cfg_index, const uint_fast32_t i)
 {
 	if (i != kcm_joystick[kcm_index].value)
 		return;
-	convert_raw_joy_axis(player_cfg_index, i);
+	convert_raw_joy_axis(Controls, player_cfg_index, i);
 }
 #endif
 
@@ -1082,12 +1093,11 @@ static unsigned allow_uncapped_turning()
 		: MouselookMode::MPAnarchy);
 }
 
-void kconfig_begin_loop()
-{
-	Controls.pitch_time = Controls.vertical_thrust_time = Controls.heading_time = Controls.sideways_thrust_time = Controls.bank_time = Controls.forward_thrust_time = 0;
 }
 
-void kconfig_read_controls(const d_event &event, int automap_flag)
+namespace dsx {
+
+void kconfig_read_controls(control_info &Controls, const d_event &event, int automap_flag)
 {
 	static fix64 mouse_delta_time = 0;
 
@@ -1114,7 +1124,7 @@ void kconfig_read_controls(const d_event &event, int automap_flag)
 			{
 				if (kcm_keyboard[i].value == key)
 				{
-					input_button_matched(kc_keyboard[i], (event.type==EVENT_KEY_COMMAND));
+					input_button_matched(Controls, kc_keyboard[i], (event.type==EVENT_KEY_COMMAND));
 				}
 			}
 			if (!automap_flag && event.type == EVENT_KEY_COMMAND)
@@ -1140,7 +1150,7 @@ void kconfig_read_controls(const d_event &event, int automap_flag)
 			{
 				if (kc_joystick[i].type == BT_JOY_BUTTON && kcm_joystick[i].value == button)
 				{
-					input_button_matched(kc_joystick[i], (event.type==EVENT_JOYSTICK_BUTTON_DOWN));
+					input_button_matched(Controls, kc_joystick[i], (event.type==EVENT_JOYSTICK_BUTTON_DOWN));
 				}
 			}
 			if (!automap_flag && event.type == EVENT_JOYSTICK_BUTTON_DOWN)
@@ -1166,7 +1176,7 @@ void kconfig_read_controls(const d_event &event, int automap_flag)
 			{
 				if (kc_mouse[i].type == BT_MOUSE_BUTTON && kcm_mouse[i].value == button)
 				{
-					input_button_matched(kc_mouse[i], (event.type==EVENT_MOUSE_BUTTON_DOWN));
+					input_button_matched(Controls, kc_mouse[i], (event.type==EVENT_MOUSE_BUTTON_DOWN));
 				}
 			}
 			if (!automap_flag && event.type == EVENT_MOUSE_BUTTON_DOWN)
@@ -1249,12 +1259,12 @@ void kconfig_read_controls(const d_event &event, int automap_flag)
 	
 #if DXX_MAX_AXES_PER_JOYSTICK
 	for (int i = 0; i < JOY_MAX_AXES; i++) {
-		convert_raw_joy_axis(15, 0, i); // Turn L/R
-		convert_raw_joy_axis(13, 1, i); // Pitch U/D
-		convert_raw_joy_axis(17, 2, i); // Slide L/R
-		convert_raw_joy_axis(19, 3, i); // Slide U/D
-		convert_raw_joy_axis(21, 4, i); // Bank
-		convert_raw_joy_axis(23, 5, i); // Throttle
+		convert_raw_joy_axis(Controls, 15, 0, i); // Turn L/R
+		convert_raw_joy_axis(Controls, 13, 1, i); // Pitch U/D
+		convert_raw_joy_axis(Controls, 17, 2, i); // Slide L/R
+		convert_raw_joy_axis(Controls, 19, 3, i); // Slide U/D
+		convert_raw_joy_axis(Controls, 21, 4, i); // Bank
+		convert_raw_joy_axis(Controls, 23, 5, i); // Throttle
 	}
 #endif
 
@@ -1397,6 +1407,8 @@ void kconfig_read_controls(const d_event &event, int automap_flag)
 		clamp_kconfig_control_with_overrun(Controls.heading_time, frametime, Controls.excess_heading_time, frametime * PlayerCfg.MouseOverrun[0]);
 		clamp_kconfig_control_with_overrun(Controls.bank_time, frametime, Controls.excess_bank_time, frametime * PlayerCfg.MouseOverrun[4]);
 	}
+}
+
 }
 
 void reset_cruise(void)