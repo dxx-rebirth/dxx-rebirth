fa7d8e2dea8012d995ef5dafd32edd903d15cfb1
Kp <kp@valhallalegends.com>
2020-06-10 02:25:32 +0000

    Use enum sound_effect for hardcoded sound index numbers

--- common/main/sounds.h
+++ common/main/sounds.h
@@ -29,138 +29,146 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #include "digi.h"
 
-//------------------- List of sound effects --------------------
-
-#define SOUND_LASER_FIRED                       10
-
-#define SOUND_WEAPON_HIT_DOOR                   27
-#define SOUND_WEAPON_HIT_BLASTABLE              11
-#define SOUND_BADASS_EXPLOSION                  11  // need something different for this if possible
-
-#define SOUND_ROBOT_HIT_PLAYER                  17
-
-#define SOUND_ROBOT_HIT                         20
-#define SOUND_ROBOT_DESTROYED                   21
-#define SOUND_VOLATILE_WALL_HIT                 21
-
-#define SOUND_LASER_HIT_CLUTTER                 30
-#define SOUND_CONTROL_CENTER_HIT                30
-#define SOUND_EXPLODING_WALL                    31  // one long sound
-#define SOUND_CONTROL_CENTER_DESTROYED          31
-
-#define SOUND_CONTROL_CENTER_WARNING_SIREN      32
-#define SOUND_MINE_BLEW_UP                      33
-
-#define SOUND_FUSION_WARMUP                     34
-#ifdef DXX_BUILD_DESCENT_II
-#define SOUND_ROBOT_SUCKED_PLAYER               SOUND_ROBOT_HIT_PLAYER // Robot sucked energy from player.
-#define SOUND_LASER_HIT_WATER                   232
-#define SOUND_MISSILE_HIT_WATER                 233
-#define SOUND_DROP_WEAPON                       39
-
-#define SOUND_FORCEFIELD_BOUNCE_PLAYER          40
-#define SOUND_FORCEFIELD_BOUNCE_WEAPON          41
-#define SOUND_FORCEFIELD_HUM                    42
-#define SOUND_FORCEFIELD_OFF                    43
-
-#define SOUND_MARKER_HIT                        50
-#define SOUND_BUDDY_MET_GOAL                    51
-
-#define SOUND_BRIEFING_HUM                      94
-#define SOUND_BRIEFING_PRINTING                 95
-#endif
-
-#define SOUND_REFUEL_STATION_GIVING_FUEL        62
-
-#define SOUND_PLAYER_HIT_WALL                   70
-#define SOUND_PLAYER_GOT_HIT                    71
-
-#define SOUND_HOSTAGE_RESCUED                   91
-
-#define SOUND_COUNTDOWN_0_SECS                  100 // countdown 100..114
-#define SOUND_COUNTDOWN_13_SECS                 113
-#define SOUND_COUNTDOWN_29_SECS                 114
-
-#define SOUND_HUD_MESSAGE                       117
-#define SOUND_HUD_KILL                          118
-
-#define SOUND_HOMING_WARNING                    122 // Warning beep: You are being tracked by a missile! Borrowed from old repair center sounds.
-
-#ifdef DXX_BUILD_DESCENT_II
-#define SOUND_HUD_JOIN_REQUEST                  123
-#define SOUND_HUD_BLUE_GOT_FLAG                 124
-#define SOUND_HUD_RED_GOT_FLAG                  125
-#define SOUND_HUD_YOU_GOT_FLAG                  126
-#define SOUND_HUD_BLUE_GOT_GOAL                 127
-#define SOUND_HUD_RED_GOT_GOAL                  128
-#define SOUND_HUD_YOU_GOT_GOAL                  129
-
-#define SOUND_LAVAFALL_HISS                     150 // under a lavafall
-#define SOUND_SHIP_IN_WATER                     152 // sitting (or moving though) water
-#define SOUND_SHIP_IN_WATERFALL                 158 // under a waterfall
-#endif
-#define SOUND_VOLATILE_WALL_HISS                151 // need a hiss sound here.
-
-#define SOUND_BAD_SELECTION                     156
-#define SOUND_ALREADY_SELECTED                  155 // Adam: New sound number here! MK, 01/30/95
-
+#ifdef dsx
 #if defined(DXX_BUILD_DESCENT_I)
-#define SOUND_GOOD_SELECTION_PRIMARY    (PCSharePig?155:153)
-#define SOUND_GOOD_SELECTION_SECONDARY  (PCSharePig?155:154)    //      Adam: New sound number here! MK, 01/30/95
-#define SOUND_CHEATER                   (PCSharePig?156:200) // moved by Victor Rachels
-#define SOUND_BOSS_SHARE_ATTACK         184
-
-#define SOUND_NASTY_ROBOT_HIT_1         190     //      ding.raw        ; tearing metal 1
-#define SOUND_NASTY_ROBOT_HIT_2         191     //      ding.raw        ; tearing metal 2
+#define SOUND_GOOD_SELECTION_PRIMARY    (PCSharePig ? SOUND_ALREADY_SELECTED : SOUND_GOOD_SELECTION_PRIMARY_FULL_DATA)
+#define SOUND_GOOD_SELECTION_SECONDARY  (PCSharePig ? SOUND_ALREADY_SELECTED : SOUND_GOOD_SELECTION_SECONDARY_FULL_DATA)    //      Adam: New sound number here! MK, 01/30/95
+#define SOUND_CHEATER                   (PCSharePig ? SOUND_BAD_SELECTION : SOUND_CHEATER_FULL_DATA) // moved by Victor Rachels
 #elif defined(DXX_BUILD_DESCENT_II)
-#define SOUND_GOOD_SELECTION_PRIMARY            153
-#define SOUND_GOOD_SELECTION_SECONDARY          154 // Adam: New sound number here! MK, 01/30/95
-#define SOUND_CLOAK_ON                          160 // USED FOR WALL CLOAK
+#define SOUND_GOOD_SELECTION_PRIMARY	SOUND_GOOD_SELECTION_PRIMARY_FULL_DATA
+#define SOUND_GOOD_SELECTION_SECONDARY	SOUND_GOOD_SELECTION_SECONDARY_FULL_DATA
+#define SOUND_CHEATER	SOUND_CHEATER_FULL_DATA
 #endif
+//------------------- List of sound effects --------------------
+namespace dcx {
 
-#define SOUND_CLOAK_OFF                         161 // sound when cloak goes away
-#define SOUND_INVULNERABILITY_OFF               163 // sound when invulnerability goes away
+	/* The games do not use exactly the same sounds, but every sound can
+	 * be classified as:
+	 * - Used in both games, with the same value
+	 * - Used only in Descent 1
+	 * - Used only in Descent 2
+	 * Therefore, this can be in dcx without causing ambiguity.  Sounds
+	 * used only for one game are unnecessarily defined in the other,
+	 * and then ignored.
+	 *
+	 * The values of sound_effect are used to index Sounds.  A uint8_t
+	 * would be sufficient to hold all values, but unsigned is used
+	 * since a uint8_t would need to be zero-extended before it could be
+	 * used in an array lookup.
+	 */
+enum sound_effect : unsigned
+{
+	SOUND_LASER_FIRED = 10,
+	SOUND_BADASS_EXPLOSION = 11,  // need something different for this if possible
+	SOUND_WEAPON_HIT_BLASTABLE = 11,
+	SOUND_ROBOT_HIT_PLAYER = 17,
+	SOUND_ROBOT_HIT = 20,
+	SOUND_VOLATILE_WALL_HIT = 21,
+	SOUND_DROP_BOMB = 26,
+	SOUND_WEAPON_HIT_DOOR = 27,
+	SOUND_CONTROL_CENTER_HIT = 30,
+	SOUND_LASER_HIT_CLUTTER = 30,
+	SOUND_CONTROL_CENTER_DESTROYED = 31,
+	SOUND_EXPLODING_WALL = 31,  // one long sound
+	SOUND_CONTROL_CENTER_WARNING_SIREN = 32,
+	SOUND_MINE_BLEW_UP = 33,
+	SOUND_FUSION_WARMUP = 34,
+	SOUND_REFUEL_STATION_GIVING_FUEL = 62,
+	SOUND_PLAYER_HIT_WALL = 70,
+	SOUND_PLAYER_GOT_HIT = 71,
+	SOUND_HOSTAGE_RESCUED = 91,
+	SOUND_COUNTDOWN_0_SECS = 100, // countdown 100..114
+	SOUND_COUNTDOWN_13_SECS = 113,
+	SOUND_COUNTDOWN_29_SECS = 114,
+	SOUND_HUD_MESSAGE = 117,
+	SOUND_HUD_KILL = 118,
+	SOUND_HOMING_WARNING = 122, // Warning beep: You are being tracked by a missile! Borrowed from old repair center sounds.
+	SOUND_VOLATILE_WALL_HISS = 151, // need a hiss sound here.
+	SOUND_GOOD_SELECTION_PRIMARY_FULL_DATA = 153,
+	SOUND_GOOD_SELECTION_SECONDARY_FULL_DATA = 154, // Adam: New sound number here! MK, 01/30/95
+	SOUND_ALREADY_SELECTED = 155, // Adam: New sound number here! MK, 01/30/95
+	SOUND_BAD_SELECTION = 156,
+	SOUND_CLOAK_OFF = 161, // sound when cloak goes away
+	SOUND_INVULNERABILITY_OFF = 163, // sound when invulnerability goes away
+	ROBOT_SEE_SOUND_DEFAULT = 170,
+	ROBOT_ATTACK_SOUND_DEFAULT = 171,
+	SOUND_BOSS_SHARE_SEE = 183,
+	SOUND_BOSS_SHARE_DIE = 185,
+	ROBOT_CLAW_SOUND_DEFAULT = 190,
+	SOUND_CHEATER_FULL_DATA = 200,
 
-#define SOUND_BOSS_SHARE_SEE                    183
-#define SOUND_BOSS_SHARE_DIE                    185
+	/* if DXX_BUILD_DESCENT_I */
+	/* Not used even there */
+#if 0
+	SOUND_BOSS_SHARE_ATTACK = 184,
 
-#define ROBOT_SEE_SOUND_DEFAULT                 170
-#define ROBOT_ATTACK_SOUND_DEFAULT              171
-#define ROBOT_CLAW_SOUND_DEFAULT                190
+	SOUND_NASTY_ROBOT_HIT_1 = 190,     //      ding.raw        ; tearing metal 1
+	SOUND_NASTY_ROBOT_HIT_2 = 191,     //      ding.raw        ; tearing metal 2
+#endif
+	/* endif */
+	/* if DXX_BUILD_DESCENT_II */
+	SOUND_LASER_HIT_WATER = 232,
+	SOUND_MISSILE_HIT_WATER = 233,
+	SOUND_DROP_WEAPON = 39,
+
+	SOUND_FORCEFIELD_BOUNCE_PLAYER = 40,
+	SOUND_FORCEFIELD_BOUNCE_WEAPON = 41,
+	SOUND_FORCEFIELD_HUM = 42,
+	SOUND_FORCEFIELD_OFF = 43,
+
+	SOUND_MARKER_HIT = 50,
+	SOUND_BUDDY_MET_GOAL = 51,
+
+	SOUND_BRIEFING_HUM = 94,
+	SOUND_BRIEFING_PRINTING = 95,
+	SOUND_HUD_JOIN_REQUEST = 123,
+	SOUND_HUD_BLUE_GOT_FLAG = 124,
+	SOUND_HUD_RED_GOT_FLAG = 125,
+	SOUND_HUD_YOU_GOT_FLAG = 126,
+	SOUND_HUD_BLUE_GOT_GOAL = 127,
+	SOUND_HUD_RED_GOT_GOAL = 128,
+	SOUND_HUD_YOU_GOT_GOAL = 129,
+
+	SOUND_LAVAFALL_HISS = 150, // under a lavafall
+	SOUND_SHIP_IN_WATER = 152, // sitting (or moving though) water
+	SOUND_SHIP_IN_WATERFALL = 158, // under a waterfall
+
+	SOUND_CLOAK_ON = 160, // USED FOR WALL CLOAK
+
+	SOUND_AMBIENT_LAVA = 222,
+	SOUND_AMBIENT_WATER = 223,
+
+	SOUND_CONVERT_ENERGY = 241,
+	SOUND_WEAPON_STOLEN = 244,
+
+	SOUND_LIGHT_BLOWNUP = 157,
+
+	SOUND_WALL_REMOVED = 246, // Wall removed, probably due to a wall switch.
+	SOUND_AFTERBURNER_IGNITE = 247,
+	SOUND_AFTERBURNER_PLAY = 248,
+
+	SOUND_SECRET_EXIT = 249,
+
+	SOUND_SEISMIC_DISTURBANCE_START = 251,
+
+	SOUND_YOU_GOT_ORB = 84,
+	SOUND_FRIEND_GOT_ORB = 85,
+	SOUND_OPPONENT_GOT_ORB = 86,
+	SOUND_OPPONENT_HAS_SCORED = 87,
+	/* endif */
+};
+
+}
+#endif
 
 #define SOUND_BIG_ENDLEVEL_EXPLOSION            SOUND_EXPLODING_WALL
 #define SOUND_TUNNEL_EXPLOSION                  SOUND_EXPLODING_WALL
 
-#define SOUND_DROP_BOMB                         26
-
 #if defined(DXX_BUILD_DESCENT_I)
 constexpr std::integral_constant<unsigned, 250> MAX_SOUNDS{};
 #elif defined(DXX_BUILD_DESCENT_II)
-#define SOUND_CHEATER                           200
-
-#define SOUND_AMBIENT_LAVA                      222
-#define SOUND_AMBIENT_WATER                     223
-
-#define SOUND_CONVERT_ENERGY                    241
-#define SOUND_WEAPON_STOLEN                     244
-
-#define SOUND_LIGHT_BLOWNUP                     157
-
-#define SOUND_WALL_REMOVED                      246 // Wall removed, probably due to a wall switch.
-#define SOUND_AFTERBURNER_IGNITE                247
-#define SOUND_AFTERBURNER_PLAY                  248
-
-#define SOUND_SECRET_EXIT                       249
-
 #define SOUND_WALL_CLOAK_ON                     SOUND_CLOAK_ON
 #define SOUND_WALL_CLOAK_OFF                    SOUND_CLOAK_OFF
-
-#define SOUND_SEISMIC_DISTURBANCE_START         251
-
-#define SOUND_YOU_GOT_ORB                       84
-#define SOUND_FRIEND_GOT_ORB                    85
-#define SOUND_OPPONENT_GOT_ORB                  86
-#define SOUND_OPPONENT_HAS_SCORED               87
 
 //--------------------------------------------------------------
 // bad to have sound 255!
