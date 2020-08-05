dxx-rebirth/dxx-rebirth

commit 4a98e796abb099eb4c592d9520e4edbef7a15d7e

Prevent stacking weapon rotation sounds
digi_play_sample_once is supposed to cancel prior instances of the
sound, but that functionality was lost in
21082c6db2dc0bafa0e2a282c19f4e9a2c0c7f6a.  As a result, the sound can be
stacked up to the limit of the engine.  Even when the functionality
existed, its implementation was wrong.

- Change these sounds to be attached to the player generating them.
- Pass the cancellation flag to other players when sending a sound.
- Send the full value of `volume`, rather than truncating it.
- Implement cancellation by killing and restarting the earlier version of a cancelled sound.
- Delete an ugly hack that prevented the patched logic from ever running.
- Fix an ancient quirk in digi_mixer that caused it to report all sounds as not playing, which then caused digi_sync_sounds to instantly cancel the new sound.

Reported-by: Mako88 <https://github.com/dxx-rebirth/dxx-rebirth/issues/88>
Reported-by: ryusei117 <https://github.com/dxx-rebirth/dxx-rebirth/issues/88#issuecomment-269597361>
Fixes: 21082c6db2dc0bafa0e2a282c19f4e9a2c0c7f6a ("Added own channel management to SDL_mixer sound interface since the builtin channel management of this lib cannot handle our needs; Little code cleanup")

similar/main/digiobj.cpp
@@ -46,6 +46,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "config.h"
 
 #include "compiler-begin.h"
+#include "compiler-exchange.h"
 #include "compiler-range_for.h"
 
 using std::max;
@@ -77,7 +78,7 @@ struct sound_object
 	union {
 		struct {
 			segnum_t			segnum;				// Used if SOF_LINK_TO_POS field is used
-			short			sidenum;
+			uint8_t sidenum;
 			vms_vector	position;
 		} pos;
 		struct {
@@ -87,22 +88,61 @@ struct sound_object
 	} link_type;
 };
 
-}
-
-namespace dsx {
-
 using sound_objects_t = array<sound_object, MAX_SOUND_OBJECTS>;
 static sound_objects_t SoundObjects;
 static short next_signature=0;
 
 static int N_active_sound_objects;
 
-static std::pair<sound_objects_t::iterator, sound_objects_t::iterator> find_sound_object_flags0()
+static void digi_kill_sound(sound_object &s)
+{
+	s.flags = 0;	// Mark as dead, so some other sound can use this sound
+	if (s.channel > -1)	{
+		N_active_sound_objects--;
+		digi_stop_sound(exchange(s.channel, -1));
+	}
+}
+
+static std::pair<sound_objects_t::iterator, sound_objects_t::iterator> find_sound_object_flags0(sound_objects_t &SoundObjects)
 {
 	const auto eso = SoundObjects.end();
 	const auto i = std::find_if(SoundObjects.begin(), eso, [](sound_object &so) { return so.flags == 0; });
 	return {i, eso};
 }
+
+static std::pair<sound_objects_t::iterator, sound_objects_t::iterator> find_sound_object(sound_objects_t &SoundObjects, const unsigned soundnum, const vcobjidx_t obj, const sound_stack once)
+{
+	if (once == sound_stack::allow_stacking)
+		return find_sound_object_flags0(SoundObjects);
+	const auto eso = SoundObjects.end();
+	sound_objects_t::iterator free_sound_object = eso;
+	for (auto i = SoundObjects.begin(); i != eso; ++i)
+	{
+		constexpr uint8_t used_obj = SOF_USED | SOF_LINK_TO_OBJ;
+		auto &so = *i;
+		const auto flags = so.flags;
+		if (flags == 0)
+			free_sound_object = i;
+		else if (so.soundnum == soundnum && (flags & used_obj) == used_obj)
+		{
+			/* No check for a signature match here.  If the signature
+			 * matches, this sound will be reclaimed to prevent
+			 * stacking.  If the signature does not match, then the old
+			 * sound is tied to a dead object and needs to be removed.
+			 */
+			if (so.link_type.obj.objnum == obj)
+			{
+				digi_kill_sound(so);
+				return {i, eso};
+			}
+		}
+	}
+	return {free_sound_object, eso};
+}
+
+}
+
+namespace dsx {
 
 /* Find the sound which actually equates to a sound number */
 int digi_xlat_sound(int soundno)
@@ -328,7 +368,7 @@ static void digi_start_sound_object(sound_object &s)
 		N_active_sound_objects++;
 }
 
-static void digi_link_sound_common(icobjptr_t viewer, sound_object &so, const vms_vector &pos, int forever, fix max_volume, const vm_distance max_distance, int soundnum, const vcsegptridx_t segnum)
+static void digi_link_sound_common(const vcobjptr_t viewer, sound_object &so, const vms_vector &pos, const int forever, const fix max_volume, const vm_distance max_distance, const int soundnum, const vcsegptridx_t segnum)
 {
 	so.signature=next_signature++;
 	if ( forever )
@@ -359,10 +399,9 @@ static void digi_link_sound_common(icobjptr_t viewer, sound_object &so, const vm
 	}
 }
 
-void digi_link_sound_to_object3( int org_soundnum, const vcobjptridx_t objnum, int forever, fix max_volume, const vm_distance max_distance, int loop_start, int loop_end )
+void digi_link_sound_to_object3(const unsigned org_soundnum, const vcobjptridx_t objnum, const uint8_t forever, const fix max_volume, const sound_stack once, const vm_distance max_distance, const int loop_start, const int loop_end)
 {
 	const auto &&viewer = vcobjptr(Viewer);
-	int volume,pan;
 	int soundnum;
 
 	soundnum = digi_xlat_sound(org_soundnum);
@@ -377,19 +416,12 @@ void digi_link_sound_to_object3( int org_soundnum, const vcobjptridx_t objnum, i
 		Int3();
 		return;
 	}
-	if (!forever)
-	{
-		// Hack to keep sounds from building up...
-		digi_get_sound_loc(viewer->orient, viewer->pos, vcsegptridx(viewer->segnum), objnum->pos, vcsegptridx(objnum->segnum), max_volume,&volume, &pan, max_distance);
-		digi_play_sample_3d(org_soundnum, pan, volume);
-		return;
-	}
 
 	if ( Newdemo_state == ND_STATE_RECORDING )		{
 		newdemo_record_link_sound_to_object3( org_soundnum, objnum, max_volume, max_distance, loop_start, loop_end );
 	}
 
-	auto f = find_sound_object_flags0();
+	const auto &&f = find_sound_object(SoundObjects, soundnum, objnum, once);
 	if (f.first == f.second)
 		return;
 	auto &so = *f.first;
@@ -401,14 +433,14 @@ void digi_link_sound_to_object3( int org_soundnum, const vcobjptridx_t objnum, i
 	digi_link_sound_common(viewer, so, objnum->pos, forever, max_volume, max_distance, soundnum, vcsegptridx(objnum->segnum));
 }
 
-void digi_link_sound_to_object2(int org_soundnum, const vcobjptridx_t objnum, int forever, fix max_volume, const vm_distance max_distance)
+void digi_link_sound_to_object2(const unsigned soundnum, const vcobjptridx_t objnum, const uint8_t forever, const fix max_volume, const sound_stack once, const vm_distance max_distance)
 {
-	digi_link_sound_to_object3( org_soundnum, objnum, forever, max_volume, max_distance, -1, -1 );
+	digi_link_sound_to_object3(soundnum, objnum, forever, max_volume, once, max_distance, -1, -1);
 }
 
-void digi_link_sound_to_object( int soundnum, const vcobjptridx_t objnum, int forever, fix max_volume )
+void digi_link_sound_to_object(const unsigned soundnum, const vcobjptridx_t objnum, const uint8_t forever, const fix max_volume, const sound_stack once)
 {
-	digi_link_sound_to_object2( soundnum, objnum, forever, max_volume, vm_distance{256*F1_0});
+	digi_link_sound_to_object2(soundnum, objnum, forever, max_volume, once, vm_distance{256*F1_0});
 }
 
 static void digi_link_sound_to_pos2(fvcobjptr &vcobjptr, const int org_soundnum, const vcsegptridx_t segnum, short sidenum, const vms_vector &pos, int forever, fix max_volume, const vm_distance max_distance)
@@ -437,7 +469,7 @@ static void digi_link_sound_to_pos2(fvcobjptr &vcobjptr, const int org_soundnum,
 		return;
 	}
 
-	auto f = find_sound_object_flags0();
+	auto f = find_sound_object_flags0(SoundObjects);
 	if (f.first == f.second)
 		return;
 	auto &so = *f.first;
@@ -454,30 +486,16 @@ void digi_link_sound_to_pos(int soundnum, const vcsegptridx_t segnum, short side
 	digi_link_sound_to_pos2(vcobjptr, soundnum, segnum, sidenum, pos, forever, max_volume, vm_distance{F1_0 * 256});
 }
 
-static void digi_kill_sound(sound_object &s)
-{
-	if (s.channel > -1)	{
-		digi_stop_sound( s.channel );
-		s.channel = -1;
-		N_active_sound_objects--;
-	}
-	s.flags = 0;	// Mark as dead, so some other sound can use this sound
-}
-
 //if soundnum==-1, kill any sound
 void digi_kill_sound_linked_to_segment( segnum_t segnum, int sidenum, int soundnum )
 {
-	int killed;
-
 	if (soundnum != -1)
 		soundnum = digi_xlat_sound(soundnum);
-	killed = 0;
 	range_for (auto &i, SoundObjects)
 	{
 		if ( (i.flags & SOF_USED) && (i.flags & SOF_LINK_TO_POS) )	{
 			if ((i.link_type.pos.segnum == segnum) && (i.link_type.pos.sidenum==sidenum) && (soundnum==-1 || i.soundnum==soundnum ))	{
 				digi_kill_sound(i);
-				killed++;
 			}
 		}
 	}
@@ -485,20 +503,14 @@ void digi_kill_sound_linked_to_segment( segnum_t segnum, int sidenum, int soundn
 
 void digi_kill_sound_linked_to_object(const vcobjptridx_t objnum)
 {
-	int killed;
-
-	killed = 0;
-
 	if ( Newdemo_state == ND_STATE_RECORDING )		{
 		newdemo_record_kill_sound_linked_to_object( objnum );
 	}
-
 	range_for (auto &i, SoundObjects)
 	{
 		if ( (i.flags & SOF_USED) && (i.flags & SOF_LINK_TO_OBJ ) )	{
 			if (i.link_type.obj.objnum == objnum)	{
 				digi_kill_sound(i);
-				killed++;
 			}
 		}
 	}

common/main/digi.h
@@ -62,13 +62,17 @@ extern void digi_close();
 extern void digi_play_sample( int sndnum, fix max_volume );
 extern void digi_play_sample_once( int sndnum, fix max_volume );
 #if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
-void digi_link_sound_to_object( int soundnum, vcobjptridx_t objnum, int forever, fix max_volume );
+enum class sound_stack : uint8_t
+{
+	allow_stacking,
+	cancel_previous,
+};
+void digi_link_sound_to_object(unsigned soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once);
 void digi_kill_sound_linked_to_segment(segnum_t segnum, int sidenum, int soundnum);
 void digi_link_sound_to_pos( int soundnum, vcsegptridx_t segnum, short sidenum, const vms_vector &pos, int forever, fix max_volume );
 // Same as above, but you pass the max distance sound can be heard.  The old way uses f1_0*256 for max_distance.
-void digi_link_sound_to_object2(int soundnum, vcobjptridx_t objnum, int forever, fix max_volume, vm_distance max_distance);
-
-void digi_link_sound_to_object3(int org_soundnum, vcobjptridx_t objnum, int forever, fix max_volume, vm_distance max_distance, int loop_start, int loop_end);
+void digi_link_sound_to_object2(unsigned soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once, vm_distance max_distance);
+void digi_link_sound_to_object3(unsigned soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once, vm_distance max_distance, int loop_start, int loop_end);
 void digi_kill_sound_linked_to_object(vcobjptridx_t);
 #endif
 

common/main/multi.h
@@ -55,6 +55,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #ifdef __cplusplus
 #include <stdexcept>
+#include "digi.h"
 #include "pack.h"
 #include "compiler-array.h"
 #include "ntstring.h"
@@ -95,7 +96,7 @@ extern int multi_protocol; // set and determinate used protocol
 #define MULTI_PROTO_UDP 1 // UDP protocol
 
 // What version of the multiplayer protocol is this? Increment each time something drastic changes in Multiplayer without the version number changes. Reset to 0 each time the version of the game changes
-#define MULTI_PROTO_VERSION	static_cast<uint16_t>(7)
+#define MULTI_PROTO_VERSION	static_cast<uint16_t>(8)
 // PROTOCOL VARIABLES AND DEFINES - END
 
 // limits for Packets (i.e. positional updates) per sec
@@ -435,6 +436,7 @@ static inline void multi_send_endlevel_start(multi_endlevel_type)
 void multi_send_player_deres(deres_type_t type);
 void multi_send_create_powerup(powerup_type_t powerup_type, vcsegidx_t segnum, vcobjidx_t objnum, const vms_vector &pos);
 }
+void multi_send_play_sound(int sound_num, fix volume, sound_stack once);
 #endif
 void multi_send_reappear();
 void multi_send_create_explosion(playernum_t);
@@ -443,7 +445,6 @@ namespace dcx {
 void multi_send_cloak(void);
 void multi_send_decloak(void);
 }
-void multi_send_play_sound(int sound_num, fix volume);
 void multi_digi_play_sample(int sndnum, fix max_volume);
 void multi_digi_play_sample_once(int soundnum, fix max_volume);
 void multi_send_score(void);

common/main/multiinternal.h
@@ -30,7 +30,7 @@
 	VALUE(MULTI_REMOVE_OBJECT         , 4)	\
 	VALUE(MULTI_MESSAGE               , 37)	/* (MAX_MESSAGE_LENGTH = 40) */	\
 	VALUE(MULTI_QUIT                  , 2)	\
-	VALUE(MULTI_PLAY_SOUND            , 4)	\
+	VALUE(MULTI_PLAY_SOUND           , 8)	\
 	VALUE(MULTI_CONTROLCEN           , 4)	\
 	VALUE(MULTI_ROBOT_CLAIM          , 5)	\
 	VALUE(MULTI_CLOAK                , 2)	\

common/main/robot.h
@@ -257,10 +257,11 @@ weapon_id_type get_robot_weapon(const robot_info &ri, const unsigned gun_num);
 static inline void boss_link_see_sound(const vcobjptridx_t objp)
 {
 #if defined(DXX_BUILD_DESCENT_I)
-	digi_link_sound_to_object2(SOUND_BOSS_SHARE_SEE, objp, 1, F1_0, vm_distance{F1_0*512});	//	F1_0*512 means play twice as loud
+	constexpr unsigned soundnum = SOUND_BOSS_SHARE_SEE;
 #elif defined(DXX_BUILD_DESCENT_II)
-	digi_link_sound_to_object2(Robot_info[get_robot_id(objp)].see_sound, objp, 1, F1_0, vm_distance{F1_0*512});	//	F1_0*512 means play twice as loud
+	const unsigned soundnum = Robot_info[get_robot_id(objp)].see_sound;
 #endif
+	digi_link_sound_to_object2(soundnum, objp, 1, F1_0, sound_stack::allow_stacking, vm_distance{F1_0*512});	//	F1_0*512 means play twice as loud
 }
 #endif
 

similar/arch/sdl/digi_mixer.cpp
@@ -246,7 +246,10 @@ void digi_mixer_set_digi_volume( int dvolume )
 	Mix_Volume(-1, fix2byte(dvolume));
 }
 
-int digi_mixer_is_channel_playing(int) { return 0; }
+int digi_mixer_is_channel_playing(const int c)
+{
+	return channels[c];
+}
 
 void digi_mixer_reset() {}
 void digi_mixer_stop_all_channels()

similar/main/ai.cpp
@@ -2173,7 +2173,7 @@ static void do_boss_dying_frame(const vmobjptridx_t objp)
 	if (Boss_dying_start_time + BOSS_DEATH_DURATION - BOSS_DEATH_SOUND_DURATION < GameTime64) {
 		if (!Boss_dying_sound_playing) {
 			Boss_dying_sound_playing = 1;
-			digi_link_sound_to_object2(SOUND_BOSS_SHARE_DIE, objp, 0, F1_0*4, vm_distance{F1_0*1024});	//	F1_0*512 means play twice as loud
+			digi_link_sound_to_object2(SOUND_BOSS_SHARE_DIE, objp, 0, F1_0*4, sound_stack::allow_stacking, vm_distance{F1_0*1024});	//	F1_0*512 means play twice as loud
                 } else if (d_rand() < FrameTime*16)
                         create_small_fireball_on_object(objp, (F1_0 + d_rand()) * 8, 0);
         } else if (d_rand() < FrameTime*8)
@@ -2184,7 +2184,7 @@ static void do_boss_dying_frame(const vmobjptridx_t objp)
 		Boss_dying_start_time=GameTime64; // make sure following only happens one time!
 		do_controlcen_destroyed_stuff(object_none);
 		explode_object(objp, F1_0/4);
-		digi_link_sound_to_object2(SOUND_BADASS_EXPLOSION, objp, 0, F2_0, vm_distance{F1_0*512});
+		digi_link_sound_to_object2(SOUND_BADASS_EXPLOSION, objp, 0, F2_0, sound_stack::allow_stacking, vm_distance{F1_0*512});
 	}
 }
 
@@ -2264,7 +2264,7 @@ static int do_robot_dying_frame(const vmobjptridx_t objp, fix64 start_time, fix 
 	if (start_time + roll_duration - sound_duration < GameTime64) {
 		if (!*dying_sound_playing) {
 			*dying_sound_playing = 1;
-			digi_link_sound_to_object2(death_sound, objp, 0, sound_scale, vm_distance{sound_scale*256});	//	F1_0*512 means play twice as loud
+			digi_link_sound_to_object2(death_sound, objp, 0, sound_scale, sound_stack::allow_stacking, vm_distance{sound_scale*256});	//	F1_0*512 means play twice as loud
 		} else if (d_rand() < FrameTime*16)
 			create_small_fireball_on_object(objp, (F1_0 + d_rand()) * (16 * expl_scale/F1_0)/8, 0);
 	} else if (d_rand() < FrameTime*8)
@@ -2288,7 +2288,7 @@ static void do_boss_dying_frame(const vmobjptridx_t objp)
 		Boss_dying_start_time=GameTime64; // make sure following only happens one time!
 		do_controlcen_destroyed_stuff(object_none);
 		explode_object(objp, F1_0/4);
-		digi_link_sound_to_object2(SOUND_BADASS_EXPLOSION, objp, 0, F2_0, vm_distance{F1_0*512});
+		digi_link_sound_to_object2(SOUND_BADASS_EXPLOSION, objp, 0, F2_0, sound_stack::allow_stacking, vm_distance{F1_0*512});
 	}
 }
 
@@ -2304,7 +2304,7 @@ static int do_any_robot_dying_frame(const vmobjptridx_t objp)
 		if (rval) {
 			objp->ctype.ai_info.dying_start_time = GameTime64; // make sure following only happens one time!
 			explode_object(objp, F1_0/4);
-			digi_link_sound_to_object2(SOUND_BADASS_EXPLOSION, objp, 0, F2_0, vm_distance{F1_0*512});
+			digi_link_sound_to_object2(SOUND_BADASS_EXPLOSION, objp, 0, F2_0, sound_stack::allow_stacking, vm_distance{F1_0*512});
 			if (Current_level_num < 0)
 			{
 				const auto id = get_robot_id(objp);

similar/main/collide.cpp
@@ -849,7 +849,7 @@ static window_event_result collide_weapon_and_wall(object_array &objects, fvmseg
 
 			if ( Weapon_info[get_weapon_id(weapon)].damage_radius ) {
 
-				digi_link_sound_to_object(SOUND_BADASS_EXPLOSION, weapon, 0, F1_0);
+				digi_link_sound_to_object(SOUND_BADASS_EXPLOSION, weapon, 0, F1_0, sound_stack::allow_stacking);
 
 				//	MK: 09/13/95: Badass in water is 1/2 normal intensity.
 				object_create_badass_explosion( weapon, hitseg, hitpt,

commit 294d54eb58c9f417ed95c293615d1e6305d362d6

Workaround for missing forever flag in sound object demo recording
In the original code there was a 'Hack to keep sounds from building up...'
which changed a non-forever linked sound to a non-linked sound. Therefore
the forever flag wasn't needed in the demo files. The commit
4a98e796abb099eb4c592d9520e4edbef7a15d7e removed the hack, which caused
non-forever sounds to start looping on demo playback.
This commit restores the hack as far as demo files are concerned.

Reported-by: zicodxx <https://github.com/dxx-rebirth/dxx-rebirth/issues/477>
Fixes: 4a98e796abb099eb4c592d9520e4edbef7a15d7e ("Prevent stacking weapon rotation sounds")

similar/main/digiobj.cpp
@@ -418,7 +418,15 @@ void digi_link_sound_to_object3(const unsigned org_soundnum, const vcobjptridx_t
 	}
 
 	if ( Newdemo_state == ND_STATE_RECORDING )		{
-		newdemo_record_link_sound_to_object3( org_soundnum, objnum, max_volume, max_distance, loop_start, loop_end );
+		if ( !forever ) { // forever flag is not recorded, use original limited sound objects hack for demo recording
+			int volume, pan;
+			auto segnum = vcsegptridx(objnum->segnum);
+			digi_get_sound_loc(viewer->orient, viewer->pos, segnum.absolute_sibling(viewer->segnum),
+			       objnum->pos, segnum, max_volume,
+			       &volume, &pan, max_distance);
+			newdemo_record_sound_3d_once( org_soundnum, pan, volume );
+		} else
+			newdemo_record_link_sound_to_object3( org_soundnum, objnum, max_volume, max_distance, loop_start, loop_end );
 	}
 
 	const auto &&f = find_sound_object(SoundObjects, soundnum, objnum, once);