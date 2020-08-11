dxx-rebirth/dxx-rebirth

commit 1c42f69ff2756b9f81f75ada89a14255e7b9fcd2

Use enum class for object_signature_t

similar/main/object.cpp
@@ -1154,7 +1154,7 @@ imobjptridx_t obj_create(const object_type_t type, const unsigned id, vmsegptrid
 	// Tell Valgrind to warn on any uninitialized fields.
 	DXX_POISON_VAR(*obj, 0xfd);
 
-	obj->signature = object_signature_t(signature.get() + 1);
+	obj->signature = next(signature);
 	obj->type 				= type;
 	obj->id 				= id;
 	obj->pos 				= pos;
@@ -1238,7 +1238,7 @@ imobjptridx_t obj_create_copy(const object &srcobj, const vmsegptridx_t newsegnu
 	*obj = srcobj;
 
 	obj_link_unchecked(Objects.vmptr, obj, newsegnum);
-	obj->signature = object_signature_t(signature.get() + 1);
+	obj->signature = next(signature);
 
 	//we probably should initialize sub-structures here
 
@@ -1865,7 +1865,7 @@ static window_event_result object_move_one(const vmobjptridx_t obj)
 			break;
 
 		default:
-			Error("Unknown control type %u in object %hu, sig/type/id = %i/%i/%i", static_cast<unsigned>(obj->control_type), static_cast<objnum_t>(obj), obj->signature.get(), obj->type, obj->id);
+			Error("Unknown control type %u in object %hu, sig/type/id = %i/%i/%i", static_cast<unsigned>(obj->control_type), static_cast<objnum_t>(obj), static_cast<uint16_t>(obj->signature), obj->type, obj->id);
 			break;
 
 	}

common/main/game.h
@@ -61,7 +61,6 @@ extern int d_tick_step;  // true once in interval of DESIGNATED_GAME_FRAMETIME
 }
 
 #if defined(DXX_BUILD_DESCENT_II)
-class object_signature_t;
 namespace dsx {
 extern struct object *Missile_viewer;
 extern object_signature_t Missile_viewer_sig;

common/main/newdemo.h
@@ -31,8 +31,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "fwd-segment.h"
 #include "fwd-window.h"
 
-class object_signature_t;
-
 #define ND_STATE_NORMAL			0
 #define ND_STATE_RECORDING		1
 #define ND_STATE_PLAYBACK		2

common/main/objnum.h
@@ -3,22 +3,11 @@
 
 typedef uint16_t objnum_t;
 
-class object_signature_t
+enum class object_signature_t : uint16_t
 {
-	uint16_t signature = 0;
-public:
-	constexpr object_signature_t() = default;
-	constexpr explicit object_signature_t(uint16_t s) :
-		signature(s)
-	{
-	}
-	bool operator==(const object_signature_t &rhs) const
-	{
-		return signature == rhs.signature;
-	}
-	bool operator!=(const object_signature_t &rhs) const
-	{
-		return !(*this == rhs);
-	}
-	uint16_t get() const { return signature; }
 };
+
+static constexpr object_signature_t next(object_signature_t o)
+{
+	return object_signature_t{static_cast<uint16_t>(static_cast<uint16_t>(o) + 1u)};
+}

similar/main/gamesave.cpp
@@ -765,7 +765,7 @@ static void write_object(const object &obj, short version, PHYSFS_File *f)
 
 			PHYSFS_writeSLE16(f, obj.ctype.laser_info.parent_type);
 			PHYSFS_writeSLE16(f, obj.ctype.laser_info.parent_num);
-			PHYSFS_writeSLE32(f, obj.ctype.laser_info.parent_signature.get());
+			PHYSFS_writeSLE32(f, static_cast<uint16_t>(obj.ctype.laser_info.parent_signature));
 
 			break;
 

similar/main/laser.cpp
@@ -1558,7 +1558,7 @@ void Laser_do_weapon_sequence(const vmobjptridx_t obj)
 
 	//delete weapons that are not moving
 	const auto Difficulty_level = GameUniqueState.Difficulty_level;
-	if (	!((d_tick_count ^ obj->signature.get()) & 3) &&
+	if (	!((d_tick_count ^ static_cast<uint16_t>(obj->signature)) & 3) &&
 			(get_weapon_id(obj) != weapon_id_type::FLARE_ID) &&
 			(Weapon_info[get_weapon_id(obj)].speed[Difficulty_level] > 0) &&
 			(vm_vec_mag_quick(obj->mtype.phys_info.velocity) < F2_0)) {

similar/main/multi.cpp
@@ -5879,7 +5879,7 @@ void multi_object_to_object_rw(object &obj, object_rw *obj_rw)
 	 */
 	*obj_rw = {};
 	DXX_POISON_DEFINED_VAR(*obj_rw, 0xfd);
-	obj_rw->signature     = obj.signature.get();
+	obj_rw->signature     = static_cast<uint16_t>(obj.signature);
 	obj_rw->type          = obj.type;
 	obj_rw->id            = obj.id;
 	obj_rw->control_type  = static_cast<uint8_t>(obj.control_type);
@@ -5934,7 +5934,7 @@ void multi_object_to_object_rw(object &obj, object_rw *obj_rw)
 		case object::control_type::weapon:
 			obj_rw->ctype.laser_info.parent_type      = obj.ctype.laser_info.parent_type;
 			obj_rw->ctype.laser_info.parent_num       = obj.ctype.laser_info.parent_num;
-			obj_rw->ctype.laser_info.parent_signature = obj.ctype.laser_info.parent_signature.get();
+			obj_rw->ctype.laser_info.parent_signature = static_cast<uint16_t>(obj.ctype.laser_info.parent_signature);
 			if (obj.ctype.laser_info.creation_time - GameTime64 < F1_0*(-18000))
 				obj_rw->ctype.laser_info.creation_time = F1_0*(-18000);
 			else
@@ -5965,7 +5965,7 @@ void multi_object_to_object_rw(object &obj, object_rw *obj_rw)
 			obj_rw->ctype.ai_info.cur_path_index         = obj.ctype.ai_info.cur_path_index;
 			obj_rw->ctype.ai_info.danger_laser_num       = obj.ctype.ai_info.danger_laser_num;
 			if (obj.ctype.ai_info.danger_laser_num != object_none)
-				obj_rw->ctype.ai_info.danger_laser_signature = obj.ctype.ai_info.danger_laser_signature.get();
+				obj_rw->ctype.ai_info.danger_laser_signature = static_cast<uint16_t>(obj.ctype.ai_info.danger_laser_signature);
 			else
 				obj_rw->ctype.ai_info.danger_laser_signature = 0;
 #if defined(DXX_BUILD_DESCENT_I)

similar/main/newdemo.cpp
@@ -576,7 +576,7 @@ namespace dsx {
 
 static uint16_t nd_get_object_signature(const vcobjptridx_t objp)
 {
-	return (objp->signature.get() << 9) ^ objp.get_unchecked_index();  // It's OKAY! We made sure, obj->signature is never has a value which short cannot handle!!! We cannot do this otherwise, without breaking the demo format!
+	return (static_cast<uint16_t>(objp->signature) << 9) ^ objp.get_unchecked_index();  // It's OKAY! We made sure, obj->signature is never has a value which short cannot handle!!! We cannot do this otherwise, without breaking the demo format!
 }
 
 static void nd_read_object(const vmobjptridx_t obj)

commit e0b10880de1a5f45e4fec058e196e83fb0f62b00

Fix build break in non-debug build diagnostic
Only non-debug builds print a warning for slew objects.

Fixes: 1c42f69ff2756b9f81f75ada89a14255e7b9fcd2 ("Use enum class for object_signature_t")
Reported-by: Kreeblah <https://github.com/dxx-rebirth/dxx-rebirth/issues/533>

similar/main/object.cpp
@@ -1833,7 +1833,7 @@ static window_event_result object_move_one(const vmobjptridx_t obj)
 		case object::control_type::slew:
 #ifdef RELEASE
 			obj->control_source = object::control_type::None;
-			con_printf(CON_URGENT, DXX_STRINGIZE_FL(__FILE__, __LINE__, "BUG: object %hu has control type object::control_type::slew, sig/type/id = %i/%i/%i"), static_cast<objnum_t>(obj), obj->signature.get(), obj->type, obj->id);
+			con_printf(CON_URGENT, DXX_STRINGIZE_FL(__FILE__, __LINE__, "BUG: object %hu has control type object::control_type::slew, sig/type/id = %hu/%i/%i"), static_cast<objnum_t>(obj), static_cast<uint16_t>(obj->signature), obj->type, obj->id);
 #else
 			if ( keyd_pressed[KEY_PAD5] ) slew_stop();
 			if ( keyd_pressed[KEY_NUMLOCK] ) 		{