dxx-rebirth/dxx-rebirth

commit c24864b180808c2e8bc8b03bd7aeb22a4b956e07

make menus controllable with joystick
In most menus, keyboard commands are synthesized
from controller buttons, leveraging the existing
axis-to-button translation.

Menu controls are currently fixed:
- button 0 (A) is confirm (Enter)
- button 1 (B) is cancel (Esc)
- button 2 (X) is switch (Space)
- button 3 (Y) is delete (Delete)
- axes 0 and 1 (main analog pad) maps to cursor keys
- all hats (D-pads) map to cursor keys

Title screens and credits can be confirmed
with any joystick button or axis motion too.

common/arch/sdl/joy.cpp
@@ -14,6 +14,7 @@
 #include <tuple>
 #include <type_traits>
 #include "joy.h"
+#include "key.h"
 #include "dxxerror.h"
 #include "timer.h"
 #include "console.h"
@@ -464,6 +465,49 @@ int apply_deadzone(int value, int deadzone)
 		return ((value + deadzone) * 128) / (128 - deadzone);
 	else
 		return 0;
+}
+
+bool joy_translate_menu_key(const d_event &event) {
+	if (event.type != EVENT_JOYSTICK_BUTTON_DOWN)
+		return false;
+#if DXX_MAX_BUTTONS_PER_JOYSTICK || DXX_MAX_HATS_PER_JOYSTICK || DXX_MAX_AXES_PER_JOYSTICK
+	// Find out which button has been pressed by decoding the button
+	// description text. This might not be the ideal way to do things,
+	// but we currently don't have data structures that allow us to do
+	// such a mapping in a saner way, so we'll take what we can get.
+	auto &e = static_cast<const d_event_joystickbutton &>(event);
+	Assert((e.button >= 0) && (e.button < joybutton_text.size()));
+	const char* name = &joybutton_text[e.button][0];
+	Assert((name[0] == 'J') && name[1] && (name[2] == ' '));
+	name = &name[3];  // skip 'Jx ' prefix
+	int key = 0;
+	if      (!strcmp(name, "B1"))  { key = KEY_ENTER; }
+	else if (!strcmp(name, "B2"))  { key = KEY_ESC; }
+	else if (!strcmp(name, "B3"))  { key = KEY_SPACEBAR; }
+	else if (!strcmp(name, "B4"))  { key = KEY_DELETE; }
+	else if (!strcmp(name, "+A1")) { key = KEY_LEFT; }
+	else if (!strcmp(name, "-A1")) { key = KEY_RIGHT; }
+	else if (!strcmp(name, "+A2")) { key = KEY_UP; }
+	else if (!strcmp(name, "-A2")) { key = KEY_DOWN; }
+	else if (name[0] == 'H')
+	{	// handle 'Hxx' / hat motion
+		Assert(name[1]);
+		switch (name[2])
+		{
+			case 0201: key = KEY_LEFT; break;
+			case 0177: key = KEY_RIGHT; break;
+			case 0202: key = KEY_UP; break;
+			case 0200: key = KEY_DOWN; break;
+			default: break;
+		}
+	}
+	if (key)
+	{
+		event_keycommand_send(key);
+		return true;
+	}
+	return false;
+#endif
 }
 
 }

common/include/joy.h
@@ -36,6 +36,7 @@ extern void joy_init();
 extern void joy_close();
 const d_event_joystick_axis_value &event_joystick_get_axis(const d_event &event);
 extern void joy_flush();
+extern bool joy_translate_menu_key(const d_event &event);
 extern int event_joystick_get_button(const d_event &event);
 extern int apply_deadzone(int value, int deadzone);
 

similar/main/credits.cpp
@@ -115,6 +115,9 @@ static window_event_result credits_handler(window *, const d_event &event, credi
 			}
 			break;
 
+		case EVENT_JOYSTICK_BUTTON_DOWN:
+			return window_event_result::close;
+
 		case EVENT_IDLE:
 			if (cr->done>NUM_LINES)
 			{

similar/main/kconfig.cpp
@@ -655,6 +655,9 @@ static window_event_result kconfig_key_command(window *, const d_event &event, k
 namespace dsx {
 static window_event_result kconfig_handler(window *wind,const d_event &event, kc_menu *menu)
 {
+	if (!menu->changing && joy_translate_menu_key(event))
+		return window_event_result::handled;
+
 	switch (event.type)
 	{
 		case EVENT_WINDOW_ACTIVATED:

similar/main/newmenu.cpp
@@ -1491,6 +1491,9 @@ static window_event_result newmenu_draw(window *wind, newmenu *menu)
 
 static window_event_result newmenu_handler(window *wind,const d_event &event, newmenu *menu)
 {
+	if (joy_translate_menu_key(event))
+		return window_event_result::handled;
+
 	if (menu->subfunction)
 	{
 		int rval = (*menu->subfunction)(menu, event, menu->userdata);
@@ -2109,6 +2112,9 @@ static window_event_result listbox_handler(window *wind,const d_event &event, li
 		if (rval != window_event_result::ignored)
 			return rval;		// event handled
 	}
+
+	if (joy_translate_menu_key(event))
+		return window_event_result::handled;
 
 	switch (event.type)
 	{

similar/main/scores.cpp
@@ -406,6 +406,9 @@ static window_event_result scores_handler(window *wind,const d_event &event, sco
 			}
 			break;
 
+		case EVENT_JOYSTICK_BUTTON_DOWN:
+			return window_event_result::close;
+
 		case EVENT_IDLE:
 			timer_delay2(50);
 			break;

similar/main/titles.cpp
@@ -141,6 +141,13 @@ static window_event_result title_handler(window *, const d_event &event, title_s
 				}
 			return result;
 
+		case EVENT_JOYSTICK_BUTTON_DOWN:
+			if (ts->allow_keys)
+			{
+				return window_event_result::close;
+			}
+			break;
+
 		case EVENT_IDLE:
 			timer_delay2(50);
 
@@ -1528,6 +1535,24 @@ static window_event_result briefing_handler(window *, const d_event &event, brie
 				return window_event_result::handled;
 			}
 			break;
+
+		case EVENT_JOYSTICK_BUTTON_DOWN:
+			// using joy_translate_menu_key doesn't work here for unclear
+			// reasons, so we build a reasonable facsimile right here
+			if (event_joystick_get_button(event) == 1)
+				return window_event_result::close;
+			if (br->new_screen)
+			{
+				if (!new_briefing_screen(br, 0))
+				{
+					return window_event_result::close;
+				}
+			}
+			else if (br->new_page)
+				init_new_page(br);
+			else
+				br->delay_count = 0;
+			return window_event_result::handled;
 
 		case EVENT_KEY_COMMAND:
 		{

common/arch/sdl/key.cpp
@@ -587,6 +587,11 @@ void key_flush()
 		restore_sticky_key(keystate, key);
 }
 
+void event_keycommand_send(int key) {
+	const d_event_keycommand event{EVENT_KEY_COMMAND, unsigned(key)};
+	event_send(event);
+}
+
 int event_key_get(const d_event &event)
 {
 	auto &e = static_cast<const d_event_keycommand &>(event);

commit b3c3066a38723fe4ff83242b1ffea96056bc4042

Fix no-joystick build
When joystick support is not present, `event_joystick_get_button` is not
declared in joy.h.

Fixes: c24864b180808c2e8bc8b03bd7aeb22a4b956e07 ("make menus controllable with joystick")

common/arch/sdl/joy.cpp
@@ -443,12 +443,14 @@ void joy_close()
 	joybutton_text.clear();
 }
 
+#if DXX_MAX_AXES_PER_JOYSTICK
 const d_event_joystick_axis_value &event_joystick_get_axis(const d_event &event)
 {
 	auto &e = static_cast<const d_event_joystick_moved &>(event);
 	Assert(e.type == EVENT_JOYSTICK_MOVED);
 	return e;
 }
+#endif
 
 void joy_flush()
 {
@@ -465,12 +467,14 @@ void joy_flush()
 #endif
 }
 
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 int event_joystick_get_button(const d_event &event)
 {
 	auto &e = static_cast<const d_event_joystickbutton &>(event);
 	Assert(e.type == EVENT_JOYSTICK_BUTTON_DOWN || e.type == EVENT_JOYSTICK_BUTTON_UP);
 	return e.button;
 }
+#endif
 
 int apply_deadzone(int value, int deadzone)
 {
@@ -482,10 +486,10 @@ int apply_deadzone(int value, int deadzone)
 		return 0;
 }
 
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 bool joy_translate_menu_key(const d_event &event) {
 	if (event.type != EVENT_JOYSTICK_BUTTON_DOWN)
 		return false;
-#if DXX_MAX_JOYSTICKS
 	auto &e = static_cast<const d_event_joystickbutton &>(event);
 	assert(e.button < joy_key_map.size());
 	auto key = joy_key_map[e.button];
@@ -495,8 +499,8 @@ bool joy_translate_menu_key(const d_event &event) {
 		return true;
 	}
 	return false;
-#endif
 }
+#endif
 
 }
 #endif

common/include/event.h
@@ -24,9 +24,13 @@ enum event_type : unsigned
 	EVENT_IDLE = 0,
 	EVENT_QUIT,
 
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 	EVENT_JOYSTICK_BUTTON_DOWN,
 	EVENT_JOYSTICK_BUTTON_UP,
+#endif
+#if DXX_MAX_AXES_PER_JOYSTICK
 	EVENT_JOYSTICK_MOVED,
+#endif
 
 	EVENT_MOUSE_BUTTON_DOWN,
 	EVENT_MOUSE_BUTTON_UP,

common/include/joy.h
@@ -34,10 +34,7 @@ struct d_event_joystick_axis_value
 
 extern void joy_init();
 extern void joy_close();
-const d_event_joystick_axis_value &event_joystick_get_axis(const d_event &event);
 extern void joy_flush();
-bool joy_translate_menu_key(const d_event &event);
-extern int event_joystick_get_button(const d_event &event);
 extern int apply_deadzone(int value, int deadzone);
 
 }
@@ -50,6 +47,8 @@ extern int apply_deadzone(int value, int deadzone);
 namespace dcx {
 
 #if DXX_MAX_BUTTONS_PER_JOYSTICK
+bool joy_translate_menu_key(const d_event &event);
+int event_joystick_get_button(const d_event &event);
 window_event_result joy_button_handler(const SDL_JoyButtonEvent *jbe);
 #else
 #define joy_button_handler(jbe) (static_cast<const SDL_JoyButtonEvent *const &>(jbe), window_event_result::ignored)
@@ -62,6 +61,7 @@ window_event_result joy_hat_handler(const SDL_JoyHatEvent *jhe);
 #endif
 
 #if DXX_MAX_AXES_PER_JOYSTICK
+const d_event_joystick_axis_value &event_joystick_get_axis(const d_event &event);
 window_event_result joy_axis_handler(const SDL_JoyAxisEvent *jae);
 #else
 #define joy_axis_handler(jbe) (static_cast<const SDL_JoyAxisEvent *const &>(jbe), window_event_result::ignored)

similar/editor/med.cpp
@@ -1116,9 +1116,13 @@ window_event_result editor_handler(UI_DIALOG *, const d_event &event, unused_ui_
 				if (!keyd_pressed[ KEY_LCTRL ] && !keyd_pressed[ KEY_RCTRL ])
 					break;
 				DXX_BOOST_FALLTHROUGH;
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 			case EVENT_JOYSTICK_BUTTON_UP:
 			case EVENT_JOYSTICK_BUTTON_DOWN:
+#endif
+#if DXX_MAX_AXES_PER_JOYSTICK
 			case EVENT_JOYSTICK_MOVED:
+#endif
 			case EVENT_KEY_COMMAND:
 			case EVENT_KEY_RELEASE:
 			case EVENT_IDLE:

similar/main/automap.cpp
@@ -1104,9 +1104,13 @@ window_event_result automap::event_handler(const d_event &event)
 #endif
 
 		case EVENT_IDLE:
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 		case EVENT_JOYSTICK_BUTTON_UP:
 		case EVENT_JOYSTICK_BUTTON_DOWN:
+#endif
+#if DXX_MAX_AXES_PER_JOYSTICK
 		case EVENT_JOYSTICK_MOVED:
+#endif
 		case EVENT_MOUSE_BUTTON_UP:
 		case EVENT_MOUSE_BUTTON_DOWN:
 		case EVENT_MOUSE_MOVED:

similar/main/credits.cpp
@@ -132,8 +132,10 @@ window_event_result credits_window::event_handler(const d_event &event)
 			}
 			break;
 
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 		case EVENT_JOYSTICK_BUTTON_DOWN:
 			return window_event_result::close;
+#endif
 
 		case EVENT_IDLE:
 			if (done > NUM_LINES)

similar/main/game.cpp
@@ -1597,9 +1597,13 @@ window_event_result game_window::event_handler(const d_event &event)
 			key_toggle_repeat(1);
 			break;
 
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 		case EVENT_JOYSTICK_BUTTON_UP:
 		case EVENT_JOYSTICK_BUTTON_DOWN:
+#endif
+#if DXX_MAX_AXES_PER_JOYSTICK
 		case EVENT_JOYSTICK_MOVED:
+#endif
 		case EVENT_MOUSE_BUTTON_UP:
 		case EVENT_MOUSE_BUTTON_DOWN:
 		case EVENT_MOUSE_MOVED:

similar/main/gamecntl.cpp
@@ -485,7 +485,11 @@ static int HandleDeathInput(const d_event &event)
 		}
 
 		if (RespawnMode == RespawnPress::Any
-			? (event.type == EVENT_JOYSTICK_BUTTON_UP || event.type == EVENT_MOUSE_BUTTON_UP)
+			? (
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
+				event.type == EVENT_JOYSTICK_BUTTON_UP ||
+#endif
+				event.type == EVENT_MOUSE_BUTTON_UP)
 			: (Controls.state.fire_primary || Controls.state.fire_secondary || Controls.state.fire_flare))
 			return 1;
 		return 0;

similar/main/kconfig.cpp
@@ -715,8 +715,10 @@ namespace {
 
 window_event_result kc_menu::event_handler(const d_event &event)
 {
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 	if (!changing && joy_translate_menu_key(event))
 		return window_event_result::handled;
+#endif
 
 	switch (event.type)
 	{
@@ -917,7 +919,7 @@ static void kc_change_key( kc_menu &menu,const d_event &event, kc_mitem &mitem )
 	kc_set_exclusive_binding(menu, mitem, BT_KEY, keycode);
 }
 
-#if DXX_MAX_BUTTONS_PER_JOYSTICK || DXX_MAX_HATS_PER_JOYSTICK
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 static void kc_change_joybutton( kc_menu &menu,const d_event &event, kc_mitem &mitem )
 {
 	int button = 255;
@@ -1152,7 +1154,7 @@ void kconfig_read_controls(control_info &Controls, const d_event &event, int aut
 				}
 			}
 			break;
-#if DXX_MAX_BUTTONS_PER_JOYSTICK || DXX_MAX_HATS_PER_JOYSTICK || DXX_MAX_AXES_PER_JOYSTICK
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 		case EVENT_JOYSTICK_BUTTON_DOWN:
 		case EVENT_JOYSTICK_BUTTON_UP:
 			if (!(PlayerCfg.ControlType & CONTROL_USING_JOYSTICK))

similar/main/newmenu.cpp
@@ -1572,8 +1572,10 @@ static window_event_result newmenu_draw(window *wind, newmenu *menu)
 
 static window_event_result newmenu_handler(window *wind,const d_event &event, newmenu *menu)
 {
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 	if (joy_translate_menu_key(event))
 		return window_event_result::handled;
+#endif
 
 	if (menu->subfunction)
 	{
@@ -2187,8 +2189,10 @@ static window_event_result listbox_handler(window *wind,const d_event &event, li
 			return rval;		// event handled
 	}
 
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 	if (joy_translate_menu_key(event))
 		return window_event_result::handled;
+#endif
 
 	switch (event.type)
 	{

similar/main/scores.cpp
@@ -404,8 +404,10 @@ static window_event_result scores_handler(window *wind,const d_event &event, sco
 			}
 			break;
 
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 		case EVENT_JOYSTICK_BUTTON_DOWN:
 			return window_event_result::close;
+#endif
 
 		case EVENT_IDLE:
 			timer_delay2(50);

similar/main/titles.cpp
@@ -145,11 +145,10 @@ static window_event_result title_handler(window *, const d_event &event, title_s
 				}
 			return result;
 
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 		case EVENT_JOYSTICK_BUTTON_DOWN:
-			{
 				return window_event_result::close;
-			}
-			break;
+#endif
 
 		case EVENT_IDLE:
 			timer_delay2(50);
@@ -1555,6 +1554,7 @@ static window_event_result briefing_handler(window *, const d_event &event, brie
 			}
 			break;
 
+#if DXX_MAX_BUTTONS_PER_JOYSTICK
 		case EVENT_JOYSTICK_BUTTON_DOWN:
 			// using joy_translate_menu_key doesn't work here for unclear
 			// reasons, so we build a reasonable facsimile right here
@@ -1572,6 +1572,7 @@ static window_event_result briefing_handler(window *, const d_event &event, brie
 			else
 				br->delay_count = 0;
 			return window_event_result::handled;
+#endif
 
 		case EVENT_KEY_COMMAND:
 		{