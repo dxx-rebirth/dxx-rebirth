dxx-rebirth/dxx-rebirth

commit de4efc4f46512e50734fd9e0f91f96a8feb249e7

Qualify more uses of shared_segment members

similar/main/endlevel.cpp
@@ -253,7 +253,7 @@ static unsigned get_tunnel_length(fvcsegptridx &vcsegptridx, const vcsegptridx_t
 	unsigned tunnel_length = 0;
 	for (;;)
 	{
-		const auto child = seg->children[exit_side];
+		const auto child = seg->shared_segment::children[exit_side];
 		if (child == segment_none)
 			return 0;
 		++tunnel_length;
@@ -285,7 +285,7 @@ static vcsegidx_t get_tunnel_transition_segment(const unsigned tunnel_length, co
 		 * this loop quits at (tunnel_length / 3), so the loop will quit
 		 * before it reaches segment_exit.
 		 */
-		const auto child = seg->children[exit_side];
+		const auto child = seg->shared_segment::children[exit_side];
 		if (!IS_CHILD(child))
 			/* This is only a sanity check.  It should never execute
 			 * unless there is a bug elsewhere.
@@ -1339,12 +1339,10 @@ static std::pair<icsegidx_t, sidenum_fast_t> find_exit_segment_side(fvcsegptridx
 {
 	range_for (const auto &&segp, vcsegptridx)
 	{
-		range_for (const auto &&e, enumerate(segp->children))
+		for (const auto &&[sidenum, child_segnum] : enumerate(segp->shared_segment::children))
 		{
-			const auto child_segnum = e.value;
 			if (child_segnum == segment_exit)
 			{
-				const auto sidenum = e.idx;
 				return {segp, sidenum};
 			}
 		}

common/main/fvi.h
@@ -105,7 +105,7 @@ struct sphere_intersects_wall_result
 };
 
 //Returns true if the object is through any walls
-sphere_intersects_wall_result sphere_intersects_wall(fvcvertptr &vcvertptr, const vms_vector &pnt, vcsegptridx_t seg, fix rad);
+sphere_intersects_wall_result sphere_intersects_wall(fvcsegptridx &vcsegptridx, fvcvertptr &vcvertptr, const vms_vector &pnt, vcsegptridx_t seg, fix rad);
 #endif
 
 #endif

similar/editor/curves.cpp
@@ -271,7 +271,7 @@ int generate_curve(const fix r1scale, const fix r4scale)
 
     if (CurveNumSegs) {
         med_form_bridge_segment( Cursegp, Side_opposite[AttachSide], Markedsegp, Markedside );
-        CurveSegs[CurveNumSegs] = vmsegptr(Markedsegp->children[Markedside]);
+        CurveSegs[CurveNumSegs] = vmsegptr(Markedsegp->shared_segment::children[Markedside]);
         CurveNumSegs++;
 	}
 
@@ -372,7 +372,7 @@ void generate_banked_curve(const fix maxscale, vms_equation coeffs)
 void delete_curve() {
 	range_for (auto &i, partial_const_range(CurveSegs, CurveNumSegs))
 	{
-        if (i->segnum != segment_none)
+        if (i->shared_segment::segnum != segment_none)
 			med_delete_segment(vmsegptridx(i));
     }
     Markedsegp = OriginalMarkedSeg;

similar/editor/eswitch.cpp
@@ -126,16 +126,17 @@ static int trigger_flag_Markedside(const TRIGGER_FLAG flag, const int value)
 	}
 
 	// If no child on Markedside return
-	if (!IS_CHILD(Markedsegp->children[Markedside])) return 0;
+	shared_segment &markedseg = Markedsegp;
+	if (!IS_CHILD(markedseg.children[Markedside])) return 0;
 
 	// If no wall just return
-	const auto wall_num = Markedsegp->shared_segment::sides[Markedside].wall_num;
+	const auto wall_num = markedseg.sides[Markedside].wall_num;
 	if (!value && wall_num == wall_none) return 0;
 	auto &Triggers = LevelUniqueWallSubsystemState.Triggers;
 	auto &vcvertptr = Vertices.vcptr;
 	auto &Walls = LevelUniqueWallSubsystemState.Walls;
 	auto &vcwallptr = Walls.vcptr;
-	const auto trigger_num = value ? add_trigger(Triggers, vcvertptr, Walls, Markedsegp, Markedside) : vcwallptr(wall_num)->trigger;
+	const auto trigger_num = value ? add_trigger(Triggers, vcvertptr, Walls, markedseg, Markedside) : vcwallptr(wall_num)->trigger;
 
 	if (trigger_num == trigger_none) {
 		editor_status(value ? "Cannot add trigger at Markedside." : "No trigger at Markedside.");
@@ -413,7 +414,8 @@ window_event_result trigger_dialog_handler(UI_DIALOG *dlg,const d_event &event, 
 	// If we change walls, we need to reset the ui code for all
 	// of the checkboxes that control the wall flags.  
 	//------------------------------------------------------------
-	const auto Markedwall = Markedsegp->shared_segment::sides[Markedside].wall_num;
+	msmusegment &&markedseg = Markedsegp;
+	const auto Markedwall = markedseg.s.sides[Markedside].wall_num;
 	auto &Walls = LevelUniqueWallSubsystemState.Walls;
 	auto &vcwallptr = Walls.vcptr;
 	const auto trigger_num = (Markedwall != wall_none) ? vcwallptr(Markedwall)->trigger : trigger_none;
@@ -445,7 +447,7 @@ window_event_result trigger_dialog_handler(UI_DIALOG *dlg,const d_event &event, 
 	// If any of the checkboxes that control the wallflags are set, then
 	// update the cooresponding wall flag.
 	//------------------------------------------------------------
-	if (IS_CHILD(Markedsegp->children[Markedside]))
+	if (IS_CHILD(markedseg.s.children[Markedside]))
 	{
 #if defined(DXX_BUILD_DESCENT_I)
 		rval = window_event_result::handled;
@@ -490,11 +492,11 @@ window_event_result trigger_dialog_handler(UI_DIALOG *dlg,const d_event &event, 
 		gr_set_current_canvas( t->wallViewBox->canvas );
 		auto &canvas = *grd_curcanv;
 
-		const auto wall_num = Markedsegp->shared_segment::sides[Markedside].wall_num;
+		const auto wall_num = markedseg.s.sides[Markedside].wall_num;
 		if (wall_num == wall_none || vcwallptr(wall_num)->trigger == trigger_none)
 			gr_clear_canvas(canvas, CBLACK);
 		else {
-			auto &us = Markedsegp->unique_segment::sides[Markedside];
+			auto &us = markedseg.u.sides[Markedside];
 			if (us.tmap_num2 > 0) 
 			{
 				gr_ubitmap(canvas, texmerge_get_cached_bitmap(us.tmap_num, us.tmap_num2));
@@ -517,7 +519,7 @@ window_event_result trigger_dialog_handler(UI_DIALOG *dlg,const d_event &event, 
 	//------------------------------------------------------------
 	if (event.type == EVENT_UI_DIALOG_DRAW)
 	{
-		if (Markedsegp->shared_segment::sides[Markedside].wall_num != wall_none)
+		if (markedseg.s.sides[Markedside].wall_num != wall_none)
 		{
 			ui_dprintf_at( MainWindow, 12, 6, "Trigger: %d    ", trigger_num);
 		}	else {

similar/editor/group.cpp
@@ -411,11 +411,11 @@ static void cgl_aux(const vmsegptridx_t segp, group::segment_array_type_t &segli
 		if (ignore_list->contains(segp))
 			return;
 
-	if (!visited[segp]) {
-		visited[segp] = true;
+	if (auto &&v = visited[segp]; !v) {
+		v = true;
 		seglistp.emplace_back(segp);
 
-		range_for (const auto c, segp->children)
+		for (const auto c : segp->shared_segment::children)
 			if (IS_CHILD(c))
 				cgl_aux(segp.absolute_sibling(c), seglistp, ignore_list, visited);
 	}
@@ -475,7 +475,7 @@ static void duplicate_group(std::array<uint8_t, MAX_VERTICES> &vertex_ids, group
 	//	and correct its vertex numbers by translating through new_vertex_ids
 	range_for(const auto &gs, new_segments)
 	{
-		auto &sp = *vmsegptr(gs);
+		shared_segment &sp = vmsegptr(gs);
 		range_for (auto &seg, sp.children)
 		{
 			if (IS_CHILD(seg)) {
@@ -538,7 +538,7 @@ static int med_copy_group(const unsigned delta_flag, const vmsegptridx_t base_se
 	int 			x;
 	int			new_current_group;
 
-	if (IS_CHILD(base_seg->children[base_side])) {
+	if (IS_CHILD(base_seg->shared_segment::children[base_side])) {
 		editor_status("Error -- unable to copy group, base_seg:base_side must be free.");
 		return 1;
 	}
@@ -593,7 +593,7 @@ static int med_copy_group(const unsigned delta_flag, const vmsegptridx_t base_se
 
 	range_for(const auto &gs, GroupList[new_current_group].segments)
 	{
-		auto &s = *vmsegptr(gs);
+		shared_segment &s = *vmsegptr(gs);
 		s.group = new_current_group;
 		s.special = SEGMENT_IS_NOTHING;
 		s.matcen_num = -1;
@@ -604,13 +604,13 @@ static int med_copy_group(const unsigned delta_flag, const vmsegptridx_t base_se
 	range_for(const auto &gs, GroupList[new_current_group].segments)
 	{
 		const auto &&segp = base_seg.absolute_sibling(gs);
-		range_for (const auto &&es, enumerate(segp->children))
-			if (IS_CHILD(es.value))
+		for (auto &&[child_segnum, sidenum] : enumerate(segp->shared_segment::children))
+			if (IS_CHILD(child_segnum))
 			{
-				if (!in_group(es.value, new_current_group))
+				if (!in_group(child_segnum, new_current_group))
 				{
-					es.value = segment_none;
-					validate_segment_side(vcvertptr, segp, es.idx);					// we have converted a connection to a side so validate the segment
+					child_segnum = segment_none;
+					validate_segment_side(vcvertptr, segp, sidenum);					// we have converted a connection to a side so validate the segment
 				}
 			}
 	}

similar/editor/medwall.cpp
@@ -732,9 +732,11 @@ int wall_restore_all()
 //	Remove a specific side.
 int wall_remove_side(const vmsegptridx_t seg, short side)
 {
-	if (IS_CHILD(seg->children[side]) && seg->shared_segment::sides[side].wall_num != wall_none)
+	const shared_segment &sseg = seg;
+	const auto seg_child = sseg.children[side];
+	if (IS_CHILD(seg_child) && sseg.sides[side].wall_num != wall_none)
 	{
-		shared_segment &csegp = *vmsegptr(seg->children[side]);
+		shared_segment &csegp = *vmsegptr(seg_child);
 		const auto Connectside = find_connect_side(seg, csegp);
 
 		remove_trigger(seg, side);

similar/main/ai.cpp
@@ -2084,7 +2084,7 @@ static const shared_segment *boss_intersects_wall(fvcvertptr &vcvertptr, const o
 	auto pos = segcenter;
 	for (uint_fast32_t posnum = 0;;)
 	{
-		const auto seg = sphere_intersects_wall(vcvertptr, pos, segp, size).seg;
+		const auto seg = sphere_intersects_wall(vcsegptridx, vcvertptr, pos, segp, size).seg;
 		if (!seg)
 			return seg;
 		if (posnum == segp->verts.size())
@@ -4434,7 +4434,7 @@ static void pae_aux(const vcsegptridx_t segnum, const player_awareness_type_t ty
 			? player_awareness_type_t::PA_PLAYER_COLLISION
 			: type;
 		const auto sublevel = level + 1;
-		range_for (auto &j, segnum->children)
+		for (const auto j : segnum->shared_segment::children)
 			if (IS_CHILD(j))
 				pae_aux(segnum.absolute_sibling(j), subtype, sublevel, New_awareness);
 	}

similar/main/aipath.cpp
@@ -336,7 +336,7 @@ if ((objp->type == OBJ_ROBOT) && (objp->ctype.ai_info.behavior == ai_behavior::A
 	auto &Walls = LevelUniqueWallSubsystemState.Walls;
 	auto &vcwallptr = Walls.vcptr;
 	while (cur_seg != end_seg) {
-		const auto &&segp = vcsegptr(cur_seg);
+		const cscusegment &&segp = vcsegptr(cur_seg);
 #if defined(DXX_BUILD_DESCENT_II)
 		if (random_flag != create_path_random_flag::nonrandom)
 			if (d_rand() < 8192)
@@ -347,7 +347,7 @@ if ((objp->type == OBJ_ROBOT) && (objp->ctype.ai_info.behavior == ai_behavior::A
 		{
 			const unsigned snum = (random_flag != create_path_random_flag::nonrandom) ? es.value : es.idx;
 
-			if (!IS_CHILD(segp->children[snum]))
+			if (!IS_CHILD(segp.s.children[snum]))
 				continue;
 #if defined(DXX_BUILD_DESCENT_I)
 #define AI_DOOR_OPENABLE_PLAYER_FLAGS
@@ -358,7 +358,7 @@ if ((objp->type == OBJ_ROBOT) && (objp->ctype.ai_info.behavior == ai_behavior::A
 			if ((WALL_IS_DOORWAY(GameBitmaps, Textures, vcwallptr, segp, snum) & WID_FLY_FLAG) || ai_door_is_openable(objp, AI_DOOR_OPENABLE_PLAYER_FLAGS segp, snum))
 #undef AI_DOOR_OPENABLE_PLAYER_FLAGS
 			{
-				const auto this_seg = segp->children[snum];
+				const auto this_seg = segp.s.children[snum];
 #if defined(DXX_BUILD_DESCENT_II)
 				Assert(this_seg != segment_none);
 				if (((cur_seg == avoid_seg) || (this_seg == avoid_seg)) && (ConsoleObject->segnum == avoid_seg)) {
@@ -586,8 +586,8 @@ int validate_path(int, point_seg *psegs, uint_fast32_t num_points)
 	{
 		auto nextseg = ps.segnum;
 		if (curseg != nextseg) {
-			const auto &&csegp = vcsegptr(curseg);
-			const auto &children = csegp->children;
+			const shared_segment &csegp = vcsegptr(curseg);
+			auto &children = csegp.children;
 			if (std::find(children.begin(), children.end(), nextseg) == children.end())
 			{
 			// Assert(sidenum != MAX_SIDES_PER_SEGMENT);	//	Hey, created path is not contiguous, why!?
@@ -1538,11 +1538,13 @@ static void test_create_all_paths(fvmobjptridx &vmobjptridx, fvcsegptridx &vcseg
 
 	range_for (const auto &&segp0, vcsegptridx)
 	{
-		if (segp0->segnum != segment_none)
+		const shared_segment &sseg0 = segp0;
+		if (sseg0.segnum != segment_none)
 		{
 			range_for (const auto &&segp1, partial_range(vcsegptridx, static_cast<segnum_t>(segp0), vcsegptridx.count()))
 			{
-				if (segp1->segnum != segment_none)
+				const shared_segment &sseg1 = segp1;
+				if (sseg1.segnum != segment_none)
 				{
 					create_path_points(vmobjptridx(object_first), segp0, segp1, Point_segs_free_ptr, MAX_PATH_LENGTH, create_path_random_flag::nonrandom, create_path_safety_flag::unsafe, segment_none);
 				}

commit d7560843b03c128b519976e33ccc1b418dee8142

Fix exit tunnel sequence
The index and the value were transposed, and since both were integer
types, the type system was unable to warn that the new code was
incorrect.  Swap the variables to bind as intended.

Reported-by: Donkyhotay <https://github.com/dxx-rebirth/dxx-rebirth/issues/544>
Fixes: de4efc4f46512e50734fd9e0f91f96a8feb249e7 ("Qualify more uses of shared_segment members")

similar/main/endlevel.cpp
@@ -1339,7 +1339,7 @@ static std::pair<icsegidx_t, sidenum_fast_t> find_exit_segment_side(fvcsegptridx
 {
 	range_for (const auto &&segp, vcsegptridx)
 	{
-		for (const auto &&[sidenum, child_segnum] : enumerate(segp->shared_segment::children))
+		for (const auto &&[child_segnum, sidenum] : enumerate(segp->shared_segment::children))
 		{
 			if (child_segnum == segment_exit)
 			{