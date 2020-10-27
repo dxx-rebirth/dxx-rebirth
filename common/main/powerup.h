dxx-rebirth/dxx-rebirth

commit ffb653c0b810bac5e1229f91ecedae515ae329e2

Pass control_info & to various functions
Switch from a direct global reference to passing control_info& as a
parameter.

common/main/powerup.h
@@ -159,10 +159,9 @@ extern void diminish_towards_max(void);
 namespace dsx {
 extern void do_megawow_powerup(int quantity);
 
+void powerup_basic_str(int redadd, int greenadd, int blueadd, int score, const char *str) __attribute_nonnull();
 }
 #endif
-void powerup_basic_str(int redadd, int greenadd, int blueadd, int score, const char *str) __attribute_nonnull();
-extern void powerup_basic(int redadd, int greenadd, int blueadd, int score, const char *format, ...) __attribute_format_printf(5, 6);
 #define powerup_basic(A1,A2,A3,A4,F,...)	dxx_call_printf_checked(powerup_basic,powerup_basic_str,(A1,A2,A3,A4),(F),##__VA_ARGS__)
 
 #endif

similar/main/powerup.cpp
@@ -131,6 +131,9 @@ void draw_powerup(const d_vclip_array &Vclip, grs_canvas &canvas, const object_b
 
 }
 
+namespace dsx {
+namespace {
+
 static void _powerup_basic_nonhud(int redadd, int greenadd, int blueadd, int score)
 {
 	PALETTE_FLASH_ADD(redadd,greenadd,blueadd);
@@ -147,6 +150,8 @@ void (powerup_basic)(int redadd, int greenadd, int blueadd, int score, const cha
 	_powerup_basic_nonhud(redadd, greenadd, blueadd, score);
 }
 
+}
+
 void powerup_basic_str(int redadd, int greenadd, int blueadd, int score, const char *str)
 {
 	HUD_init_message_literal(HM_DEFAULT, str);
@@ -155,7 +160,6 @@ void powerup_basic_str(int redadd, int greenadd, int blueadd, int score, const c
 
 //#ifndef RELEASE
 //	Give the megawow powerup!
-namespace dsx {
 void do_megawow_powerup(int quantity)
 {
 	auto &Objects = LevelUniqueObjectState.Objects;
@@ -191,10 +195,9 @@ void do_megawow_powerup(int quantity)
 		newdemo_record_laser_level(player_info.laser_level, laser_level);
 	player_info.laser_level = laser_level;
 }
-}
 //#endif
 
-namespace dsx {
+namespace {
 
 static int pick_up_energy(player_info &player_info)
 {
@@ -257,8 +260,6 @@ static int pick_up_key(const int r, const int g, const int b, player_flags &play
 }
 
 //	returns true if powerup consumed
-namespace {
-
 #if defined(DXX_BUILD_DESCENT_II)
 template <int r, int g, int b>
 struct player_hit_basic_silent_powerup
@@ -365,8 +366,6 @@ struct player_hit_quadlaser_powerup
 	}
 };
 
-}
-
 static int player_has_powerup(player_info &player_info, const char *const desc_have)
 {
 	HUD_init_message(HM_DEFAULT | HM_REDUNDANT | HM_MAYDUPL, "%s %s!", TXT_ALREADY_HAVE, desc_have);
@@ -380,6 +379,8 @@ static int player_hit_powerup(player_info &player_info, const char *const desc_h
 	return (powerup_flags & player_flag)
 		? player_has_powerup(player_info, desc_have)
 		: (pickup.template pickup<player_flag>(powerup_flags), 1);
+}
+
 }
 
 int do_powerup(const vmobjptridx_t obj)
@@ -478,10 +479,10 @@ int do_powerup(const vmobjptridx_t obj)
 				used = pick_up_energy(player_info);
 			break;
 		case POW_MISSILE_1:
-			used=pick_up_secondary(player_info, CONCUSSION_INDEX, 1);
+			used = pick_up_secondary(player_info, CONCUSSION_INDEX, 1, Controls);
 			break;
 		case POW_MISSILE_4:
-			used=pick_up_secondary(player_info, CONCUSSION_INDEX, 4);
+			used = pick_up_secondary(player_info, CONCUSSION_INDEX, 4, Controls);
 			break;
 
 		case POW_KEY_BLUE:
@@ -588,38 +589,38 @@ int do_powerup(const vmobjptridx_t obj)
 #endif
 
 		case	POW_PROXIMITY_WEAPON:
-			used=pick_up_secondary(player_info, PROXIMITY_INDEX, 4);
+			used = pick_up_secondary(player_info, PROXIMITY_INDEX, 4, Controls);
 			break;
 		case	POW_SMARTBOMB_WEAPON:
-			used=pick_up_secondary(player_info, SMART_INDEX, 1);
+			used = pick_up_secondary(player_info, SMART_INDEX, 1, Controls);
 			break;
 		case	POW_MEGA_WEAPON:
-			used=pick_up_secondary(player_info, MEGA_INDEX, 1);
+			used = pick_up_secondary(player_info, MEGA_INDEX, 1, Controls);
 			break;
 #if defined(DXX_BUILD_DESCENT_II)
 		case	POW_SMISSILE1_1:
-			used=pick_up_secondary(player_info, SMISSILE1_INDEX, 1);
+			used = pick_up_secondary(player_info, SMISSILE1_INDEX, 1, Controls);
 			break;
 		case	POW_SMISSILE1_4:
-			used=pick_up_secondary(player_info, SMISSILE1_INDEX, 4);
+			used = pick_up_secondary(player_info, SMISSILE1_INDEX, 4, Controls);
 			break;
 		case	POW_GUIDED_MISSILE_1:
-			used=pick_up_secondary(player_info, GUIDED_INDEX, 1);
+			used = pick_up_secondary(player_info, GUIDED_INDEX, 1, Controls);
 			break;
 		case	POW_GUIDED_MISSILE_4:
-			used=pick_up_secondary(player_info, GUIDED_INDEX, 4);
+			used = pick_up_secondary(player_info, GUIDED_INDEX, 4, Controls);
 			break;
 		case	POW_SMART_MINE:
-			used=pick_up_secondary(player_info, SMART_MINE_INDEX, 4);
+			used = pick_up_secondary(player_info, SMART_MINE_INDEX, 4, Controls);
 			break;
 		case	POW_MERCURY_MISSILE_1:
-			used=pick_up_secondary(player_info, SMISSILE4_INDEX, 1);
+			used = pick_up_secondary(player_info, SMISSILE4_INDEX, 1, Controls);
 			break;
 		case	POW_MERCURY_MISSILE_4:
-			used=pick_up_secondary(player_info, SMISSILE4_INDEX, 4);
+			used = pick_up_secondary(player_info, SMISSILE4_INDEX, 4, Controls);
 			break;
 		case	POW_EARTHSHAKER_MISSILE:
-			used=pick_up_secondary(player_info, SMISSILE5_INDEX, 1);
+			used = pick_up_secondary(player_info, SMISSILE5_INDEX, 1, Controls);
 			break;
 #endif
 		case	POW_VULCAN_AMMO:
@@ -630,10 +631,10 @@ int do_powerup(const vmobjptridx_t obj)
 #endif
 			break;
 		case	POW_HOMING_AMMO_1:
-			used=pick_up_secondary(player_info, HOMING_INDEX, 1);
+			used = pick_up_secondary(player_info, HOMING_INDEX, 1, Controls);
 			break;
 		case	POW_HOMING_AMMO_4:
-			used=pick_up_secondary(player_info, HOMING_INDEX, 4);
+			used = pick_up_secondary(player_info, HOMING_INDEX, 4, Controls);
 			break;
 		case	POW_CLOAK:
 			if (player_info.powerup_flags & PLAYER_FLAGS_CLOAKED) {

common/include/console.h
@@ -17,6 +17,9 @@
 #include "cli.h"
 #include "cmd.h"
 #include "d_srcloc.h"
+#ifdef dsx
+#include "kconfig.h"
+#endif
 
 namespace dcx {
 
@@ -73,6 +76,13 @@ void con_printf(con_priority_wrapper level, const char *fmt, ...) __attribute_fo
 #define con_printf(A1,F,...)	\
 	DXX_CON_PRINTF_CHECK_TRAILING_NEWLINE(F)	\
 	dxx_call_printf_checked(con_printf,con_puts,(A1),(F),##__VA_ARGS__)
-void con_showup(void);
 
 }
+
+#ifdef dsx
+namespace dsx {
+
+void con_showup(control_info &Controls);
+
+}
+#endif

common/main/ai.h
@@ -249,7 +249,7 @@ imobjptridx_t boss_spew_robot(const object_base &objp, const vms_vector &pos);
 // so we don't want to call it again (for this object) unless we have to.
 
 // -- unused, 08/07/95 -- extern void ai_turn_randomly(vms_vector *vec_to_player, object *obj, fix rate, int previous_visibility);
-void init_ai_frame(player_flags);
+void init_ai_frame(player_flags, const control_info &Controls);
 
 __attribute_warn_unused_result
 std::size_t create_bfs_list(vmobjptr_t robot, vcsegidx_t start_seg, player_flags, segnum_t *bfs_list, std::size_t max_segs);

common/main/automap.h
@@ -55,7 +55,7 @@ void automap_clear_visited(d_level_unique_automap_state &LevelUniqueAutomapState
 namespace dsx {
 void DropBuddyMarker(object &objp);
 void InitMarkerInput();
-window_event_result MarkerInputMessage(int key);
+window_event_result MarkerInputMessage(int key, control_info &Controls);
 
 constexpr std::integral_constant<std::size_t, 16> NUM_MARKERS{};
 constexpr std::integral_constant<std::size_t, 40> MARKER_MESSAGE_LEN{};

common/main/controls.h
@@ -28,17 +28,16 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #ifdef __cplusplus
 #include "fwd-object.h"
 
-#if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
 #ifdef dsx
+#include "kconfig.h"
 namespace dsx {
-void read_flying_controls(object &obj);
+void read_flying_controls(object &obj, control_info &Controls);
 }
 #if defined(DXX_BUILD_DESCENT_II)
 #include "maths.h"
 namespace dsx {
 extern fix Afterburner_charge;
 }
-#endif
 #endif
 #endif
 

common/main/fwd-weapon.h
@@ -31,6 +31,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "maths.h"
 #include "dxxsconf.h"
 #include "dsx-ns.h"
+#include "kconfig.h"
 #include "weapon_id.h"
 
 #include "fwd-object.h"
@@ -195,7 +196,7 @@ has_weapon_result player_has_secondary_weapon(const player_info &, secondary_wea
 //called when one of these weapons is picked up
 //when you pick up a secondary, you always get the weapon & ammo for it
 int pick_up_primary(player_info &, int weapon_index);
-int pick_up_secondary(player_info &, int weapon_index,int count);
+int pick_up_secondary(player_info &, int weapon_index, int count, const control_info &Controls);
 
 //called when a primary weapon is picked up
 //returns true if actually picked up

common/main/game.h
@@ -37,6 +37,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "window.h"
 #include "d_array.h"
 #include "gauges.h"
+#include "kconfig.h"
 #include "wall.h"
 
 #define DESIGNATED_GAME_FPS 30 // assuming the original intended Framerate was 30
@@ -245,6 +246,7 @@ extern d_level_unique_seismic_state LevelUniqueSeismicState;
 
 extern d_game_shared_state GameSharedState;
 extern d_game_unique_state GameUniqueState;
+void game_render_frame(const control_info &Controls);
 }
 #endif
 
@@ -252,7 +254,6 @@ extern d_game_unique_state GameUniqueState;
 #define PALETTE_FLASH_SET(_r,_g,_b) PaletteRedAdd=(_r), PaletteGreenAdd=(_g), PaletteBlueAdd=(_b)
 
 namespace dcx {
-void game_flush_respawn_inputs();
 
 extern int last_drawn_cockpit;
 
@@ -296,7 +297,8 @@ extern int Rear_view;           // if true, looking back.
 
 #ifdef dsx
 namespace dsx {
-void game_flush_inputs();    // clear all inputs
+void game_flush_respawn_inputs(control_info &Controls);
+void game_flush_inputs(control_info &Controls);    // clear all inputs
 // initalize flying
 }
 #endif
@@ -403,10 +405,10 @@ void flickering_light_read(flickering_light &fl, PHYSFS_File *fp);
 void flickering_light_write(const flickering_light &fl, PHYSFS_File *fp);
 #endif
 
-void game_render_frame_mono();
-static inline void game_render_frame_mono(int skip_flip)
+void game_render_frame_mono(const control_info &Controls);
+static inline void game_render_frame_mono(int skip_flip, const control_info &Controls)
 {
-	game_render_frame_mono();
+	game_render_frame_mono(Controls);
 	if (!skip_flip)
 		gr_flip();
 }
@@ -454,18 +456,16 @@ extern game_cheats cheats;
 
 game_window *game_setup();
 window_event_result game_handler(window *wind,const d_event &event, const unused_window_userdata_t *);
-window_event_result ReadControls(const d_event &event);
+window_event_result ReadControls(const d_event &event, control_info &Controls);
 bool allowed_to_fire_laser(const player_info &);
 void reset_globals_for_new_game();
+void check_rear_view(control_info &Controls);
+int create_special_path();
 }
 #endif
 int cheats_enabled();
 void game_disable_cheats();
-void	check_rear_view(void);
-int create_special_path(void);
-window_event_result ReadControls(const d_event &event);
 void toggle_cockpit(void);
-void game_render_frame();
 extern fix Show_view_text_timer;
 extern d_time_fix ThisLevelTime;
 extern int	Last_level_path_created;

commit a028ea14ae9d3492411871b9f49ed4dca79b1b0f

Restore format(printf) attribute for powerup_basic
Kreeblah reports that clang now warns for powerup.cpp due to a missing
__attribute_format_printf.  This was incorrectly dropped in
ffb653c0b810bac5e1229f91ecedae515ae329e2.  gcc did not warn, but clang
now warns.  Add back the attribute.

Fixes: ffb653c0b810bac5e1229f91ecedae515ae329e2 ("Pass control_info & to various functions")
Reported-by: Kreeblah <https://github.com/dxx-rebirth/dxx-rebirth/pull/547>

common/main/powerup.h
@@ -162,6 +162,5 @@ extern void do_megawow_powerup(int quantity);
 void powerup_basic_str(int redadd, int greenadd, int blueadd, int score, const char *str) __attribute_nonnull();
 }
 #endif
-#define powerup_basic(A1,A2,A3,A4,F,...)	dxx_call_printf_checked(powerup_basic,powerup_basic_str,(A1,A2,A3,A4),(F),##__VA_ARGS__)
 
 #endif

similar/main/powerup.cpp
@@ -140,6 +140,8 @@ static void _powerup_basic_nonhud(int redadd, int greenadd, int blueadd, int sco
 	add_points_to_score(ConsoleObject->ctype.player_info, score);
 }
 
+#define powerup_basic(A1,A2,A3,A4,F,...)	dxx_call_printf_checked(powerup_basic,powerup_basic_str,(A1,A2,A3,A4),(F),##__VA_ARGS__)
+__attribute_format_printf(5, 6)
 void (powerup_basic)(int redadd, int greenadd, int blueadd, int score, const char *format, ...)
 {
 	va_list	args;