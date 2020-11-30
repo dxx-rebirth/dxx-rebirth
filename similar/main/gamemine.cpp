dxx-rebirth/dxx-rebirth

commit 6d3dce4e16137b01d1d9814c9ed76d2f82f16837

Use enum class for tmap_num2
Define separate enum values for rotation data in both the high bits,
where it is usually kept, and the low bits, where it is sometimes used
for math or comparisons.

Define an enum value to represent the composite of the index and the
rotation, since the composite is not suitable for use as an array
subscript.  Add helper functions to extract the component pieces.

similar/main/gamemine.cpp
@@ -94,7 +94,8 @@ int d1_pig_present = 0; // can descent.pig from descent 1 be loaded?
  * If we can load the original d1 pig, we make sure this function is bijective.
  * This function was updated using the file config/convtabl.ini from devil 2.2.
  */
-short convert_d1_tmap_num(short d1_tmap_num) {
+uint16_t convert_d1_tmap_num(const uint16_t d1_tmap_num)
+{
 	switch (d1_tmap_num) {
 	case 0: case 2: case 4: case 5:
 		// all refer to grey rock001 (exception to bijectivity rule)
@@ -946,13 +947,11 @@ int load_mine_data_compiled(PHYSFS_File *LoadFile, const char *const Gamesave_cu
 				uside.tmap_num = convert_tmap(temp_ushort & 0x7fff);
 
 				if (New_file_format_load && !(temp_ushort & 0x8000))
-					uside.tmap_num2 = 0;
+					uside.tmap_num2 = texture2_value::None;
 				else {
 					// Read short Segments[segnum].sides[sidenum].tmap_num2;
-					uside.tmap_num2 = PHYSFSX_readShort(LoadFile);
-					uside.tmap_num2 =
-						(convert_tmap(uside.tmap_num2 & 0x3fff)) |
-						(uside.tmap_num2 & 0xc000);
+					const auto tmap_num2 = texture2_value{static_cast<uint16_t>(PHYSFSX_readShort(LoadFile))};
+					uside.tmap_num2 = build_texture2_value(convert_tmap(get_texture_index(tmap_num2)), get_texture_rotation_high(tmap_num2));
 				}
 #elif defined(DXX_BUILD_DESCENT_II)
 				if (New_file_format_load) {
@@ -964,12 +963,13 @@ int load_mine_data_compiled(PHYSFS_File *LoadFile, const char *const Gamesave_cu
 					uside.tmap_num = convert_d1_tmap_num(uside.tmap_num);
 
 				if (New_file_format_load && !(temp_ushort & 0x8000))
-					uside.tmap_num2 = 0;
+					uside.tmap_num2 = texture2_value::None;
 				else {
 					// Read short Segments[segnum].sides[sidenum].tmap_num2;
-					uside.tmap_num2 = PHYSFSX_readShort(LoadFile);
-					if (Gamesave_current_version <= 1 && uside.tmap_num2 != 0)
-						uside.tmap_num2 = convert_d1_tmap_num(uside.tmap_num2);
+					const auto tmap_num2 = static_cast<texture2_value>(PHYSFSX_readShort(LoadFile));
+					uside.tmap_num2 = (Gamesave_current_version <= 1 && uside.tmap_num2 != texture2_value::None)
+						? build_texture2_value(convert_d1_tmap_num(get_texture_index(uside.tmap_num2)), get_texture_rotation_high(tmap_num2))
+						: tmap_num2;
 				}
 #endif
 
@@ -985,7 +985,7 @@ int load_mine_data_compiled(PHYSFS_File *LoadFile, const char *const Gamesave_cu
 				}
 			} else {
 				uside.tmap_num = 0;
-				uside.tmap_num2 = 0;
+				uside.tmap_num2 = texture2_value::None;
 				uside.uvls = {};
 			}
 		}

common/include/ogl_init.h
@@ -21,6 +21,7 @@
 #include "d_gl.h"
 #include "dsx-ns.h"
 #include "fwd-gr.h"
+#include "fwd-segment.h"
 #include "palette.h"
 #include "pstypes.h"
 #include "3d.h"
@@ -102,11 +103,10 @@ void ogl_cache_level_textures();
 }
 #endif
 
-#include "3d.h"
-void _g3_draw_tmap_2(grs_canvas &, unsigned nv, const g3s_point *const *const pointlist, const g3s_uvl *uvl_list, const g3s_lrgb *light_rgb, grs_bitmap &bmbot, grs_bitmap &bm, unsigned orient);
+void _g3_draw_tmap_2(grs_canvas &, unsigned nv, const g3s_point *const *const pointlist, const g3s_uvl *uvl_list, const g3s_lrgb *light_rgb, grs_bitmap &bmbot, grs_bitmap &bm, texture2_rotation_low orient);
 
 template <std::size_t N>
-static inline void g3_draw_tmap_2(grs_canvas &canvas, const unsigned nv, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bmbot, grs_bitmap &bm, const unsigned orient)
+static inline void g3_draw_tmap_2(grs_canvas &canvas, const unsigned nv, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bmbot, grs_bitmap &bm, const texture2_rotation_low orient)
 {
 	static_assert(N <= MAX_POINTS_PER_POLY, "too many points in tmap");
 #ifdef DXX_CONSTANT_TRUE

common/main/fwd-segment.h
@@ -74,6 +74,10 @@ enum sidenum_t : uint8_t
 	WFRONT = 5
 };
 
+enum class texture2_value : uint16_t;
+enum class texture2_rotation_low : uint8_t;
+enum class texture2_rotation_high : uint16_t;
+
 //normal everyday vertices
 
 constexpr std::integral_constant<fix, 0> DEFAULT_LIGHTING{};   // (F1_0/2)

common/main/gamemine.h
@@ -148,6 +148,6 @@ namespace dsx {
 extern int d1_pig_present;
 
 /* stuff for loading descent.pig of descent 1 */
-extern short convert_d1_tmap_num(short d1_tmap_num);
+uint16_t convert_d1_tmap_num(uint16_t d1_tmap_num);
 }
 #endif

common/main/newdemo.h
@@ -110,7 +110,7 @@ extern void newdemo_record_rearview(void);
 extern void newdemo_record_restore_cockpit(void);
 #ifdef dsx
 void newdemo_record_wall_set_tmap_num1(vcsegidx_t seg, unsigned side, vcsegidx_t cseg, unsigned cside, int16_t tmap);
-void newdemo_record_wall_set_tmap_num2(vcsegidx_t seg, unsigned side, vcsegidx_t cseg, unsigned cside, int16_t tmap);
+void newdemo_record_wall_set_tmap_num2(vcsegidx_t seg, unsigned side, vcsegidx_t cseg, unsigned cside, texture2_value tmap);
 #endif
 extern void newdemo_record_multi_cloak(int pnum);
 extern void newdemo_record_multi_decloak(int pnum);

common/main/segment.h
@@ -148,10 +148,75 @@ struct shared_side
 	std::array<vms_vector, 2> normals;  // 2 normals, if quadrilateral, both the same.
 };
 
+/* texture2_value uses the low 14 bits for the array index and the high
+ * 2 bits for texture2_rotation_high
+ */
+enum class texture2_value : uint16_t
+{
+	None,
+};
+
+enum class texture2_rotation_low : uint8_t
+{
+	Normal = 0,
+	_1 = 1,
+	_2 = 2,
+	_3 = 3,
+};
+
+#define TEXTURE2_ROTATION_SHIFT	14u
+#define TEXTURE2_ROTATION_INDEX_MASK	((1u << TEXTURE2_ROTATION_SHIFT) - 1u)
+
+enum class texture2_rotation_high : uint16_t
+{
+	Normal = 0,
+	_1 = static_cast<uint16_t>(texture2_rotation_low::_1) << TEXTURE2_ROTATION_SHIFT,
+	_2 = static_cast<uint16_t>(texture2_rotation_low::_2) << TEXTURE2_ROTATION_SHIFT,
+	_3 = static_cast<uint16_t>(texture2_rotation_low::_3) << TEXTURE2_ROTATION_SHIFT,
+};
+
+static constexpr texture2_rotation_high &operator++(texture2_rotation_high &t)
+{
+	/* Cast to uint32, step to the next value, cast back.  The cast back
+	 * will truncate to the low 16 bits, causing
+	 * texture2_rotation_high::_3 to roll over to
+	 * texture2_rotation_high::Normal.
+	 */
+	return (t = static_cast<texture2_rotation_high>(static_cast<uint32_t>(t) + (1u << TEXTURE2_ROTATION_SHIFT)));
+}
+
+static constexpr uint16_t get_texture_index(const texture2_value t)
+{
+	return static_cast<uint16_t>(t) & TEXTURE2_ROTATION_INDEX_MASK;
+}
+
+static constexpr texture2_rotation_high get_texture_rotation_high(const texture2_value t)
+{
+	return static_cast<texture2_rotation_high>(static_cast<uint16_t>(t) & ~TEXTURE2_ROTATION_INDEX_MASK);
+}
+
+static constexpr texture2_rotation_low get_texture_rotation_low(const texture2_rotation_high t)
+{
+	return static_cast<texture2_rotation_low>(static_cast<uint16_t>(t) >> TEXTURE2_ROTATION_SHIFT);
+}
+
+static constexpr texture2_rotation_low get_texture_rotation_low(const texture2_value t)
+{
+	return get_texture_rotation_low(get_texture_rotation_high(t));
+}
+
+static constexpr texture2_value build_texture2_value(const unsigned t, const texture2_rotation_high rotation)
+{
+	return static_cast<texture2_value>(t | static_cast<unsigned>(rotation));
+}
+
+#undef TEXTURE2_ROTATION_SHIFT
+#undef TEXTURE2_ROTATION_INDEX_MASK
+
 struct unique_side
 {
 	int16_t tmap_num;
-	int16_t tmap_num2;
+	texture2_value tmap_num2;
 	std::array<uvl, 4>     uvls;
 };
 

common/main/texmerge.h
@@ -27,15 +27,13 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #ifndef _TEXMERGE_H
 #define _TEXMERGE_H
 
-#ifdef __cplusplus
+#include "fwd-segment.h"
 
 struct grs_bitmap;
 
 int texmerge_init();
-grs_bitmap &texmerge_get_cached_bitmap(unsigned tmap_bottom, unsigned tmap_top);
+grs_bitmap &texmerge_get_cached_bitmap(unsigned tmap_bottom, texture2_value tmap_top);
 void texmerge_close();
 void texmerge_flush();
-
-#endif
 
 #endif /* _TEXMERGE_H */

common/main/wall.h
@@ -222,8 +222,8 @@ struct wclip : public prohibit_void_ptr<wclip>
 	fix     play_time;
 	uint16_t num_frames;
 	union {
-		std::array<int16_t, MAX_CLIP_FRAMES> frames;
-		std::array<int16_t, MAX_CLIP_FRAMES_D1> d1_frames;
+		std::array<uint16_t, MAX_CLIP_FRAMES> frames;
+		std::array<uint16_t, MAX_CLIP_FRAMES_D1> d1_frames;
 	};
 	short   open_sound;
 	short   close_sound;

commit 3b5fa6f30b5785049b83272eac486cf01a88deac

Fix D2 emulation of D1 textures
Fixes: 6d3dce4e16137b01d1d9814c9ed76d2f82f16837 ("Use enum class for tmap_num2")

similar/main/gamemine.cpp
@@ -963,8 +963,8 @@ int load_mine_data_compiled(PHYSFS_File *LoadFile, const char *const Gamesave_cu
 				else {
 					// Read short Segments[segnum].sides[sidenum].tmap_num2;
 					const auto tmap_num2 = static_cast<texture2_value>(PHYSFSX_readShort(LoadFile));
-					uside.tmap_num2 = (Gamesave_current_version <= 1 && uside.tmap_num2 != texture2_value::None)
-						? build_texture2_value(convert_d1_tmap_num(get_texture_index(uside.tmap_num2)), get_texture_rotation_high(tmap_num2))
+					uside.tmap_num2 = (Gamesave_current_version <= 1 && tmap_num2 != texture2_value::None)
+						? build_texture2_value(convert_d1_tmap_num(get_texture_index(tmap_num2)), get_texture_rotation_high(tmap_num2))
 						: tmap_num2;
 				}
 #endif