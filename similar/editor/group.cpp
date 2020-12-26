dxx-rebirth/dxx-rebirth

commit 2665869c24855040837b1864daedd4cc13ab1793

Mark editor groups broken
Editor groups write `struct segment` to a file in raw form, but had no
code to enforce that this raw form remained stable over time.  Various
changes to `struct segment` have repeatedly changed its internal
structure.  Each change created an incompatible dialect of the editor
group file, and all the dialects share the same version number.

According to
```
git log -p -L'/struct segment {/,/};/:common/main/segment.h' HEAD --not 0.58.1-d1x 0.58.1-d2x --
```
`struct segment` changed layout in:

* d1c6b89f171482791284c2ac46799a1c18800996 ("Move dsx::segment -> dcx::segment")	[D1 only]
* 596ecbb38d964aa5cd9eb2bf0dbb981a8957b361 ("Rename segment::value to segment::station_idx")	[D1 only]
* 6f10a67c0952bd8fe9262d9ffccece8a58aaf868 ("Move segment::sides to end")
* c53b734abb375b475497ad0c22c641ae2e25ca9a ("Compute slide segments early")	[D2 only]
* 40e90fea22303ff96e626de6e020e7b6be4456a4 ("Move Light_subtracted[] to Segments[].light_subtracted")
* a65d774c83469218476ab2963bd85bd6fd030dc3 ("Improve packing of struct segment")
* c70c6c98b3ffdc58ee1c3f584a390a261b7c305f ("Remove obsolete segment::degenerated flag")

Mark editor groups as broken to avoid making the mess worse.  If anyone
cares about group support, it needs to be rewritten not to depend on the
internal layout of `struct segment`.

similar/editor/group.cpp
@@ -954,6 +954,7 @@ int RotateSegmentNew(vms_angvec *pbh)
 	return rval;
 }
 
+#if 0
 static array<d_fname, MAX_TEXTURES> current_tmap_list;
 
 // -----------------------------------------------------------------------------
@@ -1356,12 +1357,16 @@ static void checkforgrpext( char * f )
 		return;
 	}
 }
+#endif
 
 //short vertex_list[MAX_VERTICES];
 
 
 int SaveGroup()
 {
+	ui_messagebox(-2, -2, 1, "ERROR: Groups are broken.", "Ok");
+	return 0;
+#if 0
 	// Save group
 	int i;
 
@@ -1395,11 +1400,15 @@ int SaveGroup()
 	}
 	
 	return 1;
+#endif
 }
 
 
 int LoadGroup()
 {
+	ui_messagebox(-2, -2, 1, "ERROR: Groups are broken.", "Ok");
+	return 0;
+#if 0
 	int x;
 
 	if (num_groups == MAX_GROUPS)
@@ -1433,6 +1442,7 @@ int LoadGroup()
 	}	else
 
 	return 1;
+#endif
 }
 
 

commit e2565e6120a4ae2111d8f3ed655fdacfbbbf45d6

Move some group structures inside #if 0
Some group functionality was disabled in 2665869c2485, but the globals
to support the disabled code were not removed.  Move them into a
corresponding #if 0, since they have no value with the group code
disabled.

Fixes: 2665869c24855040837b1864daedd4cc13ab1793 ("Mark editor groups broken")

similar/editor/group.cpp
@@ -54,6 +54,7 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 static void validate_selected_segments(void);
 
+#if 0
 struct group_top_fileinfo {
 	int     fileinfo_version;
 	int     fileinfo_sizeof;
@@ -89,6 +90,7 @@ struct group_editor {
 	int     Groupsegp;
 	int     Groupside;
 } group_editor;
+#endif
 
 std::array<group, MAX_GROUPS+1> GroupList;
 std::array<segment *, MAX_GROUPS+1> Groupsegp;