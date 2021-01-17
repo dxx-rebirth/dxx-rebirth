dxx-rebirth/dxx-rebirth

commit 479884288c21841fc04036635ccac2678a47604c

Always delegate to window_create for send_creation_events

d2x-rebirth/main/movie.cpp
@@ -327,11 +327,10 @@ window_event_result movie::event_handler(const d_event &event)
 			// If PAUSE pressed, then pause movie
 			if ((key == KEY_PAUSE) || (key == KEY_COMMAND + KEY_P))
 			{
-				if (auto pause_window = std::make_unique<movie_pause_window>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT))
+				if (auto pause_window = window_create<movie_pause_window>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT))
 				{
+					(void)pause_window;
 					MVE_rmHoldMovie();
-					pause_window->send_creation_events();
-					pause_window.release();
 				}
 				return window_event_result::handled;
 			}
@@ -387,14 +386,7 @@ int RunMovie(const char *const filename, const char *const subtitles, const int 
 		return MOVIE_NOT_PLAYED;
 	}
 	const auto reshow = hide_menus();
-	auto wind = std::make_unique<movie>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);
-	if (!wind)
-	{
-		if (reshow)
-			show_menus();
-		return MOVIE_NOT_PLAYED;
-	}
-	wind->send_creation_events();
+	auto wind = window_create<movie>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);
 	init_subtitles(wind->SubtitleState, subtitles);
 
 
@@ -414,7 +406,7 @@ int RunMovie(const char *const filename, const char *const subtitles, const int 
 
 	if (MVE_rmPrepMovie(wind->pMovie, filehndl.get(), dx, dy, track)) {
 		Int3();
-		window_close(wind.get());
+		window_close(wind);
 		if (reshow)
 			show_menus();
 		return MOVIE_NOT_PLAYED;

common/include/window.h
@@ -38,10 +38,8 @@ private:
 	class window *prev;				// the previous window in the doubly linked list
 	class window *next;				// the next window in the doubly linked list
 	bool *w_exists;					// optional pointer to a tracking variable
-	
 public:
 	explicit window(grs_canvas &src, int x, int y, int w, int h);
-
 	virtual ~window();
 
 	virtual window_event_result event_handler(const d_event &) = 0;

common/ui/menubar.cpp
@@ -173,10 +173,7 @@ static void menu_other_show()
 	auto &menu = *Menu[CMENU];
 	if (!menu.wind)
 	{
-		menu.wind = new menu_window(grd_curscreen->sc_canvas, menu.x, menu.y, menu.w, menu.h, menu);
-		if (!menu.wind)
-			return;
-		menu.wind->send_creation_events();
+		menu.wind = window_create<menu_window>(grd_curscreen->sc_canvas, menu.x, menu.y, menu.w, menu.h, menu);
 	}
 	menu_show(menu);
 }
@@ -948,10 +945,7 @@ void menubar_show()
 	auto &menu = *Menu[0];
 	if (!menu.wind)
 	{
-		menu.wind = new menubar_window(grd_curscreen->sc_canvas, menu.x, menu.y, menu.w, menu.h, menu);
-		if (!menu.wind)
-			return;
-		menu.wind->send_creation_events();
+		menu.wind = window_create<menubar_window>(grd_curscreen->sc_canvas, menu.x, menu.y, menu.w, menu.h, menu);
 		menu.wind->set_modal(0);	// allow windows behind the menubar to accept events (e.g. the keypad dialogs)
 	}
 	menu_show(menu);

d2x-rebirth/main/escort.cpp
@@ -2010,7 +2010,7 @@ void do_escort_menu(void)
 	}
 
 	// Just make it the full screen size and let show_escort_menu figure it out
-	auto wind = std::make_unique<escort_menu>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);
+	auto wind = window_create<escort_menu>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);
 
 	auto &plrobj = get_local_plrobj();
 	//	This prevents the buddy from coming back if you've told him to scram.
@@ -2083,8 +2083,6 @@ void do_escort_menu(void)
 						"T.  %s Messages"
 						// -- "9.	Find the exit" CC_LSPACING_S "3\n"
 				, goal_txt, tstr);
-	wind->send_creation_events();
-	wind.release();
 }
 
 namespace {

similar/editor/info.cpp
@@ -397,10 +397,9 @@ window_event_result info_dialog_window::event_handler(const d_event &event)
 //	------------------------------------------------------------------------------------
 window *info_window_create(void)
 {
-	auto wind = std::make_unique<info_dialog_window>(*Canv_editor, PAD_X + 250, PAD_Y + 8, 180, 160);
+	auto wind = window_create<info_dialog_window>(*Canv_editor, PAD_X + 250, PAD_Y + 8, 180, 160);
 	wind->set_modal(0);
-	wind->send_creation_events();
-	return wind.release();
+	return wind;
 }
 
 }

similar/main/automap.cpp
@@ -1175,7 +1175,7 @@ void do_automap()
 	auto &Objects = LevelUniqueObjectState.Objects;
 	auto &vmobjptr = Objects.vmptr;
 	palette_array_t pal;
-	auto am = std::make_unique<automap>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);
+	auto am = window_create<automap>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);
 	const auto max_edges = LevelSharedSegmentState.Num_segments * 12;
 	am->max_edges = max_edges;
 	am->edges = std::make_unique<Edge_info[]>(max_edges);
@@ -1230,8 +1230,6 @@ void do_automap()
 	gr_palette_load( gr_palette );
 	Automap_active = 1;
 	multi_send_msgsend_state(msgsend_automap);
-	am->send_creation_events();
-	am.release();
 }
 
 namespace {

similar/main/console.cpp
@@ -465,9 +465,8 @@ void con_showup(control_info &Controls)
 {
 	game_flush_inputs(Controls);
 	con_state = CON_STATE_OPENING;
-	auto wind = std::make_unique<console_window>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);
-	wind->send_creation_events();
-	wind.release();
+	auto wind = window_create<console_window>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);
+	(void)wind;
 }
 
 }

similar/main/credits.cpp
@@ -257,7 +257,7 @@ window_event_result credits_window::event_handler(const d_event &event)
 static void credits_show_common(RAIIPHYSFS_File file, const int have_bin_file)
 {
 	palette_array_t backdrop_palette;
-	auto cr = std::make_unique<credits_window>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT, std::move(file), have_bin_file);
+	auto cr = window_create<credits_window>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT, std::move(file), have_bin_file);
 
 	set_screen_mode(SCREEN_MENU);
 #if defined(DXX_BUILD_DESCENT_II)
@@ -279,9 +279,7 @@ static void credits_show_common(RAIIPHYSFS_File file, const int have_bin_file)
 	gr_palette_load( gr_palette );
 
 	key_flush();
-	cr->send_creation_events();
 	event_process_all();
-	cr.release();
 }
 
 }

commit 3f413f15e59800a8be4c8c4257e547b0a17aaae4

Fix Descent 2 delayed crash when movies are enabled
Commit 479884288c21 unintentionally caused the movie window to linger in
the background after a movie was played, which causes a crash when the
window is used later, because part of its state has been cleared.
Use window::track to detect its termination in the usual way, and
eliminate the special logic that tried to track the window by deferring
its deletion.  This causes the window to close at the proper time,
thereby avoiding a later crash.

Fixes: 479884288c21841fc04036635ccac2678a47604c ("Always delegate to window_create for send_creation_events")

d2x-rebirth/main/movie.cpp
@@ -250,7 +250,7 @@ struct movie : window
 	MVE_StepStatus result = MVE_StepStatus::EndOfFile;
 	int frame_num = 0;
 	int paused = 0;
-	MVESTREAM_ptr_t pMovie;
+	const MVESTREAM_ptr_t pMovie;
 	d_subtitle_state SubtitleState;
 	movie(grs_canvas &src, int x, int y, int w, int h, MVESTREAM_ptr_t mvestream) :
 		window(src, x, y, w, h),
@@ -318,8 +318,7 @@ window_event_result movie::event_handler(const d_event &event)
 
 			// If ESCAPE pressed, then quit movie.
 			if (key == KEY_ESC) {
-				result = MVE_StepStatus::EndOfFile;
-				return window_event_result::handled;
+				return window_event_result::close;
 			}
 
 			// If PAUSE pressed, then pause movie
@@ -338,6 +337,8 @@ window_event_result movie::event_handler(const d_event &event)
 			if (!paused)
 			{
 				result = MVE_rmStepMovie(*pMovie.get());
+				if (result == MVE_StepStatus::EndOfFile)
+					return window_event_result::close;
 				if (result != MVE_StepStatus::Continue)
 				{
 					return window_event_result::handled;
@@ -353,12 +354,7 @@ window_event_result movie::event_handler(const d_event &event)
 			break;
 
 		case EVENT_WINDOW_CLOSE:
-			if (Quitting)
-			{
-				result = MVE_StepStatus::EndOfFile;
-			}
-			return window_event_result::handled;
-			
+			break;
 		default:
 			break;
 	}
@@ -388,6 +384,8 @@ movie_play_status RunMovie(const char *const filename, const char *const subtitl
 	}
 	const auto reshow = hide_menus();
 	auto wind = window_create<movie>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT, std::move(mvestream));
+	bool exists = true;
+	wind->track(&exists);
 	init_subtitles(wind->SubtitleState, subtitles);
 
 #if DXX_USE_OGL
@@ -399,11 +397,9 @@ movie_play_status RunMovie(const char *const filename, const char *const subtitl
 	gr_set_mode(hires_flag ? screen_mode{640, 480} : screen_mode{320, 200});
 #endif
 
-	do {
+	while (exists)
 		event_process();
-	} while(!(wind->result == MVE_StepStatus::EndOfFile));
-
-	wind->pMovie.reset();
+	wind = nullptr;
 
 	filehndl.reset();                           // Close Movie File
 	if (reshow)