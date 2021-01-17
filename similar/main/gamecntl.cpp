dxx-rebirth/dxx-rebirth

commit 859b399d204e14a9f90bc001b46eb340b7b592bf

Use mask for Secondary_last_was_super

similar/main/gamecntl.cpp
@@ -201,8 +201,6 @@ namespace dsx {
 //returns which bomb will be dropped next time the bomb key is pressed
 int which_bomb()
 {
-	int bomb;
-
 	//use the last one selected, unless there aren't any, in which case use
 	//the other if there are any
 
@@ -213,18 +211,15 @@ int which_bomb()
 
 	auto &player_info = get_local_plrobj().ctype.player_info;
 	auto &Secondary_last_was_super = player_info.Secondary_last_was_super;
-	bomb = Secondary_last_was_super[PROXIMITY_INDEX]?SMART_MINE_INDEX:PROXIMITY_INDEX;
+	const auto mask = 1 << PROXIMITY_INDEX;
+	const auto bomb = (Secondary_last_was_super & mask) ? SMART_MINE_INDEX : PROXIMITY_INDEX;
 
 	auto &secondary_ammo = player_info.secondary_ammo;
 	if (secondary_ammo[bomb] == 0 &&
 		secondary_ammo[SMART_MINE_INDEX + PROXIMITY_INDEX - bomb] != 0)
 	{
-		bomb = SMART_MINE_INDEX+PROXIMITY_INDEX-bomb;
-		Secondary_last_was_super[bomb%SUPER_WEAPON] = (bomb == SMART_MINE_INDEX);
+		Secondary_last_was_super ^= mask;
 	}
-
-
-
 	return bomb;
 }
 #endif
@@ -290,8 +285,6 @@ static void do_weapon_n_item_stuff()
 	if (Controls.state.toggle_bomb > 0)
 	{
 		auto &Secondary_last_was_super = player_info.Secondary_last_was_super;
-		int bomb = Secondary_last_was_super[PROXIMITY_INDEX]?PROXIMITY_INDEX:SMART_MINE_INDEX;
-	
 		auto &secondary_ammo = player_info.secondary_ammo;
 		int sound;
 		if (!secondary_ammo[PROXIMITY_INDEX] && !secondary_ammo[SMART_MINE_INDEX])
@@ -301,14 +294,17 @@ static void do_weapon_n_item_stuff()
 		}
 		else
 		{	
+			const auto mask = (1 << PROXIMITY_INDEX);
+			const char *desc;
+			const auto bomb = (Secondary_last_was_super & mask) ? (desc = "Proximity bombs", PROXIMITY_INDEX) : (desc = "Smart mines", SMART_MINE_INDEX);
 			if (secondary_ammo[bomb] == 0)
 			{
-				HUD_init_message(HM_DEFAULT, "No %s available!",(bomb==SMART_MINE_INDEX)?"Smart mines":"Proximity bombs");
+				HUD_init_message(HM_DEFAULT, "No %s available!", desc);
 				sound = SOUND_BAD_SELECTION;
 			}
 			else
 			{
-				Secondary_last_was_super[PROXIMITY_INDEX]=!Secondary_last_was_super[PROXIMITY_INDEX];
+				Secondary_last_was_super ^= mask;
 				sound = SOUND_GOOD_SELECTION_SECONDARY;
 			}
 		}

common/main/object.h
@@ -124,7 +124,7 @@ struct player_info
 	array<uint8_t, MAX_SECONDARY_WEAPONS>  secondary_ammo; // How much ammo of each type.
 #if defined(DXX_BUILD_DESCENT_II)
 	uint8_t Primary_last_was_super;
-	array<uint8_t, SUPER_WEAPON> Secondary_last_was_super;
+	uint8_t Secondary_last_was_super;
 #endif
 	int16_t net_killed_total;		// Number of times killed total
 	int16_t net_kills_total;		// Number of net kills total

similar/main/gauges.cpp
@@ -1146,8 +1146,7 @@ static rgb_t hud_get_secondary_weapon_fontcolor(const player_info &player_info, 
 #if defined(DXX_BUILD_DESCENT_II)
 			const auto is_super = (consider_weapon >= 5);
 			const int base_weapon = is_super ? consider_weapon - 5 : consider_weapon;
-			auto &Secondary_last_was_super = player_info.Secondary_last_was_super;
-			if (Secondary_last_was_super[base_weapon])
+			if (player_info.Secondary_last_was_super & (1 << base_weapon))
 			{
 				if (is_super)
 					return hud_rgb_green;

similar/main/state.cpp
@@ -1247,7 +1247,11 @@ int state_save_all_sub(const char *filename, const char *desc)
 		}
 		PHYSFS_write(fp, &last_was_super, MAX_PRIMARY_WEAPONS, 1);
 		auto &Secondary_last_was_super = player_info.Secondary_last_was_super;
-		std::copy(Secondary_last_was_super.begin(), Secondary_last_was_super.end(), last_was_super.begin());
+		for (uint_fast32_t j = secondary_weapon_index_t::CONCUSSION_INDEX; j != secondary_weapon_index_t::SMISSILE1_INDEX; ++j)
+		{
+			if (Secondary_last_was_super & (1 << j))
+				last_was_super[j] = 1;
+		}
 		PHYSFS_write(fp, &last_was_super, MAX_SECONDARY_WEAPONS, 1);
 	}
 
@@ -1866,7 +1870,12 @@ int state_restore_all_sub(const char *filename, const secret_restore secret)
 		}
 		PHYSFS_read(fp, &last_was_super, MAX_SECONDARY_WEAPONS, 1);
 		auto &Secondary_last_was_super = player_info.Secondary_last_was_super;
-		std::copy(last_was_super.begin(), std::next(last_was_super.begin(), Secondary_last_was_super.size()), Secondary_last_was_super.begin());
+		Secondary_last_was_super = 0;
+		for (uint_fast32_t j = secondary_weapon_index_t::CONCUSSION_INDEX; j != secondary_weapon_index_t::SMISSILE1_INDEX; ++j)
+		{
+			if (last_was_super[j])
+				Secondary_last_was_super |= 1 << j;
+		}
 	}
 
 	if (version >= 12) {

similar/main/weapon.cpp
@@ -513,8 +513,7 @@ void set_secondary_weapon_to_concussion(player_info &player_info)
 	player_info.Secondary_weapon = static_cast<secondary_weapon_index_t>(weapon_num);
 #if defined(DXX_BUILD_DESCENT_II)
 	//save flag for whether was super version
-	auto &Secondary_last_was_super = player_info.Secondary_last_was_super;
-	Secondary_last_was_super[weapon_num % SUPER_WEAPON] = (weapon_num >= SUPER_WEAPON);
+	set_weapon_last_was_super(player_info.Secondary_last_was_super, weapon_num);
 #endif
 }
 
@@ -545,8 +544,7 @@ void select_secondary_weapon(player_info &player_info, const char *const weapon_
 		Secondary_weapon = static_cast<secondary_weapon_index_t>(weapon_num);
 #if defined(DXX_BUILD_DESCENT_II)
 		//save flag for whether was super version
-		auto &Secondary_last_was_super = player_info.Secondary_last_was_super;
-		Secondary_last_was_super[weapon_num % SUPER_WEAPON] = (weapon_num >= SUPER_WEAPON);
+		set_weapon_last_was_super(player_info.Secondary_last_was_super, weapon_num);
 #endif
 	}
 	if (weapon_name)
@@ -674,8 +672,7 @@ void do_secondary_weapon_select(player_info &player_info, secondary_weapon_index
 	has_weapon_result weapon_status;
 
 	const auto current = player_info.Secondary_weapon.get_active();
-	auto &Secondary_last_was_super = player_info.Secondary_last_was_super;
-	const auto last_was_super = Secondary_last_was_super[weapon_num];
+	const auto last_was_super = player_info.Secondary_last_was_super & (1 << weapon_num);
 	const auto has_flag = weapon_status.has_weapon_flag | weapon_status.has_ammo_flag;
 
 	if (current == weapon_num || current == weapon_num+SUPER_WEAPON) {
@@ -852,9 +849,9 @@ int pick_up_secondary(player_info &player_info, int weapon_index,int count)
 			if (weapon_index_is_player_bomb(weapon_index) &&
 				!weapon_index_is_player_bomb(player_info.Secondary_weapon))
 			{
-				auto &last = player_info.Secondary_last_was_super[PROXIMITY_INDEX];
-				if (weapon_order < SOrderList(last ? SMART_MINE_INDEX : PROXIMITY_INDEX))
-					last = (weapon_index == SMART_MINE_INDEX);
+				const auto mask = 1 << PROXIMITY_INDEX;
+				if (weapon_order < SOrderList((player_info.Secondary_last_was_super & mask) ? SMART_MINE_INDEX : PROXIMITY_INDEX))
+					set_weapon_last_was_super(player_info.Secondary_last_was_super, mask, weapon_index == SMART_MINE_INDEX);
 			}
 #endif
 	}

commit c7680621a8e2edd0323d7cce82f8bbd4755e9ade

Switch mine type if only alternate mine is available
v0.58.1 did this, but the functionality was accidentally removed in
859b399d204e.  Restore it.

Fixes: 859b399d204e14a9f90bc001b46eb340b7b592bf ("Use mask for Secondary_last_was_super")

similar/main/gamecntl.cpp
@@ -262,11 +262,19 @@ int which_bomb()
 	const auto bomb = (Secondary_last_was_super & mask) ? SMART_MINE_INDEX : PROXIMITY_INDEX;
 
 	auto &secondary_ammo = player_info.secondary_ammo;
-	if (secondary_ammo[bomb] == 0 &&
-		secondary_ammo[SMART_MINE_INDEX + PROXIMITY_INDEX - bomb] != 0)
+	if (secondary_ammo[bomb])
+		/* Player has the requested bomb type available.  Use it. */
+		return bomb;
+	const auto alt_bomb = SMART_MINE_INDEX + PROXIMITY_INDEX - bomb;
+	if (secondary_ammo[alt_bomb])
 	{
+		/* Player has the alternate bomb type, but not the requested
+		 * bomb type.  Switch.
+		 */
 		Secondary_last_was_super ^= mask;
+		return alt_bomb;
 	}
+	/* Player has no bombs of either type. */
 	return bomb;
 }
 #endif