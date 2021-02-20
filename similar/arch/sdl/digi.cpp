dxx-rebirth/dxx-rebirth

commit fbd05a15923860f8090aeeb9c2e1fe7b15f10c04

optimize include files (include what you use)

similar/arch/sdl/digi.cpp
@@ -11,13 +11,11 @@
 
 #include "dxxerror.h"
 #include "vecmat.h"
-#include "piggy.h"
 #include "config.h"
 #include "digi.h"
 #include "sounds.h"
 #include "console.h"
 #include "rbaudio.h"
-#include "jukebox.h"
 #include "args.h"
 #include <stdlib.h>
 #include <stdio.h>

commit 9aab870cad4d84a3d14eaf43ef59f73d43936bea

Fix build break in digi.cpp
fbd05a159238 dropped an include of "jukebox.h" as unnecessary.  jukebox
was almost unnecessary, but jukebox includes physfsx, and physfsx
includes stdexcept.  stdexcept is necessary.  Include stdexcept
directly.

Fixes: fbd05a15923860f8090aeeb9c2e1fe7b15f10c04 ("optimize include files (include what you use)")

similar/arch/sdl/digi.cpp
@@ -9,6 +9,7 @@
  * Library-independent stub for dynamic selection of sound system
  */
 
+#include <stdexcept>
 #include "dxxerror.h"
 #include "vecmat.h"
 #include "config.h"