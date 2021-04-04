dxx-rebirth/dxx-rebirth

commit 6bc0e822d2cad952410696db37435ab1b0a4b758

Handle HUD overlays in separate screen rects for stereo renderings.

similar/arch/ogl/ogl.cpp
@@ -1303,11 +1303,11 @@ void ogl_stereo_frame(int xeye, int xoff)
 		else {
 			glGetIntegerv(GL_VIEWPORT, ogl_stereo_viewport.data());
 			// center unsqueezed side-by-side format
-			if (VR_half_width && VR_half_height)
+			if (VR_stereo == STEREO_SIDE_BY_SIDE2)
 				ogl_stereo_viewport[1] -= ogl_stereo_viewport[3]/2;		// y = h/4
 			glViewport(ogl_stereo_viewport[0], ogl_stereo_viewport[1], ogl_stereo_viewport[2], ogl_stereo_viewport[3]);
 		}
-
+		// rightward image shift adjustment for left eye offset
 		glMatrixMode(GL_PROJECTION);
 		glGetFloatv(GL_PROJECTION_MATRIX, ogl_stereo_transform.data());
 		ogl_stereo_transform[8] -= dxoff;		// xoff < 0
@@ -1320,18 +1320,22 @@ void ogl_stereo_frame(int xeye, int xoff)
 			glDrawBuffer(GL_BACK_RIGHT);
 		else {
 			glGetIntegerv(GL_VIEWPORT, ogl_stereo_viewport.data());
-			// half-width viewports for side-by-side format
-			if (VR_half_width)
-				ogl_stereo_viewport[0] += ogl_stereo_viewport[2];		// x = w/2
-			// half-height viewports for above/below format
-			else if (VR_half_height)
-				ogl_stereo_viewport[1] -= ogl_stereo_viewport[3];		// y = h/2
+			switch (VR_stereo) {
 			// center unsqueezed side-by-side format
-			if (VR_half_width && VR_half_height)
+			case STEREO_SIDE_BY_SIDE2:
 				ogl_stereo_viewport[1] -= ogl_stereo_viewport[3]/2;		// y = h/4
+			// half-width viewports for side-by-side format
+			case STEREO_SIDE_BY_SIDE:
+				ogl_stereo_viewport[0] += ogl_stereo_viewport[2];		// x = w/2
+				break;
+			// half-height viewports for above/below format
+			case STEREO_ABOVE_BELOW:
+				ogl_stereo_viewport[1] -= ogl_stereo_viewport[3];		// y = h/2
+				break;
+			}
 			glViewport(ogl_stereo_viewport[0], ogl_stereo_viewport[1], ogl_stereo_viewport[2], ogl_stereo_viewport[3]);
 		}
-
+		// leftward image shift adjustment for right eye offset
 		glMatrixMode(GL_PROJECTION);
 		glGetFloatv(GL_PROJECTION_MATRIX, ogl_stereo_transform.data());
 		ogl_stereo_transform[8] += dxoff;		// xoff < 0

common/main/game.h
@@ -204,6 +204,8 @@ extern bool VR_half_width;
 extern bool VR_half_height;
 extern fix  VR_eye_width;
 extern int  VR_eye_offset;
+extern grs_canvas VR_hud_left;
+extern grs_canvas VR_hud_right;
 }
 
 #define MAX_PALETTE_ADD 30

similar/main/game.cpp
@@ -191,13 +191,15 @@ bool VR_half_width = false;
 bool VR_half_height = false;
 fix  VR_eye_width = F1_0;
 int  VR_eye_offset = 0;
+grs_canvas VR_hud_left;
+grs_canvas VR_hud_right;
 }
 
 namespace dsx {
 
 void init_stereo()
 {
-#ifdef DXX_USE_OGL
+#if DXX_USE_OGL
 	// init stereo options
 	if (CGameArg.OglStereo || CGameArg.OglStereoView) {
 		if (!VR_stereo && !VR_eye_offset)
@@ -328,6 +330,25 @@ void game_init_render_sub_buffers( int x, int y, int w, int h )
 {
 	gr_clear_canvas(*grd_curcanv, 0);
 	gr_init_sub_canvas(Screen_3d_window, grd_curscreen->sc_canvas, x, y, w, h);
+
+	switch (VR_stereo) {
+		case STEREO_NONE:
+			gr_init_sub_canvas(VR_hud_left,  grd_curscreen->sc_canvas, x, y, w, h);
+			gr_init_sub_canvas(VR_hud_right, grd_curscreen->sc_canvas, x, y, w, h);
+			break;
+		case STEREO_ABOVE_BELOW:
+			gr_init_sub_canvas(VR_hud_left,  grd_curscreen->sc_canvas, x, y, w, h);
+			gr_init_sub_canvas(VR_hud_right, grd_curscreen->sc_canvas, x, y+h, w, h);
+			break;
+		case STEREO_SIDE_BY_SIDE:
+			gr_init_sub_canvas(VR_hud_left,  grd_curscreen->sc_canvas, x, y, w, h);
+			gr_init_sub_canvas(VR_hud_right, grd_curscreen->sc_canvas, x+w, y, w, h);
+			break;
+		case STEREO_SIDE_BY_SIDE2:
+			gr_init_sub_canvas(VR_hud_left,  grd_curscreen->sc_canvas, x, y+h/2, w, h);
+			gr_init_sub_canvas(VR_hud_right, grd_curscreen->sc_canvas, x+w, y+h/2, w, h);
+			break;
+	}
 }
 
 }

similar/main/gamerend.cpp
@@ -778,7 +778,7 @@ int BigWindowSwitch=0;
 //render a frame for the game
 void game_render_frame_mono(const control_info &Controls)
 {
-	int no_draw_hud = VR_stereo;
+	int no_draw_hud = 0; //VR_stereo;
 
 	gr_set_current_canvas(Screen_3d_window);
 #if defined(DXX_BUILD_DESCENT_II)
@@ -867,8 +867,17 @@ void game_render_frame_mono(const control_info &Controls)
 		Game_mode = GM_NORMAL;
 
 	gr_set_current_canvas(Screen_3d_window);
-	if (!no_draw_hud)
+	if (!no_draw_hud) {
+		if (VR_stereo) {
+//			gr_set_current_canvas(VR_hud_left);
+			game_draw_hud_stuff(VR_hud_left, Controls);
+//			gr_set_current_canvas(VR_hud_right);
+			game_draw_hud_stuff(VR_hud_right, Controls);
+//			gr_set_default_canvas();
+		}
+		else
 		game_draw_hud_stuff(*grd_curcanv, Controls);
+	}
 
 #if defined(DXX_BUILD_DESCENT_II)
 	gr_set_default_canvas();

commit 8cc8e80eaec20c76b99333c7637adc683f24e860

Fix VR build failure due to implicit fallthrough
The fallthrough appears to be intentional.  Add an annotation to allow
it.

Fixes: 6bc0e822d2cad952410696db37435ab1b0a4b758 ("Handle HUD overlays in separate screen rects for stereo renderings.")

similar/arch/ogl/ogl.cpp
@@ -1324,6 +1324,7 @@ void ogl_stereo_frame(int xeye, int xoff)
 			// center unsqueezed side-by-side format
 			case STEREO_SIDE_BY_SIDE2:
 				ogl_stereo_viewport[1] -= ogl_stereo_viewport[3]/2;		// y = h/4
+				DXX_BOOST_FALLTHROUGH;
 			// half-width viewports for side-by-side format
 			case STEREO_SIDE_BY_SIDE:
 				ogl_stereo_viewport[0] += ogl_stereo_viewport[2];		// x = w/2