dxx-rebirth/dxx-rebirth

commit 61f186bc1863a887d0257bb7bb23e62035f2d3ce

Use enum class for texture1_value

similar/main/fvi.cpp
@@ -1206,7 +1206,11 @@ int check_trans_wall(const vms_vector &pnt,const vcsegptridx_t seg,int sidenum,i
 	const auto tmap_num = side.tmap_num;
 	const grs_bitmap &rbm = (side.tmap_num2 != texture2_value::None)
 		? texmerge_get_cached_bitmap(tmap_num, side.tmap_num2)
-		: GameBitmaps[Textures[PIGGY_PAGE_IN(Textures[tmap_num]), tmap_num].index];
+		: ( [tmap_num] {
+			auto &texture1 = Textures[get_texture_index(tmap_num)];
+			PIGGY_PAGE_IN(texture1);
+			return GameBitmaps[texture1.index];
+		} () );
 	const auto bm = rle_expand_texture(rbm);
 
 	bmx = static_cast<unsigned>(f2i(u*bm->bm_w)) % bm->bm_w;

common/include/editor/editor.h
@@ -248,7 +248,7 @@ int med_rotate_segment(vmsegptridx_t seg, const vms_matrix &rotmat);
 //    Creates wall at sp->sides[side], making it part of segment sp
 //    Removable walls must be placed between two connected segments.  You should add the removable
 //    wall on both sides.  In fact, you really must.
-void create_removable_wall(fvcvertptr &vcvertptr, vmsegptridx_t sp, unsigned side, unsigned tmap_num);
+void create_removable_wall(fvcvertptr &vcvertptr, vmsegptridx_t sp, unsigned side, texture1_value tmap_num);
 }
 #endif
 
@@ -371,7 +371,7 @@ int med_form_joint(vmsegptridx_t seg1, int side1, vmsegptridx_t seg2, int side2)
 }
 
 // The current texture... use by saying something=bm_lock_bitmap(CurrentTexture)
-extern int CurrentTexture;
+extern texture_index CurrentTexture;
 
 namespace dsx {
 void med_propagate_tmaps_to_segments(vcsegptridx_t base_seg,vmsegptridx_t con_seg, int uv_only_flag);

common/include/editor/texpage.h
@@ -38,7 +38,7 @@ extern int TextureMetals;
 void init_replacements(void);
 void do_replacements(void);
 void do_replacements_all(void);
-int texpage_grab_current(int n);
+int texpage_grab_current(texture1_value n);
 int texpage_goto_first();
 void texpage_init( UI_DIALOG * dlg );
 void texpage_close();

common/main/fwd-segment.h
@@ -75,6 +75,8 @@ enum sidenum_t : uint8_t
 	WFRONT = 5
 };
 
+using texture_index = uint16_t;
+enum class texture1_value : uint16_t;
 enum class texture2_value : uint16_t;
 enum class texture2_rotation_low : uint8_t;
 enum class texture2_rotation_high : uint16_t;

common/main/newdemo.h
@@ -109,7 +109,7 @@ extern void newdemo_record_letterbox(void);
 extern void newdemo_record_rearview(void);
 extern void newdemo_record_restore_cockpit(void);
 #ifdef dsx
-void newdemo_record_wall_set_tmap_num1(vcsegidx_t seg, unsigned side, vcsegidx_t cseg, unsigned cside, int16_t tmap);
+void newdemo_record_wall_set_tmap_num1(vcsegidx_t seg, unsigned side, vcsegidx_t cseg, unsigned cside, texture1_value tmap);
 void newdemo_record_wall_set_tmap_num2(vcsegidx_t seg, unsigned side, vcsegidx_t cseg, unsigned cside, texture2_value tmap);
 #endif
 extern void newdemo_record_multi_cloak(int pnum);

common/main/segment.h
@@ -148,6 +148,11 @@ struct shared_side
 	std::array<vms_vector, 2> normals;  // 2 normals, if quadrilateral, both the same.
 };
 
+enum class texture1_value : uint16_t
+{
+	None,
+};
+
 /* texture2_value uses the low 14 bits for the array index and the high
  * 2 bits for texture2_rotation_high
  */
@@ -185,9 +190,14 @@ static constexpr texture2_rotation_high &operator++(texture2_rotation_high &t)
 	return (t = static_cast<texture2_rotation_high>(static_cast<uint32_t>(t) + (1u << TEXTURE2_ROTATION_SHIFT)));
 }
 
-static constexpr uint16_t get_texture_index(const texture2_value t)
+static constexpr texture_index get_texture_index(const texture1_value t)
 {
-	return static_cast<uint16_t>(t) & TEXTURE2_ROTATION_INDEX_MASK;
+	return static_cast<texture_index>(t);
+}
+
+static constexpr texture_index get_texture_index(const texture2_value t)
+{
+	return static_cast<texture_index>(static_cast<uint16_t>(t) & TEXTURE2_ROTATION_INDEX_MASK);
 }
 
 static constexpr texture2_rotation_high get_texture_rotation_high(const texture2_value t)
@@ -205,9 +215,14 @@ static constexpr texture2_rotation_low get_texture_rotation_low(const texture2_v
 	return get_texture_rotation_low(get_texture_rotation_high(t));
 }
 
-static constexpr texture2_value build_texture2_value(const unsigned t, const texture2_rotation_high rotation)
+static constexpr texture1_value build_texture1_value(const texture_index t)
 {
-	return static_cast<texture2_value>(t | static_cast<unsigned>(rotation));
+	return static_cast<texture1_value>(t);
+}
+
+static constexpr texture2_value build_texture2_value(const texture_index t, const texture2_rotation_high rotation)
+{
+	return static_cast<texture2_value>(static_cast<uint16_t>(t) | static_cast<uint16_t>(rotation));
 }
 
 #undef TEXTURE2_ROTATION_SHIFT
@@ -215,7 +230,7 @@ static constexpr texture2_value build_texture2_value(const unsigned t, const tex
 
 struct unique_side
 {
-	int16_t tmap_num;
+	texture1_value tmap_num;
 	texture2_value tmap_num2;
 	std::array<uvl, 4>     uvls;
 };

common/main/texmerge.h
@@ -32,7 +32,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 struct grs_bitmap;
 
 int texmerge_init();
-grs_bitmap &texmerge_get_cached_bitmap(unsigned tmap_bottom, texture2_value tmap_top);
+grs_bitmap &texmerge_get_cached_bitmap(texture1_value tmap_bottom, texture2_value tmap_top);
 void texmerge_close();
 void texmerge_flush();
 

similar/arch/ogl/ogl.cpp
@@ -455,13 +455,15 @@ void ogl_cache_level_textures(void)
 			{
 				const auto tmap1 = side.tmap_num;
 				const auto tmap2 = side.tmap_num2;
-				if (tmap1<0 || tmap1>=NumTextures){
+				const auto tmap1idx = get_texture_index(tmap1);
+				if (tmap1idx >= NumTextures){
 					glmprintf((CON_DEBUG, "ogl_cache_level_textures %p %p %i %i", seg.get_unchecked_pointer(), &side, tmap1, NumTextures));
 					//				tmap1=0;
 					continue;
 				}
-				PIGGY_PAGE_IN(Textures[tmap1]);
-				grs_bitmap *bm = &GameBitmaps[Textures[tmap1].index];
+				auto &texture1 = Textures[tmap1idx];
+				PIGGY_PAGE_IN(texture1);
+				grs_bitmap *bm = &GameBitmaps[texture1.index];
 				if (tmap2 != texture2_value::None)
 				{
 					const auto texture2 = Textures[get_texture_index(tmap2)];

commit 58a1d86c05da1e85bc63bb3d42e70c37bc3dd10f

Fix return type of check_trans_wall lambda (#588)
The deduced return type is `grs_bitmap`, which is inefficient, but not
wrong on its own.  However, `rle_expand_texture` uses the address of its
argument as a long-term cache key, so it must never be called with the
address of a stack-local variable.  When the return type is
`grs_bitmap`, the argument to `rle_expand_texture` is a reference to a
stack-local variable.  Fix this by setting the return type to
`const grs_bitmap &`, so that the argument to `rle_expand_texture` is a
reference to an element in the global GameBitmaps array.

AlumiuN proposed an initial fix, but based on analysis of why that fix
worked, I elected to use a different, smaller, fix instead.

Reported-by: CHILLYBUS <https://github.com/dxx-rebirth/dxx-rebirth/issues/588>
Reported-by: KynikossDragonn <https://github.com/dxx-rebirth/dxx-rebirth/issues/588#issuecomment-825978696>
Analyzed-by: AlumiuN <https://github.com/dxx-rebirth/dxx-rebirth/issues/588#issuecomment-826009993>
Analyzed-by: 4C1T <https://github.com/dxx-rebirth/dxx-rebirth/issues/588#issuecomment-826016402>
Proposed-fix-by: AlumiuN <https://github.com/AlumiuN/dxx-rebirth/commit/c4d1f419465d8a64c8015cd5f07ec262ee9443d2>
Fixes: 61f186bc1863a887d0257bb7bb23e62035f2d3ce ("Use enum class for texture1_value")

similar/main/fvi.cpp
@@ -1209,7 +1209,7 @@ int check_trans_wall(const vms_vector &pnt,const vcsegptridx_t seg,int sidenum,i
 	const auto tmap_num = side.tmap_num;
 	const grs_bitmap &rbm = (side.tmap_num2 != texture2_value::None)
 		? texmerge_get_cached_bitmap(tmap_num, side.tmap_num2)
-		: ( [tmap_num] {
+		: ( [tmap_num]() -> const grs_bitmap & {
 			auto &texture1 = Textures[get_texture_index(tmap_num)];
 			PIGGY_PAGE_IN(texture1);
 			return GameBitmaps[texture1.index];