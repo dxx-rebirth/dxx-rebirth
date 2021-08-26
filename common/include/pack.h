506ab6e5de8c382c95eb1461581e2704f4c77beb
Kp <kp@valhallalegends.com>
2021-08-26 03:13:45 +0000

    Make prohibit_void_ptr methods constexpr

--- common/include/pack.h
+++ common/include/pack.h
@@ -24,11 +24,11 @@ public:
 		bool operator>=(U &&) const = delete;
 	template <typename U>
 		bool operator!=(U &&rhs) const { return !operator==(static_cast<U &&>(rhs)); }
-	exact_type(T *t) : p(t) {}
+	constexpr exact_type(T *t) : p(t) {}
 	// Conversion to the exact type is permitted
-	operator const T *() const { return p; }
-	operator typename std::remove_const<T>::type *() const { return p; }
-	bool operator==(const T *rhs) const { return p == rhs; }
+	constexpr operator const T *() const { return p; }
+	constexpr operator typename std::remove_const<T>::type *() const { return p; }
+	constexpr bool operator==(const T *rhs) const { return p == rhs; }
 };
 
 template <typename T>
@@ -36,6 +36,6 @@ class prohibit_void_ptr
 {
 public:
 	// Return a proxy when the address is taken
-	exact_type<T> operator&() { return static_cast<T*>(this); }
-	exact_type<T const> operator&() const { return static_cast<T const*>(this); }
+	constexpr exact_type<T> operator&() { return static_cast<T*>(this); }
+	constexpr exact_type<T const> operator&() const { return static_cast<T const*>(this); }
 };
