dxx-rebirth/dxx-rebirth

commit 2b718da343480629ca15f2fec285fd1964e689a4

Use enum class for Game_mode, Newdemo_game_mode

common/main/game.h
@@ -38,6 +38,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "d_array.h"
 #include "kconfig.h"
 #include "wall.h"
+#include "d_underlying_value.h"
 
 #define DESIGNATED_GAME_FPS 30 // assuming the original intended Framerate was 30
 #define DESIGNATED_GAME_FRAMETIME (F1_0/DESIGNATED_GAME_FPS) 
@@ -58,6 +59,7 @@ extern fix64 GameTime64;            // time in game (sum of FrameTime)
 extern int d_tick_count; // increments according to DESIGNATED_GAME_FRAMETIME
 extern int d_tick_step;  // true once in interval of DESIGNATED_GAME_FRAMETIME
 enum class gauge_inset_window_view : unsigned;
+
 }
 
 #if defined(DXX_BUILD_DESCENT_II)
@@ -70,31 +72,60 @@ extern enumerated_array<game_marker_index, 2, gauge_inset_window_view> Marker_vi
 }
 #endif
 
-// The following bits define the game modes.
-//#define GM_EDITOR       1       // You came into the game from the editor. Now obsolete - FYI only
-// #define GM_SERIAL       2       // You are in serial mode // OBSOLETE
-#define GM_NETWORK      4       // You are in network mode
-#define GM_MULTI_ROBOTS 8       // You are in a multiplayer mode with robots.
-#define GM_MULTI_COOP   16      // You are in a multiplayer mode and can't hurt other players.
-// #define GM_MODEM        32      // You are in a modem (serial) game // OBSOLETE
-#define GM_UNKNOWN      64      // You are not in any mode, kind of dangerous...
-#define GM_TEAM         256     // Team mode for network play
-#if defined(DXX_BUILD_DESCENT_I)
-#define GM_BOUNTY       512     // New bounty mode by Matt1360
-#elif defined(DXX_BUILD_DESCENT_II)
-#define GM_CAPTURE      512     // Capture the flag mode for D2
-#define GM_HOARD        1024    // New hoard mode for D2 Christmas
-#define GM_BOUNTY	2048	/* New bounty mode by Matt1360 */
-#endif
-#define GM_NORMAL       0       // You are in normal play mode, no multiplayer stuff
-#define GM_MULTI        GM_NETWORK     // You are in some type of multiplayer game	(GM_NETWORK /* | GM_SERIAL | GM_MODEM */)
-
-
 #define NDL 5       // Number of difficulty levels.
 
 namespace dcx {
-extern int Game_mode;
+
+enum class game_mode_flag : uint16_t
+{
+	normal = 0,
+	network			= 1u << 2,
+	multi_robots	= 1u << 3,
+	multi_coop		= 1u << 4,
+	unknown			= 1u << 6,
+	team			= 1u << 8,
+	bounty			= 1u << 9,
+#if defined(DXX_BUILD_DESCENT_II)
+	capture			= 1u << 10,
+	hoard			= 1u << 11,
+#endif
+};
+
+enum class game_mode_flags : uint16_t
+{
+	normal = 0,
+	anarchy_no_robots = static_cast<uint16_t>(game_mode_flag::network),
+	team_anarchy_no_robots = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::team),
+	anarchy_with_robots = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::multi_robots),
+	cooperative = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::multi_robots) | static_cast<uint16_t>(game_mode_flag::multi_coop),
+	bounty = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::bounty),
+#if defined(DXX_BUILD_DESCENT_II)
+	capture_flag = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::team) | static_cast<uint16_t>(game_mode_flag::capture),
+	hoard = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::hoard),
+	team_hoard = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::team) | static_cast<uint16_t>(game_mode_flag::hoard),
+#endif
+};
+
+// The following bits define the game modes.
+#define GM_NETWORK		game_mode_flag::network       // You are in network mode
+#define GM_MULTI_ROBOTS	game_mode_flag::multi_robots       // You are in a multiplayer mode with robots.
+#define GM_MULTI_COOP	game_mode_flag::multi_coop      // You are in a multiplayer mode and can't hurt other players.
+#define GM_UNKNOWN		game_mode_flag::unknown      // You are not in any mode, kind of dangerous...
+#define GM_TEAM			game_mode_flag::team     // Team mode for network play
+#define GM_BOUNTY		game_mode_flag::bounty     // New bounty mode by Matt1360
+#define GM_CAPTURE		game_mode_flag::capture     // Capture the flag mode for D2
+#define GM_HOARD		game_mode_flag::hoard    // New hoard mode for D2 Christmas
+#define GM_NORMAL		game_mode_flags::normal       // You are in normal play mode, no multiplayer stuff
+#define GM_MULTI        GM_NETWORK     // You are in some type of multiplayer game	(GM_NETWORK /* | GM_SERIAL | GM_MODEM */)
+extern game_mode_flags Game_mode;
+extern game_mode_flags Newdemo_game_mode;
 extern screen_mode Game_screen_mode;
+
+static constexpr auto operator&(const game_mode_flags game_mode, const game_mode_flag f)
+{
+	return underlying_value(game_mode) & underlying_value(f);
+}
+
 }
 
 #ifndef NDEBUG      // if debugging, these are variables
@@ -488,6 +519,8 @@ extern int force_cockpit_redraw;
 namespace dsx {
 extern ubyte DemoDoingRight,DemoDoingLeft;
 extern fix64	Time_flash_last_played;
+
+playernum_t get_marker_owner(game_mode_flags, game_marker_index, unsigned max_numplayers);
 }
 #endif
 

common/main/automap.h
@@ -82,10 +82,10 @@ struct d_marker_state : d_marker_object_numbers
 	{
 		return MarkerBeingDefined != player_marker_index::None;
 	}
-	static unsigned get_markers_per_player(unsigned game_mode, unsigned netgame_max_players);
+	static unsigned get_markers_per_player(game_mode_flags game_mode, unsigned netgame_max_players);
 };
 
-game_marker_index convert_player_marker_index_to_game_marker_index(unsigned game_mode, unsigned max_numplayers, unsigned player_num, player_marker_index base_marker_num);
+game_marker_index convert_player_marker_index_to_game_marker_index(game_mode_flags game_mode, unsigned max_numplayers, unsigned player_num, player_marker_index base_marker_num);
 xrange<player_marker_index> get_player_marker_range(unsigned maxdrop);
 extern marker_message_text_t Marker_input;
 extern d_marker_state MarkerState;

common/main/newdemo.h
@@ -57,7 +57,6 @@ extern int Newdemo_state;
 extern int NewdemoFrameCount;
 
 extern int Newdemo_vcr_state;
-extern int Newdemo_game_mode;
 extern sbyte Newdemo_do_interpolate;
 extern int Newdemo_show_percentage;
 

common/main/object.h
@@ -659,8 +659,6 @@ static inline player_marker_index &operator++(player_marker_index &i)
 	return i;
 }
 
-playernum_t get_marker_owner(unsigned, game_marker_index, unsigned max_numplayers);
-
 struct d_unique_buddy_state
 {
 	enum class Escort_goal_reachability : uint8_t

similar/editor/kmine.cpp
@@ -137,7 +137,7 @@ int CreateNewMine()
 		gamestate = editor_gamestate::none;
 		init_info = 1;
 		ResetFilename();
-		Game_mode = GM_UNKNOWN;
+		Game_mode = game_mode_flags::normal;
 		Current_level_num = 1;		// make level 1
 	}
 	return 1;

similar/main/automap.cpp
@@ -364,7 +364,7 @@ static float MarkerScale=2.0;
 
 d_marker_state MarkerState;
 
-game_marker_index convert_player_marker_index_to_game_marker_index(const unsigned game_mode, const unsigned max_numplayers, const unsigned player_num, const player_marker_index player_marker_num)
+game_marker_index convert_player_marker_index_to_game_marker_index(const game_mode_flags game_mode, const unsigned max_numplayers, const unsigned player_num, const player_marker_index player_marker_num)
 {
 	if (game_mode & GM_MULTI_COOP)
 		return static_cast<game_marker_index>((player_num * MAX_DROP_MULTI_COOP) + static_cast<unsigned>(player_marker_num));
@@ -373,7 +373,7 @@ game_marker_index convert_player_marker_index_to_game_marker_index(const unsigne
 	return game_marker_index{player_marker_num};
 }
 
-unsigned d_marker_state::get_markers_per_player(const unsigned game_mode, const unsigned max_numplayers)
+unsigned d_marker_state::get_markers_per_player(const game_mode_flags game_mode, const unsigned max_numplayers)
 {
 	if (game_mode & GM_MULTI_COOP)
 		return MAX_DROP_MULTI_COOP;
@@ -388,7 +388,7 @@ xrange<player_marker_index> get_player_marker_range(const unsigned maxdrop)
 	return {base, static_cast<player_marker_index>(static_cast<unsigned>(base) + maxdrop)};
 }
 
-playernum_t get_marker_owner(const unsigned game_mode, const game_marker_index gmi, const unsigned max_numplayers)
+playernum_t get_marker_owner(const game_mode_flags game_mode, const game_marker_index gmi, const unsigned max_numplayers)
 {
 	const auto ugmi = static_cast<unsigned>(gmi);
 	if (game_mode & GM_MULTI_COOP)
@@ -409,7 +409,7 @@ playernum_t get_marker_owner(const unsigned game_mode, const game_marker_index g
 
 namespace {
 
-xrange<game_marker_index> get_game_marker_range(const unsigned game_mode, const unsigned max_numplayers, const unsigned player_num, const unsigned maxdrop)
+xrange<game_marker_index> get_game_marker_range(const game_mode_flags game_mode, const unsigned max_numplayers, const unsigned player_num, const unsigned maxdrop)
 {
 	const auto base = convert_player_marker_index_to_game_marker_index(game_mode, max_numplayers, player_num, player_marker_index::_0);
 	return {base, static_cast<game_marker_index>(static_cast<unsigned>(base) + maxdrop)};

similar/main/fireball.cpp
@@ -726,7 +726,7 @@ static vmsegptridx_t choose_drop_segment(fvmsegptridx &vmsegptridx, fvcvertptr &
 				continue;
 			if (plr.connected == CONNECT_DISCONNECTED)
 				continue;
-			if ((Game_mode & (GM_TEAM|GM_CAPTURE)) && get_team(pnum) == team_of_drop_player)
+			if ((Game_mode & GM_TEAM) && get_team(pnum) == team_of_drop_player)
 				continue;
 			*r++ = pnum;
 		}

similar/main/game.cpp
@@ -128,7 +128,7 @@ int	force_cockpit_redraw=0;
 int	PaletteRedAdd, PaletteGreenAdd, PaletteBlueAdd;
 
 int	Game_suspended=0; //if non-zero, nothing moves but player
-int Game_mode;
+game_mode_flags Game_mode;
 int	Global_missile_firing_count = 0;
 }
 

commit 9fc7a147777c8230e5667ee9f1fe2ebfb0088edb

Fix LTO build of Descent 2
enum game_mode_flags must be visible in common code, which does not
define DXX_BUILD_DESCENT_II.  To avoid One Definition Rule errors,
game_mode_flags must have the same definition in all files.
Fortunately, the DXX_BUILD_DESCENT_II definition is a superset of the
common definition, so the preprocessor guards can be replaced with
advisory comments.

Fixes: 2b718da343480629ca15f2fec285fd1964e689a4 ("Use enum class for Game_mode, Newdemo_game_mode")

common/main/game.h
@@ -85,10 +85,10 @@ enum class game_mode_flag : uint16_t
 	unknown			= 1u << 6,
 	team			= 1u << 8,
 	bounty			= 1u << 9,
-#if defined(DXX_BUILD_DESCENT_II)
+	/* if DXX_BUILD_DESCENT_II */
 	capture			= 1u << 10,
 	hoard			= 1u << 11,
-#endif
+	/* endif */
 };
 
 enum class game_mode_flags : uint16_t
@@ -99,11 +99,11 @@ enum class game_mode_flags : uint16_t
 	anarchy_with_robots = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::multi_robots),
 	cooperative = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::multi_robots) | static_cast<uint16_t>(game_mode_flag::multi_coop),
 	bounty = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::bounty),
-#if defined(DXX_BUILD_DESCENT_II)
+	/* if DXX_BUILD_DESCENT_II */
 	capture_flag = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::team) | static_cast<uint16_t>(game_mode_flag::capture),
 	hoard = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::hoard),
 	team_hoard = static_cast<uint16_t>(game_mode_flag::network) | static_cast<uint16_t>(game_mode_flag::team) | static_cast<uint16_t>(game_mode_flag::hoard),
-#endif
+	/* endif */
 };
 
 // The following bits define the game modes.