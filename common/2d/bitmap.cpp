dxx-rebirth/dxx-rebirth

commit 4d6affd74d33df6ee2c081b56159caa245c7fa10

Fix SDL-only briefing crash
Trim the bitmap's current width and height to be not more than its
parent.  Otherwise, an automatic screen resize, such as triggered when
playing the robot briefings in Descent 2, will give the window a new
smaller bm_data, but continue to use the old dimensions.  This then
causes load_briefing_screen -> gr_bitmap_scale_to -> scale_line to write
off the end of the smaller bm_data, causing memory corruption and
eventually a crash.

common/2d/bitmap.cpp
@@ -126,8 +126,8 @@ void gr_init_sub_bitmap (grs_bitmap &bm, grs_bitmap &bmParent, uint16_t x, uint1
 	if (x > bmParent.bm_w ||
 		y > bmParent.bm_h)
 		throw std::overflow_error("offset beyond parent dimensions");
-	bm.bm_w = w;
-	bm.bm_h = h;
+	bm.bm_w = std::min<uint16_t>(w, bmParent.bm_w - subx);
+	bm.bm_h = std::min<uint16_t>(h, bmParent.bm_h - suby);
 	bm.set_flags(bmParent.get_flags());
 	bm.set_type(bmParent.get_type());
 	bm.bm_rowsize = bmParent.bm_rowsize;

commit b6a26ddc8cb2750351710e700996c44799dc6b3f

Fix high score background truncation
Commit 4d6affd74d33 ("Fix SDL-only briefing crash") added a hack to
prevent a crash when the briefing window is automatically resized.
However, a logic error in that hack causes some subcanvases that were
not wrong to be truncated.  Change the logic to fix those subcanvases,
such as the one used for rendering a background in the High Scores
window.  The new logic still avoids the crash in in SDL-only mode.

Fixes: 4d6affd74d33df6ee2c081b56159caa245c7fa10 ("Fix SDL-only briefing crash")

common/2d/bitmap.cpp
@@ -126,8 +126,8 @@ void gr_init_sub_bitmap (grs_bitmap &bm, grs_bitmap &bmParent, uint16_t x, uint1
 	if (x > bmParent.bm_w ||
 		y > bmParent.bm_h)
 		throw std::overflow_error("offset beyond parent dimensions");
-	bm.bm_w = std::min<uint16_t>(w, bmParent.bm_w - subx);
-	bm.bm_h = std::min<uint16_t>(h, bmParent.bm_h - suby);
+	bm.bm_w = std::min<uint16_t>(w, bmParent.bm_w - x);
+	bm.bm_h = std::min<uint16_t>(h, bmParent.bm_h - y);
 	bm.set_flags(bmParent.get_flags());
 	bm.set_type(bmParent.get_type());
 	bm.bm_rowsize = bmParent.bm_rowsize;