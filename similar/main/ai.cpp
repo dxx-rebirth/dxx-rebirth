dxx-rebirth/dxx-rebirth

commit 9fdf6005df970cad6fdbb2f545836ccf0ad6f03a

Convert ai_static::GOALSIDE to sidenum_t

similar/main/ai.cpp
@@ -27,6 +27,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include <cstdlib>
 #include <stdio.h>
 #include <time.h>
+#include <optional>
 
 #include "inferno.h"
 #include "game.h"
@@ -1954,7 +1955,7 @@ namespace {
 
 //	-----------------------------------------------------------------------------------------------------------
 //	Return side of openable door in segment, if any.  If none, return side_none.
-static unsigned openable_doors_in_segment(fvcwallptr &vcwallptr, const shared_segment &segp)
+static std::optional<sidenum_t> openable_doors_in_segment(fvcwallptr &vcwallptr, const shared_segment &segp)
 {
 #if defined(DXX_BUILD_DESCENT_II)
 	auto &WallAnims = GameSharedState.WallAnims;
@@ -1977,10 +1978,10 @@ static unsigned openable_doors_in_segment(fvcwallptr &vcwallptr, const shared_se
 			if (WallAnims[w.clip_num].flags & WCF_HIDDEN)
 				continue;
 #endif
-			return idx;
+			return static_cast<sidenum_t>(idx);
 		}
 	}
-	return side_none;
+	return std::nullopt;
 }
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -3056,8 +3057,8 @@ static int openable_door_on_near_path(fvcsegptr &vcsegptr, fvcwallptr &vcwallptr
 	const auto path_length = aip.path_length;
 	if (path_length < 1)
 		return 0;
-	if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(obj.segnum)) - side_none)
-		return r;
+	if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(obj.segnum)); r.has_value())
+		return *r;
 	if (path_length < 2)
 		return 0;
 	size_t idx;
@@ -3068,16 +3069,16 @@ static int openable_door_on_near_path(fvcsegptr &vcsegptr, fvcwallptr &vcwallptr
 	 */
 	if (idx < Point_segs.size() && Point_segs[idx].segnum != segment_none)
 	{
-		if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(Point_segs[idx].segnum)) - side_none)
-			return r;
+		if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(Point_segs[idx].segnum)); r.has_value())
+			return *r;
 	}
 	if (path_length < 3)
 		return 0;
 	idx = aip.hide_index + aip.cur_path_index + 2*aip.PATH_DIR;
 	if (idx < Point_segs.size() && Point_segs[idx].segnum != segment_none)
 	{
-		if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(Point_segs[idx].segnum)) - side_none)
-			return r;
+		if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(Point_segs[idx].segnum)); r.has_value())
+			return *r;
 	}
 	return 0;
 }
@@ -3651,11 +3652,10 @@ _exit_cheat:
 			{
 				auto &Walls = LevelUniqueWallSubsystemState.Walls;
 				auto &vcwallptr = Walls.vcptr;
-				const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(obj->segnum));
-				if (r != side_none)
+				if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(obj->segnum)); r.has_value())
 				{
 					ailp.mode = ai_mode::AIM_OPEN_DOOR;
-					aip->GOALSIDE = r;
+					aip->GOALSIDE = *r;
 				}
 				else if (ailp.mode != ai_mode::AIM_FOLLOW_PATH)
 				{

common/main/aistruct.h
@@ -216,7 +216,7 @@ struct ai_static : public prohibit_void_ptr<ai_static>
 #elif defined(DXX_BUILD_DESCENT_II)
 	int8_t SUB_FLAGS;				// bit 0: Set -> Robot's current gun in different segment than robot's center.
 #endif
-	int8_t GOALSIDE;				// for guys who open doors, this is the side they are going after.
+	sidenum_t GOALSIDE;				// for guys who open doors, this is the side they are going after.
 	int8_t CLOAKED{};					// Cloaked now.
 	int8_t SKIP_AI_COUNT{};			// Skip AI this frame, but decrement in do_ai_frame.
 	int8_t REMOTE_OWNER{};			// Who is controlling this remote AI object (multiplayer use only)

common/main/fwd-segment.h
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include <optional>
 #include <type_traits>
 #include <physfs.h>
 #include "maths.h"
@@ -80,6 +81,9 @@ enum sidenum_t : uint8_t
 	WBACK = 4,
 	WFRONT = 5
 };
+
+[[nodiscard]]
+std::optional<sidenum_t> build_sidenum_from_untrusted(uint8_t untrusted);
 
 using texture_index = uint16_t;
 enum class texture1_value : uint16_t;

similar/main/gamesave.cpp
@@ -491,7 +491,7 @@ static void read_object(const vmobjptr_t obj,PHYSFS_File *f,int version)
 #elif defined(DXX_BUILD_DESCENT_II)
 			obj->ctype.ai_info.SUB_FLAGS = ai_info_flags[4];
 #endif
-			obj->ctype.ai_info.GOALSIDE = ai_info_flags[5];
+			obj->ctype.ai_info.GOALSIDE = build_sidenum_from_untrusted(ai_info_flags[5]).value();
 			obj->ctype.ai_info.CLOAKED = ai_info_flags[6];
 			obj->ctype.ai_info.SKIP_AI_COUNT = ai_info_flags[7];
 			obj->ctype.ai_info.REMOTE_OWNER = ai_info_flags[8];

similar/main/multi.cpp
@@ -6195,7 +6195,7 @@ void multi_object_rw_to_object(const object_rw *const obj_rw, object &obj)
 #elif defined(DXX_BUILD_DESCENT_II)
 			obj.ctype.ai_info.SUB_FLAGS = obj_rw->ctype.ai_info.flags[4];
 #endif
-			obj.ctype.ai_info.GOALSIDE = obj_rw->ctype.ai_info.flags[5];
+			obj.ctype.ai_info.GOALSIDE = build_sidenum_from_untrusted(obj_rw->ctype.ai_info.flags[5]).value();
 			obj.ctype.ai_info.CLOAKED = obj_rw->ctype.ai_info.flags[6];
 			obj.ctype.ai_info.SKIP_AI_COUNT = obj_rw->ctype.ai_info.flags[7];
 			obj.ctype.ai_info.REMOTE_OWNER = obj_rw->ctype.ai_info.flags[8];

similar/main/segment.cpp
@@ -32,6 +32,22 @@ ASSERT_SERIAL_UDT_MESSAGE_SIZE(composite_side, 6);
 
 }
 
+std::optional<sidenum_t> build_sidenum_from_untrusted(const uint8_t untrusted)
+{
+	switch (untrusted)
+	{
+		case static_cast<uint8_t>(sidenum_t::WLEFT):
+		case static_cast<uint8_t>(sidenum_t::WTOP):
+		case static_cast<uint8_t>(sidenum_t::WRIGHT):
+		case static_cast<uint8_t>(sidenum_t::WBOTTOM):
+		case static_cast<uint8_t>(sidenum_t::WBACK):
+		case static_cast<uint8_t>(sidenum_t::WFRONT):
+			return sidenum_t{untrusted};
+		default:
+			return std::nullopt;
+	}
+}
+
 void segment_side_wall_tmap_write(PHYSFS_File *fp, const shared_side &sside, const unique_side &uside)
 {
 	PHYSFSX_serialize_write(fp, composite_side{sside, uside});

similar/main/state.cpp
@@ -612,7 +612,7 @@ static void state_object_rw_to_object(const object_rw *const obj_rw, object &obj
 #elif defined(DXX_BUILD_DESCENT_II)
 			obj.ctype.ai_info.SUB_FLAGS = obj_rw->ctype.ai_info.flags[4];
 #endif
-			obj.ctype.ai_info.GOALSIDE = obj_rw->ctype.ai_info.flags[5];
+			obj.ctype.ai_info.GOALSIDE = build_sidenum_from_untrusted(obj_rw->ctype.ai_info.flags[5]).value();
 			obj.ctype.ai_info.CLOAKED = obj_rw->ctype.ai_info.flags[6];
 			obj.ctype.ai_info.SKIP_AI_COUNT = obj_rw->ctype.ai_info.flags[7];
 			obj.ctype.ai_info.REMOTE_OWNER = obj_rw->ctype.ai_info.flags[8];

commit b3f250f3b618d1d1c31c1cec1264f95ca4168a75

Fix openable_door_on_near_path for side WLEFT
openable_door_on_near_path should return 0 for no door and any non-zero
value for door-found.  Commit 9fdf6005df97 changed the logic to return 0
for no door, and the side number value for door-found.  This is wrong,
since WLEFT has integer value 0, so the caller will interpret a return
of door-found, side=WLEFT as no-door-found.

Fixes: 9fdf6005df970cad6fdbb2f545836ccf0ad6f03a ("Convert ai_static::GOALSIDE to sidenum_t")

similar/main/ai.cpp
@@ -3098,8 +3098,8 @@ static int openable_door_on_near_path(fvcsegptr &vcsegptr, fvcwallptr &vcwallptr
 	const auto path_length = aip.path_length;
 	if (path_length < 1)
 		return 0;
-	if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(obj.segnum)); r.has_value())
-		return *r;
+	if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(obj.segnum)).has_value())
+		return r;
 	if (path_length < 2)
 		return 0;
 	size_t idx;
@@ -3110,16 +3110,16 @@ static int openable_door_on_near_path(fvcsegptr &vcsegptr, fvcwallptr &vcwallptr
 	 */
 	if (idx < Point_segs.size() && Point_segs[idx].segnum != segment_none)
 	{
-		if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(Point_segs[idx].segnum)); r.has_value())
-			return *r;
+		if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(Point_segs[idx].segnum)).has_value())
+			return r;
 	}
 	if (path_length < 3)
 		return 0;
 	idx = aip.hide_index + aip.cur_path_index + 2*aip.PATH_DIR;
 	if (idx < Point_segs.size() && Point_segs[idx].segnum != segment_none)
 	{
-		if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(Point_segs[idx].segnum)); r.has_value())
-			return *r;
+		if (const auto r = openable_doors_in_segment(vcwallptr, vcsegptr(Point_segs[idx].segnum)).has_value())
+			return r;
 	}
 	return 0;
 }