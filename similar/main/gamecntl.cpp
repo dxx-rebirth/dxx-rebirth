dxx-rebirth/dxx-rebirth

commit 8c8b7419b62e57632c3766bc2c47aac00c3944d3

Improved conditionals for stereo vs non-stereo modes.
Hot-key handling for cycling thru stereo modes & parallax adjustments.
Simplified stereo adjustments for left/right eye separation & offsets.
Not quite compatible yet with Bob Akka stereo method from Descent 1.5.
Function key assignments different (F5..F8) due to pre-empted hot-keys.

ALT+SHIFT+F5:	decrease eye separation (VR_eye_width)
ALT+SHIFT+F6:	increase eye separation (VR_eye_width)
    SHIFT+F5:	decrease image shift (VR_eye_offset)
    SHIFT+F6:	increase image shift (VR_eye_offset)
ALT|SHIFT+F7:	reset eye separation + image shift
ALT|SHIFT+F8:	cycle thru half-width + half-height formats

Note for compiling dxx-rebirth snapshot on MacOSX:

export CXXFLAGS=-Wno-uninitialized
scons macos_add_frameworks=False

similar/main/gamecntl.cpp
@@ -912,6 +912,35 @@ static window_event_result HandleSystemKey(int key)
 			break;
 #endif
 
+		case KEY_SHIFTED + KEY_F5:
+			VR_eye_offset -= 1;
+			break;
+		case KEY_SHIFTED + KEY_F6:
+			VR_eye_offset += 1;
+			break;
+		case KEY_SHIFTED + KEY_ALTED + KEY_F5:
+			VR_eye_width -= (F1_0/10); //*= 10/11;
+			break;
+		case KEY_SHIFTED + KEY_ALTED + KEY_F6:
+			VR_eye_width += (F1_0/10); //*= 11/10;
+			break;
+		case KEY_SHIFTED + KEY_F7:
+		case KEY_SHIFTED + KEY_ALTED + KEY_F7:
+			VR_eye_width = F1_0;
+			VR_eye_offset = 0;
+			break;
+		case KEY_SHIFTED + KEY_F8:
+		case KEY_SHIFTED + KEY_ALTED + KEY_F8:
+			if (CGameArg.OglStereo)
+				++VR_stereo %= 4;
+			switch (VR_stereo) {
+				case 0: VR_half_width = VR_half_height = false; break;
+				case 1: VR_half_width = false; VR_half_height = true; break;
+				case 2: VR_half_width = true; VR_half_height = false; break;
+				case 3: VR_half_width = VR_half_height = true; break;
+			}
+			break;
+
 			/*
 			 * Jukebox hotkeys -- MD2211, 2007
 			 * Now for all music

common/include/ogl_init.h
@@ -68,9 +68,9 @@ void ogl_loadbmtexture_f(grs_bitmap &bm, opengl_texture_filter texfilt, bool tex
 void ogl_freebmtexture(grs_bitmap &bm);
 
 void ogl_start_frame(grs_canvas &);
+void ogl_stereo_frame(int xeye, int xoff);
 void ogl_end_frame(void);
 void ogl_set_screen_mode(void);
-void ogl_set_frame(int xeye, int xoff);
 
 struct ogl_colors
 {

common/main/game.h
@@ -399,10 +399,10 @@ void flickering_light_read(flickering_light &fl, PHYSFS_File *fp);
 void flickering_light_write(const flickering_light &fl, PHYSFS_File *fp);
 #endif
 
-extern	bool VR_stereo;
+extern	int  VR_stereo;
 extern	bool VR_half_width;
 extern	bool VR_half_height;
-extern	int  VR_eye_width;
+extern	fix  VR_eye_width;
 extern	int  VR_eye_offset;
 
 void game_render_frame_mono(const control_info &Controls);

similar/arch/ogl/ogl.cpp
@@ -1291,6 +1291,10 @@ void ogl_start_frame(grs_canvas &canvas)
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();//clear matrix
 
+	if (!VR_stereo)
+		return;
+
+	// cache viewport + projection transform for left/right stereo views
 	glGetFloatv(GL_PROJECTION_MATRIX, &ogl_stereo_transform[0][0]);
 	glGetFloatv(GL_PROJECTION_MATRIX, &ogl_stereo_transform[1][0]);
 
@@ -1306,12 +1310,21 @@ void ogl_start_frame(grs_canvas &canvas)
 	// half-height viewports for above/below format
 	else if (VR_half_height)
 		ogl_stereo_viewport[0][1] -= ogl_stereo_viewport[0][3];		// y = h/2
+
+	// center unsqueezed side-by-side format
+	if (VR_half_width && VR_half_height) {
+		ogl_stereo_viewport[0][1] -= ogl_stereo_viewport[0][3]/2;	// y = h/4
+		ogl_stereo_viewport[1][1] -= ogl_stereo_viewport[1][3]/2;	// y = h/4
+	}
 }
 
-void ogl_set_frame(int xeye, int xoff)
+void ogl_stereo_frame(int xeye, int xoff)
 {
 	float dxeye = 0.0f; // f2fl(xeye);
-	float dxoff = xoff * 1.0f / grd_curscreen->sc_canvas.cv_bitmap.bm_w;
+	float dxoff = xoff * 2.0f / grd_curscreen->sc_canvas.cv_bitmap.bm_w;
+
+	if (!VR_stereo)
+		return;
 
 	if (xeye < 0) {
 		// left eye view

similar/main/game.cpp
@@ -190,6 +190,16 @@ screen_mode Game_screen_mode = initial_large_game_screen_mode;
 
 namespace dsx {
 
+void init_stereo()
+{
+	// init stereo options
+	if (CGameArg.OglStereo) {
+		VR_stereo = true;
+		VR_half_height = true;
+		PlayerCfg.CockpitMode[1] = CM_FULL_SCREEN;
+	}
+}
+
 //initialize the various canvases on the game screen
 //called every time the screen mode or cockpit changes
 void init_cockpit()
@@ -198,13 +208,6 @@ void init_cockpit()
 
 	if (Screen_mode != SCREEN_GAME)
 		return;
-
-	// init stereo options
-	if (CGameArg.OglStereo) {
-		PlayerCfg.CockpitMode[1] = CM_FULL_SCREEN;
-		VR_stereo = true;
-		VR_half_height = true;
-	}
 
 	if ( Screen_mode == SCREEN_EDITOR )
 		PlayerCfg.CockpitMode[1] = CM_FULL_SCREEN;
@@ -1592,6 +1595,7 @@ game_window *game_setup()
 
 	auto game_wind = window_create<game_window>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);
 	reset_palette_add();
+	init_stereo();
 	init_cockpit();
 	init_gauges();
 	netplayerinfo_on = 0;

similar/main/gamerend.cpp
@@ -384,10 +384,10 @@ static void draw_window_label(object_array &Objects, grs_canvas &canvas)
 
 namespace dsx {
 
-	bool VR_stereo = false;
+	int  VR_stereo = false;
 	bool VR_half_width = false;
 	bool VR_half_height = false;
-	int  VR_eye_width = F1_0;
+	fix  VR_eye_width = F1_0;
 	int  VR_eye_offset = 0;
 
 namespace {

similar/main/render.cpp
@@ -1217,7 +1217,7 @@ void render_frame(grs_canvas &canvas, fix eye_offset, window_rendered_data &wind
 
 #if DXX_USE_OGL
 	// select stereo viewport/transform/buffer per left/right eye
-	ogl_set_frame(eye_offset, VR_eye_offset);
+	ogl_stereo_frame(eye_offset, VR_eye_offset);
 #endif
 
 	auto Viewer_eye = Viewer->pos;

commit 834aeea23fc7b61c1e95e4f20ce4851d477e502b

Fix drop-primary/drop-secondary keys
Commit 8c8b7419b62e defined Shift-F5 and Shift-F6 to adjust the VR
handling.  However, Shift-F5 and Shift-F6 already had a meaning: drop
primary weapon / drop secondary weapon.  The ordering of the logic
causes the new VR handling to suppress the old drop handling, thus
preventing players from dropping weapons.  Disable the VR meaning for
these keys so that drop-weapon works again, since F1 documents those
keys as drop-weapon.

Fixes: 8c8b7419b62e57632c3766bc2c47aac00c3944d3 ("Improved conditionals for stereo vs non-stereo modes.")

similar/main/gamecntl.cpp
@@ -976,6 +976,11 @@ static window_event_result HandleSystemKey(int key)
 #endif
 
 #if DXX_USE_OGL
+#if 0
+			/* These conflict with the drop-primary and drop-secondary
+			 * keybindings.  Dropping items is more common than using VR, so
+			 * disable these.
+			 */
 		case KEY_SHIFTED + KEY_F5:
 			VR_eye_offset -= 1;
 			reset_cockpit();
@@ -984,6 +989,7 @@ static window_event_result HandleSystemKey(int key)
 			VR_eye_offset += 1;
 			reset_cockpit();
 			break;
+#endif
 		case KEY_SHIFTED + KEY_ALTED + KEY_F5:
 			VR_eye_width -= (F1_0/10); //*= 10/11;
 			reset_cockpit();