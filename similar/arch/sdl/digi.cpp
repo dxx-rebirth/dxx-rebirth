dxx-rebirth/dxx-rebirth

commit 3b5b69cb97fd9752a5388a35b66f3fc74a3242cb

Improve error reporting for hmp_open / hmp2mid
Rework the error paths to return path-specific status codes so that the
caller can report exactly which step caused an HMP file to be rejected.
On error, print this reason numerically and, if the reason was a PhysFS
error, also print the PhysFS error code numerically and symbolically.

similar/arch/sdl/digi.cpp
@@ -262,8 +262,10 @@ int digi_win32_play_midi_song( const char * filename, int loop )
 	if (filename == NULL)
 		return 0;
 
-	if ((cur_hmp = hmp_open(filename)))
+	if (auto &&[cur_hmp, hoe, pec] = hmp_open(filename); cur_hmp)
 	{
+		(void)hoe;
+		(void)pec;
 		/* 
 		 * FIXME: to be implemented as soon as we have some kind or checksum function - replacement for ugly hack in hmp.c for descent.hmp
 		 * if (***filesize check*** && ***CRC32 or MD5 check***)

common/arch/sdl/digi_mixer_music.cpp
@@ -159,10 +159,16 @@ int mix_play_file(const char *filename, int loop, void (*const entry_hook_finish
 	// It's a .hmp!
 	if (const auto fptr = strrchr(filename, '.'); fptr && !d_stricmp(fptr, ".hmp"))
 	{
-		hmp2mid(filename, current_music_hndlbuf);
-		current_music_type = load_mus_data(current_music_hndlbuf.data(), current_music_hndlbuf.size(), loop, hook_finished_track);
-		if (current_music_type != CurrentMusicType::None)
-			return 1;
+		if (auto &&[v, hoe] = hmp2mid(filename); hoe == hmp_open_error::None)
+		{
+			current_music_hndlbuf = std::move(v);
+			current_music_type = load_mus_data(current_music_hndlbuf.data(), current_music_hndlbuf.size(), loop, hook_finished_track);
+			if (current_music_type != CurrentMusicType::None)
+				return 1;
+		}
+		else
+			/* hmp2mid printed an error message, so there is no need for another one here */
+			return 0;
 	}
 
 	// try loading music via given filename

common/include/hmp.h
@@ -8,6 +8,7 @@
 #define __HMP_H
 
 #include <memory>
+#include <tuple>
 #include <vector>
 #ifdef _WIN32
 #include <windows.h>
@@ -92,8 +93,35 @@ struct hmp_file
 	unsigned int midi_division;
 };
 
-std::unique_ptr<hmp_file> hmp_open(const char *filename);
-void hmp2mid(const char *hmp_name, std::vector<uint8_t> &midbuf);
+enum class hmp_open_error : uint8_t
+{
+	None,
+	/* All PhysFS errors must be before any non-PhysFS errors.  The error
+	 * printing code relies on this to determine which error numbers are
+	 * physfs_* and which are not.
+	 */
+	physfs_open,
+	physfs_read_header,
+	physfs_seek0,
+	physfs_read_tracks,
+	physfs_seek1,
+	physfs_read_tempo,
+	physfs_seek2,
+	physfs_read_track_length,
+	physfs_read_track_data,
+	/* Non-PhysFS reasons start here */
+	content_header,
+	track_count,
+};
+
+/* PHYSFS_ErrorCode member is only meaningful if hmp_open_error is a
+ * hmp_open_error::physfs_* code.  Otherwise, it is formally undefined.
+ * Informally, it is 0. */
+using hmp_open_result = std::tuple<std::unique_ptr<hmp_file>, hmp_open_error, PHYSFS_ErrorCode>;
+using hmpmid_result = std::tuple<std::vector<uint8_t>, hmp_open_error>;
+
+hmp_open_result hmp_open(const char *filename);
+hmpmid_result hmp2mid(const char *hmp_name);
 #ifdef _WIN32
 void hmp_setvolume(hmp_file *hmp, int volume);
 int hmp_play(hmp_file *hmp, int bLoop);

common/misc/hmp.cpp
@@ -26,6 +26,7 @@
 #include "dsx-ns.h"
 #include "compiler-range_for.h"
 #include "d_range.h"
+#include "d_underlying_value.h"
 #include "partial_range.h"
 #include <memory>
 
@@ -49,71 +50,68 @@ hmp_file::~hmp_file()
 #endif
 }
 
-std::unique_ptr<hmp_file> hmp_open(const char *filename) {
-	int data, tempo;
-	auto fp = PHYSFSX_openReadBuffered(filename).first;
+hmp_open_result hmp_open(const char *const filename)
+{
+	int tempo;
+	const auto fpe = PHYSFSX_openReadBuffered(filename);
 
-	if (!fp)
-		return NULL;
+	if (!fpe.first)
+		return {nullptr, hmp_open_error::physfs_open, fpe.second};
+	auto &fp = fpe.first;
 
-	std::unique_ptr<hmp_file> hmp(new hmp_file{});
 	char buf[8];
-	if ((PHYSFS_read(fp, buf, 1, 8) != 8) || (memcmp(buf, "HMIMIDIP", 8)))
-	{
-		return NULL;
-	}
+	const auto physfs_error = [](hmp_open_error e) {
+		return hmp_open_result{nullptr, e, PHYSFS_getLastErrorCode()};
+	};
+	const auto data_error = [](hmp_open_error e) {
+		return hmp_open_result{nullptr, e, PHYSFS_ErrorCode{}};
+	};
+	if (PHYSFS_read(fp, buf, 1, 8) != 8)
+		return physfs_error(hmp_open_error::physfs_read_header);
+	if (memcmp(buf, "HMIMIDIP", 8))
+		return data_error(hmp_open_error::content_header);
 
-	if (PHYSFSX_fseek(fp, 0x30, SEEK_SET))
-	{
-		return NULL;
-	}
+	if (!PHYSFS_seek(fp, 0x30))
+		return physfs_error(hmp_open_error::physfs_seek0);
 
 	unsigned num_tracks;
 	if (PHYSFS_read(fp, &num_tracks, 4, 1) != 1)
-	{
-		return NULL;
-	}
+		return physfs_error(hmp_open_error::physfs_read_tracks);
 
 	if ((num_tracks < 1) || (num_tracks > HMP_TRACKS))
-	{
-		return NULL;
-	}
+		return data_error(hmp_open_error::track_count);
+	std::unique_ptr<hmp_file> hmp(new hmp_file{});
 	hmp->num_trks = num_tracks;
 
-	if (PHYSFSX_fseek(fp, 0x38, SEEK_SET))
-	{
-		return NULL;
-	}
+	if (!PHYSFS_seek(fp, 0x38))
+		return physfs_error(hmp_open_error::physfs_seek1);
 	if (PHYSFS_read(fp, &tempo, 4, 1) != 1)
-	{
-		return NULL;
-	}
+		return physfs_error(hmp_open_error::physfs_read_tempo);
 	hmp->tempo = INTEL_INT(tempo);
 
-	if (PHYSFSX_fseek(fp, 0x308, SEEK_SET))
-	{
-		return NULL;
-	}
+	if (!PHYSFS_seek(fp, 0x308))
+		return physfs_error(hmp_open_error::physfs_seek2);
 
 	range_for (auto &i, partial_range(hmp->trks, num_tracks))
 	{
-		if ((PHYSFSX_fseek(fp, 4, SEEK_CUR)) || (PHYSFS_read(fp, &data, 4, 1) != 1))
-		{
-			return NULL;
-		}
+		std::array<int, 3> tdata;
+		/* Discard tdata[0] and tdata[2].  They are read to avoid needing a
+		 * seek operation to skip them.
+		 */
+		if (PHYSFS_read(fp, tdata.data(), sizeof(tdata[0]), tdata.size()) != tdata.size())
+			return physfs_error(hmp_open_error::physfs_read_track_length);
 
+		int data = tdata[1];
 		data -= 12;
 		i.len = data;
 		i.data = std::make_unique<uint8_t[]>(data);
 		/* finally, read track data */
-		if ((PHYSFSX_fseek(fp, 4, SEEK_CUR)) || (PHYSFS_read(fp, i.data.get(), data, 1) != 1))
-		{
-			return NULL;
-		}
+		if (PHYSFS_read(fp, i.data.get(), data, 1) != 1)
+			return physfs_error(hmp_open_error::physfs_read_track_data);
 		i.loop_set = 0;
 	}
 	hmp->filesize = PHYSFS_fileLength(fp);
-	return hmp;
+	return {std::move(hmp), hmp_open_error::None, PHYSFS_ErrorCode{}};
 }
 
 #ifdef _WIN32
@@ -693,13 +691,20 @@ struct midhdr
 
 DEFINE_SERIAL_CONST_UDT_TO_MESSAGE(midhdr, m, (magic_header, m.num_trks, m.time_div, tempo));
 
-void hmp2mid(const char *hmp_name, std::vector<uint8_t> &midbuf)
+hmpmid_result hmp2mid(const char *hmp_name)
 {
-	std::unique_ptr<hmp_file> hmp = hmp_open(hmp_name);
+	auto &&[hmp, hoe, pec] = hmp_open(hmp_name);
 	if (!hmp)
-		return;
+	{
+		if (underlying_value(hoe) <= static_cast<unsigned>(hmp_open_error::physfs_read_track_data))
+			con_printf(CON_CRITICAL, "Failed to read HMP music %s: hmp_open_error=%u; PHYSFS_ErrorCode=%u/%s", hmp_name, underlying_value(hoe), pec, PHYSFS_getErrorByCode(pec));
+		else
+			con_printf(CON_CRITICAL, "Failed to read HMP music %s: hmp_open_error=%u", hmp_name, underlying_value(hoe));
+		return {std::vector<uint8_t>{}, hoe};
+	}
 
 	const midhdr mh(hmp.get());
+	std::vector<uint8_t> midbuf;
 	// write MIDI-header
 	midbuf.resize(serial::message_type<decltype(mh)>::maximum_size);
 	be_bytebuffer_t bb(&midbuf[0]);
@@ -716,6 +721,7 @@ void hmp2mid(const char *hmp_name, std::vector<uint8_t> &midbuf)
 		be_bytebuffer_t bbmi(&midbuf[midtrklenpos]);
 		serial::process_buffer(bbmi, static_cast<int32_t>(size_after - size_before));
 	}
+	return {midbuf, hmp_open_error::None};
 }
 
 }

commit 9e0431b861e2cb3688e7e4abcc6e4a66214b17a0

digi_win32_play_midi_song: write hmp_open result directly to global
Commit 3b5b69cb97fd changed the logic to store the result of hmp_open
into a local that was destroyed at the end of the function.  This was
incorrect, as destruction of the returned value will immediately halt
the music.  Revert to the previous behavior, where the result is written
to the global `cur_hmp` so that it persists past the end of the
function.

Reported-by: norbert79 <https://github.com/dxx-rebirth/dxx-rebirth/issues/634>
Analyzed-by: AlumiuN <https://github.com/dxx-rebirth/dxx-rebirth/issues/634#issuecomment-1110510761>
Proposed-fix-by: AlumiuN <https://github.com/dxx-rebirth/dxx-rebirth/issues/634#issuecomment-1110510761>
Fixes: 3b5b69cb97fd9752a5388a35b66f3fc74a3242cb ("Improve error reporting for hmp_open / hmp2mid")

similar/arch/sdl/digi.cpp
@@ -262,10 +262,8 @@ int digi_win32_play_midi_song( const char * filename, int loop )
 	if (filename == NULL)
 		return 0;
 
-	if (auto &&[cur_hmp, hoe, pec] = hmp_open(filename); cur_hmp)
+	if ((cur_hmp = std::get<0>(hmp_open(filename))))
 	{
-		(void)hoe;
-		(void)pec;
 		/* 
 		 * FIXME: to be implemented as soon as we have some kind or checksum function - replacement for ugly hack in hmp.c for descent.hmp
 		 * if (***filesize check*** && ***CRC32 or MD5 check***)