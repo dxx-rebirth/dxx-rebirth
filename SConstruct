dxx-rebirth/dxx-rebirth

commit bc6d043f5d99b7768488bdfb4e7b7bcb79ac7dc2

Extend SConstruct compilation_database support to cover Program()
This is not required by clang, but should be useful for anyone who wants
to produce a machine-readable list of all the compile commands run for
building the program.  Previously, compilation_database would describe
how to build object files from sources, but not how to link those
objects.

SConstruct
@@ -4361,6 +4361,21 @@ class DXXCommon(LazyObjectConstructor):
 			for o in objects])
 		return objects
 
+	def _compilation_database_Program(self,target=None,source=None,*args,**kwargs):
+		env = self.env
+		Program = env.__compilation_database_Program
+		objects = Program(target=target, source=source, *args, **kwargs)
+		source = [s[0] for s in source] if source else []
+		directory = env.Dir('.').get_abspath()
+		self._compilation_database_entries.extend([
+			{
+				'command' : env.Override(kwargs).subst(env['LINKCOM'], target=[o], source=source),
+				'directory' : directory,
+				'output' : str(o),
+				}
+			for o in objects])
+		return objects
+
 	def create_special_target_nodes(self,archive):
 		env = self.env
 		StaticObject = env.StaticObject
@@ -4372,6 +4387,8 @@ class DXXCommon(LazyObjectConstructor):
 		if user_settings.compilation_database:
 			env.__compilation_database_StaticObject = StaticObject
 			env.StaticObject = self._compilation_database_StaticObject
+			env.__compilation_database_Program = env.Program
+			env.Program = self._compilation_database_Program
 		if user_settings.check_header_includes:
 			# Create header targets before creating the PCHManager, so that
 			# create_header_targets() does not call the PCHManager

commit e66da397a5691b837be101f7ac9f993c9f190a00

Add dummy 'file' key to executable entry in compilation database
clang rejects the entire database if any entry lacks a 'file' key,
regardless of whether the entry is used.

Fixes: bc6d043f5d99b7768488bdfb4e7b7bcb79ac7dc2 ("Extend SConstruct compilation_database support to cover Program()")

SConstruct
@@ -4312,6 +4312,17 @@ class DXXCommon(LazyObjectConstructor):
 			{
 				'command' : env.Override(kwargs).subst(env['LINKCOM'], target=[o], source=source),
 				'directory' : directory,
+				# The `'file'` key is mandatory.
+				# `clang/lib/Tooling/JSONCompilationDatabase.cpp` will reject a
+				# database that has any entries which lack a `'file'` key.  The
+				# `'file'` key does not make sense for an output executable, so
+				# invent a plausible looking string.  This should be safe,
+				# since clang cannot usefully target an output executable, so
+				# it should never need to interpret this `'file'` value.  This
+				# dictionary entry is only present as a convenience to other
+				# tools which may want to use the compilation database to know
+				# the link flags.
+				'file' : str(list(map(repr, source))),
 				'output' : str(o),
 				}
 			for o in objects])