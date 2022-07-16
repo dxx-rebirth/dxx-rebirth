1d23e5848fefc80bf79291bbe5853c78c409daa8
Kp <kp@valhallalegends.com>
2022-07-16 15:26:12 +0000

    Use enumerated_array for UDP_mdata_store::pkt_num

--- common/main/net_udp.h
+++ common/main/net_udp.h
@@ -166,7 +166,7 @@ struct UDP_mdata_store : prohibit_void_ptr<UDP_mdata_store>
 {
 	fix64				pkt_initial_timestamp;			// initial timestamp to see if packet is outdated
 	enumerated_array<fix64, MAX_PLAYERS, playernum_t>		pkt_timestamp;		// Packet timestamp
-	std::array<uint32_t, MAX_PLAYERS>	pkt_num;			// Packet number
+	enumerated_array<uint32_t, MAX_PLAYERS, playernum_t>	pkt_num;			// Packet number
 	sbyte				used;
 	ubyte				Player_num;				// sender of this packet
 	uint16_t			data_size;
