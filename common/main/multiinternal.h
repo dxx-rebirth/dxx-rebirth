f78c3989f9ce24c731c425ccbcd47bf5332681a8
Kp <kp@valhallalegends.com>
2022-07-23 20:58:10 +0000

    Remove obsolete multi_send_data overload
    All callers now supply a multi_command<>, so remove the pointer-based
    overload.

--- common/main/multiinternal.h
+++ common/main/multiinternal.h
@@ -135,22 +135,6 @@ struct multi_command : public std::array<uint8_t, command_length<C>::value>
 void _multi_send_data(const uint8_t *buf, unsigned len, multiplayer_data_priority priority);
 
 template <multiplayer_command_t C>
-static inline void multi_send_data(uint8_t *const buf, const unsigned len, const multiplayer_data_priority priority)
-{
-	buf[0] = C;
-	constexpr unsigned expected = command_length<C>::value;
-#ifdef DXX_CONSTANT_TRUE
-	if (DXX_CONSTANT_TRUE(len != expected))
-		DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_multi_send_data, "wrong packet size");
-#endif
-	if (len != expected)
-	{
-		Error("multi_send_data: Packet type %i length: %i, expected: %i\n", C, len, expected);
-	}
-	_multi_send_data(buf, len, priority);
-}
-
-template <multiplayer_command_t C>
 static inline void multi_send_data(const multi_command<C> &buf, const multiplayer_data_priority priority)
 {
 	_multi_send_data(buf.data(), buf.size(), priority);
