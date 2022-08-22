dxx-rebirth/dxx-rebirth

commit 8be306efa78878286af1574691d6fe13de39b39f

Use enum class for cockpit_mode_t

similar/main/game.cpp
@@ -218,7 +218,7 @@ void init_stereo()
 		}
 		VR_eye_width = (F1_0 * 7) / 10;	// Descent 1.5 defaults
 		VR_sync_width = (20 * SHEIGHT) / 480;
-		PlayerCfg.CockpitMode[1] = CM_FULL_SCREEN;
+		PlayerCfg.CockpitMode[1] = cockpit_mode_t::full_screen;
 	}
 	else {
 		VR_stereo = StereoFormat::None;
@@ -236,7 +236,7 @@ void init_cockpit()
 		return;
 
 	if ( Screen_mode == SCREEN_EDITOR )
-		PlayerCfg.CockpitMode[1] = CM_FULL_SCREEN;
+		PlayerCfg.CockpitMode[1] = cockpit_mode_t::full_screen;
 
 #if !DXX_USE_OGL
 	if (PlayerCfg.CockpitMode[1] != CM_LETTERBOX)
@@ -255,26 +255,30 @@ void init_cockpit()
 	auto &canvas = *grd_curcanv;
 
 	switch( PlayerCfg.CockpitMode[1] ) {
-		case CM_FULL_COCKPIT:
+		case cockpit_mode_t::full_cockpit:
 			game_init_render_sub_buffers(canvas, 0, 0, SWIDTH, (SHEIGHT*2)/3);
 			break;
 
-		case CM_REAR_VIEW:
+		case cockpit_mode_t::rear_view:
 		{
 			unsigned x1 = 0, y1 = 0, x2 = SWIDTH, y2 = (SHEIGHT*2)/3;
-			int mode = PlayerCfg.CockpitMode[1];
+			const auto mode =
 #if defined(DXX_BUILD_DESCENT_II)
-			mode += (HIRESMODE?(Num_cockpits/2):0);
+				HIRESMODE
+				? static_cast<cockpit_mode_t>(static_cast<uint8_t>(cockpit_mode_t::rear_view) + (Num_cockpits / 2))
+				:
 #endif
+				cockpit_mode_t::rear_view;
 
-			PIGGY_PAGE_IN(cockpit_bitmap[mode]);
-			auto &bm = GameBitmaps[cockpit_bitmap[mode].index];
+			auto &cb = cockpit_bitmap[mode];
+			PIGGY_PAGE_IN(cb);
+			auto &bm = GameBitmaps[cb.index];
 			gr_bitblt_find_transparent_area(bm, x1, y1, x2, y2);
 			game_init_render_sub_buffers(canvas, x1 * (static_cast<float>(SWIDTH) / bm.bm_w), y1 * (static_cast<float>(SHEIGHT) / bm.bm_h), (x2 - x1 + 1) * (static_cast<float>(SWIDTH) / bm.bm_w), (y2 - y1 + 2) * (static_cast<float>(SHEIGHT) / bm.bm_h));
 			break;
 		}
 
-		case CM_FULL_SCREEN:
+		case cockpit_mode_t::full_screen:
 			{
 				unsigned w = SWIDTH;
 				unsigned h = SHEIGHT;
@@ -305,11 +309,11 @@ void init_cockpit()
 			}
 			break;
 
-		case CM_STATUS_BAR:
+		case cockpit_mode_t::status_bar:
 			game_init_render_sub_buffers(canvas, 0, 0, SWIDTH, (HIRESMODE?(SHEIGHT*2)/2.6:(SHEIGHT*2)/2.72));
 			break;
 
-		case CM_LETTERBOX:	{
+		case cockpit_mode_t::letterbox:	{
 			const unsigned gsm_height = grd_curscreen->get_screen_height();
 			const unsigned w = grd_curscreen->get_screen_width();
 			const unsigned h = (gsm_height * 3) / 4; // true letterbox size (16:9)
@@ -332,10 +336,10 @@ void init_cockpit()
 }
 
 //selects a given cockpit (or lack of one).  See types in game.h
-void select_cockpit(cockpit_mode_t mode)
+void select_cockpit(const cockpit_mode_t mode)
 {
 	// skip switching cockpit views while stereo viewport active
-	if (VR_stereo != StereoFormat::None && mode != CM_FULL_SCREEN)
+	if (VR_stereo != StereoFormat::None && mode != cockpit_mode_t::full_screen)
 		return;
 
 	if (mode != PlayerCfg.CockpitMode[1]) {		//new mode
@@ -350,7 +354,7 @@ namespace dcx {
 void reset_cockpit()
 {
 	force_cockpit_redraw=1;
-	last_drawn_cockpit = -1;
+	last_drawn_cockpit = cockpit_mode_t{UINT8_MAX};
 }
 
 void game_init_render_sub_buffers(grs_canvas &canvas, const int x, const int y, const int w, const int h)
@@ -1591,7 +1595,7 @@ static leave_type leave_mode;
 static void end_rear_view()
 {
 	Rear_view = 0;
-	if (PlayerCfg.CockpitMode[1] == CM_REAR_VIEW)
+	if (PlayerCfg.CockpitMode[1] == cockpit_mode_t::rear_view)
 		select_cockpit(PlayerCfg.CockpitMode[0]);
 	if (Newdemo_state == ND_STATE_RECORDING)
 		newdemo_record_restore_rearview();
@@ -1629,8 +1633,8 @@ void check_rear_view(control_info &Controls)
 				Rear_view = 1;
 				leave_mode = leave_type::maybe_on_release;		//means wait for another key
 				entry_time = timer_query();
-				if (PlayerCfg.CockpitMode[1] == CM_FULL_COCKPIT)
-					select_cockpit(CM_REAR_VIEW);
+				if (PlayerCfg.CockpitMode[1] == cockpit_mode_t::full_cockpit)
+					select_cockpit(cockpit_mode_t::rear_view);
 				if (Newdemo_state == ND_STATE_RECORDING)
 					newdemo_record_rearview();
 			}
@@ -1698,7 +1702,7 @@ game_window *game_setup()
 {
 
 	PlayerCfg.CockpitMode[1] = PlayerCfg.CockpitMode[0];
-	last_drawn_cockpit = -1;	// Force cockpit to redraw next time a frame renders.
+	last_drawn_cockpit = cockpit_mode_t{UINT8_MAX};	// Force cockpit to redraw next time a frame renders.
 	Endlevel_sequence = 0;
 
 	auto game_wind = window_create<game_window>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT);

common/main/bm.h
@@ -28,6 +28,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include <physfs.h>
 #include "maths.h"
 #include "fwd-vclip.h"
+#include "fwd-game.h"
 #include "d_array.h"
 #include "inferno.h"
 
@@ -116,7 +117,6 @@ extern unsigned Num_cockpits;
 }
 
 namespace dsx {
-extern std::array<bitmap_index, N_COCKPIT_BITMAPS> cockpit_bitmap;
 #if DXX_USE_EDITOR
 using tmap_xlate_table_array = std::array<short, MAX_TEXTURES>;
 extern tmap_xlate_table_array tmap_xlate_table;
@@ -190,6 +190,7 @@ extern int First_multi_bitmap_num;
 void compute_average_rgb(grs_bitmap *bm, std::array<fix, 3> &rgb);
 
 namespace dsx {
+extern enumerated_array<bitmap_index, N_COCKPIT_BITMAPS, cockpit_mode_t> cockpit_bitmap;
 void load_robot_replacements(const d_fname &level_name);
 // Initializes all bitmaps from BITMAPS.TBL file.
 int gamedata_read_tbl(d_vclip_array &Vclip, int pc_shareware);

common/main/fwd-game.h
@@ -58,6 +58,7 @@ extern int d_tick_step;  // true once in interval of DESIGNATED_GAME_FRAMETIME
 enum class gauge_inset_window_view : unsigned;
 enum class game_mode_flag : uint16_t;
 enum class game_mode_flags : uint16_t;
+enum class cockpit_mode_t : uint8_t;
 
 // The following bits define the game modes.
 #define GM_NETWORK		game_mode_flag::network       // You are in network mode
@@ -101,7 +102,7 @@ extern int  VR_sync_width;
 extern grs_subcanvas VR_hud_left;
 extern grs_subcanvas VR_hud_right;
 
-extern int last_drawn_cockpit;
+extern cockpit_mode_t last_drawn_cockpit;
 
 class pause_game_world_time;
 
@@ -255,7 +256,6 @@ extern int Slew_on;                 // in slew or sim mode?
 
 #endif
 
-enum cockpit_mode_t : int;
 extern int Rear_view;           // if true, looking back.
 
 // selects a given cockpit (or lack of one).

common/main/game.h
@@ -195,17 +195,17 @@ public:
 	~pause_game_world_time();
 };
 
-}
-
-enum cockpit_mode_t : int
+enum class cockpit_mode_t : uint8_t
 {
 //valid modes for cockpit
-	CM_FULL_COCKPIT,   // normal screen with cockpit
-	CM_REAR_VIEW,   // looking back with bitmap
-	CM_STATUS_BAR,   // small status bar, w/ reticle
-	CM_FULL_SCREEN,   // full screen, no cockpit (w/ reticle)
-	CM_LETTERBOX   // half-height window (for cutscenes)
+	full_cockpit,   // normal screen with cockpit
+	rear_view,   // looking back with bitmap
+	status_bar,   // small status bar, w/ reticle
+	full_screen,   // full screen, no cockpit (w/ reticle)
+	letterbox   // half-height window (for cutscenes)
 };
+
+}
 
 #ifdef dsx
 #if defined(DXX_BUILD_DESCENT_I)

common/main/playsave.h
@@ -83,9 +83,10 @@ struct hli
 	uint8_t LevelNum;
 };
 
-#if defined(DXX_BUILD_DESCENT_I) || defined(DXX_BUILD_DESCENT_II)
+#ifdef dsx
 #include "kconfig.h"
 #include "multi.h"
+#include "fwd-game.h"
 #include "fwd-weapon.h"
 #include "d_array.h"
 

similar/main/bm.cpp
@@ -94,7 +94,6 @@ static void bm_free_extra_objbitmaps();
 Textures_array Textures;		// All textures.
 //for each model, a model number for dying & dead variants, or -1 if none
 std::array<int, MAX_POLYGON_MODELS> Dying_modelnums, Dead_modelnums;
-std::array<bitmap_index, N_COCKPIT_BITMAPS> cockpit_bitmap;
 }
 
 //right now there's only one player ship, but we can have another by
@@ -115,6 +114,7 @@ int             First_multi_bitmap_num=-1;
 
 namespace dsx {
 
+enumerated_array<bitmap_index, N_COCKPIT_BITMAPS, cockpit_mode_t> cockpit_bitmap;
 enumerated_array<bitmap_index, MAX_OBJ_BITMAPS, object_bitmap_index> ObjBitmaps;
 std::array<object_bitmap_index, MAX_OBJ_BITMAPS> ObjBitmapPtrs;     // These point back into ObjBitmaps, since some are used twice.
 

similar/main/bmread.cpp
@@ -2097,8 +2097,9 @@ void bm_read_some_file(d_vclip_array &Vclip, int skip)
 	case bm_type::cockpit:	{
 		bitmap_index bitmap;
 		bitmap = bm_load_sub(skip, arg);
-		Assert( Num_cockpits < N_COCKPIT_BITMAPS );
-		cockpit_bitmap[Num_cockpits++] = bitmap;
+		if (Num_cockpits >= N_COCKPIT_BITMAPS)
+			throw std::runtime_error("too many cockpit bitmaps");
+		cockpit_bitmap[static_cast<cockpit_mode_t>(Num_cockpits++)] = bitmap;
 #if defined(DXX_BUILD_DESCENT_II)
 		return;
 #endif

similar/main/endlevel.cpp
@@ -954,7 +954,7 @@ window_event_result do_endlevel_frame()
 
 					Viewer = endlevel_camera = objnum;
 
-					select_cockpit(CM_LETTERBOX);
+					select_cockpit(cockpit_mode_t::letterbox);
 
 					fly_objects[1] = fly_objects[0];
 					fly_objects[1].obj = endlevel_camera;

commit 4e66bff3f6a6a09658c2e8b67bb5b8c8772e38e1

Fix SDL-only build of game.cpp
Fixes: 8be306efa78878286af1574691d6fe13de39b39f ("Use enum class for cockpit_mode_t")

similar/main/game.cpp
@@ -257,14 +257,14 @@ void init_cockpit()
 		PlayerCfg.CockpitMode[1] = cockpit_mode_t::full_screen;
 
 #if !DXX_USE_OGL
-	if (PlayerCfg.CockpitMode[1] != CM_LETTERBOX)
+	if (PlayerCfg.CockpitMode[1] != cockpit_mode_t::letterbox)
 	{
 #if defined(DXX_BUILD_DESCENT_II)
 		int HiresGFXAvailable = !GameArg.GfxSkipHiresGFX;
 #endif
 		auto full_screen_mode = HiresGFXAvailable ? initial_large_game_screen_mode : initial_small_game_screen_mode;
 		if (Game_screen_mode != full_screen_mode) {
-			PlayerCfg.CockpitMode[1] = CM_FULL_SCREEN;
+			PlayerCfg.CockpitMode[1] = cockpit_mode_t::full_screen;
 		}
 	}
 #endif