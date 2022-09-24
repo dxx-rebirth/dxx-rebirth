dxx-rebirth/dxx-rebirth

commit 0142c02edd15bb30785013266c82e5781aa19d11

Use Mix_LoadMUSType_RW for named files, too
When a file name is available, use SDL_RWFromFile + Mix_LoadMUSType_RW,
rather than calling Mix_LoadMUS(filename).  This allows the calling code
to be more consistent.

common/arch/sdl/digi_mixer_music.cpp
@@ -50,9 +50,31 @@ class current_music_t : std::unique_ptr<Mix_Music, Music_delete>
 	using music_pointer = std::unique_ptr<Mix_Music, Music_delete>;
 public:
 	using music_pointer::reset;
+	void reset(SDL_RWops *rw);
 	using music_pointer::operator bool;
 	using music_pointer::get;
 };
+
+void current_music_t::reset(SDL_RWops *const rw)
+{
+	if (!rw)
+	{
+		/* As a special case, exit early if rw is nullptr.  SDL is
+		 * guaranteed to fail in this case, but will set an error message
+		 * when it does so.  The error message about a nullptr SDL_RWops
+		 * will replace any prior error message, which might have been more
+		 * useful.
+		 */
+		reset();
+		return;
+	}
+	reset(Mix_LoadMUSType_RW(rw, MUS_NONE, SDL_TRUE));
+	if (!*this)
+		/* If the underlying resource failed to load, then SDL does not
+		 * free the RWops structure.  Free it here.
+		 */
+		SDL_RWclose(rw);
+}
 
 static current_music_t current_music;
 static std::vector<uint8_t> current_music_hndlbuf;
@@ -245,7 +267,7 @@ static CurrentMusicType load_mus_data(const uint8_t *data, size_t size, int loop
 #endif
 	{
 		const auto rw = SDL_RWFromConstMem(data, size);
-		current_music.reset(Mix_LoadMUSType_RW(rw, MUS_NONE, SDL_TRUE));
+		current_music.reset(rw);
 		if (current_music)
 		{
 			mix_set_music_type_sdlmixer(loop, hook_finished_track);
@@ -268,7 +290,8 @@ static CurrentMusicType load_mus_file(const char *filename, int loop, void (*con
 	else
 #endif
 	{
-		current_music.reset(Mix_LoadMUS(filename));
+		const auto rw = SDL_RWFromFile(filename, "rb");
+		current_music.reset(rw);
 		if (current_music)
 		{
 			mix_set_music_type_sdlmixer(loop, hook_finished_track);

commit 53d81e78de7606bb95f1896f529533b244238bf5

Fix SDL2 double-free on failure to load music
Fixes: 0142c02edd15bb30785013266c82e5781aa19d11 ("Use Mix_LoadMUSType_RW for named files, too")

common/arch/sdl/digi_mixer_music.cpp
@@ -32,12 +32,6 @@ namespace dcx {
 
 namespace {
 
-#if SDL_MIXER_MAJOR_VERSION == 2
-#define DXX_SDL_MIXER_MANAGES_RWOPS	1
-#else
-#define DXX_SDL_MIXER_MANAGES_RWOPS	0
-#endif
-
 struct Music_delete
 {
 	void operator()(Mix_Music *m) const
@@ -70,11 +64,19 @@ void current_music_t::reset(SDL_RWops *const rw)
 		return;
 	}
 	reset(Mix_LoadMUSType_RW(rw, MUS_NONE, SDL_TRUE));
-	if (!*this)
-		/* If the underlying resource failed to load, then SDL does not
-		 * free the RWops structure.  Free it here.
+	if constexpr (SDL_MIXER_MAJOR_VERSION == 1)
+	{
+		/* In SDL_mixer-1, setting freesrc==SDL_TRUE only transfers ownership
+		 * of the RWops structure on success.  On failure, the structure is
+		 * still owned by the caller, and must be freed here.
+		 *
+		 * In SDL_mixer-2, setting freesrc==SDL_TRUE always transfers ownership
+		 * of the RWops structure.  On failure, SDL_mixer-2 will free the RWops
+		 * before returning, so the structure must not be freed here.
 		 */
-		SDL_RWclose(rw);
+		if (!*this)
+			SDL_RWclose(rw);
+	}
 }
 
 static current_music_t current_music;