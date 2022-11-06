dxx-rebirth/dxx-rebirth

commit 4a8d7c7574ef0bcdfc5e478dd65b38a7abefcc56

Default to 1024x768 for new users, not 640x480
640x480 is too small to render some dialogs correctly, and is much
smaller than even a small laptop screen.  Smaller resolutions are still
supported, if the user chooses to switch.

similar/main/game.cpp
@@ -187,7 +187,9 @@ void reset_palette_add()
 	PaletteBlueAdd		= 0;
 }
 
-screen_mode Game_screen_mode{640, 480};
+constexpr screen_mode initial_small_game_screen_mode{320, 200};
+constexpr screen_mode initial_large_game_screen_mode{1024, 768};
+screen_mode Game_screen_mode = initial_large_game_screen_mode;
 
 }
 
@@ -211,7 +213,7 @@ void init_cockpit()
 #if defined(DXX_BUILD_DESCENT_II)
 		int HiresGFXAvailable = !GameArg.GfxSkipHiresGFX;
 #endif
-		auto full_screen_mode = HiresGFXAvailable ? screen_mode{640, 480} : screen_mode{320, 200};
+		auto full_screen_mode = HiresGFXAvailable ? initial_large_game_screen_mode : initial_small_game_screen_mode;
 		if (Game_screen_mode != full_screen_mode) {
 			PlayerCfg.CockpitMode[1] = CM_FULL_SCREEN;
 		}

similar/main/config.cpp
@@ -128,8 +128,8 @@ int ReadConfigFile()
 	CGameCfg.GammaLevel = 0;
 	GameCfg.LastPlayer = {};
 	CGameCfg.LastMission = "";
-	GameCfg.ResolutionX = 640;
-	GameCfg.ResolutionY = 480;
+	GameCfg.ResolutionX = 1024;
+	GameCfg.ResolutionY = 768;
 	GameCfg.AspectX = 3;
 	GameCfg.AspectY = 4;
 	CGameCfg.WindowMode = false;

commit 16c86edabc275b728fd94ff117b9f790594f7b04

Fix cockpit switch in SDL-only build
The cockpit graphic is not scaled in the SDL build, so it is only
meaningful to show when the game window dimensions match the graphic's
dimensions.  Prior to 4a8d7c7574ef, this worked, because the
`initial_large_game_screen_mode` was the same as the graphic's
dimensions.  That commit changed `initial_large_game_screen_mode` to
prefer a larger screen resolution for new users.  It should have
retained the original dimensions for this test.  Make that change here.

Fixes: 4a8d7c7574ef0bcdfc5e478dd65b38a7abefcc56 ("Default to 1024x768 for new users, not 640x480")

similar/main/game.cpp
@@ -197,9 +197,6 @@ void reset_palette_add()
 	PaletteBlueAdd		= 0;
 }
 
-#if !DXX_USE_OGL
-constexpr screen_mode initial_small_game_screen_mode{320, 200};
-#endif
 constexpr screen_mode initial_large_game_screen_mode{1024, 768};
 screen_mode Game_screen_mode = initial_large_game_screen_mode;
 
@@ -267,7 +264,9 @@ void init_cockpit()
 #if defined(DXX_BUILD_DESCENT_II)
 		int HiresGFXAvailable = !GameArg.GfxSkipHiresGFX;
 #endif
-		auto full_screen_mode = HiresGFXAvailable ? initial_large_game_screen_mode : initial_small_game_screen_mode;
+		const auto full_screen_mode = HiresGFXAvailable
+			? screen_mode /* large_game_screen_mode */{640, 480}
+			: screen_mode /* small_game_screen_mode */{320, 200};
 		if (Game_screen_mode != full_screen_mode) {
 			PlayerCfg.CockpitMode[1] = cockpit_mode_t::full_screen;
 		}