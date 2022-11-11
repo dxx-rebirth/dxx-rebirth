dxx-rebirth/dxx-rebirth

commit d2478d0708973088c3ffc12cf3029703b58622e5

Require support for C++17 attribute [[fallthrough]]

SConstruct
@@ -463,6 +463,20 @@ void f_%(N)s()
 	(void)b_%(N)s;
 	(void)c_%(N)s;
 '''),
+		Cxx17RequiredFeature('attribute [[fallthrough]]', '''
+static inline void f_%(N)s() {}
+''',
+'''
+	switch (argc)
+	{
+		case 1:
+			f_%(N)s();
+			[[fallthrough]];
+		case 2:
+			f_%(N)s();
+			break;
+	}
+'''),
 		Cxx14RequiredFeature('template variables', '''
 template <unsigned U_%(N)s>
 int a_%(N)s = U_%(N)s + 1;
@@ -2646,21 +2660,14 @@ where the cast is useless.
 	def check_warn_implicit_fallthrough(self,context,text,main,testflags,_successflags={'CXXFLAGS' : ['-Wimplicit-fallthrough=5']}):
 		self.Compile(context, text=text, main=main, msg='for -Wimplicit-fallthrough=5', testflags=testflags, successflags=_successflags)
 
-	@_implicit_test
-	def check_no_warn_implicit_fallthrough(self,context,_successflags={'CXXFLAGS' : ['-Wno-implicit-fallthrough']}):
-		self.Compile(context, text='', main='', msg='for -Wno-implicit-fallthrough', successflags=_successflags)
-
 	@_custom_test
-	def check_boost_config(self,context,_successflags={'CPPDEFINES' : [('DXX_BOOST_FALLTHROUGH', 'BOOST_FALLTHROUGH')]}):
-		text ='''
-#include <boost/config.hpp>
-'''
+	def check_warn_implicit_fallthrough(self,context,_successflags={'CXXFLAGS' : ['-Wimplicit-fallthrough=5']}):
 		main = '''
 	int a = 1;
 	switch (argc) {
 		case 1:
 			++ a;
-			DXX_BOOST_FALLTHROUGH;
+			[[fallthrough]];
 		case 2:
 			++ a;
 			break;
@@ -2668,18 +2675,7 @@ where the cast is useless.
 	(void)a;
 '''
 		sconf = context.sconf
-		if not self.Compile(context, text=text, main=main, msg='for Boost.Config', successflags=_successflags):
-			sconf.Define('DXX_BOOST_FALLTHROUGH', '((void)0)')
-			self.check_no_warn_implicit_fallthrough(context)
-			return
-		else:
-			sconf.config_h_text += '''
-#ifndef DXX_SCONF_NO_INCLUDES
-/* For BOOST_FALLTHROUGH */
-#include <boost/config.hpp>
-#endif
-'''
-		self.check_warn_implicit_fallthrough(context, text, main, testflags=_successflags)
+		self.Compile(context, text='', main=main, msg='whether compiler accepts -Wimplicit-fallthrough=5', successflags=_successflags)
 
 	@_custom_test
 	def check_compiler_overzealous_unused_lambda_capture(self,context):

common/texmap/ntmap.cpp
@@ -849,7 +849,7 @@ void draw_tmap(grs_canvas &canvas, const grs_bitmap &rbp, uint_fast32_t nverts, 
 				}
 				else
 				{
-					DXX_BOOST_FALLTHROUGH;
+					[[fallthrough]];
 				case 2:								// perspective every 8th pixel interpolation
 				case 3:								// perspective every pixel interpolation
 					ntexture_map_lighted(*bp, Tmap1);

common/ui/dialog.cpp
@@ -74,7 +74,7 @@ window_event_result UI_DIALOG::event_handler(const d_event &event)
 	{
 		case EVENT_IDLE:
 			timer_delay2(50);
-			DXX_BOOST_FALLTHROUGH;
+			[[fallthrough]];
 		case EVENT_MOUSE_BUTTON_DOWN:
 		case EVENT_MOUSE_BUTTON_UP:
 		case EVENT_MOUSE_MOVED:

d2x-rebirth/libmve/decoder8.cpp
@@ -240,7 +240,7 @@ static void dispatchDecoder(unsigned char **pFrame, unsigned char codeType, cons
 	case 0x0:
 		/* block is copied from block in current frame */
 		copyFrame(*pFrame, *pFrame + (g_vBackBuf2 - g_vBackBuf1));
-		DXX_BOOST_FALLTHROUGH;
+		[[fallthrough]];
 	case 0x1:
 		/* block is unchanged from two frames ago */
 		*pFrame += 8;

d2x-rebirth/main/escort.cpp
@@ -957,7 +957,7 @@ static void escort_create_path_to_goal(const vmobjptridx_t objp, const player_in
 				Int3();	//	Oops, Illegal value in Escort_goal_object.
 				con_printf(CON_URGENT, "BUG: buddy goal is %.8x, resetting to SCRAM", Escort_goal_object);
 				BuddyState.Escort_goal_object = ESCORT_GOAL_SCRAM;
-				DXX_BOOST_FALLTHROUGH;
+				[[fallthrough]];
 			case ESCORT_GOAL_SCRAM:
 				set_escort_goal_non_object(BuddyState);
 				create_n_segment_path(objp, 16 + d_rand() * 16, segment_none);
@@ -1659,7 +1659,7 @@ static int maybe_steal_primary_weapon(const vmobjptr_t playerobjp, const primary
 			if (!player_info.vulcan_ammo)
 				return 0;
 			is_energy_weapon = false;
-			DXX_BOOST_FALLTHROUGH;
+			[[fallthrough]];
 		default:
 			if (!(player_info.primary_weapon_flags & HAS_PRIMARY_FLAG(weapon_num)))
 				return 0;

d2x-rebirth/main/movie.cpp
@@ -271,7 +271,7 @@ window_event_result movie_pause_window::event_handler(const d_event &event)
 		case EVENT_MOUSE_BUTTON_DOWN:
 			if (event_mouse_get_button(event) != 0)
 				return window_event_result::ignored;
-			DXX_BOOST_FALLTHROUGH;
+			[[fallthrough]];
 		case EVENT_KEY_COMMAND:
 			if (const auto result = call_default_handler(event); result == window_event_result::ignored)
 				return window_event_result::close;

similar/arch/ogl/ogl.cpp
@@ -1340,7 +1340,7 @@ void ogl_stereo_frame(const bool left_eye, const int xoff)
 					// center unsqueezed side-by-side format
 				case StereoFormat::SideBySideHalfHeight:
 					ogl_stereo_viewport[1] -= ogl_stereo_viewport[3] / 2;		// y = h/4
-					DXX_BOOST_FALLTHROUGH;
+					[[fallthrough]];
 					// half-width viewports for side-by-side format
 				case StereoFormat::SideBySideFullHeight:
 					ogl_stereo_viewport[0] += ogl_stereo_viewport[2];		// x = w/2
@@ -1471,7 +1471,7 @@ static void ogl_filltexbuf(const palette_array_t &pal, const uint8_t *const data
 					case GL_RGBA:
 						(*(texp++)) = 255;
 						(*(texp++)) = 255;
-						DXX_BOOST_FALLTHROUGH;
+						[[fallthrough]];
 					case GL_LUMINANCE_ALPHA:
 						(*(texp++)) = 255;
 						(*(texp++)) = 0; // transparent pixel
@@ -1492,13 +1492,13 @@ static void ogl_filltexbuf(const palette_array_t &pal, const uint8_t *const data
 				{
 					case GL_RGBA:
 						(*(texp++))=0;
-						DXX_BOOST_FALLTHROUGH;
+						[[fallthrough]];
 					case GL_RGB:
 						(*(texp++))=0;
-						DXX_BOOST_FALLTHROUGH;
+						[[fallthrough]];
 					case GL_LUMINANCE_ALPHA:
 						(*(texp++))=0;
-						DXX_BOOST_FALLTHROUGH;
+						[[fallthrough]];
 					case GL_LUMINANCE:
 						(*(texp++))=0;//transparent pixel
 						break;
@@ -1518,7 +1518,7 @@ static void ogl_filltexbuf(const palette_array_t &pal, const uint8_t *const data
 				{
 					case GL_LUMINANCE_ALPHA:
 						(*(texp++))=255;
-						DXX_BOOST_FALLTHROUGH;
+						[[fallthrough]];
 					case GL_LUMINANCE://these could prolly be done to make the intensity based upon the intensity of the resulting color, but its not needed for anything (yet?) so no point. :)
 						(*(texp++))=255;
 						break;

similar/editor/med.cpp
@@ -1063,7 +1063,7 @@ window_event_result editor_dialog::callback_handler(const d_event &event)
 			case EVENT_MOUSE_MOVED:
 				if (!keyd_pressed[ KEY_LCTRL ] && !keyd_pressed[ KEY_RCTRL ])
 					break;
-				DXX_BOOST_FALLTHROUGH;
+				[[fallthrough]];
 #if DXX_MAX_BUTTONS_PER_JOYSTICK
 			case EVENT_JOYSTICK_BUTTON_UP:
 			case EVENT_JOYSTICK_BUTTON_DOWN:

commit 1e836729f5fe69fa4f2078775c3df4b156530834

Remove obsolete test for -Wimplicit-fallthrough
Commit d2478d070897 made support for C++17 fallthrough mandatory, and
should have deleted this test.  Delete it now.

Fixes: d2478d0708973088c3ffc12cf3029703b58622e5 ("Require support for C++17 attribute [[fallthrough]]")

SConstruct
@@ -2541,10 +2541,6 @@ where the cast is useless.
 	return 0;
 ''', msg='for struct timespec', successflags=_successflags)
 
-	@_implicit_test
-	def check_warn_implicit_fallthrough(self,context,text,main,testflags,_successflags={'CXXFLAGS' : ['-Wimplicit-fallthrough=5']}):
-		self.Compile(context, text=text, main=main, msg='for -Wimplicit-fallthrough=5', testflags=testflags, successflags=_successflags)
-
 	@_custom_test
 	def check_warn_implicit_fallthrough(self,context,_successflags={'CXXFLAGS' : ['-Wimplicit-fallthrough=5']}):
 		main = '''