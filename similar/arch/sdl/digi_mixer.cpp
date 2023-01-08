dxx-rebirth/dxx-rebirth

commit 5165efbc46b9f31a4f36b935322387e86955894a

Use custom fixed-point audio resampler: - Hopefully fixes issues with poor quality resampling when using SDL_AudioCVT - Converts 11025 Hz or 22050 Hz samples to the default 44100 Hz outputs - Uses high order brick wall filter like Sound Blaster 16

similar/arch/sdl/digi_mixer.cpp
@@ -153,6 +153,123 @@ void digi_mixer_close() {
 namespace dsx {
 
 /*
+ * Blackman windowed-sinc filter coefficients at 1/4 bandwidth of upsampled
+ * frequency. Chosen for linear phase and approximates ~10th order IIR
+ *
+ * MATLAB/Octave code:
+
+	N = 51;   % Num coeffs (odd)
+	B = 0.25;  % 1/4 band
+	half = (N-1)/2;
+	n = (-half:half); % the sample index
+	% Windowed sinc
+	h_ideal = 2 * B .* sinc(B*n);
+	h_win = blackman(N);
+	b = h_win .* h_ideal.';
+	% Convert to fix point to ultimately apply to signed 16-bit data
+	b_s16 = int32(round(b * (2^16 -1)));  % coeffs!
+
+ */
+#define FILTER_LEN 51
+static int32_t coeffs_quarterband[FILTER_LEN] =
+{
+		0, 0, -7, -25, -35, 0, 94, 200, 205, 0, -395, -751, -702, 0, 1178, 2127,
+		1907, 0, -3050, -5490, -5011, 0, 9275, 20326, 29311, 32767, 29311,
+		20326, 9275, 0, -5011, -5490, -3050, 0, 1907, 2127, 1178, 0, -702,
+		-751, -395, 0, 205, 200, 94, 0, -35, -25, -7, 0, 0
+};
+
+// Coefficient set for half-band (e.g. 22050 -> 44100)
+static int32_t coeffs_halfband[FILTER_LEN] =
+{
+		0, 0, -11, 0, 49, 0, -133, 0, 290, 0, -558, 0, 992, 0, -1666, 0, 2697, 0,
+		-4313, 0, 7086, 0, -13117, 0, 41452, 65535, 41452, 0, -13117, 0, 7086, 0,
+		-4313, 0, 2697, 0, -1666, 0, 992, 0, -558, 0, 290, 0, -133, 0, 49, 0, -11,
+		0, 0
+};
+
+// Fixed-point FIR filtering
+// Not optimal: consider optimization with 1/4, 1/2 band filters, and symmetric kernels
+static void filter_fir(int16_t *signal, int16_t *output, int signalLen, int32_t *coeffs, int coeffsLen)
+{
+	// A FIR filter is just a 1D convolution
+	// Keep only signalLen samples
+	for(int nn = 0; nn < signalLen; nn++)
+	{
+		// Determin start/stop indices for convolved chunk
+		int min_idx = std::max(0, nn-coeffsLen+1);
+		int max_idx = std::min(nn, signalLen-1);
+
+		int32_t cur_output = 0;  // Increase bit size for fixed point expansion
+		// Sum over each sample * coefficient in this column
+		for(int kk = min_idx; kk <= max_idx; kk++)
+		{
+			int product = int32_t(signal[kk]) * coeffs[nn-kk];
+			cur_output = cur_output + product;
+		}
+
+		// Save and fit back into int16
+		output[nn] = int16_t(cur_output >> 16);  // Arithmetic shift
+	}
+}
+
+
+static void upsample(uint8_t *input, int16_t *output, int inputLen, int factor)
+{
+	for(int ii = 0; ii < inputLen; ii++)
+	{
+		// Save input sample, convert to signed, and scale
+		output[ii*factor] = 256 * (int16_t(input[ii]) - 128);
+
+		// Pad the rest with zeros
+		for(int jj = 1; jj < factor; jj++)
+		{
+			int idx = (ii*factor) + jj;
+			output[idx] = 0;
+		}
+	}
+}
+
+
+static void replicateChannel(int16_t *input, int16_t *output, int inputLen, int chFactor)
+{
+	for(int ii = 0; ii < inputLen; ii++)
+	{
+		// Duplicate and interleave as many channels as needed
+		for(int jj = 0; jj < chFactor; jj++)
+		{
+			int idx = (ii*chFactor) + jj;
+			output[idx] = input[ii];
+		}
+	}
+}
+
+
+static void convert_audio(uint8_t *input, int16_t *output, int inputLen, int upFactor, int chFactor)
+{
+	// First upsample
+	int upsampledLen = inputLen*upFactor;
+
+	auto stage1 = std::make_unique<int16_t[]>(upsampledLen);
+	auto stage2 = std::make_unique<int16_t[]>(upsampledLen);
+
+	upsample(input, stage1.get(), inputLen, upFactor);
+
+	// We expect a 4x upscaling 11025 -> 44100
+	int32_t *coeffs = &coeffs_quarterband[0];
+	if(upFactor )  // But maybe 2x for d2x in some cases
+		coeffs = &coeffs_halfband[0];
+
+	// Apply LPF filter to smooth out upscaled points
+	// There will be some uniform amplitude loss here, but less than -3dB
+	filter_fir(stage1.get(), stage2.get(), upsampledLen, coeffs, FILTER_LEN);
+
+	// Replicate channel
+	replicateChannel(stage2.get(), output, upsampledLen, chFactor);
+}
+
+
+/*
  * Play-time conversion. Performs output conversion only once per sound effect used.
  * Once the sound sample has been converted, it is cached in SoundChunks[]
  */
@@ -161,43 +278,37 @@ static void mixdigi_convert_sound(const unsigned i)
 	if (SoundChunks[i].abuf)
 		//proceed only if not converted yet
 		return;
-	SDL_AudioCVT cvt;
+
 	Uint8 *data = GameSounds[i].data;
 	Uint32 dlen = GameSounds[i].length;
 	int freq;
 	int out_freq;
-	Uint16 out_format;
 	int out_channels;
 #if defined(DXX_BUILD_DESCENT_I)
 	out_freq = digi_sample_rate;
-	out_format = MIX_OUTPUT_FORMAT;
+	//	out_format = MIX_OUTPUT_FORMAT;
 	out_channels = MIX_OUTPUT_CHANNELS;
 	freq = GameSounds[i].freq;
 #elif defined(DXX_BUILD_DESCENT_II)
+	Uint16 out_format;
 	Mix_QuerySpec(&out_freq, &out_format, &out_channels); // get current output settings
 	freq = GameArg.SndDigiSampleRate;
 #endif
 
 	if (data)
 	{
-		if (SDL_BuildAudioCVT(&cvt, AUDIO_U8, 1, freq, out_format, out_channels, out_freq) == -1)
-		{
-			con_printf(CON_URGENT, "%s:%u: SDL_BuildAudioCVT failed: sound=%i dlen=%u freq=%i out_format=%i out_channels=%i out_freq=%i", __FILE__, __LINE__, i, dlen, freq, out_format, out_channels, out_freq);
-			return;
-		}
+		// Create output memory
+		int upFactor = out_freq / freq;  // Should be integer, 2 or 4
+		int formatFactor = 2;  // U8 -> S16 is two bytes
+		int convertedSize = dlen * upFactor * out_channels * formatFactor;
 
-		auto cvtbuf = std::make_unique<Uint8[]>(dlen * cvt.len_mult);
-		cvt.buf = cvtbuf.get();
-		cvt.len = dlen;
-		memcpy(cvt.buf, data, dlen);
-		if (SDL_ConvertAudio(&cvt))
-		{
-			con_printf(CON_URGENT, "%s:%u: SDL_ConvertAudio failed: sound=%i dlen=%u freq=%i out_format=%i out_channels=%i out_freq=%i", __FILE__, __LINE__, i, dlen, freq, out_format, out_channels, out_freq);
-			return;
-		}
+		auto cvtbuf = std::make_unique<Uint8[]>(convertedSize);
+		uint8_t *buf = cvtbuf.release();
 
-		SoundChunks[i].abuf = cvtbuf.release();
-		SoundChunks[i].alen = cvt.len_cvt;
+		convert_audio(data, reinterpret_cast<int16_t*>(buf), dlen, upFactor, out_channels);
+
+		SoundChunks[i].abuf = buf;
+		SoundChunks[i].alen = convertedSize;
 		SoundChunks[i].allocated = 1;
 		SoundChunks[i].volume = 128; // Max volume = 128
 	}

commit 9fd0cc96a58b5d5dc92e91ae375d0e9b025b4f17

Use correct coefficient table for 11Khz sounds
The new resampler sets upFactor to either `2` (for 22Khz sounds) or
`4` (for 11Khz sounds).  However, the logic to pick which coefficient
table to use tested for `0` versus non-zero, so it always picked the
coefficient table meant for 22Khz sounds, even when the sample was an
11Khz sound.  This caused a high pitched ringing in sampled sounds.
Switch to use an `enum class` to prevent using zero-vs-nonzero tests
without a cast.  Change the existing test to test for the 22Khz `enum`
value, as originally intended.  In testing, this eliminated the ringing
effect in 11Khz sounds.

Identified-by: Chris <https://forum.zdoom.org/viewtopic.php?p=1234989> (in response to a request by KynikossDragonn)
Fixes: 5165efbc46b9f31a4f36b935322387e86955894a ("Use custom fixed-point audio resampler: - Hopefully fixes issues with poor quality resampling when using SDL_AudioCVT - Converts 11025 Hz or 22050 Hz samples to the default 44100 Hz outputs - Uses high order brick wall filter like Sound Blaster 16")

similar/arch/sdl/digi_mixer.cpp
@@ -38,6 +38,7 @@
 #include "piggy.h"
 #include "u_mem.h"
 #include <memory>
+#include "compiler-cf_assert.h"
 #include "d_bitset.h"
 #include "d_range.h"
 #include "d_underlying_value.h"
@@ -166,6 +167,17 @@ void digi_mixer_close() {
 namespace {
 
 #if DXX_FEATURE_INTERNAL_RESAMPLER
+
+enum class upscale_factor : uint8_t
+{
+	/* The enum values must be the ratio of the destination divided by the
+	 * source, because the numeric value is used to compute how much buffer
+	 * space to allocate.  Do not renumber these constants.
+	 */
+	from_22khz_to_44khz = 2,
+	from_11khz_to_44khz = 4,
+};
+
 /*
  * Blackman windowed-sinc filter coefficients at 1/4 bandwidth of upsampled
  * frequency. Chosen for linear phase and approximates ~10th order IIR
@@ -235,8 +247,18 @@ static auto filter_fir(const unique_span<int8_t> signal_storage, const std::span
 	return result;
 }
 
-static auto upsample(const std::span<const uint8_t> input, const std::size_t upsampledLen, const std::size_t factor)
+static auto upsample(const std::span<const uint8_t> input, const upscale_factor upFactor)
 {
+	const std::size_t factor = underlying_value(upFactor);
+	switch (upFactor)
+	{
+		case upscale_factor::from_11khz_to_44khz:
+		case upscale_factor::from_22khz_to_44khz:
+			break;
+		default:
+			cf_assert(false);
+	}
+	const auto upsampledLen = input.size() * factor;
 	/* Caution: `output` is sparsely initialized, so the value-initialization
 	 * from `make_unique` is necessary.  This site cannot be converted to
 	 * `make_unique_for_overwrite`.
@@ -272,19 +294,20 @@ static auto replicateChannel(const unique_span<int16_t> input_storage, const std
 	return result;
 }
 
-static std::unique_ptr<Uint8[]> convert_audio(const std::span<const uint8_t> input, const std::size_t outsize, const int upFactor, const std::size_t chFactor)
+static std::unique_ptr<Uint8[]> convert_audio(const std::span<const uint8_t> input, const std::size_t outsize, const upscale_factor upFactor, const std::size_t chFactor)
 {
-	const auto upsampledLen = input.size() * upFactor;
-
 	// We expect a 4x upscaling 11025 -> 44100
 	// But maybe 2x for d2x in some cases
-	auto &coeffs = upFactor ? coeffs_halfband : coeffs_quarterband;
+	auto &coeffs = (upFactor == upscale_factor::from_22khz_to_44khz)
+		? coeffs_halfband
+		/* Otherwise, assume upscale_factor::from_11khz_to_44khz */
+		: coeffs_quarterband;
 
 	return replicateChannel(
 	// First upsample
 	// Apply LPF filter to smooth out upscaled points
 	// There will be some uniform amplitude loss here, but less than -3dB
-		filter_fir(upsample(input, upsampledLen, upFactor), coeffs),
+		filter_fir(upsample(input, upFactor), coeffs),
 		outsize, chFactor);
 }
 #endif
@@ -327,10 +350,23 @@ static void mixdigi_convert_sound(const unsigned i)
 
 	{
 #if DXX_FEATURE_INTERNAL_RESAMPLER
+		/* Only a small set of conversions are supported.  List them out
+		 * explicitly instead of using division.  This also allows the
+		 * conversion factor to be an `enum class`, which emphasizes its
+		 * limited legal values.
+		 */
+		if (out_freq != underlying_value(sound_sample_rate::_44k))
+			return;
+		upscale_factor upFactor;
+		if (freq == underlying_value(sound_sample_rate::_11k))
+			upFactor = upscale_factor::from_11khz_to_44khz;
+		else if (freq == underlying_value(sound_sample_rate::_22k))
+			upFactor = upscale_factor::from_22khz_to_44khz;
+		else
+			return;
 		// Create output memory
-		int upFactor = out_freq / freq;  // Should be integer, 2 or 4
 		int formatFactor = 2;  // U8 -> S16 is two bytes
-		int convertedSize = data.size() * upFactor * out_channels * formatFactor;
+		const std::size_t convertedSize = data.size() * underlying_value(upFactor) * out_channels * formatFactor;
 
 		auto cvtbuf = convert_audio(data, convertedSize, upFactor, out_channels);
 #else