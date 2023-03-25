dxx-rebirth/dxx-rebirth

commit 2ded2b2d0c31c6d1762b3a5107bb134675edad75

Make kmatrix background optional
Previously, if the background failed to load, the kmatrix window was
immediately aborted.  Change the logic so that a failure to load uses a
blank black background, but still shows the kmatrix window.

similar/2d/pcx.cpp
@@ -132,10 +132,10 @@ static pcx_result pcx_read_bitmap(const char *const filename, grs_main_bitmap &b
 	}
 	return pcx_result::SUCCESS;
 }
-#else
-static pcx_result pcx_read_blank(const char *const filename, grs_main_bitmap &bmp, palette_array_t &palette)
+#endif
+
+static pcx_result pcx_read_blank(grs_main_bitmap &bmp, palette_array_t &palette)
 {
-	con_printf(CON_NORMAL, "%s:%u: PCX support disabled at compile time; cannot read file \"%s\"", __FILE__, __LINE__, filename);
 	constexpr unsigned xsize = 640;
 	constexpr unsigned ysize = 480;
 	gr_init_bitmap_alloc(bmp, bm_mode::linear, 0, 0, xsize, ysize, xsize);
@@ -155,6 +155,13 @@ static pcx_result pcx_read_blank(const char *const filename, grs_main_bitmap &bm
 	palette = {};
 	palette[border] = {63, 63, 63};
 	return pcx_result::SUCCESS;
+}
+
+#if !DXX_USE_SDLIMAGE
+static pcx_result pcx_support_not_compiled(const char *const filename, grs_main_bitmap &bmp, palette_array_t &palette)
+{
+	con_printf(CON_NORMAL, "%s:%u: PCX support disabled at compile time; cannot read file \"%s\"", __FILE__, __LINE__, filename);
+	pcx_read_blank(bmp, palette);
 }
 #endif
 
@@ -220,7 +227,7 @@ pcx_result bald_guy_load(const char *const filename, grs_main_bitmap &bmp, palet
 	RWops_ptr rw(SDL_RWFromConstMem(bguy_data.get(), data_size));
 	return pcx_read_bitmap(filename, bmp, palette, std::move(rw));
 #else
-	return pcx_read_blank(filename, bmp, palette);
+	return pcx_support_not_compiled(filename, bmp, palette);
 #endif
 }
 
@@ -245,8 +252,16 @@ pcx_result pcx_read_bitmap(const char *const filename, grs_main_bitmap &bmp, pal
 	}
 	return pcx_read_bitmap(filename, bmp, palette, std::move(rw));
 #else
-	return pcx_read_blank(filename, bmp, palette);
+	return pcx_support_not_compiled(filename, bmp, palette);
 #endif
+}
+
+pcx_result pcx_read_bitmap_or_default(const char *const filename, grs_main_bitmap &bmp, palette_array_t &palette)
+{
+	const auto r = pcx_read_bitmap(filename, bmp, palette);
+	if (r != pcx_result::SUCCESS)
+		pcx_read_blank(bmp, palette);
+	return r;
 }
 
 #if !DXX_USE_OGL && DXX_USE_SCREENSHOT_FORMAT_LEGACY

common/include/pcx.h
@@ -52,6 +52,7 @@ enum class pcx_result
 // If palette==NULL the palette isn't read in.  Returns error code.
 
 pcx_result pcx_read_bitmap(const char * filename, grs_main_bitmap &bmp, palette_array_t &palette);
+pcx_result pcx_read_bitmap_or_default(const char * filename, grs_main_bitmap &bmp, palette_array_t &palette);
 
 // Writes the bitmap bmp to filename, using palette. Returns error code.
 

similar/main/automap.cpp
@@ -758,8 +758,7 @@ static void draw_automap(fvcobjptr &vcobjptr, automap &am)
 	gr_set_default_canvas();
 	{
 		auto &canvas = *grd_curcanv;
-		if (am.automap_background.get_bitmap_data())
-			show_fullscr(canvas, am.automap_background);
+		show_fullscr(canvas, am.automap_background);
 		gr_set_fontcolor(canvas, BM_XRGB(20, 20, 20), -1);
 	{
 		int x, y;
@@ -1219,11 +1218,10 @@ void do_automap()
 	// ZICO - code from above to show frame in OGL correctly. Redundant, but better readable.
 	// KREATOR - Now applies to all platforms so double buffering is supported
 	{
-		const auto pcx_error = pcx_read_bitmap(MAP_BACKGROUND_FILENAME, am->automap_background, pal);
+		const auto pcx_error = pcx_read_bitmap_or_default(MAP_BACKGROUND_FILENAME, am->automap_background, pal);
 		if (pcx_error != pcx_result::SUCCESS)
 			con_printf(CON_URGENT, DXX_STRINGIZE_FL(__FILE__, __LINE__, "automap: File %s - PCX error: %s"), MAP_BACKGROUND_FILENAME, pcx_errormsg(pcx_error));
-		else
-			gr_remap_bitmap_good(am->automap_background, pal, -1, -1);
+		gr_remap_bitmap_good(am->automap_background, pal, -1, -1);
 	}
 	init_automap_subcanvas(am->automap_view, grd_curscreen->sc_canvas);
 

similar/main/credits.cpp
@@ -264,12 +264,7 @@ static void credits_show_common(RAIIPHYSFS_File file, const int have_bin_file)
 	gr_use_palette_table( "credits.256" );
 #endif
 
-	const auto pcx_error = pcx_read_bitmap(STARS_BACKGROUND, cr->backdrop,backdrop_palette);
-	if (pcx_error != pcx_result::SUCCESS)
-	{
-		return;
-	}
-
+	pcx_read_bitmap_or_default(STARS_BACKGROUND, cr->backdrop, backdrop_palette);
 	songs_play_song( SONG_CREDITS, 1 );
 
 	gr_remap_bitmap_good(cr->backdrop,backdrop_palette, -1, -1);

similar/main/gameseq.cpp
@@ -1344,8 +1344,7 @@ static void do_screen_message(const char *msg)
 	if (Game_mode & GM_MULTI)
 		return;
 	grs_main_bitmap background;
-	if (pcx_read_bitmap(GLITZ_BACKGROUND, background, gr_palette) != pcx_result::SUCCESS)
-		return;
+	pcx_read_bitmap_or_default(GLITZ_BACKGROUND, background, gr_palette);
 
 	gr_palette_load(gr_palette);
 	std::array<newmenu_item, 1> nm_message_items{{

similar/main/kmatrix.cpp
@@ -440,10 +440,7 @@ kmatrix_result kmatrix_view(const kmatrix_network network, control_info &Control
 	{
 	const auto pkm = window_create<kmatrix_window>(grd_curscreen->sc_canvas, 0, 0, SWIDTH, SHEIGHT, result);
 	auto &km = *pkm;
-	if (pcx_read_bitmap(STARS_BACKGROUND, km.background, gr_palette) != pcx_result::SUCCESS)
-	{
-		return kmatrix_result::abort;
-	}
+	pcx_read_bitmap_or_default(STARS_BACKGROUND, km.background, gr_palette);
 	gr_palette_load(gr_palette);
 
 	km.network = network;

similar/main/newmenu.cpp
@@ -282,9 +282,7 @@ static void nm_draw_background1(grs_canvas &canvas, const char * filename)
 	{
 		if (nm_background1.bm_data == NULL)
 		{
-			const auto pcx_error = pcx_read_bitmap(filename, nm_background1, gr_palette);
-			if (pcx_error != pcx_result::SUCCESS)
-				return;
+			pcx_read_bitmap_or_default(filename, nm_background1, gr_palette);
 		}
 		gr_palette_load( gr_palette );
 		show_fullscr(canvas, nm_background1);
@@ -310,9 +308,7 @@ void nm_draw_background(grs_canvas &canvas, int x1, int y1, int x2, int y2)
 	if (nm_background.bm_data == NULL)
 	{
 		palette_array_t background_palette;
-		const auto pcx_error = pcx_read_bitmap(MENU_BACKGROUND_BITMAP, nm_background,background_palette);
-		if (pcx_error != pcx_result::SUCCESS)
-			return;
+		pcx_read_bitmap_or_default(MENU_BACKGROUND_BITMAP, nm_background, background_palette);
 		gr_remap_bitmap_good(nm_background, background_palette, -1, -1);
 		BGScaleX=(static_cast<float>(SWIDTH)/nm_background.bm_w);
 		BGScaleY=(static_cast<float>(SHEIGHT)/nm_background.bm_h);

similar/main/titles.cpp
@@ -183,11 +183,10 @@ static void show_title_screen(const char *filename)
 	filename = new_filename;
 
 	ts->timer = timer_query() + i2f(3);
-	const auto pcx_error = pcx_read_bitmap(filename, ts->title_bm, gr_palette);
+	const auto pcx_error = pcx_read_bitmap_or_default(filename, ts->title_bm, gr_palette);
 	if (pcx_error != pcx_result::SUCCESS)
 	{
 		con_printf(CON_URGENT, "%s:%u: error: loading briefing screen <%s> failed: PCX load error: %s (%u)", __FILE__, __LINE__, filename, pcx_errormsg(pcx_error), static_cast<unsigned>(pcx_error));
-		return;
 	}
 
 	gr_palette_load( gr_palette );
@@ -685,6 +684,7 @@ static void put_char_delay(const grs_font &cv_font, briefing *const br, const ch
 }
 
 static void init_spinning_robot(grs_canvas &canvas, briefing &br);
+__attribute_warn_unused_result
 static int load_briefing_screen(grs_canvas &, briefing *br, const char *fname);
 
 // Process a character for the briefing,
@@ -805,7 +805,8 @@ static int briefing_process_char(grs_canvas &canvas, briefing *const br)
 
 				if ((HIRESMODE && PHYSFSX_exists(fname2,1)) || !PHYSFSX_exists(fname,1))
 					strcpy(fname,fname2);
-				load_briefing_screen(*grd_curcanv, br, fname);
+				if (!load_briefing_screen(*grd_curcanv, br, fname))
+					return 1;
 			}
 
 #endif
@@ -837,7 +838,8 @@ static int briefing_process_char(grs_canvas &canvas, briefing *const br)
 			if (!br->got_z) {
 				Int3(); // Hey ryan!!!! You gotta load a screen before you start
 				// printing to it! You know, $Z !!!
-				load_briefing_screen(*grd_curcanv, br, HIRESMODE ? "end01b.pcx" : "end01.pcx");
+				if (!load_briefing_screen(*grd_curcanv, br, HIRESMODE ? "end01b.pcx" : "end01.pcx"))
+					return 1;
 			}
 
 			br->chattering = 0;
@@ -947,10 +949,12 @@ static int briefing_process_char(grs_canvas &canvas, briefing *const br)
 			if (br->text_y > br->screen->text_uly + br->screen->text_height) {
 #if defined(DXX_BUILD_DESCENT_I)
 				const auto descent_hog_size = PHYSFSX_fsize("descent.hog");
-				load_briefing_screen(*grd_curcanv, br, get_d1_briefing_screens(descent_hog_size)[br->cur_screen].bs_name);
+				auto &bs = get_d1_briefing_screens(descent_hog_size)[br->cur_screen];
 #elif defined(DXX_BUILD_DESCENT_II)
-				load_briefing_screen(*grd_curcanv, br, Briefing_screens[br->cur_screen].bs_name);
+				auto &bs = Briefing_screens[br->cur_screen];
 #endif
+				if (!load_briefing_screen(*grd_curcanv, br, bs.bs_name))
+					return 1;
 				br->text_x = br->screen->text_ulx;
 				br->text_y = br->screen->text_uly;
 			}
@@ -965,7 +969,8 @@ static int briefing_process_char(grs_canvas &canvas, briefing *const br)
 			LevelError("briefing wrote to screen without using $Z to load a screen; loading default.");
 			//Int3(); // Hey ryan!!!! You gotta load a screen before you start
 			// printing to it! You know, $Z !!!
-			load_briefing_screen(*grd_curcanv, br, HIRESMODE ? "end01b.pcx" : "end01.pcx");
+			if (!load_briefing_screen(*grd_curcanv, br, HIRESMODE ? "end01b.pcx" : "end01.pcx"))
+				return 1;
 		}
 #endif
 		put_char_delay(game_font, br, ch);
@@ -1204,12 +1209,15 @@ static void show_spinning_robot_frame(briefing *br, int robot_num)
 //-----------------------------------------------------------------------------
 #define KEY_DELAY_DEFAULT       ((F1_0*20)/1000)
 
-static void init_new_page(briefing *br)
+__attribute_warn_unused_result
+static int init_new_page(briefing *br)
 {
 	br->new_page = 0;
 	br->robot_num = -1;
 
-	load_briefing_screen(*grd_curcanv, br, br->background_name.data());
+	const auto r = load_briefing_screen(*grd_curcanv, br, br->background_name.data());
+	if (!r)
+		return r;
 	br->text_x = br->screen->text_ulx;
 	br->text_y = br->screen->text_uly;
 
@@ -1226,6 +1234,7 @@ static void init_new_page(briefing *br)
 
 	br->start_time = 0;
 	br->delay_count = KEY_DELAY_DEFAULT;
+	return r;
 }
 
 #if defined(DXX_BUILD_DESCENT_II)
@@ -1311,7 +1320,7 @@ static int load_briefing_screen(grs_canvas &canvas, briefing *const br, const ch
 		bald_guy_load("btexture.xxx", br->background, gr_palette) == pcx_result::SUCCESS)
 	{
 	}
-	else if ((pcx_error = pcx_read_bitmap(fname2, br->background, gr_palette)) != pcx_result::SUCCESS)
+	else if ((pcx_error = pcx_read_bitmap_or_default(fname2, br->background, gr_palette)) != pcx_result::SUCCESS)
 	{
 		con_printf(CON_URGENT, "%s:%u: error: loading briefing screen <%s> failed: PCX load error: %s (%u)", __FILE__, __LINE__, fname2, pcx_errormsg(pcx_error), static_cast<unsigned>(pcx_error));
 	}
@@ -1347,7 +1356,7 @@ static int load_briefing_screen(grs_canvas &canvas, briefing *const br, const ch
 	}
 
 	pcx_result pcx_error;
-	if ((pcx_error = pcx_read_bitmap(fname, br->background, gr_palette)) != pcx_result::SUCCESS)
+	if ((pcx_error = pcx_read_bitmap_or_default(fname, br->background, gr_palette)) != pcx_result::SUCCESS)
 	{
 		con_printf(CON_URGENT, "%s:%u: error: loading briefing screen <%s> failed: PCX load error: %s (%u)", __FILE__, __LINE__, fname, pcx_errormsg(pcx_error), static_cast<unsigned>(pcx_error));
 		return 0;
@@ -1393,6 +1402,7 @@ static void free_briefing_screen(briefing *br)
 		br->screen.reset();
 }
 
+__attribute_warn_unused_result
 static int new_briefing_screen(briefing *br, int first)
 {
 	br->new_screen = 0;
@@ -1520,7 +1530,10 @@ window_event_result briefing::event_handler(const d_event &event)
 					}
 				}
 				else if (this->new_page)
-					init_new_page(this);
+				{
+					if (!init_new_page(this))
+						return window_event_result::close;
+				}
 				else
 					this->delay_count = 0;
 				return window_event_result::handled;
@@ -1541,7 +1554,10 @@ window_event_result briefing::event_handler(const d_event &event)
 				}
 			}
 			else if (this->new_page)
-				init_new_page(this);
+			{
+				if (!init_new_page(this))
+					return window_event_result::close;
+			}
 			else
 				this->delay_count = 0;
 			return window_event_result::handled;
@@ -1575,7 +1591,10 @@ window_event_result briefing::event_handler(const d_event &event)
 						}
 					}
 					else if (this->new_page)
-						init_new_page(this);
+					{
+						if (!init_new_page(this))
+							return window_event_result::close;
+					}
 					break;
 			}
 			break;

commit 488278ddfeef3f4b6965d151fb9cf628abb09e1a

Fix build for sdlimage=0
Fixes: 2ded2b2d0c31c6d1762b3a5107bb134675edad75 ("Make kmatrix background optional")

similar/2d/pcx.cpp
@@ -145,7 +145,7 @@ static pcx_result pcx_read_blank(grs_main_bitmap &bmp, palette_array_t &palette)
 static pcx_result pcx_support_not_compiled(const char *const filename, grs_main_bitmap &bmp, palette_array_t &palette)
 {
 	con_printf(CON_NORMAL, "%s:%u: PCX support disabled at compile time; cannot read file \"%s\"", __FILE__, __LINE__, filename);
-	pcx_read_blank(bmp, palette);
+	return pcx_read_blank(bmp, palette);
 }
 #endif
 