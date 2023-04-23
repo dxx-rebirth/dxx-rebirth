dxx-rebirth/dxx-rebirth

commit b1c5307eb1f6e36500cae30cc92439d9602ceb21

Use array for palette_array_t

similar/2d/palette.cpp
@@ -31,6 +31,9 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "maths.h"
 #include "palette.h"
 
+#include "dxxsconf.h"
+#include "compiler-range_for.h"
+
 #define SQUARE(x) ((x)*(x))
 
 #define	MAX_COMPUTED_COLORS	32
@@ -52,26 +55,36 @@ int gr_palette_gamma_param = 0;
 
 void copy_bound_palette(palette_array_t &d, const palette_array_t &s)
 {
-	for (unsigned i=0; i < 768; i++ )
-	{
+	auto a = [](rgb_t c) {
 		const ubyte bound = 63;
-		d[i] = std::min(s[i], bound);
-	}
+		c.r = std::min(c.r, bound);
+		c.g = std::min(c.g, bound);
+		c.b = std::min(c.b, bound);
+		return c;
+	};
+	std::transform(s.begin(), s.end(), d.begin(), a);
 }
 
 void copy_diminish_palette(palette_array_t &palette, const ubyte *p)
 {
-	for (unsigned i = 0; i < 768; i++) {
-		palette[i] = *p;
-		palette[i] >>= 2;
-		p++;
+	for (unsigned i = 0; i < palette.size(); i++) {
+		palette[i].r = *p++ >> 2;
+		palette[i].g = *p++ >> 2;
+		palette[i].b = *p++ >> 2;
 	}
+}
+
+static void diminish_entry(rgb_t &c)
+{
+	c.r >>= 2;
+	c.g >>= 2;
+	c.b >>= 2; 
 }
 
 void diminish_palette(palette_array_t &palette)
 {
-	for (unsigned i=0; i < 768; i++ )
-		palette[i] >>= 2;
+	range_for (rgb_t &c, palette)
+		diminish_entry(c);
 }
 
 void gr_palette_set_gamma( int gamma )
@@ -95,7 +108,7 @@ int gr_palette_get_gamma()
 #if defined(DXX_BUILD_DESCENT_II)
 void gr_copy_palette(palette_array_t &gr_palette, const palette_array_t &pal)
 {
-	        memcpy(gr_palette, pal, sizeof(gr_palette));
+	gr_palette = pal;
 
 	        Num_computed_colors = 0;
 }
@@ -125,7 +138,7 @@ void gr_use_palette_table(const char * filename )
 	fsize	= PHYSFS_fileLength( fp );
 	Assert( fsize == 9472 );
 	(void)fsize;
-	PHYSFS_read( fp, gr_palette, 256*3, 1 );
+	PHYSFS_read( fp, &gr_palette[0], sizeof(gr_palette[0]), gr_palette.size() );
 	PHYSFS_read( fp, gr_fade_table, 256*34, 1 );
 	PHYSFS_close(fp);
 
@@ -209,7 +222,7 @@ int gr_find_closest_color( int r, int g, int b )
 //	g &= 63;
 //	b &= 63;
 
-	best_value = SQUARE(r-gr_palette[0])+SQUARE(g-gr_palette[1])+SQUARE(b-gr_palette[2]);
+	best_value = SQUARE(r-gr_palette[0].r)+SQUARE(g-gr_palette[0].g)+SQUARE(b-gr_palette[0].b);
 	best_index = 0;
 	if (best_value==0) {
 		add_computed_color(r, g, b, best_index);
@@ -217,9 +230,9 @@ int gr_find_closest_color( int r, int g, int b )
 	}
 	j=0;
 	// only go to 255, 'cause we dont want to check the transparent color.
-	for (i=1; i<254; i++ )	{
-		j += 3;
-		value = SQUARE(r-gr_palette[j])+SQUARE(g-gr_palette[j+1])+SQUARE(b-gr_palette[j+2]);
+	for (i=1; i < 254; i++ )	{
+		++j;
+		value = SQUARE(r-gr_palette[j].r)+SQUARE(g-gr_palette[j].g)+SQUARE(b-gr_palette[j].b);
 		if ( value < best_value )	{
 			if (value==0) {
 				add_computed_color(r, g, b, i);
@@ -248,16 +261,16 @@ int gr_find_closest_color_current( int r, int g, int b )
 //	g &= 63;
 //	b &= 63;
 
-	best_value = SQUARE(r-gr_current_pal[0])+SQUARE(g-gr_current_pal[1])+SQUARE(b-gr_current_pal[2]);
+	best_value = SQUARE(r-gr_current_pal[0].r)+SQUARE(g-gr_current_pal[0].g)+SQUARE(b-gr_current_pal[0].b);
 	best_index = 0;
 	if (best_value==0)
  		return best_index;
 
 	j=0;
 	// only go to 255, 'cause we dont want to check the transparent color.
-	for (i=1; i<254; i++ )	{
-		j += 3;
-		value = SQUARE(r-gr_current_pal[j])+SQUARE(g-gr_current_pal[j+1])+SQUARE(b-gr_current_pal[j+2]);
+	for (i=1; i < 254; i++ )	{
+		++j;
+		value = SQUARE(r-gr_current_pal[j].r)+SQUARE(g-gr_current_pal[j].g)+SQUARE(b-gr_current_pal[j].b);
 		if ( value < best_value )	{
 			if (value==0)
 				return i;
@@ -273,12 +286,12 @@ void gr_make_cthru_table(ubyte * table, ubyte r, ubyte g, ubyte b )
 	int i;
 	ubyte r1, g1, b1;
 
-	for (i=0; i<256; i++ )	{
-		r1 = gr_palette[i*3+0] + r;
+	for (i=0; i < 256; i++ )	{
+		r1 = gr_palette[i].r + r;
 		if ( r1 > 63 ) r1 = 63;
-		g1 = gr_palette[i*3+1] + g;
+		g1 = gr_palette[i].g + g;
 		if ( g1 > 63 ) g1 = 63;
-		b1 = gr_palette[i*3+2] + b;
+		b1 = gr_palette[i].b + b;
 		if ( b1 > 63 ) b1 = 63;
 		table[i] = gr_find_closest_color( r1, g1, b1 );
 	}
@@ -296,9 +309,9 @@ void gr_make_blend_table(ubyte *blend_table, ubyte r, ubyte g, ubyte b)
 		alpha = 1.0 - (float)j / ((float)GR_FADE_LEVELS - 1);
 		for (i = 0; i < 255; i++)
 		{
-			r1 = (ubyte)((1.0 - alpha) * (float)gr_palette[i * 3 + 0] + (alpha * (float)r));
-			g1 = (ubyte)((1.0 - alpha) * (float)gr_palette[i * 3 + 1] + (alpha * (float)g));
-			b1 = (ubyte)((1.0 - alpha) * (float)gr_palette[i * 3 + 2] + (alpha * (float)b));
+			r1 = (ubyte)((1.0 - alpha) * (float)gr_palette[i].r + (alpha * (float)r));
+			g1 = (ubyte)((1.0 - alpha) * (float)gr_palette[i].g + (alpha * (float)g));
+			b1 = (ubyte)((1.0 - alpha) * (float)gr_palette[i].b + (alpha * (float)b));
 			blend_table[i + j * 256] = gr_find_closest_color(r1, g1, b1);
 		}
 		blend_table[i + j * 256] = 255; // leave white alone

common/2d/bitmap.cpp
@@ -183,15 +183,14 @@ void gr_set_super_transparent (grs_bitmap *pbm, int bTransparent)
 	}
 }
 
-void build_colormap_good( palette_array_t &pa, ubyte * colormap, int * freq )
+void build_colormap_good( palette_array_t &palette, ubyte * colormap, int * freq )
 {
 	int i, r, g, b;
-	ubyte *palette = pa;
 
-	for (i=0; i<256; i++ )	{
-		r = *palette++;
-		g = *palette++;
-		b = *palette++;
+	for (i=0; i < 256; i++ )	{
+		r = palette[i].r;
+		g = palette[i].g;
+		b = palette[i].b;
  		*colormap++ = gr_find_closest_color( r, g, b );
 		*freq++ = 0;
 	}

common/include/internal.h
@@ -56,12 +56,12 @@ extern void ogl_swap_buffers_internal(void);
 //#define PAL2Tr(c) ((gr_palette[c*3])/63.0)
 //#define PAL2Tg(c) ((gr_palette[c*3+1])/63.0)
 //#define PAL2Tb(c) ((gr_palette[c*3+2])/63.0)
-#define CPAL2Tr(c) ((gr_current_pal[c*3])/63.0)
-#define CPAL2Tg(c) ((gr_current_pal[c*3+1])/63.0)
-#define CPAL2Tb(c) ((gr_current_pal[c*3+2])/63.0)
-#define PAL2Tr(c) (((*ogl_pal)[c*3])/63.0)
-#define PAL2Tg(c) (((*ogl_pal)[c*3+1])/63.0)
-#define PAL2Tb(c) (((*ogl_pal)[c*3+2])/63.0)
+#define CPAL2Tr(c) ((gr_current_pal[c].r)/63.0)
+#define CPAL2Tg(c) ((gr_current_pal[c].g)/63.0)
+#define CPAL2Tb(c) ((gr_current_pal[c].b)/63.0)
+#define PAL2Tr(c) (((*ogl_pal)[c].r)/63.0)
+#define PAL2Tg(c) (((*ogl_pal)[c].g)/63.0)
+#define PAL2Tb(c) (((*ogl_pal)[c].b)/63.0)
 //inline GLfloat PAL2Tr(int c);
 //inline GLfloat PAL2Tg(int c);
 //inline GLfloat PAL2Tb(int c);

common/include/palette.h
@@ -24,7 +24,16 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "pstypes.h"
 
 #ifdef __cplusplus
-typedef ubyte palette_array_t[256*3];
+#include "dxxsconf.h"
+#include "compiler-array.h"
+
+struct rgb_t {
+	ubyte r,g,b;
+};
+
+static inline bool operator==(const rgb_t &a, const rgb_t &b) { return a.r == b.r && a.g == b.g && a.b == b.b; }
+
+struct palette_array_t : public array<rgb_t, 256> {};
 
 #ifdef DXX_BUILD_DESCENT_II
 #define DEFAULT_LEVEL_PALETTE "groupa.256" //don't confuse with D2_DEFAULT_PALETTE

common/include/pcx.h
@@ -25,6 +25,7 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #ifdef __cplusplus
 
 struct grs_bitmap;
+struct palette_array_t;
 
 #define PCX_ERROR_NONE          0
 #define PCX_ERROR_OPENING       1

d1x-rebirth/main/bmread.cpp
@@ -148,9 +148,9 @@ int compute_average_pixel(grs_bitmap *n)
 	for (row=0; row<n->bm_h; row++)
 		for (column=0; column<n->bm_w; column++) {
 			color = gr_gpixel (n, column, row);
-			total_red += gr_palette[color*3];
-			total_green += gr_palette[color*3+1];
-			total_blue += gr_palette[color*3+2];
+			total_red += gr_palette[color].r;
+			total_green += gr_palette[color].g;
+			total_blue += gr_palette[color].b;
 		}
 
 	total_red /= (n->bm_h * n->bm_w);
@@ -167,7 +167,7 @@ int compute_average_pixel(grs_bitmap *n)
 static bitmap_index bm_load_sub(int skip, char * filename )
 {
 	bitmap_index bitmap_num;
-	ubyte newpal[256*3];
+	palette_array_t newpal;
 	int iff_error;		//reference parm to avoid warning message
 	char fname[20];
 

d2x-rebirth/main/bmread.cpp
@@ -152,9 +152,9 @@ int compute_average_pixel(grs_bitmap *n)
 	for (row=0; row<n->bm_h; row++)
 		for (column=0; column<n->bm_w; column++) {
 			color = *pptr++;
-			total_red += gr_palette[color*3];
-			total_green += gr_palette[color*3+1];
-			total_blue += gr_palette[color*3+2];
+			total_red += gr_palette[color].r;
+			total_green += gr_palette[color].g;
+			total_blue += gr_palette[color].b;
 		}
 
 	total_red /= (n->bm_h * n->bm_w);

d2x-rebirth/main/gamepal.cpp
@@ -74,7 +74,7 @@ int load_palette(const char *name,int used_for_level,int no_change_screen)
 
 	if (d_stricmp(last_palette_loaded,name) != 0) {
 
-		memcpy(old_pal,gr_palette,sizeof(old_pal));
+		old_pal = gr_palette;
 
 		strncpy(last_palette_loaded,name,sizeof(last_palette_loaded));
 

commit 5b4bf16bfa68449e5a7534bcabce67bb0a787822

Remove support for -DSWAP_0_255
This has been broken since b1c5307eb1f6e36500cae30cc92439d9602ceb21
changed the type of gr_palette from `uint8_t[256*3]` to
`array<rgb_t, 256>`.  Remove it, since no one has reported it in 10
years.

similar/2d/palette.cpp
@@ -190,23 +190,6 @@ void gr_use_palette_table(const char * filename )
 		i[255] = 255;
 #if defined(DXX_BUILD_DESCENT_II)
 	Num_computed_colors = 0;	//	Flush palette cache.
-// swap colors 0 and 255 of the palette along with fade table entries
-
-#ifdef SWAP_0_255
-	for (i = 0; i < 3; i++) {
-		ubyte c;
-		c = gr_palette[i];
-		gr_palette[i] = gr_palette[765+i];
-		gr_palette[765+i] = c;
-	}
-
-	for (i = 0; i < GR_FADE_LEVELS * 256; i++) {
-		if (gr_fade_table[i] == 0)
-			gr_fade_table[i] = 255;
-	}
-	for (i=0; i<GR_FADE_LEVELS; i++)
-		gr_fade_table[i*256] = TRANSPARENCY_COLOR;
-#endif
 #endif
 }
 