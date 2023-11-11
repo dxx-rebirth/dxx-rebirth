dxx-rebirth/dxx-rebirth

commit 8fe9b8322d6cf09fa648ea0688a6e48276e954d9

Check return value of PHYSFSX_fgets

similar/main/piggy.cpp
@@ -1986,7 +1986,10 @@ static void read_d1_tmap_nums_from_hog(PHYSFS_File *d1_pig)
 			decode_text_line((inputline));
 		else
 			while (inputline[(i=strlen(inputline))-2]=='\\')
-				PHYSFSX_fgets(inputline,bitmaps,i-2); // strip comments
+			{
+				if (PHYSFSX_fgets(inputline, bitmaps, i - 2)) // strip comments
+					break;
+			}
 		REMOVE_EOL(inputline);
                 if (strchr(inputline, ';')!=NULL) REMOVE_COMMENTS(inputline);
 		if (strlen(inputline) == LINEBUF_SIZE-1) {

common/include/physfsx.h
@@ -253,9 +253,12 @@ template <>
 struct PHYSFSX_gets_line_t<0>
 {
 #define DXX_ALLOCATE_PHYSFS_LINE(n)	std::make_unique<char[]>(n)
+#if !DXX_HAVE_POISON
+	const
+#endif
 	std::unique_ptr<char[]> m_line;
-	std::size_t m_length;
-	PHYSFSX_gets_line_t(std::size_t n) :
+	const std::size_t m_length;
+	PHYSFSX_gets_line_t(const std::size_t n) :
 #if !DXX_HAVE_POISON
 		m_line(DXX_ALLOCATE_PHYSFS_LINE(n)),
 #endif
@@ -282,7 +285,9 @@ struct PHYSFSX_gets_line_t<0>
 
 class PHYSFSX_fgets_t
 {
+	[[nodiscard]]
 	static char *get(char *buf, std::size_t n, PHYSFS_File *const fp);
+	[[nodiscard]]
 	static char *get(char *buf, std::size_t offset, std::size_t n, PHYSFS_File *const fp)
 	{
 		if (offset > n)
@@ -291,12 +296,14 @@ class PHYSFSX_fgets_t
 	}
 public:
 	template <std::size_t n>
+		[[nodiscard]]
 		__attribute_nonnull()
 		char *operator()(PHYSFSX_gets_line_t<n> &buf, PHYSFS_File *const fp, std::size_t offset = 0) const
 		{
 			return get(&buf.next()[0], offset, buf.size(), fp);
 		}
 	template <std::size_t n>
+		[[nodiscard]]
 		__attribute_nonnull()
 		char *operator()(ntstring<n> &buf, PHYSFS_File *const fp, std::size_t offset = 0) const
 		{

common/ui/keypad.cpp
@@ -391,7 +391,8 @@ int ui_pad_read( int n, const char * filename )
 	PHYSFSX_gets_line_t<100> buffer;
 	while ( linenumber < 22)
 	{
-		PHYSFSX_fgets( buffer, infile );
+		if (!PHYSFSX_fgets(buffer, infile))
+			break;
 
 		auto &line = buffer.line();
 		const auto lb = line.begin();

similar/main/bmread.cpp
@@ -588,7 +588,8 @@ int gamedata_read_tbl(d_level_shared_robot_info_state &LevelSharedRobotInfoState
 					inputline[l-2] = ' ';				//add one
 					l++;
 				}
-				PHYSFSX_fgets(inputline,InfoFile,l-2);
+				if (!PHYSFSX_fgets(inputline, InfoFile, l - 2))
+					break;
 				linenum++;
 			}
 		}

similar/main/gamesave.cpp
@@ -966,22 +966,11 @@ static int load_game_data(
 	}
 #endif
 
-	if (game_top_fileinfo_version >= 31) //load mine filename
+	if (game_top_fileinfo_version >= 14) //load mine filename
+	{
 		// read newline-terminated string, not sure what version this changed.
-		PHYSFSX_fgets(Current_level_name,LoadFile);
-	else if (game_top_fileinfo_version >= 14) { //load mine filename
-		// read null-terminated string
-		//must do read one char at a time, since no PHYSFSX_fgets()
-		for (auto p = Current_level_name.next().begin(); (*p = PHYSFSX_fgetc(LoadFile));)
-		{
-			if (++p == Current_level_name.line().end())
-			{
-				p[-1] = 0;
-				while (PHYSFSX_fgetc(LoadFile))
-					;
-				break;
-			}
-		}
+		if (!PHYSFSX_fgets(Current_level_name, LoadFile))
+			*Current_level_name = 0;
 	}
 	else
 		Current_level_name.next()[0]=0;
@@ -1372,7 +1361,10 @@ int load_level(
 	}
 
 	if (Gamesave_current_version > 1)
-		PHYSFSX_fgets(Current_level_palette,LoadFile);
+	{
+		if (!PHYSFSX_fgets(Current_level_palette, LoadFile))
+			Current_level_palette[0] = 0;
+	}
 	if (Gamesave_current_version <= 1 || Current_level_palette[0]==0) // descent 1 level
 		strcpy(Current_level_palette.next().data(), DEFAULT_LEVEL_PALETTE);
 

similar/main/playsave.cpp
@@ -636,23 +636,20 @@ static void plyr_read_stats_v(int *k, int *d)
 	if (auto f = PHYSFSX_openReadBuffered(filename).first)
 	{
 		PHYSFSX_gets_line_t<256> line;
-		if(!PHYSFS_eof(f))
+		if (!PHYSFS_eof(f) && PHYSFSX_fgets(line, f))
 		{
-			 PHYSFSX_fgets(line,f);
 			 const char *value=splitword(line,':');
 			 if(!strcmp(line,"kills") && value)
 				*k=atoi(value);
 		}
-		if(!PHYSFS_eof(f))
+		if (!PHYSFS_eof(f) && PHYSFSX_fgets(line, f))
                 {
-			 PHYSFSX_fgets(line,f);
 			 const char *value=splitword(line,':');
 			 if(!strcmp(line,"deaths") && value)
 				*d=atoi(value);
 		 }
-		if(!PHYSFS_eof(f))
+		if (!PHYSFS_eof(f) && PHYSFSX_fgets(line, f))
 		{
-			 PHYSFSX_fgets(line,f);
 			 const char *value=splitword(line,':');
 			 if(value && !strcmp(line,"key") && strlen(value)>10){
 				 if (value[0]=='0' && value[1]=='1'){
@@ -1226,8 +1223,9 @@ int read_player_file()
 	}
 
 	//read guidebot name
-	if (player_file_version >= 18)
-		PHYSFSX_fgets(PlayerCfg.GuidebotName, file);
+	if (player_file_version >= 18 && PHYSFSX_fgets(PlayerCfg.GuidebotName, file))
+	{
+	}
 	else
 		PlayerCfg.GuidebotName = "GUIDE-BOT";
 	PlayerCfg.GuidebotNameReal = PlayerCfg.GuidebotName;
@@ -1235,7 +1233,12 @@ int read_player_file()
 		if (player_file_version >= 24) 
 		{
 			PHYSFSX_gets_line_t<128> buf;
-			PHYSFSX_fgets(buf, file);			// Just read it in fpr DPS.
+			if (PHYSFSX_fgets(buf, file))			// Just read it in fpr DPS.
+			{
+				/* Nothing to do.  Buffer contents are ignored.  This is only
+				 * read for its side effect on the file position.
+				 */
+			}
 		}
 	}
 #endif

commit 7f3ef61793a01a3fc39df0b5bd079e3552bec59a

Fix inverted end-of-file test in read_d1_tmap_nums_from_hog
Commit 8fe9b8322d6c added a test for failure in read_d1_tmap_nums_from_hog.
However, it exits on success, not failure.  Invert the test.

Fixes: 8fe9b8322d6cf09fa648ea0688a6e48276e954d9 ("Check return value of PHYSFSX_fgets")

similar/main/piggy.cpp
@@ -1937,7 +1937,7 @@ static void read_d1_tmap_nums_from_hog(PHYSFS_File *d1_pig)
 		else
 			while (inputline[(i=strlen(inputline))-2]=='\\')
 			{
-				if (PHYSFSX_fgets(inputline, bitmaps, i - 2)) // strip comments
+				if (!PHYSFSX_fgets(inputline, bitmaps, i - 2)) // strip comments
 					break;
 			}
 		REMOVE_EOL(inputline);