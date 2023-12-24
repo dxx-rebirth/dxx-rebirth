dxx-rebirth/dxx-rebirth

commit 7a183c18d909e6af5a39b95955afd856f2c2a02c

Use enum class for robot_id

similar/main/fireball.cpp
@@ -353,7 +353,7 @@ static bool can_collide(const object *const weapon_object, const object_base &it
 				return false;
 			if (parent_object->type != OBJ_ROBOT)
 				return true;
-			return get_robot_id(*parent_object) != iter_object.id;
+			return get_robot_id(*parent_object) != get_robot_id(iter_object);
 		default:
 			return false;
 	}
@@ -1196,11 +1196,12 @@ static bool drop_robot_egg(const d_robot_info_array &Robot_info, const int type,
 				/* ObjId appears to serve as both a polygon_model_index and as
 				 * a robot index.
 				 */
-				const auto robot_id = static_cast<unsigned>(ObjId[type]);
+				const auto rid = static_cast<robot_id>(ObjId[type]);
 #elif defined(DXX_BUILD_DESCENT_II)
-				const auto robot_id = id;
+				const auto rid = static_cast<robot_id>(id);
 #endif
-				const auto &&objp = robot_create(Robot_info, id, segnum, new_pos, &vmd_identity_matrix, Polygon_models[Robot_info[robot_id].model_num].rad, ai_behavior::AIB_NORMAL);
+				auto &robptr = Robot_info[rid];
+				const auto &&objp = robot_create(Robot_info, rid, segnum, new_pos, &vmd_identity_matrix, Polygon_models[robptr.model_num].rad, ai_behavior::AIB_NORMAL);
 				if (objp == object_none)
 					break;
 				auto &obj = *objp;
@@ -1213,19 +1214,19 @@ static bool drop_robot_egg(const d_robot_info_array &Robot_info, const int type,
 				}
 				//Set polygon-object-specific data
 
-				obj.rtype.pobj_info.model_num = Robot_info[get_robot_id(obj)].model_num;
+				obj.rtype.pobj_info.model_num = robptr.model_num;
 				obj.rtype.pobj_info.subobj_flags = 0;
 
 				//set Physics info
 		
 				obj.mtype.phys_info.velocity = new_velocity;
 
-				obj.mtype.phys_info.mass = Robot_info[get_robot_id(obj)].mass;
-				obj.mtype.phys_info.drag = Robot_info[get_robot_id(obj)].drag;
+				obj.mtype.phys_info.mass = robptr.mass;
+				obj.mtype.phys_info.drag = robptr.drag;
 
 				obj.mtype.phys_info.flags |= (PF_LEVELLING);
 
-				obj.shields = Robot_info[get_robot_id(obj)].strength;
+				obj.shields = robptr.strength;
 
 				ai_local		*ailp = &obj.ctype.ai_info.ail;
 				ailp->player_awareness_type = player_awareness_type_t::PA_WEAPON_ROBOT_COLLISION;
@@ -1344,7 +1345,7 @@ static void explode_model(object_base &obj)
 	if (n_models > 1) {
 		for (unsigned i = 1; i < n_models; ++i)
 #if defined(DXX_BUILD_DESCENT_II)
-			if (!(i == 5 && obj.type == OBJ_ROBOT && get_robot_id(obj) == 44))	//energy sucker energy part
+			if (!(i == 5 && obj.type == OBJ_ROBOT && get_robot_id(obj) == robot_id::energy_bandit))	//energy sucker energy part
 #endif
 				object_create_debris(vmsegptridx, obj, i);
 

common/include/editor/editor.h
@@ -540,7 +540,7 @@ void close_editor_screen(void);
 #ifdef dsx
 namespace dsx {
 //    From eobject.c
-int place_object(d_level_unique_object_state &LevelUniqueObjectState, const d_level_shared_polygon_model_state &LevelSharedPolygonModelState, const d_robot_info_array &Robot_info, const d_level_shared_segment_state &LevelSharedSegmentState, d_level_unique_segment_state &LevelUniqueSegmentState, vmsegptridx_t segp, const vms_vector &object_pos, short object_type, short object_id);
+int place_object(d_level_unique_object_state &LevelUniqueObjectState, const d_level_shared_polygon_model_state &LevelSharedPolygonModelState, const d_robot_info_array &Robot_info, const d_level_shared_segment_state &LevelSharedSegmentState, d_level_unique_segment_state &LevelUniqueSegmentState, vmsegptridx_t segp, const vms_vector &object_pos, short object_type, uint8_t object_id);
 
 // from ksegsize.c
 void med_extract_up_vector_from_segment_side(const shared_segment &sp, sidenum_t sidenum, vms_vector &vp);

common/main/ai.h
@@ -73,9 +73,6 @@ struct PHYSFS_File;
 #define MAX_DEPTH_TO_SEARCH_FOR_PLAYER  10
 #define BOSS_GATE_MATCEN_NUM            -1
 
-#define ROBOT_BRAIN 7
-#define ROBOT_BOSS1 17
-
 #define ROBOT_FIRE_AGITATION 94
 
 #ifdef dsx
@@ -147,7 +144,7 @@ struct d_level_shared_boss_state : ::dcx::d_level_shared_boss_state
 
 extern d_level_shared_boss_state LevelSharedBossState;
 void move_towards_segment_center(const robot_info &robptr, const d_level_shared_segment_state &, object_base &objp);
-imobjptridx_t gate_in_robot(const d_robot_info_array &Robot_info, unsigned type, vmsegptridx_t segnum);
+imobjptridx_t gate_in_robot(const d_robot_info_array &Robot_info, robot_id type, vmsegptridx_t segnum);
 void do_ai_frame(const d_level_shared_robot_info_state &LevelSharedRobotInfoState, vmobjptridx_t objp);
 #if defined(DXX_BUILD_DESCENT_I)
 #define do_ai_frame_all(Robot_info) do_ai_frame_all()
@@ -256,7 +253,6 @@ void buddy_message_str(const char * str) __attribute_nonnull();
 void buddy_message(const char * format, ... ) __attribute_format_printf(1, 2);
 #define buddy_message(F,...)	dxx_call_printf_checked(buddy_message,buddy_message_str,(),(F),##__VA_ARGS__)
 
-#define SPECIAL_REACTOR_ROBOT   65
 extern void special_reactor_stuff(void);
 #endif
 }

common/main/fwd-robot.h
@@ -33,6 +33,8 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 namespace dcx {
 
 enum class robot_gun_number : uint8_t;
+enum class robot_id : uint8_t;
+
 constexpr std::integral_constant<std::size_t, 8> MAX_GUNS{};      //should be multiple of 4 for ubyte array
 
 enum class robot_animation_state : uint8_t;
@@ -51,6 +53,7 @@ struct d_level_shared_robot_joint_state;
 
 #ifdef dsx
 namespace dsx {
+
 #if defined(DXX_BUILD_DESCENT_II)
 //robot info flags
 #define RIF_BIG_RADIUS  1   //pad the radius to fix robots firing through walls
@@ -70,14 +73,14 @@ constexpr std::integral_constant<unsigned, 85> MAX_ROBOT_TYPES{};
 constexpr std::integral_constant<unsigned, 1600> MAX_ROBOT_JOINTS{};
 #endif
 
-using d_robot_info_array = std::array<robot_info, MAX_ROBOT_TYPES>;
+using d_robot_info_array = enumerated_array<robot_info, MAX_ROBOT_TYPES, robot_id>;
 
 //the array of robots types
 struct d_level_shared_robot_info_state;
 extern d_level_shared_robot_info_state LevelSharedRobotInfoState;
 
 #if DXX_USE_EDITOR
-using robot_names_array = std::array<std::array<char, ROBOT_NAME_LENGTH>, MAX_ROBOT_TYPES>;
+using robot_names_array = enumerated_array<std::array<char, ROBOT_NAME_LENGTH>, MAX_ROBOT_TYPES, robot_id>;
 extern robot_names_array Robot_names;
 #endif
 

common/main/multibot.h
@@ -56,7 +56,7 @@ void multi_send_robot_position(object &objnum, multi_send_robot_position_priorit
 void multi_send_robot_fire(vmobjptridx_t objnum, robot_gun_number gun_num, const vms_vector &fire);
 void multi_send_claim_robot(vmobjptridx_t objnum);
 void multi_send_robot_explode(imobjptridx_t objnum, objnum_t killer);
-void multi_send_create_robot(station_number robotcen, objnum_t objnum, int type);
+void multi_send_create_robot(station_number robotcen, objnum_t objnum, robot_id type);
 namespace dsx {
 void multi_send_boss_teleport(vmobjptridx_t bossobjnum, vcsegidx_t where);
 }

common/main/object.h
@@ -786,9 +786,9 @@ static inline uint8_t get_fireball_id(const object_base &o)
 	return o.id;
 }
 
-static inline uint8_t get_robot_id(const object_base &o)
+static inline robot_id get_robot_id(const object_base &o)
 {
-	return o.id;
+	return robot_id{o.id};
 }
 
 static inline void set_player_id(object_base &o, const uint8_t id)
@@ -801,9 +801,9 @@ static inline void set_reactor_id(object_base &o, const uint8_t id)
 	o.id = id;
 }
 
-static inline void set_robot_id(object_base &o, const uint8_t id)
+static inline void set_robot_id(object_base &o, const robot_id id)
 {
-	o.id = id;
+	o.id = {static_cast<std::underlying_type<robot_id>::type>(id)};
 }
 
 void check_warn_object_type(const object_base &, object_type_t, const char *file, unsigned line);

common/main/robot.h
@@ -48,6 +48,18 @@ enum class robot_animation_state : uint8_t
 	flinch,
 };
 
+enum class robot_id : uint8_t
+{
+	brain = 7,
+	toaster = 10,
+	baby_spider = 14,
+	/* if DXX_BUILD_DESCENT_II */
+	energy_bandit = 44,
+	special_reactor = 65,
+	/* endif */
+	None = UINT8_MAX,
+};
+
 //describes the position of a certain joint
 struct jointpos : prohibit_void_ptr<jointpos>
 {
@@ -184,15 +196,15 @@ struct d_level_shared_robot_info_state
 imobjptridx_t find_escort(fvmobjptridx &vmobjptridx, const d_robot_info_array &Robot_info);
 #endif
 
-imobjptridx_t robot_create(const d_robot_info_array &Robot_info, unsigned id, vmsegptridx_t segnum, const vms_vector &pos, const vms_matrix *orient, fix size, ai_behavior behavior, const imsegidx_t hide_segment = segment_none);
-void recreate_thief(const d_robot_info_array &Robot_info, uint8_t thief_id);
+imobjptridx_t robot_create(const d_robot_info_array &Robot_info, robot_id id, vmsegptridx_t segnum, const vms_vector &pos, const vms_matrix *orient, fix size, ai_behavior behavior, const imsegidx_t hide_segment = segment_none);
+void recreate_thief(const d_robot_info_array &Robot_info, robot_id thief_id);
 
 // Drops objects contained in objp.
 bool object_create_robot_egg(const d_robot_info_array &Robot_info, object_base &objp);
 bool object_create_robot_egg(const d_robot_info_array &Robot_info, int type, int id, unsigned num, const vms_vector &init_vel, const vms_vector &pos, vmsegptridx_t segnum);
 
 // Create a matcen robot
-imobjptridx_t create_morph_robot(const d_robot_info_array &Robot_info, vmsegptridx_t segp, const vms_vector &object_pos, unsigned object_id);
+imobjptridx_t create_morph_robot(const d_robot_info_array &Robot_info, vmsegptridx_t segp, const vms_vector &object_pos, robot_id object_id);
 
 // do whatever this thing does in a frame
 void do_controlcen_frame(const d_robot_info_array &Robot_info, vmobjptridx_t obj);
@@ -226,7 +238,7 @@ struct d_level_shared_robot_joint_state : ::dcx::d_level_shared_robot_joint_stat
 //  On exit:
 //      Returns number of joints in list.
 //      jp_list_ptr is stuffed with a pointer to a static array of joint positions.  This pointer is valid forever.
-ranges::subrange<const jointpos *> robot_get_anim_state(const d_robot_info_array &, const std::array<jointpos, MAX_ROBOT_JOINTS> &, unsigned robot_type, robot_gun_number gun_num, robot_animation_state state);
+ranges::subrange<const jointpos *> robot_get_anim_state(const d_robot_info_array &, const std::array<jointpos, MAX_ROBOT_JOINTS> &, robot_id robot_type, robot_gun_number gun_num, robot_animation_state state);
 
 /*
  * reads n robot_info structs from a PHYSFS_File

d1x-rebirth/main/custom.cpp
@@ -103,8 +103,6 @@ struct bitmap_original
 
 constexpr std::integral_constant<uint8_t, 0x80> BM_FLAG_CUSTOMIZED{};
 
-}
-
 static enumerated_array<bitmap_original, MAX_BITMAP_FILES, bitmap_index> BitmapOriginal;
 static std::array<snd_info, MAX_SOUND_FILES> SoundOriginal;
 
@@ -471,7 +469,11 @@ static int read_d2_robot_info(PHYSFS_File *fp, robot_info &ri)
 	return 1;
 }
 
+}
+
 namespace dsx {
+
+namespace {
 
 static void load_hxm(const d_fname &hxmname)
 {
@@ -508,7 +510,8 @@ static void load_hxm(const d_fname &hxmname)
 			}
 			else
 			{
-				if (!(read_d2_robot_info(f, Robot_info[repl_num])))
+				static_assert(MAX_ROBOT_TYPES <= UINT8_MAX);
+				if (!(read_d2_robot_info(f, Robot_info[static_cast<robot_id>(repl_num)])))
 				{
 					return;
 				}
@@ -574,8 +577,6 @@ static void load_hxm(const d_fname &hxmname)
 	}
 }
 
-}
-
 // undo customized items
 static void custom_remove()
 {
@@ -618,6 +619,10 @@ static void custom_remove()
 			GameSounds[i].length = SoundOriginal[i].length & 0x7fffffff;
 			SoundOriginal[i].length = 0;
 		}
+}
+
+}
+
 }
 
 void load_custom_data(const d_fname &level_name)

commit d7249052acebc884bf69de7c2a4bbb16677c2596

Fix Descent 1 robots-drop-robots logic
Commit 7a183c18d909 ("Use enum class for robot_id") made an incorrect
change to the Descent 1 call to `robot_create`, causing the game to drop
the wrong type of robot.  Descent 2 was not affected.  This commit both
fixes the incorrect type, which is readily visible, and fixes the
created robot's radius, which Descent 1 has always done wrong and
Descent 2 has always done right.

The second argument to `robot_create` is the ID of the robot to create
(Hulk, Lifter, Spider, etc.).  Prior to 7a183c18d909, the second
argument was `id`, which came from the caller and was of type `int`.
Both games had a related variable `robot_id` used for lookup in the
`Robot_info` array.  In Descent 1, `robot_id` was set from a cast of
`ObjId[type]`.  In Descent 2, `robot_id` was set from a cast of `id`.

Based on the observation that `Robot_info[robot_id]` was used to read
`.model_num`, I believed that `robot_id` was suitable for use as the ID
of the robot to create.  For Descent 2, this was true.  For Descent 1,
it was not true.  Based on that belief, commit 7a183c18d909 changed the
second parameter from `id` to `robot_id` (which was also renamed to
`rid`).  For Descent 1, this caused the wrong robot to drop.  For
Descent 2, this was a no-op change.

Reported-by: JeodC <https://github.com/dxx-rebirth/dxx-rebirth/issues/742>
Bisected-by: InfernalCore <https://github.com/dxx-rebirth/dxx-rebirth/issues/742#issuecomment-1866698281>
Fixes: 7a183c18d909e6af5a39b95955afd856f2c2a02c ("Use enum class for robot_id")

similar/main/fireball.cpp
@@ -1197,14 +1197,7 @@ static bool drop_robot_egg(const d_robot_info_array &Robot_info, const contained
 //				new_pos.y += (d_rand()-16384)*7;
 //				new_pos.z += (d_rand()-16384)*6;
 
-#if defined(DXX_BUILD_DESCENT_I)
-				/* ObjId appears to serve as both a polygon_model_index and as
-				 * a robot index.
-				 */
-				const auto rid = static_cast<robot_id>(ObjId[underlying_value(type)]);
-#elif defined(DXX_BUILD_DESCENT_II)
 				const auto rid = static_cast<robot_id>(id);
-#endif
 				auto &robptr = Robot_info[rid];
 				const auto &&objp = robot_create(Robot_info, rid, segnum, new_pos, &vmd_identity_matrix, Polygon_models[robptr.model_num].rad, ai_behavior::AIB_NORMAL);
 				if (objp == object_none)