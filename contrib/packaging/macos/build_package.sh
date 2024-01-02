7162bff4e06b7011611673c5481e9b64e4614c52
Jeod <47716344+JeodC@users.noreply.github.com>
2024-01-02 18:59:07 -0500

    Fix

--- contrib/packaging/macos/build_package.sh
+++ contrib/packaging/macos/build_package.sh
@@ -10,16 +10,13 @@ build_app() {
     prettyname="$2"
     
     cd build
-        
+    
+    # Create a single zip file containing both applications
+    zip -r -X DXX-Rebirth-MacOS.zip D1X-Rebirth.app D2X-Rebirth.app
+    
     cd ..
 }
 
 # Build both applications
 build_app "d1x-rebirth" "D1X-Rebirth"
 build_app "d2x-rebirth" "D2X-Rebirth"
-
-# Create a single zip file containing both applications
-zip -r -X DXX-Rebirth-MacOS.zip D1X-Rebirth.app D2X-Rebirth.app
-
-# Clean up
-rm -rf D1X-Rebirth.app D2X-Rebirth.app
