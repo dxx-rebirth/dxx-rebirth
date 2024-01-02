fc3c096f47ac59a26b46a13930ca3918f90aefcd
Jeod <47716344+JeodC@users.noreply.github.com>
2024-01-02 16:12:58 -0500

    Consolidate .apps into one .zip

--- contrib/packaging/macos/build_package.sh
+++ contrib/packaging/macos/build_package.sh
@@ -5,24 +5,27 @@ export MACOSX_DEPLOYMENT_TARGET=10.14
 
 GIT_HASH=$(git rev-parse --short HEAD)
 
-
 build_app() {
     name="$1"
     prettyname="$2"
     
     zipfilename="${prettyname}-${GIT_HASH}.app.zip"
     
-    cd build 
+    # Create a subdirectory for each app at the top level
+    mkdir -p ${prettyname}.app/Contents
+
+    # Move the application content to the respective app directory
+    mv build/${prettyname}.app/* ${prettyname}.app/Contents/
+
+    # zip up and output to top-level dir
+    zip -r -X ${zipfilename} ${prettyname}.app
     
-    # Libs should already be bundled in with the `macos_bundle_libs=1` option to scons
-        
-    # zip up and output to top level dir
-    zip -r -X ../${zipfilename} ${prettyname}.app
-    
-    cd ..
+    # Clean up
+    rm -rf ${prettyname}.app
 }
 
+# Build D1X-Rebirth
 build_app "d1x-rebirth" "D1X-Rebirth"
-build_app "d2x-rebirth" "D2X-Rebirth"
 
-# Clean up
+# Build D2X-Rebirth
+build_app "d2x-rebirth" "D2X-Rebirth"
