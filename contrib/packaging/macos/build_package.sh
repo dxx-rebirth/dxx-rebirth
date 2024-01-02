7c51c7269ae140cec02d05fc4dd3c31198de6302
Jeod <47716344+JeodC@users.noreply.github.com>
2024-01-02 16:50:45 -0500

    Make corrections

--- contrib/packaging/macos/build_package.sh
+++ contrib/packaging/macos/build_package.sh
@@ -9,13 +9,13 @@ build_app() {
     name="$1"
     prettyname="$2"
     
-    zipfilename="${prettyname}-${GIT_HASH}.app.zip"
+    zipfilename="${prettyname}.app.zip"
     
     # Create a subdirectory for each app at the top level
-    mkdir -p ${prettyname}.app/Contents
+    mkdir -p ${prettyname}.app/${prettyname}
 
     # Move the application content to the respective app directory
-    mv build/${prettyname}.app/* ${prettyname}.app/Contents/
+    mv build/${prettyname}.app/* ${prettyname}.app/${prettyname}
 
     # zip up and output to top-level dir
     zip -r -X ${zipfilename} ${prettyname}.app
@@ -29,3 +29,9 @@ build_app "d1x-rebirth" "D1X-Rebirth"
 
 # Build D2X-Rebirth
 build_app "d2x-rebirth" "D2X-Rebirth"
+
+# zip up and output to top-level dir
+zip -r -X dxx-rebirth-macos.zip D1X-Rebirth D2X-Rebirth
+
+# Clean up
+rm -rf D1X-Rebirth D2X-Rebirth
