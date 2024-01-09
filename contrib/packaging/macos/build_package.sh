dc439a682f3e1f9a51638517b893a358ecb1df11
Jeod <47716344+JeodC@users.noreply.github.com>
2024-01-09 16:10:17 -0500

    Replace packaging
    Consolidates .app files into one .zip file

--- contrib/packaging/macos/build_package.sh
+++ contrib/packaging/macos/build_package.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 set -x
 
-export MACOSX_DEPLOYMENT_TARGET=10.14
+export MACOSX_DEPLOYMENT_TARGET=13
 
 GIT_HASH=$(git rev-parse --short HEAD)
-
 
 build_app() {
     name="$1"
     prettyname="$2"
     
-    zipfilename="${prettyname}-${GIT_HASH}.app.zip"
+    cd build
     
-    cd build 
-    
-    # Libs should already be bundled in with the `macos_bundle_libs=1` option to scons
-        
-    # zip up and output to top level dir
-    zip -r -X ../${zipfilename} ${prettyname}.app
+    # Create a single zip file containing both applications
+    zip -r -X ../DXX-Rebirth-MacOS.zip D1X-Rebirth.app D2X-Rebirth.app
     
     cd ..
 }
 
+# Build both applications
 build_app "d1x-rebirth" "D1X-Rebirth"
 build_app "d2x-rebirth" "D2X-Rebirth"
-
-# Clean up
