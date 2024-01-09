9eccbd0b80b9c0c23379e70b6a6debe6eb97e327
JeodC <47716344+JeodC@users.noreply.github.com>
2024-01-09 17:42:26 -0500

    Remove lingering zsync references
    zsync files are no longer generated and so they are not required here.

--- contrib/packaging/linux/build_package.sh
+++ contrib/packaging/linux/build_package.sh
@@ -38,9 +38,6 @@ build_appimage() {
     # Package!
     "./appimagetool-x86_64.AppImage" --no-appstream --verbose "${appdir}" "${appimagename}"
 
-    # Generate .zsync file
-    "./appimagetool-x86_64.AppImage" --generate-zsync "${appimagename}"
-
     # Clean
     rm -rf "${appdir}"
 }
@@ -50,7 +47,7 @@ build_appimage "d1x-rebirth" "D1X-Rebirth"
 build_appimage "d2x-rebirth" "D2X-Rebirth"
 
 # Consolidate into a single zip file
-zip -r -X DXX-Rebirth.zip D1X-Rebirth.AppImage D1X-Rebirth.AppImage.zsync D2X-Rebirth.AppImage D2X-Rebirth.AppImage.zsync
+zip -r -X DXX-Rebirth.zip D1X-Rebirth.AppImage D2X-Rebirth.AppImage
 
 # Clean
-rm -f appimagetool* AppRun* D1X-Rebirth.AppImage D1X-Rebirth.AppImage.zsync D2X-Rebirth.AppImage D2X-Rebirth.AppImage.zsync
+rm -f appimagetool* AppRun* D1X-Rebirth.AppImage D2X-Rebirth.AppImage
