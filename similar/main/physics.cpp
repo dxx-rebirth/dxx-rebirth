bd41c4c494ed9ae2a4ba640316a9a22f5c1418e3
Kp <kp@valhallalegends.com>
2024-04-14 20:31:52 +0000

    Use constexpr for ROLL_RATE

--- similar/main/physics.cpp
+++ similar/main/physics.cpp
@@ -52,8 +52,8 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 //Global variables for physics system
 
-#define ROLL_RATE	0x2000
 #define TURNROLL_SCALE	(0x4ec4/2)
+constexpr fix ROLL_RATE{0x2000};
 constexpr fixang DAMP_ANG{0x400};	//min angle to bank
 
 //check point against each side of segment. return bitmask, where bit
