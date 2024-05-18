dxx-rebirth/dxx-rebirth

commit 9cefda24e587b59597670e9d615ed427b9558f44

Pass tmap_drawer_type as parameter, rather than using a global

similar/main/morph.cpp
@@ -567,7 +567,7 @@ static void draw_model(grs_canvas &canvas, polygon_model_points &robot_points, p
 			// so we need to reread them all in.
 			// Make sure that they can all fit in memory.
 			const auto morph_vecs = md->get_morph_vecs();
-			g3_draw_morphing_model(canvas, &pm->model_data[pm->submodel_ptrs[submodel_num]], &texture_list[0], anim_angles, light, &morph_vecs[md->submodel_startpoints[submodel_num]], robot_points);
+			g3_draw_morphing_model(canvas, draw_tmap, &pm->model_data[pm->submodel_ptrs[submodel_num]], &texture_list[0], anim_angles, light, &morph_vecs[md->submodel_startpoints[submodel_num]], robot_points);
 		}
 		else {
 			const auto &&orient = vm_angles_2_matrix(anim_angles[mn]);

similar/main/polyobj.cpp
@@ -391,9 +391,9 @@ void free_model(polymodel &po)
 
 namespace dsx {
 
-void draw_polygon_model(const enumerated_array<polymodel, MAX_POLYGON_MODELS, polygon_model_index> &Polygon_models, grs_canvas &canvas, const vms_vector &pos, const vms_matrix &orient, const submodel_angles anim_angles, const polygon_model_index model_num, const unsigned flags, const g3s_lrgb light, const glow_values_t *const glow_values, const alternate_textures alt_textures)
+void draw_polygon_model(const enumerated_array<polymodel, MAX_POLYGON_MODELS, polygon_model_index> &Polygon_models, grs_canvas &canvas, const tmap_drawer_type tmap_drawer_ptr, const vms_vector &pos, const vms_matrix &orient, const submodel_angles anim_angles, const polygon_model_index model_num, const unsigned flags, const g3s_lrgb light, const glow_values_t *const glow_values, const alternate_textures alt_textures)
 {
-	draw_polygon_model(canvas, pos, orient, anim_angles, Polygon_models[model_num], flags, light, glow_values, alt_textures);
+	draw_polygon_model(canvas, tmap_drawer_ptr, pos, orient, anim_angles, Polygon_models[model_num], flags, light, glow_values, alt_textures);
 }
 
 static polygon_model_index build_polygon_model_index_from_polygon_simpler_model_index(const polygon_simpler_model_index i)
@@ -401,7 +401,7 @@ static polygon_model_index build_polygon_model_index_from_polygon_simpler_model_
 	return static_cast<polygon_model_index>(underlying_value(i) - 1);
 }
 
-void draw_polygon_model(grs_canvas &canvas, const vms_vector &pos, const vms_matrix &orient, const submodel_angles anim_angles, const polymodel &pm, unsigned flags, const g3s_lrgb light, const glow_values_t *const glow_values, const alternate_textures alt_textures)
+void draw_polygon_model(grs_canvas &canvas, const tmap_drawer_type tmap_drawer_ptr, const vms_vector &pos, const vms_matrix &orient, const submodel_angles anim_angles, const polymodel &pm, unsigned flags, const g3s_lrgb light, const glow_values_t *const glow_values, const alternate_textures alt_textures)
 {
 	auto &Polygon_models = LevelSharedPolygonModelState.Polygon_models;
 	const polymodel *po = &pm;
@@ -452,7 +452,7 @@ void draw_polygon_model(grs_canvas &canvas, const vms_vector &pos, const vms_mat
 
 	if (flags == 0)		//draw entire object
 
-		g3_draw_polygon_model(&texture_list[0], robot_points, canvas, anim_angles, light, glow_values, po->model_data.get());
+		g3_draw_polygon_model(&texture_list[0], robot_points, canvas, tmap_drawer_ptr, anim_angles, light, glow_values, po->model_data.get());
 
 	else {
 		for (int i=0;flags;flags>>=1,i++)
@@ -461,7 +461,7 @@ void draw_polygon_model(grs_canvas &canvas, const vms_vector &pos, const vms_mat
 
 				//if submodel, rotate around its center point, not pivot point
 				auto &&subctx = g3_start_instance_matrix();
-				g3_draw_polygon_model(&texture_list[0], robot_points, canvas, anim_angles, light, glow_values, &po->model_data[po->submodel_ptrs[i]]);
+				g3_draw_polygon_model(&texture_list[0], robot_points, canvas, tmap_drawer_ptr, anim_angles, light, glow_values, &po->model_data[po->submodel_ptrs[i]]);
 				g3_done_instance(subctx);
 			}	
 	}
@@ -609,7 +609,7 @@ void draw_model_picture(grs_canvas &canvas, const polymodel &mn, const vms_angve
 		temp_pos.z = DEFAULT_VIEW_DIST;
 
 	const auto &&temp_orient = vm_angles_2_matrix(orient_angles);
-	draw_polygon_model(canvas, temp_pos, temp_orient, nullptr, mn, 0, lrgb, nullptr, alternate_textures{});
+	draw_polygon_model(canvas, draw_tmap, temp_pos, temp_orient, nullptr, mn, 0, lrgb, nullptr, alternate_textures{});
 	g3_end_frame();
 }
 

common/3d/draw.cpp
@@ -26,8 +26,6 @@
 
 namespace dcx {
 
-tmap_drawer_type tmap_drawer_ptr = draw_tmap;
-
 #if DXX_USE_OGL
 namespace {
 
@@ -49,16 +47,7 @@ g3_draw_line_colors::g3_draw_line_colors(const color_palette_index color) :
 	color_array{build_color_array_from_color_palette_index(color)}
 {
 }
-#endif
-
-//specifies 2d drawing routines to use instead of defaults.  Passing
-//NULL for either or both restores defaults
-void g3_set_special_render(tmap_drawer_type tmap_drawer)
-{
-	tmap_drawer_ptr = tmap_drawer;
-}
-
-#if !DXX_USE_OGL
+#else
 namespace {
 
 //deal with a clipped line
@@ -123,7 +112,7 @@ bool do_facing_check(const std::array<cg3s_point *, 3> &vertlist)
 #if !DXX_USE_OGL
 namespace {
 
-static void must_clip_tmap_face(grs_canvas &, int nv, g3s_codes cc, grs_bitmap &bm, polygon_clip_points &Vbuf0, polygon_clip_points &Vbuf1);
+static void must_clip_tmap_face(grs_canvas &, std::size_t nv, g3s_codes cc, grs_bitmap &bm, polygon_clip_points &Vbuf0, polygon_clip_points &Vbuf1, tmap_drawer_type tmap_drawer_ptr);
 
 //deal with face that must be clipped
 static void must_clip_flat_face(grs_canvas &canvas, int nv, g3s_codes cc, polygon_clip_points &Vbuf0, polygon_clip_points &Vbuf1, const uint8_t color)
@@ -202,8 +191,7 @@ void _g3_draw_poly(grs_canvas &canvas, const std::span<cg3s_point *const> pointl
 }
 
 //draw a texture-mapped face.
-//returns 1 if off screen, 0 if drew
-void _g3_draw_tmap(grs_canvas &canvas, const std::span<cg3s_point *const> pointlist, const g3s_uvl *const uvl_list, const g3s_lrgb *const light_rgb, grs_bitmap &bm)
+void _g3_draw_tmap(grs_canvas &canvas, const std::span<cg3s_point *const> pointlist, const g3s_uvl *const uvl_list, const g3s_lrgb *const light_rgb, grs_bitmap &bm, const tmap_drawer_type tmap_drawer_ptr)
 {
 	g3s_codes cc;
 
@@ -233,7 +221,7 @@ void _g3_draw_tmap(grs_canvas &canvas, const std::span<cg3s_point *const> pointl
 
 	if (cc.uor != clipping_code::None)
 	{
-		must_clip_tmap_face(canvas, pointlist.size(), cc, bm, Vbuf0, Vbuf1);
+		must_clip_tmap_face(canvas, pointlist.size(), cc, bm, Vbuf0, Vbuf1, tmap_drawer_ptr);
 		return;
 	}
 
@@ -254,7 +242,7 @@ void _g3_draw_tmap(grs_canvas &canvas, const std::span<cg3s_point *const> pointl
 
 namespace {
 
-static void must_clip_tmap_face(grs_canvas &canvas, int nv, g3s_codes cc, grs_bitmap &bm, polygon_clip_points &Vbuf0, polygon_clip_points &Vbuf1)
+static void must_clip_tmap_face(grs_canvas &canvas, const std::size_t nv, g3s_codes cc, grs_bitmap &bm, polygon_clip_points &Vbuf0, polygon_clip_points &Vbuf1, const tmap_drawer_type tmap_drawer_ptr)
 {
 	temporary_points_t tp;
 	auto &bufptr = clip_polygon(Vbuf0,Vbuf1,&nv,&cc,tp);
@@ -265,7 +253,6 @@ static void must_clip_tmap_face(grs_canvas &canvas, int nv, g3s_codes cc, grs_bi
 
 			if (!(p->p3_flags&projection_flag::projected))
 				g3_project_point(*p);
-	
 			if (p->p3_flags&projection_flag::overflow) {
 				Int3();		//should not overflow after clip
 				goto free_points;

common/3d/rod.cpp
@@ -102,7 +102,7 @@ static clipping_code calc_rod_corners(rod_4point &rod_point_group, const g3s_poi
 
 //draw a bitmap object that is always facing you
 //returns 1 if off screen, 0 if drew
-void g3_draw_rod_tmap(grs_canvas &canvas, grs_bitmap &bitmap, const g3s_point &bot_point, fix bot_width, const g3s_point &top_point, fix top_width, g3s_lrgb light)
+void g3_draw_rod_tmap(grs_canvas &canvas, grs_bitmap &bitmap, const g3s_point &bot_point, fix bot_width, const g3s_point &top_point, fix top_width, g3s_lrgb light, const tmap_drawer_type tmap_drawer_ptr)
 {
 	rod_4point rod;
 	if (calc_rod_corners(rod, bot_point, bot_width, top_point, top_width) != clipping_code::None)
@@ -122,7 +122,7 @@ void g3_draw_rod_tmap(grs_canvas &canvas, grs_bitmap &bitmap, const g3s_point &b
 		light,
 	}};
 
-	g3_draw_tmap(canvas, rod.point_list, uvl_list, lrgb_list, bitmap);
+	g3_draw_tmap(canvas, rod.point_list, uvl_list, lrgb_list, bitmap, tmap_drawer_ptr);
 }
 
 #if !DXX_USE_OGL

common/include/3d.h
@@ -245,7 +245,6 @@ void g3_add_delta_vec(g3s_point &dest,const g3s_point &src,const vms_vector &del
 //Drawing functions:
 
 //draw a flat-shaded face.
-//returns 1 if off screen, 0 if drew
 void _g3_draw_poly(grs_canvas &, std::span<cg3s_point *const> pointlist, uint8_t color);
 template <std::size_t N>
 static inline void g3_draw_poly(grs_canvas &canvas, const uint_fast32_t nv, const std::array<cg3s_point *, N> &pointlist, const uint8_t color)
@@ -254,30 +253,6 @@ static inline void g3_draw_poly(grs_canvas &canvas, const uint_fast32_t nv, cons
 }
 
 constexpr std::integral_constant<std::size_t, 64> MAX_POINTS_PER_POLY{};
-
-//draw a texture-mapped face.
-//returns 1 if off screen, 0 if drew
-void _g3_draw_tmap(grs_canvas &canvas, std::span<cg3s_point *const> pointlist, const g3s_uvl *uvl_list, const g3s_lrgb *light_rgb, grs_bitmap &bm);
-
-template <std::size_t N>
-static inline void g3_draw_tmap(grs_canvas &canvas, unsigned nv, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bm)
-{
-	static_assert(N <= MAX_POINTS_PER_POLY, "too many points in tmap");
-#ifdef DXX_CONSTANT_TRUE
-	if (DXX_CONSTANT_TRUE(nv > N))
-		DXX_ALWAYS_ERROR_FUNCTION("reading beyond array");
-#endif
-	if (nv > N)
-		return;
-	_g3_draw_tmap(canvas, std::span(pointlist).first(nv), &uvl_list[0], &light_rgb[0], bm);
-}
-
-template <std::size_t N>
-requires(N <= MAX_POINTS_PER_POLY)
-static inline void g3_draw_tmap(grs_canvas &canvas, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bm)
-{
-	_g3_draw_tmap(canvas, pointlist, uvl_list.data(), light_rgb.data(), bm);
-}
 
 //draw a sortof sphere - i.e., the 2d radius is proportional to the 3d
 //radius, but not to the distance from the eye
@@ -306,27 +281,10 @@ static inline void g3_check_and_draw_poly(grs_canvas &canvas, const std::array<c
 		g3_draw_poly(canvas, pointlist.size(), pointlist, color);
 }
 
-template <std::size_t N>
-static inline void g3_check_and_draw_tmap(grs_canvas &canvas, unsigned nv, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bm)
-{
-	if (do_facing_check(pointlist))
-		g3_draw_tmap(canvas, nv, pointlist, uvl_list, light_rgb, bm);
-}
-
-template <std::size_t N>
-static inline void g3_check_and_draw_tmap(grs_canvas &canvas, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bm)
-{
-	g3_check_and_draw_tmap(canvas, N, pointlist, uvl_list, light_rgb, bm);
-}
-
 //draws a line. takes two points.
 #if !DXX_USE_OGL
 struct temporary_points_t;
 #endif
-
-//draw a bitmap object that is always facing you
-//returns 1 if off screen, 0 if drew
-void g3_draw_rod_tmap(grs_canvas &, grs_bitmap &bitmap, const g3s_point &bot_point, fix bot_width, const g3s_point &top_point, fix top_width, g3s_lrgb light);
 
 //draws a bitmap with the specified 3d width & height
 //returns 1 if off screen, 0 if drew
@@ -382,11 +340,47 @@ using tmap_drawer_type = void (*)(grs_canvas &, const grs_bitmap &bm, std::span<
 //	(ie, avoids cracking) edge/delta computation.
 void gr_upoly_tmap(grs_canvas &, uint_fast32_t nverts, const std::array<fix, MAX_POINTS_IN_POLY * 2> &vert, uint8_t color);
 #endif
+
+//draw a bitmap object that is always facing you
+void g3_draw_rod_tmap(grs_canvas &, grs_bitmap &bitmap, const g3s_point &bot_point, fix bot_width, const g3s_point &top_point, fix top_width, g3s_lrgb light, tmap_drawer_type tmap_drawer_ptr);
+
+//draw a texture-mapped face.
+void _g3_draw_tmap(grs_canvas &canvas, std::span<cg3s_point *const> pointlist, const g3s_uvl *uvl_list, const g3s_lrgb *light_rgb, grs_bitmap &bm, const tmap_drawer_type tmap_drawer_ptr);
+
+template <std::size_t N>
+static inline void g3_draw_tmap(grs_canvas &canvas, unsigned nv, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bm, const tmap_drawer_type tmap_drawer_ptr)
+{
+	static_assert(N <= MAX_POINTS_PER_POLY, "too many points in tmap");
+#ifdef DXX_CONSTANT_TRUE
+	if (DXX_CONSTANT_TRUE(nv > N))
+		DXX_ALWAYS_ERROR_FUNCTION("reading beyond array");
+#endif
+	if (nv > N)
+		return;
+	_g3_draw_tmap(canvas, std::span(pointlist).first(nv), &uvl_list[0], &light_rgb[0], bm, tmap_drawer_ptr);
+}
+
+template <std::size_t N>
+requires(N <= MAX_POINTS_PER_POLY)
+static inline void g3_draw_tmap(grs_canvas &canvas, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bm, const tmap_drawer_type tmap_drawer_ptr)
+{
+	_g3_draw_tmap(canvas, pointlist, uvl_list.data(), light_rgb.data(), bm, tmap_drawer_ptr);
+}
+
+template <std::size_t N>
+static inline void g3_check_and_draw_tmap(grs_canvas &canvas, unsigned nv, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bm, const tmap_drawer_type tmap_drawer_ptr)
+{
+	if (do_facing_check(pointlist))
+		g3_draw_tmap(canvas, nv, pointlist, uvl_list, light_rgb, bm, tmap_drawer_ptr);
+}
+
+template <std::size_t N>
+static inline void g3_check_and_draw_tmap(grs_canvas &canvas, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bm, const tmap_drawer_type tmap_drawer_ptr)
+{
+	g3_check_and_draw_tmap(canvas, N, pointlist, uvl_list, light_rgb, bm, tmap_drawer_ptr);
+}
+
 void g3_draw_line(const g3_draw_line_context &context, cg3s_point &p0, cg3s_point &p1);
-
-void g3_set_special_render(tmap_drawer_type tmap_drawer);
-
-extern tmap_drawer_type tmap_drawer_ptr;
 
 }
 

common/include/interp.h
@@ -41,7 +41,7 @@ struct glow_values_t : public std::array<fix, glow_array_size> {};
 
 //calls the object interpreter to render an object.  The object renderer
 //is really a seperate pipeline. returns true if drew
-void g3_draw_polygon_model(grs_bitmap *const *model_bitmaps, polygon_model_points &Interp_point_list, grs_canvas &, submodel_angles anim_angles, g3s_lrgb model_light, const glow_values_t *glow_values, const uint8_t *p);
+void g3_draw_polygon_model(grs_bitmap *const *model_bitmaps, polygon_model_points &Interp_point_list, grs_canvas &, tmap_drawer_type tmap_drawer_ptr, submodel_angles anim_angles, g3s_lrgb model_light, const glow_values_t *glow_values, const uint8_t *p);
 
 //init code for bitmap models
 int16_t g3_init_polygon_model(std::span<uint8_t> model);
@@ -54,7 +54,7 @@ void g3_validate_polygon_model(std::span<uint8_t> model);
 #ifdef dsx
 namespace dsx {
 //alternate interpreter for morphing object
-void g3_draw_morphing_model(grs_canvas &, const uint8_t *model_ptr, grs_bitmap *const *model_bitmaps, submodel_angles anim_angles, g3s_lrgb light, const vms_vector *new_points, polygon_model_points &Interp_point_list);
+void g3_draw_morphing_model(grs_canvas &, tmap_drawer_type tmap_drawer_ptr, const uint8_t *model_ptr, grs_bitmap *const *model_bitmaps, submodel_angles anim_angles, g3s_lrgb light, const vms_vector *new_points, polygon_model_points &Interp_point_list);
 
 // check a polymodel for it's color and return it
 int g3_poly_get_color(const uint8_t *model_ptr);

common/include/ogl_init.h
@@ -116,10 +116,10 @@ void ogl_cache_level_textures();
 }
 #endif
 
-void _g3_draw_tmap_2(grs_canvas &, std::span<const g3s_point *const> pointlist, std::span<const g3s_uvl, 4> uvl_list, std::span<const g3s_lrgb, 4> light_rgb, grs_bitmap &bmbot, grs_bitmap &bm, texture2_rotation_low orient);
+void _g3_draw_tmap_2(grs_canvas &, std::span<const g3s_point *const> pointlist, std::span<const g3s_uvl, 4> uvl_list, std::span<const g3s_lrgb, 4> light_rgb, grs_bitmap &bmbot, grs_bitmap &bm, texture2_rotation_low orient, tmap_drawer_type tmap_drawer_ptr);
 
 template <std::size_t N>
-static inline void g3_draw_tmap_2(grs_canvas &canvas, const unsigned nv, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bmbot, grs_bitmap &bm, const texture2_rotation_low orient)
+static inline void g3_draw_tmap_2(grs_canvas &canvas, const unsigned nv, const std::array<cg3s_point *, N> &pointlist, const std::array<g3s_uvl, N> &uvl_list, const std::array<g3s_lrgb, N> &light_rgb, grs_bitmap &bmbot, grs_bitmap &bm, const texture2_rotation_low orient, const tmap_drawer_type tmap_drawer_ptr)
 {
 	static_assert(N <= MAX_POINTS_PER_POLY, "too many points in tmap");
 #ifdef DXX_CONSTANT_TRUE
@@ -128,7 +128,7 @@ static inline void g3_draw_tmap_2(grs_canvas &canvas, const unsigned nv, const s
 #endif
 	if (nv > N)
 		return;
-	_g3_draw_tmap_2(canvas, std::span(pointlist).first(nv), uvl_list, light_rgb, bmbot, bm, orient);
+	_g3_draw_tmap_2(canvas, std::span(pointlist).first(nv), uvl_list, light_rgb, bmbot, bm, orient, tmap_drawer_ptr);
 }
 
 namespace dcx {

common/main/polyobj.h
@@ -93,10 +93,10 @@ struct polymodel : prohibit_void_ptr<polymodel>
 class submodel_angles
 {
 	using array_type = const std::array<vms_angvec, MAX_SUBMODELS>;
-	array_type *p;
+	array_type *p{};
 public:
-	submodel_angles(std::nullptr_t) : p(nullptr) {}
-	submodel_angles(array_type &a) : p(&a) {}
+	submodel_angles(std::nullptr_t) : p{nullptr} {}
+	submodel_angles(array_type &a) : p{&a} {}
 	explicit operator bool() const { return p != nullptr; }
 	typename array_type::const_reference operator[](std::size_t i) const
 	{
@@ -167,8 +167,8 @@ public:
 #ifdef dsx
 namespace dsx {
 // draw a polygon model
-void draw_polygon_model(const enumerated_array<polymodel, MAX_POLYGON_MODELS, polygon_model_index> &, grs_canvas &, const vms_vector &pos, const vms_matrix &orient, submodel_angles anim_angles, const polygon_model_index model_num, unsigned flags, g3s_lrgb light, const glow_values_t *glow_values, alternate_textures);
-void draw_polygon_model(grs_canvas &, const vms_vector &pos, const vms_matrix &orient, submodel_angles anim_angles, const polymodel &model_num, unsigned flags, g3s_lrgb light, const glow_values_t *glow_values, alternate_textures);
+void draw_polygon_model(const enumerated_array<polymodel, MAX_POLYGON_MODELS, polygon_model_index> &, grs_canvas &, tmap_drawer_type tmap_drawer_ptr, const vms_vector &pos, const vms_matrix &orient, submodel_angles anim_angles, const polygon_model_index model_num, unsigned flags, g3s_lrgb light, const glow_values_t *glow_values, alternate_textures);
+void draw_polygon_model(grs_canvas &, tmap_drawer_type tmap_drawer_ptr, const vms_vector &pos, const vms_matrix &orient, submodel_angles anim_angles, const polymodel &model_num, unsigned flags, g3s_lrgb light, const glow_values_t *glow_values, alternate_textures);
 }
 #endif
 

commit b750c0e4415af2be2bad91288b1307c9f4f9f63f

Fix SDL-only build of morph.cpp, polyobj.cpp
Commit 9cefda24e587 added a reference to `draw_tmap`, but this symbol is
declared in different headers for OpenGL vs SDL-only.  These files only
included the header that declares it for OpenGL, so the symbol is
undeclared in SDL-only.  Add the appropriate header so that the symbol
is declared for SDL-only.

Fixes: 9cefda24e587b59597670e9d615ed427b9558f44 ("Pass tmap_drawer_type as parameter, rather than using a global")

similar/main/morph.cpp
@@ -49,6 +49,10 @@ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "d_zip.h"
 #include "partial_range.h"
 
+#if !DXX_USE_OGL
+#include "texmap.h"
+#endif
+
 using std::max;
 
 namespace dcx {

similar/main/polyobj.cpp
@@ -48,6 +48,8 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "render.h"
 #if DXX_USE_OGL
 #include "ogl_init.h"
+#else
+#include "texmap.h"
 #endif
 #include "bm.h"
 