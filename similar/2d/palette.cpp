dxx-rebirth/dxx-rebirth

commit 1c13d3c8d3d73e97618a8d8cf2df10372010127d

Improve error reporting for PHYSFSX_openReadBuffered
Return the PHYSFS error code on failure, so that callers can report why
the open failed.

similar/2d/palette.cpp
@@ -146,21 +146,22 @@ void gr_use_palette_table(const char * filename )
 {
 	int fsize;
 
-	auto fp = PHYSFSX_openReadBuffered(filename);
+	auto &&[fp, physfserr] = PHYSFSX_openReadBuffered(filename);
+	if (!fp)
+	{
 #if defined(DXX_BUILD_DESCENT_I)
-#define FAILURE_FORMAT	"Can't open palette file <%s>"
+		Error("Failed to open palette file <%s>: %s", filename, PHYSFS_getErrorByCode(physfserr));
 #elif defined(DXX_BUILD_DESCENT_II)
-#define FAILURE_FORMAT	"Can open neither palette file <%s> nor default palette file <" DEFAULT_LEVEL_PALETTE ">"
 	// the following is a hack to enable the loading of d2 levels
 	// even if only the d2 mac shareware datafiles are present.
 	// However, if the pig file is present but the palette file isn't,
 	// the textures in the level will look wierd...
-	if (!fp)
-		fp = PHYSFSX_openReadBuffered( DEFAULT_LEVEL_PALETTE );
+		auto &&[fp2, physfserr2] = PHYSFSX_openReadBuffered(DEFAULT_LEVEL_PALETTE);
+		if (!fp)
+			Error("Failed to open both palette file <%s> and default palette file <" DEFAULT_LEVEL_PALETTE ">: (\"%s\", \"%s\")", filename, PHYSFS_getErrorByCode(physfserr), PHYSFS_getErrorByCode(physfserr2));
+		fp = std::move(fp2);
 #endif
-	if (!fp)
-		Error(FAILURE_FORMAT,
-		      filename);
+	}
 
 	fsize	= PHYSFS_fileLength( fp );
 	Assert( fsize == 9472 );

common/include/physfsx.h
@@ -493,7 +493,7 @@ extern int PHYSFSX_rename(const char *oldpath, const char *newpath);
 
 #define PHYSFSX_exists(F,I)	((I) ? PHYSFSX_exists_ignorecase(F) : PHYSFS_exists(F))
 int PHYSFSX_exists_ignorecase(const char *filename);
-RAIIPHYSFS_File PHYSFSX_openReadBuffered(const char *filename);
+std::pair<RAIIPHYSFS_File, PHYSFS_ErrorCode> PHYSFSX_openReadBuffered(const char *filename);
 RAIIPHYSFS_File PHYSFSX_openWriteBuffered(const char *filename);
 extern void PHYSFSX_addArchiveContent();
 extern void PHYSFSX_removeArchiveContent();

common/main/cmd.cpp
@@ -393,9 +393,9 @@ static void cmd_exec(unsigned long argc, const char *const *const argv)
 		cmd_insertf("help %s", argv[0]);
 		return;
 	}
-	auto f = PHYSFSX_openReadBuffered(argv[1]);
+	auto &&[f, physfserr] = PHYSFSX_openReadBuffered(argv[1]);
 	if (!f) {
-		con_printf(CON_CRITICAL, "exec: %s not found", argv[1]);
+		con_printf(CON_CRITICAL, "exec: failed to open \"%s\": %s", argv[1], PHYSFS_getErrorByCode(physfserr));
 		return;
 	}
 	std::forward_list<cmd_queue_t> l;

common/misc/hmp.cpp
@@ -51,7 +51,7 @@ hmp_file::~hmp_file()
 
 std::unique_ptr<hmp_file> hmp_open(const char *filename) {
 	int data, tempo;
-	auto fp = PHYSFSX_openReadBuffered(filename);
+	auto fp = PHYSFSX_openReadBuffered(filename).first;
 
 	if (!fp)
 		return NULL;

common/ui/keypad.cpp
@@ -371,9 +371,9 @@ int ui_pad_read( int n, const char * filename )
 	int linenumber = 0;
 	int keycode, functionnumber;
 
-	auto infile = PHYSFSX_openReadBuffered(filename);
+	auto &&[infile, physfserr] = PHYSFSX_openReadBuffered(filename);
 	if (!infile) {
-		Warning( "Could not find %s\n", filename );
+		Warning("UI pad: failed to open \"%s\": %s\n", filename, PHYSFS_getErrorByCode(physfserr));
 		return 0;
 	}
 	auto &kpn = *(KeyPad[n] = std::make_unique<UI_KEYPAD>());

common/ui/menubar.cpp
@@ -806,11 +806,10 @@ int menubar_init(grs_canvas &canvas, const char *const file)
 	// This method should be faster than explicitly setting all the variables (I think)
 	Menu = {};
 
-	auto infile = PHYSFSX_openReadBuffered(file);
-
+	auto &&[infile, physfserr] = PHYSFSX_openReadBuffered(file);
 	if (!infile)
 	{
-		Warning("Could not find %s\n", file);
+		Warning("Menubar: failed to open \"%s\": %s\n", file, PHYSFS_getErrorByCode(physfserr));
 		return 0;
 	}
 

d1x-rebirth/main/custom.cpp
@@ -253,7 +253,7 @@ static int load_pigpog(const d_fname &pogname)
 	grs_bitmap *bmp;
 	digi_sound *snd;
 	uint8_t *p;
-	auto f = PHYSFSX_openReadBuffered(pogname);
+	auto f = PHYSFSX_openReadBuffered(pogname).first;
 	int i, j, rc = -1;
 	unsigned int x = 0;
 
@@ -478,7 +478,7 @@ static void load_hxm(const d_fname &hxmname)
 	auto &Robot_joints = LevelSharedRobotJointState.Robot_joints;
 	unsigned int repl_num;
 	int i;
-	auto f = PHYSFSX_openReadBuffered(hxmname);
+	auto f = PHYSFSX_openReadBuffered(hxmname).first;
 	int n_items;
 
 	if (!f)

d2x-rebirth/main/movie.cpp
@@ -516,17 +516,18 @@ static int init_subtitles(d_subtitle_state &SubtitleState, const char *const fil
 		return 0;
 	}
 
-	auto ifile = PHYSFSX_openReadBuffered(filename);		//try text version
+	auto &&[ifile, physfserr] = PHYSFSX_openReadBuffered(filename);		//try text version
 
 	if (!ifile) {								//no text version, try binary version
 		char filename2[FILENAME_LEN];
 		change_filename_extension(filename2, filename, ".txb");
-		ifile = PHYSFSX_openReadBuffered(filename2);
-		if (!ifile)
+		auto &&[ifile2, physfserr2] = PHYSFSX_openReadBuffered(filename2);
+		if (!ifile2)
 		{
-			con_printf(CON_VERBOSE, "Rebirth: skipping subtitles because cannot open \"%s\" or \"%s\"", filename, filename2);
+			con_printf(CON_VERBOSE, "Rebirth: skipping subtitles because cannot open \"%s\" or \"%s\" (\"%s\", \"%s\")", filename, filename2, PHYSFS_getErrorByCode(physfserr), PHYSFS_getErrorByCode(physfserr2));
 			return 0;
 		}
+		ifile = std::move(ifile2);
 		have_binary = 1;
 		con_printf(CON_VERBOSE, "Rebirth: found encoded subtitles in \"%s\"", filename2);
 	}

commit 840f18dbf3f49a1e8ad942dc324098e22f9b159a

Factor out opening requested palette file
Reduce the lifetime of the PhysFS error code.  Fix a bug that would
report an error if the requested palette was not found, even if the
fallback was loaded successfully.

Fixes: 1c13d3c8d3d73e97618a8d8cf2df10372010127d ("Improve error reporting for PHYSFSX_openReadBuffered")

similar/2d/palette.cpp
@@ -94,6 +94,63 @@ static unsigned Num_computed_colors;
 
 namespace dsx {
 
+namespace {
+
+RAIIPHYSFS_File gr_open_palette_file(const char *const filename)
+{
+	if (auto [fp_requested_palette, physfserr_requested_palette]{PHYSFSX_openReadBuffered(filename)}; fp_requested_palette)
+	{
+		/* gcc interprets `return fp_requested_palette;` to move
+		 * `fp_requested_palette` to the caller.
+		 *
+		 * clang interprets `return fp_requested_palette;` to copy
+		 * `fp_requested_palette` to the caller, then errors out because the
+		 * copy constructor is inaccessible.
+		 *
+		 * Use an explicit `std::move` to cause clang to use the move
+		 * constructor.  From inspection, gcc generates the same code with an
+		 * explicit `std::move` as with an implicit move, so this is not a
+		 * pessimizing move.  However, for future proofing, only call
+		 * `std::move` when using clang.
+		 *
+		 * clang version results:
+		 * <clang-16: untested
+		 * clang-16: fails
+		 * clang-17: fails
+		 * >clang-17: untested
+		 */
+		return
+#ifdef __clang__
+			std::move
+#endif
+			(fp_requested_palette);
+	}
+	else
+	{
+#if defined(DXX_BUILD_DESCENT_I)
+		Error("Failed to open palette file <%s>: %s", filename, PHYSFS_getErrorByCode(physfserr_requested_palette));
+#elif defined(DXX_BUILD_DESCENT_II)
+	// the following is a hack to enable the loading of d2 levels
+	// even if only the d2 mac shareware datafiles are present.
+	// However, if the pig file is present but the palette file isn't,
+	// the textures in the level will look wierd...
+		static char default_level_palette[]{DEFAULT_LEVEL_PALETTE};
+		if (auto [fp_fallback_default_palette, physfserr_fallback_default_palette]{PHYSFSX_openReadBuffered_updateCase(default_level_palette)}; fp_fallback_default_palette)
+		{
+			return
+#ifdef __clang__
+				std::move
+#endif
+				(fp_fallback_default_palette);
+		}
+		else
+			Error("Failed to open both palette file <%s> and default palette file <" DEFAULT_LEVEL_PALETTE ">: (\"%s\", \"%s\")", filename, PHYSFS_getErrorByCode(physfserr_requested_palette), PHYSFS_getErrorByCode(physfserr_fallback_default_palette));
+#endif
+	}
+}
+
+}
+
 #if defined(DXX_BUILD_DESCENT_II)
 void gr_copy_palette(palette_array_t &gr_palette, const palette_array_t &pal)
 {
@@ -105,25 +162,8 @@ void gr_copy_palette(palette_array_t &gr_palette, const palette_array_t &pal)
 
 void gr_use_palette_table(const char * filename )
 {
+	auto fp{gr_open_palette_file(filename)};
 	int fsize;
-
-	auto &&[fp, physfserr] = PHYSFSX_openReadBuffered(filename);
-	if (!fp)
-	{
-#if defined(DXX_BUILD_DESCENT_I)
-		Error("Failed to open palette file <%s>: %s", filename, PHYSFS_getErrorByCode(physfserr));
-#elif defined(DXX_BUILD_DESCENT_II)
-	// the following is a hack to enable the loading of d2 levels
-	// even if only the d2 mac shareware datafiles are present.
-	// However, if the pig file is present but the palette file isn't,
-	// the textures in the level will look wierd...
-		static char default_level_palette[]{DEFAULT_LEVEL_PALETTE};
-		auto &&[fp2, physfserr2] = PHYSFSX_openReadBuffered_updateCase(default_level_palette);
-		if (!fp)
-			Error("Failed to open both palette file <%s> and default palette file <" DEFAULT_LEVEL_PALETTE ">: (\"%s\", \"%s\")", filename, PHYSFS_getErrorByCode(physfserr), PHYSFS_getErrorByCode(physfserr2));
-		fp = std::move(fp2);
-#endif
-	}
 
 	fsize	= PHYSFS_fileLength( fp );
 	Assert( fsize == 9472 );