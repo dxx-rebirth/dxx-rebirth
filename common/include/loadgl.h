dxx-rebirth/dxx-rebirth

commit 793ee24eee12179b1a6f7594bf419019f6a543ec

Block including GL-specific headers in SDL-only builds
These headers may unconditionally include OpenGL system headers, which
may not be available on a minimal system targeting an SDL-only build.
Prevent their inclusion to avoid this unnecessary dependency.

Reported-by: mikrosk <https://github.com/dxx-rebirth/dxx-rebirth/issues/731>

common/include/loadgl.h
@@ -14,6 +14,10 @@
 #ifndef __LOADGL_H__
 #define __LOADGL_H__
 
+#if !DXX_USE_OGL
+#error "This file can only be included in OpenGL enabled builds."
+#endif
+
 #ifdef _WIN32
 #include <windows.h>
 #define OGLFUNCCALL	__stdcall

common/include/ogl_init.h
@@ -12,6 +12,10 @@
 #ifndef _OGL_INIT_H_ 
 #define _OGL_INIT_H_
 
+#if !DXX_USE_OGL
+#error "This file can only be included in OpenGL enabled builds."
+#endif
+
 #include "dxxsconf.h"
 #ifdef _MSC_VER
 #include <windows.h>

common/include/ogl_sync.h
@@ -12,6 +12,10 @@
 
 #pragma once
 
+#if !DXX_USE_OGL
+#error "This file can only be included in OpenGL enabled builds."
+#endif
+
 #include <memory>
 #include "maths.h"
 #include "args.h"
@@ -19,7 +23,6 @@
 
 namespace dcx {
 
-#if DXX_USE_OGL
 class ogl_sync {
 	class sync_deleter
 	{
@@ -39,6 +42,5 @@ class ogl_sync {
 		void init(SyncGLMethod sync_method, int wait);
 		void deinit();
 };
-#endif
 
 }

common/include/d_gl.h
@@ -5,6 +5,10 @@
  * terms and a link to the Git history.
  */
 
+#if !DXX_USE_OGL
+#error "This file can only be included in OpenGL enabled builds."
+#endif
+
 #ifdef _WIN32
 #include "loadgl.h"
 #else

similar/main/config.cpp
@@ -41,7 +41,9 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "u_mem.h"
 #include "physfsx.h"
 #include "nvparse.h"
+#if DXX_USE_OGL
 #include "ogl_init.h"
+#endif
 #include <memory>
 
 namespace dcx {

commit 5e50d293436394d719da9971e14f1aa57887af6f

loadgl.h, ogl_init.h, ogl_sync.h: fix check_headers_include=1 build
Fixes: 793ee24eee12179b1a6f7594bf419019f6a543ec ("Block including GL-specific headers in SDL-only builds")

common/include/loadgl.h
@@ -14,6 +14,8 @@
 #ifndef __LOADGL_H__
 #define __LOADGL_H__
 
+#include "dxxsconf.h"
+
 #if !DXX_USE_OGL
 #error "This file can only be included in OpenGL enabled builds."
 #endif

common/include/ogl_init.h
@@ -12,11 +12,12 @@
 #ifndef _OGL_INIT_H_ 
 #define _OGL_INIT_H_
 
+#include "dxxsconf.h"
+
 #if !DXX_USE_OGL
 #error "This file can only be included in OpenGL enabled builds."
 #endif
 
-#include "dxxsconf.h"
 #ifdef _MSC_VER
 #include <windows.h>
 #include <stddef.h>

common/include/ogl_sync.h
@@ -12,6 +12,8 @@
 
 #pragma once
 
+#include "dxxsconf.h"
+
 #if !DXX_USE_OGL
 #error "This file can only be included in OpenGL enabled builds."
 #endif