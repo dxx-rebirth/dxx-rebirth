dxx-rebirth/dxx-rebirth

commit bbf1577403ce64f55db59707b9504b72b2d93028

Allow GET_INTEL_INT to return int32_t
Some callers cast the result from uint32_t to int32_t.  Allow the
function to return int32_t if requested, so that the caller does not
need an explicit cast.

common/include/byteutil.h
@@ -108,11 +108,15 @@ static constexpr int32_t INTEL_INT(const int32_t &x)
 }
 #undef byteutil_choose_endian
 
+template <std::integral result_type = std::uint32_t>
+requires(
+	sizeof(result_type) == sizeof(uint32_t)
+)
 [[nodiscard]]
-static inline uint32_t GET_INTEL_INT(const auto *p)
+static inline result_type GET_INTEL_INT(const auto *p)
 {
-	uint32_t u;
-	byteutil_unaligned_copy(uint32_t, u, p);
+	result_type u;
+	byteutil_unaligned_copy(result_type, u, p);
 	return INTEL_INT(u);
 }
 

similar/main/multi.cpp
@@ -222,9 +222,9 @@ std::optional<network_state> build_network_state_from_untrusted(const uint8_t un
 vms_vector multi_get_vector(const std::span<const uint8_t, 12> buf)
 {
 	return vms_vector{
-		static_cast<int32_t>(GET_INTEL_INT(&buf[0])),
-		static_cast<int32_t>(GET_INTEL_INT(&buf[4])),
-		static_cast<int32_t>(GET_INTEL_INT(&buf[8])),
+		GET_INTEL_INT<int32_t>(&buf[0]),
+		GET_INTEL_INT<int32_t>(&buf[4]),
+		GET_INTEL_INT<int32_t>(&buf[8]),
 	};
 }
 
@@ -2277,7 +2277,7 @@ static void multi_do_play_sound(object_array &Objects, const playernum_t pnum, c
 
 	const unsigned sound_num = buf[2];
 	const uint8_t once = buf[3];
-	const fix volume = GET_INTEL_INT(&buf[4]);
+	const fix volume{GET_INTEL_INT<int32_t>(&buf[4])};
 
 	const auto objnum = plr.objnum;
 	digi_link_sound_to_object(sound_num, vcobjptridx(objnum), 0, volume, static_cast<sound_stack>(once));
@@ -2297,7 +2297,7 @@ static void multi_do_score(fvmobjptr &vmobjptr, const playernum_t pnum, const mu
 		return;
 	}
 
-	const int score = GET_INTEL_INT(&buf[2]);
+	const auto score{GET_INTEL_INT<int32_t>(&buf[2])};
 	if (Newdemo_state == ND_STATE_RECORDING) {
 		newdemo_record_multi_score(pnum, score);
 	}
@@ -2402,11 +2402,10 @@ static void multi_do_hostage_door_status(fvmsegptridx &vmsegptridx, wall_array &
 	// Update hit point status of a door
 
 	int count = 1;
-	fix hps;
 
 	const wallnum_t wallnum{GET_INTEL_SHORT(&buf[count])};
 	count += 2;
-	hps = GET_INTEL_INT(&buf[count]);           count += 4;
+	const fix hps{GET_INTEL_INT<int32_t>(&buf[count])};           count += 4;
 
 	auto &vmwallptr = Walls.vmptr;
 	auto &w = *vmwallptr(wallnum);
@@ -3879,7 +3878,7 @@ static void multi_do_drop_weapon(fvmobjptr &vmobjptr, const playernum_t pnum, co
 	const auto powerup_id = static_cast<powerup_type_t>(buf[1]);
 	const objnum_t remote_objnum = GET_INTEL_SHORT(&buf[2]);
 	const uint16_t ammo = GET_INTEL_SHORT(&buf[4]);
-	const auto seed = GET_INTEL_INT(&buf[6]);
+	const auto seed{GET_INTEL_INT(&buf[6])};
 	const auto &&objnum = spit_powerup(LevelUniqueObjectState, LevelSharedSegmentState, LevelUniqueSegmentState, Vclip, vmobjptr(vcplayerptr(pnum)->objnum), powerup_id, seed);
 	if (objnum == object_none)
 		return;
@@ -4104,7 +4103,7 @@ void multi_send_heartbeat ()
 
 static void multi_do_heartbeat(const multiplayer_rspan<multiplayer_command_t::MULTI_HEARTBEAT> buf)
 {
-	const fix num = GET_INTEL_INT(&buf[1]);
+	const fix num{GET_INTEL_INT<int32_t>(&buf[1])};
 	ThisLevelTime = d_time_fix(num);
 }
 
@@ -4171,7 +4170,7 @@ namespace {
 
 static void multi_do_seismic(multiplayer_rspan<multiplayer_command_t::MULTI_SEISMIC> buf)
 {
-	const fix duration = GET_INTEL_INT(&buf[1]);
+	const fix duration{GET_INTEL_INT<int32_t>(&buf[1])};
 	LevelUniqueSeismicState.Seismic_disturbance_end_time = GameTime64 + duration;
 	digi_play_sample (SOUND_SEISMIC_DISTURBANCE_START, F1_0);
 }
@@ -4227,7 +4226,7 @@ static void multi_do_flags(fvmobjptr &vmobjptr, const playernum_t pnum, const mu
 {
 	if (pnum!=Player_num)
 	{
-		const uint32_t flags = GET_INTEL_INT(&buf[2]);
+		const auto flags{GET_INTEL_INT(&buf[2])};
 		vmobjptr(vcplayerptr(pnum)->objnum)->ctype.player_info.powerup_flags = player_flags(flags);
 	}
 }
@@ -4603,7 +4602,7 @@ static void multi_do_drop_flag(const playernum_t pnum, const multiplayer_rspan<m
 	auto &vmobjptr = Objects.vmptr;
 	const auto powerup_id = static_cast<powerup_type_t>(buf[1]);
 	const objnum_t remote_objnum = GET_INTEL_SHORT(&buf[2]);
-	const int seed = GET_INTEL_INT(&buf[6]);
+	const auto seed{GET_INTEL_INT<int32_t>(&buf[6])};
 
 	const auto &&objp = vmobjptr(vcplayerptr(pnum)->objnum);
 
@@ -4933,11 +4932,10 @@ static void multi_do_save_game(const multiplayer_rspan<multiplayer_command_t::MU
 {
 	int count = 1;
 	ubyte slot;
-	uint id;
 	d_game_unique_state::savegame_description desc;
 
 	slot = buf[count];			count += 1;
-	id = GET_INTEL_INT(&buf[count]);			count += 4;
+	const auto id{GET_INTEL_INT(&buf[count])};			count += 4;
 	memcpy(desc.data(), &buf[count], desc.size());
 	desc.back() = 0;
 
@@ -4956,7 +4954,7 @@ static void multi_do_restore_game(const multiplayer_rspan<multiplayer_command_t:
 	ubyte slot;
 
 	slot = buf[count];			count += 1;
-	const unsigned id = GET_INTEL_INT(&buf[count]);			count += 4;
+	const unsigned id{GET_INTEL_INT(&buf[count])};			count += 4;
 
 	multi_restore_game( slot, id );
 }

similar/main/net_udp.cpp
@@ -2491,16 +2491,15 @@ static void net_udp_read_object_packet(const d_level_shared_robot_info_state &Le
 	// Object from another net player we need to sync with
 	sbyte obj_owner;
 	static int mode = 0, object_count = 0, my_pnum = 0;
-	int remote_objnum = 0, nobj = 0, loc = 5;
-	
-	nobj = GET_INTEL_INT(data + 1);
+	unsigned loc{5};
+	const int nobj{GET_INTEL_INT<int32_t>(&data[1])};
 
 	for (int i = 0; i < nobj; i++)
 	{
-		const unsigned uobjnum = GET_INTEL_INT(data + loc);
+		const unsigned uobjnum{GET_INTEL_INT(&data[loc])};
 		objnum_t objnum = uobjnum;                         loc += 4;
 		obj_owner = data[loc];                                      loc += 1;
-		remote_objnum = GET_INTEL_INT(data + loc);                  loc += 4;
+		const int remote_objnum{GET_INTEL_INT<int32_t>(&data[loc])};                  loc += 4;
 
 		if (objnum == object_none) 
 		{
@@ -5599,13 +5598,12 @@ static int net_udp_noloss_validate_mdata(uint32_t pkt_num, ubyte sender_pnum, co
 void net_udp_noloss_got_ack(const std::span<const uint8_t> data)
 {
 	int len = 0;
-	uint32_t pkt_num = 0;
 	ubyte sender_pnum = 0, dest_pnum = 0;
 
 															len++;
 	sender_pnum = data[len];											len++;
 	dest_pnum = data[len];												len++;
-	pkt_num = GET_INTEL_INT(&data[len]);										len += 4;
+	const uint32_t pkt_num{GET_INTEL_INT(&data[len])};										len += 4;
 
 	for (int i = 0; i < UDP_mdata_queue_highest; i++)
 	{

similar/main/polyobj.cpp
@@ -79,8 +79,7 @@ static int32_t pof_read_int(const std::span<const uint8_t> bufp, std::size_t &Po
 {
 	const auto s = bufp.subspan(Pof_addr, 4);
 	Pof_addr += 4;
-	const auto r = GET_INTEL_INT(s.data());
-	return r;
+	return {GET_INTEL_INT<int32_t>(s.data())};
 }
 
 static std::size_t pof_cfread(void *const dst, const size_t elsize, const std::span<const uint8_t> bufp, std::size_t &Pof_addr)

commit bf28adb9127a2d1c6e7b2d534cdd2d156bdfcb4a

byteutil.h: fix check_headers_include=1 build
Fixes: bbf1577403ce64f55db59707b9504b72b2d93028 ("Allow GET_INTEL_INT to return int32_t")

common/include/byteutil.h
@@ -32,6 +32,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #pragma once
 
+#include <concepts>
 #include <cstdint>
 #include <string.h>    // for memcpy
 #include <type_traits>