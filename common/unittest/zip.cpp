dxx-rebirth/dxx-rebirth

commit cda3e7febbda52dd32d170ea9771e7231e8906bf

Extend zip from forward_iterator to bidirectional_iterator
Propagate the most derived iterator type.

common/main/d_zip.h
@@ -6,6 +6,7 @@
  */
 #pragma once
 
+#include <cstdint>
 #include <iterator>
 #include <tuple>
 #include <type_traits>
@@ -35,6 +36,12 @@ void increment_iterator(std::tuple<range_iterator_type...> &iterator, std::index
 }
 
 template <std::size_t... N, typename... range_iterator_type>
+void decrement_iterator(std::tuple<range_iterator_type...> &iterator, std::index_sequence<N...>)
+{
+	discard_arguments(--(std::get<N>(iterator))...);
+}
+
+template <std::size_t... N, typename... range_iterator_type>
 auto dereference_iterator(const std::tuple<range_iterator_type...> &iterator, std::index_sequence<N...>)
 {
 	/* std::make_tuple is not appropriate here, because the result of
@@ -51,6 +58,15 @@ auto dereference_iterator(const std::tuple<range_iterator_type...> &iterator, st
 		decltype(*(std::get<N>(iterator)))...
 		>(*(std::get<N>(iterator))...);
 }
+
+/* This is declared, but never defined.  It is used only for a decltype()
+ * expression.
+ *
+ * Given a tuple of iterator types, return the most-derived iterator_category
+ * that all the iterator types satisfy.
+ */
+template <typename... range_iterator_type>
+typename std::common_type<typename std::iterator_traits<range_iterator_type>::iterator_category...>::type common_iterator_category(std::tuple<range_iterator_type...>);
 
 /* The overloads of get_static_size are declared, but never defined.  They
  * exist only for use in decltype() expressions.
@@ -276,7 +292,7 @@ public:
 	using value_type = decltype(d_zip::detail::dereference_iterator(std::declval<range_iterator_type>(), index_sequence_type()));
 	using pointer = value_type *;
 	using reference = value_type &;
-	using iterator_category = std::forward_iterator_tag;
+	using iterator_category = decltype(d_zip::detail::common_iterator_category(std::declval<range_iterator_type>()));
 	auto operator*() const
 	{
 		return d_zip::detail::dereference_iterator(*this, index_sequence_type());
@@ -291,14 +307,29 @@ public:
 	 */
 	zip_iterator operator++(int)
 	{
-		auto result = *this;
-		d_zip::detail::increment_iterator(*this, index_sequence_type());
+		auto result{*this};
+		++ *this;
+		return result;
+	}
+	zip_iterator &operator--()
+		requires(std::derived_from<std::bidirectional_iterator_tag, iterator_category>)
+	{
+		d_zip::detail::decrement_iterator(*this, index_sequence_type());
+		return *this;
+	}
+	zip_iterator operator--(int)
+		requires(std::derived_from<std::bidirectional_iterator_tag, iterator_category>)
+	{
+		auto result{*this};
+		-- *this;
 		return result;
 	}
 	auto operator-(const zip_iterator &i) const
+		requires(std::derived_from<std::random_access_iterator_tag, iterator_category>)
 	{
 		return std::get<0>(static_cast<const range_iterator_type &>(*this)) - std::get<0>(static_cast<const range_iterator_type &>(i));
 	}
+	constexpr bool operator==(const zip_iterator &) const = default;
 	constexpr bool operator==(const range_sentinel_type &i) const
 	{
 		return d_zip::detail::compare_zip_iterators<range_iterator_type>(*this, i, index_sequence_type());

commit 6aa572150f76dbf566d4e994dee93976093a7c83

zip: fix transposed types for std::derived_from
The C++11 type trait `std::is_base_of` is defined as
`std::is_base_of<Base, Derived>`.  However, the C++20 concept
`derived_from` is defined as `std::derived_from<Derived, Base>`, with
arguments in the opposite order.  `zip` used `std::derived_from` as if
it were `std::derived_from<Base, Derived>`.  This allowed zip to accept
types that should have been rejected.  Swap the argument order.

Add `static_assert` based unit tests to check that `zip` is a
`forward_range` / `bidirectional_range` for appropriate inputs.

Omit testing for `std::ranges::random_access_range` because `zip` does
not satisfy `random_access_range`.  `zip` supports some aspects of
random access, but not all aspects.  Nothing in Rebirth needs the full
support at this time, and the support is not worth adding solely to
create a unit test.

Fixes: cda3e7febbda52dd32d170ea9771e7231e8906bf ("Extend zip from forward_iterator to bidirectional_iterator")

common/main/d_zip.h
@@ -315,20 +315,24 @@ public:
 		return result;
 	}
 	zip_iterator &operator--()
-		requires(std::derived_from<std::bidirectional_iterator_tag, iterator_category>)
+		requires(std::derived_from<iterator_category, std::bidirectional_iterator_tag>)
 	{
 		for_index_sequence::decrement_iterator(static_cast<range_iterator_tuple &>(*this));
 		return *this;
 	}
 	zip_iterator operator--(int)
-		requires(std::derived_from<std::bidirectional_iterator_tag, iterator_category>)
+		requires(
+			requires(zip_iterator i) {
+				-- i;
+			}
+		)
 	{
 		auto result{*this};
 		-- *this;
 		return result;
 	}
 	auto operator-(const zip_iterator &i) const
-		requires(std::derived_from<std::random_access_iterator_tag, iterator_category>)
+		requires(std::derived_from<iterator_category, std::random_access_iterator_tag>)
 	{
 		return std::get<0>(static_cast<const range_iterator_tuple &>(*this)) - std::get<0>(static_cast<const range_iterator_tuple &>(i));
 	}

common/unittest/zip.cpp
@@ -1,6 +1,7 @@
 #include "d_range.h"
 #include "d_zip.h"
 #include <array>
+#include <forward_list>
 #include <vector>
 
 #define BOOST_TEST_DYN_LINK
@@ -188,3 +189,12 @@ static_assert(d_zip::detail::range_static_extent{1} == d_zip::detail::minimum_st
 static_assert(d_zip::detail::range_static_extent{1} == d_zip::detail::minimum_static_size(d_zip::detail::range_extent_sequence<d_zip::detail::range_static_extent{std::dynamic_extent}, d_zip::detail::range_static_extent{1}>()));
 static_assert(d_zip::detail::range_static_extent{2} == d_zip::detail::minimum_static_size(d_zip::detail::range_extent_sequence<d_zip::detail::range_static_extent{std::dynamic_extent}, d_zip::detail::range_static_extent{2}, d_zip::detail::range_static_extent{3}>()));
 static_assert(d_zip::detail::range_static_extent{2} == d_zip::detail::minimum_static_size(d_zip::detail::range_extent_sequence<d_zip::detail::range_static_extent{std::dynamic_extent}, d_zip::detail::range_static_extent{3}, d_zip::detail::range_static_extent{std::dynamic_extent}, d_zip::detail::range_static_extent{2}>()));
+
+using zip_forward_only = decltype(zip(std::declval<std::array<int, 2> &>(), std::declval<std::forward_list<int> &>()));
+using zip_bidirectional_only = decltype(zip(std::declval<std::array<int, 2> &>(), std::declval<std::list<int> &>()));
+
+static_assert(std::ranges::forward_range<zip_forward_only>);
+static_assert(not std::ranges::bidirectional_range<zip_forward_only>);
+
+static_assert(std::ranges::bidirectional_range<zip_bidirectional_only>);
+static_assert(not std::ranges::random_access_range<zip_bidirectional_only>);