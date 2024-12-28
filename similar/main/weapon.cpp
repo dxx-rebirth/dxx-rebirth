dxx-rebirth/dxx-rebirth

commit 49bc677729333064317e4a91fd57e960525ad1f2

Use `sound_effect` for arguments and members, where appropriate
Replace varied uses of byte/short/int for sound effects with
sound_effect.

similar/main/weapon.cpp
@@ -1268,8 +1268,6 @@ void init_smega_detonates()
 	LevelUniqueSeismicState.Earthshaker_detonate_times = {};
 }
 
-constexpr std::integral_constant<int, sound_effect::SOUND_SEISMIC_DISTURBANCE_START> Seismic_sound{};
-
 namespace {
 
 static void start_seismic_sound()
@@ -1277,7 +1275,7 @@ static void start_seismic_sound()
 	if (LevelUniqueSeismicState.Next_seismic_sound_time)
 		return;
 	LevelUniqueSeismicState.Next_seismic_sound_time = GameTime64 + d_rand()/2;
-	digi_play_sample_looping(Seismic_sound, F1_0, -1, -1);
+	digi_play_sample_looping(sound_effect::SOUND_SEISMIC_DISTURBANCE_START, F1_0, -1, -1);
 }
 
 static void apply_seismic_effect(const int entry_fc)
@@ -1840,8 +1838,8 @@ void postprocess_udt(auto && /* accessor */, v2_weapon_info &w)
 	w.hires_picture = w.picture;
 }
 
-DEFINE_SERIAL_UDT_TO_MESSAGE(v2_weapon_info, w, (w.render, w.persistent, w.model_num, serial::pad<1>(), w.model_num_inner, serial::pad<1>(), w.flash_vclip, w.robot_hit_vclip, w.flash_sound, w.wall_hit_vclip, w.fire_count, w.robot_hit_sound, w.ammo_usage, w.weapon_vclip, w.wall_hit_sound, w.destroyable, w.matter, w.bounce, w.homing_flag, w.speedvar, w.flags, w.flash, w.afterburner_size, w.energy_usage, w.fire_wait, w.bitmap, w.blob_size, w.flash_size, w.impact_size, w.strength, w.speed, w.mass, w.drag, w.thrust, w.po_len_to_width_ratio, w.light, w.lifetime, w.damage_radius, w.picture));
-DEFINE_SERIAL_UDT_TO_MESSAGE(weapon_info, w, (w.render, w.persistent, w.model_num, serial::pad<1>(), w.model_num_inner, serial::pad<1>(), w.flash_vclip, w.robot_hit_vclip, w.flash_sound, w.wall_hit_vclip, w.fire_count, w.robot_hit_sound, w.ammo_usage, w.weapon_vclip, w.wall_hit_sound, w.destroyable, w.matter, w.bounce, w.homing_flag, w.speedvar, w.flags, w.flash, w.afterburner_size, w.children, w.energy_usage, w.fire_wait, w.multi_damage_scale, w.bitmap, w.blob_size, w.flash_size, w.impact_size, w.strength, w.speed, w.mass, w.drag, w.thrust, w.po_len_to_width_ratio, w.light, w.lifetime, w.damage_radius, w.picture, w.hires_picture));
+DEFINE_SERIAL_UDT_TO_MESSAGE(v2_weapon_info, w, (w.render, w.persistent, w.model_num, serial::pad<1>(), w.model_num_inner, serial::pad<1>(), w.flash_vclip, w.robot_hit_vclip, w.flash_sound, serial::pad<1>(), w.wall_hit_vclip, w.fire_count, w.robot_hit_sound, serial::pad<1>(), w.ammo_usage, w.weapon_vclip, w.wall_hit_sound, serial::pad<1>(), w.destroyable, w.matter, w.bounce, w.homing_flag, w.speedvar, w.flags, w.flash, w.afterburner_size, w.energy_usage, w.fire_wait, w.bitmap, w.blob_size, w.flash_size, w.impact_size, w.strength, w.speed, w.mass, w.drag, w.thrust, w.po_len_to_width_ratio, w.light, w.lifetime, w.damage_radius, w.picture));
+DEFINE_SERIAL_UDT_TO_MESSAGE(weapon_info, w, (w.render, w.persistent, w.model_num, serial::pad<1>(), w.model_num_inner, serial::pad<1>(), w.flash_vclip, w.robot_hit_vclip, w.flash_sound, serial::pad<1>(), w.wall_hit_vclip, w.fire_count, w.robot_hit_sound, serial::pad<1>(), w.ammo_usage, w.weapon_vclip, w.wall_hit_sound, serial::pad<1>(), w.destroyable, w.matter, w.bounce, w.homing_flag, w.speedvar, w.flags, w.flash, w.afterburner_size, w.children, w.energy_usage, w.fire_wait, w.multi_damage_scale, w.bitmap, w.blob_size, w.flash_size, w.impact_size, w.strength, w.speed, w.mass, w.drag, w.thrust, w.po_len_to_width_ratio, w.light, w.lifetime, w.damage_radius, w.picture, w.hires_picture));
 ASSERT_SERIAL_UDT_MESSAGE_SIZE(weapon_info, 125);
 ASSERT_SERIAL_UDT_MESSAGE_SIZE(v2_weapon_info, 118);
 #endif

common/include/digi_audio.h
@@ -9,12 +9,11 @@
 
 #include "maths.h"
 
-#ifdef __cplusplus
-
 #ifdef DXX_BUILD_DESCENT
 namespace dcx {
 enum class sound_pan : int;
 enum class sound_channel : uint8_t;
+enum sound_effect : uint8_t;
 struct sound_object;
 constexpr std::integral_constant<int, 16> digi_max_channels{};
 }
@@ -22,7 +21,7 @@ namespace dsx {
 int digi_audio_init();
 void digi_audio_close();
 void digi_audio_stop_all_channels();
-sound_channel digi_audio_start_sound(short, fix, sound_pan, int, int, int, sound_object *);
+sound_channel digi_audio_start_sound(sound_effect, fix, sound_pan, int, int, int, sound_object *);
 int digi_audio_is_channel_playing(sound_channel);
 void digi_audio_set_channel_volume(sound_channel, int);
 void digi_audio_set_channel_pan(sound_channel, sound_pan);
@@ -30,6 +29,4 @@ void digi_audio_stop_sound(sound_channel);
 void digi_audio_end_sound(sound_channel);
 void digi_audio_set_digi_volume(int);
 }
-#endif
-
 #endif

common/include/digi_mixer.h
@@ -70,6 +70,6 @@ void digi_mixer_stop_all_channels();
 int digi_mixer_init();
 }
 namespace dsx {
-sound_channel digi_mixer_start_sound(short, fix, sound_pan, int, int, int, sound_object *);
+sound_channel digi_mixer_start_sound(sound_effect, fix, sound_pan, int, int, int, sound_object *);
 }
 #endif

common/main/digi.h
@@ -102,19 +102,19 @@ extern int digi_init();
 extern void digi_close();
 
 // Volume is max at F1_0.
-extern void digi_play_sample( int sndnum, fix max_volume );
-extern void digi_play_sample_once( int sndnum, fix max_volume );
+void digi_play_sample(sound_effect sndnum, fix max_volume);
+void digi_play_sample_once(sound_effect sndnum, fix max_volume);
 #ifdef DXX_BUILD_DESCENT
-void digi_link_sound_to_object(unsigned soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once);
-void digi_kill_sound_linked_to_segment(vmsegidx_t segnum, sidenum_t sidenum, int soundnum);
-void digi_link_sound_to_pos(unsigned soundnum, vcsegptridx_t segnum, sidenum_t sidenum, const vms_vector &pos, int forever, fix max_volume);
+void digi_link_sound_to_object(sound_effect soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once);
+void digi_kill_sound_linked_to_segment(vmsegidx_t segnum, sidenum_t sidenum, sound_effect soundnum);
+void digi_link_sound_to_pos(sound_effect soundnum, vcsegptridx_t segnum, sidenum_t sidenum, const vms_vector &pos, int forever, fix max_volume);
 // Same as above, but you pass the max distance sound can be heard.  The old way uses f1_0*256 for max_distance.
-void digi_link_sound_to_object2(unsigned soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once, vm_distance max_distance);
-void digi_link_sound_to_object3(unsigned soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once, vm_distance max_distance, int loop_start, int loop_end);
+void digi_link_sound_to_object2(sound_effect soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once, vm_distance max_distance);
+void digi_link_sound_to_object3(sound_effect soundnum, vcobjptridx_t objnum, uint8_t forever, fix max_volume, sound_stack once, vm_distance max_distance, int loop_start, int loop_end);
 void digi_kill_sound_linked_to_object(vcobjptridx_t);
 #endif
 
-void digi_play_sample_3d(int soundno, sound_pan angle, int volume); // Volume from 0-0x7fff
+void digi_play_sample_3d(sound_effect soundno, sound_pan angle, int volume); // Volume from 0-0x7fff
 
 extern void digi_init_sounds();
 extern void digi_sync_sounds();
@@ -124,13 +124,13 @@ extern void digi_set_digi_volume( int dvolume );
 extern void digi_pause_digi_sounds();
 extern void digi_resume_digi_sounds();
 
-extern int digi_xlat_sound(int soundno);
+sound_effect digi_xlat_sound(sound_effect soundno);
 
 void digi_stop_sound(sound_channel channel);
 
 // Volume 0-F1_0
 constexpr sound_object *sound_object_none = nullptr;
-sound_channel digi_start_sound(short soundnum, fix volume, sound_pan pan, int looping, int loop_start, int loop_end, sound_object *);
+sound_channel digi_start_sound(sound_effect soundnum, fix volume, sound_pan pan, int looping, int loop_start, int loop_end, sound_object *);
 
 // Stops all sounds that are playing
 void digi_stop_all_channels();
@@ -142,12 +142,12 @@ void digi_set_channel_pan(sound_channel channel, sound_pan pan);
 void digi_set_channel_volume(sound_channel channel, int volume);
 int digi_is_channel_playing(sound_channel channel);
 
-extern void digi_play_sample_looping( int soundno, fix max_volume,int loop_start, int loop_end );
+void digi_play_sample_looping(sound_effect soundno, fix max_volume,int loop_start, int loop_end );
 extern void digi_change_looping_volume( fix volume );
 extern void digi_stop_looping_sound();
 
 // Plays a queued voice sound.
-extern void digi_start_sound_queued( short soundnum, fix volume );
+void digi_start_sound_queued(sound_effect soundnum, fix volume);
 
 // Following declarations are for the runtime switching system
 

common/main/effects.h
@@ -68,7 +68,7 @@ struct eclip : public prohibit_void_ptr<eclip>
 	vclip_index dest_vclip;     //what vclip to play when exploding
 	int     dest_eclip;     //what eclip to play when exploding
 	fix     dest_size;      //3d size of explosion
-	int     sound_num;      //what sound this makes
+	sound_effect sound_num;      //what sound this makes
 	segnum_t     segnum;
 	sidenum_t sidenum; //what seg & side, for one-shot clips
 };

common/main/multi.h
@@ -368,7 +368,7 @@ static inline laser_level map_granted_flags_to_laser_level(const packed_spawn_gr
 player_flags map_granted_flags_to_player_flags(packed_spawn_granted_items grant);
 uint_fast32_t map_granted_flags_to_primary_weapon_flags(packed_spawn_granted_items grant);
 uint16_t map_granted_flags_to_vulcan_ammo(packed_spawn_granted_items grant);
-void multi_digi_link_sound_to_pos(int soundnum, vcsegptridx_t segnum, sidenum_t sidenum, const vms_vector &pos, int forever, fix max_volume);
+void multi_digi_link_sound_to_pos(sound_effect soundnum, vcsegptridx_t segnum, sidenum_t sidenum, const vms_vector &pos, int forever, fix max_volume);
 void multi_object_to_object_rw(const object &obj, object_rw *obj_rw);
 void multi_object_rw_to_object(const object_rw *obj_rw, object &obj);
 
@@ -450,8 +450,7 @@ static inline void multi_send_endlevel_start(multi_endlevel_type)
 void multi_send_player_deres(deres_type_t type);
 void multi_send_create_powerup(powerup_type_t powerup_type, vcsegidx_t segnum, vcobjidx_t objnum, const vms_vector &pos);
 }
-void multi_send_play_sound(int sound_num, fix volume, sound_stack once);
-#endif
+void multi_send_play_sound(sound_effect sound_num, fix volume, sound_stack once);
 void multi_send_reappear();
 void multi_send_create_explosion(playernum_t);
 void multi_send_controlcen_fire(const vms_vector &to_target, int gun_num, objnum_t objnum);
@@ -459,11 +458,10 @@ namespace dcx {
 void multi_send_cloak(void);
 void multi_send_decloak(void);
 }
-void multi_digi_play_sample(int sndnum, fix max_volume);
-void multi_digi_play_sample_once(int soundnum, fix max_volume);
+void multi_digi_play_sample(sound_effect sndnum, fix max_volume);
+void multi_digi_play_sample_once(sound_effect soundnum, fix max_volume);
 void multi_send_score(void);
 void multi_send_trigger(trgnum_t trigger);
-#ifdef DXX_BUILD_DESCENT
 #if DXX_BUILD_DESCENT == 2
 namespace dsx {
 void multi_send_flags(playernum_t);

common/main/newdemo.h
@@ -25,7 +25,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #pragma once
 
-#ifdef __cplusplus
 #include "physfsx.h"
 #include "fwd-object.h"
 #include "fwd-segment.h"
@@ -33,6 +32,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include "fwd-weapon.h"
 #include "fwd-window.h"
 #include "dsx-ns.h"
+#include "sounds.h"
 
 #define ND_STATE_NORMAL			0
 #define ND_STATE_RECORDING		1
@@ -86,10 +86,9 @@ void newdemo_record_kill_sound_linked_to_object(vcobjptridx_t);
 void newdemo_start_playback(const char *filename);
 void newdemo_record_morph_frame(vcobjptridx_t);
 }
-#endif
-void newdemo_record_sound_3d_once( int soundno, sound_pan angle, int volume );
-extern void newdemo_record_sound_once( int soundno );
-extern void newdemo_record_sound( int soundno );
+void newdemo_record_sound_3d_once(sound_effect soundno, sound_pan angle, int volume );
+void newdemo_record_sound_once(sound_effect soundno);
+void newdemo_record_sound(sound_effect soundno);
 void newdemo_record_wall_hit_process(segnum_t segnum, sidenum_t side, int damage, int playernum);
 extern void newdemo_record_player_stats(int shields, int energy, int score );
 void newdemo_record_wall_toggle(segnum_t segnum, sidenum_t side);
@@ -106,7 +105,6 @@ extern void newdemo_record_rearview(void);
 extern void newdemo_record_restore_cockpit(void);
 extern void newdemo_record_multi_cloak(int pnum);
 extern void newdemo_record_multi_decloak(int pnum);
-#ifdef DXX_BUILD_DESCENT
 namespace dsx {
 void newdemo_record_player_weapon(primary_weapon_index_t);
 void newdemo_record_player_weapon(secondary_weapon_index_t);
@@ -114,7 +112,6 @@ void newdemo_record_wall_set_tmap_num1(vcsegidx_t seg, sidenum_t side, vcsegidx_
 void newdemo_record_wall_set_tmap_num2(vcsegidx_t seg, sidenum_t side, vcsegidx_t cseg, sidenum_t cside, texture2_value tmap);
 extern void newdemo_set_new_level(int level_num);
 }
-#endif
 extern void newdemo_record_restore_rearview(void);
 
 extern void newdemo_record_multi_death(int pnum);
@@ -128,7 +125,6 @@ extern void newdemo_record_primary_ammo(int new_ammo);
 extern void newdemo_record_secondary_ammo(int new_ammo);
 void newdemo_record_door_opening(segnum_t segnum, sidenum_t side);
 
-#ifdef DXX_BUILD_DESCENT
 void newdemo_record_laser_level(laser_level old_level, laser_level new_level);
 namespace dsx {
 #if DXX_BUILD_DESCENT == 2
@@ -157,7 +153,6 @@ extern window_event_result newdemo_goto_beginning();
 namespace dsx {
 extern void newdemo_stop_playback();
 }
-#endif
 extern void newdemo_start_recording();
 extern void newdemo_stop_recording();
 
@@ -165,7 +160,6 @@ extern int newdemo_swap_endian(const char *filename);
 
 extern int newdemo_get_percent_done();
 
-extern void newdemo_record_link_sound_to_object3( int soundno, objnum_t objnum, fix max_volume, fix  max_distance, int loop_start, int loop_end );
+void newdemo_record_link_sound_to_object3(sound_effect soundno, objnum_t objnum, fix max_volume, fix  max_distance, int loop_start, int loop_end);
 int newdemo_count_demos();
-
 #endif

common/main/powerup.h
@@ -26,20 +26,21 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #pragma once
 #include "fwd-powerup.h"
 #include "pack.h"
+#include "sounds.h"
 
+#ifdef DXX_BUILD_DESCENT
 namespace dcx {
 
 struct powerup_type_info : public prohibit_void_ptr<powerup_type_info>
 {
 	vclip_index vclip_num;
-	int hit_sound;
+	sound_effect hit_sound;
 	fix size;       // 3d size of longest dimension
 	fix light;      // amount of light cast by this powerup, set in bitmaps.tbl
 };
 
 }
 
-#ifdef DXX_BUILD_DESCENT
 namespace dsx {
 
 enum class powerup_type_t : uint8_t

commit 565e9e0156c5e6fdf77f8a82b3e7cb4988dbb9e2

Fix D1 weapon_info parsing
The serialization path for `d1x::weapon_info` lacked a `static_assert`
to check that the size was correct.  This omission allowed a size change
in commit 49bc677729333064317e4a91fd57e960525ad1f2 to go undetected,
resulting in the game reading the wrong number of bytes from the file
each time it tried to deserialize a `d1x::weapon_info`.  In turn, that
caused the loaded `weapon_info` to be invalid, and caused secondary
errors elsewhere as the incorrect file position lead to incorrect loads
of other data structures.

Add the missing `serial::pad<1>()` uses that should have been in
commit 49bc677729333064317e4a91fd57e960525ad1f2.  Also, add a static
assertion on the size, so that any future mistakes are caught at compile
time.

Reported-by: adolfogp <https://github.com/dxx-rebirth/dxx-rebirth/issues/807>
Fixes: 49bc677729333064317e4a91fd57e960525ad1f2 ("Use `sound_effect` for arguments and members, where appropriate")

similar/main/weapon.cpp
@@ -1821,7 +1821,8 @@ class is_cxx_array<enumerated_array<T, NDL, Difficulty_level_type>> : public is_
 }
 
 #if DXX_BUILD_DESCENT == 1
-DEFINE_SERIAL_UDT_TO_MESSAGE(dsx::weapon_info, w, (w.render, w.model_num, w.model_num_inner, w.persistent, w.flash_vclip, w.flash_sound, w.robot_hit_vclip, w.robot_hit_sound, w.wall_hit_vclip, w.wall_hit_sound, w.fire_count, w.ammo_usage, w.weapon_vclip, w.destroyable, w.matter, w.bounce, w.homing_flag, w.dum1, w.dum2, w.dum3, w.energy_usage, w.fire_wait, w.bitmap, w.blob_size, w.flash_size, w.impact_size, w.strength, w.speed, w.mass, w.drag, w.thrust, w.po_len_to_width_ratio, w.light, w.lifetime, w.damage_radius, w.picture));
+DEFINE_SERIAL_UDT_TO_MESSAGE(dsx::weapon_info, w, (w.render, w.model_num, w.model_num_inner, w.persistent, w.flash_vclip, w.flash_sound, serial::pad<1>(), w.robot_hit_vclip, w.robot_hit_sound, serial::pad<1>(), w.wall_hit_vclip, w.wall_hit_sound, serial::pad<1>(), w.fire_count, w.ammo_usage, w.weapon_vclip, w.destroyable, w.matter, w.bounce, w.homing_flag, w.dum1, w.dum2, w.dum3, w.energy_usage, w.fire_wait, w.bitmap, w.blob_size, w.flash_size, w.impact_size, w.strength, w.speed, w.mass, w.drag, w.thrust, w.po_len_to_width_ratio, w.light, w.lifetime, w.damage_radius, w.picture));
+ASSERT_SERIAL_UDT_MESSAGE_SIZE(dsx::weapon_info, 115);
 #elif DXX_BUILD_DESCENT == 2
 namespace {
 