dxx-rebirth/dxx-rebirth

commit 5c514df2177e055b354e7c3657c38f80ecf95b44

Require exact case for `BITMAPS.TBL`, `BITMAPS.BIN`
These appear not to be part of the game data, and were likely used only
during early development.  As such, players are unlikely to have these
files at all.

similar/main/piggy.cpp
@@ -485,7 +485,7 @@ properties_init_result properties_init(d_level_shared_robot_info_state &LevelSha
 	Piggy_fp = PHYSFSX_openReadBuffered(DEFAULT_PIGFILE_REGISTERED).first;
 	if (!Piggy_fp)
 	{
-		if (!PHYSFSX_exists_ignorecase("BITMAPS.TBL") && !PHYSFSX_exists_ignorecase("BITMAPS.BIN"))
+		if (!PHYSFS_exists("BITMAPS.TBL") && !PHYSFS_exists("BITMAPS.BIN"))
 			Error("Cannot find " DEFAULT_PIGFILE_REGISTERED " or BITMAPS.TBL");
 		return properties_init_result::use_gamedata_read_tbl;	// need to run gamedata_read_tbl
 	}
@@ -521,7 +521,7 @@ properties_init_result properties_init(d_level_shared_robot_info_state &LevelSha
 	properties_init_result retval;
 	if (PCSharePig)
 		retval = properties_init_result::shareware;	// run gamedata_read_tbl in shareware mode
-	else if (GameArg.EdiNoBm || (!PHYSFSX_exists_ignorecase("BITMAPS.TBL") && !PHYSFSX_exists_ignorecase("BITMAPS.BIN")))
+	else if (GameArg.EdiNoBm || (!PHYSFS_exists("BITMAPS.TBL") && !PHYSFS_exists("BITMAPS.BIN")))
 	{
 		properties_read_cmp(LevelSharedRobotInfoState, Vclip, Piggy_fp);	// Note connection to above if!!!
 		range_for (auto &i, GameBitmapXlat)

commit d96665375a8bd273dcc0d0b21c87249861b0e00f

properties_init: use case-insensitive search for BITMAPS.BIN
Descent 1 v1.0 ships `bitmaps.bin`, not `BITMAPS.BIN`.  To complicate
the situation, it also ships broken fallback data, so if Rebirth does
not find and use `bitmaps.bin`, then the game crashes:

```
terminate called after throwing an instance of 'partial_range_t<dcx::polymodel*, dcx::polygon_model_index>::partial_range_error'
  what():  similar/main/bm.cpp:212: end 3351824328 past 0x5555557b5b48 end 85 "Polygon_models"
```

Switch back to using a case-insensitive search, so that Rebirth detects
`bitmaps.bin` and reads that instead of the broken fallback data.

Reported-by: retrogamedesigner <https://github.com/dxx-rebirth/dxx-rebirth/issues/812>
Fixes: 5c514df2177e055b354e7c3657c38f80ecf95b44 ("Require exact case for `BITMAPS.TBL`, `BITMAPS.BIN`")

similar/main/piggy.cpp
@@ -523,9 +523,11 @@ properties_init_result properties_init(d_level_shared_robot_info_state &LevelSha
 	HiresGFXAvailable = MacPig;	// for now at least
 
 	properties_init_result retval;
+	static char bitmaps_bin_basename[]{"bitmaps.bin"};
+
 	if (PCSharePig)
 		retval = properties_init_result::shareware;	// run gamedata_read_tbl in shareware mode
-	else if (GameArg.EdiNoBm || (!PHYSFS_exists("BITMAPS.TBL") && !PHYSFS_exists("BITMAPS.BIN")))
+	else if (GameArg.EdiNoBm || (!PHYSFS_exists("BITMAPS.TBL") && !PHYSFSX_exists_ignorecase(bitmaps_bin_basename)))
 	{
 		properties_read_cmp(LevelSharedRobotInfoState, Vclip, Piggy_fp);	// Note connection to above if!!!
 		range_for (auto &i, GameBitmapXlat)