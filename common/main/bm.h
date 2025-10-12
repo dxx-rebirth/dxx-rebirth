dfbf31852078a82ff90be3c78c5acff87c8b8666
Kp <3164941+vLKp@users.noreply.github.com>
2025-10-12 21:14:16 +0000

    Add type alias for cockpit_bitmap type
    This allows removing `constexpr std::size_t N_COCKPIT_BITMAPS`, which
    was used only in the declaration and definition of this type.

--- common/main/bm.h
+++ common/main/bm.h
@@ -166,13 +166,11 @@ extern std::array<polygon_model_index, MAX_OBJTYPE> ObjId;			// ID of a robot, w
 extern fix	ObjStrength[MAX_OBJTYPE];	// initial strength of each object
 
 constexpr std::integral_constant<unsigned, 210> MAX_OBJ_BITMAPS{};
-constexpr std::size_t N_COCKPIT_BITMAPS{4};
 
 #elif DXX_BUILD_DESCENT == 2
 
 extern int Robot_replacements_loaded;
 constexpr std::integral_constant<unsigned, 610> MAX_OBJ_BITMAPS{};
-constexpr std::size_t N_COCKPIT_BITMAPS{6};
 extern unsigned N_ObjBitmaps;
 #endif
 
@@ -195,13 +193,19 @@ enum class cockpit_bitmap_index : uint8_t
 #endif
 };
 
+/* In case of invalid preprocessor directive, set an impossible size of
+ * `SIZE_MAX` to force a compile error.
+ */
+using cockpit_bitmap_array = enumerated_array<bitmap_index, /* N_COCKPIT_BITMAPS = */ (DXX_BUILD_DESCENT == 1 ? 4 : (DXX_BUILD_DESCENT == 2 ? 6 : SIZE_MAX)), cockpit_bitmap_index>;
+
+extern cockpit_bitmap_array cockpit_bitmap;
+
 }
 
 extern int First_multi_bitmap_num;
 void compute_average_rgb(grs_bitmap *bm, std::array<fix, 3> &rgb);
 
 namespace dsx {
-extern enumerated_array<bitmap_index, N_COCKPIT_BITMAPS, cockpit_bitmap_index> cockpit_bitmap;
 void load_robot_replacements(const d_fname &level_name);
 #if DXX_BUILD_DESCENT == 1 || (DXX_BUILD_DESCENT == 2 && DXX_USE_EDITOR)
 // Initializes all bitmaps from BITMAPS.TBL file.
