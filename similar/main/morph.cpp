dxx-rebirth/dxx-rebirth

commit 85a903413784fba925b786b114bd6c7c1b140549

Move morph_data::morph_deltas into a trailing allocation
Initially, this just makes the code more complicated.  In the end, it
will allow choosing the array size dynamically.

similar/main/morph.cpp
@@ -99,7 +99,7 @@ submodel_data parse_model_data_header(const polymodel &pm, const unsigned submod
 
 void *morph_data::operator new(std::size_t, const max_vectors max_vecs)
 {
-	return ::operator new(sizeof(morph_data) + (max_vecs.count * (sizeof(fix) + sizeof(vms_vector))));
+	return ::operator new(sizeof(morph_data) + (max_vecs.count * (sizeof(fix) + sizeof(vms_vector) + sizeof(vms_vector))));
 }
 
 morph_data::ptr morph_data::create(object_base &o)
@@ -111,11 +111,12 @@ morph_data::morph_data(object_base &o) :
 	obj(&o), Morph_sig(o.signature)
 {
 	DXX_POISON_VAR(submodel_active, 0xcc);
-	DXX_POISON_VAR(morph_deltas, 0xcc);
 	const auto morph_times = get_morph_times();
 	DXX_POISON_MEMORY(morph_times.begin(), morph_times.end(), 0xcc);
 	const auto morph_vecs = get_morph_times();
 	DXX_POISON_MEMORY(morph_vecs.begin(), morph_vecs.end(), 0xcc);
+	const auto morph_deltas = get_morph_times();
+	DXX_POISON_MEMORY(morph_deltas.begin(), morph_deltas.end(), 0xcc);
 	DXX_POISON_VAR(n_morphing_points, 0xcc);
 	DXX_POISON_VAR(submodel_startpoints, 0xcc);
 }
@@ -128,6 +129,11 @@ span<fix> morph_data::get_morph_times()
 span<vms_vector> morph_data::get_morph_vecs()
 {
 	return {reinterpret_cast<vms_vector *>(get_morph_times().end()), MAX_VECS};
+}
+
+span<vms_vector> morph_data::get_morph_deltas()
+{
+	return {get_morph_vecs().end(), MAX_VECS};
 }
 
 d_level_unique_morph_object_state::~d_level_unique_morph_object_state() = default;
@@ -227,10 +233,11 @@ static void init_points(const polymodel &pm, const vms_vector *const box_size, c
 
 	const auto morph_times = md->get_morph_times();
 	const auto morph_vecs = md->get_morph_vecs();
+	const auto morph_deltas = md->get_morph_deltas();
 	auto &&zr = zip(
 		unchecked_partial_range(reinterpret_cast<const vms_vector *>(sd.body), sd.nverts),
 		partial_range(morph_vecs, startpoint, endpoint),
-		partial_range(md->morph_deltas, startpoint, endpoint),
+		partial_range(morph_deltas, startpoint, endpoint),
 		partial_range(morph_times, startpoint, endpoint)
 	);
 	range_for (auto &&z, zr)
@@ -264,10 +271,11 @@ static void update_points(const polymodel &pm, const unsigned submodel_num, morp
 
 	const auto morph_times = md->get_morph_times();
 	const auto morph_vecs = md->get_morph_vecs();
+	const auto morph_deltas = md->get_morph_deltas();
 	auto &&zr = zip(
 		unchecked_partial_range(reinterpret_cast<const vms_vector *>(sd.body), sd.nverts),
 		partial_range(morph_vecs, startpoint, endpoint),
-		partial_range(md->morph_deltas, startpoint, endpoint),
+		partial_range(morph_deltas, startpoint, endpoint),
 		partial_range(morph_times, startpoint, endpoint)
 	);
 	range_for (auto &&z, zr)

common/main/morph.h
@@ -74,13 +74,13 @@ struct morph_data : prohibit_void_ptr<morph_data>
 	uint8_t n_submodels_active;
 	physics_info morph_save_phys_info;
 	array<submodel_state, MAX_SUBMODELS> submodel_active;         // which submodels are active
-	array<vms_vector, MAX_VECS> morph_deltas;
 	array<int, MAX_SUBMODELS>
 		n_morphing_points,       // how many active points in each part
 		submodel_startpoints;    // first point for each submodel
 	static ptr create(object_base &);
 	span<fix> get_morph_times();
 	span<vms_vector> get_morph_vecs();
+	span<vms_vector> get_morph_deltas();
 private:
 	static void *operator new(std::size_t bytes, max_vectors);
 	explicit morph_data(object_base &o);

commit e6d9da625fcd1c1d9c1ca5b2050beb3fa06a4aa3

Fix incorrect morph_data poison calls
Due to a copy-paste error in commit 73612f6667df, the variable that
should have been a span covering morph_vecs was instead a span that
redundantly covered morph_times, so the attempt to poison morph_vecs
instead re-poisoned the already poisoned morph_times.  Likewise, a
matching error in commit 85a903413 caused morph_deltas to redundantly
cover morph_times.

Switch to poison the correct variables.

Fixes: 73612f6667df672fe530559cdc90142b3811cafb ("Move morph_data::morph_vecs into a trailing allocation")
Fixes: 85a903413784fba925b786b114bd6c7c1b140549 ("Move morph_data::morph_deltas into a trailing allocation")

similar/main/morph.cpp
@@ -215,12 +215,9 @@ morph_data::morph_data(object_base &o, const max_vectors m) :
 	obj(&o), Morph_sig(o.signature), max_vecs(m)
 {
 	DXX_POISON_VAR(submodel_active, 0xcc);
-	const auto morph_times{get_morph_times()};
-	DXX_POISON_MEMORY(morph_times, 0xcc);
-	const auto morph_vecs{get_morph_times()};
-	DXX_POISON_MEMORY(morph_vecs, 0xcc);
-	const auto morph_deltas{get_morph_times()};
-	DXX_POISON_MEMORY(morph_deltas, 0xcc);
+	DXX_POISON_MEMORY(get_morph_times(), 0xcc);
+	DXX_POISON_MEMORY(get_morph_vecs(), 0xcc);
+	DXX_POISON_MEMORY(get_morph_deltas(), 0xcc);
 	DXX_POISON_VAR(n_morphing_points, 0xcc);
 	DXX_POISON_VAR(submodel_startpoints, 0xcc);
 }