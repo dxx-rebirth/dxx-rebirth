dxx-rebirth/dxx-rebirth

commit 8decd6ec9931f8301ac3d9f63d99ec4a7486778a

Simplify DXX_ALWAYS_ERROR_FUNCTION
Since this should never be invoked, simplify the definition.  The new
version is slightly less informative in case a logic error causes it to
be called, but is far shorter and easier to understand.

SConstruct
@@ -1653,6 +1653,12 @@ to the marked function.
 help:assume compiler supports __attribute__((error))
 """
 		self._check_function_dce_attribute(context, 'error')
+		context.sconf.config_h_text += '''
+#ifndef DXX_SCONF_NO_INCLUDES
+__attribute_error("must never be called")
+void DXX_ALWAYS_ERROR_FUNCTION(const char *);
+#endif
+'''
 	def _check_function_dce_attribute(self,context,attribute):
 		__attribute__ = '__%s__' % attribute
 		f = '''
@@ -1873,119 +1879,6 @@ available.
 		Define = context.sconf.Define
 		Define('DXX_BEGIN_COMPOUND_STATEMENT', t[0])
 		Define('DXX_END_COMPOUND_STATEMENT', t[1])
-
-	@_custom_test
-	def check_compiler_always_error_optimizer(self,context,
-	# Good case: <gcc-6 takes this path.  Declare a function with
-	# __attribute__((__error__)) and call it.
-	_macro_value_simple=_quote_macro_value('''( DXX_BEGIN_COMPOUND_STATEMENT {
-	void F() __attribute_error(S);
-	F();
-} DXX_END_COMPOUND_STATEMENT )
-'''),
-	# Bad case: >=gcc-6 takes this path.  Declare a local scope class
-	# with a static method marked __attribute__((__error__)) and call
-	# that method.
-	_macro_value_complicated=_quote_macro_value('''( DXX_BEGIN_COMPOUND_STATEMENT {
-	struct DXX_ALWAYS_ERROR_FUNCTION {
-		__attribute_error(S)
-		/* The function must be declared inline because it is a member
-		** method, but must be marked noinline to discourage gcc from
-		** inlining it.  If it is inlined, then the error message is not
-		** generated even if F() is called.
-		**
-		** To make matters worse, if this function calls an undefined
-		** global scope function with __attribute__((__error__)), the
-		** compiler retains that call even when F() is never called.  That
-		** does not produce a compile error, but does cause a link error
-		** when the intentionally undefined global function is not found.
-		**/
-		__attribute__((__noinline__))
-		static void F()
-		{
-			/* If the function body is empty, noinline is not sufficient
-			** to prevent the compiler from skipping the error message.
-			** Use a no-op asm() with clobber statements to discourage
-			** gcc from inlining F().  This is only called if the build
-			** is supposed to fail, so the clobber has no performance
-			** consequences.
-			**/
-			__asm__ __volatile__("" ::: "memory", "cc");
-		}
-	};
-	DXX_ALWAYS_ERROR_FUNCTION::F();
-} DXX_END_COMPOUND_STATEMENT )
-''')
-	):
-		'''
-Rebirth defines a macro DXX_ALWAYS_ERROR_FUNCTION that, when expanded,
-results in a call to an undefined function.  If the optimizer cannot
-prove the call to be unreachable, the build fails.  This is used to
-diagnose certain types of always-incorrect code, such as accessing
-elements beyond the end of an array.
-
-Functions declared in local scope make the name available until the end
-of the containing block.  The name is treated as if it was declared in
-the same scope as the function itself, so a name declared inside a
-function in an anonymous namespace is also considered to be in the
-anonymous namespace.
-
-Starting in gcc-6, declaring a function while in local scope in an
-anonymous namespace triggers a compiler diagnostic about "used but never
-defined" even if the optimizer proves the call to be unreachable.
-Proving the call to be unreachable would cause the function not to be
-used.  Failing to prove it to be unreachable would lead to the
-__attribute__((__error__)) marker triggering an error message from the
-compiler.  Before gcc-6, the compiler permitted such declarations
-provided that the optimizer proved the function was never called.  The
-compiler never permitted such declarations when the optimizer failed to
-prove the function was never called.
-
-This SConf test checks whether the compiler warns when that construct is
-used.  If the compiler does not warn, a simple definition of
-DXX_ALWAYS_ERROR_FUNCTION is used.  If the compiler warns, a complicated
-and fragile definition of DXX_ALWAYS_ERROR_FUNCTION is used.
-As stated in `import this`:
-
-	Simple is better than complex.
-	Complex is better than complicated.
-
-Therefore, we prefer the simple form whenever the compiler allows it,
-even though the complicated form works for both old and new compilers.
-'''
-		context.sconf.Define('DXX_ALWAYS_ERROR_FUNCTION(F,S)',
-			_macro_value_simple if self.Compile(context, text='''
-namespace {
-void f()
-{
-	(void)("i"[0] == 's' &&
-		(
-			(
-				{
-					void e() __attribute_error("");
-					e();
-				}
-			), 0
-		)
-	);
-}
-}
-''', main='f();', msg='whether compiler allows dead calls to undefined functions in the anonymous namespace')	\
-			else _macro_value_complicated
-		, '''
-Declare a function named F and immediately call it.  If gcc's
-__attribute__((__error__)) is supported, __attribute_error will expand
-to use __attribute__((__error__)) with the explanatory string S, causing
-it to be a compilation error if this expression is not optimized out.
-
-Use this macro to implement static assertions that depend on values that
-are known to the optimizer, but are not considered "compile time
-constant expressions" for the purpose of the static_assert intrinsic.
-
-C++11 deleted functions cannot be used here because the compiler raises
-an error for the call before the optimizer has an opportunity to delete
-the call via a dead code elimination pass.
-''')
 
 	@_custom_test
 	def check_attribute_always_inline(self,context):

common/include/3d.h
@@ -268,7 +268,7 @@ static inline void g3_draw_tmap(grs_canvas &canvas, unsigned nv, const std::arra
 	static_assert(N <= MAX_POINTS_PER_POLY, "too many points in tmap");
 #ifdef DXX_CONSTANT_TRUE
 	if (DXX_CONSTANT_TRUE(nv > N))
-		DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_tmap_overread, "reading beyond array");
+		DXX_ALWAYS_ERROR_FUNCTION("reading beyond array");
 #endif
 	if (nv > N)
 		return;

common/include/console.h
@@ -68,7 +68,7 @@ void con_printf(con_priority_wrapper level, const char *fmt, ...) __attribute_fo
 #ifdef DXX_CONSTANT_TRUE
 #define DXX_CON_PRINTF_CHECK_TRAILING_NEWLINE(F)	\
 	(DXX_CONSTANT_TRUE(sizeof((F)) > 1 && (F)[sizeof((F)) - 2] == '\n') &&	\
-		(DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_trailing_newline, "trailing literal newline on con_printf"), 0)),
+		(DXX_ALWAYS_ERROR_FUNCTION("trailing literal newline on con_printf"), 0)),
 #else
 #define DXX_CON_PRINTF_CHECK_TRAILING_NEWLINE(C)
 #endif

common/include/fmtcheck.h
@@ -17,7 +17,7 @@
 #ifdef DXX_CONSTANT_TRUE
 #define DXX_PRINTF_CHECK_HAS_NONTRIVIAL_FORMAT_STRING_(V,P,FMT)	\
 	static_cast<void>(DXX_CONSTANT_TRUE((FMT) && (FMT)[0] == '%' && (FMT)[1] == 's' && (FMT)[2] == 0) &&	\
-		(DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_trivial_string_specifier_argument_##V, "bare %s argument to " #V "; use " #P " directly"), 0))
+		(DXX_ALWAYS_ERROR_FUNCTION("bare %s argument to " #V "; use " #P " directly"), 0))
 #else
 #define DXX_PRINTF_CHECK_HAS_NONTRIVIAL_FORMAT_STRING_(V,P,FMT)	\
 	((void)(FMT))

common/include/ntstring.h
@@ -58,7 +58,7 @@ public:
 		{
 #ifdef DXX_CONSTANT_TRUE
 			if (DXX_CONSTANT_TRUE(n > N))
-				DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_overread, "read size exceeds array size");
+				DXX_ALWAYS_ERROR_FUNCTION("read size exceeds array size");
 #endif
 			return copy_if(out_offset, i.data(), n);
 		}

common/include/ogl_init.h
@@ -119,7 +119,7 @@ static inline void g3_draw_tmap_2(grs_canvas &canvas, const unsigned nv, const s
 	static_assert(N <= MAX_POINTS_PER_POLY, "too many points in tmap");
 #ifdef DXX_CONSTANT_TRUE
 	if (DXX_CONSTANT_TRUE(nv > N))
-		DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_tmap_overread, "reading beyond array");
+		DXX_ALWAYS_ERROR_FUNCTION("reading beyond array");
 #endif
 	if (nv > N)
 		return;

common/include/partial_range.h
@@ -233,7 +233,7 @@ inline void check_range_bounds(const char *file, unsigned line, const char *estr
 	 * not define DXX_CONSTANT_TRUE and the macro expands to nothing.
 	 */
 #define PARTIAL_RANGE_COMPILE_CHECK_BOUND(EXPR,S)	\
-	(DXX_CONSTANT_TRUE(EXPR > d) && (DXX_ALWAYS_ERROR_FUNCTION(partial_range_will_always_throw_##S, #S " will always throw"), 0))
+	(DXX_CONSTANT_TRUE(EXPR > d) && (DXX_ALWAYS_ERROR_FUNCTION(#S " will always throw"), 0))
 #else
 #define PARTIAL_RANGE_COMPILE_CHECK_BOUND(EXPR,S)	static_cast<void>(0)
 #endif
@@ -341,7 +341,7 @@ inline partial_range_t<iterator_type, index_type> unchecked_partial_range_advanc
 	 * likely indicates a bug.
 	 */
 	if (DXX_CONSTANT_TRUE(!(index_begin < index_end)))
-		DXX_ALWAYS_ERROR_FUNCTION(partial_range_is_always_empty, "offset never less than length");
+		DXX_ALWAYS_ERROR_FUNCTION("offset never less than length");
 #endif
 #ifdef DXX_HAVE_BUILTIN_OBJECT_SIZE
 	/* Avoid iterator dereference if range is empty */

common/include/physfsx.h
@@ -48,17 +48,17 @@
 #ifdef DXX_CONSTANT_TRUE
 #define _DXX_PHYSFS_CHECK_SIZE(S,C,v)	DXX_CONSTANT_TRUE((std::size_t{S} * std::size_t{C}) > (v))
 #define DXX_PHYSFS_CHECK_READ_SIZE_OBJECT_SIZE(S,C,v)	\
-	(void)(__builtin_object_size(v, 1) != static_cast<size_t>(-1) && _DXX_PHYSFS_CHECK_SIZE(S,C,__builtin_object_size(v, 1)) && (DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_overwrite, "read size exceeds element size"), 0))
+	(void)(__builtin_object_size(v, 1) != static_cast<size_t>(-1) && _DXX_PHYSFS_CHECK_SIZE(S,C,__builtin_object_size(v, 1)) && (DXX_ALWAYS_ERROR_FUNCTION("read size exceeds element size"), 0))
 #define DXX_PHYSFS_CHECK_READ_SIZE_ARRAY_SIZE(S,C)	\
-	(void)(_DXX_PHYSFS_CHECK_SIZE(S,C,sizeof(v)) && (DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_overwrite, "read size exceeds array size"), 0))
+	(void)(_DXX_PHYSFS_CHECK_SIZE(S,C,sizeof(v)) && (DXX_ALWAYS_ERROR_FUNCTION("read size exceeds array size"), 0))
 #define DXX_PHYSFS_CHECK_WRITE_SIZE_OBJECT_SIZE(S,C,v)	\
-	(void)(__builtin_object_size(v, 1) != static_cast<size_t>(-1) && _DXX_PHYSFS_CHECK_SIZE(S,C,__builtin_object_size(v, 1)) && (DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_overwrite, "write size exceeds element size"), 0))
+	(void)(__builtin_object_size(v, 1) != static_cast<size_t>(-1) && _DXX_PHYSFS_CHECK_SIZE(S,C,__builtin_object_size(v, 1)) && (DXX_ALWAYS_ERROR_FUNCTION("write size exceeds element size"), 0))
 #define DXX_PHYSFS_CHECK_WRITE_ELEMENT_SIZE_CONSTANT(S,C)	\
 	((void)(dxx_builtin_constant_p(S) || dxx_builtin_constant_p(C) || \
-		(DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_nonconstant_size, "array element size is not constant"), 0)))
+		(DXX_ALWAYS_ERROR_FUNCTION("array element size is not constant"), 0)))
 #define DXX_PHYSFS_CHECK_WRITE_SIZE_ARRAY_SIZE(S,C)	\
 	((void)(_DXX_PHYSFS_CHECK_SIZE(S,C,sizeof(v)) && \
-		(DXX_ALWAYS_ERROR_FUNCTION(dxx_trap_overread, "write size exceeds array size"), 0)))
+		(DXX_ALWAYS_ERROR_FUNCTION("write size exceeds array size"), 0)))
 #else
 #define DXX_PHYSFS_CHECK_READ_SIZE_OBJECT_SIZE(S,C,v)	((void)0)
 #define DXX_PHYSFS_CHECK_READ_SIZE_ARRAY_SIZE(S,C)	((void)0)

commit 3e6eccf1d7cb3988c89ef9ba91f22d3561daf40c

Remove unused `_quote_macro_value`
Commit 8decd6ec9931f8301ac3d9f63d99ec4a7486778a removed all uses of
`_quote_macro_value`.  Remove the definition.

Fixes: 8decd6ec9931f8301ac3d9f63d99ec4a7486778a ("Simplify DXX_ALWAYS_ERROR_FUNCTION")

SConstruct
@@ -856,8 +856,6 @@ I%(N)s a%(N)s()
 		# Force all tests to be Link tests when LTO is enabled.
 		self.Compile = self.Link if user_settings.lto else self._Compile
 		self.custom_tests = tuple(t for t in self.custom_tests if all(predicate(user_settings) for predicate in t.predicate))
-	def _quote_macro_value(v: str) -> str:
-		return v.strip().replace('\n', ' \\\n')
 	def _check_sconf_forced(self, calling_function: str) -> tuple:
 		return self._check_forced(calling_function), self._check_expected(calling_function)
 	@staticmethod