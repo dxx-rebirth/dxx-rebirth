dxx-rebirth/dxx-rebirth

commit 7630a0130129a63bb981271a7395f80a1f6efb60

Decorate initialization exceptions with the causing profile
When using the `dxx=profile1+profile2,` syntax, an error initializing a
profile may be unclear about which profile failed.  Decorate the
exception to help the user identify the failed profile.

SConstruct
@@ -5235,6 +5235,19 @@ class DXXProgram(DXXCommon):
 		print(f'===== {self.PROGRAM_NAME} {extra_version} {compute_extra_version.revparse_HEAD} =====')
 		user_settings.register_variables(prefix, variables, filtered_help)
 
+	# Run `init()`, but decorate any exception that occurs with a description
+	# of the profile for this instance.  This allows the user to see which
+	# profile triggered the exception.
+	def init_with_decoration(self, substenv):
+		try:
+			self.init(substenv)
+		except Exception as e:
+			# Patch the exception's arguments instead of using Python exception
+			# chaining.  Using `raise Exception('') from e` causes SCons not to
+			# show the traceback for `e`.
+			e.args = (f'Failed to initialize profile {self._argument_prefix_list!r}: {e.args[0]}',) + e.args[1:]
+			raise
+
 	def init(self,substenv):
 		user_settings = self.user_settings
 		user_settings.read_variables(self, self.variables, substenv)
@@ -5662,7 +5675,7 @@ def main(register_program,_d1xp=D1XProgram,_d2xp=D2XProgram):
 	d2x=prefix-list  Enable D2X-Rebirth with prefix-list modifiers
 	dxx=VALUE        Equivalent to d1x=VALUE d2x=VALUE
 """ +	\
-		''.join([f'{d.program_message_prefix}:\n{d.init(substenv)}' for d in dxx])
+		''.join([f'{d.program_message_prefix}:\n{d.init_with_decoration(substenv)}' for d in dxx])
 	)
 	if not dxx:
 		return

commit 27ee191d0903b5c3f8f15391da140f53cefcc9be

Fix `scons -h` output
Commit 7630a0130129a63bb981271a7395f80a1f6efb60 changed the `Help` setup
to always show `None` instead of the program-specific help text.  Change
`init_with_decoration` to return the help text.

Fixes: 7630a0130129a63bb981271a7395f80a1f6efb60 ("Decorate initialization exceptions with the causing profile")

SConstruct
@@ -5236,9 +5236,9 @@ class DXXProgram(DXXCommon):
 	# Run `init()`, but decorate any exception that occurs with a description
 	# of the profile for this instance.  This allows the user to see which
 	# profile triggered the exception.
-	def init_with_decoration(self, substenv):
+	def init_with_decoration(self, substenv) -> str:
 		try:
-			self.init(substenv)
+			return self.init(substenv)
 		except Exception as e:
 			# Patch the exception's arguments instead of using Python exception
 			# chaining.  Using `raise Exception('') from e` causes SCons not to
@@ -5247,7 +5247,7 @@ class DXXProgram(DXXCommon):
 			e.args = (f'Failed to initialize profile {self._argument_prefix_list!r}{f": {a[0]}" if a else ""}',) + a[1:]
 			raise
 
-	def init(self,substenv):
+	def init(self,substenv) -> str:
 		user_settings = self.user_settings
 		user_settings.read_variables(self, self.variables, substenv)
 		archive = DXXProgram.static_archive_construction.get(user_settings.builddir, None)