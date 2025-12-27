dxx-rebirth/dxx-rebirth

commit 37cf0e729dae34b216822ac2cbeaf2c71305b25a

`texmerge_get_cached_bitmap`: pass GameBitmaps, Textures as context

common/main/texmerge.h
@@ -27,14 +27,23 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #ifndef _TEXMERGE_H
 #define _TEXMERGE_H
 
+#include "dsx-ns.h"
+#include "fwd-piggy.h"	// GameBitmaps_array
 #include "fwd-segment.h"
+#include "textures.h"	// Textures_array
 
 namespace dcx {
 struct grs_bitmap;
 }
 
-grs_bitmap &texmerge_get_cached_bitmap(texture1_value tmap_bottom, texture2_value tmap_top);
 void texmerge_close();
 void texmerge_flush();
+
+namespace dsx {
+
+[[nodiscard]]
+grs_bitmap &texmerge_get_cached_bitmap(GameBitmaps_array &GameBitmaps, const Textures_array &Textures, texture1_value tmap_bottom, texture2_value tmap_top);
+
+}
 
 #endif /* _TEXMERGE_H */

similar/arch/ogl/ogl.cpp
@@ -469,7 +469,7 @@ void ogl_cache_level_textures(void)
 					PIGGY_PAGE_IN(texture2);
 					auto &bm2 = GameBitmaps[texture2];
 					if (CGameArg.DbgUseOldTextureMerge || bm2.get_flag_mask(BM_FLAG_SUPER_TRANSPARENT))
-						bm = &texmerge_get_cached_bitmap( tmap1, tmap2 );
+						bm = &texmerge_get_cached_bitmap(GameBitmaps, Textures, tmap1, tmap2);
 					else {
 						ogl_loadbmtexture(bm2, 1);
 					}

similar/editor/eswitch.cpp
@@ -487,7 +487,7 @@ window_event_result trigger_dialog::callback_handler(const d_event &event)
 			auto &us = markedseg.u.sides[Markedside];
 			if (us.tmap_num2 != texture2_value::None) 
 			{
-				gr_ubitmap(canvas, texmerge_get_cached_bitmap(us.tmap_num, us.tmap_num2));
+				gr_ubitmap(canvas, texmerge_get_cached_bitmap(GameBitmaps, Textures, us.tmap_num, us.tmap_num2));
 			} else {
 				const auto tmap_num = us.tmap_num;
 				if (tmap_num != texture1_value::None)

similar/editor/medwall.cpp
@@ -621,7 +621,7 @@ window_event_result wall_dialog::callback_handler(const d_event &event)
 					auto &curside = Cursegp->unique_segment::sides[Curside];
 					const auto tmap_num = curside.tmap_num;
 					if (curside.tmap_num2 != texture2_value::None)
-						gr_ubitmap(*grd_curcanv, texmerge_get_cached_bitmap(tmap_num, curside.tmap_num2));
+						gr_ubitmap(*grd_curcanv, texmerge_get_cached_bitmap(GameBitmaps, Textures, tmap_num, curside.tmap_num2));
 					else	{
 						const auto texture1 = Textures[get_texture_index(tmap_num)];
 						PIGGY_PAGE_IN(texture1);

similar/main/fvi.cpp
@@ -1177,7 +1177,7 @@ int check_trans_wall(const vms_vector &pnt, const vcsegptridx_t seg, const siden
 
 	const auto tmap_num{side.tmap_num};
 	const grs_bitmap &rbm = (side.tmap_num2 != texture2_value::None)
-		? texmerge_get_cached_bitmap(tmap_num, side.tmap_num2)
+		? texmerge_get_cached_bitmap(GameBitmaps, Textures, tmap_num, side.tmap_num2)
 		/* gcc-13 issues a -Wdangling-reference warning if this lambda returns
 		 * a `const grs_bitmap &`, but no warning if the lambda returns a
 		 * `const grs_bitmap *` to the same storage, and then converts it into

similar/main/render.cpp
@@ -283,7 +283,7 @@ static void render_face(grs_canvas &canvas, const shared_segment &segp, const si
 			if (bm2->get_flag_mask(BM_FLAG_SUPER_TRANSPARENT))
 			{
 				bm2 = nullptr;
-			bm = &texmerge_get_cached_bitmap( tmap1, tmap2 );
+				bm = &texmerge_get_cached_bitmap(GameBitmaps, Textures, tmap1, tmap2);
 			}
 		}
 	}else
@@ -291,7 +291,7 @@ static void render_face(grs_canvas &canvas, const shared_segment &segp, const si
 		// New code for overlapping textures...
 		if (tmap2 != texture2_value::None)
 		{
-			bm = &texmerge_get_cached_bitmap( tmap1, tmap2 );
+			bm = &texmerge_get_cached_bitmap(GameBitmaps, Textures, tmap1, tmap2);
 		} else {
 			const auto texture1 = Textures[get_texture_index(tmap1)];
 			bm = &GameBitmaps[texture1];
@@ -395,7 +395,7 @@ static void check_face(grs_canvas &canvas, const vmsegidx_t segnum, const sidenu
 		(void)tmap2;
 #else
 		grs_bitmap *const bm = (tmap2 != texture2_value::None)
-			? &texmerge_get_cached_bitmap( tmap1, tmap2 )
+			? &texmerge_get_cached_bitmap(GameBitmaps, Textures, tmap1, tmap2)
 			: &GameBitmaps[Textures[get_texture_index(tmap1)]];
 #endif
 		range_for (const uint_fast32_t i, xrange(nv))

similar/main/texmerge.cpp
@@ -233,7 +233,9 @@ void texmerge_close()
 
 //--unused-- int info_printed = 0;
 
-grs_bitmap &texmerge_get_cached_bitmap(const texture1_value tmap_bottom, const texture2_value tmap_top)
+namespace dsx {
+
+grs_bitmap &texmerge_get_cached_bitmap(GameBitmaps_array &GameBitmaps, const Textures_array &Textures, const texture1_value tmap_bottom, const texture2_value tmap_top)
 {
 	const auto texture_top{Textures[get_texture_index(tmap_top)]};
 	const auto texture_bottom{Textures[get_texture_index(tmap_bottom)]};
@@ -297,4 +299,6 @@ grs_bitmap &texmerge_get_cached_bitmap(const texture1_value tmap_bottom, const t
 	least_recently_used->bitmap = std::move(merged_bitmap);
 	least_recently_used->last_time_used = timer_query();
 	return mb;
+}
+
 }

commit 476cf24c53dcc7e5bd13ff2d6a897637ebba6b38

`texmerge.h`: fix check_header_includes=1 build
Fixes: 37cf0e729dae34b216822ac2cbeaf2c71305b25a ("`texmerge_get_cached_bitmap`: pass GameBitmaps, Textures as context")

common/main/texmerge.h
@@ -40,11 +40,13 @@ void texmerge_flush();
 
 }
 
+#ifdef DXX_BUILD_DESCENT
 namespace dsx {
 
 [[nodiscard]]
 grs_bitmap &texmerge_get_cached_bitmap(GameBitmaps_array &GameBitmaps, const Textures_array &Textures, texture1_value tmap_bottom, texture2_value tmap_top);
 
 }
+#endif
 
 #endif /* _TEXMERGE_H */