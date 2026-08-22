dxx-rebirth/dxx-rebirth

commit a7cbf60922c7dcf4302d65b8eece7345835650fa

Return error code from PHYSFSRWOPS_openRead*
This improves error reporting for movies.

common/main/physfsrwops.h
@@ -37,6 +37,7 @@
 
 #ifdef __cplusplus
 #include <memory>
+#include <utility>
 
 struct RWops_delete
 {
@@ -58,8 +59,8 @@ typedef std::unique_ptr<SDL_RWops, RWops_delete> RWops_ptr;
  *  @return A valid SDL_RWops structure on success, NULL on error. Specifics
  *           of the error can be gleaned from PHYSFS_getLastError().
  */
-RWops_ptr PHYSFSRWOPS_openRead(const char *fname);
-RWops_ptr PHYSFSRWOPS_openReadBuffered(const char *fname, PHYSFS_uint64);
+std::pair<RWops_ptr, PHYSFS_ErrorCode> PHYSFSRWOPS_openRead(const char *fname);
+std::pair<RWops_ptr, PHYSFS_ErrorCode> PHYSFSRWOPS_openReadBuffered(const char *fname, PHYSFS_uint64);
 
 #endif
 

common/misc/physfsrwops.cpp
@@ -155,12 +155,13 @@ static int physfsrwops_close(SDL_RWops *rw)
 } /* physfsrwops_close */
 
 
-static RWops_ptr create_rwops(RAIIPHYSFS_File handle)
+static std::pair<RWops_ptr, PHYSFS_ErrorCode> create_rwops(RAIIPHYSFS_File handle)
 {
     if (!handle)
 	{
-        SDL_SetError("PhysicsFS error: %s", PHYSFS_getLastError());
-		return nullptr;
+		const auto err = PHYSFS_getLastErrorCode();
+        SDL_SetError("PhysicsFS error: %s", PHYSFS_getErrorByCode(err));
+		return {nullptr, err};
 	}
     else
     {
@@ -173,16 +174,18 @@ static RWops_ptr create_rwops(RAIIPHYSFS_File handle)
             retval->close = physfsrwops_close;
             retval->hidden.unknown.data1 = handle.release();
         } /* if */
-		return retval;
+		else
+			return {nullptr, PHYSFS_ERR_OTHER_ERROR};
+		return {std::move(retval), PHYSFS_ERR_OK};
     } /* else */
 } /* create_rwops */
 
-RWops_ptr PHYSFSRWOPS_openRead(const char *fname)
+std::pair<RWops_ptr, PHYSFS_ErrorCode> PHYSFSRWOPS_openRead(const char *fname)
 {
     return(create_rwops(RAIIPHYSFS_File{PHYSFS_openRead(fname)}));
 } /* PHYSFSRWOPS_openRead */
 
-RWops_ptr PHYSFSRWOPS_openReadBuffered(const char *fname, const PHYSFS_uint64 bufferSize)
+std::pair<RWops_ptr, PHYSFS_ErrorCode> PHYSFSRWOPS_openReadBuffered(const char *fname, const PHYSFS_uint64 bufferSize)
 {
 	RAIIPHYSFS_File fp{PHYSFS_openRead(fname)};
 	PHYSFS_setBuffer(fp, bufferSize);

d2x-rebirth/main/movie.cpp
@@ -374,10 +374,10 @@ movie_play_status RunMovie(const char *const filename, const char *const subtitl
 
 	// Open Movie file.  If it doesn't exist, no movie, just return.
 
-	auto filehndl = PHYSFSRWOPS_openRead(filename);
+	auto &&[filehndl, physfserr] = PHYSFSRWOPS_openRead(filename);
 	if (!filehndl)
 	{
-		con_printf(must_have ? CON_URGENT : CON_VERBOSE, "Failed to open movie <%s>: %s", filename, PHYSFS_getLastError());
+		con_printf(must_have ? CON_URGENT : CON_VERBOSE, "Failed to open movie <%s>: %s", filename, PHYSFS_getErrorByCode(physfserr));
 		return movie_play_status::skipped;
 	}
 	MVESTREAM_ptr_t mvestream;
@@ -461,11 +461,11 @@ int InitRobotMovie(const char *filename, MVESTREAM_ptr_t &pMovie)
 
 	MVE_sndInit(-1);        //tell movies to play no sound for robots
 
-	RoboFile = PHYSFSRWOPS_openRead(filename);
-
+	auto &&[filehndl, physfserr] = PHYSFSRWOPS_openRead(filename);
+	RoboFile = std::move(filehndl);
 	if (!RoboFile)
 	{
-		con_printf(CON_URGENT, "Can't open movie <%s>: %s", filename, PHYSFS_getLastError());
+		con_printf(CON_URGENT, "Failed to open movie <%s>: %s", filename, PHYSFS_getErrorByCode(physfserr));
 		return 0;
 	}
 	if (MVE_rmPrepMovie(pMovie, RoboFile.get(), SWIDTH/2.3, SHEIGHT/2.3, 0)) {

similar/2d/pcx.cpp
@@ -243,10 +243,10 @@ pcx_result pcx_read_bitmap(const char *const filename, grs_main_bitmap &bmp, pal
 	 * for users to detect the latency and report it as an issue.  On
 	 * Linux, unbuffered access is still fast.
 	 */
-	auto rw = PHYSFSRWOPS_openReadBuffered(filename, 1024 * 1024);
+	auto &&[rw, physfserr] = PHYSFSRWOPS_openReadBuffered(filename, 1024 * 1024);
 	if (!rw)
 	{
-		con_printf(CON_NORMAL, "%s:%u: failed to open \"%s\"", __FILE__, __LINE__, filename);
+		con_printf(CON_NORMAL, "%s:%u: failed to open \"%s\": %s", __FILE__, __LINE__, filename, PHYSFS_getErrorByCode(physfserr));
 		return pcx_result::ERROR_OPENING;
 	}
 	return pcx_read_bitmap(filename, bmp, palette, std::move(rw));

commit 6060cd80430c6d914725e8c93e4d9d80c60b17ef

`PHYSFSRWOPS_openRead`: update comment describing return value
Fixes: a7cbf60922c7dcf4302d65b8eece7345835650fa ("Return error code from PHYSFSRWOPS_openRead*")

common/main/physfsrwops.h
@@ -35,7 +35,6 @@
 #endif
 #include <SDL.h>
 
-#ifdef __cplusplus
 #include <memory>
 #include <utility>
 
@@ -56,12 +55,9 @@ typedef std::unique_ptr<SDL_RWops, RWops_delete> RWops_ptr;
  *  opening files through this method.
  *
  *   @param filename File to open in platform-independent notation.
- *  @return A valid SDL_RWops structure on success, NULL on error. Specifics
- *           of the error can be gleaned from PHYSFS_getLastError().
+ *  @return A pair containing: (1) valid SDL_RWops structure on success or nullptr on error and (2) PHYSFS_ERR_OK on success or a specific error code on error.
  */
 std::pair<RWops_ptr, PHYSFS_ErrorCode> PHYSFSRWOPS_openRead(const char *fname);
-
-#endif
 
 /* end of physfsrwops.h ... */
 